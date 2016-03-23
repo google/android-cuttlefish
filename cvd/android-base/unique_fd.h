453413390877a538ee3b6f7664c3dd6a12f37bae
Elliott Hughes <enh@google.com>
2016-03-23 15:04:52 -0700

    Fix <android-base/*.h> header guards.
    I've been meaning to do this for a while, and it came up on the bug below
    that there have been conflicts with similar-named files in <base/*.h>,
    so let's rule out one possible explanation.
    
    Bug: http://b/27804373
    Change-Id: I69e5d52b6260c573c308513420aee0e281426bd4

--- cvd/android-base/unique_fd.h
+++ cvd/android-base/unique_fd.h
@@ -21,18 +21,18 @@
 
 #include <android-base/macros.h>
 
-// Container for a file descriptor that automatically closes the descriptor as
-// it goes out of scope.
-//
-//      unique_fd ufd(open("/some/path", "r"));
-//      if (ufd.get() == -1) return error;
-//
-//      // Do something useful, possibly including 'return'.
-//
-//      return 0; // Descriptor is closed for you.
-//
-// unique_fd is also known as ScopedFd/ScopedFD/scoped_fd; mentioned here to help
-// you find this class if you're searching for one of those names.
+/* Container for a file descriptor that automatically closes the descriptor as
+ * it goes out of scope.
+ *
+ *      unique_fd ufd(open("/some/path", "r"));
+ *
+ *      if (ufd.get() < 0) // invalid descriptor
+ *          return error;
+ *
+ *      // Do something useful
+ *
+ *      return 0; // descriptor is closed here
+ */
 namespace android {
 namespace base {
 
@@ -44,18 +44,14 @@ class unique_fd final {
   ~unique_fd() { clear(); }
 
   unique_fd(unique_fd&& other) : value_(other.release()) {}
-  unique_fd& operator=(unique_fd&& s) {
+  unique_fd& operator = (unique_fd&& s) {
     reset(s.release());
     return *this;
   }
 
   void reset(int new_value) {
-    if (value_ != -1) {
-      // Even if close(2) fails with EINTR, the fd will have been closed.
-      // Using TEMP_FAILURE_RETRY will either lead to EBADF or closing someone else's fd.
-      // http://lkml.indiana.edu/hypermail/linux/kernel/0509.1/0877.html
+    if (value_ >= 0)
       close(value_);
-    }
     value_ = new_value;
   }
 
@@ -65,7 +61,7 @@ class unique_fd final {
 
   int get() const { return value_; }
 
-  int release() __attribute__((warn_unused_result)) {
+  int release() {
     int ret = value_;
     value_ = -1;
     return ret;
@@ -80,4 +76,4 @@ class unique_fd final {
 }  // namespace base
 }  // namespace android
 
-#endif // ANDROID_BASE_UNIQUE_FD_H
+#endif  // ANDROID_BASE_UNIQUE_FD_H
