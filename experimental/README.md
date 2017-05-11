aaea5c36ee08283c518170d72b9857a50aafc398
Tomasz Wiszkowski <ender@google.com>
2017-05-11 10:03:26 -0700

    Set of scripts for experiments with Nested AVD.
    Please look up the README.md file under experimental/ folder.

---
+++ experimental/README.md
@@ -0,0 +1,29 @@
+### Overview
+
+**This is an experimental folder with just basic stuff to get everyone going.**
+
+To get started
+  - create a folder with your binary files, say 'android',
+  - to this folder copy or link the following files:
+    - android_system_disk_syslinux.img
+    - kernel
+    - gce_ramdisk.img
+    - ramdisk.img
+    - system.img
+    - userdata.img
+
+Use the 'start' script as a super-user. This tool will configure bridge and
+start qemu for you in a screen:
+
+    sudo ./start
+
+### Useful info:
+
+  - Metadata attributes sit in `etc/avd/metadata.json` file.
+  - Your AVD will be assigned an IP in range 192.168.99.10 - 192.168.99.13.
+  - Network interface to QEMU will be named 'android0'. Bridge is 'abr0'.
+  - Metadata is served from 192.168.99.1, port 16925 (notice subtle
+    difference!).
+
+Good luck, have fun!
+ender
---
+++ experimental/etc/avd/android-start-qemu.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+function die {
+  echo "$@"
+  sleep 10
+  exit
+}
+
+SCRIPT_DIR=$(dirname "$0")
+
+if [ "$#" -ge "0" ]; then
+  IMAGE_DIR=${1}
+else
+  IMAGE_DIR="."
+fi
+
+[ "$EUID" -ne 0 ] && die "Must run as super-user."
+[ ! -e ${IMAGE_DIR}/kernel ] && die "Need kernel."
+[ ! -e ${IMAGE_DIR}/gce_ramdisk.img ] && die "Need GCE ramdisk."
+[ ! -e ${IMAGE_DIR}/system.img ] && die "Need system.img."
+[ ! -e ${IMAGE_DIR}/userdata.img ] && die "Need userdata.img."
+
+if [ ! -e ${IMAGE_DIR}/data.img ]; then
+  truncate -s 2G ${IMAGE_DIR}/data.img
+  mkfs.ext4 -F ${IMAGE_DIR}/data.img
+fi
+
+qemu-system-x86_64 \
+  -kernel ${IMAGE_DIR}/kernel \
+  -initrd ${IMAGE_DIR}/gce_ramdisk.img \
+  -serial tcp::4000,server \
+  -serial tcp::4001,server \
+  -nographic \
+  -hda ${IMAGE_DIR}/android_system_disk_syslinux.img \
+  -smp 4 \
+  -m 2048 \
+  -enable-kvm \
+  -netdev type=tap,id=net0,ifname=android0,script=${SCRIPT_DIR}/android-ifup,downscript=${SCRIPT_DIR}/android-ifdown \
+  -device e1000,netdev=net0 \
+  -drive file=${IMAGE_DIR}/ramdisk.img,index=0,if=virtio,media=disk \
+  -drive file=${IMAGE_DIR}/system.img,index=1,if=virtio,media=disk \
+  -drive file=${IMAGE_DIR}/userdata.img,index=2,if=virtio,media=disk \
+  -drive file=${IMAGE_DIR}/data.img,index=3,if=virtio,media=disk \
+  -append "root=/dev/sda1 init=/gce_init console=ttyS0 init=/init androidboot.hardware=gce_x86 androidboot.console=ttyS0 security=selinux androidboot.selinux=permissive enforcing=0 loop.max_part=7 QEMU"
