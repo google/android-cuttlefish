e05834b43f192bcc67646fdb983e6bf6c6826683
Greg Hartman <ghartman@google.com>
2017-06-19 09:50:07 -0700

    Add vendor_setup.sh for cuttlefish
    Change-Id: I9faacaf9270e9a94d6e2cb99fdaf0177f91f90bc
    (cherry picked from commit 606b4b99ce10af6702735a1fa0df2c0662bac3ad)

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -26,3 +26,15 @@ add_lunch_combo aosp_cf_x86_64_tablet-userdebug
 add_lunch_combo aosp_cf_x86_64_tablet_3g-userdebug
 add_lunch_combo aosp_cf_x86_64_tv-userdebug
 add_lunch_combo aosp_cf_x86_64_wear-userdebug
+add_lunch_combo cf_x86_auto-userdebug
+add_lunch_combo cf_x86_phone-userdebug
+add_lunch_combo cf_x86_tablet-userdebug
+add_lunch_combo cf_x86_tablet_3g-userdebug
+add_lunch_combo cf_x86_tv-userdebug
+add_lunch_combo cf_x86_wear-userdebug
+add_lunch_combo cf_x86_64_auto-userdebug
+add_lunch_combo cf_x86_64_phone-userdebug
+add_lunch_combo cf_x86_64_tablet-userdebug
+add_lunch_combo cf_x86_64_tablet_3g-userdebug
+add_lunch_combo cf_x86_64_tv-userdebug
+add_lunch_combo cf_x86_64_wear-userdebug
