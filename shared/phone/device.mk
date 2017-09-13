2cbe06bffea3a4c6c5a859477af751b9457cf8c4
Tomasz Wiszkowski <ender@google.com>
2017-09-13 09:02:20 -0700

    Drop hardcoded obsolete dhcp properties.
    These properties are no longer relevant or accurate.
    
    Change-Id: I5e63c0e33e91fc4ad47147a0d2efa3d094b015f1

--- shared/phone/device.mk
+++ shared/phone/device.mk
@@ -21,9 +21,6 @@ $(call inherit-product, device/google/cuttlefish/shared/device.mk)
 PRODUCT_CHARACTERISTICS := nosdcard
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    dhcp.rmnet0.dns1=8.8.8.8 \
-    dhcp.rmnet0.gateway=192.168.1.1 \
-    dhcp.rmnet0.ipaddress=192.168.1.10 \
     gsm.sim.operator.alpha=Android \
     gsm.sim.operator.iso-country=us \
     gsm.sim.operator.numeric=310260 \
@@ -42,8 +39,6 @@ PRODUCT_PACKAGES += \
     Telecom \
     TeleService \
     libvsoc-ril \
-    libvsoc-ril-deprecated \
-    libvsoc-ril-testing \
     rild \
 
 PRODUCT_COPY_FILES += \
