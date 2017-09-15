4dc2cfc6a6a7637135d542a5e6feeb85912eacf5
Tomasz Wiszkowski <ender@google.com>
2017-09-15 14:31:53 -0700

    Add support for epoll.
    epoll, unlike select, allows us to detect halfway closed connections.
    
    BUG=65160456
    Change-Id: I3780611dcb46218663c21a275cf0fd18192003d8

--- cvd/common/libs/fs/shared_fd.cpp
+++ cvd/common/libs/fs/shared_fd.cpp
@@ -230,6 +230,11 @@ SharedFD SharedFD::Event(int initval, int flags) {
       new FileInstance(eventfd(initval, flags), errno));
 }
 
+SharedFD SharedFD::Epoll(int flags) {
+  return std::shared_ptr<FileInstance>(
+      new FileInstance(epoll_create1(flags), errno));
+}
+
 inline bool SharedFD::SocketPair(int domain, int type, int protocol,
                                  SharedFD* fd0, SharedFD* fd1) {
   int fds[2];
