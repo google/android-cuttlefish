e6d90153f3b8dc691f153065acf5ff8ab65431fa
Tomasz Wiszkowski <ender@google.com>
2017-09-21 10:22:53 -0700

    Disable ramdisk builds and tests.
    now that gce_ramdisk is pretty much gone, time has come to rip out
    metadata and services that depend on it.
    I was hoping to keep these around for a bit longer, but it simply
    doesn't make sense.
    
    Change-Id: I5f1792dafa34b2520af5c4bc45e798f51e39cb4e
    (cherry picked from commit dd42361577d0943a1bf744eada29e3e64a4635d5)

--- vadb/usb_cmd_data_transfer.h
+++ vadb/usb_cmd_data_transfer.h
@@ -18,7 +18,7 @@
 #include <memory>
 #include <stdint.h>
 
-#include "guest/usbforward/protocol.h"
+#include "guest/commands/usbforward/protocol.h"
 #include "host/vadb/usb_cmd.h"
 #include "host/vadb/usbip/device.h"
 
