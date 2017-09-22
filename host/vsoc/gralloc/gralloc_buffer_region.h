0b2497345da159e43599a69137798401deed09a8
Jorge E. Moreira <jemoreira@google.com>
2017-09-22 10:28:02 -0700

    Added host side gralloc region class.
    Host and guest side gralloc regions are different because on the
    guest it needs to meet security constraints, talk to the vsoc
    driver, while in host those are irrelevant. Also, by design, there
    is no support for gralloc buffer allocation host side.
    
    Bug: 64158954
    Change-Id: I776f9df17110e1c373a1c2bddadc72476ab85c73

---
+++ host/vsoc/gralloc/gralloc_buffer_region.h
@@ -0,0 +1,47 @@
+#pragma once
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "common/vsoc/lib/typed_region_view.h"
+#include "common/vsoc/shm/gralloc_layout.h"
+#include "uapi/vsoc_shm.h"
+
+#include <string>
+
+namespace vsoc {
+namespace gralloc {
+
+// Allows access to the gralloc buffer region from host side. It needs to be a
+// different class than the one on guest side because of the required
+// interactions with the kernel on the guest.
+// Initially this class only returns a pointer to a buffer in memory given a
+// region offset, which is enough for now since it's only used by the hwcomposer
+// (which gets all other information from the guest side hwcomposer) and by the
+// VNC server (which uses only the frame buffer and gets the information it
+// needs from the framebuffer region).
+class GrallocBufferRegion
+    : vsoc::TypedRegionView<vsoc::layout::gralloc::GrallocBufferLayout> {
+ public:
+  static GrallocBufferRegion* GetInstance();
+
+  uint8_t* OffsetToBufferPtr(vsoc_reg_off_t offset);
+ protected:
+  GrallocBufferRegion(char* domain);
+  bool is_open_{};
+};
+
+}
+}
