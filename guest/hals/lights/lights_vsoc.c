dd809b7ccc655c637af1d2571fd65aa0cd2d7ab9
Tomasz Wiszkowski <ender@google.com>
2017-09-26 12:31:40 -0700

    Minor adjustment to vsoc lights hal:
    - update build targets,
    - fix style issues by running clang-format --style=google
    - use api_level_fixes to fix static initialization warnings,
    - rename lights_gce.c to lights_vsoc.c
    
    Change-Id: I7494b9de3a2ef3610d5839aad37814dd1bc8f3f1

--- guest/hals/lights/lights_vsoc.c
+++ guest/hals/lights/lights_vsoc.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 The Android Open Source Project
+ * Copyright (C) 2017 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,35 +13,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/* This implements a lights hardware library for the Android emulator.
- * the following code should be built as a shared library that will be
- * placed into /system/lib/hw/lights.goldfish.so
- *
- * It will be loaded by the code in hardware/libhardware/hardware.c
- * which is itself called from
- * ./frameworks/base/services/jni/com_android_server_HardwareService.cpp
- */
-
-#ifdef LOG_TAG
-#undef LOG_TAG
-#endif
-#define LOG_TAG "GceLights"
-
 #include <cutils/log.h>
-#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <hardware/lights.h>
+#include <pthread.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <pthread.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
-#include <hardware/lights.h>
+#include <unistd.h>
 
-static int set_light(
-    struct light_device_t* dev, struct light_state_t const* state) {
+#include "guest/libs/platform_support/api_level_fixes.h"
+
+static int set_light(struct light_device_t* dev,
+                     struct light_state_t const* state) {
   ALOGI("%s: dev %p state %p", __FUNCTION__, dev, state);
   if (state) {
     ALOGI("%s: state value %d\n", __FUNCTION__, state->color);
@@ -50,18 +37,17 @@ static int set_light(
 }
 
 static int close_lights(struct light_device_t* dev) {
-  free( dev );
+  free(dev);
   return 0;
 }
 
-static int open_lights(
-    const struct hw_module_t* module, char const *name,
-    struct hw_device_t **device)  {
-  struct light_device_t *dev = malloc( sizeof(struct light_device_t) );
+static int open_lights(const struct hw_module_t* module,
+                       char const* name __unused, struct hw_device_t** device) {
+  struct light_device_t* dev = malloc(sizeof(struct light_device_t));
   if (dev == NULL) {
     return -EINVAL;
   }
-  memset( dev, 0, sizeof(*dev) );
+  memset(dev, 0, sizeof(*dev));
 
   dev->common.tag = HARDWARE_DEVICE_TAG;
   dev->common.version = 0;
@@ -73,15 +59,15 @@ static int open_lights(
 }
 
 static struct hw_module_methods_t lights_module_methods = {
-    .open =  open_lights,
+    VSOC_STATIC_INITIALIZER(open) open_lights,
 };
 
 struct hw_module_t HAL_MODULE_INFO_SYM = {
-    .tag = HARDWARE_MODULE_TAG,
-    .version_major = 1,
-    .version_minor = 0,
-    .id = LIGHTS_HARDWARE_MODULE_ID,
-    .name = "Android GCE lights Module",
-    .author = "Google",
-    .methods = &lights_module_methods,
+    VSOC_STATIC_INITIALIZER(tag) HARDWARE_MODULE_TAG,
+    VSOC_STATIC_INITIALIZER(version_major) 1,
+    VSOC_STATIC_INITIALIZER(version_minor) 0,
+    VSOC_STATIC_INITIALIZER(id) LIGHTS_HARDWARE_MODULE_ID,
+    VSOC_STATIC_INITIALIZER(name) "Android GCE lights Module",
+    VSOC_STATIC_INITIALIZER(author) "Google",
+    VSOC_STATIC_INITIALIZER(methods) & lights_module_methods,
 };
