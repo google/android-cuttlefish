117a30795ab5fd56aeecc5f0f12599c24d3c7a81
Ping-Hao Wu <pinghao@google.com>
2017-11-03 15:29:50 -0700

    Include additional camera features
    CTS SystemFeaturesTest#testCameraFeatures checks for these features:
    FEATURE_CAMERA_LEVEL_FULL
    FEATURE_CAMERA_CAPABILITY_MANUAL_SENSOR
    FEATURE_CAMERA_CAPABILITY_MANUAL_POST_PROCESSING
    FEATURE_CAMERA_CAPABILITY_RAW
    
    Bug: 64391899
    Bug: 68802239
    Test: Pass android.app.cts.SystemFeaturesTest#testCameraFeatures
    Change-Id: I7eb600286b5c3727e999b2b172a508f9b2baee12

--- shared/device.mk
+++ shared/device.mk
@@ -117,7 +117,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/native/data/etc/android.hardware.camera.full.xml:system/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.camera.raw.xml:system/etc/permissions/android.hardware.camera.raw.xml \
     frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
