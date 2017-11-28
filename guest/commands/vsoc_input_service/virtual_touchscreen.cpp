ae2fcae3ef4ff8f2c205a76b685916199fc4b2d0
Jorge E. Moreira <jemoreira@google.com>
2017-11-28 11:16:59 -0800

    VSoC input service
    Adds a service to android that waits for input events on each device's
    queue and forwards them to the appropriate virtual device in the
    kernel. It also takes care of configuring said virtual devices.
    
    Test: run locally
    Bug: 67895611
    Change-Id: I4b8fa1c9af6b05820ae5f97d8cace06af699d48d

---
+++ guest/commands/vsoc_input_service/virtual_touchscreen.cpp
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "virtual_touchscreen.h"
+
+namespace vsoc_input_service {
+
+const std::vector<const uint32_t>& VirtualTouchScreen::GetEventTypes() const {
+  static const std::vector<const uint32_t> evt_types{EV_ABS, EV_KEY};
+  return evt_types;
+}
+const std::vector<const uint32_t>& VirtualTouchScreen::GetKeys() const {
+  static const std::vector<const uint32_t> keys{BTN_TOUCH};
+  return keys;
+}
+const std::vector<const uint32_t>& VirtualTouchScreen::GetProperties() const {
+  static const std::vector<const uint32_t> properties{INPUT_PROP_DIRECT};
+  return properties;
+}
+const std::vector<const uint32_t>& VirtualTouchScreen::GetAbs() const {
+  static const std::vector<const uint32_t> abs{ABS_X, ABS_Y};
+  return abs;
+}
+
+VirtualTouchScreen::VirtualTouchScreen(uint32_t width, uint32_t height)
+    : VirtualDeviceBase("VSoC touchscreen", 0x6006) {
+  dev_.absmin[ABS_X] = 0;
+  dev_.absmax[ABS_X] = width;
+  dev_.absmin[ABS_Y] = 0;
+  dev_.absmax[ABS_Y] = height;
+}
+
+}  // namespace vsoc_input_service
