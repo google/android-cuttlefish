e66d287330ca1791263731bbc7d40f0051d3e94c
Greg Hartman <ghartman@google.com>
2017-12-12 20:30:37 +0000

    Send SharedFD debug info to the log am: 67fa8587cd am: e0fe1cd689
    Change-Id: I90fb17088de115f49d3787d235965cd5dec82b9e

--- cvd/common/libs/fs/shared_fd.cpp
+++ cvd/common/libs/fs/shared_fd.cpp
@@ -30,7 +30,7 @@
 // #define ENABLE_GCE_SHARED_FD_LOGGING 1
 
 namespace {
-using avd::SharedFDSet;
+using cvd::SharedFDSet;
 
 void MarkAll(const SharedFDSet& input, fd_set* dest, int* max_index) {
   for (SharedFDSet::const_iterator it = input.begin(); it != input.end();
@@ -39,17 +39,13 @@ void MarkAll(const SharedFDSet& input, fd_set* dest, int* max_index) {
   }
 }
 
-void CheckMarked(fd_set* in_out_mask, fd_set* error_mask,
-                 SharedFDSet* in_out_set, SharedFDSet* error_set) {
+void CheckMarked(fd_set* in_out_mask, SharedFDSet* in_out_set) {
   if (!in_out_set) {
     return;
   }
   SharedFDSet save;
   save.swap(in_out_set);
   for (SharedFDSet::iterator it = save.begin(); it != save.end(); ++it) {
-    if (error_set && (*it)->IsSet(error_mask)) {
-      error_set->Set(*it);
-    }
     if ((*it)->IsSet(in_out_mask)) {
       in_out_set->Set(*it);
     }
@@ -57,7 +53,7 @@ void CheckMarked(fd_set* in_out_mask, fd_set* error_mask,
 }
 }  // namespace
 
-namespace avd {
+namespace cvd {
 
 bool FileInstance::CopyFrom(FileInstance& in) {
   AutoFreeBuffer buffer;
@@ -147,14 +143,16 @@ int Select(SharedFDSet* read_set, SharedFDSet* write_set,
   }
   fd_set errorfds;
   FD_ZERO(&errorfds);
+  if (error_set) {
+    MarkAll(*error_set, &errorfds, &max_index);
+  }
+
   int rval = TEMP_FAILURE_RETRY(
       select(max_index, &readfds, &writefds, &errorfds, timeout));
   FileInstance::Log("select\n");
-  if (error_set) {
-    error_set->Zero();
-  }
-  CheckMarked(&readfds, &errorfds, read_set, error_set);
-  CheckMarked(&writefds, &errorfds, write_set, error_set);
+  CheckMarked(&readfds, read_set);
+  CheckMarked(&writefds, write_set);
+  CheckMarked(&errorfds, error_set);
   return rval;
 }
 
@@ -192,6 +190,15 @@ SharedFD SharedFD::SocketSeqPacketClient(const char* name) {
   return SocketLocalClient(name, false, SOCK_SEQPACKET);
 }
 
+SharedFD SharedFD::TimerFD(int clock, int flags) {
+  int fd = timerfd_create(clock, flags);
+  if (fd == -1) {
+    return SharedFD(std::shared_ptr<FileInstance>(new FileInstance(fd, errno)));
+  } else {
+    return SharedFD(std::shared_ptr<FileInstance>(new FileInstance(fd, 0)));
+  }
+}
+
 SharedFD SharedFD::Accept(const FileInstance& listener, struct sockaddr* addr,
                           socklen_t* addrlen) {
   return SharedFD(
@@ -221,6 +228,11 @@ bool SharedFD::Pipe(SharedFD* fd0, SharedFD* fd1) {
 SharedFD SharedFD::Event(int initval, int flags) {
   return std::shared_ptr<FileInstance>(
       new FileInstance(eventfd(initval, flags), errno));
+}
+
+SharedFD SharedFD::Epoll(int flags) {
+  return std::shared_ptr<FileInstance>(
+      new FileInstance(epoll_create1(flags), errno));
 }
 
 inline bool SharedFD::SocketPair(int domain, int type, int protocol,
@@ -253,47 +265,6 @@ SharedFD SharedFD::Socket(int domain, int socket_type, int protocol) {
   }
 }
 
-SharedFD SharedFD::SocketInAddrAnyServer(int in_port, int in_type) {
-  errno = 0;
-
-  // TODO(ender): this code is very similar to SocketSeqPacketServer
-  struct sockaddr_in6 addr = {
-      AF_INET6,        // sin6_family
-      htons(in_port),  // sin6_port
-      0,               // sin6_flowinfo
-      in6addr_any,     // sin6_addr
-      0,               // sin6_scope_id
-  };
-
-  SharedFD rval = SharedFD::Socket(PF_INET6, in_type, 0);
-  if (!rval->IsOpen()) {
-    return rval;
-  }
-
-  int n = 1;
-  if (rval->SetSockOpt(SOL_SOCKET, SO_REUSEADDR, &n, sizeof(n)) == -1) {
-    LOG(ERROR) << "SetSockOpt failed " << rval->StrError();
-    return SharedFD(
-        std::shared_ptr<FileInstance>(new FileInstance(-1, rval->GetErrno())));
-  }
-  if (rval->Bind((struct sockaddr*)&addr, sizeof(addr)) == -1) {
-    LOG(ERROR) << "Bind failed; port=" << in_port << ": " << rval->StrError();
-    return SharedFD(
-        std::shared_ptr<FileInstance>(new FileInstance(-1, rval->GetErrno())));
-  }
-
-  if (in_type == SOCK_STREAM) {
-    // Follows the default from socket_local_server
-    if (rval->Listen(1) == -1) {
-      LOG(ERROR) << "Listen failed: " << rval->StrError();
-      return SharedFD(std::shared_ptr<FileInstance>(
-          new FileInstance(-1, rval->GetErrno())));
-    }
-  }
-
-  return rval;
-}
-
 SharedFD SharedFD::SocketLocalClient(const char* name, bool abstract,
                                      int in_type) {
   struct sockaddr_un addr;
@@ -307,6 +278,37 @@ SharedFD SharedFD::SocketLocalClient(const char* name, bool abstract,
     LOG(ERROR) << "Connect failed; name=" << name << ": " << rval->StrError();
     return SharedFD(
         std::shared_ptr<FileInstance>(new FileInstance(-1, rval->GetErrno())));
+  }
+  return rval;
+}
+
+SharedFD SharedFD::SocketLocalServer(int port, int type) {
+  struct sockaddr_in addr;
+  memset(&addr, 0, sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(port);
+  addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+  SharedFD rval = SharedFD::Socket(AF_INET, type, 0);
+  if(!rval->IsOpen()) {
+    return rval;
+  }
+  int n = 1;
+  if (rval->SetSockOpt(SOL_SOCKET, SO_REUSEADDR, &n, sizeof(n)) == -1) {
+    LOG(ERROR) << "SetSockOpt failed " << rval->StrError();
+    return SharedFD(
+        std::shared_ptr<FileInstance>(new FileInstance(-1, rval->GetErrno())));
+  }
+  if(rval->Bind((struct sockaddr *) &addr, sizeof(addr)) < 0) {
+    LOG(ERROR) << "Bind failed " << rval->StrError();
+    return SharedFD(
+        std::shared_ptr<FileInstance>(new FileInstance(-1, rval->GetErrno())));
+  }
+  if (type == SOCK_STREAM) {
+    if (rval->Listen(4) < 0) {
+      LOG(ERROR) << "Listen failed " << rval->StrError();
+      return SharedFD(std::shared_ptr<FileInstance>(
+          new FileInstance(-1, rval->GetErrno())));
+    }
   }
   return rval;
 }
@@ -359,4 +361,4 @@ SharedFD SharedFD::SocketLocalServer(const char* name, bool abstract,
   return rval;
 }
 
-}  // namespace avd
+}  // namespace cvd
