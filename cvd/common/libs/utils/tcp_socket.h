a5facf9e019e8e4f2bbb2f8ed3c0a4f1813e0932
Greg Hartman <ghartman@google.com>
2017-12-12 20:38:56 +0000

    Ran clang-format over vnc_server code am: 7075378988
    Change-Id: Iaab8a41c26161f2e74f57e6845a606b9640aba18

--- cvd/common/libs/utils/tcp_socket.h
+++ cvd/common/libs/utils/tcp_socket.h
@@ -1,30 +1,15 @@
-#pragma once
+#ifndef DEVICE_GOOGLE_GCE_GCE_UTILS_GCE_VNC_SERVER_TCPSOCKET_H_
+#define DEVICE_GOOGLE_GCE_GCE_UTILS_GCE_VNC_SERVER_TCPSOCKET_H_
 
-/*
- * Copyright (C) 2017 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-#include "host/frontend/vnc_server/vnc_utils.h"
-
-#include <unistd.h>
+#include "vnc_utils.h"
 
 #include <cstddef>
 #include <cstdint>
 #include <mutex>
 
-namespace cvd {
+#include <unistd.h>
+
+namespace avd {
 namespace vnc {
 
 class ServerSocket;
@@ -33,15 +18,25 @@ class ServerSocket;
 // Send is thread safe in this regard, Recv is not.
 class ClientSocket {
  public:
-  ClientSocket(ClientSocket&& other) : fd_{other.fd_} {}
+  ClientSocket(ClientSocket&& other) : fd_{other.fd_} { other.fd_ = -1; }
 
   ClientSocket& operator=(ClientSocket&& other) {
+    if (fd_ >= 0) {
+      close(fd_);
+    }
     fd_ = other.fd_;
+    other.fd_ = -1;
     return *this;
   }
 
   ClientSocket(const ClientSocket&) = delete;
   ClientSocket& operator=(const ClientSocket&) = delete;
+
+  ~ClientSocket() {
+    if (fd_ >= 0) {
+      close(fd_);
+    }
+  }
 
   Message Recv(std::size_t length);
   ssize_t Send(const std::uint8_t* data, std::size_t size);
@@ -56,9 +51,9 @@ class ClientSocket {
 
  private:
   friend ServerSocket;
-  explicit ClientSocket(cvd::SharedFD fd) : fd_(fd) {}
+  explicit ClientSocket(int fd) : fd_(fd) {}
 
-  cvd::SharedFD fd_;
+  int fd_ = -1;
   bool other_side_closed_{};
   std::mutex send_lock_;
 };
@@ -70,11 +65,19 @@ class ServerSocket {
   ServerSocket(const ServerSocket&) = delete;
   ServerSocket& operator=(const ServerSocket&) = delete;
 
+  ~ServerSocket() {
+    if (fd_ >= 0) {
+      close(fd_);
+    }
+  }
+
   ClientSocket Accept();
 
  private:
-  cvd::SharedFD fd_;
+  int fd_ = -1;
 };
 
 }  // namespace vnc
-}  // namespace cvd
+}  // namespace avd
+
+#endif
