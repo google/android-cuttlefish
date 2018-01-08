0f709d10b32bd3c20b5f53e5e9701dd8028df2e5
Steven Moreland <smoreland@google.com>
2018-01-08 12:49:08 -0800

    Audio HALs: use binderized service
    Bug: 71707530
    Test: boot, lshal
    Change-Id: Ic7de9a4d4b525185ebcb3d685d4136e8dce4fbba
    Merged-In: Ic7de9a4d4b525185ebcb3d685d4136e8dce4fbba
    (cherry picked from commit 40a1dd44d8422be888368a93a9e09ad876292842)
    (cherry picked from commit a3fbf834f021ef994dba6a93158d7634ff2a3d3f)

--- shared/device.mk
+++ shared/device.mk
@@ -172,7 +172,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     audio.primary.vsoc \
     android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio@2.0-service
 
 #
 # Drm HAL
