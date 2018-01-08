9ef5ec522327f47cd4b9646dd694398c6d524f43
Steven Moreland <smoreland@google.com>
2018-01-08 13:08:36 -0800

    Keymaster HAL: use binderized hal
    Bug: 71707530
    Test: boot, lshal
    Change-Id: I996e43d8e1c6bc840c2cb56dde3c1cd51074c779
    Merged-In: I996e43d8e1c6bc840c2cb56dde3c1cd51074c779
    (cherry picked from commit 13bb81152db7ab06ad63028e9716a11c0b32a05e)
    (cherry picked from commit 96a6752be8422bab4b15aaf16da002911428c3cd)

--- shared/device.mk
+++ shared/device.mk
@@ -223,7 +223,8 @@ PRODUCT_PACKAGES += \
 # Keymaster HAL
 #
 PRODUCT_PACKAGES += \
-     android.hardware.keymaster@3.0-impl
+     android.hardware.keymaster@3.0-impl \
+     android.hardware.keymaster@3.0-service
 
 #
 # Power HAL
