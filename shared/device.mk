bb7384770f6d09b9b7492a864bb6e8db6417d7e2
Steven Moreland <smoreland@google.com>
2018-01-08 12:46:04 -0800

    Power HAL: use binderized service
    Bug: 71707530
    Test: boot, lshal
    Change-Id: I78dd886747d9b411ec0413732d40d582be10cee0

--- shared/device.mk
+++ shared/device.mk
@@ -286,7 +286,8 @@ PRODUCT_PACKAGES += \
 #
 PRODUCT_PACKAGES += \
     power.vsoc \
-    android.hardware.power@1.0-impl
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
 
 #
 # USB
