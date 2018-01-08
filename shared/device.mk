1911a05d533ad9f3602bdfd55ab2a2dcc8b295ad
Steven Moreland <smoreland@google.com>
2018-01-08 13:10:53 -0800

    DRM HAL: use binderized hal
    Bug: 71707530
    Test: boot, lshal
    Change-Id: Ib1dae2139a52af0ae83a3edfc1ef60e104d54709
    Merged-In: Ib1dae2139a52af0ae83a3edfc1ef60e104d54709
    (cherry picked from commit 9bf4f597ad280ec78e645d2ec58ec5f59bd18da1)
    (cherry picked from commit a9a9d420ca073e6dedea7b26e35a3a2387b93750)

--- shared/device.mk
+++ shared/device.mk
@@ -179,7 +179,8 @@ PRODUCT_PACKAGES += \
 # Drm HAL
 #
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
 
 #
 # Dumpstate HAL
