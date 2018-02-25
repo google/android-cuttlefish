8d5d1069260f9d0ac173650b8798fcdc95c2965d
android-build-team Robot <android-build-team-robot@google.com>
2018-02-25 08:28:29 +0000

    Snap for 4620899 from eb9d882cc72aaf559cf459b35c46bb386818a323 to pi-release
    Change-Id: I8f34b1869fbf7cff852c54e91dc66c6eb1cc88c5

--- cvd/common/libs/fs/shared_fd.h
+++ cvd/common/libs/fs/shared_fd.h
@@ -418,6 +418,13 @@ class FileInstance {
     return SendMsg(&msg, flags);
   }
 
+  int Shutdown(int how) {
+    errno = 0;
+    int rval = shutdown(fd_, how);
+    errno_ = errno;
+    return rval;
+  }
+
   ssize_t SendTo(const void* buf, size_t len, int flags,
                  const struct sockaddr* dest_addr, socklen_t addrlen) {
     errno = 0;
