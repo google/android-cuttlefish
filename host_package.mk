f15945d5979eaf3046aa4d2c877eff90318df073
Greg Hartman <ghartman@google.com>
2018-04-09 17:37:11 -0700

    Add missing library that broke booting
    BUG: 77826190
    BUG: 73011528
    Test: Build, upload, and boot
    Change-Id: Ifd2f0e36d36345422fe233c5242ef60c48b67fe8
    Merged-In: Ifd2f0e36d36345422fe233c5242ef60c48b67fe8
    (cherry picked from commit e658d66447f736432a319faa21d68ec7542e5a08)
    (cherry picked from commit d70e295cc66cfe3aaa4d07ccf60e834709dccf50)
    (cherry picked from commit c5ca1652ef851fc925339c1086d21b7b8f804bea)

--- host_package.mk
+++ host_package.mk
@@ -40,6 +40,7 @@ cvd_host_shared_libraries := \
     vsoc_lib \
     libcuttlefish_fs \
     cuttlefish_auto_resources \
+    libcuttlefish_strings \
     cuttlefish_tcp_socket \
     liblog \
     libnl \
