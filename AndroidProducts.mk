3262dbb861bce88824eacb04193359199c781371
Victor Chang <vfcc@google.com>
2018-04-27 15:10:48 -0700

    Add Android Things build target for Cuttlefish.
    Bug: 78638727
    Test: Boots.
    Change-Id: I84cec612a3a97c35fb8033544f81ea34a9133b93

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -23,6 +23,7 @@ PRODUCT_MAKEFILES := \
 	aosp_cf_x86_64_tv:$(LOCAL_DIR)/vsoc_x86_64/tv/device.mk \
 	aosp_cf_x86_64_wear:$(LOCAL_DIR)/vsoc_x86_64/wear/device.mk \
 	aosp_cf_x86_auto:$(LOCAL_DIR)/vsoc_x86/auto/device.mk \
+	aosp_cf_x86_iot:$(LOCAL_DIR)/vsoc_x86/iot/device.mk \
 	aosp_cf_x86_pasan:$(LOCAL_DIR)/vsoc_x86/pasan/device.mk \
 	aosp_cf_x86_phone:$(LOCAL_DIR)/vsoc_x86/phone/device.mk \
 	aosp_cf_x86_tablet:$(LOCAL_DIR)/vsoc_x86/tablet/device.mk \
@@ -32,6 +33,7 @@ PRODUCT_MAKEFILES := \
 
 COMMON_LUNCH_CHOICES := \
 	aosp_cf_x86_auto-userdebug \
+	aosp_cf_x86_iot-userdebug \
 	aosp_cf_x86_phone-userdebug \
 	aosp_cf_x86_tablet-userdebug \
 	aosp_cf_x86_tablet_3g-userdebug \
