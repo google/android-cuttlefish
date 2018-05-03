1ad50a58978815147e9bcf05210e692b87c4ae52
Greg Hartman <ghartman@google.com>
2018-05-03 16:27:11 -0700

    Clean up and simplify WiFi
    * Add a flag to save a pcap file
    * Strip netlink headers before forwarding the packet
    * Remove libraries and dead code
    
    BUG: 78296257
    Change-Id: I6a4b2bb4899a989cf214b627cdfd78fe5b3d844d
    Test: Local build and boot on pi-gce-dev

--- common/commands/wifi_relay/nl_client.cpp
+++ common/commands/wifi_relay/nl_client.cpp
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "common/libs/wifi/nl_client.h"
+#include "common/commands/wifi_relay/nl_client.h"
 
 #include <glog/logging.h>
 
