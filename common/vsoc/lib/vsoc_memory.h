9947cc79447f3ee36ddb8c64f429eb96f11c6304
Jorge E. Moreira <jemoreira@google.com>
2018-05-18 14:49:43 -0700

    Moves dealing with VSoC layout to vsoc_lib library
    After moving the logic to calculate region sizes to vsoc_lib the
    knowledge about shared memory layout was split between two independent
    modules. It makes sense to let the vsoc_lib library be in charge of
    the details of the memory layout while keeping the ivsering just
    knowing how to 'serve'.
    
    The information about the memory layout that the ivserver and other
    host processes need is accessible through a simple interface defined
    in common/vsoc/lib/vsoc_memory.h.
    
    Bug: 79602833
    Test: run locally
    Change-Id: Ib80bb42bd0d3cdb1dea635b2153258d31d7c92a7

--- common/vsoc/lib/vsoc_memory.h
+++ common/vsoc/lib/vsoc_memory.h
@@ -15,44 +15,55 @@
  * limitations under the License.
  */
 
+#include <stdint.h>
+
 #include <vector>
 
 namespace vsoc {
 
-class RegionMemoryLayout {
+class VSoCRegionLayout {
  public:
-  RegionMemoryLayout(const char* region_name, size_t region_size,
-                     int guest_to_host_signal_table_log_size,
-                     int host_to_guest_signal_table_log_size,
-                     const char* managed_by)
-      : region_name_(region_name),
-        region_size_(region_size),
-        guest_to_host_signal_table_log_size_(
-            guest_to_host_signal_table_log_size),
-        host_to_guest_signal_table_log_size_(
-            host_to_guest_signal_table_log_size),
-        managed_by_(managed_by) {}
+  virtual const char* region_name() const = 0;
+  virtual const char* managed_by() const = 0;
 
-  const char* region_name() const { return region_name_; }
-  size_t region_size() const { return region_size_; }
-  int guest_to_host_signal_table_log_size() const {
-    return guest_to_host_signal_table_log_size_;
-  }
-  int host_to_guest_signal_table_log_size() const {
-    return host_to_guest_signal_table_log_size_;
-  }
-  const char * managed_by() const {
-    return managed_by_;
-  }
-
- private:
-  const char* region_name_{};
-  size_t region_size_{};
-  int guest_to_host_signal_table_log_size_{};
-  int host_to_guest_signal_table_log_size_{};
-  const char* managed_by_{};
+  virtual size_t layout_size() const = 0;
+  virtual int guest_to_host_signal_table_log_size() const = 0;
+  virtual int host_to_guest_signal_table_log_size() const = 0;
+  virtual uint32_t begin_offset() const = 0;
+  virtual size_t region_size() const = 0;
+ protected:
+  VSoCRegionLayout() = default;
+  virtual ~VSoCRegionLayout() = default;
 };
 
-const std::vector<RegionMemoryLayout>& GetVsocMemoryLayout();
+class VSoCMemoryLayout {
+ public:
+  // Returns a pointer to the memory layout singleton.
+  static VSoCMemoryLayout* Get();
+
+  VSoCMemoryLayout(const VSoCMemoryLayout&) = delete;
+  VSoCMemoryLayout(VSoCMemoryLayout&&) = delete;
+
+  virtual std::vector<const VSoCRegionLayout *> GetRegions() const = 0;
+  virtual const VSoCRegionLayout* GetRegionByName(
+      const char* region_name) const = 0;
+
+  // Returns the smallest number of bytes that is a power of 2 and can
+  // accomodate the entire memory layout.
+  virtual uint32_t GetMemoryFileSize() const = 0;
+
+  // Writes the layout to memory. shared_memory should be the result of mmap-ing
+  // a file of at least GetMemoryFileSize() bytes.
+  virtual void WriteLayout(void * shared_memory) const = 0;
+
+  // Increase the size of the data section of a region to make the entire region
+  // at least new_min_size bytes. The actual region size will be no less than
+  // the minimum size required to accomodate the signal tables and the memory
+  // layout, and will be aligned to page size.
+  virtual bool ResizeRegion(const char* region_name, size_t new_min_size) = 0;
+ protected:
+  VSoCMemoryLayout() = default;
+  virtual ~VSoCMemoryLayout() = default;
+};
 
 }  // namespace vsoc
