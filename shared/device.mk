f305854585b2228242f88cce691af4b96a531466
Alistair Strachan <astrachan@google.com>
2018-06-12 15:59:02 -0700

    Treble-ize the init script.
    When system-as-root is enabled, the system image (and therefore the root
    directory) might be wholly substituted. We shouldn't install *any*
    vendor customizations there.
    
    Bug: 110097226
    Change-Id: I42462752363662bff3a3f94291106c72e342fd00
    Merged-In: I42462752363662bff3a3f94291106c72e342fd00

--- shared/device.mk
+++ shared/device.mk
@@ -104,7 +104,7 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 PRODUCT_COPY_FILES += \
     device/google/cuttlefish/shared/config/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     device/google/cuttlefish/shared/config/camera_v1.json:$(TARGET_COPY_OUT_VENDOR)/etc/config/camera.json \
-    device/google/cuttlefish/shared/config/init.vsoc.rc:root/init.vsoc.rc \
+    device/google/cuttlefish/shared/config/init.vsoc.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.vsoc.rc \
     device/google/cuttlefish/shared/config/media_codecs.xml:system/etc/media_codecs.xml \
     device/google/cuttlefish/shared/config/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     device/google/cuttlefish/shared/config/media_profiles.xml:system/etc/media_profiles.xml \
