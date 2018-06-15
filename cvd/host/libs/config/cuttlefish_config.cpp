dc8d4d500ebd06c0e84ba00f0ae2bcce8864d744
Cody Schuffelen <schuffelen@google.com>
2018-06-14 18:20:02 -0700

    Allocate a virtio bridge in the host for wifi.
    Test: Apply kernel change, crun ..., enable wifi.
    Bug: 78245952
    Change-Id: If3b9ddf5d4753573a852a69e0bb08f23769f2025
    Merged-In: If3b9ddf5d4753573a852a69e0bb08f23769f2025
    (cherry picked from commit 975175ab791a6dc040e07e9a54f07017f54b923e)

--- cvd/host/libs/config/cuttlefish_config.cpp
+++ cvd/host/libs/config/cuttlefish_config.cpp
@@ -99,6 +99,8 @@ const char* kIvshmemVectorCount = "ivshmem_vector_count";
 
 const char* kMobileBridgeName = "mobile_bridge_name";
 const char* kMobileTapName = "mobile_tap_name";
+const char* kWifiBridgeName = "wifi_bridge_name";
+const char* kWifiTapName = "wifi_tap_name";
 const char* kWifiGuestMacAddr = "wifi_guest_mac_addr";
 const char* kWifiHostMacAddr = "wifi_host_mac_addr";
 const char* kEntropySource = "entropy_source";
@@ -295,6 +297,14 @@ void CuttlefishConfig::set_mobile_bridge_name(
   (*dictionary_)[kMobileBridgeName] = mobile_bridge_name;
 }
 
+std::string CuttlefishConfig::wifi_bridge_name() const {
+  return (*dictionary_)[kWifiBridgeName].asString();
+}
+void CuttlefishConfig::set_wifi_bridge_name(
+    const std::string& wifi_bridge_name) {
+  (*dictionary_)[kWifiBridgeName] = wifi_bridge_name;
+}
+
 std::string CuttlefishConfig::wifi_guest_mac_addr() const {
   return (*dictionary_)[kWifiGuestMacAddr].asString();
 }
@@ -316,6 +326,13 @@ std::string CuttlefishConfig::mobile_tap_name() const {
 }
 void CuttlefishConfig::set_mobile_tap_name(const std::string& mobile_tap_name) {
   (*dictionary_)[kMobileTapName] = mobile_tap_name;
+}
+
+std::string CuttlefishConfig::wifi_tap_name() const {
+  return (*dictionary_)[kWifiTapName].asString();
+}
+void CuttlefishConfig::set_wifi_tap_name(const std::string& wifi_tap_name) {
+  (*dictionary_)[kWifiTapName] = wifi_tap_name;
 }
 
 std::string CuttlefishConfig::entropy_source() const {
