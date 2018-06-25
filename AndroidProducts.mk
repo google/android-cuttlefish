7e54ac73e3bca1f7087dc72fde26866aab8a3e93
Greg Hartman <ghartman@google.com>
2018-06-25 14:33:59 -0700

    Revert "Turn off aosp_cf*_auto builds"
    This reverts commit fef54684c67ccbc2b5d3787eef82063aa0f1b8de.
    
    Change-Id: Ibe60d6f2f00989e979b025b3cd5da4a2a0791d30
    Merged-In: Ibe60d6f2f00989e979b025b3cd5da4a2a0791d30
    BUG: 110784510
    Test: AOSP builds work

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,11 +15,13 @@
 #
 
 PRODUCT_MAKEFILES := \
+	aosp_cf_x86_64_auto:$(LOCAL_DIR)/vsoc_x86_64/auto/device.mk \
 	aosp_cf_x86_64_phone:$(LOCAL_DIR)/vsoc_x86_64/phone/device.mk \
 	aosp_cf_x86_64_tablet:$(LOCAL_DIR)/vsoc_x86_64/tablet/device.mk \
 	aosp_cf_x86_64_tablet_3g:$(LOCAL_DIR)/vsoc_x86_64/tablet_3g/device.mk \
 	aosp_cf_x86_64_tv:$(LOCAL_DIR)/vsoc_x86_64/tv/device.mk \
 	aosp_cf_x86_64_wear:$(LOCAL_DIR)/vsoc_x86_64/wear/device.mk \
+	aosp_cf_x86_auto:$(LOCAL_DIR)/vsoc_x86/auto/device.mk \
 	aosp_cf_x86_phone:$(LOCAL_DIR)/vsoc_x86/phone/device.mk \
 	aosp_cf_x86_tablet:$(LOCAL_DIR)/vsoc_x86/tablet/device.mk \
 	aosp_cf_x86_tablet_3g:$(LOCAL_DIR)/vsoc_x86/tablet_3g/device.mk \
@@ -27,11 +29,13 @@ PRODUCT_MAKEFILES := \
 	aosp_cf_x86_wear:$(LOCAL_DIR)/vsoc_x86/wear/device.mk \
 
 COMMON_LUNCH_CHOICES := \
+	aosp_cf_x86_auto-userdebug \
 	aosp_cf_x86_phone-userdebug \
 	aosp_cf_x86_tablet-userdebug \
 	aosp_cf_x86_tablet_3g-userdebug \
 	aosp_cf_x86_tv-userdebug \
 	aosp_cf_x86_wear-userdebug \
+	aosp_cf_x86_64_auto-userdebug \
 	aosp_cf_x86_64_phone-userdebug \
 	aosp_cf_x86_64_tablet-userdebug \
 	aosp_cf_x86_64_tablet_3g-userdebug \
