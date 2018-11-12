334f3a92aeeb9457edbbd03b4c35e49fe676790f
Jorge E. Moreira <jemoreira@google.com>
2018-11-12 10:22:53 -0800

    Extract managed region tests into its own target
    This change also causes these tests to stop running on boot.
    
    Bug: 119332324
    Bug: 64462568
    Test: build & run locally
    Change-Id: I0da644f6b2be853903613834cb18f7ecbe620e6f

--- guest/vsoc/lib/guest_region_e2e_test.cpp
+++ guest/vsoc/lib/guest_region_e2e_test.cpp
@@ -18,28 +18,11 @@
  * End-to-end test to ensure that mapping of vsoc regions works on the guest.
  */
 
+#include "guest/vsoc/lib/e2e_test_common.h"
 #include "common/vsoc/lib/e2e_test_region_view.h"
-// TODO(b/64462568) Move the manager tests to a separate target
-#include "guest/vsoc/lib/manager_region_view.h"
 
 #include <android-base/logging.h>
 #include <gtest/gtest.h>
-
-#define DEATH_TEST_MESSAGE "abort converted to exit of 2 during death test"
-
-using vsoc::layout::e2e_test::E2EManagedTestRegionLayout;
-using vsoc::layout::e2e_test::E2EManagerTestRegionLayout;
-
-static inline void disable_tombstones() {
-  // We don't want a tombstone, and we're already in the child, so we modify the
-  // behavior of LOG(ABORT) to print the well known message and do an
-  // error-based exit.
-  android::base::SetAborter([](const char*) {
-    fputs(DEATH_TEST_MESSAGE, stderr);
-    fflush(stderr);
-    exit(2);
-  });
-}
 
 template <typename View>
 void DeathTestView() {
@@ -149,91 +132,6 @@ TEST(RegionTest, MissingRegionDeathTest) {
   EXPECT_EXIT(DeathTestView<vsoc::E2EUnfindableRegionView>(),
               testing::ExitedWithCode(2),
               ".*" DEATH_TEST_MESSAGE ".*");
-}
-
-// Region view classes to allow calling the Open() function from the test.
-class E2EManagedTestRegionView
-    : public vsoc::TypedRegionView<
-        E2EManagedTestRegionView,
-        E2EManagedTestRegionLayout> {
- public:
-  using vsoc::TypedRegionView<
-      E2EManagedTestRegionView, E2EManagedTestRegionLayout>::Open;
-};
-class E2EManagerTestRegionView
-    : public vsoc::ManagerRegionView<
-        E2EManagerTestRegionView,
-        E2EManagerTestRegionLayout> {
- public:
-  using vsoc::ManagerRegionView<
-      E2EManagerTestRegionView, E2EManagerTestRegionLayout>::Open;
-};
-
-class ManagedRegionTest {
- public:
-  void testManagedRegionFailMap() {
-    E2EManagedTestRegionView managed_region;
-    disable_tombstones();
-    // managed_region.Open should never return.
-    EXPECT_FALSE(managed_region.Open());
-  }
-
-  void testManagedRegionMap() {
-    EXPECT_TRUE(manager_region_.Open());
-
-    // Maps correctly with permission
-    const uint32_t owned_value = 65, begin_offset = 4096, end_offset = 8192;
-    int perm_fd = manager_region_.CreateFdScopedPermission(
-        &manager_region_.data()->data[0], owned_value, begin_offset,
-        end_offset);
-    EXPECT_TRUE(perm_fd >= 0);
-    fd_scoped_permission perm;
-    ASSERT_TRUE(ioctl(perm_fd, VSOC_GET_FD_SCOPED_PERMISSION, &perm) == 0);
-    void* mapped_ptr = mmap(NULL, perm.end_offset - perm.begin_offset,
-                            PROT_WRITE | PROT_READ, MAP_SHARED, perm_fd, 0);
-    EXPECT_FALSE(mapped_ptr == MAP_FAILED);
-
-    // Owned value gets written
-    EXPECT_TRUE(manager_region_.data()->data[0] == owned_value);
-
-    // Data written to the mapped memory stays there after unmap
-    std::string str = "managed by e2e_manager";
-    strcpy(reinterpret_cast<char*>(mapped_ptr), str.c_str());
-    EXPECT_TRUE(munmap(mapped_ptr, end_offset - begin_offset) == 0);
-    mapped_ptr = mmap(NULL, end_offset - begin_offset, PROT_WRITE | PROT_READ,
-                      MAP_SHARED, perm_fd, 0);
-    EXPECT_FALSE(mapped_ptr == MAP_FAILED);
-    EXPECT_TRUE(strcmp(reinterpret_cast<char*>(mapped_ptr), str.c_str()) == 0);
-
-    // Create permission elsewhere in the region, map same offset and length,
-    // ensure data isn't there
-    EXPECT_TRUE(munmap(mapped_ptr, end_offset - begin_offset) == 0);
-    close(perm_fd);
-    EXPECT_TRUE(manager_region_.data()->data[0] == 0);
-    perm_fd = manager_region_.CreateFdScopedPermission(
-        &manager_region_.data()->data[1], owned_value, begin_offset + 4096,
-        end_offset + 4096);
-    EXPECT_TRUE(perm_fd >= 0);
-    mapped_ptr = mmap(NULL, end_offset - begin_offset, PROT_WRITE | PROT_READ,
-                      MAP_SHARED, perm_fd, 0);
-    EXPECT_FALSE(mapped_ptr == MAP_FAILED);
-    EXPECT_FALSE(strcmp(reinterpret_cast<char*>(mapped_ptr), str.c_str()) == 0);
-  }
-  ManagedRegionTest() {}
-
- private:
-  E2EManagerTestRegionView manager_region_;
-};
-
-TEST(ManagedRegionTest, ManagedRegionFailMap) {
-  ManagedRegionTest test;
-  EXPECT_EXIT(test.testManagedRegionFailMap(), testing::ExitedWithCode(2),
-              ".*" DEATH_TEST_MESSAGE ".*");
-}
-
-TEST(ManagedRegionTest, ManagedRegionMap) {
-  ManagedRegionTest test;
-  test.testManagedRegionMap();
 }
 
 int main(int argc, char** argv) {
