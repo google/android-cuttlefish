433544c13cfd36a9f71e5e5fd89b90b7e64a567b
Yu-Han Yang <yuhany@google.com>
2018-11-14 12:54:09 -0800

    Use GNSS HAL v2.0 in cuttlefish
    Bug: 112260995
    Bug: 120277977
    Test: atest VtsHalGnssV2_0TargetTest
    Change-Id: I9a9d4fb412d291468e01fc918e669651e81f6e31

--- shared/device.mk
+++ shared/device.mk
@@ -239,9 +239,7 @@ PRODUCT_PACKAGES += \
 # GPS
 #
 PRODUCT_PACKAGES += \
-    gps.vsoc \
-    android.hardware.gnss@1.0-impl \
-    android.hardware.gnss@1.0-service
+    android.hardware.gnss@2.0-service
 
 # Health
 PRODUCT_PACKAGES += \
