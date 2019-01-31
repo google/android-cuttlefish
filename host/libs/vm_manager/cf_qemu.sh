494eaf67f89c036061fc635cf78ba33ad37e473a
Greg Hartman <ghartman@google.com>
2019-01-30 20:31:39 -0800

    Remove spurious addr= lines from the qemu config
    Test: Local build and boot
    BUG: 77276633
    Change-Id: I30f4276a070f63450b5d97c4f74eeb151e3ffd09
    (cherry picked from commit 4d9b8250a8c38c258bfcdb5d70a8851252feebb3)

--- host/libs/vm_manager/cf_qemu.sh
+++ host/libs/vm_manager/cf_qemu.sh
@@ -67,7 +67,7 @@ fi
 
 if [[ "${qemu_binary##*/}" = "qemu-system-aarch64" ]]; then
   # On ARM, the early console can be PCI, and ISA is not supported
-  kernel_console_serial="pci-serial,addr=0xb"
+  kernel_console_serial="pci-serial"
   machine="virt,gic_version=2"
   cpu=cortex-a53
 else
@@ -98,22 +98,22 @@ args=(
     -append "${kernel_cmdline:-"loop.max_part=7 console=ttyS0 androidboot.console=ttyS1 androidboot.hardware=vsoc enforcing=0 audit=1 androidboot.selinux=permissive mac80211_hwsim.radios=0 security=selinux buildvariant=userdebug  androidboot.serialno=CUTTLEFISHCVD01 androidboot.lcd_density=160"}"
     -dtb "${dtb_path:-${HOME}/config/cuttlefish.dtb}"
     -device "piix3-usb-uhci,id=usb,addr=0x1.0x2"
-    -device "virtio-serial-pci,id=virtio-serial0,addr=0x4"
+    -device "virtio-serial-pci,id=virtio-serial0"
     -drive "file=${system_image_path:-${HOME}/system.img},format=raw,if=none,id=drive-virtio-disk0,aio=threads"
-    -device "virtio-blk-pci,scsi=off,addr=0x5,drive=drive-virtio-disk0,id=virtio-disk0,bootindex=1"
+    -device "virtio-blk-pci,scsi=off,drive=drive-virtio-disk0,id=virtio-disk0,bootindex=1"
     -drive "file=${data_image_path:-${HOME}/userdata.img},format=raw,if=none,id=drive-virtio-disk1,aio=threads"
-    -device "virtio-blk-pci,scsi=off,addr=0x6,drive=drive-virtio-disk1,id=virtio-disk1"
+    -device "virtio-blk-pci,scsi=off,drive=drive-virtio-disk1,id=virtio-disk1"
     -drive "file=${cache_image_path:-${HOME}/cache.img},format=raw,if=none,id=drive-virtio-disk2,aio=threads"
-    -device "virtio-blk-pci,scsi=off,addr=0x7,drive=drive-virtio-disk2,id=virtio-disk2"
+    -device "virtio-blk-pci,scsi=off,drive=drive-virtio-disk2,id=virtio-disk2"
     -drive "file=${vendor_image_path:-${HOME}/vendor.img},format=raw,if=none,id=drive-virtio-disk3,aio=threads"
-    -device "virtio-blk-pci,scsi=off,addr=0x8,drive=drive-virtio-disk3,id=virtio-disk3"
+    -device "virtio-blk-pci,scsi=off,drive=drive-virtio-disk3,id=virtio-disk3"
     -netdev "tap,id=hostnet0,ifname=${wifi_tap_name:-${default_wifi_tap_name}},script=no,downscript=no"
-    -device "virtio-net-pci,netdev=hostnet0,id=net0,addr=0x2"
+    -device "virtio-net-pci,netdev=hostnet0,id=net0"
     -netdev "tap,id=hostnet1,ifname=${mobile_tap_name:-${default_mobile_tap_name}},script=no,downscript=no"
-    -device "virtio-net-pci,netdev=hostnet1,id=net1,addr=0x3"
-    -device "virtio-balloon-pci,id=balloon0,addr=0x9"
+    -device "virtio-net-pci,netdev=hostnet1,id=net1"
+    -device "virtio-balloon-pci,id=balloon0"
     -object "rng-random,id=objrng0,filename=/dev/urandom"
-    -device "virtio-rng-pci,rng=objrng0,id=rng0,max-bytes=1024,period=2000,addr=0xa"
+    -device "virtio-rng-pci,rng=objrng0,id=rng0,max-bytes=1024,period=2000"
     -cpu "${cpu}"
     -msg "timestamp=on"
 )
@@ -183,11 +183,11 @@ args+=(
     -chardev "socket,id=charserial0,path=${kernel_log_socket_name:-${default_dir}/kernel-log}"
     -device "${kernel_console_serial},chardev=charserial0,id=serial0"
     -chardev "socket,id=charserial1,path=${console_path:-${default_dir}/console},server,nowait"
-    -device "pci-serial,chardev=charserial1,id=serial1,addr=0xc"
+    -device "pci-serial,chardev=charserial1,id=serial1"
     -chardev "file,id=charchannel0,path=${logcat_path:-${default_dir}/logcat},append=on"
     -device "virtserialport,bus=virtio-serial0.0,nr=1,chardev=charchannel0,id=channel0,name=cf-logcat"
     -chardev "socket,path=${ivshmem_qemu_socket_path:-${default_dir}/ivshmem_socket_qemu},id=ivsocket"
-    -device "ivshmem-doorbell,chardev=ivsocket,vectors=${ivshmem_vector_count},addr=0xd"
+    -device "ivshmem-doorbell,chardev=ivsocket,vectors=${ivshmem_vector_count}"
 )
 
 if [[ -n "${gdb_flag}" ]]; then
