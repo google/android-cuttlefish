fb95b0d71f4a56ea537899d8d494d179c0739710
Jorge E. Moreira <jemoreira@google.com>
2019-02-05 17:40:20 -0800

    Set start_stream_audio to false by default when vm_manager=crosvm
    Bug: 122978436
    Test: build&run locally with both vm_managers
    Change-Id: Ica7d03704778cfb6e71c9c5f57c6419af799c9f8

--- host/commands/launch/flags.cc
+++ host/commands/launch/flags.cc
@@ -477,6 +477,8 @@ void SetDefaultFlagsForCrosvm() {
                                google::FlagSettingMode::SET_FLAGS_DEFAULT);
   SetCommandLineOptionWithMode("start_vnc_server", "false",
                                google::FlagSettingMode::SET_FLAGS_DEFAULT);
+  SetCommandLineOptionWithMode("start_stream_audio", "false",
+                               google::FlagSettingMode::SET_FLAGS_DEFAULT);
 }
 
 bool ParseCommandLineFlags(int* argc, char*** argv) {
