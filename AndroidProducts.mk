dc5dfa1712f11d5fc8a54630470a3214fb364559
Greg Hartman <ghartman@google.com>
2019-04-17 16:40:46 -0700

    Reduce cuttlefish targets
    Bug: 130846989
    Bug: 130887841
    Change-Id: Id4ffc88fac0e0d240f57ae517be088d6429170df
    Merged-In: Id4ffc88fac0e0d240f57ae517be088d6429170df

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,41 +15,16 @@
 #
 
 PRODUCT_MAKEFILES := \
-	aosp_cf_arm_phone:$(LOCAL_DIR)/vsoc_arm/phone/device.mk \
 	aosp_cf_arm64_phone:$(LOCAL_DIR)/vsoc_arm64/phone/device.mk \
-	aosp_cf_x86_64_auto:$(LOCAL_DIR)/vsoc_x86_64/auto/device.mk \
-	aosp_cf_x86_64_pasan:$(LOCAL_DIR)/vsoc_x86_64/pasan/device.mk \
-	aosp_cf_x86_64_phone:$(LOCAL_DIR)/vsoc_x86_64/phone/device.mk \
-	aosp_cf_x86_64_go_phone:$(LOCAL_DIR)/vsoc_x86_64/go_phone/device.mk \
-	aosp_cf_x86_64_go_512_phone:$(LOCAL_DIR)/vsoc_x86_64/go_512_phone/device.mk \
-	aosp_cf_x86_64_gsi:$(LOCAL_DIR)/vsoc_x86_64/gsi/aosp_cf_x86_64_gsi.mk \
-	aosp_cf_x86_64_tablet:$(LOCAL_DIR)/vsoc_x86_64/tablet/device.mk \
-	aosp_cf_x86_64_tablet_3g:$(LOCAL_DIR)/vsoc_x86_64/tablet_3g/device.mk \
-	aosp_cf_x86_64_tv:$(LOCAL_DIR)/vsoc_x86_64/tv/device.mk \
-	aosp_cf_x86_64_wear:$(LOCAL_DIR)/vsoc_x86_64/wear/device.mk \
 	aosp_cf_x86_auto:$(LOCAL_DIR)/vsoc_x86/auto/device.mk \
 	aosp_cf_x86_pasan:$(LOCAL_DIR)/vsoc_x86/pasan/device.mk \
 	aosp_cf_x86_phone:$(LOCAL_DIR)/vsoc_x86/phone/device.mk \
 	aosp_cf_x86_go_phone:$(LOCAL_DIR)/vsoc_x86/go_phone/device.mk \
-	aosp_cf_x86_go_512_phone:$(LOCAL_DIR)/vsoc_x86/go_512_phone/device.mk \
 	aosp_cf_x86_gsi:$(LOCAL_DIR)/vsoc_x86/gsi/aosp_cf_x86_gsi.mk \
-	aosp_cf_x86_tablet:$(LOCAL_DIR)/vsoc_x86/tablet/device.mk \
-	aosp_cf_x86_tablet_3g:$(LOCAL_DIR)/vsoc_x86/tablet_3g/device.mk \
-	aosp_cf_x86_tv:$(LOCAL_DIR)/vsoc_x86/tv/device.mk \
-	aosp_cf_x86_wear:$(LOCAL_DIR)/vsoc_x86/wear/device.mk \
+	aosp_cf_x86_tv:$(LOCAL_DIR)/vsoc_x86/tv/device.mk
 
 COMMON_LUNCH_CHOICES := \
-	aosp_cf_arm_phone-userdebug \
 	aosp_cf_arm64_phone-userdebug \
 	aosp_cf_x86_auto-userdebug \
 	aosp_cf_x86_phone-userdebug \
-	aosp_cf_x86_tablet-userdebug \
-	aosp_cf_x86_tablet_3g-userdebug \
-	aosp_cf_x86_tv-userdebug \
-	aosp_cf_x86_wear-userdebug \
-	aosp_cf_x86_64_auto-userdebug \
-	aosp_cf_x86_64_phone-userdebug \
-	aosp_cf_x86_64_tablet-userdebug \
-	aosp_cf_x86_64_tablet_3g-userdebug \
-	aosp_cf_x86_64_tv-userdebug \
-	aosp_cf_x86_64_wear-userdebug
+	aosp_cf_x86_tv-userdebug
