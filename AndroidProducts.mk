92fde49277740c82ba865acd88fdc3dbbcd53a41
Alistair Strachan <astrachan@google.com>
2019-04-23 12:51:54 -0700

    Bring back aosp_cf_x86_64_phone
    Bug: 130887841
    Change-Id: If253bbad93c77bc778da1a0a81fd7bad5207f591

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,7 +6,7 @@
 # You may obtain a copy of the License at
 #
 #      http://www.apache.org/licenses/LICENSE-2.0
-#u
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,6 +16,7 @@
 
 PRODUCT_MAKEFILES := \
 	aosp_cf_arm64_phone:$(LOCAL_DIR)/vsoc_arm64/phone/device.mk \
+	aosp_cf_x86_64_phone:$(LOCAL_DIR)/vsoc_x86_64/phone/device.mk \
 	aosp_cf_x86_auto:$(LOCAL_DIR)/vsoc_x86/auto/device.mk \
 	aosp_cf_x86_pasan:$(LOCAL_DIR)/vsoc_x86/pasan/device.mk \
 	aosp_cf_x86_phone:$(LOCAL_DIR)/vsoc_x86/phone/device.mk \
@@ -25,6 +26,7 @@ PRODUCT_MAKEFILES := \
 
 COMMON_LUNCH_CHOICES := \
 	aosp_cf_arm64_phone-userdebug \
+	aosp_cf_x86_64_phone-userdebug \
 	aosp_cf_x86_auto-userdebug \
 	aosp_cf_x86_phone-userdebug \
 	aosp_cf_x86_tv-userdebug
