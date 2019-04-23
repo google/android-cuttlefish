c8cde53ceef3193b92a6192bd248dc4cd023d893
Alistair Strachan <astrachan@google.com>
2019-04-23 12:51:54 -0700

    Bring back aosp_cf_x86_64_phone
    Bug: 130887841
    Change-Id: If253bbad93c77bc778da1a0a81fd7bad5207f591
    Merged-In: If253bbad93c77bc778da1a0a81fd7bad5207f591

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,6 +16,7 @@
 
 PRODUCT_MAKEFILES := \
 	aosp_cf_arm64_phone:$(LOCAL_DIR)/vsoc_arm64/phone/device.mk \
+	aosp_cf_x86_64_phone:$(LOCAL_DIR)/vsoc_x86_64/phone/device.mk \
 	aosp_cf_x86_auto:$(LOCAL_DIR)/vsoc_x86/auto/device.mk \
 	aosp_cf_x86_pasan:$(LOCAL_DIR)/vsoc_x86/pasan/device.mk \
 	aosp_cf_x86_phone:$(LOCAL_DIR)/vsoc_x86/phone/device.mk \
@@ -25,6 +26,7 @@ PRODUCT_MAKEFILES := \
 
 COMMON_LUNCH_CHOICES := \
 	aosp_cf_arm64_phone-userdebug \
+	aosp_cf_x86_64_phone-userdebug \
 	aosp_cf_x86_auto-userdebug \
 	aosp_cf_x86_phone-userdebug \
 	aosp_cf_x86_tv-userdebug
