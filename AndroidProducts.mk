e0ce666e403a42670b8ae05e095e90198565e4ce
Isaac Chen <ycchen@google.com>
2019-05-17 14:56:33 +0800

    Base CF phone products on mainline makefiles
    GSI related products have been changed to use mainline makefiles. This CL
    change CF phone products (for x86, x86_64, and arm64) to base on mainline
    makefiles, too, so their system images can be (almost) the same as GSI.
    
    Bug: 112341596
    Test: Built the following 3 products successfully:
            aosp_cf_x86_phone-userdebug
            aosp_cf_x86_64_phone-userdebug
            aosp_cf_arm64_phone-userdebug
          The first can boot and browse the web successfully.
          The second can boot to home screen successfully.
    
    Change-Id: Ie0a841c4e0187412e2f8a549ef33b58133d44aa9
    (cherry picked from commit c90d5deb0cfa111c95949bd71050b4b9ddb48df4)

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,11 +15,11 @@
 #
 
 PRODUCT_MAKEFILES := \
-	aosp_cf_arm64_phone:$(LOCAL_DIR)/vsoc_arm64/phone/device.mk \
-	aosp_cf_x86_64_phone:$(LOCAL_DIR)/vsoc_x86_64/phone/device.mk \
+	aosp_cf_arm64_phone:$(LOCAL_DIR)/vsoc_arm64/phone/aosp_cf.mk \
+	aosp_cf_x86_64_phone:$(LOCAL_DIR)/vsoc_x86_64/phone/aosp_cf.mk \
 	aosp_cf_x86_auto:$(LOCAL_DIR)/vsoc_x86/auto/device.mk \
 	aosp_cf_x86_pasan:$(LOCAL_DIR)/vsoc_x86/pasan/device.mk \
-	aosp_cf_x86_phone:$(LOCAL_DIR)/vsoc_x86/phone/device.mk \
+	aosp_cf_x86_phone:$(LOCAL_DIR)/vsoc_x86/phone/aosp_cf.mk \
 	aosp_cf_x86_go_phone:$(LOCAL_DIR)/vsoc_x86/go_phone/device.mk \
 	aosp_cf_x86_go_512_phone:$(LOCAL_DIR)/vsoc_x86/go_512_phone/device.mk \
 	aosp_cf_x86_gsi:$(LOCAL_DIR)/vsoc_x86/gsi/aosp_cf_x86_gsi.mk \
