5f55a8819d1f1943dbc2154424e51f373dca6e9b
Alistair Delva <adelva@google.com>
2019-07-19 13:24:18 -0700

    Enable dynamic partitions on tv and auto
    This config has had a long soak time on phone and it does not affect the
    differentiating features of these lunch targets.
    
    Change-Id: Ide80b1cf71d35704f2db854ac6c9312ad948ab07
    Signed-off-by: Alistair Delva <adelva@google.com>
    Merged-In: Ide80b1cf71d35704f2db854ac6c9312ad948ab07

--- shared/auto/device.mk
+++ shared/auto/device.mk
@@ -20,8 +20,6 @@
 DEVICE_MANIFEST_FILE += device/google/cuttlefish/shared/config/manifest.xml
 DEVICE_MANIFEST_FILE += device/google/cuttlefish/shared/auto/manifest.xml
 
-TARGET_BUILD_SYSTEM_ROOT_IMAGE ?= true
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, device/google/cuttlefish/shared/device.mk)
 
@@ -97,3 +95,11 @@ $(call inherit-product, packages/services/Car/car_product/build/car.mk)
 
 # Placed here due to b/110784510
 PRODUCT_BRAND := generic
+
+TARGET_USE_DYNAMIC_PARTITIONS ?= true
+ifeq ($(TARGET_USE_DYNAMIC_PARTITIONS),true)
+  PRODUCT_USE_DYNAMIC_PARTITIONS := true
+  TARGET_BUILD_SYSTEM_ROOT_IMAGE := false
+else
+  TARGET_BUILD_SYSTEM_ROOT_IMAGE ?= true
+endif
