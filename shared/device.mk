84d6d817ba27e55d35296f0429be228c24463324
Edwin Wong <edwinwong@google.com>
2019-10-18 13:29:22 -0700

    Add Widevine L3 drm service to cuttlefish
    Test: run gts testL3LongLicenseShortPlayback
      Test in master
    
    bug: 141621537
    Change-Id: I8fd07b6f911e359d63d19f3f0cca407a213a15cd

--- shared/device.mk
+++ shared/device.mk
@@ -220,7 +220,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.2-service.clearkey \
+    android.hardware.drm@1.2-service.widevine
 
 #
 # Dumpstate HAL
