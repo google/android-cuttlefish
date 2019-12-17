ad6b097e5e54ae5f382585e96fee81ed074a2ef3
Malcolm Chen <refuhoo@google.com>
2019-12-17 15:52:16 -0800

    Add cuttlefish implementation for 1.5 setRadioPower
    Bug: 143683647
    Test: sanity
    Change-Id: I46acf989541e4f380158758edfde34d8bc34572e

--- guest/hals/ril/libril/ril_service.h
+++ guest/hals/ril/libril/ril_service.h
@@ -798,6 +798,10 @@ int areUiccApplicationsEnabledResponse(int slotId,
                                      int responseType, int serial, RIL_Errno e,
                                      void *response, size_t responselen);
 
+int setRadioPowerResponse_1_5(int slotId, int responseType, int serial, RIL_Errno e,
+                              void *response, size_t responselen);
+
+
 pthread_rwlock_t * getRadioServiceRwlock(int slotId);
 
 void setNitzTimeReceived(int slotId, long timeReceived);
