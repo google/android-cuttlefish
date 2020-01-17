0276551c8b411d5cc96ca3df8778344c16338414
Jorge E. Moreira <jemoreira@google.com>
2020-01-17 15:58:22 -0800

    Extract common packetizer code to the base class
    Bug: 142904735
    Test: launch_cvd -start_webrtc
    Change-Id: I6bf7fdd4edef3b6ae6b58e59b3c148a9a73cc2d3

--- host/frontend/gcastv2/webrtc/Packetizer.cpp
+++ host/frontend/gcastv2/webrtc/Packetizer.cpp
@@ -16,7 +16,21 @@
 
 #include <webrtc/Packetizer.h>
 
+#include <https/SafeCallbackable.h>
 #include <webrtc/RTPSender.h>
+
+Packetizer::Packetizer(std::shared_ptr<RunLoop> runLoop,
+                       std::shared_ptr<StreamingSource> source)
+    : mNumSamplesRead(0),
+      mStartTimeMedia(0),
+      mRunLoop(runLoop),
+      mStreamingSource(source) {}
+
+Packetizer::~Packetizer() {
+    if (mStreamingSource) {
+        mStreamingSource->stop();
+    }
+}
 
 void Packetizer::queueRTPDatagram(std::vector<uint8_t> *packet) {
     auto it = mSenders.begin();
@@ -36,3 +50,53 @@ void Packetizer::addSender(std::shared_ptr<RTPSender> sender) {
     mSenders.push_back(sender);
 }
 
+int32_t Packetizer::requestIDRFrame() {
+    return mStreamingSource->requestIDRFrame();
+}
+
+void Packetizer::run() {
+    auto weak_this = weak_from_this();
+
+    mStreamingSource->setCallback(
+            [weak_this](const std::shared_ptr<android::SBuffer> &accessUnit) {
+                auto me = weak_this.lock();
+                if (me) {
+                    me->mRunLoop->post(
+                            makeSafeCallback(
+                                me.get(), &Packetizer::onFrame, accessUnit));
+                }
+            });
+
+    mStreamingSource->start();
+}
+
+void Packetizer::onFrame(const std::shared_ptr<android::SBuffer>& accessUnit) {
+    int64_t timeUs = accessUnit->time_us();
+    CHECK(timeUs);
+
+    auto now = std::chrono::steady_clock::now();
+
+    if (mNumSamplesRead == 0) {
+        mStartTimeMedia = timeUs;
+        mStartTimeReal = now;
+    }
+
+    ++mNumSamplesRead;
+
+    LOG(VERBOSE)
+        << "got accessUnit of size "
+        << accessUnit->size()
+        << " at time "
+        << timeUs;
+
+    packetize(accessUnit, timeUs);
+}
+
+uint32_t Packetizer::timeSinceStart() const {
+    if (mNumSamplesRead) return 0;
+
+    auto now = std::chrono::steady_clock::now();
+    return std::chrono::duration_cast<std::chrono::microseconds>(now -
+                                                                 mStartTimeReal)
+        .count();
+}
