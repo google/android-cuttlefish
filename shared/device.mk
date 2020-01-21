e012312f706903ceb902316d2da960eb7920a74b
Shuzhen Wang <shuzhenwang@google.com>
2020-01-21 12:54:08 -0800

    Cuttlefish: Uprev to 2.6 camera provider
    Also remove 2.6 provider from missing provider list.
    
    Bug: 148181647
    Test: Cuttlefish Camera CTS
    Change-Id: I88e8fc5b4cd1397ba0f6bc946b7d37bb86813b16

--- shared/device.mk
+++ shared/device.mk
@@ -237,9 +237,9 @@ PRODUCT_PACKAGES += \
 # Camera
 #
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-service-google \
+    android.hardware.camera.provider@2.6-service-google \
     libgooglecamerahwl_impl \
-    android.hardware.camera.provider@2.4-impl-google \
+    android.hardware.camera.provider@2.6-impl-google \
 
 #
 # Gatekeeper
