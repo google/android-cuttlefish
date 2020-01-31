2c348339621bd98a834dfd85b9c854ad48e1f811
Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
2020-01-31 16:29:05 +0000

    [skip ci] Merge changes I9e786c0a,Ibe6dc92b am: 0d2132abca am: 85b2b12ba1 am: 5d0d41e030
    Change-Id: I44fdd4f836e0480042a18094e0ac04cd60d0f0cd
    GitOrigin-RevId: e7df7ff09f7bec1a6c8a4aced61fecfc7f8f59ac

--- staging/common/libs/fs/shared_fd_test.cpp
+++ staging/common/libs/fs/shared_fd_test.cpp
@@ -23,48 +23,17 @@
 
 #include <string>
 
-using cvd::InbandMessageHeader;
 using cvd::SharedFD;
 
-char hello[] = "Hello, world!";
 char pipe_message[] = "Testing the pipe";
 
 TEST(SendFD, Basic) {
-  char dirname[] = "/tmp/sfdtestXXXXXX";
-  char* socket = mkdtemp(dirname);
-  EXPECT_TRUE(socket != NULL);
-  std::string path(dirname);
-  path += "/s";
-  SharedFD server = SharedFD::SocketSeqPacketServer(path.c_str(), 0700);
-  EXPECT_TRUE(server->IsOpen());
-  int rval = fork();
-  EXPECT_NE(-1, rval);
-  if (!rval) {
-    struct iovec iov { hello, sizeof(hello) };
-    SharedFD client = SharedFD::SocketSeqPacketClient(path.c_str());
-    InbandMessageHeader hdr{};
-    hdr.msg_iov = &iov;
-    hdr.msg_iovlen = 1;
-    SharedFD fds[2];
-    SharedFD::Pipe(fds, fds + 1);
-    ssize_t rval = client->SendMsgAndFDs(hdr, 0, fds);
-    printf("SendMsg sent %zd (%s)\n", rval, client->StrError());
-    exit(0);
-  }
-  server->Listen(2);
-  SharedFD peer = SharedFD::Accept(*server);
-  EXPECT_TRUE(peer->IsOpen());
-  char buf[80];
-  struct iovec iov { buf, sizeof(buf) };
-  InbandMessageHeader hdr{};
-  hdr.msg_iov = &iov;
-  hdr.msg_iovlen = 1;
   SharedFD fds[2];
-  peer->RecvMsgAndFDs(hdr, 0, &fds);
-  EXPECT_EQ(0, strcmp(buf, hello));
+  SharedFD::Pipe(fds, fds + 1);
   EXPECT_TRUE(fds[0]->IsOpen());
   EXPECT_TRUE(fds[1]->IsOpen());
   EXPECT_EQ(sizeof(pipe_message), fds[1]->Write(pipe_message, sizeof(pipe_message)));
+  char buf[80];
   EXPECT_EQ(sizeof(pipe_message), fds[0]->Read(buf, sizeof(buf)));
   EXPECT_EQ(0, strcmp(buf, pipe_message));
 }
