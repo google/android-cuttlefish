804adbfaade3a49dec7e474cbe7c103047bd2210
Sarah Chin <sarahchin@google.com>
2020-02-03 12:39:58 -0800

    Fix OOB vulnerability in setGsm/CdmaSmsBroadcastConfigInfo
    Error if length >25
    
    Test: lunch cf_x86_phone-userdeug && mm
    Bug: 144046782
    Change-Id: I9b3b2cfb8269472c02c673949082b1dad7418760

--- guest/hals/ril/libril/ril_service.cpp
+++ guest/hals/ril/libril/ril_service.cpp
@@ -1932,6 +1932,12 @@ Return<void> RadioImpl_1_5::setGsmBroadcastConfig(int32_t serial,
     }
 
     int num = configInfo.size();
+    if (num > MAX_BROADCAST_SMS_CONFIG_INFO) {
+        RLOGE("setGsmBroadcastConfig: Invalid configInfo length %s",
+                requestToString(pRI->pCI->requestNumber));
+        sendErrorResponse(pRI, RIL_E_INVALID_ARGUMENTS);
+        return Void();
+    }
     RIL_GSM_BroadcastSmsConfigInfo gsmBci[num];
     RIL_GSM_BroadcastSmsConfigInfo *gsmBciPtrs[num];
 
@@ -1979,6 +1985,12 @@ Return<void> RadioImpl_1_5::setCdmaBroadcastConfig(int32_t serial,
     }
 
     int num = configInfo.size();
+    if (num > MAX_BROADCAST_SMS_CONFIG_INFO) {
+        RLOGE("setCdmaBroadcastConfig: Invalid configInfo length %s",
+                requestToString(pRI->pCI->requestNumber));
+        sendErrorResponse(pRI, RIL_E_INVALID_ARGUMENTS);
+        return Void();
+    }
     RIL_CDMA_BroadcastSmsConfigInfo cdmaBci[num];
     RIL_CDMA_BroadcastSmsConfigInfo *cdmaBciPtrs[num];
 
