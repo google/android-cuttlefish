5233dfa86b9873628caac43964833a19fa0a06a8
Robert Shih <robertshih@google.com>
2020-02-04 11:56:56 -0800

    Remove pre-1.3 drm hals
    Bug: 139313724
    Test: x86 vts_treble_vintf_vendor_test
    Change-Id: I2972452517ef4bbc1decf68e03c0c16b4ed2a7ad

--- shared/device.mk
+++ shared/device.mk
@@ -238,8 +238,6 @@ PRODUCT_PACKAGES += \
 # Drm HAL
 #
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
     android.hardware.drm@1.3-service.clearkey \
     android.hardware.drm@1.3-service.widevine
 
