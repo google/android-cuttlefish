01e6821fc2c7fc422dcf6a8fa60aa51f73157f4f
Ivailo Karamanolev <ivailok@google.com>
2020-02-12 19:27:54 +0100

    Install default Lights AIDL HAL on cuttlefish
    Bug: 142230898
    Test: atest VtsHalLightTargetTest
    Test: atest hal_implementation_test
    Change-Id: I446caf5e0f6d70e508f6eb97916c705485c163d6

--- shared/device.mk
+++ shared/device.mk
@@ -313,9 +313,7 @@ PRODUCT_PACKAGES += \
 # Lights
 #
 PRODUCT_PACKAGES += \
-    lights.cutf \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service
+    android.hardware.lights-service.example \
 
 #
 # Keymaster HAL
