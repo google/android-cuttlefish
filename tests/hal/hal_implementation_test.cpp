90016484781e1c53eeff3af8b76d1374154d6cbf
Anthony Stange <stange@google.com>
2020-02-20 14:02:09 -0500

    Remove contexthub impl test exemption
    The contexthub HAL now has a HAL implementation for cuttlefish so the
    exemption is no longer needed.
    
    Bug: 130077250
    Test: presubmits
    Change-Id: Ic03201289943943b98963e883cdc80c0fca944e1

--- tests/hal/hal_implementation_test.cpp
+++ tests/hal/hal_implementation_test.cpp
@@ -48,7 +48,6 @@ static const std::set<std::string> kKnownMissingHidl = {
     "android.hardware.broadcastradio@2.0",
     "android.hardware.cas.native@1.0",
     "android.hardware.confirmationui@1.0",
-    "android.hardware.contexthub@1.0",
     "android.hardware.configstore@1.1", // deprecated, see b/149050985, b/149050733
     "android.hardware.fastboot@1.0",
     "android.hardware.gnss.measurement_corrections@1.1", // is sub-interface of gnss
