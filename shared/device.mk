827e25edf077af7bdefb226a9b77a414111f05e7
Alistair Delva <adelva@google.com>
2020-02-27 02:00:09 +0000

    Revert "Enable casefolding and project quotas on Cuttlefish"
    This reverts commit b2ce67da764e7f7b5b49016d05c78215156c5136.
    
    Reason for revert: broke 4.14 and 4.19 kernel testing
    
    Bug: 150321462
    Change-Id: I60337e30cdba9797b0631343fe6952714fccce4f

--- shared/device.mk
+++ shared/device.mk
@@ -25,8 +25,6 @@ PRODUCT_BUILD_BOOT_IMAGE := true
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 DISABLE_RILD_OEM_HOOK := true
 
-TARGET_USERDATAIMAGE_FILE_SYSTEM_TYPE ?= f2fs
-
 AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS += \
     product \
@@ -36,11 +34,6 @@ AB_OTA_PARTITIONS += \
 
 # Enable Virtual A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
-
-# Enable Scoped Storage related changes for f2fs
-ifeq ($(TARGET_USERDATAIMAGE_FILE_SYSTEM_TYPE),f2fs)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-endif
 
 # Properties that are not vendor-specific. These will go in the product
 # partition, instead of the vendor partition, and do not need vendor
