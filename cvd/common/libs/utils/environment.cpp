cbabb6b573c01b21056940f1ec1318998ab25fbd
Steve Kim <kwstephenkim@google.com>
2020-03-13 12:46:26 -0700

    enable crosvm sandbox for x86_64
    To enable sandbox, added command line arguments to launch_cvd
    Added build files to include the seccomp policy files in the host
    package
    
    Bug: 149774856
    Test: run locally w/ and w/o sandbox, checked the tar.gz
    Change-Id: I4557eca2e8b2167a2fa7d6f4f719603ee940acd2

--- cvd/common/libs/utils/environment.cpp
+++ cvd/common/libs/utils/environment.cpp
@@ -17,6 +17,8 @@
 #include "common/libs/utils/environment.h"
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
 
 namespace cvd {
 
@@ -27,6 +29,57 @@ std::string StringFromEnv(const std::string& varname,
     return defval;
   }
   return valstr;
+}
+
+/**
+ * at runtime, return the arch of the host: e.g. aarch64, x86_64, etc
+ *
+ * uses "`which uname` -m"
+ *
+ * @return arch string on success, "" on failure
+ */
+std::string HostArch() {
+  static std::string arch;
+  static bool cached = false;
+
+  if (cached) {
+    return arch;
+  }
+  cached = true;
+
+  // good to check if uname exists and is executable
+  // or, guarantee uname is availabe by dependency list
+  FILE* pip = popen("uname -m", "r");
+  if (!pip) {
+    return std::string{};
+  }
+
+  auto read_from_file =
+      [](FILE* fp, size_t len) {
+        /*
+         * to see if input is longer than len,
+         * we read up to len+1. If the length is len+1,
+         * then the input is too long
+         */
+        decltype(len) upper = len + 1;
+        std::string format("%");
+        format.append(std::to_string(upper)).append("s");
+        std::shared_ptr<char> buf(new char[upper],
+                                  std::default_delete<char[]>());
+        if (fscanf(fp, format.c_str(), buf.get()) == EOF) {
+          return std::string{};
+        }
+        std::string result(buf.get());
+        return (result.length() < upper) ? result : std::string{};
+      };
+  arch = read_from_file(pip, 20);
+  pclose(pip);
+
+  // l and r trim on arch
+  static const char* whitespace = "\t\n\r\f\v ";
+  arch.erase(arch.find_last_not_of(whitespace) + 1); // r trim
+  arch.erase(0, arch.find_first_not_of(whitespace)); // l trim
+  return arch;
 }
 
 }  // namespace cvd
