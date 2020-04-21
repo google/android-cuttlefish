683adf95cd435d63cbfb860e979bab467d7c6253
Enrico Granata <egranata@google.com>
2020-04-21 15:41:39 -0700

    Allow downstream Cuttlefish devices to format userdata as ext4
    This is technically already possible as one can set TARGET_USERDATAIMAGE_FILE_SYSTEM_TYPE
    to be ext4 instead of the default f2fs, but then fstab only allows for mounting
    userdata as f2fs.
    
    Allow setting an ext4-based fstab file if that's what TARGET_USERDATAIMAGE_FILE_SYSTEM_TYPE
    points to.
    There are other issues around this that will need solving before the setting actually kicks in,
    but this is a first step.
    
    Bug: 149054714
    Test: build
    Change-Id: I1857ac76bb9700a038954420361e689c79dbc77a
    Merged-In: I1857ac76bb9700a038954420361e689c79dbc77a

--- shared/device.mk
+++ shared/device.mk
@@ -204,9 +204,20 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.app_widgets.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.app_widgets.xml \
     system/bt/vendor_libs/test_vendor_lib/data/controller_properties.json:vendor/etc/bluetooth/controller_properties.json \
     device/google/cuttlefish/shared/config/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
+
+ifeq ($(TARGET_USERDATAIMAGE_FILE_SYSTEM_TYPE),f2fs)
+PRODUCT_COPY_FILES += \
     device/google/cuttlefish/shared/config/fstab:$(TARGET_COPY_OUT_RAMDISK)/fstab.cutf_cvm \
     device/google/cuttlefish/shared/config/fstab:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.cutf_cvm \
-    device/google/cuttlefish/shared/config/fstab:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.cutf_cvm \
+    device/google/cuttlefish/shared/config/fstab:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.cutf_cvm
+endif
+
+ifeq ($(TARGET_USERDATAIMAGE_FILE_SYSTEM_TYPE),ext4)
+PRODUCT_COPY_FILES += \
+    device/google/cuttlefish/shared/config/fstab.ext4:$(TARGET_COPY_OUT_RAMDISK)/fstab.cutf_cvm \
+    device/google/cuttlefish/shared/config/fstab.ext4:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.cutf_cvm \
+    device/google/cuttlefish/shared/config/fstab.ext4:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.cutf_cvm
+endif
 
 ifeq ($(TARGET_VULKAN_SUPPORT),true)
 PRODUCT_COPY_FILES += \
