fac587ae58f95569c16c50a1d872a283c135baf3
A. Cody Schuffelen <schuffelen@google.com>
2020-05-07 15:49:37 -0700

    Replace keymaster_command with AndroidKeymasterCommand
    AndroidKeymasterCommand is defined by keymaster.
    
    Bug: 155328112
    Test: m android.hardware.keymaster@4.0-service.remote
    Change-Id: Iff3c271513e74f003f3f3eeb77057da9044bb61b

--- guest/hals/keymaster/remote/remote_keymaster.cpp
+++ guest/hals/keymaster/remote/remote_keymaster.cpp
@@ -19,7 +19,6 @@
 #include <android-base/logging.h>
 #include <keymaster/android_keymaster_messages.h>
 #include <keymaster/keymaster_configuration.h>
-#include "common/libs/security/keymaster_ipc.h"
 
 namespace keymaster {
 
@@ -29,9 +28,9 @@ RemoteKeymaster::RemoteKeymaster(cvd::KeymasterChannel* channel)
 RemoteKeymaster::~RemoteKeymaster() {
 }
 
-void RemoteKeymaster::ForwardCommand(enum keymaster_command command, const Serializable& req,
+void RemoteKeymaster::ForwardCommand(AndroidKeymasterCommand command, const Serializable& req,
                                      KeymasterResponse* rsp) {
-    if (!channel_->SendMessage(command, req)) {
+    if (!channel_->SendRequest(command, req)) {
         LOG(ERROR) << "Failed to send keymaster message: " << command;
         rsp->error = KM_ERROR_UNKNOWN_ERROR;
         return;
@@ -69,46 +68,46 @@ bool RemoteKeymaster::Initialize() {
 }
 
 void RemoteKeymaster::GetVersion(const GetVersionRequest& request, GetVersionResponse* response) {
-    ForwardCommand(KM_GET_VERSION, request, response);
+    ForwardCommand(GET_VERSION, request, response);
 }
 
 void RemoteKeymaster::SupportedAlgorithms(const SupportedAlgorithmsRequest& request,
                                           SupportedAlgorithmsResponse* response) {
-    ForwardCommand(KM_GET_SUPPORTED_ALGORITHMS, request, response);
+    ForwardCommand(GET_SUPPORTED_ALGORITHMS, request, response);
 }
 
 void RemoteKeymaster::SupportedBlockModes(const SupportedBlockModesRequest& request,
                                           SupportedBlockModesResponse* response) {
-    ForwardCommand(KM_GET_SUPPORTED_BLOCK_MODES, request, response);
+    ForwardCommand(GET_SUPPORTED_BLOCK_MODES, request, response);
 }
 
 void RemoteKeymaster::SupportedPaddingModes(const SupportedPaddingModesRequest& request,
                                             SupportedPaddingModesResponse* response) {
-    ForwardCommand(KM_GET_SUPPORTED_PADDING_MODES, request, response);
+    ForwardCommand(GET_SUPPORTED_PADDING_MODES, request, response);
 }
 
 void RemoteKeymaster::SupportedDigests(const SupportedDigestsRequest& request,
                                        SupportedDigestsResponse* response) {
-    ForwardCommand(KM_GET_SUPPORTED_DIGESTS, request, response);
+    ForwardCommand(GET_SUPPORTED_DIGESTS, request, response);
 }
 
 void RemoteKeymaster::SupportedImportFormats(const SupportedImportFormatsRequest& request,
                                              SupportedImportFormatsResponse* response) {
-    ForwardCommand(KM_GET_SUPPORTED_IMPORT_FORMATS, request, response);
+    ForwardCommand(GET_SUPPORTED_IMPORT_FORMATS, request, response);
 }
 
 void RemoteKeymaster::SupportedExportFormats(const SupportedExportFormatsRequest& request,
                                              SupportedExportFormatsResponse* response) {
-    ForwardCommand(KM_GET_SUPPORTED_EXPORT_FORMATS, request, response);
+    ForwardCommand(GET_SUPPORTED_EXPORT_FORMATS, request, response);
 }
 
 void RemoteKeymaster::AddRngEntropy(const AddEntropyRequest& request,
                                     AddEntropyResponse* response) {
-    ForwardCommand(KM_ADD_RNG_ENTROPY, request, response);
+    ForwardCommand(ADD_RNG_ENTROPY, request, response);
 }
 
 void RemoteKeymaster::Configure(const ConfigureRequest& request, ConfigureResponse* response) {
-    ForwardCommand(KM_CONFIGURE, request, response);
+    ForwardCommand(CONFIGURE, request, response);
 }
 
 void RemoteKeymaster::GenerateKey(const GenerateKeyRequest& request,
@@ -120,83 +119,83 @@ void RemoteKeymaster::GenerateKey(const GenerateKeyRequest& request,
         datedRequest.key_description.push_back(TAG_CREATION_DATETIME, java_time(time(NULL)));
     }
 
-    ForwardCommand(KM_GENERATE_KEY, datedRequest, response);
+    ForwardCommand(GENERATE_KEY, datedRequest, response);
 }
 
 void RemoteKeymaster::GetKeyCharacteristics(const GetKeyCharacteristicsRequest& request,
                                             GetKeyCharacteristicsResponse* response) {
-    ForwardCommand(KM_GET_KEY_CHARACTERISTICS, request, response);
+    ForwardCommand(GET_KEY_CHARACTERISTICS, request, response);
 }
 
 void RemoteKeymaster::ImportKey(const ImportKeyRequest& request, ImportKeyResponse* response) {
-    ForwardCommand(KM_IMPORT_KEY, request, response);
+    ForwardCommand(IMPORT_KEY, request, response);
 }
 
 void RemoteKeymaster::ImportWrappedKey(const ImportWrappedKeyRequest& request,
                                        ImportWrappedKeyResponse* response) {
-    ForwardCommand(KM_IMPORT_WRAPPED_KEY, request, response);
+    ForwardCommand(IMPORT_WRAPPED_KEY, request, response);
 }
 
 void RemoteKeymaster::ExportKey(const ExportKeyRequest& request, ExportKeyResponse* response) {
-    ForwardCommand(KM_EXPORT_KEY, request, response);
+    ForwardCommand(EXPORT_KEY, request, response);
 }
 
 void RemoteKeymaster::AttestKey(const AttestKeyRequest& request, AttestKeyResponse* response) {
-    ForwardCommand(KM_ATTEST_KEY, request, response);
+    ForwardCommand(ATTEST_KEY, request, response);
 }
 
 void RemoteKeymaster::UpgradeKey(const UpgradeKeyRequest& request, UpgradeKeyResponse* response) {
-    ForwardCommand(KM_UPGRADE_KEY, request, response);
+    ForwardCommand(UPGRADE_KEY, request, response);
 }
 
 void RemoteKeymaster::DeleteKey(const DeleteKeyRequest& request, DeleteKeyResponse* response) {
-    ForwardCommand(KM_DELETE_KEY, request, response);
+    ForwardCommand(DELETE_KEY, request, response);
 }
 
 void RemoteKeymaster::DeleteAllKeys(const DeleteAllKeysRequest& request,
                                     DeleteAllKeysResponse* response) {
-    ForwardCommand(KM_DELETE_ALL_KEYS, request, response);
+    ForwardCommand(DELETE_ALL_KEYS, request, response);
 }
 
 void RemoteKeymaster::BeginOperation(const BeginOperationRequest& request,
                                      BeginOperationResponse* response) {
-    ForwardCommand(KM_BEGIN_OPERATION, request, response);
+    ForwardCommand(BEGIN_OPERATION, request, response);
 }
 
 void RemoteKeymaster::UpdateOperation(const UpdateOperationRequest& request,
                                       UpdateOperationResponse* response) {
-    ForwardCommand(KM_UPDATE_OPERATION, request, response);
+    ForwardCommand(UPDATE_OPERATION, request, response);
 }
 
 void RemoteKeymaster::FinishOperation(const FinishOperationRequest& request,
                                       FinishOperationResponse* response) {
-    ForwardCommand(KM_FINISH_OPERATION, request, response);
+    ForwardCommand(FINISH_OPERATION, request, response);
 }
 
 void RemoteKeymaster::AbortOperation(const AbortOperationRequest& request,
                                      AbortOperationResponse* response) {
-    ForwardCommand(KM_ABORT_OPERATION, request, response);
+    ForwardCommand(ABORT_OPERATION, request, response);
 }
 
 GetHmacSharingParametersResponse RemoteKeymaster::GetHmacSharingParameters() {
     // Dummy empty buffer to allow ForwardCommand to have something to serialize
     Buffer request;
     GetHmacSharingParametersResponse response;
-    ForwardCommand(KM_GET_HMAC_SHARING_PARAMETERS, request, &response);
+    ForwardCommand(GET_HMAC_SHARING_PARAMETERS, request, &response);
     return response;
 }
 
 ComputeSharedHmacResponse RemoteKeymaster::ComputeSharedHmac(
         const ComputeSharedHmacRequest& request) {
     ComputeSharedHmacResponse response;
-    ForwardCommand(KM_COMPUTE_SHARED_HMAC, request, &response);
+    ForwardCommand(COMPUTE_SHARED_HMAC, request, &response);
     return response;
 }
 
 VerifyAuthorizationResponse RemoteKeymaster::VerifyAuthorization(
         const VerifyAuthorizationRequest& request) {
     VerifyAuthorizationResponse response;
-    ForwardCommand(KM_VERIFY_AUTHORIZATION, request, &response);
+    ForwardCommand(VERIFY_AUTHORIZATION, request, &response);
     return response;
 }
 
