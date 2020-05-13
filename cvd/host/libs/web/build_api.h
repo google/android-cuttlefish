2728126d97d0078c4a5a570d87d60485d3b11180
A. Cody Schuffelen <schuffelen@google.com>
2020-05-13 16:51:15 -0700

    Use the product name for discovering output files.
    Bug: 156535024
    Test: fetch_cvd with target in b/156535024
    Test: fetch_cvd on local images
    Change-Id: I185dc178374ae8a35a9a92176a807b00c079a5b9

--- cvd/host/libs/web/build_api.h
+++ cvd/host/libs/web/build_api.h
@@ -56,6 +56,7 @@ struct DeviceBuild {
 
   std::string id;
   std::string target;
+  std::string product;
 };
 
 std::ostream& operator<<(std::ostream&, const DeviceBuild&);
@@ -63,12 +64,12 @@ std::ostream& operator<<(std::ostream&, const DeviceBuild&);
 struct DirectoryBuild {
   // TODO(schuffelen): Support local builds other than "eng"
   DirectoryBuild(const std::vector<std::string>& paths,
-                 const std::string& target)
-      : paths(paths), target(target), id("eng") {}
+                 const std::string& target);
 
   std::vector<std::string> paths;
   std::string target;
   std::string id;
+  std::string product;
 };
 
 std::ostream& operator<<(std::ostream&, const DirectoryBuild&);
@@ -90,6 +91,8 @@ public:
                             const std::string& target);
 
   std::string BuildStatus(const DeviceBuild&);
+
+  std::string ProductName(const DeviceBuild&);
 
   std::vector<Artifact> Artifacts(const DeviceBuild&);
 
