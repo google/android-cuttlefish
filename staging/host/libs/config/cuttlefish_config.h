c9c9384dfb001afb10dfa0b196d7c07089daa37a
Wonguk Jeong <wonguk.jeong@samsung.com>
2020-05-17 09:38:19 +0000

    [skip ci] Add ril_dns parameter to set mobile network dns am: ddb7a3ad16 am: 236b52a666 am: 26763695c6 am: cf524b8ceb am: 973bf5b5dc
    Change-Id: I3a1772e8b9541931d9e09aefcf8d46303c56b0d8
    GitOrigin-RevId: 2878719de6b49cd65a8cf961ade4cdfe91d46371

--- staging/host/libs/config/cuttlefish_config.h
+++ staging/host/libs/config/cuttlefish_config.h
@@ -309,6 +309,10 @@ class CuttlefishConfig {
   void set_sig_server_strict(bool strict);
   bool sig_server_strict() const;
 
+  // The dns address of mobile network (RIL)
+  void set_ril_dns(const std::string& ril_dns);
+  std::string ril_dns() const;
+
   class InstanceSpecific;
   class MutableInstanceSpecific;
 
