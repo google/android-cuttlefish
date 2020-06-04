01a06e68c70e66bd29d326e91da15fb6568c9a8b
Hao Chen <chenhaosjtuacm@google.com>
2020-06-03 18:30:57 -0700

    [skip ci] Run VHAL server with launch_cvd
    Test: build and create Acloud local instance, also tested 2 cvd
    instances by `launch_cvd`, works as normal
    Bug: 157603385
    
    Change-Id: I6c28712f9386d7ad08606786a4bf48ac9b3f5c82
    GitOrigin-RevId: 75f825b90ff135a7c421e800684562b8c6b04252

--- staging/host/commands/assemble_cvd/flags.cc
+++ staging/host/commands/assemble_cvd/flags.cc
@@ -234,6 +234,8 @@ DEFINE_string(logcat_mode, "", "How to send android's log messages from "
                                "guest to host. One of [serial, vsock]");
 DEFINE_bool(enable_tombstone_receiver, true, "Enables the tombstone logger on "
             "both the guest and the host");
+DEFINE_bool(enable_vehicle_hal_grpc_server, true, "Enables the vehicle HAL "
+            "emulation gRPC server on the host");
 DEFINE_bool(use_bootloader, false, "Boots the device using a bootloader");
 DEFINE_string(bootloader, "", "Bootloader binary path");
 DEFINE_string(boot_slot, "", "Force booting into the given slot. If empty, "
@@ -459,6 +461,10 @@ vsoc::CuttlefishConfig InitializeCuttlefishConfiguration(
   tmp_config_obj.set_tombstone_receiver_binary(
       vsoc::DefaultHostArtifactsPath("bin/tombstone_receiver"));
 
+  tmp_config_obj.set_enable_vehicle_hal_grpc_server(FLAGS_enable_vehicle_hal_grpc_server);
+  tmp_config_obj.set_vehicle_hal_grpc_server_binary(
+      vsoc::DefaultHostArtifactsPath("bin/android.hardware.automotive.vehicle@2.0-virtualization-grpc-server"));
+
   tmp_config_obj.set_use_bootloader(FLAGS_use_bootloader);
   tmp_config_obj.set_bootloader(FLAGS_bootloader);
 
@@ -502,6 +508,7 @@ vsoc::CuttlefishConfig InitializeCuttlefishConfiguration(
     instance.set_adb_ip_and_port("127.0.0.1:" + std::to_string(6520 + num - 1));
     instance.set_tpm_port(2321 + (num * 2) - 2);
     instance.set_tombstone_receiver_port(6600 + num - 1);
+    instance.set_vehicle_hal_server_port(9210 + num - 1);
     instance.set_logcat_port(6700 + num - 1);
     instance.set_config_server_port(6800 + num - 1);
 
