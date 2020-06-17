b22e34e63978f78d9a5e47c3c0ef6f120902b9fd
A. Cody Schuffelen <schuffelen@google.com>
2020-06-17 14:59:47 -0700

    Add host support for gatekeeper.
    SoftGateKeeper is imported from here:
    https://cs.android.com/android/platform/superproject/+/master:hardware/interfaces/gatekeeper/1.0/software/SoftGateKeeper.h;drc=43773f6c42191841fbd030e298e223e3e27206e1
    
    Test: make -j && launch_cvd
    Bug: 155694540
    Change-Id: I6e82747a045b43f5d50c3c6dac8aa8e769ba1992

--- host/libs/config/cuttlefish_config.cpp
+++ host/libs/config/cuttlefish_config.cpp
@@ -185,6 +185,7 @@ const char* kRilDns = "ril_dns";
 const char* kKgdb = "kgdb";
 
 const char* kKeymasterVsockPort = "keymaster_vsock_port";
+const char* kGatekeeperVsockPort = "gatekeeper_vsock_port";
 const char* kWifiMacAddress = "wifi_mac_address";
 }  // namespace
 
@@ -669,6 +670,14 @@ int CuttlefishConfig::InstanceSpecific::keymaster_vsock_port() const {
 
 void CuttlefishConfig::MutableInstanceSpecific::set_keymaster_vsock_port(int keymaster_vsock_port) {
   (*Dictionary())[kKeymasterVsockPort] = keymaster_vsock_port;
+}
+
+int CuttlefishConfig::InstanceSpecific::gatekeeper_vsock_port() const {
+  return (*Dictionary())[kGatekeeperVsockPort].asInt();
+}
+
+void CuttlefishConfig::MutableInstanceSpecific::set_gatekeeper_vsock_port(int gatekeeper_vsock_port) {
+  (*Dictionary())[kGatekeeperVsockPort] = gatekeeper_vsock_port;
 }
 
 int CuttlefishConfig::InstanceSpecific::tombstone_receiver_port() const {
