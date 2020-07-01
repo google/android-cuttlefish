6c3b1e534435b817bc73a9b63dd2a9c9032d412e
Alistair Delva <adelva@google.com>
2020-07-01 14:22:26 -0700

    Specify 'format=raw' for sdcard block device
    Before adding sdcard, all block devices set up for qemu were qcow2, so
    the autodetection worked fine. Now we have sdcard, which is actually
    raw, and the autodetection fails, it sets it up as raw, but the device
    won't be writable:
    
    Automatically detecting the format is dangerous for raw images, write
    operations on block 0 will be restricted.
    Specify the 'raw' format explicitly to remove the restrictions.
    
    Specify format=raw for subsequent block devices to future-proof this.
    
    Change-Id: I7815d2b47702ed4d8e38922b9baec529e3a88581

--- host/libs/vm_manager/qemu_manager.cpp
+++ host/libs/vm_manager/qemu_manager.cpp
@@ -172,10 +172,11 @@ std::vector<cuttlefish::Command> QemuManager::StartCommands() {
 
   for (size_t i = 0; i < instance.virtual_disk_paths().size(); i++) {
     auto bootindex = i == 0 ? ",bootindex=1" : "";
+    auto format = i == 0 ? "" : ",format=raw";
     auto disk = instance.virtual_disk_paths()[i];
     qemu_cmd.AddParameter("-drive");
     qemu_cmd.AddParameter("file=", disk, ",if=none,id=drive-virtio-disk", i,
-                          ",aio=threads");
+                          ",aio=threads", format);
     qemu_cmd.AddParameter("-device");
     qemu_cmd.AddParameter("virtio-blk-pci,scsi=off,drive=drive-virtio-disk", i,
                           ",id=virtio-disk", i, bootindex);
