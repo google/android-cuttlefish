d29d737247090d29055ef5bb001f8b0f7f521dbf
Treehugger Robot <treehugger-gerrit@google.com>
2020-07-06 19:59:31 +0000

    [skip ci] Merge changes Iafd018b8,I717a7797 am: 09ced002dd am: bfbef9659c am: 8fed92e6af am: 4f10b2f970 am: d1c728fca9
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/1353820
    
    Change-Id: I62f4f3459d2fd6af47c932c471bbe2cf4e80aabd
    GitOrigin-RevId: 48ca927622f51a340cbf2a51e72c5dc06aeb47ff

--- staging/host/frontend/gcastv2/signaling_server/server_config.cpp
+++ staging/host/frontend/gcastv2/signaling_server/server_config.cpp
@@ -32,7 +32,9 @@ Json::Value ServerConfig::ToJson() const {
   Json::Value ice_servers(Json::ValueType::arrayValue);
   for (const auto& str : stun_servers_) {
     Json::Value server;
-    server["urls"] = StartsWith(str, kStunPrefix)? str: kStunPrefix + str;
+    Json::Value urls(Json::ValueType::arrayValue);
+    urls.append(StartsWith(str, kStunPrefix)? str: kStunPrefix + str);
+    server["urls"] = urls;
     ice_servers.append(server);
   }
   Json::Value server_config;
