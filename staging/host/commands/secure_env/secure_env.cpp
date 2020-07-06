7d27e6db59f41c6de471da29298d19afcc2cd7eb
Treehugger Robot <treehugger-gerrit@google.com>
2020-07-06 21:39:52 +0000

    Merge "Update secure env to support using the in-process TPM." am: cbff025aa4 am: b450e2827d am: af69b502b8 am: 739c3c6bba am: ab111ccdc3
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/1325373
    
    Change-Id: I8f14e4eb936aa62ee09d998db6e7e3a114f0acf7
    GitOrigin-RevId: 78da4f1e6e438c15f9d7f1869d4cbd8dbc66bc16

--- staging/host/commands/secure_env/secure_env.cpp
+++ staging/host/commands/secure_env/secure_env.cpp
@@ -17,10 +17,15 @@
 #include <gflags/gflags.h>
 #include <keymaster/android_keymaster.h>
 #include <keymaster/contexts/pure_soft_keymaster_context.h>
+#include <tss2/tss2_esys.h>
+#include <tss2/tss2_rc.h>
 
 #include "common/libs/fs/shared_fd.h"
 #include "common/libs/security/keymaster_channel.h"
+#include "host/commands/secure_env/in_process_tpm.h"
 #include "host/commands/secure_env/keymaster_responder.h"
+#include "host/commands/secure_env/tpm_keymaster_context.h"
+#include "host/commands/secure_env/tpm_resource_manager.h"
 #include "host/libs/config/logging.h"
 
 // Copied from AndroidKeymaster4Device
@@ -28,14 +33,60 @@ constexpr size_t kOperationTableSize = 16;
 
 DEFINE_int32(keymaster_fd, -1, "A file descriptor for keymaster communication");
 
+DEFINE_string(keymaster_impl,
+              "software",
+              "The keymaster implementation. "
+              "\"in_process_tpm\" or \"software\"");
+
 int main(int argc, char** argv) {
   cuttlefish::DefaultSubprocessLogging(argv);
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   // keymaster::AndroidKeymaster puts the given pointer into a UniquePtr,
   // taking ownership.
-  keymaster::PureSoftKeymasterContext* keymaster_context
-      = new keymaster::PureSoftKeymasterContext(KM_SECURITY_LEVEL_SOFTWARE);
-  keymaster::AndroidKeymaster keymaster{keymaster_context, kOperationTableSize};
+  keymaster::KeymasterContext* keymaster_context;
+
+  std::unique_ptr<InProcessTpm> in_process_tpm;
+  std::unique_ptr<ESYS_CONTEXT, void(*)(ESYS_CONTEXT*)> esys(
+      nullptr, [](ESYS_CONTEXT* esys) { Esys_Finalize(&esys); });
+  std::unique_ptr<TpmResourceManager> resource_manager;
+
+  if (FLAGS_keymaster_impl == "software") {
+    keymaster_context =
+        new keymaster::PureSoftKeymasterContext(KM_SECURITY_LEVEL_SOFTWARE);
+  } else if (FLAGS_keymaster_impl == "in_process_tpm") {
+    in_process_tpm.reset(new InProcessTpm());
+    ESYS_CONTEXT* esys_ptr = nullptr;
+    auto rc =
+        Esys_Initialize(&esys_ptr, in_process_tpm->TctiContext(), nullptr);
+    if (rc != TPM2_RC_SUCCESS) {
+      LOG(FATAL) << "Could not initialize esys: " << Tss2_RC_Decode(rc)
+                 << " (" << rc << ")";
+    }
+    esys.reset(esys_ptr);
+    rc = Esys_Startup(esys.get(), TPM2_SU_CLEAR);
+    if (rc != TPM2_RC_SUCCESS) {
+      LOG(FATAL) << "TPM2_Startup failed: " << Tss2_RC_Decode(rc)
+                 << " (" << rc << ")";
+    }
+    // TODO(schuffelen): Call this only on first boot.
+    rc = Esys_Clear(
+        esys.get(),
+        ESYS_TR_RH_PLATFORM,
+        ESYS_TR_PASSWORD,
+        ESYS_TR_NONE,
+        ESYS_TR_NONE);
+    if (rc != TPM2_RC_SUCCESS) {
+      LOG(FATAL) << "TPM2_Clear failed: " << Tss2_RC_Decode(rc)
+                 << " (" << rc << ")";
+    }
+    resource_manager.reset(new TpmResourceManager(esys.get()));
+    keymaster_context = new TpmKeymasterContext(resource_manager.get());
+  } else {
+    LOG(FATAL) << "Unknown keymaster implementation " << FLAGS_keymaster_impl;
+    return -1;
+  }
+  keymaster::AndroidKeymaster keymaster{
+      keymaster_context, kOperationTableSize};
 
   CHECK(FLAGS_keymaster_fd != -1)
       << "TODO(schuffelen): Add keymaster_fd alternative";
