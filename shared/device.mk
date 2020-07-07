a4dc14267fb890b82df85291f82f0a4e08f31563
Mikhail Naganov <mnaganov@google.com>
2020-07-07 22:37:51 +0000

    Remove Soundtrigger HIDL interface from cuttlefish
    cuttlefish doesn't have soundtrigger hardware and lacks
    the corresponding vendor binary module. Leaving the HIDL
    interface for soundtrigger was causing soundtrigger
    middleware to believe that something is wrong with the
    HAL and perform unneeded restart of audio HAL service
    during device boot.
    
    Bug: 151795571
    Bug: 159966243
    Test: verify that cuttlefish device boots and audio server
          and HAL do not get restarted during the boot
    Change-Id: I2ba4bbd125f3697601566208b48a7220cea86bac
    Merged-In: I2ba4bbd125f3697601566208b48a7220cea86bac

--- shared/device.mk
+++ shared/device.mk
@@ -285,7 +285,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio@2.0-service \
-    android.hardware.soundtrigger@2.3-impl \
 
 #
 # BiometricsFace HAL
