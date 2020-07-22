8b227257b328dc9c7de721f47f36612f59ed1dfd
Treehugger Robot <treehugger-gerrit@google.com>
2020-07-22 00:52:31 +0000

    [skip ci] Merge "Add host support for gatekeeper." am: 71b29ce28f am: 57da9c0f14 am: 10ca3cd867 am: 47e390c0c9 am: cead0d9b2f
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/1341987
    
    Change-Id: I7671325b4d4bcdb99c6e5fc88d614f79e3f7c67e
    GitOrigin-RevId: 237c81f7004f0ce206363242f556ec50d3e6629f

--- staging/host/commands/secure_env/secure_env.cpp
+++ staging/host/commands/secure_env/secure_env.cpp
@@ -13,6 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <thread>
+
 #include <android-base/logging.h>
 #include <gflags/gflags.h>
 #include <keymaster/android_keymaster.h>
@@ -21,9 +23,12 @@
 #include <tss2/tss2_rc.h>
 
 #include "common/libs/fs/shared_fd.h"
+#include "common/libs/security/gatekeeper_channel.h"
 #include "common/libs/security/keymaster_channel.h"
+#include "host/commands/secure_env/gatekeeper_responder.h"
 #include "host/commands/secure_env/in_process_tpm.h"
 #include "host/commands/secure_env/keymaster_responder.h"
+#include "host/commands/secure_env/soft_gatekeeper.h"
 #include "host/commands/secure_env/tpm_keymaster_context.h"
 #include "host/commands/secure_env/tpm_resource_manager.h"
 #include "host/libs/config/logging.h"
@@ -32,6 +37,7 @@
 constexpr size_t kOperationTableSize = 16;
 
 DEFINE_int32(keymaster_fd, -1, "A file descriptor for keymaster communication");
+DEFINE_int32(gatekeeper_fd, -1, "A file descriptor for gatekeeper communication");
 
 DEFINE_string(keymaster_impl,
               "software",
@@ -44,6 +50,8 @@ int main(int argc, char** argv) {
   // keymaster::AndroidKeymaster puts the given pointer into a UniquePtr,
   // taking ownership.
   keymaster::KeymasterContext* keymaster_context;
+
+  gatekeeper::SoftGateKeeper gatekeeper;
 
   std::unique_ptr<InProcessTpm> in_process_tpm;
   std::unique_ptr<ESYS_CONTEXT, void(*)(ESYS_CONTEXT*)> esys(
@@ -90,16 +98,47 @@ int main(int argc, char** argv) {
 
   CHECK(FLAGS_keymaster_fd != -1)
       << "TODO(schuffelen): Add keymaster_fd alternative";
-  auto server = cuttlefish::SharedFD::Dup(FLAGS_keymaster_fd);
-  CHECK(server->IsOpen()) << "Could not dup server fd: " << server->StrError();
+  auto keymaster_server = cuttlefish::SharedFD::Dup(FLAGS_keymaster_fd);
+  CHECK(keymaster_server->IsOpen()) << "Could not dup server fd: "
+                                    << keymaster_server->StrError();
   close(FLAGS_keymaster_fd);
-  auto conn = cuttlefish::SharedFD::Accept(*server);
-  CHECK(conn->IsOpen()) << "Unable to open connection: " << conn->StrError();
-  cuttlefish::KeymasterChannel keymaster_channel(conn);
 
-  KeymasterResponder keymaster_responder(&keymaster_channel, &keymaster);
+  CHECK(FLAGS_gatekeeper_fd != -1)
+      << "TODO(schuffelen): Add gatekeeper_fd alternative";
+  auto gatekeeper_server = cuttlefish::SharedFD::Dup(FLAGS_gatekeeper_fd);
+  CHECK(gatekeeper_server->IsOpen()) << "Could not dup server fd: "
+                                     << gatekeeper_server->StrError();
+  close(FLAGS_gatekeeper_fd);
 
-  // TODO(schuffelen): Do this in a thread when adding other HALs
-  while (keymaster_responder.ProcessMessage()) {
-  }
+
+  std::thread keymaster_thread([&keymaster_server, &keymaster]() {
+    while (true) {
+      auto keymaster_conn = cuttlefish::SharedFD::Accept(*keymaster_server);
+      CHECK(keymaster_conn->IsOpen()) << "Unable to open connection: "
+                                      << keymaster_conn->StrError();
+      cuttlefish::KeymasterChannel keymaster_channel(keymaster_conn);
+
+      KeymasterResponder keymaster_responder(&keymaster_channel, &keymaster);
+
+      while (keymaster_responder.ProcessMessage()) {
+      }
+    }
+  });
+
+  std::thread gatekeeper_thread([&gatekeeper_server, &gatekeeper]() {
+    while (true) {
+      auto gatekeeper_conn = cuttlefish::SharedFD::Accept(*gatekeeper_server);
+      CHECK(gatekeeper_conn->IsOpen()) << "Unable to open connection: "
+                                      << gatekeeper_conn->StrError();
+      cuttlefish::GatekeeperChannel gatekeeper_channel(gatekeeper_conn);
+
+      GatekeeperResponder gatekeeper_responder(&gatekeeper_channel, &gatekeeper);
+
+      while (gatekeeper_responder.ProcessMessage()) {
+      }
+    }
+  });
+
+  keymaster_thread.join();
+  gatekeeper_thread.join();
 }
