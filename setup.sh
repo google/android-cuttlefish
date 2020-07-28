c3857d1a0441ae9962691994fa8731e0606d6bd2
Iliyan Malchev <malchev@google.com>
2020-07-27 22:36:19 -0700

    cleanup of setup.sh
    -- autoindent the whole thing (it was a mess of mixed indentation)
    -- cvd_exists --> cvd_container_exists
    -- add cvd_container_running to test whether a container is up and
       running
    -- when passing the -x option to cvd_docker_create, error out when
       DISPLAY is not set or is empty
    -- Passing -x to cvd_docker_create does not imply -s anymore.  You can
       either:
    	source setup.sh
    	cvd_docker_create cf -x
    	cvd_login_cf
       or:
    	source setup.sh
    	cvd_docker_create cf -s -x # automatically logs you in
    -- added a loop to wait for the container to come up after it gets
       created
    -- pass the port mappings into docker run and use docker exec in
       cvd_login_<name> instead of ssh; this is needed for the -x option,
       since we cannot easily pass DISPLAY via the ssh session, and is
       faster
    -- on source setup.sh, clean up stale cvd_{start,login,stop}_<name>
       entries.  If containers get manipulater from another shell session,
       these definitions can get out of sync; re-sourcing setup.sh will
       clean them up.
    
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- setup.sh
+++ setup.sh
@@ -1,84 +1,89 @@
 if [ "${BASH_SOURCE[0]}" -ef "$0" ]; then
-        echo "source this script, do not execute it!"
-        exit 1
+	echo "source this script, do not execute it!"
+	exit 1
 fi
 
 # set -o errexit
 # set -x
 
 function cvd_get_id {
-  echo "${1:-cuttlefish}"
+	echo "${1:-cuttlefish}"
 }
 
-function cvd_exists {
-  local name="$(cvd_get_id $1)"
-  [[ $(docker ps --filter "name=^/${name}$" --format '{{.Names}}') == "${name}" ]] && echo "${name}";
+function cvd_container_exists {
+	local name="$(cvd_get_id $1)"
+	[[ $(docker ps --filter "name=^/${name}$" --format '{{.Names}}') == "${name}" ]] && echo "${name}";
+}
+
+function cvd_container_running {
+	local name="$(cvd_get_id $1)"
+	[[ $(docker inspect -f "{{.State.Running}}" ${name}) == 'true' ]] && echo "${name}"
 }
 
 function cvd_get_ip {
-  local name="$(cvd_exists $1)"
-  [[ -n "${name}" ]] && \
-    echo $(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "${name}")
-}
+	local name="$(cvd_container_exists $1)"
+	[[ -n "${name}" ]] && \
+		echo $(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "${name}")
+	}
 
 function cvd_docker_list {
-  docker ps -a --filter="ancestor=cuttlefish"
+	docker ps -a --filter="ancestor=cuttlefish"
 }
 
 function help_on_container_create {
-  echo "   cvd_docker_create <options> [NAME] # by default names 'cuttlefish'"
-  echo "     Options:"
-  echo "       -n | --name jellyfish        : override default name"
-  echo "                                    : for backward compat, [NAME] will override this"
-  echo "       -s | --singleshot            : run the container, log in once, then delete it on logout"
-  echo "                                    : otherwise, the container is created as a daemon"
-  echo "       -x | --with_host_x           : run the container in singleshot and"
-  echo "                                    : share X of the docker host"
-  echo "       -a | --android_build_top dir : equivalent as setting ANDROID_BUILD_TOP"
-  echo "                                    : if ANDROID_BUILD_TOP is already set, the option is ignored"
-  echo "                                    : dir should be the level directory of android tree"
-  echo "       -m | --share_dir dir1:dir2   : mount a host directory dir1 at dir2 of docker container"
-  echo "                                    : dir1 should be an absolute path or relative path to $PWD"
-  echo "                                    : dir2 should be an absolute path or relative path to /home/vsoc-01/"
-  echo "                                    : $HOME is not allowed as dir1"
-  echo "                                    : /home/vsoc-01 is not allowed as dir2"
-  echo "                                    : For multiple mounts, use multiple -m options per pair"
-  echo "       -h | --help                  : print this help message"
-  echo "        The optional [NAME] will override -n option for backward compatibility"
+	echo "   cvd_docker_create <options> [NAME] # by default names 'cuttlefish'"
+	echo "     Options:"
+	echo "       -n | --name jellyfish        : override default name"
+	echo "                                    : for backward compat, [NAME] will override this"
+	echo "       -s | --singleshot            : run the container, log in once, then delete it on logout"
+	echo "                                    : otherwise, the container is created as a daemon"
+	echo "       -x | --with_host_x           : run the container in singleshot and"
+	echo "                                    : share X of the docker host"
+	echo "       -a | --android_build_top dir : equivalent as setting ANDROID_BUILD_TOP"
+	echo "                                    : if ANDROID_BUILD_TOP is already set, the option is ignored"
+	echo "                                    : dir should be the level directory of android tree"
+	echo "       -m | --share_dir dir1:dir2   : mount a host directory dir1 at dir2 of docker container"
+	echo "                                    : dir1 should be an absolute path or relative path to $PWD"
+	echo "                                    : dir2 should be an absolute path or relative path to /home/vsoc-01/"
+	echo "                                    : $HOME is not allowed as dir1"
+	echo "                                    : /home/vsoc-01 is not allowed as dir2"
+	echo "                                    : For multiple mounts, use multiple -m options per pair"
+	echo "       -h | --help                  : print this help message"
+	echo "        The optional [NAME] will override -n option for backward compatibility"
 }
 
 function help_on_sourcing {
-  echo "Create a cuttlefish container:"
-  help_on_container_create
-  echo ""
-  echo "To list existing Cuttlefish containers:"
-  echo "   cvd_docker_list"
-  echo ""
-  echo "Existing Cuttlefish containers:"
-  cvd_docker_list
+	echo "Create a cuttlefish container:"
+	help_on_container_create
+	echo ""
+	echo "To list existing Cuttlefish containers:"
+	echo "   cvd_docker_list"
+	echo ""
+	echo "Existing Cuttlefish containers:"
+	cvd_docker_list
 }
 
 function help_on_container_start {
-  local name=$1
+	local name=$1
 
-  echo "Log into container ${name}: $(__gen_login_func_name ${name})"
-#  echo "Log into container ${name} with ssh:"
-#  echo "    ssh vsoc-01@\${ip_${name}"}
-#  echo "Log into container ${name} with docker:"
-#  echo "    docker exec -it --user vsoc-01 $(cvd_get_id ${name}) /bin/bash"
-  echo "Start Cuttlefish: $(__gen_start_func_name ${name})"
-  echo "Stop Cuttlefish: $(__gen_stop_func_name ${name})"
-  echo "Delete container ${name}:"
-  [[ "${name}" == 'cuttlefish' ]] && echo "    cvd_docker_rm"
-  [[ "${name}" != 'cuttlefish' ]] && echo "    cvd_docker_rm ${name}"
+	echo "Log into container ${name}: $(__gen_login_func_name ${name})"
+	#  echo "Log into container ${name} with ssh:"
+	#  echo "    ssh vsoc-01@\${ip_${name}"}
+	#  echo "Log into container ${name} with docker:"
+	#  echo "    docker exec -it --user vsoc-01 $(cvd_get_id ${name}) /bin/bash"
+	echo "Start Cuttlefish: $(__gen_start_func_name ${name})"
+	echo "Stop Cuttlefish: $(__gen_stop_func_name ${name})"
+	echo "Delete container ${name}:"
+	[[ "${name}" == 'cuttlefish' ]] && echo "    cvd_docker_rm"
+	[[ "${name}" != 'cuttlefish' ]] && echo "    cvd_docker_rm ${name}"
 }
 
 function is_absolute_path {
-  local sub=$1
-  if [[ -n ${sub:0:1} ]] && [[ "${sub:0:1}" == "/" ]]; then
-    return 0
-  fi
-  return 1
+	local sub=$1
+	if [[ -n ${sub:0:1} ]] && [[ "${sub:0:1}" == "/" ]]; then
+		return 0
+	fi
+	return 1
 }
 
 # set up android_prod_out_dir, android_host_out_dir,
@@ -111,14 +116,14 @@ function setup_android_build_envs {
 }
 
 function cvd_docker_create {
-  local name=""
-  local singleshot="false"
-  local with_host_x="false"
-  local need_help="false"
-  local share_dir="false"
-  local -a shared_dir_pairs=()
-  local android_build_top="false"
-  local to_build_top_dir="${ANDROID_BUILD_TOP}"
+	local name=""
+	local singleshot="false"
+	local with_host_x="false"
+	local need_help="false"
+	local share_dir="false"
+	local -a shared_dir_pairs=()
+	local android_build_top="false"
+	local to_build_top_dir="${ANDROID_BUILD_TOP}"
 
   # n | --name=cuttlefish | --name jellyfish
   # s | --singleshot
@@ -129,51 +134,55 @@ function cvd_docker_create {
 
   local params
   if params=$(getopt -o 'n:a:m:sxh' -l 'name:,android_build_top:,share_dir:,singleshot,with_host_x,help' --name "$0" -- "$@"); then
-  eval set -- "${params}"
-  while true; do
-    case "$1" in
-    -n|--name)
-      name=$2
-      shift 2
-      ;;
-    -m|--share_dir)
-      share_dir="true"
-      shared_dir_pairs+=("$2")
-      shift 2
-      ;;
-    -s|--singleshot)
-      singleshot="true"
-      shift
-      ;;
-    -x|--with_host_x)
-      with_host_x="true"
-      singleshot="true"
-      shift
-      ;;
-    -a|--android_build_top)
-      if [[ -z ${ANDROID_BUILD_TOP} ]]; then
-        android_build_top="true"
-        to_build_top_dir="$2"
-      fi
-      shift 2
-      ;;
-   -h|--help)
-      need_help="true"
-      shift
-      ;;
-   --)
-      shift
-      break
-      ;;
-    esac
-  done
+	  eval set -- "${params}"
+	  while true; do
+		  case "$1" in
+			  -n|--name)
+				  name=$2
+				  shift 2
+				  ;;
+			  -m|--share_dir)
+				  share_dir="true"
+				  shared_dir_pairs+=("$2")
+				  shift 2
+				  ;;
+			  -s|--singleshot)
+				  singleshot="true"
+				  shift
+				  ;;
+			  -x|--with_host_x)
+				  if [[ -n "${DISPLAY}" ]]; then
+					  with_host_x="true"
+				  else
+					  echo "Can't use host's X: DISPLAY is not set." 1>&2
+					  return
+				  fi
+				  shift
+				  ;;
+			  -a|--android_build_top)
+				  if [[ -z ${ANDROID_BUILD_TOP} ]]; then
+					  android_build_top="true"
+                      to_build_top_dir="$2"
+				  fi
+				  shift 2
+				  ;;
+			  -h|--help)
+				  need_help="true"
+				  shift
+				  ;;
+			  --)
+				  shift
+				  break
+				  ;;
+		  esac
+	  done
   else
-    need_help="true"
+	  need_help="true"
   fi
 
   if [[ "${need_help}" == "true" ]]; then
-    help_on_container_create
-    return
+	  help_on_container_create
+	  return
   fi
 
   local android_build_top_dir="${ANDROID_BUILD_TOP}"
@@ -193,61 +202,79 @@ function cvd_docker_create {
   unset _rest
 
   local name="$(cvd_get_id $name)"
-  local container="$(cvd_exists $name)"
+  local container="$(cvd_container_exists $name)"
 
   local -a volumes=()
   if [[ -z "${container}" ]]; then
-    echo "Container ${name} does not exist.";
-    echo "Starting container ${name} from image cuttlefish.";
+	  echo "Container ${name} does not exist.";
+	  echo "Starting container ${name} from image cuttlefish.";
 
-    # if ANDROID_BUILD_TOP is given either via an env variable or commandline option,
-    # we will use the host package and images from there
-    #
-    if [[ -n "${android_build_top_dir}" ]]; then
-      local home="$(mktemp -d)"
-      echo "Detected Android build environment.  Setting up in ${home}."
-      tar xz -C "${home}" -f "${android_host_out_dir}"/cvd-host_package.tar.gz
-      for f in "${android_prod_out_dir}"/*.img; do
-        volumes+=("-v ${f}:/home/vsoc-01/$(basename ${f}):rw")
-      done
-      volumes+=("-v ${home}:/home/vsoc-01:rw")
-    fi
+	# if ANDROID_BUILD_TOP is given either via an env variable or commandline option,
+	# we will use the host package and images from there
+	#
+	if [[ -n "${android_build_top_dir}" ]]; then
+		local home="$(mktemp -d)"
+		echo "Detected Android build environment.  Setting up in ${home}."
+		tar xz -C "${home}" -f "${android_host_out_dir}"/cvd-host_package.tar.gz
+		for f in "${android_prod_out_dir}"/*.img; do
+			volumes+=("-v ${f}:/home/vsoc-01/$(basename ${f}):rw")
+		done
+		volumes+=("-v ${home}:/home/vsoc-01:rw")
+	fi
 
-    if [[ $share_dir == "true" ]]; then
-      local host_pwd="$(realpath "$PWD")"
-      local guest_home="/home/vsoc-01"
-      for sub in "${shared_dir_pairs[@]}"; do
-        if ! echo ${sub} | egrep ":" > /dev/null; then
-          echo "${sub} is ill-formated. should be host_dir:mnt_dir"
-          echo "try $0 --help"
-          exit 1
-        fi
-        local host_dir="$(echo $sub | cut -d ':' -f 1)"
-        local guest_dir="$(echo $sub | cut -d ':' -f 2)"
-        if ! is_absolute_path ${host_dir}; then
-          host_dir="${host_pwd}/${subdir}"
-        fi
-        if ! is_absolute_path ${guest_dir}; then
-          guest_dir="${guest_home}/${guest_dir}"
-        fi
-        volumes+=("-v ${host_dir}:${guest_dir}")
-      done
-    fi
+	if [[ $share_dir == "true" ]]; then
+		local host_pwd=$(realpath "$PWD")
+        local guest_home="/home/vsoc-01"
+		for sub in "${shared_dir_pairs[@]}"; do
+			if ! echo ${sub} | egrep ":" > /dev/null; then
+                echo "${sub} is ill-formated. should be host_dir:mnt_dir"
+                echo "try $0 --help"
+            fi
+            local host_dir="$(echo $sub | cut -d ':' -f 1)"
+            local guest_dir="$(echo $sub | cut -d ':' -f 2)"
+            if ! is_absolute_path ${host_dir}; then
+                host_dir="${host_pwd}/${subdir}"
+            fi
+            if ! is_absolute_path ${guest_dir}; then
+                guest_dir="${guest_home}/${guest_dir}"
+            fi
+            volumes+=("-v ${host_dir}:${guest_dir}")
+		done
+	fi
 
-    local -a as_host_x=()
-    if [[ "$with_host_x" == "true" ]]; then
-      as_host_x+=("-e DISPLAY=$DISPLAY")
-      as_host_x+=("-v /tmp/.X11-unix:/tmp/.X11-unix")
-    fi
+	local -a as_host_x=()
+	if [[ "$with_host_x" == "true" ]]; then
+		as_host_x+=("-e DISPLAY=$DISPLAY")
+		as_host_x+=("-v /tmp/.X11-unix:/tmp/.X11-unix")
+	fi
 
-    docker run -d ${as_host_x[@]} \
-           --name "${name}" -h "${name}" \
-           --privileged \
-           -v /sys/fs/cgroup:/sys/fs/cgroup:ro ${volumes[@]} \
-           cuttlefish
+	docker run -d ${as_host_x[@]} \
+		--name "${name}" -h "${name}" \
+		-p 8443:8443 \
+		-p 6250:6250 \
+		-p 15550:15550 \
+		-p 15551:15551 \
+		--privileged \
+		-v /sys/fs/cgroup:/sys/fs/cgroup:ro ${volumes[@]} \
+		cuttlefish
 
-    __gen_funcs ${name}
+	echo "Waiting for ${name} to boot."
+	while true; do
+		if [[ -z "$(cvd_container_exists ${name})" ]]; then
+			echo "Container ${name}  does not exist yet.  Sleep 1 second"
+			sleep 1
+			continue
+		fi
+		if [[ -z "$(cvd_container_running ${name})" ]]; then
+			echo "Container ${name} is not running yet.  Sleep 1 second"
+			sleep 1
+			continue
+		fi
+		break
+	done
+	echo "Done waiting for ${name} to boot."
 
+	__gen_funcs ${name}
     # define and export ip_${name} for the ip address
     local ip_addr_var_name="ip_${name}"
     declare ${ip_addr_var_name}="$(cvd_get_ip "${name}")"
@@ -258,103 +285,130 @@ function cvd_docker_create {
         return
     fi
 
-    help_on_container_start ${name}
-    echo
-    help_on_sourcing
+	if [[ "$singleshot" == "true" ]]; then
+		cvd_login_${name}
+		cvd_docker_rm ${name}
+		return
+	fi
 
-  else
-    echo "Container ${name} exists";
-    if [ $(docker inspect -f "{{.State.Running}}" ${name}) != 'true' ]; then
-      echo "Container ${name} is not running.";
-    else
-      echo "Container ${name} is already running.";
-      __gen_funcs ${name}
-      help_on_container_start ${name}
-      echo
-      help_on_sourcing
-    fi
+	help_on_container_start ${name}
+	echo
+	help_on_sourcing
+
+else
+	echo "Container ${name} exists";
+	if [[ -z "$(cvd_container_running ${name})" ]]; then
+		echo "Container ${name} is not running.";
+	else
+		echo "Container ${name} is already running.";
+		__gen_funcs ${name}
+		help_on_container_start ${name}
+		echo
+		help_on_sourcing
+	fi
   fi
 }
 
 function cvd_docker_rm {
-  local name=${1:-cuttlefish}
-  local ip_addr_var_name="ip_${name}"
-  unset ${ip_addr_var_name}
+	local name=${1:-cuttlefish}
+	local ip_addr_var_name="ip_${name}"
+    unset ${ip_addr_var_name}
 
-  if [ -n "$(docker ps -q -a -f name=${name})" ]; then
-    homedir=$(docker inspect -f '{{range $mount:=.Mounts}}{{if and (eq .Destination "/home/vsoc-01") (eq .Type "bind")}}{{- printf "%q" $mount.Source}}{{end}}{{end}}' "${name}" | sed 's/"//g')
-    echo "Deleting container ${name}."
-    docker rm -f ${name}
-    echo "Cleaning up homedir ${homedir}."
-    rm -rf ${homedir}
-    unset -f $(__gen_start_func_name ${name})
-    unset -f $(__gen_login_func_name ${name})
-    unset -f $(__gen_stop_func_name ${name})
-  else
-    echo "Nothing to stop: container ${name} does not exist."
-  fi
+	if [ -n "$(docker ps -q -a -f name=${name})" ]; then
+		homedir=$(docker inspect -f '{{range $mount:=.Mounts}}{{if and (eq .Destination "/home/vsoc-01") (eq .Type "bind")}}{{- printf "%q" $mount.Source}}{{end}}{{end}}' "${name}" | sed 's/"//g')
+		echo "Deleting container ${name}."
+		docker rm -f ${name}
+		echo "Cleaning up homedir ${homedir}."
+		rm -rf ${homedir}
+		unset -f $(__gen_start_func_name ${name})
+		unset -f $(__gen_login_func_name ${name})
+		unset -f $(__gen_stop_func_name ${name})
+	else
+		echo "Nothing to stop: container ${name} does not exist."
+	fi
 }
 
 function __gen_login_func_name {
-  local name=$1
-  echo -n "cvd_login_${name}"
+	local name=$1
+	echo -n "cvd_login_${name}"
 }
 
 function __gen_start_func_name {
-  local name=$1
-  echo -n "cvd_start_${name}"
+	local name=$1
+	echo -n "cvd_start_${name}"
 }
 
 function __gen_stop_func_name {
-  local name=$1
-  echo -n "cvd_stop_${name}"
+	local name=$1
+	echo -n "cvd_stop_${name}"
 }
 
 # $1 = container name; must not be empty
 function __gen_funcs {
-  local name=$1
+	local name=$1
 
-  local login_func
-  local start_func
-  local stop_func
+	local login_func
+	local start_func
+	local stop_func
 
-  read -r -d '' login_func <<EOF
+	read -r -d '' login_func <<EOF
 function $(__gen_login_func_name ${name}) {
-  ssh \
-    -L8443:localhost:8443 \
-    -L6520:localhost:6520 \
-    -L6444:localhost:6444 \
-    -L15550:localhost:15550 -L15551:localhost:15551 \
-    vsoc-01@$(cvd_get_ip ${name}) -- "\$@";
-#  docker exec -it --user vsoc-01 "${name}" ./bin/launch_cvd "$@";
+  local _cmd="/bin/bash"
+  if [[ -n "\$@" ]]; then
+	_cmd="\$@"
+  fi
+  docker exec -it --user vsoc-01 "${name}" \${_cmd}
+#  ssh \
+#    -L8443:localhost:8443 \
+#    -L6520:localhost:6520 \
+#    -L6444:localhost:6444 \
+#    -L15550:localhost:15550 -L15551:localhost:15551 \
+#	\${_x} \
+#    vsoc-01@$(cvd_get_ip ${name}) \
+#	"\$@";
 }
 EOF
 
-  read -r -d '' start_func <<EOF
+read -r -d '' start_func <<EOF
 function $(__gen_start_func_name ${name}) {
   $(__gen_login_func_name ${name}) ./bin/launch_cvd "\$@"
 }
 EOF
 
-  read -r -d '' stop_func <<EOF
+read -r -d '' stop_func <<EOF
 function $(__gen_stop_func_name ${name}) {
   docker exec -it --user vsoc-01 "${name}" ./bin/stop_cvd;
 }
 EOF
 
-  eval "${login_func}"
-  eval "${start_func}"
-  eval "${stop_func}"
-  eval "export ip_${name}=$(cvd_get_ip $(cvd_get_id ${name}))"
+eval "${login_func}"
+eval "${start_func}"
+eval "${stop_func}"
+eval "export ip_${name}=$(cvd_get_ip $(cvd_get_id ${name}))"
 
-  echo "To log into container ${name} without starting Android, call $(__gen_login_func_name ${name})"
-  echo "To start Android in container ${name}, call $(__gen_start_func_name ${name})"
-  echo "To stop Android in container ${name}, call $(__gen_stop_func_name ${name})"
+echo "To log into container ${name} without starting Android, call $(__gen_login_func_name ${name})"
+echo "To start Android in container ${name}, call $(__gen_start_func_name ${name})"
+echo "To stop Android in container ${name}, call $(__gen_stop_func_name ${name})"
 }
 
 help_on_sourcing
 
-for cf in $(docker ps -q -a --filter="ancestor=cuttlefish" --format "table {{.Names}}" | tail -n+2); do
-  __gen_funcs "${cf}"
-done
+function cvd_clean_autogens() {
+	for f in $(compgen -A function cvd_login_); do
+		unset -f ${f}
+	done
+	for f in $(compgen -A function cvd_start_); do
+		unset -f ${f}
+	done
+	for f in $(compgen -A function cvd_stop_); do
+		unset -f ${f}
+	done
+}
+# If containers were removed in different shell sessions, this session might
+# have stale instances of the auto-generated functions.
+cvd_clean_autogens
+unset -f cvd_clean_autogens
 
+for cf in $(docker ps -q -a --filter="ancestor=cuttlefish" --format "table {{.Names}}" | tail -n+2); do
+	__gen_funcs "${cf}"
+done
