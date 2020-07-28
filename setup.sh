536cfd152b8327e2aca97525c30d5b3410c75c94
Iliyan Malchev <malchev@google.com>
2020-07-27 22:20:47 -0700

    cleanup

--- setup.sh
+++ setup.sh
@@ -91,9 +91,8 @@ function cvd_docker_create {
   # h | --help
 
   local params
-  params=$(getopt -o 'n:m:sxh' -l 'name:,share_home:,singleshot,with_host_x,help' --name "$0" -- "$@") || return
+  if params=$(getopt -o 'n:m:sxh' -l 'name:,share_home:,singleshot,with_host_x,help' --name "$0" -- "$@"); then
   eval set -- "${params}"
-  unset params
   while true; do
     case "$1" in
     -n|--name)
@@ -123,13 +122,11 @@ function cvd_docker_create {
       shift
       break
       ;;
-    *)
-      echo "Not implemented: $1" >&2
-      need_help="true"
-      break
-      ;;
     esac
   done
+  else
+    need_help="true"
+  fi
 
   if [[ "${need_help}" == "true" ]]; then
     help_on_container_create
