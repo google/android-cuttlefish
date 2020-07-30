641fa8644a905b9e71002a88c964ba6e2fc1288a
Treehugger Robot <treehugger-gerrit@google.com>
2020-07-30 18:34:10 +0000

    Merge "[WebRTC] Replace webrtc::VideoSink<T> with a custom class" am: aafac5d2d8 am: 39f9b1e662 am: 745efd5d0e am: 6d147944f2 am: 86451176e2
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/1368858
    
    Change-Id: I6f067586476e0d024130d42c00f790d481e15fa8
    GitOrigin-RevId: f2e81135ef6b74a841cd4203217780b93c43d10d

--- staging/host/frontend/webrtc/cvd_video_frame_buffer.cpp
+++ staging/host/frontend/webrtc/cvd_video_frame_buffer.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#include "host/frontend/webrtc/video_frame_buffer.h"
+#include "host/frontend/webrtc/cvd_video_frame_buffer.h"
 
 #include "common/libs/utils/size_utils.h"
 
@@ -37,11 +37,9 @@ CvdVideoFrameBuffer::CvdVideoFrameBuffer(int width, int height)
       u_(AlignStride((width + 1) / 2) * ((height + 1) / 2) + kPlanePadding),
       v_(AlignStride((width + 1) / 2) * ((height + 1) / 2) + kPlanePadding) {}
 
-// From VideoFrameBuffer
 int CvdVideoFrameBuffer::width() const { return width_; }
 int CvdVideoFrameBuffer::height() const { return height_; }
 
-// From class PlanarYuvBuffer
 int CvdVideoFrameBuffer::StrideY() const { return AlignStride(width_); }
 int CvdVideoFrameBuffer::StrideU() const {
   return AlignStride((width_ + 1) / 2);
@@ -50,7 +48,6 @@ int CvdVideoFrameBuffer::StrideV() const {
   return AlignStride((width_ + 1) / 2);
 }
 
-// From class PlanarYuv8Buffer
 const uint8_t *CvdVideoFrameBuffer::DataY() const { return y_.data(); }
 const uint8_t *CvdVideoFrameBuffer::DataU() const { return u_.data(); }
 const uint8_t *CvdVideoFrameBuffer::DataV() const { return v_.data(); }
--- staging/host/frontend/webrtc/lib/video_track_source_impl.cpp
+++ staging/host/frontend/webrtc/lib/video_track_source_impl.cpp
@@ -16,26 +16,65 @@
 
 #include "host/frontend/webrtc/lib/video_track_source_impl.h"
 
+#include <api/video/video_frame_buffer.h>
 
 namespace cuttlefish {
 namespace webrtc_streaming {
 
+namespace {
+
+class VideoFrameWrapper : public webrtc::I420BufferInterface {
+ public:
+  VideoFrameWrapper(
+      std::shared_ptr<::cuttlefish::webrtc_streaming::VideoFrameBuffer>
+          frame_buffer)
+      : frame_buffer_(frame_buffer) {}
+  ~VideoFrameWrapper() override = default;
+  // From VideoFrameBuffer
+  int width() const override { return frame_buffer_->width(); }
+  int height() const override { return frame_buffer_->height(); }
+
+  // From class PlanarYuvBuffer
+  int StrideY() const override { return frame_buffer_->StrideY(); }
+  int StrideU() const override { return frame_buffer_->StrideU(); }
+  int StrideV() const override { return frame_buffer_->StrideV(); }
+
+  // From class PlanarYuv8Buffer
+  const uint8_t *DataY() const override { return frame_buffer_->DataY(); }
+  const uint8_t *DataU() const override { return frame_buffer_->DataU(); }
+  const uint8_t *DataV() const override { return frame_buffer_->DataV(); }
+
+ private:
+  std::shared_ptr<::cuttlefish::webrtc_streaming::VideoFrameBuffer>
+      frame_buffer_;
+};
+
+}  // namespace
+
 VideoTrackSourceImpl::VideoTrackSourceImpl(int width, int height)
     : webrtc::VideoTrackSource(false), width_(width), height_(height) {}
 
-void VideoTrackSourceImpl::OnFrame(const webrtc::VideoFrame& frame) {
-  broadcaster_.OnFrame(frame);
+void VideoTrackSourceImpl::OnFrame(std::shared_ptr<VideoFrameBuffer> frame,
+                                   int64_t timestamp_us) {
+  auto video_frame =
+      webrtc::VideoFrame::Builder()
+          .set_video_frame_buffer(
+              new rtc::RefCountedObject<VideoFrameWrapper>(frame))
+          .set_timestamp_us(timestamp_us)
+          .build();
+  broadcaster_.OnFrame(video_frame);
 }
 
-bool VideoTrackSourceImpl::GetStats(Stats* stats) {
+bool VideoTrackSourceImpl::GetStats(Stats *stats) {
   stats->input_height = height_;
   stats->input_width = width_;
   return true;
 }
 
 bool VideoTrackSourceImpl::SupportsEncodedOutput() const { return false; }
-rtc::VideoSourceInterface<webrtc::VideoFrame>* VideoTrackSourceImpl::source() {
+rtc::VideoSourceInterface<webrtc::VideoFrame> *VideoTrackSourceImpl::source() {
   return &broadcaster_;
 }
 
-}}
+}  // namespace webrtc_streaming
+}  // namespace cuttlefish
