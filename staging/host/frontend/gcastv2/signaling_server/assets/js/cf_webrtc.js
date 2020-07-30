50a4a52d5a8b93cde95e039e049cb081dac62abd
Treehugger Robot <treehugger-gerrit@google.com>
2020-07-30 18:34:56 +0000

    Merge "[WebRTC] Add missing copyright notices" am: d85be1e493 am: 22f2709899 am: ae444498f1 am: 8724a5d70a am: f765c8117a
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/1378900
    
    Change-Id: Ide7ef55e779c0bb75dee2a6a37f735b8567d0b9f
    GitOrigin-RevId: 0dd4c4c5f05acdc4c832bef45dae73cfc6ffbcf0

--- staging/host/frontend/gcastv2/signaling_server/assets/js/cf_webrtc.js
+++ staging/host/frontend/gcastv2/signaling_server/assets/js/cf_webrtc.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 function createDataChannel(pc, label, onMessage) {
   console.log('creating data channel: ' + label);
   let dataChannel = pc.createDataChannel(label);
@@ -92,6 +108,8 @@ class DeviceConnection {
     });
   }
 
+  // TODO (b/124121375): This should probably be an array of pointer events and
+  // have different properties.
   sendMultiTouch({id, x, y, initialDown, slot, display_label}) {
     this._sendJsonInput({
       type: 'multi-touch',
