249fc27fa5e67c78ebc0279b86305c75ca59617b
Jorge E. Moreira <jemoreira@google.com>
2020-07-30 03:27:11 +0000

    [skip ci] [WebRTC] Use chromium's libwebrtc to stream cuttlefish (1/2) am: e1dd8ea7c1 am: c7c9b20cc2 am: 1c7518f088 am: 979d260fa5 am: 60526691fb
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/1353821
    
    Change-Id: I5e712b9271cdea08d2037686525fdcedabaf4a8b
    GitOrigin-RevId: 3abf263382664f7fc8f92734cbcd92785c4ac1f3

---
+++ staging/host/frontend/webrtc/lib/client_handler.h
@@ -0,0 +1,135 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <functional>
+#include <memory>
+#include <optional>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include <json/json.h>
+
+#include <api/peer_connection_interface.h>
+#include <pc/video_track_source.h>
+
+#include "host/frontend/webrtc/lib/connection_observer.h"
+
+namespace cuttlefish {
+namespace webrtc_streaming {
+
+class ClientHandler : public webrtc::PeerConnectionObserver,
+                      public std::enable_shared_from_this<ClientHandler> {
+ public:
+  static std::shared_ptr<ClientHandler> Create(
+      int client_id, std::shared_ptr<ConnectionObserver> observer,
+      std::function<void(const Json::Value&)> send_client_cb,
+      std::function<void()> on_connection_closed_cb);
+  ~ClientHandler() override;
+
+  bool SetPeerConnection(
+      rtc::scoped_refptr<webrtc::PeerConnectionInterface> peer_connection);
+
+  bool AddDisplay(rtc::scoped_refptr<webrtc::VideoTrackInterface> track,
+                  const std::string& label);
+
+  void HandleMessage(const Json::Value& client_message);
+
+  // CreateSessionDescriptionObserver implementation
+  void OnCreateSDPSuccess(webrtc::SessionDescriptionInterface* desc);
+  void OnCreateSDPFailure(webrtc::RTCError error);
+
+  // SetSessionDescriptionObserver implementation
+  void OnSetSDPFailure(webrtc::RTCError error);
+
+  // PeerConnectionObserver implementation
+  void OnSignalingChange(
+      webrtc::PeerConnectionInterface::SignalingState new_state) override;
+  void OnDataChannel(
+      rtc::scoped_refptr<webrtc::DataChannelInterface> data_channel) override;
+  void OnRenegotiationNeeded() override;
+  void OnStandardizedIceConnectionChange(
+      webrtc::PeerConnectionInterface::IceConnectionState new_state) override;
+  void OnConnectionChange(
+      webrtc::PeerConnectionInterface::PeerConnectionState new_state) override;
+  void OnIceGatheringChange(
+      webrtc::PeerConnectionInterface::IceGatheringState new_state) override;
+  void OnIceCandidate(const webrtc::IceCandidateInterface* candidate) override;
+  // Gathering of an ICE candidate failed.
+  // See https://w3c.github.io/webrtc-pc/#event-icecandidateerror
+  // |host_candidate| is a stringified socket address.
+  void OnIceCandidateError(const std::string& host_candidate,
+                           const std::string& url, int error_code,
+                           const std::string& error_text) override;
+  // Gathering of an ICE candidate failed.
+  // See https://w3c.github.io/webrtc-pc/#event-icecandidateerror
+  void OnIceCandidateError(const std::string& address, int port,
+                           const std::string& url, int error_code,
+                           const std::string& error_text) override;
+  void OnIceCandidatesRemoved(
+      const std::vector<cricket::Candidate>& candidates) override;
+  void OnTrack(
+      rtc::scoped_refptr<webrtc::RtpTransceiverInterface> transceiver) override;
+  void OnRemoveTrack(
+      rtc::scoped_refptr<webrtc::RtpReceiverInterface> receiver) override;
+
+ private:
+  class InputHandler : public webrtc::DataChannelObserver {
+   public:
+    InputHandler(rtc::scoped_refptr<webrtc::DataChannelInterface> input_channel,
+                 std::shared_ptr<ConnectionObserver> observer);
+    ~InputHandler() override;
+
+    void OnStateChange() override;
+    void OnMessage(const webrtc::DataBuffer& msg) override;
+
+   private:
+    rtc::scoped_refptr<webrtc::DataChannelInterface> input_channel_;
+    std::shared_ptr<ConnectionObserver> observer_;
+  };
+  enum OptionBits : uint32_t {
+    disableAudio = 1,
+    bundleTracks = 2,
+    enableData = 4,
+    useSingleCertificateForAllTracks = 8,
+    useTCP = 16,
+  };
+
+  ClientHandler(int client_id, std::shared_ptr<ConnectionObserver> observer,
+                std::function<void(const Json::Value&)> send_client_cb,
+                std::function<void()> on_connection_closed_cb);
+
+  // Intentionally private, disconnect the client by destroying the object.
+  void Close();
+
+  void LogAndReplyError(const std::string& error_msg) const;
+
+  void ParseOptions(const Json::Value& options);
+
+  int client_id_;
+  uint32_t options_;
+  std::shared_ptr<ConnectionObserver> observer_;
+  std::function<void(const Json::Value&)> send_to_client_;
+  std::function<void()> on_connection_closed_cb_;
+  rtc::scoped_refptr<webrtc::PeerConnectionInterface> peer_connection_;
+  std::vector<rtc::scoped_refptr<webrtc::DataChannelInterface>> data_channels_;
+  std::shared_ptr<InputHandler> input_handler_;
+};
+
+}  // namespace webrtc_streaming
+}  // namespace cuttlefish
---
+++ staging/host/frontend/webrtc/lib/connection_observer.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+namespace cuttlefish {
+namespace webrtc_streaming {
+
+class ConnectionObserver {
+ public:
+  ConnectionObserver() = default;
+  virtual ~ConnectionObserver() = default;
+
+  virtual void OnConnected() = 0;
+  virtual void OnTouchEvent(const std::string& display_label, int x, int y,
+                            bool down) = 0;
+  virtual void OnMultiTouchEvent(const std::string& label, int id, int slot,
+                                 int x, int y, bool initialDown) = 0;
+  virtual void OnKeyboardEvent(uint16_t keycode, bool down) = 0;
+  virtual void OnAdbChannelOpen(
+      std::function<bool(const uint8_t*, size_t)> adb_message_sender) = 0;
+  virtual void OnAdbMessage(const uint8_t* msg, size_t size) = 0;
+};
+
+class ConnectionObserverFactory {
+ public:
+  virtual ~ConnectionObserverFactory() = default;
+  // Called when a new connection is requested
+  virtual std::shared_ptr<ConnectionObserver> CreateObserver() = 0;
+};
+
+}  // namespace webrtc_streaming
+}  // namespace cuttlefish
