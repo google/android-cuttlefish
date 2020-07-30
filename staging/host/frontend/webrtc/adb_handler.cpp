60562d6390eb2d0ac301ecc78d515faf77ea3571
Jorge E. Moreira <jemoreira@google.com>
2020-07-30 04:31:59 +0000

    [skip ci] [WebRTC] Use chromium's libwebrtc to stream cuttlefish (2/2) am: e7faaf9d4a am: a55f59878d am: 0bb58ab50f am: a45234c01f am: a356d0ad00
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/1354162
    
    Change-Id: Ia3a65188beb36ed9aa9195ae04d978dfff397f16
    GitOrigin-RevId: af85aca97c649d6c0f4209fe3009347d74454089

---
+++ staging/host/frontend/webrtc/adb_handler.cpp
@@ -0,0 +1,217 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "host/frontend/webrtc/adb_handler.h"
+
+#include <unistd.h>
+
+#include <https/BaseConnection.h>
+#include <https/Support.h>
+
+#include <android-base/logging.h>
+
+using namespace android;
+
+namespace cuttlefish {
+namespace webrtc_streaming {
+
+struct AdbHandler::AdbConnection : public BaseConnection {
+  explicit AdbConnection(AdbHandler *parent, std::shared_ptr<RunLoop> runLoop,
+                         int sock);
+
+  void send(const void *_data, size_t size);
+
+ protected:
+  long processClientRequest(const void *data, size_t size) override;
+  void onDisconnect(int err) override;
+
+ private:
+  AdbHandler *mParent;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+AdbHandler::AdbConnection::AdbConnection(AdbHandler *parent,
+                                         std::shared_ptr<RunLoop> runLoop,
+                                         int sock)
+    : BaseConnection(runLoop, sock), mParent(parent) {}
+
+// Thanks for calling it a crc32, adb documentation!
+static uint32_t computeNotACrc32(const void *_data, size_t size) {
+  auto data = static_cast<const uint8_t *>(_data);
+  uint32_t sum = 0;
+  for (size_t i = 0; i < size; ++i) {
+    sum += data[i];
+  }
+
+  return sum;
+}
+
+static int verifyAdbHeader(const void *_data, size_t size,
+                           size_t *_payloadLength) {
+  auto data = static_cast<const uint8_t *>(_data);
+
+  *_payloadLength = 0;
+
+  if (size < 24) {
+    return -EAGAIN;
+  }
+
+  uint32_t command = U32LE_AT(data);
+  uint32_t magic = U32LE_AT(data + 20);
+
+  if (command != (magic ^ 0xffffffff)) {
+    return -EINVAL;
+  }
+
+  uint32_t payloadLength = U32LE_AT(data + 12);
+
+  if (size < 24 + payloadLength) {
+    return -EAGAIN;
+  }
+
+  auto payloadCrc = U32LE_AT(data + 16);
+  auto crc32 = computeNotACrc32(data + 24, payloadLength);
+
+  if (payloadCrc != crc32) {
+    return -EINVAL;
+  }
+
+  *_payloadLength = payloadLength;
+
+  return 0;
+}
+
+long AdbHandler::AdbConnection::processClientRequest(const void *_data,
+                                                     size_t size) {
+  auto data = static_cast<const uint8_t *>(_data);
+
+  LOG(VERBOSE) << "AdbConnection::processClientRequest (size = " << size << ")";
+
+  LOG(VERBOSE) << hexdump(data, size);
+
+  size_t payloadLength;
+  int err = verifyAdbHeader(data, size, &payloadLength);
+
+  if (err) {
+    return err;
+  }
+
+  mParent->send_to_client_(data, payloadLength + 24);
+  return payloadLength + 24;
+}
+
+void AdbHandler::AdbConnection::onDisconnect(int err) {
+  LOG(VERBOSE) << "AdbConnection::onDisconnect(err=" << err << ")";
+
+  mParent->send_to_client_(nullptr /* data */, 0 /* size */);
+}
+
+void AdbHandler::AdbConnection::send(const void *_data, size_t size) {
+  BaseConnection::send(_data, size);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+AdbHandler::AdbHandler(
+    std::shared_ptr<RunLoop> runLoop, const std::string &adb_host_and_port,
+    std::function<void(const uint8_t *, size_t)> send_to_client)
+    : mRunLoop(runLoop), mSocket(-1), send_to_client_(send_to_client) {
+  LOG(VERBOSE) << "Connecting to " << adb_host_and_port;
+
+  auto err = setupSocket(adb_host_and_port);
+  CHECK(!err);
+
+  mAdbConnection = std::make_shared<AdbConnection>(this, mRunLoop, mSocket);
+}
+
+AdbHandler::~AdbHandler() {
+  if (mSocket >= 0) {
+    close(mSocket);
+    mSocket = -1;
+  }
+}
+
+void AdbHandler::run() { mAdbConnection->run(); }
+
+int AdbHandler::setupSocket(const std::string &adb_host_and_port) {
+  auto colonPos = adb_host_and_port.find(':');
+  if (colonPos == std::string::npos) {
+    return -EINVAL;
+  }
+
+  auto host = adb_host_and_port.substr(0, colonPos);
+
+  const char *portString = adb_host_and_port.c_str() + colonPos + 1;
+  char *end;
+  unsigned long port = strtoul(portString, &end, 10);
+
+  if (end == portString || *end != '\0' || port > 65535) {
+    return -EINVAL;
+  }
+
+  int err;
+
+  int sock = socket(PF_INET, SOCK_STREAM, 0);
+
+  if (sock < 0) {
+    err = -errno;
+    goto bail;
+  }
+
+  makeFdNonblocking(sock);
+
+  sockaddr_in addr;
+  memset(addr.sin_zero, 0, sizeof(addr.sin_zero));
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = inet_addr(host.c_str());
+  addr.sin_port = htons(port);
+
+  if (connect(sock, reinterpret_cast<const sockaddr *>(&addr), sizeof(addr)) <
+          0 &&
+      errno != EINPROGRESS) {
+    err = -errno;
+    goto bail2;
+  }
+
+  mSocket = sock;
+
+  return 0;
+
+bail2:
+  close(sock);
+  sock = -1;
+
+bail:
+  return err;
+}
+
+void AdbHandler::handleMessage(const uint8_t *msg, size_t len) {
+  LOG(VERBOSE) << hexdump(msg, len);
+
+  size_t payloadLength;
+  int err = verifyAdbHeader(msg, len, &payloadLength);
+
+  if (err || len != 24 + payloadLength) {
+    LOG(ERROR) << "Invalid adb message received.";
+    return;
+  }
+
+  mAdbConnection->send(msg, len);
+}
+
+}  // namespace webrtc_streaming
+}  // namespace cuttlefish
---
+++ staging/host/frontend/webrtc/connection_observer.cpp
@@ -0,0 +1,165 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "host/frontend/webrtc/connection_observer.h"
+
+#include <linux/input.h>
+
+#include <thread>
+#include <vector>
+
+#include <android-base/logging.h>
+#include <gflags/gflags.h>
+
+#include "common/libs/fs/shared_buf.h"
+#include "host/frontend/webrtc/adb_handler.h"
+#include "host/libs/config/cuttlefish_config.h"
+
+DECLARE_bool(write_virtio_input);
+
+namespace cuttlefish {
+
+// TODO (b/147511234): de-dup this from vnc server and here
+struct virtio_input_event {
+  uint16_t type;
+  uint16_t code;
+  int32_t value;
+};
+
+struct InputEventBuffer {
+  virtual ~InputEventBuffer() = default;
+  virtual void AddEvent(uint16_t type, uint16_t code, int32_t value) = 0;
+  virtual size_t size() const = 0;
+  virtual const void *data() const = 0;
+};
+
+template <typename T>
+struct InputEventBufferImpl : public InputEventBuffer {
+  InputEventBufferImpl() {
+    buffer_.reserve(6);  // 6 is usually enough
+  }
+  void AddEvent(uint16_t type, uint16_t code, int32_t value) override {
+    buffer_.push_back({.type = type, .code = code, .value = value});
+  }
+  T *data() { return buffer_.data(); }
+  const void *data() const override { return buffer_.data(); }
+  std::size_t size() const override { return buffer_.size() * sizeof(T); }
+
+ private:
+  std::vector<T> buffer_;
+};
+
+std::unique_ptr<InputEventBuffer> GetEventBuffer() {
+  if (FLAGS_write_virtio_input) {
+    return std::unique_ptr<InputEventBuffer>(
+        new InputEventBufferImpl<virtio_input_event>());
+  } else {
+    return std::unique_ptr<InputEventBuffer>(
+        new InputEventBufferImpl<input_event>());
+  }
+}
+
+class ConnectionObserverImpl
+    : public cuttlefish::webrtc_streaming::ConnectionObserver {
+ public:
+  ConnectionObserverImpl(cuttlefish::SharedFD touch_fd, cuttlefish::SharedFD keyboard_fd,
+                         std::weak_ptr<DisplayHandler> display_handler,
+                         std::shared_ptr<RunLoop> run_loop)
+      : touch_client_(touch_fd),
+        keyboard_client_(keyboard_fd),
+        weak_display_handler_(display_handler),
+        run_loop_(run_loop) {}
+  virtual ~ConnectionObserverImpl() = default;
+
+  void OnConnected() override {
+    auto display_handler = weak_display_handler_.lock();
+    if (display_handler) {
+      // A long time may pass before the next frame comes up from the guest.
+      // Send the last one to avoid showing a black screen to the user during
+      // that time.
+      display_handler->SendLastFrame();
+    }
+  }
+  void OnTouchEvent(const std::string &display_label, int x, int y,
+                    bool down) override {
+    auto buffer = GetEventBuffer();
+    if (!buffer) {
+      LOG(ERROR) << "Failed to allocate event buffer";
+      return;
+    }
+    buffer->AddEvent(EV_ABS, ABS_X, x);
+    buffer->AddEvent(EV_ABS, ABS_Y, y);
+    buffer->AddEvent(EV_KEY, BTN_TOUCH, down);
+    buffer->AddEvent(EV_SYN, 0, 0);
+    cuttlefish::WriteAll(touch_client_, reinterpret_cast<const char *>(buffer->data()),
+                  buffer->size());
+  }
+  void OnMultiTouchEvent(const std::string &display_label, int id, int slot,
+                         int x, int y, bool initialDown) override {
+    OnTouchEvent(display_label, x, y, initialDown);
+  }
+  void OnKeyboardEvent(uint16_t code, bool down) override {
+    auto buffer = GetEventBuffer();
+    if (!buffer) {
+      LOG(ERROR) << "Failed to allocate event buffer";
+      return;
+    }
+    buffer->AddEvent(EV_KEY, code, down);
+    buffer->AddEvent(EV_SYN, 0, 0);
+    cuttlefish::WriteAll(keyboard_client_,
+                  reinterpret_cast<const char *>(buffer->data()),
+                  buffer->size());
+  }
+  void OnAdbChannelOpen(std::function<bool(const uint8_t *, size_t)>
+                            adb_message_sender) override {
+    LOG(VERBOSE) << "Adb Channel open";
+    adb_handler_.reset(new cuttlefish::webrtc_streaming::AdbHandler(
+        run_loop_,
+        cuttlefish::CuttlefishConfig::Get()->ForDefaultInstance().adb_ip_and_port(),
+        adb_message_sender));
+    adb_handler_->run();
+  }
+  void OnAdbMessage(const uint8_t *msg, size_t size) override {
+    adb_handler_->handleMessage(msg, size);
+  }
+
+ private:
+  cuttlefish::SharedFD touch_client_;
+  cuttlefish::SharedFD keyboard_client_;
+  std::shared_ptr<cuttlefish::webrtc_streaming::AdbHandler> adb_handler_;
+  std::weak_ptr<DisplayHandler> weak_display_handler_;
+  std::shared_ptr<RunLoop> run_loop_;
+};
+
+CfConnectionObserverFactory::CfConnectionObserverFactory(
+    cuttlefish::SharedFD touch_fd, cuttlefish::SharedFD keyboard_fd)
+    : touch_fd_(touch_fd),
+      keyboard_fd_(keyboard_fd),
+      run_loop_(RunLoop::main()),
+      run_loop_thread_([this]() { run_loop_->run(); }) {}
+
+std::shared_ptr<cuttlefish::webrtc_streaming::ConnectionObserver>
+CfConnectionObserverFactory::CreateObserver() {
+  return std::shared_ptr<cuttlefish::webrtc_streaming::ConnectionObserver>(
+      new ConnectionObserverImpl(touch_fd_, keyboard_fd_, weak_display_handler_,
+                                 run_loop_));
+}
+
+void CfConnectionObserverFactory::SetDisplayHandler(
+    std::weak_ptr<DisplayHandler> display_handler) {
+  weak_display_handler_ = display_handler;
+}
+}  // namespace cuttlefish
