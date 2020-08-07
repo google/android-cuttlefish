bb99ab647632a79bc821f0456421a8cf3aefa415
Yipeng Cao <tccyp@google.com>
2020-08-07 21:51:49 +0000

    [skip ci] Merge "Added gnss_grpc_proxy_server for cuttlefish" am: b06718dcd9 am: cbc2c66d07 am: 10a9b81c92 am: 4b87b2f8f5 am: a11ec48b2d
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/1391936
    
    Change-Id: I4a34dd1432b55f5e67eb30b43471d0db7f66231e
    GitOrigin-RevId: 928b3fae534a9dbd90d5023f6f734292120bd522

--- staging/host/commands/run_cvd/main.cc
+++ staging/host/commands/run_cvd/main.cc
@@ -537,6 +537,7 @@ int main(int argc, char** argv) {
   LaunchConfigServer(*config, &process_monitor);
   LaunchTombstoneReceiverIfEnabled(*config, &process_monitor);
   LaunchTpm(&process_monitor, *config);
+  LaunchGnssGrpcProxyServerIfEnabled(*config, &process_monitor);
   LaunchSecureEnvironment(&process_monitor, *config);
   LaunchVerhicleHalServerIfEnabled(*config, &process_monitor);
 
