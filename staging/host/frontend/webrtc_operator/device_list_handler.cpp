b45fcd065274e2d8bc09eab5b92c7948cd7b2427
Treehugger Robot <treehugger-gerrit@google.com>
2020-08-08 04:26:39 +0000

    [skip ci] Merge changes I6c11c13c,If9e1d098,I59cb4c0a am: e80f4a545c am: a3cf654571 am: 9c069f1191 am: 7b97cd4f93 am: 2ddc2ce850
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/1392748
    
    Change-Id: Ib96b6d43ebabae9e479824f979f21c4f5b4684df
    GitOrigin-RevId: 2f217c516804b016c0ada05263325b1d6b988a59

---
+++ staging/host/frontend/webrtc_operator/device_list_handler.cpp
@@ -0,0 +1,42 @@
+//
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "host/frontend/webrtc_operator/device_list_handler.h"
+
+namespace cuttlefish {
+
+DeviceListHandler::DeviceListHandler(struct lws* wsi,
+                                     const DeviceRegistry& registry)
+    : WebSocketHandler(wsi), registry_(registry) {}
+
+void DeviceListHandler::OnReceive(const uint8_t* /*msg*/, size_t /*len*/,
+                                  bool /*binary*/) {
+  // Ignore the message, just send the reply
+  Json::Value reply(Json::ValueType::arrayValue);
+
+  for (const auto& id : registry_.ListDeviceIds()) {
+    reply.append(id);
+  }
+  Json::FastWriter json_writer;
+  auto replyAsString = json_writer.write(reply);
+  EnqueueMessage(replyAsString.c_str(), replyAsString.size());
+  Close();
+}
+
+void DeviceListHandler::OnConnected() {}
+
+void DeviceListHandler::OnClosed() {}
+
+}  // namespace cuttlefish
