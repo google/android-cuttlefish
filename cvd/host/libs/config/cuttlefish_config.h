9dbfe409a2fdf3849fbd6b7f8480681fa61efeee
Ram Muthiah <rammuthiah@google.com>
2020-08-12 18:54:48 -0700

    Removed the tombstone enable flag
    The tombstone collector daemon has been on without issue for over one
    android release cycle. It is stable and the kernel commandline is overly
    cluttered. Make the process default enabled and reclaim the bytes taken
    up by androidboot.tombstone_transmit.
    
    Bug: 163918544
    Test: Treehugger and local boot
    Change-Id: I46053525d850069b7e587f3706e90f45c116b493

--- cvd/host/libs/config/cuttlefish_config.h
+++ cvd/host/libs/config/cuttlefish_config.h
@@ -238,9 +238,6 @@ class CuttlefishConfig {
   void set_blank_data_image_fmt(const std::string& blank_data_image_fmt);
   std::string blank_data_image_fmt() const;
 
-  void set_enable_tombstone_receiver(bool enable_tombstone_receiver);
-  bool enable_tombstone_receiver() const;
-
   void set_tombstone_receiver_binary(const std::string& binary);
   std::string tombstone_receiver_binary() const;
 
