814270ee180e7692b3f8dabcbee11c742e6a74bf
Paul Kirth <paulkirth@google.com>
2020-09-03 20:04:17 +0000

    Add minimal mode flag to cuttlefish config
    Minimal mode disables as much normal functionality as possible, while
    still being able to boot and use a device. These flags will enable the
    various parts of the configuration to make changes to disable
    non-essential services and host-side components.
    
    Bug: 148293798
    Test: make -j
    Change-Id: Id83e246d2331fe7d056e4bc1236ae75d0f08de7d

--- cvd/host/libs/config/cuttlefish_config.cpp
+++ cvd/host/libs/config/cuttlefish_config.cpp
@@ -114,9 +114,6 @@ const char* kEnableWebRTC = "enable_webrtc";
 const char* kWebRTCAssetsDir = "webrtc_assets_dir";
 const char* kWebRTCEnableADBWebSocket = "webrtc_enable_adb_websocket";
 
-const char* kEnableVehicleHalServer = "enable_vehicle_hal_server";
-const char* kVehicleHalServerBinary = "vehicle_hal_server_binary";
-
 const char* kRestartSubprocesses = "restart_subprocesses";
 const char* kRunAdbConnector = "run_adb_connector";
 
@@ -157,6 +154,9 @@ const char* kModemSimulatorSimType = "modem_simulator_sim_type";
 const char* kRilDns = "ril_dns";
 
 const char* kKgdb = "kgdb";
+
+const char* kEnableMinimalMode = "enable_minimal_mode";
+
 }  // namespace
 
 namespace cuttlefish {
@@ -422,22 +422,6 @@ void CuttlefishConfig::set_enable_webrtc(bool enable_webrtc) {
 
 bool CuttlefishConfig::enable_webrtc() const {
   return (*dictionary_)[kEnableWebRTC].asBool();
-}
-
-void CuttlefishConfig::set_enable_vehicle_hal_grpc_server(bool enable_vehicle_hal_grpc_server) {
-  (*dictionary_)[kEnableVehicleHalServer] = enable_vehicle_hal_grpc_server;
-}
-
-bool CuttlefishConfig::enable_vehicle_hal_grpc_server() const {
-  return (*dictionary_)[kEnableVehicleHalServer].asBool();
-}
-
-void CuttlefishConfig::set_vehicle_hal_grpc_server_binary(const std::string& vehicle_hal_server_binary) {
-  (*dictionary_)[kVehicleHalServerBinary] = vehicle_hal_server_binary;
-}
-
-std::string CuttlefishConfig::vehicle_hal_grpc_server_binary() const {
-  return (*dictionary_)[kVehicleHalServerBinary].asString();
 }
 
 void CuttlefishConfig::set_webrtc_assets_dir(const std::string& webrtc_assets_dir) {
@@ -729,6 +713,14 @@ void CuttlefishConfig::set_kgdb(bool kgdb) {
 }
 bool CuttlefishConfig::kgdb() const {
   return (*dictionary_)[kKgdb].asBool();
+}
+
+bool CuttlefishConfig::enable_minimal_mode() const {
+  return (*dictionary_)[kEnableMinimalMode].asBool();
+}
+
+void CuttlefishConfig::set_enable_minimal_mode(bool enable_minimal_mode) {
+  (*dictionary_)[kEnableMinimalMode] = enable_minimal_mode;
 }
 
 // Creates the (initially empty) config object and populates it with values from
