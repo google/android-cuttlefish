d7b63b26998f7f5e230a5d6816bed001b029d6e4
Ram Muthiah <rammuthiah@google.com>
2020-09-09 16:14:33 -0700

    Relocate composite disk to the runtime directory
    Bug: 158712998
    Test: consecutive local boots on multiple instances
    Change-Id: Ica4dd808976f64e1f4a2ab04a5d35e17f31e9862

--- cvd/host/libs/config/cuttlefish_config_instance.cpp
+++ cvd/host/libs/config/cuttlefish_config_instance.cpp
@@ -48,8 +48,6 @@ const char* kAdbIPAndPort = "adb_ip_and_port";
 
 const char* kConfigServerPort = "config_server_port";
 const char* kVncServerPort = "vnc_server_port";
-const char* kVehicleHalServerPort = "vehicle_hal_server_port";
-const char* kAudioControlServerPort = "audiocontrol_server_port";
 const char* kTombstoneReceiverPort = "tombstone_receiver_port";
 
 const char* kWebrtcDeviceId = "webrtc_device_id";
@@ -181,6 +179,10 @@ std::string CuttlefishConfig::InstanceSpecific::launcher_log_path() const {
 
 std::string CuttlefishConfig::InstanceSpecific::sdcard_path() const {
   return cuttlefish::AbsolutePath(PerInstancePath("sdcard.img"));
+}
+
+std::string CuttlefishConfig::InstanceSpecific::composite_disk_path() const {
+  return cuttlefish::AbsolutePath(PerInstancePath("composite.img"));
 }
 
 std::string CuttlefishConfig::InstanceSpecific::mobile_bridge_name() const {
@@ -337,22 +339,6 @@ int CuttlefishConfig::InstanceSpecific::tombstone_receiver_port() const {
 
 void CuttlefishConfig::MutableInstanceSpecific::set_tombstone_receiver_port(int tombstone_receiver_port) {
   (*Dictionary())[kTombstoneReceiverPort] = tombstone_receiver_port;
-}
-
-int CuttlefishConfig::InstanceSpecific::vehicle_hal_server_port() const {
-  return (*Dictionary())[kVehicleHalServerPort].asInt();
-}
-
-void CuttlefishConfig::MutableInstanceSpecific::set_vehicle_hal_server_port(int vehicle_hal_server_port) {
-  (*Dictionary())[kVehicleHalServerPort] = vehicle_hal_server_port;
-}
-
-int CuttlefishConfig::InstanceSpecific::audiocontrol_server_port() const {
-  return (*Dictionary())[kAudioControlServerPort].asInt();
-}
-
-void CuttlefishConfig::MutableInstanceSpecific::set_audiocontrol_server_port(int audiocontrol_server_port) {
-  (*Dictionary())[kAudioControlServerPort] = audiocontrol_server_port;
 }
 
 int CuttlefishConfig::InstanceSpecific::config_server_port() const {
