5a0b2834a2f969fd95ef8408b0806431534695bd
A. Cody Schuffelen <schuffelen@google.com>
2020-09-09 19:01:59 -0700

    TPM-based KeymasterEnforcement implementation.
    Shares data with the existing TPM Gatekeeper implementation.
    
    Test: m -j
    Bug: 155694409
    Change-Id: I37017b957d38b3f9271743b78ad9c985a7e40c72

--- host/commands/secure_env/tpm_random_source.h
+++ host/commands/secure_env/tpm_random_source.h
@@ -13,6 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#pragma once
+
 #include <keymaster/random_source.h>
 
 struct ESYS_CONTEXT;
