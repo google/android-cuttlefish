49c854f13ba5ec45b1a1676e4a628578487f015c
Treehugger Robot <treehugger-gerrit@google.com>
2020-09-12 04:45:43 +0000

    Merge "Added instance specific env images"

--- cvd/host/libs/config/cuttlefish_config_instance.cpp
+++ cvd/host/libs/config/cuttlefish_config_instance.cpp
@@ -48,6 +48,8 @@ const char* kAdbIPAndPort = "adb_ip_and_port";
 
 const char* kConfigServerPort = "config_server_port";
 const char* kVncServerPort = "vnc_server_port";
+const char* kVehicleHalServerPort = "vehicle_hal_server_port";
+const char* kAudioControlServerPort = "audiocontrol_server_port";
 const char* kTombstoneReceiverPort = "tombstone_receiver_port";
 
 const char* kWebrtcDeviceId = "webrtc_device_id";
@@ -343,6 +345,22 @@ int CuttlefishConfig::InstanceSpecific::tombstone_receiver_port() const {
 
 void CuttlefishConfig::MutableInstanceSpecific::set_tombstone_receiver_port(int tombstone_receiver_port) {
   (*Dictionary())[kTombstoneReceiverPort] = tombstone_receiver_port;
+}
+
+int CuttlefishConfig::InstanceSpecific::vehicle_hal_server_port() const {
+  return (*Dictionary())[kVehicleHalServerPort].asInt();
+}
+
+void CuttlefishConfig::MutableInstanceSpecific::set_vehicle_hal_server_port(int vehicle_hal_server_port) {
+  (*Dictionary())[kVehicleHalServerPort] = vehicle_hal_server_port;
+}
+
+int CuttlefishConfig::InstanceSpecific::audiocontrol_server_port() const {
+  return (*Dictionary())[kAudioControlServerPort].asInt();
+}
+
+void CuttlefishConfig::MutableInstanceSpecific::set_audiocontrol_server_port(int audiocontrol_server_port) {
+  (*Dictionary())[kAudioControlServerPort] = audiocontrol_server_port;
 }
 
 int CuttlefishConfig::InstanceSpecific::config_server_port() const {
