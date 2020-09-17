34fd845bcec212b76a4f8ab9d864cdfb21bf805d
Alistair Delva <adelva@google.com>
2020-09-16 21:28:52 -0700

    Fix -console=true with QEMU
    The serial console code had bitrotted and it was still using a socket
    instead of a pty, which was very confusing. Update it to use a pipe
    and rely on the newly refactored ConsoleForwarder. QEMU does support
    'pty' natively, but it does not allow the pty path to be specified
    or retrieved, so it's not very usable.
    
    Change-Id: I92dbbe9b1ac77d38a42d7640d608094e4dfee959

--- cvd/host/libs/config/cuttlefish_config.h
+++ cvd/host/libs/config/cuttlefish_config.h
@@ -393,6 +393,7 @@ class CuttlefishConfig {
 
     std::string kernel_log_pipe_name() const;
 
+    std::string console_pipe_prefix() const;
     std::string console_in_pipe_name() const;
     std::string console_out_pipe_name() const;
 
