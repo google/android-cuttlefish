1225f12b7d7f681a1acd0d72aae5ab96e3ac27df
A. Cody Schuffelen <schuffelen@google.com>
2020-09-29 17:19:39 -0700

    Implement orderly crosvm guest shutdown for stop_cvd.
    The previous implementation used the crosvm control socket to stop the
    virtual machine, which does a disorderly shutdown. These disorderly
    shutdowns were causing data loss, where data written by the guest wasn't
    visible after a restart. The orderly shutdown process allows the guest
    to stop its own processes and unmount its disks, flushing outstanding
    writes back to the disk.
    
    This only fixes the problem for crosvm, by taking advantage of the
    crosvm process exiting when the guest reboots. QEMU doesn't exit when
    its guest reboots, so a QEMU solution needs an alternate way to track
    whether the shutdown command succeeded.
    
    A dedicated virtio-console device is used to singal shutdown.
    
    Bug: 169690680
    Test: launch_cvd , adb push , stop_cvd , launch_cvd , adb shell ls
    Change-Id: I44beca14e33e73e17d5ead6f871fdfd3d4cef2d7

--- shared/device.mk
+++ shared/device.mk
@@ -112,6 +112,7 @@ PRODUCT_PACKAGES += \
     cuttlefish_rotate \
     rename_netiface \
     setup_wifi \
+    shutdown_helper \
     socket_vsock_proxy \
     tombstone_transmit \
     tombstone_producer \
