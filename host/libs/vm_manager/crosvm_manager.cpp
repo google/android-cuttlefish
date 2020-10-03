9e6788e02cb9e90c05c0df11096b68f322ab9409
Alistair Delva <adelva@google.com>
2020-10-02 18:44:55 -0700

    Support enabling vhost-net acceleration
    This currently must be opted into, as it requires /dev/vhost-net to be
    writable by the user, and that requires an upgrade of cuttlefish-common.
    
    Bug: 162001748
    Change-Id: I6c94270e26ec3f0e688f7687c3d8149e7a73f188

--- host/libs/vm_manager/crosvm_manager.cpp
+++ host/libs/vm_manager/crosvm_manager.cpp
@@ -294,6 +294,10 @@ std::vector<cuttlefish::Command> CrosvmManager::StartCommands(
     crosvm_cmd.AddParameter(config.GetKernelImageToUse());
   }
 
+  if (config.vhost_net()) {
+    crosvm_cmd.AddParameter("--vhost-net");
+  }
+
   // Only run the leases workaround if we are not using the new network
   // bridge architecture - in that case, we have a wider DHCP address
   // space and stale leases should be much less of an issue
