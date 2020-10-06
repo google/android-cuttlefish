32170269be94056c267c08d4b511c9203b40eecb
Treehugger Robot <treehugger-gerrit@google.com>
2020-10-06 22:07:44 +0000

    [skip ci] Merge "Move concrete/static methods out of VmManager." am: 433a4a22a3 am: 19c062de7e am: 2256f17ce8 am: 0004178ad5
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/1450077
    
    Change-Id: I429cf926eb49061c7b607d96ae62cbe1ea167b2e
    GitOrigin-RevId: b9fc96e819f3ab27cc043e3b1707a81e2a2dd400

--- staging/host/libs/vm_manager/vm_manager.cpp
+++ staging/host/libs/vm_manager/vm_manager.cpp
@@ -19,9 +19,7 @@
 #include <memory>
 
 #include <android-base/logging.h>
-#include <sys/utsname.h>
 
-#include "common/libs/utils/users.h"
 #include "host/libs/config/cuttlefish_config.h"
 #include "host/libs/vm_manager/qemu_manager.h"
 #include "host/libs/vm_manager/crosvm_manager.h"
@@ -45,78 +43,6 @@ std::unique_ptr<VmManager> GetVmManager(const std::string& name) {
     return {};
   }
   return vmm;
-}
-
-bool VmManager::UserInGroup(const std::string& group,
-                            std::vector<std::string>* config_commands) {
-  if (!InGroup(group)) {
-    LOG(ERROR) << "User must be a member of " << group;
-    config_commands->push_back("# Add your user to the " + group + " group:");
-    config_commands->push_back("sudo usermod -aG " + group + " $USER");
-    return false;
-  }
-  return true;
-}
-
-std::pair<int,int> VmManager::GetLinuxVersion() {
-  struct utsname info;
-  if (!uname(&info)) {
-    char* digit = strtok(info.release, "+.-");
-    int major = atoi(digit);
-    if (digit) {
-      digit = strtok(NULL, "+.-");
-      int minor = atoi(digit);
-      return std::pair<int,int>{major, minor};
-    }
-  }
-  LOG(ERROR) << "Failed to detect Linux kernel version";
-  return invalid_linux_version;
-}
-
-bool VmManager::LinuxVersionAtLeast(std::vector<std::string>* config_commands,
-                                    const std::pair<int,int>& version,
-                                    int major, int minor) {
-  if (version.first > major ||
-      (version.first == major && version.second >= minor)) {
-    return true;
-  }
-
-  LOG(ERROR) << "Kernel version must be >=" << major << "." << minor
-             << ", have " << version.first << "." << version.second;
-  config_commands->push_back("# Please upgrade your kernel to >=" +
-                             std::to_string(major) + "." +
-                             std::to_string(minor));
-  return false;
-}
-
-bool VmManager::ValidateHostConfiguration(
-    std::vector<std::string>* config_commands) const {
-  // if we can't detect the kernel version, just fail
-  auto version = VmManager::GetLinuxVersion();
-  if (version == invalid_linux_version) {
-    return false;
-  }
-
-  // the check for cvdnetwork needs to happen even if the user is not in kvm, so
-  // we can't just say UserInGroup("kvm") && UserInGroup("cvdnetwork")
-  auto in_cvdnetwork = VmManager::UserInGroup("cvdnetwork", config_commands);
-
-  // if we're in the virtaccess group this is likely to be a CrOS environment.
-  auto is_cros = InGroup("virtaccess");
-  if (is_cros) {
-    // relax the minimum kernel requirement slightly, as chromeos-4.4 has the
-    // needed backports to enable vhost_vsock
-    auto linux_ver_4_4 =
-      VmManager::LinuxVersionAtLeast(config_commands, version, 4, 4);
-    return in_cvdnetwork && linux_ver_4_4;
-  } else {
-    // this is regular Linux, so use the Debian group name and be more
-    // conservative with the kernel version check.
-    auto in_kvm = VmManager::UserInGroup("kvm", config_commands);
-    auto linux_ver_4_8 =
-      VmManager::LinuxVersionAtLeast(config_commands, version, 4, 8);
-    return in_cvdnetwork && in_kvm && linux_ver_4_8;
-  }
 }
 
 } // namespace vm_manager
