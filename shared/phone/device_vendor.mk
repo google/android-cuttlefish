790d2979d03b5c9cfbf4a71e8ca09d709797b8d6
Kevin Chyn <kchyn@google.com>
2020-11-19 22:42:42 -0800

    Add FEATURE_FINGERPRINT to cf_x86_phone
    Bug: 172957689
    Test: adb shell dumpsys biometric
    Test: atest CtsBiometricsTestCases
    Change-Id: I4deb25b1364c490eb1b732190bc21dc0da2ffecc

--- shared/phone/device_vendor.mk
+++ shared/phone/device_vendor.mk
@@ -45,6 +45,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.biometrics.face.xml \
     frameworks/native/data/etc/android.hardware.faketouch.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.faketouch.xml \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml
 
