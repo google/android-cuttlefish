a5f8d76011c7af1290812626ebb8b8e20db55c38
Jorge E. Moreira <jemoreira@google.com>
2021-01-13 02:09:48 +0000

    [skip ci] Use an eventfd to stop the kernel_log_handler background thread am: f8248057d6 am: a52b64bbd6 am: bc7132021a
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/1548859
    
    MUST ONLY BE SUBMITTED BY AUTOMERGER
    
    Change-Id: I32b3d38cd8eb5fc2c7f011b45599085a52eab83c
    GitOrigin-RevId: b1a501a1ebb07923b67e244dace30e0d5bebc33f

--- staging/host/frontend/webrtc/kernel_log_events_handler.cpp
+++ staging/host/frontend/webrtc/kernel_log_events_handler.cpp
@@ -18,6 +18,7 @@
 
 #include <android-base/logging.h>
 
+#include <common/libs/fs/shared_select.h>
 #include <host/commands/kernel_log_monitor/kernel_log_server.h>
 #include <host/commands/kernel_log_monitor/utils.h>
 #include <host/libs/config/cuttlefish_config.h>
@@ -32,32 +33,57 @@ KernelLogEventsHandler::KernelLogEventsHandler(
     std::function<void(const Json::Value&)> send_to_client)
     : send_to_client_(send_to_client),
       kernel_log_fd_(kernel_log_fd),
+      eventfd_(SharedFD::Event()),
+      running_(true),
       read_thread_([this]() { ReadLoop(); }) {}
 
 KernelLogEventsHandler::~KernelLogEventsHandler() {
-        (*kernel_log_fd_).Close();
-        read_thread_.join();
+  running_ = false;
+  eventfd_->EventfdWrite(1);
+  read_thread_.join();
 }
 
 void KernelLogEventsHandler::ReadLoop() {
-  while (1) {
-    std::optional<monitor::ReadEventResult> read_result =
-        monitor::ReadEvent(kernel_log_fd_);
-    if (!read_result) {
-      LOG(ERROR) << "Failed to read kernel log event: " << kernel_log_fd_->StrError();
+  CHECK(eventfd_->IsOpen()) << "Failed to create event fd: "
+                           << eventfd_->StrError();
+  while (running_) {
+    SharedFDSet read_set;
+    read_set.Set(eventfd_);
+    read_set.Set(kernel_log_fd_);
+    auto select_ret = Select(&read_set, nullptr, nullptr, nullptr);
+    if (select_ret < 0) {
+      LOG(ERROR) << "Error on select call";
       break;
     }
-
-    if (read_result->event == monitor::Event::BootStarted) {
-      Json::Value message;
-      message["event"] = kBootStartedMessage;
-      send_to_client_(message);
+    if (read_set.IsSet(eventfd_)) {
+      eventfd_t evt;
+      (void)eventfd_->EventfdRead(&evt);
+      if (!running_) {
+        // There won't be anyone listening for kernel log events if the thread
+        // was asked to stop, so break out of the loop without reading.
+        break;
+      }
     }
-    if (read_result->event == monitor::Event::ScreenChanged) {
-      Json::Value message;
-      message["event"] = kScreenChangedMessage;
-      message["metadata"] = read_result->metadata;
-      send_to_client_(message);
+    if (read_set.IsSet(kernel_log_fd_)) {
+      std::optional<monitor::ReadEventResult> read_result =
+          monitor::ReadEvent(kernel_log_fd_);
+      if (!read_result) {
+        LOG(ERROR) << "Failed to read kernel log event: "
+                   << kernel_log_fd_->StrError();
+        break;
+      }
+
+      if (read_result->event == monitor::Event::BootStarted) {
+        Json::Value message;
+        message["event"] = kBootStartedMessage;
+        send_to_client_(message);
+      }
+      if (read_result->event == monitor::Event::ScreenChanged) {
+        Json::Value message;
+        message["event"] = kScreenChangedMessage;
+        message["metadata"] = read_result->metadata;
+        send_to_client_(message);
+      }
     }
   }
 }
