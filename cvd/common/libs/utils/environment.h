9a51f95198dbcc017a6247d069cd3a7cb65a8348
Peter Collingbourne <pcc@google.com>
2021-03-11 14:19:35 -0800

    Fix support for QEMU on non-native architectures.
    Add the target architecture to the cuttlefish config, inferred
    from the kernel config. Fix several assumptions about the host
    architecture being the same as the target. When running on a non-native
    architecture, default to QEMU, emulate all available CPU features
    and increase the timeout multiplier to compensate for the slower
    execution speed.
    
    Bug: 161538883
    Change-Id: I57ffb1f2af3bdf7ede4db96a27455fef93c14b99

--- cvd/common/libs/utils/environment.h
+++ cvd/common/libs/utils/environment.h
@@ -19,10 +19,19 @@
 
 namespace cuttlefish {
 
+enum class Arch {
+  Arm,
+  Arm64,
+  X86,
+  X86_64,
+};
+
 std::string StringFromEnv(const std::string& varname,
                           const std::string& defval);
 
-std::string HostArch();
+std::string HostArchStr();
+Arch HostArch();
+bool IsHostCompatible(Arch arch);
 
 bool IsRunningInContainer();
 
