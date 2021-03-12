d9d2afa2d4d7bfbf06d0d5ad81161cbb9b57a7f0
Jeongik Cha <jeongik@google.com>
2021-03-12 16:56:24 +0900

    Make controller prop and default commands configurable in rootcanal
    Test: set up default commands and then atest
      android.bluetooth.cts.BluetoothLeScanTest
    Bug: 182508776
    Bug: 182230648
    Change-Id: Ibfd1f7ee612d69b9c2bd1cd5a1d581b1969c3a9f

--- cvd/host/libs/config/cuttlefish_config.h
+++ cvd/host/libs/config/cuttlefish_config.h
@@ -25,7 +25,6 @@
 #include <set>
 #include <vector>
 
-#include "common/libs/utils/environment.h"
 #include "host/libs/config/custom_actions.h"
 
 namespace Json {
@@ -115,8 +114,14 @@ class CuttlefishConfig {
   std::vector<DisplayConfig> display_configs() const;
   void set_display_configs(const std::vector<DisplayConfig>& display_configs);
 
-  int gdb_port() const;
-  void set_gdb_port(int gdb_port);
+  std::string kernel_image_path() const;
+  void set_kernel_image_path(const std::string& kernel_image_path);
+
+  std::string gdb_flag() const;
+  void set_gdb_flag(const std::string& gdb);
+
+  std::string initramfs_path() const;
+  void set_initramfs_path(const std::string& initramfs_path);
 
   bool deprecated_boot_completed() const;
   void set_deprecated_boot_completed(bool deprecated_boot_completed);
@@ -133,8 +138,8 @@ class CuttlefishConfig {
   void set_setupwizard_mode(const std::string& title);
   std::string setupwizard_mode() const;
 
-  void set_qemu_binary_dir(const std::string& qemu_binary_dir);
-  std::string qemu_binary_dir() const;
+  void set_qemu_binary(const std::string& qemu_binary);
+  std::string qemu_binary() const;
 
   void set_crosvm_binary(const std::string& crosvm_binary);
   std::string crosvm_binary() const;
@@ -193,11 +198,10 @@ class CuttlefishConfig {
   void set_bootloader(const std::string& bootloader_path);
   std::string bootloader() const;
 
-  // TODO (b/163575714) add virtio console support to the bootloader so the
-  // virtio console path for the console device can be taken again. When that
-  // happens, this function can be deleted along with all the code paths it
-  // forces.
-  bool use_bootloader() const { return true; };
+  // TODO (b/163575714) add virtio console support to the bootloader so the virtio
+  // console path for the console device can be taken again. When that happens, this
+  // function can be deleted along with all the code paths it forces.
+  bool use_bootloader() const {return true;};
 
   void set_boot_slot(const std::string& boot_slot);
   std::string boot_slot() const;
@@ -303,17 +307,10 @@ class CuttlefishConfig {
   void set_smt(bool smt);
   bool smt() const;
 
-  void set_enable_audio(bool enable);
   bool enable_audio() const;
 
   void set_protected_vm(bool protected_vm);
   bool protected_vm() const;
-
-  void set_target_arch(Arch target_arch);
-  Arch target_arch() const;
-
-  void set_bootconfig_supported(bool bootconfig_supported);
-  bool bootconfig_supported() const;
 
   class InstanceSpecific;
   class MutableInstanceSpecific;
@@ -370,6 +367,9 @@ class CuttlefishConfig {
     int rootcanal_hci_port() const;
     int rootcanal_link_port() const;
     int rootcanal_test_port() const;
+    std::string rootcanal_config_file() const;
+    std::string rootcanal_default_commands_file() const;
+
     std::string adb_device_name() const;
     std::string device_title() const;
     std::string gnss_file_path() const;
@@ -395,7 +395,6 @@ class CuttlefishConfig {
 
     std::string touch_socket_path() const;
     std::string keyboard_socket_path() const;
-    std::string switches_socket_path() const;
     std::string frames_socket_path() const;
 
     std::string access_kregistry_path() const;
@@ -424,7 +423,7 @@ class CuttlefishConfig {
 
     std::string sdcard_path() const;
 
-    std::string os_composite_disk_path() const;
+    std::string composite_disk_path() const;
 
     std::string uboot_env_image_path() const;
 
@@ -475,6 +474,9 @@ class CuttlefishConfig {
     void set_rootcanal_hci_port(int rootcanal_hci_port);
     void set_rootcanal_link_port(int rootcanal_link_port);
     void set_rootcanal_test_port(int rootcanal_test_port);
+    void set_rootcanal_config_file(const std::string& rootcanal_config_file);
+    void set_rootcanal_default_commands_file(
+        const std::string& rootcanal_default_commands_file);
     void set_device_title(const std::string& title);
     void set_mobile_bridge_name(const std::string& mobile_bridge_name);
     void set_mobile_tap_name(const std::string& mobile_tap_name);
