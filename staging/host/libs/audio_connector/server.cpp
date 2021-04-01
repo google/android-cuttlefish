5243ca5d47cac03a9fb06b9b838b4fd7396f72fe
Treehugger Robot <treehugger-gerrit@google.com>
2021-04-01 20:35:27 +0000

    [skip ci] Merge changes I49f17c5e,Icae6aec9 am: 5a645e7687 am: cba43af0aa am: 557b0b9ece am: 8d75b5c90c
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/1653718
    
    Change-Id: I16640b6afba7908294d2dd4ca17366303a892ace
    GitOrigin-RevId: 3bf87fe61fbfa31b91eb2d76d35d259be248f2ae

--- staging/host/libs/audio_connector/server.cpp
+++ staging/host/libs/audio_connector/server.cpp
@@ -63,6 +63,33 @@ bool CreateSocketPair(SharedFD* local, SharedFD* remote) {
   return ret;
 }
 
+std::function<void(AudioStatus, uint32_t, uint32_t)> SendStatusCallback(
+    uint32_t buffer_offset, SharedFD socket) {
+  // Consumption of an audio buffer is an asynchronous event, which could
+  // trigger after the client disconnected. A WeakFD ensures that the response
+  // will only be sent if there is still a client available.
+  auto weak_socket = WeakFD(socket);
+  return
+      [buffer_offset, weak_socket](AudioStatus status, uint32_t latency_bytes,
+                                   uint32_t consumed_length) {
+        auto socket = weak_socket.lock();
+        if (!socket->IsOpen()) {
+          return;
+        }
+        IoStatusMsg reply;
+        reply.status.status = Le32(static_cast<uint32_t>(status));
+        reply.status.latency_bytes = Le32(latency_bytes);
+        reply.buffer_offset = buffer_offset;
+        reply.consumed_length = consumed_length;
+        // Send the acknowledgment non-blockingly to avoid a slow client from
+        // blocking the server.
+        auto sent = socket->Send(&reply, sizeof(reply), MSG_DONTWAIT);
+        if (sent < sizeof(reply)) {
+          LOG(ERROR) << "Failed to send entire reply: " << socket->StrError();
+        }
+      };
+}
+
 }  // namespace
 
 std::unique_ptr<AudioClientConnection> AudioServer::AcceptClient(
@@ -126,52 +153,148 @@ std::unique_ptr<AudioClientConnection> AudioClientConnection::Create(
       event_socket, tx_socket, rx_socket));
 }
 
-bool AudioClientConnection::ReceivePending(AudioServerExecutor& executor) {
-  SharedFDSet read_set;
-  read_set.Set(control_socket_);
-  read_set.Set(tx_socket_);
-  read_set.Set(rx_socket_);
-  // Don't add event_socket_ since the client doesn't write to it
-  errno = 0;
-  auto select_ret = Select(&read_set, nullptr, nullptr, nullptr);
-  if (select_ret < 0) {
-    auto error = errno;
-    LOG(ERROR) << "Error while waiting for new messages from client: "
-               << strerror(error);
+bool AudioClientConnection::ReceiveCommands(AudioServerExecutor& executor) {
+  // The largest msg the client will send is 24 bytes long, using uint64_t
+  // guarantees it's aligned to 64 bits.
+  uint64_t recv_buffer[3];
+  auto recv_size =
+      ReceiveMsg(control_socket_, &recv_buffer, sizeof(recv_buffer));
+  if (recv_size <= 0) {
     return false;
   }
-  bool return_value = true;
-  if (read_set.IsSet(control_socket_)) {
-    // The largest msg the client will send is 24 bytes long, using uint64_t
-    // guarantees it's aligned to 64 bits.
-    uint64_t recv_buffer[3];
-    auto recv_size =
-        ReceiveMsg(control_socket_, &recv_buffer, sizeof(recv_buffer));
-    if (recv_size <= 0) {
-      return false;
-    }
-    const auto msg_hdr =
-        reinterpret_cast<const virtio_snd_hdr*>(&recv_buffer[0]);
-    return_value &= WithCommand(msg_hdr, recv_size, executor);
-  }
-  if (read_set.IsSet(tx_socket_)) {
-    // The largest msg the client will send is 12 bytes long, using uint32_t
-    // guarantees it's aligned to 32 bits.
-    uint32_t recv_buffer[3];
-    auto recv_size = ReceiveMsg(tx_socket_, &recv_buffer, sizeof(recv_buffer));
-    if (recv_size <= 0) {
-      return false;
-    }
-    const auto msg_hdr =
-        reinterpret_cast<const IoTransferMsg*>(&recv_buffer[0]);
-    return_value &= WithIOBuffer(*msg_hdr, recv_size, executor);
-  }
-  if (read_set.IsSet(rx_socket_)) {
-    // TODO (b/163867676): remove this when capture streams are implemented
-    LOG(FATAL) << "The client send data over the rx channel!";
+  const auto cmd_hdr = reinterpret_cast<const virtio_snd_hdr*>(&recv_buffer[0]);
+  if (recv_size < sizeof(virtio_snd_hdr)) {
+    LOG(ERROR) << "Received control message is too small: " << recv_size;
     return false;
   }
-  return return_value;
+  switch (static_cast<AudioCommandType>(cmd_hdr->code.as_uint32_t())) {
+    case AudioCommandType::VIRTIO_SND_R_PCM_INFO: {
+      if (recv_size < sizeof(virtio_snd_query_info)) {
+        LOG(ERROR) << "Received QUERY_INFO message is too small: " << recv_size;
+        return false;
+      }
+      auto query_info = reinterpret_cast<const virtio_snd_query_info*>(cmd_hdr);
+      auto info_count = query_info->count.as_uint32_t();
+      auto start_id = query_info->start_id.as_uint32_t();
+      std::unique_ptr<virtio_snd_pcm_info[]> reply(
+          new virtio_snd_pcm_info[info_count]);
+      StreamInfoCommand cmd(start_id, info_count, reply.get());
+
+      executor.StreamsInfo(cmd);
+      return CmdReply(cmd.status(), reply.get(),
+                      info_count * sizeof(reply[0]));
+    }
+    case AudioCommandType::VIRTIO_SND_R_PCM_SET_PARAMS: {
+      if (recv_size < sizeof(virtio_snd_pcm_set_params)) {
+        LOG(ERROR) << "Received SET_PARAMS message is too small: " << recv_size;
+        return false;
+      }
+      auto set_param_msg =
+          reinterpret_cast<const virtio_snd_pcm_set_params*>(cmd_hdr);
+      StreamSetParamsCommand cmd(set_param_msg->hdr.stream_id.as_uint32_t(),
+                                 set_param_msg->buffer_bytes.as_uint32_t(),
+                                 set_param_msg->period_bytes.as_uint32_t(),
+                                 set_param_msg->features.as_uint32_t(),
+                                 set_param_msg->channels, set_param_msg->format,
+                                 set_param_msg->rate);
+      executor.SetStreamParameters(cmd);
+      return CmdReply(cmd.status());
+    }
+    case AudioCommandType::VIRTIO_SND_R_PCM_PREPARE: {
+      if (recv_size < sizeof(virtio_snd_pcm_hdr)) {
+        LOG(ERROR) << "Received PREPARE message is too small: " << recv_size;
+        return false;
+      }
+      auto pcm_op_msg = reinterpret_cast<const virtio_snd_pcm_hdr*>(cmd_hdr);
+      StreamControlCommand cmd(AudioCommandType::VIRTIO_SND_R_PCM_PREPARE,
+                               pcm_op_msg->stream_id.as_uint32_t());
+      executor.PrepareStream(cmd);
+      return CmdReply(cmd.status());
+    }
+    case AudioCommandType::VIRTIO_SND_R_PCM_RELEASE: {
+      if (recv_size < sizeof(virtio_snd_pcm_hdr)) {
+        LOG(ERROR) << "Received RELEASE message is too small: " << recv_size;
+        return false;
+      }
+      auto pcm_op_msg = reinterpret_cast<const virtio_snd_pcm_hdr*>(cmd_hdr);
+      StreamControlCommand cmd(AudioCommandType::VIRTIO_SND_R_PCM_RELEASE,
+                               pcm_op_msg->stream_id.as_uint32_t());
+      executor.ReleaseStream(cmd);
+      return CmdReply(cmd.status());
+    }
+    case AudioCommandType::VIRTIO_SND_R_PCM_START: {
+      if (recv_size < sizeof(virtio_snd_pcm_hdr)) {
+        LOG(ERROR) << "Received START message is too small: " << recv_size;
+        return false;
+      }
+      auto pcm_op_msg = reinterpret_cast<const virtio_snd_pcm_hdr*>(cmd_hdr);
+      StreamControlCommand cmd(AudioCommandType::VIRTIO_SND_R_PCM_START,
+                               pcm_op_msg->stream_id.as_uint32_t());
+      executor.StartStream(cmd);
+      return CmdReply(cmd.status());
+    }
+    case AudioCommandType::VIRTIO_SND_R_PCM_STOP: {
+      if (recv_size < sizeof(virtio_snd_pcm_hdr)) {
+        LOG(ERROR) << "Received STOP message is too small: " << recv_size;
+        return false;
+      }
+      auto pcm_op_msg = reinterpret_cast<const virtio_snd_pcm_hdr*>(cmd_hdr);
+      StreamControlCommand cmd(AudioCommandType::VIRTIO_SND_R_PCM_STOP,
+                               pcm_op_msg->stream_id.as_uint32_t());
+      executor.StopStream(cmd);
+      return CmdReply(cmd.status());
+    }
+    case AudioCommandType::VIRTIO_SND_R_CHMAP_INFO:
+    case AudioCommandType::VIRTIO_SND_R_JACK_INFO:
+    case AudioCommandType::VIRTIO_SND_R_JACK_REMAP:
+      LOG(ERROR) << "Unsupported command type: " << cmd_hdr->code.as_uint32_t();
+      return CmdReply(AudioStatus::VIRTIO_SND_S_NOT_SUPP);
+    default:
+      LOG(ERROR) << "Unknown command type: " << cmd_hdr->code.as_uint32_t();
+      return CmdReply(AudioStatus::VIRTIO_SND_S_BAD_MSG);
+  }
+  return true;
+}
+
+bool AudioClientConnection::ReceivePlayback(AudioServerExecutor& executor) {
+  // The largest msg the client will send is 12 bytes long, using uint32_t
+  // guarantees it's aligned to 32 bits.
+  uint32_t recv_buffer[3];
+  auto recv_size = ReceiveMsg(tx_socket_, &recv_buffer, sizeof(recv_buffer));
+  if (recv_size <= 0) {
+    return false;
+  }
+  const auto msg_hdr = reinterpret_cast<const IoTransferMsg*>(&recv_buffer[0]);
+
+  if (recv_size < sizeof(IoTransferMsg)) {
+    LOG(ERROR) << "Received PCM_XFER message is too small: " << recv_size;
+    return false;
+  }
+  TxBuffer buffer(msg_hdr->io_xfer,
+                  TxBufferAt(msg_hdr->buffer_offset, msg_hdr->buffer_len),
+                  msg_hdr->buffer_len,
+                  SendStatusCallback(msg_hdr->buffer_offset, tx_socket_));
+  executor.OnPlaybackBuffer(std::move(buffer));
+  return true;
+}
+
+bool AudioClientConnection::ReceiveCapture(AudioServerExecutor& executor) {
+  uint32_t recv_buffer[3];
+  auto recv_size = ReceiveMsg(rx_socket_, &recv_buffer, sizeof(recv_buffer));
+  if (recv_size <= 0) {
+    return false;
+  }
+  const auto msg_hdr = reinterpret_cast<const IoTransferMsg*>(&recv_buffer[0]);
+  if (recv_size < sizeof(IoTransferMsg)) {
+    LOG(ERROR) << "Received PCM_XFER message is too small: " << recv_size;
+    return false;
+  }
+  RxBuffer buffer(msg_hdr->io_xfer,
+                  RxBufferAt(msg_hdr->buffer_offset, msg_hdr->buffer_len),
+                  msg_hdr->buffer_len,
+                  SendStatusCallback(msg_hdr->buffer_offset, rx_socket_));
+  executor.OnCaptureBuffer(std::move(buffer));
+  return true;
 }
 
 bool AudioClientConnection::CmdReply(AudioStatus status, const void* data,
@@ -197,146 +320,22 @@ bool AudioClientConnection::CmdReply(AudioStatus status, const void* data,
   return true;
 }
 
-bool AudioClientConnection::WithCommand(const virtio_snd_hdr* cmd_hdr,
-                                        size_t msg_len,
-                                        AudioServerExecutor& executor) {
-  if (msg_len < sizeof(virtio_snd_hdr)) {
-    LOG(ERROR) << "Received control message is too small: " << msg_len;
-    return false;
-  }
-  switch (static_cast<AudioCommandType>(cmd_hdr->code.as_uint32_t())) {
-    case AudioCommandType::VIRTIO_SND_R_PCM_INFO: {
-      if (msg_len < sizeof(virtio_snd_query_info)) {
-        LOG(ERROR) << "Received QUERY_INFO message is too small: " << msg_len;
-        return false;
-      }
-      auto query_info = reinterpret_cast<const virtio_snd_query_info*>(cmd_hdr);
-      auto info_count = query_info->count.as_uint32_t();
-      auto start_id = query_info->start_id.as_uint32_t();
-      std::unique_ptr<virtio_snd_pcm_info[]> reply(
-          new virtio_snd_pcm_info[info_count]);
-      StreamInfoCommand cmd(start_id, info_count, reply.get());
-
-      executor.StreamsInfo(cmd);
-      return CmdReply(cmd.status(), reply.get(),
-                      info_count * sizeof(virtio_snd_pcm_info));
-    }
-    case AudioCommandType::VIRTIO_SND_R_PCM_SET_PARAMS: {
-      if (msg_len < sizeof(virtio_snd_pcm_set_params)) {
-        LOG(ERROR) << "Received SET_PARAMS message is too small: " << msg_len;
-        return false;
-      }
-      auto set_param_msg =
-          reinterpret_cast<const virtio_snd_pcm_set_params*>(cmd_hdr);
-      StreamSetParamsCommand cmd(set_param_msg->hdr.stream_id.as_uint32_t(),
-                                 set_param_msg->buffer_bytes.as_uint32_t(),
-                                 set_param_msg->period_bytes.as_uint32_t(),
-                                 set_param_msg->features.as_uint32_t(),
-                                 set_param_msg->channels, set_param_msg->format,
-                                 set_param_msg->rate);
-      executor.SetStreamParameters(cmd);
-      return CmdReply(cmd.status());
-    }
-    case AudioCommandType::VIRTIO_SND_R_PCM_PREPARE: {
-      if (msg_len < sizeof(virtio_snd_pcm_hdr)) {
-        LOG(ERROR) << "Received PREPARE message is too small: " << msg_len;
-        return false;
-      }
-      auto pcm_op_msg = reinterpret_cast<const virtio_snd_pcm_hdr*>(cmd_hdr);
-      StreamControlCommand cmd(AudioCommandType::VIRTIO_SND_R_PCM_PREPARE,
-                               pcm_op_msg->stream_id.as_uint32_t());
-      executor.PrepareStream(cmd);
-      return CmdReply(cmd.status());
-    }
-    case AudioCommandType::VIRTIO_SND_R_PCM_RELEASE: {
-      if (msg_len < sizeof(virtio_snd_pcm_hdr)) {
-        LOG(ERROR) << "Received RELEASE message is too small: " << msg_len;
-        return false;
-      }
-      auto pcm_op_msg = reinterpret_cast<const virtio_snd_pcm_hdr*>(cmd_hdr);
-      StreamControlCommand cmd(AudioCommandType::VIRTIO_SND_R_PCM_RELEASE,
-                               pcm_op_msg->stream_id.as_uint32_t());
-      executor.ReleaseStream(cmd);
-      return CmdReply(cmd.status());
-    }
-    case AudioCommandType::VIRTIO_SND_R_PCM_START: {
-      if (msg_len < sizeof(virtio_snd_pcm_hdr)) {
-        LOG(ERROR) << "Received START message is too small: " << msg_len;
-        return false;
-      }
-      auto pcm_op_msg = reinterpret_cast<const virtio_snd_pcm_hdr*>(cmd_hdr);
-      StreamControlCommand cmd(AudioCommandType::VIRTIO_SND_R_PCM_START,
-                               pcm_op_msg->stream_id.as_uint32_t());
-      executor.StartStream(cmd);
-      return CmdReply(cmd.status());
-    }
-    case AudioCommandType::VIRTIO_SND_R_PCM_STOP: {
-      if (msg_len < sizeof(virtio_snd_pcm_hdr)) {
-        LOG(ERROR) << "Received STOP message is too small: " << msg_len;
-        return false;
-      }
-      auto pcm_op_msg = reinterpret_cast<const virtio_snd_pcm_hdr*>(cmd_hdr);
-      StreamControlCommand cmd(AudioCommandType::VIRTIO_SND_R_PCM_STOP,
-                               pcm_op_msg->stream_id.as_uint32_t());
-      executor.StopStream(cmd);
-      return CmdReply(cmd.status());
-    }
-    case AudioCommandType::VIRTIO_SND_R_CHMAP_INFO:
-    case AudioCommandType::VIRTIO_SND_R_JACK_INFO:
-    case AudioCommandType::VIRTIO_SND_R_JACK_REMAP:
-      LOG(ERROR) << "Unsupported command type: " << cmd_hdr->code.as_uint32_t();
-      return CmdReply(AudioStatus::VIRTIO_SND_S_NOT_SUPP);
-    default:
-      LOG(ERROR) << "Unknown command type: " << cmd_hdr->code.as_uint32_t();
-      return CmdReply(AudioStatus::VIRTIO_SND_S_BAD_MSG);
-  }
-  return true;
-}
-
-bool AudioClientConnection::WithIOBuffer(const IoTransferMsg& msg,
-                                         size_t msg_len,
-                                         AudioServerExecutor& executor) {
-  if (msg_len < sizeof(IoTransferMsg)) {
-    LOG(ERROR) << "Received PCM_XFER message is too small: " << msg_len;
-    return false;
-  }
-  // Consumption of an audio buffer is an asynchronous event, which could
-  // trigger after the client disconnected. A WeakFD ensures that the response
-  // will only be sent if there is still a client available.
-  auto weak_tx_socket = WeakFD(tx_socket_);
-  TxBuffer tx_buffer(
-      msg.io_xfer, TxBufferAt(msg.buffer_offset, msg.buffer_len),
-      msg.buffer_len,
-      [weak_tx_socket](AudioStatus status, uint32_t latency_bytes,
-                       uint32_t consumed_length) {
-        auto tx_socket = weak_tx_socket.lock();
-        if (!tx_socket->IsOpen()) {
-          return;
-        }
-        IoStatusMsg reply;
-        reply.status.status = Le32(static_cast<uint32_t>(status));
-        reply.status.latency_bytes = Le32(latency_bytes);
-        reply.consumed_length = consumed_length;
-        // Send the acknowledgment non-blockingly to avoid a slow client from
-        // blocking the server.
-        auto sent = tx_socket->Send(&reply, sizeof(reply), MSG_DONTWAIT);
-        if (sent < sizeof(reply)) {
-          LOG(ERROR) << "Failed to send entire reply: "
-                     << tx_socket->StrError();
-        }
-      });
-  executor.OnBuffer(std::move(tx_buffer));
-  return true;
-}
-
 const volatile uint8_t* AudioClientConnection::TxBufferAt(size_t offset,
                                                           size_t len) const {
   CHECK(offset < tx_shm_.len() && tx_shm_.len() - offset > len)
       << "Tx buffer bounds outside the buffer area: " << offset << " " << len;
-  return &reinterpret_cast<const volatile uint8_t*>(tx_shm_.get())[offset];
+  const void* ptr = tx_shm_.get();
+  return &reinterpret_cast<const volatile uint8_t*>(ptr)[offset];
 }
 
-bool AudioClientConnection::SendRxBuffer(/*TODO*/) { return false; }
+volatile uint8_t* AudioClientConnection::RxBufferAt(size_t offset,
+                                                    size_t len) {
+  CHECK(offset < rx_shm_.len() && rx_shm_.len() - offset > len)
+      << "Rx buffer bounds outside the buffer area: " << offset << " " << len;
+  void* ptr = rx_shm_.get();
+  return &reinterpret_cast<volatile uint8_t*>(ptr)[offset];
+}
+
 bool AudioClientConnection::SendEvent(/*TODO*/) { return false; }
 
 ssize_t AudioClientConnection::ReceiveMsg(SharedFD socket, void* buffer,
