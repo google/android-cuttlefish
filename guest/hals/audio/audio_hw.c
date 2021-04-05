6e072b2225705d1a344a40c5548c8239bcff946c
Nate Myren <ntmyren@google.com>
2021-04-05 22:21:04 +0000

    Revert "Restrict input streams to 48KHz only in audio HAL"
    Revert submission 1653718
    
    Reason for revert: possible cause of b/184349683
    Reverted Changes:
    I49f17c5e8:Restrict input streams to 48KHz only in audio HAL
    Icae6aec9a:Support capture streams in webrtc and VioS server
    Bug: 184349683
    
    Change-Id: I283695e9212ceb647b43384c791d8fbd507cc9a1

--- guest/hals/audio/audio_hw.c
+++ guest/hals/audio/audio_hw.c
@@ -738,8 +738,7 @@ static int refine_output_parameters(uint32_t *sample_rate, audio_format_t *forma
 
 static int refine_input_parameters(uint32_t *sample_rate, audio_format_t *format, audio_channel_mask_t *channel_mask)
 {
-    // Crosvm only supports 48kHz streams for input
-    static const uint32_t sample_rates [] = {48000};
+    static const uint32_t sample_rates [] = {8000, 11025, 16000, 22050, 44100, 48000};
     static const int sample_rates_count = sizeof(sample_rates)/sizeof(uint32_t);
     bool inval = false;
     // Only PCM_16_bit is supported. If this is changed, stereo to mono drop
