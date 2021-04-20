edafd9c71a92fab045754b8e3cb2533d70c2d824
Cody Schuffelen <schuffelen@google.com>
2021-04-20 23:44:12 +0000

    Revert "Fix + install  guest/libs/wpa_supplicant_8_lib"
    Revert submission 1675032
    
    Reason for revert: Build break on goldfish
    Reverted Changes:
    I9362c0eba:Revert "Remove wpa_supplicant_8_lib"
    Iea2108d93:Fix + install  guest/libs/wpa_supplicant_8_lib
    
    Bug: 137088260
    Change-Id: Id652a1251bad5bf21d34352faaa5392f917a1f26

--- guest/libs/wpa_supplicant_8_lib/driver_cmd_nl80211.h
+++ guest/libs/wpa_supplicant_8_lib/driver_cmd_nl80211.h
@@ -14,9 +14,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <linux/if_ether.h>
+#include <guest/libs/platform_support/api_level_fixes.h>
+
 #include <memory.h>
-#include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,6 +24,7 @@
 #include "common.h"
 #include "linux_ioctl.h"
 #include "wpa_supplicant_i.h"
+#include <linux/if_ether.h>
 
 #define VSOC_WPA_SUPPLICANT_DEBUG 0
 
@@ -39,3 +40,65 @@ typedef struct android_wifi_priv_cmd {
   int used_len;
   int total_len;
 } android_wifi_priv_cmd;
+
+#if VSOC_PLATFORM_SDK_BEFORE(K)
+
+#include "driver.h"
+
+struct i802_bss {
+  struct wpa_driver_nl80211_data* drv;
+  struct i802_bss* next;
+  int ifindex;
+  char ifname[IFNAMSIZ + 1];
+  char brname[IFNAMSIZ];
+
+  unsigned int beacon_set:1;
+  unsigned int added_if_into_bridge:1;
+  unsigned int added_bridge:1;
+  unsigned int in_deinit:1;
+
+  u8 addr[ETH_ALEN];
+
+  int freq;
+
+  void* ctx;
+  struct nl_handle* nl_preq;
+  struct nl_handle* nl_mgmt;
+  struct nl_cb* nl_cb;
+
+  struct nl80211_wiphy_data *wiphy_data;
+  struct dl_list wiphy_list;
+};
+
+struct nl80211_global {
+  struct dl_list interfaces;
+  int if_add_ifindex;
+  struct netlink_data *netlink;
+  struct nl_cb* nl_cb;
+  struct nl_handle* nl;
+  int nl80211_id;
+  int ioctl_sock;  // socket for ioctl() use
+
+  struct nl_handle* nl_event;
+};
+
+struct wpa_driver_nl80211_data {
+  struct nl80211_global* global;
+  struct dl_list list;
+  struct dl_list wiphy_list;
+  char phyname[32];
+  void* ctx;
+  int ifindex;
+  int if_removed;
+  int if_disabled;
+  int ignore_if_down_event;
+  struct rfkill_data* rfkill;
+  struct wpa_driver_capa capa;
+  u8* extended_capa;
+  u8* extended_capa_mask;
+  unsigned int extended_capa_len;
+  int has_capability;
+  // More internal data follows.
+};
+
+#endif  // VSOC_PLATFORM_SDK_AFTER(J)
