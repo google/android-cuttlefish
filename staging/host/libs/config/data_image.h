915e859c60439b953b82031cd9614897ea26496a
Abhijeet Kaur <abkaur@google.com>
2021-04-22 10:23:24 +0000

    [skip ci] Merge "Revert "Reland "Support auto-generation of ESP, add otheros_root""" am: 04da127e32 am: be6aeef4cc am: 386c51a796 am: 3650066f46
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/1683287
    
    Change-Id: I77f4643c0a854ceed545559594d3548033b107a6
    GitOrigin-RevId: 14597c71b04d766e5422fa60e3bfdbc02d2fea55

--- staging/host/libs/config/data_image.h
+++ staging/host/libs/config/data_image.h
@@ -15,9 +15,6 @@ enum class DataImageResult {
 DataImageResult ApplyDataImagePolicy(const CuttlefishConfig& config,
                                      const std::string& path);
 bool InitializeMiscImage(const std::string& misc_image);
-bool InitializeEspImage(const std::string& esp_image,
-                        const std::string& kernel_path,
-                        const std::string& initramfs_path);
 void CreateBlankImage(
     const std::string& image, int num_mb, const std::string& image_fmt);
 
