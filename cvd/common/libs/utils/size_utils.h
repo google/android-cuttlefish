e1a033c2656fd5f6e0497227e300c1c950aa193f
Devin Moore <devinmoore@google.com>
2021-05-04 13:16:30 -0700

    Add instance specific bootconfig to persistent disk
    These bootconfig params were being repacked into the vendor_boot.img
    which does not persist with OTA. They need to be placed in a persistent
    disk like the u-boot environment image where the kernel parameters are
    placed.
    
    Test: boot with multiple instances
    Test: launch_cvd -bootloader u-boot.rom -num_instances=4
    Bug: 184730755
    Change-Id: I1ea97a0684f462fc4337bccc331de7b47ce5aefc

--- cvd/common/libs/utils/size_utils.h
+++ cvd/common/libs/utils/size_utils.h
@@ -19,6 +19,12 @@
 
 namespace cuttlefish {
 
+// Keep the full disk size a multiple of 64k, for crosvm's virtio_blk driver
+constexpr int DISK_SIZE_SHIFT = 16;
+
+// Keep all partitions 4k aligned, for host performance reasons
+constexpr int PARTITION_SIZE_SHIFT = 12;
+
 // Returns the smallest multiple of 2^align_log greater than or equal to val.
 uint64_t AlignToPowerOf2(uint64_t val, uint8_t align_log);
 
