3860333bd2454b59e27344d23376a06c49719b8a
Treehugger Robot <treehugger-gerrit@google.com>
2021-05-07 20:30:36 +0000

    [skip ci] Merge "Avoid excessive memory allocation in CvdVideoFrameBuffer" am: 57987fe728 am: 70415418cf am: d349c56c5d am: 148b02441e
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/1698505
    
    Change-Id: I437afcc37fc9e449b3695831db291c91cc16e43f
    GitOrigin-RevId: d8348211e4a453f5057432cb799fb549b3ea2ae8

--- staging/host/frontend/webrtc/cvd_video_frame_buffer.cpp
+++ staging/host/frontend/webrtc/cvd_video_frame_buffer.cpp
@@ -16,6 +16,9 @@
 
 #include "host/frontend/webrtc/cvd_video_frame_buffer.h"
 
+#include <map>
+#include <mutex>
+#include <vector>
 #include "common/libs/utils/size_utils.h"
 
 namespace cuttlefish {
@@ -28,14 +31,42 @@ inline int AlignStride(int width) {
   return AlignToPowerOf2(width, kLogAlignment);
 }
 
+std::multimap<int, std::vector<uint8_t>> pool;
+std::mutex pool_mutex;
+std::vector<uint8_t> FromPool(int size) {
+  {
+    std::lock_guard<std::mutex> lock(pool_mutex);
+    auto it = pool.find(size);
+    if (it != pool.end()) {
+      auto ret = std::move(it->second);
+      pool.erase(it);
+      return ret;
+    }
+  }
+  return std::vector<uint8_t>(size);
+}
+
+void BackToPool(std::vector<uint8_t> item) {
+  std::lock_guard<std::mutex> lock(pool_mutex);
+  pool.insert({item.size(), std::move(item)});
+}
+
 }  // namespace
 
 CvdVideoFrameBuffer::CvdVideoFrameBuffer(int width, int height)
     : width_(width),
       height_(height),
-      y_(AlignStride(width) * height + kPlanePadding),
-      u_(AlignStride((width + 1) / 2) * ((height + 1) / 2) + kPlanePadding),
-      v_(AlignStride((width + 1) / 2) * ((height + 1) / 2) + kPlanePadding) {}
+      y_(FromPool(AlignStride(width) * height + kPlanePadding)),
+      u_(FromPool(AlignStride((width + 1) / 2) * ((height + 1) / 2) +
+                  kPlanePadding)),
+      v_(FromPool(AlignStride((width + 1) / 2) * ((height + 1) / 2) +
+                  kPlanePadding)) {}
+
+CvdVideoFrameBuffer::~CvdVideoFrameBuffer() {
+  BackToPool(std::move(y_));
+  BackToPool(std::move(u_));
+  BackToPool(std::move(v_));
+}
 
 int CvdVideoFrameBuffer::width() const { return width_; }
 int CvdVideoFrameBuffer::height() const { return height_; }
