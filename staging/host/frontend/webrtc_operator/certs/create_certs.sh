546fcfff7549b3cbb3ff66a0bdb674b2ad36a68f
Guang Zhu <guangzhu@google.com>
2021-05-08 15:52:29 +0000

    [skip ci] Merge "Regenerate webrtc keys." am: fbfbb92163 am: ff30121745 am: 555f553110 am: 930e758d5a
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/1700143
    
    Change-Id: I4fd5a20e5c50e7dce9a632f2a69376438f2c66b2
    GitOrigin-RevId: dda0722c189b8b6dc94bab0158ff843eabba9236

--- staging/host/frontend/webrtc_operator/certs/create_certs.sh
+++ staging/host/frontend/webrtc_operator/certs/create_certs.sh
@@ -3,15 +3,15 @@
 # As explained in
 #  https://gist.github.com/darrenjs/4645f115d10aa4b5cebf57483ec82eca
 
-openssl genrsa -des3 -passout pass:x -out server.pass.key 2048
-openssl rsa -passin pass:x -in server.pass.key -out server.key
+openssl genrsa -des3 -passout pass:xxxx -out server.pass.key 2048
+openssl rsa -passin pass:xxxx -in server.pass.key -out server.key
 rm -f server.pass.key
 
 openssl req \
     -subj "/C=US/ST=California/L=Santa Clara/O=Beyond Aggravated/CN=localhost" \
     -new -key server.key -out server.csr
 
-openssl x509 -req -sha256 -days 365 -in server.csr -signkey server.key -out server.crt
+openssl x509 -req -sha256 -days 99999 -in server.csr -signkey server.key -out server.crt
 rm -f server.csr
 
 # Now create the list of certificates we trust as a client.
