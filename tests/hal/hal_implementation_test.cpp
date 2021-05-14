57af8dec2df4442cf7662a97c3cc269678e020e6
Devin Moore <devinmoore@google.com>
2021-05-14 10:12:49 -0700

    Check AIDL interface versions for hal_implementation_test
    Make sure we have the latest hal version on cuttlefish.
    
    Test: atest hal_implementation_test libaidlmetadata_test
    Bug: 178493361
    Change-Id: I2b6ec74021cbc4f1f45d0638a9b39f64bc32f8eb

--- tests/hal/hal_implementation_test.cpp
+++ tests/hal/hal_implementation_test.cpp
@@ -99,31 +99,47 @@ static const std::set<std::string> kKnownMissingHidl = {
 };
 // clang-format on
 
-static const std::set<std::string> kKnownMissingAidl = {
+struct VersionedAidlPackage {
+  std::string name;
+  size_t version;
+  bool operator<(const VersionedAidlPackage& rhs) const {
+    return (name < rhs.name || (name == rhs.name && version < rhs.version));
+  }
+};
+
+static const std::set<VersionedAidlPackage> kKnownMissingAidl = {
     // types-only packages, which never expect a default implementation
-    "android.hardware.biometrics.common.",
-    "android.hardware.common.",
-    "android.hardware.common.fmq.",
-    "android.hardware.graphics.common.",
+    {"android.hardware.biometrics.common.", 1},
+    {"android.hardware.common.", 1},
+    {"android.hardware.common.", 2},
+    {"android.hardware.common.fmq.", 1},
+    {"android.hardware.graphics.common.", 1},
+    {"android.hardware.graphics.common.", 2},
 
     // These KeyMaster types are in an AIDL types-only HAL because they're used
     // by the Identity Credential AIDL HAL. Remove this when fully porting
     // KeyMaster to AIDL.
-    "android.hardware.keymaster.",
+    {"android.hardware.keymaster.", 1},
+    {"android.hardware.keymaster.", 2},
+    {"android.hardware.keymaster.", 3},
 
     // Sound trigger doesn't have a default implementation.
-    "android.hardware.soundtrigger3.",
-    "android.media.soundtrigger.",
-    "android.media.audio.common.",
+    {"android.hardware.soundtrigger3.", 1},
+    {"android.media.soundtrigger.", 1},
+    {"android.media.audio.common.", 1},
 
     // These types are only used in Automotive.
-    "android.automotive.computepipe.registry.",
-    "android.automotive.computepipe.runner.",
-    "android.automotive.watchdog.",
-    "android.frameworks.automotive.powerpolicy.",
-    "android.frameworks.automotive.telemetry.",
-    "android.hardware.automotive.audiocontrol.",
-    "android.hardware.automotive.occupant_awareness.",
+    {"android.automotive.computepipe.registry.", 1},
+    {"android.automotive.computepipe.runner.", 1},
+    {"android.automotive.watchdog.", 2},
+    {"android.automotive.watchdog.", 3},
+    {"android.frameworks.automotive.powerpolicy.", 1},
+    {"android.frameworks.automotive.telemetry.", 1},
+    {"android.hardware.automotive.audiocontrol.", 1},
+    {"android.hardware.automotive.occupant_awareness.", 1},
+
+    // This version needs to be implemented (b/190505425)
+    {"android.system.keystore2.", 2},
 };
 
 // AOSP packages which are never considered
@@ -187,13 +203,13 @@ static bool isAospAidlInterface(const std::string& name) {
          !base::StartsWith(name, "android.aidl.tests");
 }
 
-static std::set<std::string> allAidlManifestInterfaces() {
-  std::set<std::string> ret;
+static std::set<VersionedAidlPackage> allAidlManifestInterfaces() {
+  std::set<VersionedAidlPackage> ret;
   auto setInserter = [&](const vintf::ManifestInstance& i) -> bool {
     if (i.format() != vintf::HalFormat::AIDL) {
       return true;  // continue
     }
-    ret.insert(i.package() + "." + i.interface());
+    ret.insert({i.package() + "." + i.interface(), i.version().minorVer});
     return true;  // continue
   };
   vintf::VintfObject::GetDeviceHalManifest()->forEachInstance(setInserter);
@@ -265,9 +281,10 @@ TEST(Hal, HidlInterfacesImplemented) {
 }
 
 TEST(Hal, AllAidlInterfacesAreInAosp) {
-  for (const std::string& name : allAidlManifestInterfaces()) {
-    EXPECT_TRUE(isAospAidlInterface(name))
-        << "This device should only have AOSP interfaces, not: " << name;
+  for (const auto& package : allAidlManifestInterfaces()) {
+    EXPECT_TRUE(isAospAidlInterface(package.name))
+        << "This device should only have AOSP interfaces, not: "
+        << package.name;
   }
 }
 
@@ -278,46 +295,84 @@ std::string getAidlPackage(const std::string& aidlType) {
   return aidlType.substr(0, lastDot + 1);
 }
 
-TEST(Hal, AidlInterfacesImplemented) {
-  std::set<std::string> manifest = allAidlManifestInterfaces();
-  std::set<std::string> thoughtMissing = kKnownMissingAidl;
+struct AidlPackageCheck {
+  bool hasRegistration;
+  bool knownMissing;
+};
 
-  for (const auto& iface : AidlInterfaceMetadata::all()) {
-    ASSERT_FALSE(iface.types.empty()) << iface.name;  // sanity
-    if (std::none_of(iface.types.begin(), iface.types.end(),
+TEST(Hal, AidlInterfacesImplemented) {
+  std::set<VersionedAidlPackage> manifest = allAidlManifestInterfaces();
+  std::set<VersionedAidlPackage> thoughtMissing = kKnownMissingAidl;
+
+  for (const auto& treePackage : AidlInterfaceMetadata::all()) {
+    ASSERT_FALSE(treePackage.types.empty()) << treePackage.name;
+    if (std::none_of(treePackage.types.begin(), treePackage.types.end(),
                      isAospAidlInterface))
       continue;
-    if (iface.stability != "vintf") continue;
+    if (treePackage.stability != "vintf") continue;
 
-    bool hasRegistration = false;
-    bool knownMissing = false;
-    for (const std::string& type : iface.types) {
-      if (manifest.erase(type) > 0) hasRegistration = true;
-      if (thoughtMissing.erase(getAidlPackage(type)) > 0) knownMissing = true;
+    // expect versions from 1 to latest version. If the package has development
+    // the latest version is the latest known version + 1. Each of these need
+    // to be checked for registration and knownMissing.
+    std::map<size_t, AidlPackageCheck> expectedVersions;
+    for (const auto version : treePackage.versions) {
+      expectedVersions[version] = {false, false};
+    }
+    if (treePackage.has_development) {
+      size_t version =
+          treePackage.versions.empty() ? 1 : *treePackage.versions.rbegin() + 1;
+      expectedVersions[version] = {false, false};
     }
 
-    if (knownMissing) {
-      if (hasRegistration) {
-        ADD_FAILURE() << "Interface in missing list, but available: "
-                      << iface.name
-                      << " which declares the following types:\n    "
-                      << base::Join(iface.types, "\n    ");
+    // Check all types and versions defined by the package for registration.
+    // The package version is considered registered if any of those types are
+    // present in the manifest with the same version.
+    // The package version is considered known missing if it is found in
+    // thoughtMissing.
+    bool latestRegistered = false;
+    for (const std::string& type : treePackage.types) {
+      for (auto& [version, check] : expectedVersions) {
+        if (manifest.erase({type, version}) > 0) {
+          if (version == expectedVersions.rbegin()->first) {
+            latestRegistered = true;
+          }
+          check.hasRegistration = true;
+        }
+        if (thoughtMissing.erase({getAidlPackage(type), version}) > 0)
+          check.knownMissing = true;
       }
-
-      continue;
     }
 
-    EXPECT_TRUE(hasRegistration)
-        << iface.name << " which declares the following types:\n    "
-        << base::Join(iface.types, "\n    ");
+    if (!latestRegistered && !expectedVersions.rbegin()->second.knownMissing) {
+      ADD_FAILURE() << "The latest version ("
+                    << expectedVersions.rbegin()->first
+                    << ") of the package is not implemented: "
+                    << treePackage.name
+                    << " which declares the following types:\n    "
+                    << base::Join(treePackage.types, "\n    ");
+    }
+
+    for (const auto& [version, check] : expectedVersions) {
+      if (check.knownMissing) {
+        if (check.hasRegistration) {
+          ADD_FAILURE() << "Package in missing list, but available: "
+                        << treePackage.name << " V" << version
+                        << " which declares the following types:\n    "
+                        << base::Join(treePackage.types, "\n    ");
+        }
+
+        continue;
+      }
+    }
   }
 
-  for (const std::string& iface : thoughtMissing) {
-    ADD_FAILURE() << "Interface in manifest list and cannot find it anywhere: "
-                  << iface;
+  for (const auto& package : thoughtMissing) {
+    ADD_FAILURE() << "Interface in missing list and cannot find it anywhere: "
+                  << package.name << " V" << package.version;
   }
 
-  for (const std::string& iface : manifest) {
-    ADD_FAILURE() << "Can't find manifest entry in tree: " << iface;
+  for (const auto& package : manifest) {
+    ADD_FAILURE() << "Can't find manifest entry in tree: " << package.name
+                  << " version: " << package.version;
   }
 }
