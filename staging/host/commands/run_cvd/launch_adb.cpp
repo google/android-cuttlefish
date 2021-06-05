db2fb68de2993592b0010978d4706e1bb7f072b5
Treehugger Robot <treehugger-gerrit@google.com>
2021-06-05 00:21:27 +0000

    [skip ci] Merge changes Id4b2c27e,If4616f38 am: 4ab933633a am: aaf4cf0c48 am: a751b3bdd8 am: 7693529a76
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/1719280
    
    Change-Id: Ie5b951810f0ed483730e0ab5f526be69908944aa
    GitOrigin-RevId: fbe85d653615964754717b3c8f02a78ab6943bc7

--- staging/host/commands/run_cvd/launch_adb.cpp
+++ staging/host/commands/run_cvd/launch_adb.cpp
@@ -67,90 +67,148 @@ bool AdbVsockConnectorEnabled(const CuttlefishConfig& config) {
          AdbModeEnabled(config, AdbMode::NativeVsock);
 }
 
+class AdbConnector : public CommandSource {
+ public:
+  INJECT(AdbConnector(const CuttlefishConfig& config)) : config_(config) {}
+
+  // CommandSource
+  std::vector<Command> Commands() override {
+    Command console_forwarder_cmd(ConsoleForwarderBinary());
+    Command adb_connector(AdbConnectorBinary());
+    std::set<std::string> addresses;
+
+    if (AdbTcpConnectorEnabled(config_)) {
+      addresses.insert(GetAdbConnectorTcpArg(config_));
+    }
+    if (AdbVsockConnectorEnabled(config_)) {
+      addresses.insert(GetAdbConnectorVsockArg(config_));
+    }
+
+    if (addresses.size() == 0) {
+      return {};
+    }
+    std::string address_arg = "--addresses=";
+    for (auto& arg : addresses) {
+      address_arg += arg + ",";
+    }
+    address_arg.pop_back();
+    adb_connector.AddParameter(address_arg);
+    std::vector<Command> commands;
+    commands.emplace_back(std::move(adb_connector));
+    return std::move(commands);
+  }
+
+  // Feature
+  bool Enabled() const override {
+    return AdbTcpConnectorEnabled(config_) || AdbVsockConnectorEnabled(config_);
+  }
+  std::string Name() const override { return "AdbConnector"; }
+  std::unordered_set<Feature*> Dependencies() const override { return {}; }
+
+ protected:
+  bool Setup() override { return true; }
+
+ private:
+  const CuttlefishConfig& config_;
+};
+
+class SocketVsockProxy : public CommandSource {
+ public:
+  INJECT(SocketVsockProxy(const CuttlefishConfig& config,
+                          const CuttlefishConfig::InstanceSpecific& instance,
+                          KernelLogPipeProvider& log_pipe_provider))
+      : config_(config),
+        instance_(instance),
+        log_pipe_provider_(log_pipe_provider) {}
+
+  // CommandSource
+  std::vector<Command> Commands() override {
+    std::vector<Command> commands;
+    if (AdbVsockTunnelEnabled(config_)) {
+      Command adb_tunnel(SocketVsockProxyBinary());
+      adb_tunnel.AddParameter("-adbd_events_fd=", kernel_log_pipe_);
+      /**
+       * This socket_vsock_proxy (a.k.a. sv proxy) runs on the host. It assumes
+       * that another sv proxy runs inside the guest. see:
+       * shared/config/init.vendor.rc The sv proxy in the guest exposes
+       * vsock:cid:6520 across the cuttlefish instances in multi-tenancy. cid is
+       * different per instance.
+       *
+       * This host sv proxy should cooperate with the guest sv proxy. Thus, one
+       * end of the tunnel is vsock:cid:6520 regardless of instance number.
+       * Another end faces the host adb daemon via tcp. Thus, the server type is
+       * tcp here. The tcp port differs from instance to instance, and is
+       * instance.host_port()
+       *
+       */
+      adb_tunnel.AddParameter("--server=tcp");
+      adb_tunnel.AddParameter("--vsock_port=6520");
+      adb_tunnel.AddParameter("--server_fd=", tcp_server_);
+      adb_tunnel.AddParameter("--vsock_cid=", instance_.vsock_guest_cid());
+      commands.emplace_back(std::move(adb_tunnel));
+    }
+    if (AdbVsockHalfTunnelEnabled(config_)) {
+      Command adb_tunnel(SocketVsockProxyBinary());
+      adb_tunnel.AddParameter("-adbd_events_fd=", kernel_log_pipe_);
+      /*
+       * This socket_vsock_proxy (a.k.a. sv proxy) runs on the host, and
+       * cooperates with the adbd inside the guest. See this file:
+       *  shared/device.mk, especially the line says "persist.adb.tcp.port="
+       *
+       * The guest adbd is listening on vsock:cid:5555 across cuttlefish
+       * instances. Sv proxy faces the host adb daemon via tcp. The server type
+       * should be therefore tcp, and the port should differ from instance to
+       * instance and be equal to instance.host_port()
+       */
+      adb_tunnel.AddParameter("--server=tcp");
+      adb_tunnel.AddParameter("--vsock_port=", 5555);
+      adb_tunnel.AddParameter("--server_fd=", tcp_server_);
+      adb_tunnel.AddParameter("--vsock_cid=", instance_.vsock_guest_cid());
+      commands.emplace_back(std::move(adb_tunnel));
+    }
+    return commands;
+  }
+
+  // Feature
+  bool Enabled() const override {
+    return AdbVsockTunnelEnabled(config_) || AdbVsockHalfTunnelEnabled(config_);
+  }
+  std::string Name() const override { return "SocketVsockProxy"; }
+  std::unordered_set<Feature*> Dependencies() const override {
+    return {static_cast<Feature*>(&log_pipe_provider_)};
+  }
+
+ protected:
+  bool Setup() override {
+    tcp_server_ =
+        SharedFD::SocketLocalServer(instance_.host_port(), SOCK_STREAM);
+    if (!tcp_server_->IsOpen()) {
+      LOG(ERROR) << "Unable to create socket_vsock_proxy server socket: "
+                 << tcp_server_->StrError();
+      return false;
+    }
+    kernel_log_pipe_ = log_pipe_provider_.KernelLogPipe();
+    return true;
+  }
+
+ private:
+  const CuttlefishConfig& config_;
+  const CuttlefishConfig::InstanceSpecific& instance_;
+  KernelLogPipeProvider& log_pipe_provider_;
+  SharedFD kernel_log_pipe_;
+  SharedFD tcp_server_;
+};
+
 }  // namespace
 
-std::vector<Command> LaunchAdbConnectorIfEnabled(
-    const CuttlefishConfig& config) {
-  Command adb_connector(AdbConnectorBinary());
-  std::set<std::string> addresses;
-
-  if (AdbTcpConnectorEnabled(config)) {
-    addresses.insert(GetAdbConnectorTcpArg(config));
-  }
-  if (AdbVsockConnectorEnabled(config)) {
-    addresses.insert(GetAdbConnectorVsockArg(config));
-  }
-
-  if (addresses.size() == 0) {
-    return {};
-  }
-  std::string address_arg = "--addresses=";
-  for (auto& arg : addresses) {
-    address_arg += arg + ",";
-  }
-  address_arg.pop_back();
-  adb_connector.AddParameter(address_arg);
-  std::vector<Command> commands;
-  commands.emplace_back(std::move(adb_connector));
-  return std::move(commands);
-}
-
-std::vector<Command> LaunchSocketVsockProxyIfEnabled(
-    const CuttlefishConfig& config, SharedFD adbd_events_pipe) {
-  auto instance = config.ForDefaultInstance();
-  auto append = [](const std::string& s, const int i) -> std::string {
-    return s + std::to_string(i);
-  };
-  auto tcp_server =
-      SharedFD::SocketLocalServer(instance.host_port(), SOCK_STREAM);
-  CHECK(tcp_server->IsOpen())
-      << "Unable to create socket_vsock_proxy server socket: "
-      << tcp_server->StrError();
-  std::vector<Command> commands;
-  if (AdbVsockTunnelEnabled(config)) {
-    Command adb_tunnel(SocketVsockProxyBinary());
-    adb_tunnel.AddParameter("-adbd_events_fd=", adbd_events_pipe);
-    /**
-     * This socket_vsock_proxy (a.k.a. sv proxy) runs on the host. It assumes
-     * that another sv proxy runs inside the guest. see:
-     * shared/config/init.vendor.rc The sv proxy in the guest exposes
-     * vsock:cid:6520 across the cuttlefish instances in multi-tenancy. cid is
-     * different per instance.
-     *
-     * This host sv proxy should cooperate with the guest sv proxy. Thus, one
-     * end of the tunnel is vsock:cid:6520 regardless of instance number.
-     * Another end faces the host adb daemon via tcp. Thus, the server type is
-     * tcp here. The tcp port differs from instance to instance, and is
-     * instance.host_port()
-     *
-     */
-    adb_tunnel.AddParameter("--server=tcp");
-    adb_tunnel.AddParameter("--vsock_port=6520");
-    adb_tunnel.AddParameter(std::string{"--server_fd="}, tcp_server);
-    adb_tunnel.AddParameter(std::string{"--vsock_cid="} +
-                            std::to_string(instance.vsock_guest_cid()));
-    commands.emplace_back(std::move(adb_tunnel));
-  }
-  if (AdbVsockHalfTunnelEnabled(config)) {
-    Command adb_tunnel(SocketVsockProxyBinary());
-    adb_tunnel.AddParameter("-adbd_events_fd=", adbd_events_pipe);
-    /*
-     * This socket_vsock_proxy (a.k.a. sv proxy) runs on the host, and
-     * cooperates with the adbd inside the guest. See this file:
-     *  shared/device.mk, especially the line says "persist.adb.tcp.port="
-     *
-     * The guest adbd is listening on vsock:cid:5555 across cuttlefish
-     * instances. Sv proxy faces the host adb daemon via tcp. The server type
-     * should be therefore tcp, and the port should differ from instance to
-     * instance and be equal to instance.host_port()
-     */
-    adb_tunnel.AddParameter("--server=tcp");
-    adb_tunnel.AddParameter(append("--vsock_port=", 5555));
-    adb_tunnel.AddParameter(std::string{"--server_fd="}, tcp_server);
-    adb_tunnel.AddParameter(append("--vsock_cid=", instance.vsock_guest_cid()));
-    commands.emplace_back(std::move(adb_tunnel));
-  }
-  return commands;
+fruit::Component<fruit::Required<const CuttlefishConfig, KernelLogPipeProvider,
+                                 const CuttlefishConfig::InstanceSpecific>>
+launchAdbComponent() {
+  return fruit::createComponent()
+      .addMultibinding<CommandSource, AdbConnector>()
+      .addMultibinding<CommandSource, SocketVsockProxy>()
+      .addMultibinding<Feature, AdbConnector>()
+      .addMultibinding<Feature, SocketVsockProxy>();
 }
 
 }  // namespace cuttlefish
