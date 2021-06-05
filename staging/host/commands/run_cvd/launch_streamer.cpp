5eeac2a0701cc4027efef2211fc737187daeef74
Treehugger Robot <treehugger-gerrit@google.com>
2021-06-05 00:21:27 +0000

    Merge changes Id4b2c27e,If4616f38 am: 4ab933633a am: aaf4cf0c48 am: a751b3bdd8 am: 7693529a76
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/1719280
    
    Change-Id: Ie5b951810f0ed483730e0ab5f526be69908944aa
    GitOrigin-RevId: fbe85d653615964754717b3c8f02a78ab6943bc7

--- staging/host/commands/run_cvd/launch_streamer.cpp
+++ staging/host/commands/run_cvd/launch_streamer.cpp
@@ -41,69 +41,6 @@ SharedFD CreateUnixInputServer(const std::string& path) {
   return server;
 }
 
-// Creates the frame and input sockets and add the relevant arguments to the vnc
-// server and webrtc commands
-void CreateStreamerServers(Command* cmd, const CuttlefishConfig& config) {
-  SharedFD touch_server;
-  SharedFD keyboard_server;
-
-  auto instance = config.ForDefaultInstance();
-  if (config.vm_manager() == vm_manager::QemuManager::name()) {
-    cmd->AddParameter("-write_virtio_input");
-
-    touch_server =
-        SharedFD::VsockServer(instance.touch_server_port(), SOCK_STREAM);
-    keyboard_server =
-        SharedFD::VsockServer(instance.keyboard_server_port(), SOCK_STREAM);
-  } else {
-    touch_server = CreateUnixInputServer(instance.touch_socket_path());
-    keyboard_server = CreateUnixInputServer(instance.keyboard_socket_path());
-  }
-  if (!touch_server->IsOpen()) {
-    LOG(ERROR) << "Could not open touch server: " << touch_server->StrError();
-    return;
-  }
-  cmd->AddParameter("-touch_fd=", touch_server);
-
-  if (!keyboard_server->IsOpen()) {
-    LOG(ERROR) << "Could not open keyboard server: "
-               << keyboard_server->StrError();
-    return;
-  }
-  cmd->AddParameter("-keyboard_fd=", keyboard_server);
-
-  if (config.enable_webrtc() &&
-      config.vm_manager() == vm_manager::CrosvmManager::name()) {
-    SharedFD switches_server =
-        CreateUnixInputServer(instance.switches_socket_path());
-    if (!switches_server->IsOpen()) {
-      LOG(ERROR) << "Could not open switches server: "
-                 << switches_server->StrError();
-      return;
-    }
-    cmd->AddParameter("-switches_fd=", switches_server);
-  }
-
-  SharedFD frames_server = CreateUnixInputServer(instance.frames_socket_path());
-  if (!frames_server->IsOpen()) {
-    LOG(ERROR) << "Could not open frames server: " << frames_server->StrError();
-    return;
-  }
-  cmd->AddParameter("-frame_server_fd=", frames_server);
-
-  if (config.enable_audio()) {
-    auto path = config.ForDefaultInstance().audio_server_path();
-    auto audio_server =
-        SharedFD::SocketLocalServer(path.c_str(), false, SOCK_SEQPACKET, 0666);
-    if (!audio_server->IsOpen()) {
-      LOG(ERROR) << "Could not create audio server: "
-                 << audio_server->StrError();
-      return;
-    }
-    cmd->AddParameter("--audio_server_fd=", audio_server);
-  }
-}
-
 std::vector<Command> LaunchCustomActionServers(Command& webrtc_cmd,
                                                const CuttlefishConfig& config) {
   bool first = true;
@@ -141,84 +78,236 @@ std::vector<Command> LaunchCustomActionServers(Command& webrtc_cmd,
   return commands;
 }
 
+// Creates the frame and input sockets and add the relevant arguments to the vnc
+// server and webrtc commands
+class StreamerSockets : public virtual Feature {
+ public:
+  INJECT(StreamerSockets(const CuttlefishConfig& config,
+                         const CuttlefishConfig::InstanceSpecific& instance))
+      : config_(config), instance_(instance) {}
+
+  void AppendCommandArguments(Command& cmd) {
+    if (config_.vm_manager() == vm_manager::QemuManager::name()) {
+      cmd.AddParameter("-write_virtio_input");
+    }
+    cmd.AddParameter("-touch_fd=", touch_server_);
+    cmd.AddParameter("-keyboard_fd=", keyboard_server_);
+    cmd.AddParameter("-frame_server_fd=", frames_server_);
+    if (config_.enable_audio()) {
+      cmd.AddParameter("--audio_server_fd=", audio_server_);
+    }
+  }
+
+  // Feature
+  bool Enabled() const override { return true; }
+  std::string Name() const override { return "StreamerSockets"; }
+  std::unordered_set<Feature*> Dependencies() const override { return {}; }
+
+ protected:
+  bool Setup() override {
+    if (config_.vm_manager() == vm_manager::QemuManager::name()) {
+      touch_server_ =
+          SharedFD::VsockServer(instance_.touch_server_port(), SOCK_STREAM);
+      keyboard_server_ =
+          SharedFD::VsockServer(instance_.keyboard_server_port(), SOCK_STREAM);
+    } else {
+      touch_server_ = CreateUnixInputServer(instance_.touch_socket_path());
+      keyboard_server_ =
+          CreateUnixInputServer(instance_.keyboard_socket_path());
+    }
+    if (!touch_server_->IsOpen()) {
+      LOG(ERROR) << "Failed to open touch server: "
+                 << touch_server_->StrError();
+      return false;
+    }
+    if (!keyboard_server_->IsOpen()) {
+      LOG(ERROR) << "Failed to open keyboard server"
+                 << keyboard_server_->StrError();
+      return false;
+    }
+    frames_server_ = CreateUnixInputServer(instance_.frames_socket_path());
+    if (!frames_server_->IsOpen()) {
+      LOG(ERROR) << "Could not open frames server: "
+                 << frames_server_->StrError();
+      return false;
+    }
+    // TODO(schuffelen): Make this a separate optional feature?
+    if (config_.enable_audio()) {
+      auto path = config_.ForDefaultInstance().audio_server_path();
+      audio_server_ =
+          SharedFD::SocketLocalServer(path, false, SOCK_SEQPACKET, 0666);
+      if (!audio_server_->IsOpen()) {
+        LOG(ERROR) << "Could not create audio server: "
+                   << audio_server_->StrError();
+        return false;
+      }
+    }
+    return true;
+  }
+
+ private:
+  const CuttlefishConfig& config_;
+  const CuttlefishConfig::InstanceSpecific& instance_;
+  SharedFD touch_server_;
+  SharedFD keyboard_server_;
+  SharedFD frames_server_;
+  SharedFD audio_server_;
+};
+
+class VncServer : public virtual CommandSource {
+ public:
+  INJECT(VncServer(const CuttlefishConfig& config,
+                   const CuttlefishConfig::InstanceSpecific& instance,
+                   StreamerSockets& sockets))
+      : config_(config), instance_(instance), sockets_(sockets) {}
+  // CommandSource
+  std::vector<Command> Commands() override {
+    Command vnc_server(VncServerBinary());
+    vnc_server.AddParameter("-port=", instance_.vnc_server_port());
+    sockets_.AppendCommandArguments(vnc_server);
+
+    std::vector<Command> commands;
+    commands.emplace_back(std::move(vnc_server));
+    return commands;
+  }
+
+  // Feature
+  bool Enabled() const override { return config_.enable_vnc_server(); }
+  std::string Name() const override { return "VncServer"; }
+  std::unordered_set<Feature*> Dependencies() const override {
+    return {static_cast<Feature*>(&sockets_)};
+  }
+
+ protected:
+  bool Setup() override { return true; }
+
+ private:
+  const CuttlefishConfig& config_;
+  const CuttlefishConfig::InstanceSpecific& instance_;
+  StreamerSockets& sockets_;
+};
+
+class WebRtcServer : public virtual CommandSource {
+ public:
+  INJECT(WebRtcServer(const CuttlefishConfig& config,
+                      const CuttlefishConfig::InstanceSpecific& instance,
+                      StreamerSockets& sockets,
+                      KernelLogPipeProvider& log_pipe_provider))
+      : config_(config),
+        instance_(instance),
+        sockets_(sockets),
+        log_pipe_provider_(log_pipe_provider) {}
+  // CommandSource
+  std::vector<Command> Commands() override {
+    std::vector<Command> commands;
+    if (instance_.start_webrtc_sig_server()) {
+      Command sig_server(WebRtcSigServerBinary());
+      sig_server.AddParameter("-assets_dir=", config_.webrtc_assets_dir());
+      if (!config_.webrtc_certs_dir().empty()) {
+        sig_server.AddParameter("-certs_dir=", config_.webrtc_certs_dir());
+      }
+      sig_server.AddParameter("-http_server_port=", config_.sig_server_port());
+      commands.emplace_back(std::move(sig_server));
+    }
+
+    auto stopper = [host_socket = std::move(host_socket_)](Subprocess* proc) {
+      struct timeval timeout;
+      timeout.tv_sec = 3;
+      timeout.tv_usec = 0;
+      CHECK(host_socket->SetSockOpt(SOL_SOCKET, SO_RCVTIMEO, &timeout,
+                                    sizeof(timeout)) == 0)
+          << "Could not set receive timeout";
+
+      WriteAll(host_socket, "C");
+      char response[1];
+      int read_ret = host_socket->Read(response, sizeof(response));
+      if (read_ret != 0) {
+        LOG(ERROR) << "Failed to read response from webrtc";
+      }
+      cuttlefish::KillSubprocess(proc);
+      return true;
+    };
+
+    Command webrtc(WebRtcBinary(), SubprocessStopper(stopper));
+    webrtc.UnsetFromEnvironment({"http_proxy"});
+    sockets_.AppendCommandArguments(webrtc);
+    if (config_.vm_manager() == vm_manager::CrosvmManager::name()) {
+      webrtc.AddParameter("-switches_fd=", switches_server_);
+    }
+    // Currently there is no way to ensure the signaling server will already
+    // have bound the socket to the port by the time the webrtc process runs
+    // (the common technique of doing it from the launcher is not possible here
+    // as the server library being used creates its own sockets). However, this
+    // issue is mitigated slightly by doing some retrying and backoff in the
+    // webrtc process when connecting to the websocket, so it shouldn't be an
+    // issue most of the time.
+    webrtc.AddParameter("--command_fd=", client_socket_);
+    webrtc.AddParameter("-kernel_log_events_fd=", kernel_log_events_pipe_);
+
+    // TODO get from launcher params
+    for (auto& action : LaunchCustomActionServers(webrtc, config_)) {
+      commands.emplace_back(std::move(action));
+    }
+    commands.emplace_back(std::move(webrtc));
+
+    return commands;
+  }
+
+  // Feature
+  bool Enabled() const override { return config_.enable_webrtc(); }
+  std::string Name() const override { return "WebRtcServer"; }
+  std::unordered_set<Feature*> Dependencies() const override {
+    return {static_cast<Feature*>(&sockets_),
+            static_cast<Feature*>(&log_pipe_provider_)};
+  }
+
+ protected:
+  bool Setup() override {
+    if (!SharedFD::SocketPair(AF_LOCAL, SOCK_STREAM, 0, &client_socket_,
+                              &host_socket_)) {
+      LOG(ERROR) << "Could not open command socket for webRTC";
+      return false;
+    }
+    if (config_.vm_manager() == vm_manager::CrosvmManager::name()) {
+      switches_server_ =
+          CreateUnixInputServer(instance_.switches_socket_path());
+      if (!switches_server_->IsOpen()) {
+        LOG(ERROR) << "Could not open switches server: "
+                   << switches_server_->StrError();
+        return false;
+      }
+    }
+    kernel_log_events_pipe_ = log_pipe_provider_.KernelLogPipe();
+    if (!kernel_log_events_pipe_->IsOpen()) {
+      LOG(ERROR) << "Failed to get a kernel log events pipe: "
+                 << kernel_log_events_pipe_->StrError();
+      return false;
+    }
+    return true;
+  }
+
+ private:
+  const CuttlefishConfig& config_;
+  const CuttlefishConfig::InstanceSpecific& instance_;
+  StreamerSockets& sockets_;
+  KernelLogPipeProvider& log_pipe_provider_;
+  SharedFD kernel_log_events_pipe_;
+  SharedFD client_socket_;
+  SharedFD host_socket_;
+  SharedFD switches_server_;
+};
+
 }  // namespace
 
-std::vector<Command> LaunchVNCServer(const CuttlefishConfig& config) {
-  auto instance = config.ForDefaultInstance();
-  // Launch the vnc server, don't wait for it to complete
-  auto port_options = "-port=" + std::to_string(instance.vnc_server_port());
-  Command vnc_server(VncServerBinary());
-  vnc_server.AddParameter(port_options);
-
-  CreateStreamerServers(&vnc_server, config);
-
-  std::vector<Command> commands;
-  commands.emplace_back(std::move(vnc_server));
-  return std::move(commands);
-}
-
-std::vector<Command> LaunchWebRTC(const CuttlefishConfig& config,
-                                  SharedFD kernel_log_events_pipe) {
-  std::vector<Command> commands;
-  if (config.ForDefaultInstance().start_webrtc_sig_server()) {
-    Command sig_server(WebRtcSigServerBinary());
-    sig_server.AddParameter("-assets_dir=", config.webrtc_assets_dir());
-    if (!config.webrtc_certs_dir().empty()) {
-      sig_server.AddParameter("-certs_dir=", config.webrtc_certs_dir());
-    }
-    sig_server.AddParameter("-http_server_port=", config.sig_server_port());
-    commands.emplace_back(std::move(sig_server));
-  }
-
-  // Currently there is no way to ensure the signaling server will already have
-  // bound the socket to the port by the time the webrtc process runs (the
-  // common technique of doing it from the launcher is not possible here as the
-  // server library being used creates its own sockets). However, this issue is
-  // mitigated slightly by doing some retrying and backoff in the webrtc process
-  // when connecting to the websocket, so it shouldn't be an issue most of the
-  // time.
-  SharedFD client_socket;
-  SharedFD host_socket;
-  CHECK(SharedFD::SocketPair(AF_LOCAL, SOCK_STREAM, 0, &client_socket,
-                             &host_socket))
-      << "Could not open command socket for webRTC";
-
-  auto stopper = [host_socket = std::move(host_socket)](Subprocess* proc) {
-    struct timeval timeout;
-    timeout.tv_sec = 3;
-    timeout.tv_usec = 0;
-    CHECK(host_socket->SetSockOpt(SOL_SOCKET, SO_RCVTIMEO, &timeout,
-                                  sizeof(timeout)) == 0)
-        << "Could not set receive timeout";
-
-    WriteAll(host_socket, "C");
-    char response[1];
-    int read_ret = host_socket->Read(response, sizeof(response));
-    if (read_ret != 0) {
-      LOG(ERROR) << "Failed to read response from webrtc";
-    }
-    cuttlefish::KillSubprocess(proc);
-    return true;
-  };
-
-  Command webrtc(WebRtcBinary(), SubprocessStopper(stopper));
-
-  webrtc.UnsetFromEnvironment({"http_proxy"});
-
-  CreateStreamerServers(&webrtc, config);
-
-  webrtc.AddParameter("--command_fd=", client_socket);
-  webrtc.AddParameter("-kernel_log_events_fd=", kernel_log_events_pipe);
-
-  auto actions = LaunchCustomActionServers(webrtc, config);
-
-  // TODO get from launcher params
-  commands.emplace_back(std::move(webrtc));
-  for (auto& action : actions) {
-    commands.emplace_back(std::move(action));
-  }
-
-  return commands;
+fruit::Component<fruit::Required<const CuttlefishConfig, KernelLogPipeProvider,
+                                 const CuttlefishConfig::InstanceSpecific>>
+launchStreamerComponent() {
+  return fruit::createComponent()
+      .addMultibinding<CommandSource, WebRtcServer>()
+      .addMultibinding<CommandSource, VncServer>()
+      .addMultibinding<Feature, StreamerSockets>()
+      .addMultibinding<Feature, WebRtcServer>()
+      .addMultibinding<Feature, VncServer>();
 }
 
 }  // namespace cuttlefish
