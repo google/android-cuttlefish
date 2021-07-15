db75deb0c504e69184eae753d93f14c84b0121de
Treehugger Robot <treehugger-gerrit@google.com>
2021-07-15 19:24:03 +0000

    Merge "Support different MAC addresses for each instance"

--- cvd/host/libs/config/cuttlefish_config.h
+++ cvd/host/libs/config/cuttlefish_config.h
@@ -157,9 +157,6 @@ class CuttlefishConfig {
   void set_enable_vehicle_hal_grpc_server(bool enable_vhal_server);
   bool enable_vehicle_hal_grpc_server() const;
 
-  void set_vehicle_hal_grpc_server_binary(const std::string& vhal_server_binary);
-  std::string vehicle_hal_grpc_server_binary() const;
-
   void set_custom_actions(const std::vector<CustomActionConfig>& actions);
   std::vector<CustomActionConfig> custom_actions() const;
 
@@ -389,7 +386,7 @@ class CuttlefishConfig {
 
     std::string instance_internal_dir() const;
 
-    std::string touch_socket_path() const;
+    std::string touch_socket_path(int screen_idx) const;
     std::string keyboard_socket_path() const;
     std::string switches_socket_path() const;
     std::string frames_socket_path() const;
