c7286108846894801e271f4a14b4d76ac86a3bf3
Jorge E. Moreira <jemoreira@google.com>
2021-08-05 04:43:45 +0000

    Use virtio-snd instead of AC97 am: b157b57cd8 am: 2c25fd56b7 am: cbc757cd86 am: 4e32dca2ad
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/1740417
    
    Change-Id: Id7130ed9fb178d656b51cfb8aae472c0f7643bb9
    GitOrigin-RevId: c9fd5f974c7410584f804f18312f71aa002bf561

--- staging/host/libs/vm_manager/crosvm_manager.cpp
+++ staging/host/libs/vm_manager/crosvm_manager.cpp
@@ -414,7 +414,7 @@ std::vector<Command> CrosvmManager::StartCommands(
       << VmManager::kMaxDisks + VmManager::kDefaultNumHvcs << " devices";
 
   if (config.enable_audio()) {
-    crosvm_cmd.AddParameter("--ac97=backend=vios,server=" +
+    crosvm_cmd.AddParameter("--sound=",
                             config.ForDefaultInstance().audio_server_path());
   }
 
