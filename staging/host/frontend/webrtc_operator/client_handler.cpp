40e2209e592305d4806fb88a360601693109d94a
Treehugger Robot <treehugger-gerrit@google.com>
2021-09-01 05:47:25 +0000

    [skip ci] Merge "Add a webrtc signaling flow based on http polling" am: 61c1204fa5 am: 8a967f27dc am: 3e8ba8a5c9 am: dec37d07c0 am: 228599d957
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/1811217
    
    Change-Id: I80cffd3f366e177b7d2aeec0719f24b89f328443
    GitOrigin-RevId: 963a76c2ca003e8e5b6c76e454fb7151a0d3a817

--- staging/host/frontend/webrtc_operator/client_handler.cpp
+++ staging/host/frontend/webrtc_operator/client_handler.cpp
@@ -15,6 +15,9 @@
 
 #include "host/frontend/webrtc_operator/client_handler.h"
 
+#include <algorithm>
+#include <random>
+
 #include <android-base/logging.h>
 
 #include "host/frontend/webrtc_operator/constants/signaling_constants.h"
@@ -22,27 +25,42 @@
 
 namespace cuttlefish {
 
-ClientHandler::ClientHandler(struct lws* wsi, DeviceRegistry* registry,
+namespace {
+std::string RandomClientSecret(size_t len) {
+  static constexpr auto chars =
+      "0123456789"
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      "abcdefghijklmnopqrstuvwxyz";
+  std::string ret(len, '\0');
+  std::default_random_engine e{std::random_device{}()};
+  std::uniform_int_distribution<int> random{
+      0, static_cast<int>(std::strlen(chars)) - 1};
+  std::generate_n(ret.begin(), len, [&]() { return chars[random(e)]; });
+  return ret;
+}
+}
+
+ClientWSHandler::ClientWSHandler(struct lws* wsi, DeviceRegistry* registry,
                              const ServerConfig& server_config)
     : SignalHandler(wsi, registry, server_config),
       device_handler_(),
       client_id_(0) {}
 
-void ClientHandler::OnClosed() {
+void ClientWSHandler::OnClosed() {
   auto device_handler = device_handler_.lock();
   if (device_handler) {
     device_handler->SendClientDisconnectMessage(client_id_);
   }
 }
 
-void ClientHandler::SendDeviceMessage(const Json::Value& device_message) {
+void ClientWSHandler::SendDeviceMessage(const Json::Value& device_message) {
   Json::Value message;
   message[webrtc_signaling::kTypeField] = webrtc_signaling::kDeviceMessageType;
   message[webrtc_signaling::kPayloadField] = device_message;
   Reply(message);
 }
 
-void ClientHandler::handleMessage(const std::string& type,
+void ClientWSHandler::handleMessage(const std::string& type,
                                   const Json::Value& message) {
   if (type == webrtc_signaling::kConnectType) {
     handleConnectionRequest(message);
@@ -53,7 +71,7 @@ void ClientHandler::handleMessage(const std::string& type,
   }
 }
 
-void ClientHandler::handleConnectionRequest(const Json::Value& message) {
+void ClientWSHandler::handleConnectionRequest(const Json::Value& message) {
   if (client_id_ > 0) {
     LogAndReplyError(
         "Attempt to connect to multiple devices over same websocket");
@@ -90,7 +108,7 @@ void ClientHandler::handleConnectionRequest(const Json::Value& message) {
   Reply(device_info_reply);
 }
 
-void ClientHandler::handleForward(const Json::Value& message) {
+void ClientWSHandler::handleForward(const Json::Value& message) {
   if (client_id_ == 0) {
     LogAndReplyError("Forward failed: No device asociated to client");
     Close();
@@ -112,14 +130,232 @@ void ClientHandler::handleForward(const Json::Value& message) {
                                     message[webrtc_signaling::kPayloadField]);
 }
 
-ClientHandlerFactory::ClientHandlerFactory(DeviceRegistry* registry,
+ClientWSHandlerFactory::ClientWSHandlerFactory(DeviceRegistry* registry,
                                            const ServerConfig& server_config)
   : registry_(registry),
     server_config_(server_config) {}
 
-std::shared_ptr<WebSocketHandler> ClientHandlerFactory::Build(struct lws* wsi) {
+std::shared_ptr<WebSocketHandler> ClientWSHandlerFactory::Build(struct lws* wsi) {
   return std::shared_ptr<WebSocketHandler>(
-      new ClientHandler(wsi, registry_, server_config_));
+      new ClientWSHandler(wsi, registry_, server_config_));
+}
+
+/******************************************************************************/
+
+class PollConnectionHandler : public ClientHandler {
+ public:
+  PollConnectionHandler() = default;
+
+  void SendDeviceMessage(const Json::Value& message) override {
+    constexpr size_t kMaxMessagesInQueue = 1000;
+    if (messages_.size() > kMaxMessagesInQueue) {
+      LOG(ERROR) << "Polling client " << client_id_ << " reached "
+                 << kMaxMessagesInQueue
+                 << " messages queued. Started to drop messages.";
+      return;
+    }
+    messages_.push_back(message);
+  }
+
+  std::vector<Json::Value> PollMessages() {
+    std::vector<Json::Value> ret;
+    std::swap(ret, messages_);
+    return ret;
+  }
+
+  void SetDeviceHandler(std::weak_ptr<DeviceHandler> device_handler) {
+    device_handler_ = device_handler;
+  }
+
+  void SetClientId(size_t client_id) { client_id_ = client_id; }
+
+  size_t client_id() const { return client_id_; }
+  std::shared_ptr<DeviceHandler> device_handler() const {
+    return device_handler_.lock();
+  }
+
+ private:
+  size_t client_id_ = 0;
+  std::weak_ptr<DeviceHandler> device_handler_;
+  std::vector<Json::Value> messages_;
+};
+
+std::shared_ptr<PollConnectionHandler> PollConnectionStore::Get(
+    const std::string& conn_id) const {
+  if (!handlers_.count(conn_id)) {
+    return nullptr;
+  }
+  return handlers_.at(conn_id);
+}
+
+std::string PollConnectionStore::Add(std::shared_ptr<PollConnectionHandler> handler) {
+  std::string conn_id;
+  do {
+    conn_id = RandomClientSecret(64);
+  } while (handlers_.count(conn_id));
+  handlers_[conn_id] = handler;
+  return conn_id;
+}
+
+ClientDynHandler::ClientDynHandler(struct lws* wsi,
+                                   PollConnectionStore* poll_store)
+    : DynHandler(wsi), poll_store_(poll_store) {}
+
+HttpStatusCode ClientDynHandler::DoGet() {
+  // No message from the client uses the GET method because all of them
+  // change the server state somehow
+  return HttpStatusCode::MethodNotAllowed;
+}
+
+void ClientDynHandler::Reply(const Json::Value& json) {
+  Json::StreamWriterBuilder factory;
+  auto replyAsString = Json::writeString(factory, json);
+  AppendDataOut(replyAsString);
+}
+
+void ClientDynHandler::ReplyError(const std::string& message) {
+  LOG(ERROR) << message;
+  Json::Value reply;
+  reply["type"] = "error";
+  reply["error"] = message;
+  Reply(reply);
+}
+
+HttpStatusCode ClientDynHandler::DoPost() {
+  auto& data = GetDataIn();
+  Json::Value json_message;
+  std::shared_ptr<PollConnectionHandler> poll_handler;
+  if (data.size() > 0) {
+    Json::CharReaderBuilder builder;
+    std::unique_ptr<Json::CharReader> json_reader(builder.newCharReader());
+    std::string error_message;
+    if (!json_reader->parse(data.c_str(), data.c_str() + data.size(), &json_message,
+                            &error_message)) {
+      ReplyError("Error parsing JSON: " + error_message);
+      // Rate limiting would be a good idea here
+      return HttpStatusCode::BadRequest;
+    }
+
+    std::string conn_id;
+    if (json_message.isMember(webrtc_signaling::kClientSecretField)) {
+      conn_id =
+          json_message[webrtc_signaling::kClientSecretField].asString();
+      poll_handler = poll_store_->Get(conn_id);
+      if (!poll_handler) {
+        ReplyError("Error: Unknown connection id" + conn_id);
+        return HttpStatusCode::Unauthorized;
+      }
+    }
+  }
+  return DoPostInner(poll_handler, json_message);
+}
+
+HttpStatusCode ClientDynHandler::Poll(
+    std::shared_ptr<PollConnectionHandler> poll_handler) {
+  if (!poll_handler) {
+    ReplyError("Poll failed: No device associated to client");
+    return HttpStatusCode::Unauthorized;
+  }
+  auto messages = poll_handler->PollMessages();
+  Json::Value reply(Json::arrayValue);
+  for (auto& msg : messages) {
+    reply.append(msg);
+  }
+  Reply(reply);
+  return HttpStatusCode::Ok;
+}
+
+ConnectHandler::ConnectHandler(struct lws* wsi, DeviceRegistry* registry,
+                               PollConnectionStore* poll_store)
+    : ClientDynHandler(wsi, poll_store), registry_(registry) {}
+
+HttpStatusCode ConnectHandler::DoPostInner(
+    std::shared_ptr<PollConnectionHandler> poll_handler,
+    const Json::Value& message) {
+  if (!message.isMember(webrtc_signaling::kDeviceIdField) ||
+      !message[webrtc_signaling::kDeviceIdField].isString()) {
+    ReplyError("Invalid connection request: Missing device id");
+    return HttpStatusCode::BadRequest;
+  }
+  auto device_id = message[webrtc_signaling::kDeviceIdField].asString();
+
+  auto device_handler = registry_->GetDevice(device_id);
+  if (!device_handler) {
+    ReplyError("Connection failed: Device not found: '" + device_id + "'");
+    return HttpStatusCode::NotFound;
+  }
+
+  poll_handler = std::make_shared<PollConnectionHandler>();
+  poll_handler->SetClientId(device_handler->RegisterClient(poll_handler));
+  poll_handler->SetDeviceHandler(device_handler);
+  auto conn_id = poll_store_->Add(poll_handler);
+
+  Json::Value device_info_reply;
+  device_info_reply[webrtc_signaling::kClientSecretField] = conn_id;
+  device_info_reply[webrtc_signaling::kTypeField] =
+      webrtc_signaling::kDeviceInfoType;
+  device_info_reply[webrtc_signaling::kDeviceInfoField] =
+      device_handler->device_info();
+  Reply(device_info_reply);
+
+  return HttpStatusCode::Ok;
+}
+
+ForwardHandler::ForwardHandler(struct lws* wsi,
+                               PollConnectionStore* poll_store)
+    : ClientDynHandler(wsi, poll_store) {}
+
+HttpStatusCode ForwardHandler::DoPostInner(
+    std::shared_ptr<PollConnectionHandler> poll_handler,
+    const Json::Value& message) {
+  if (!poll_handler) {
+    ReplyError("Forward failed: No device associated to client");
+    return HttpStatusCode::Unauthorized;
+  }
+  auto client_id = poll_handler->client_id();
+  if (client_id == 0) {
+    ReplyError("Forward failed: No device associated to client");
+    return HttpStatusCode::Unauthorized;
+  }
+  if (!message.isMember(webrtc_signaling::kPayloadField)) {
+    ReplyError("Forward failed: No payload present in message");
+    return HttpStatusCode::BadRequest;
+  }
+  auto device_handler = poll_handler->device_handler();
+  if (!device_handler) {
+    ReplyError("Forward failed: Device disconnected");
+    return HttpStatusCode::NotFound;
+  }
+  device_handler->SendClientMessage(client_id,
+                                    message[webrtc_signaling::kPayloadField]);
+  // Don't waste an HTTP session returning nothing, send any pending device
+  // messages to the client instead.
+  return Poll(poll_handler);
+}
+
+PollHandler::PollHandler(struct lws* wsi, PollConnectionStore* poll_store)
+    : ClientDynHandler(wsi, poll_store) {}
+
+HttpStatusCode PollHandler::DoPostInner(
+    std::shared_ptr<PollConnectionHandler> poll_handler,
+    const Json::Value& /*message*/) {
+  return Poll(poll_handler);
+}
+
+ConfigHandler::ConfigHandler(struct lws* wsi, const ServerConfig& server_config)
+    : DynHandler(wsi), server_config_(server_config) {}
+
+HttpStatusCode ConfigHandler::DoGet() {
+  Json::Value reply = server_config_.ToJson();
+  reply[webrtc_signaling::kTypeField] = webrtc_signaling::kConfigType;
+  Json::StreamWriterBuilder factory;
+  auto replyAsString = Json::writeString(factory, reply);
+  AppendDataOut(replyAsString);
+  return HttpStatusCode::Ok;
+}
+
+HttpStatusCode ConfigHandler::DoPost() {
+  return HttpStatusCode::MethodNotAllowed;
 }
 
 }  // namespace cuttlefish
