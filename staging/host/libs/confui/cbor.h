ead49dd7701e8a90f20520b1124fda9d70b76487
Treehugger Robot <treehugger-gerrit@google.com>
2021-09-11 14:58:17 +0000

    Merge "User confirmation is signed by the host service" am: 35203d9cf1 am: 567b02cae7 am: 8328af78df am: 97f1d37de8 am: 47fcdbf0eb
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/1819357
    
    Change-Id: Ib5c0983700f7f47d4451bff79c90cdaf33034393
    GitOrigin-RevId: 046938eb05fa686c1a9a91e2aa0b0b3d73728d98

---
+++ staging/host/libs/confui/cbor.h
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2021, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <openssl/hmac.h>
+#include <openssl/sha.h>
+
+#include <array>
+#include <cstdint>
+#include <optional>
+#include <string>
+#include <vector>
+
+#include <android/hardware/keymaster/4.0/types.h>
+
+#include "host/libs/confui/cbor_utils.h"
+
+namespace cuttlefish {
+namespace confui {
+
+/** take prompt_text_, extra_data
+ * returns "message"
+ * using "message", communicating with host keymaster,
+ * returns signed confirmation when appropriate
+ */
+class Cbor {
+  // TODO(kwstephenkim): replace this using cbor libraries under external
+  enum class Error : uint32_t {
+    OK = 0,
+    OUT_OF_DATA = 1,
+    MALFORMED = 2,
+    MALFORMED_UTF8 = 3,
+  };
+
+  enum class MessageSize : uint32_t { MAX = 6144u };
+
+  enum class Type : uint8_t {
+    NUMBER = 0,
+    NEGATIVE = 1,
+    BYTE_STRING = 2,
+    TEXT_STRING = 3,
+    ARRAY = 4,
+    MAP = 5,
+    TAG = 6,
+    FLOAT = 7,
+  };
+
+ public:
+  using HardwareAuthToken =
+      android::hardware::keymaster::V4_0::HardwareAuthToken;
+  Cbor(const std::string& prompt_text,
+       const std::vector<std::uint8_t>& extra_data)
+      : prompt_text_(prompt_text),
+        extra_data_(extra_data),
+        buffer_status_{Error::OK} {
+    Init();
+  }
+
+  bool IsOk() const { return buffer_status_ == Error::OK; }
+  Error GetErrorCode() const { return buffer_status_; }
+  bool IsMessageTooLong() const { return buffer_status_ == Error::OUT_OF_DATA; }
+  bool IsMalformedUtf8() const {
+    return buffer_status_ == Error::MALFORMED_UTF8;
+  }
+  std::vector<std::uint8_t>&& GetMessage() {
+    return std::move(formatted_message_buffer_);
+  }
+  const std::uint32_t kMax = static_cast<std::uint32_t>(MessageSize::MAX);
+
+ private:
+  std::string prompt_text_;
+  std::vector<std::uint8_t> extra_data_;
+
+  // should be shorter than or equal to buf[std::uint32_t(MessageSize::MAX)]
+  std::vector<std::uint8_t> formatted_message_buffer_;
+  Error buffer_status_;
+  void Init();
+  /**
+   * formatted_message_buffer_.emplace_back(created_header)
+   */
+  bool WriteHeader(const Cbor::Type type, const std::uint64_t value);
+  bool WriteTextToBuffer(const std::string& text);
+  bool WriteBytesToBuffer(const std::vector<std::uint8_t>& bytes);
+
+  inline uint8_t getByte(const uint64_t& v, const uint8_t index) {
+    return v >> (index * 8);
+  }
+  bool WriteBytes(uint64_t value, uint8_t size);
+  Error CheckUTF8Copy(const std::string& text);
+};
+
+// TODO(kwstephenkim): replace this with signing using secure env host services
+std::optional<std::vector<std::uint8_t>> sign(
+    const std::vector<std::uint8_t>& message);
+
+}  // namespace confui
+}  // end of namespace cuttlefish
