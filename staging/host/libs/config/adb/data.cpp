b89d4e0ecb160ee3d625b2119537752456c37b1a
Treehugger Robot <treehugger-gerrit@google.com>
2021-09-15 23:33:32 +0000

    [skip ci] Merge changes Ic7f2d33e,I3b3db0ee,I033a96ff,Ieef2a9d2 am: 3268503fbd am: 6ef1b1941b am: 4b7740a6f0 am: f470ae74b9 am: 6881f26def
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/1781549
    
    Change-Id: I121e1b0b55c279bdf46dd8329a4534b407c85aa4
    GitOrigin-RevId: 27335943b9150197a2d5fbf42ffe937286d5a7f3

---
+++ staging/host/libs/config/adb/data.cpp
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "host/libs/config/adb/adb.h"
+
+#include <fruit/fruit.h>
+#include <set>
+
+namespace cuttlefish {
+namespace {}
+
+class AdbConfigImpl : public AdbConfig {
+ public:
+  INJECT(AdbConfigImpl()) {}
+
+  const std::set<AdbMode>& Modes() const override { return modes_; }
+  bool SetModes(const std::set<AdbMode>& modes) override {
+    modes_ = modes;
+    return true;
+  }
+  bool SetModes(std::set<AdbMode>&& modes) override {
+    modes_ = std::move(modes);
+    return true;
+  }
+
+  bool RunConnector() const override { return run_connector_; }
+  bool SetRunConnector(bool run) override {
+    run_connector_ = run;
+    return true;
+  }
+
+ private:
+  std::set<AdbMode> modes_;
+  bool run_connector_;
+};
+
+fruit::Component<AdbConfig> AdbConfigComponent() {
+  return fruit::createComponent().bind<AdbConfig, AdbConfigImpl>();
+}
+
+}  // namespace cuttlefish
