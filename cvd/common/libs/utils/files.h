19052ef87b3af875de9e6768f2af4309eb68b7f8
A. Cody Schuffelen <schuffelen@google.com>
2021-09-22 06:12:41 +0000

    [automerger skipped] Copy assemble_cvd.log into launcher.log am: 94b29a57c7 am: 049b03744e -s ours
    am skip reason: Merged-In Ic8945ec6a7064f59900ff811147a6a8090356442 with SHA-1 6093ecb2ce is already in history
    
    Original change: https://googleplex-android-review.googlesource.com/c/device/google/cuttlefish/+/15855666
    
    Change-Id: I830ed1cd492453b5c1b504fdeff30a73f58e6f34

--- cvd/common/libs/utils/files.h
+++ cvd/common/libs/utils/files.h
@@ -20,15 +20,19 @@
 #include <chrono>
 #include <string>
 
-namespace cvd {
+namespace cuttlefish {
 bool FileExists(const std::string& path);
 bool FileHasContent(const std::string& path);
+std::vector<std::string> DirectoryContents(const std::string& path);
 bool DirectoryExists(const std::string& path);
+bool IsDirectoryEmpty(const std::string& path);
 off_t FileSize(const std::string& path);
 bool RemoveFile(const std::string& file);
 bool RenameFile(const std::string& old_name, const std::string& new_name);
 std::string ReadFile(const std::string& file);
 std::chrono::system_clock::time_point FileModificationTime(const std::string& path);
+std::string cpp_dirname(const std::string& str);
+std::string cpp_basename(const std::string& str);
 
 // The returned value may contain .. or . if these are present in the path
 // argument.
@@ -36,4 +40,10 @@ std::chrono::system_clock::time_point FileModificationTime(const std::string& pa
 std::string AbsolutePath(const std::string& path);
 
 std::string CurrentDirectory();
-}  // namespace cvd
+
+struct FileSizes {
+  off_t sparse_size;
+  off_t disk_size;
+};
+FileSizes SparseFileSizes(const std::string& path);
+}  // namespace cuttlefish
