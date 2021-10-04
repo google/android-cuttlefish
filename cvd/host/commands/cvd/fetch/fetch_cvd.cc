88f279c1b103c47c06fdce3da77c9d7030ffbc3f
Alistair Delva <adelva@google.com>
2021-10-04 22:40:19 +0000

    [automerger skipped] DO NOT MERGE Switch arm64 to an uncompressed kernel am: 91b0c19655 am: 0ee7d4c9cc -s ours am: 2ac69e8e51 -s ours am: b5d268ad72 -s ours
    am skip reason: subject contains skip directive
    
    Original change: https://googleplex-android-review.googlesource.com/c/device/google/cuttlefish/+/15954246
    
    Change-Id: I00cb6723dd45ee179c09e55b10aa344fac6c95cf

--- cvd/host/commands/cvd/fetch/fetch_cvd.cc
+++ cvd/host/commands/cvd/fetch/fetch_cvd.cc
@@ -17,6 +17,7 @@
 #include <iterator>
 #include <string>
 
+#include <curl/curl.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
@@ -78,9 +79,9 @@ std::string TargetBuildZipFromArtifacts(
     const std::vector<Artifact>& artifacts) {
   std::string product = std::visit([](auto&& arg) { return arg.product; }, build);
   auto id = std::visit([](auto&& arg) { return arg.id; }, build);
-  auto match = product + "-" + name + "-" + id;
+  auto match = product + "-" + name + "-" + id + ".zip";
   for (const auto& artifact : artifacts) {
-    if (artifact.Name().find(match) != std::string::npos) {
+    if (artifact.Name() == match) {
       return artifact.Name();
     }
   }
@@ -268,13 +269,16 @@ int FetchCvdMain(int argc, char** argv) {
 
   curl_global_init(CURL_GLOBAL_DEFAULT);
   {
+    auto curl = CurlWrapper::Create();
+    auto retrying_curl = CurlWrapper::WithServerErrorRetry(
+        *curl, 10, std::chrono::milliseconds(5000));
     std::unique_ptr<CredentialSource> credential_source;
     if (FLAGS_credential_source == "gce") {
-      credential_source = GceMetadataCredentialSource::make();
+      credential_source = GceMetadataCredentialSource::make(*retrying_curl);
     } else if (FLAGS_credential_source != "") {
       credential_source = FixedCredentialSource::make(FLAGS_credential_source);
     }
-    BuildApi build_api(std::move(credential_source));
+    BuildApi build_api(*retrying_curl, credential_source.get());
 
     auto default_build = ArgumentToBuild(&build_api, FLAGS_default_build,
                                          DEFAULT_BUILD_TARGET,
