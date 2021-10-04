c2d2e8d5544e257b1d487404b5235a2e74c102fe
Alistair Delva <adelva@google.com>
2021-10-04 22:19:02 +0000

    [automerger skipped] DO NOT MERGE Switch arm64 to an uncompressed kernel am: 91b0c19655 am: 0ee7d4c9cc -s ours
    am skip reason: subject contains skip directive
    
    Original change: https://googleplex-android-review.googlesource.com/c/device/google/cuttlefish/+/15954246
    
    Change-Id: I1c1e9472f22c1041cc0ce4096511fc645d04d649

--- cvd/host/libs/config/known_paths.h
+++ cvd/host/libs/config/known_paths.h
@@ -30,11 +30,8 @@ std::string ModemSimulatorBinary();
 std::string RootCanalBinary();
 std::string SocketVsockProxyBinary();
 std::string TombstoneReceiverBinary();
-std::string VehicleHalGrpcServerBinary();
-std::string VncServerBinary();
 std::string WebRtcBinary();
 std::string WebRtcSigServerBinary();
-std::string WmediumdBinary();
-std::string WmediumdGenConfigBinary();
+std::string VncServerBinary();
 
 } // namespace cuttlefish
