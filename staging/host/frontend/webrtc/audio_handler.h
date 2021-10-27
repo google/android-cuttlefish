43e0ceabbf5a79b73cffc2e3fca62073bb32ff06
Treehugger Robot <treehugger-gerrit@google.com>
2021-10-27 19:31:26 +0000

    [skip ci] Merge "Revert "Add buffering for audio capture"" am: 115513c47b am: a1106e3159 am: 6b8df3f3e4 am: 12bdef86a7 am: 99dd9134eb
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/1871241
    
    Change-Id: I4f5c1c747120d843a94ac5fd159b7ab0245d97be
    GitOrigin-RevId: 1e335cd6cd9794bd2d0737410e6be1270a705948

--- staging/host/frontend/webrtc/audio_handler.h
+++ staging/host/frontend/webrtc/audio_handler.h
@@ -35,12 +35,9 @@ class AudioHandler : public AudioServerExecutor {
 
     void Reset(size_t size);
     size_t Add(const volatile uint8_t* data, size_t max_len);
-    size_t Take(uint8_t* dst, size_t len);
     bool empty() const;
     bool full() const;
-    size_t freeCapacity() const;
     uint8_t* data();
-    uint8_t* end();
   };
   struct StreamDesc {
     std::mutex mtx;
