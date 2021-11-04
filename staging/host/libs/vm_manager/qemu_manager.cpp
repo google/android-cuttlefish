85290d990f0fefb90543e2aaf9d0744e911bf526
Treehugger Robot <treehugger-gerrit@google.com>
2021-11-04 04:07:29 +0000

    [skip ci] Merge changes from topic "cf-arm-kernel" am: 0b0368cbf8 am: 280af9a56f am: ae622c27d2 am: 278dc836d2 am: cb5a305473
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/1875118
    
    Change-Id: If45e678841959f28cdc69e53361e6e3671e5d6fe
    GitOrigin-RevId: 9416a07a4cdbc11219b79491cd781fff05a2b264

--- staging/host/libs/vm_manager/qemu_manager.cpp
+++ staging/host/libs/vm_manager/qemu_manager.cpp
@@ -232,6 +232,7 @@ std::vector<Command> QemuManager::StartCommands(
   };
 
   bool is_arm = arch_ == Arch::Arm || arch_ == Arch::Arm64;
+  bool is_arm64 = arch_ == Arch::Arm64;
 
   auto access_kregistry_size_bytes = 0;
   if (FileExists(instance.access_kregistry_path())) {
@@ -253,8 +254,22 @@ std::vector<Command> QemuManager::StartCommands(
   qemu_cmd.AddParameter("guest=", instance.instance_name(), ",debug-threads=on");
 
   qemu_cmd.AddParameter("-machine");
-  auto machine = is_arm ? "virt,gic-version=2,mte=on"
-                        : "pc-i440fx-2.8,accel=kvm,nvdimm=on";
+  std::string machine = is_arm ? "virt" : "pc-i440fx-2.8,nvdimm=on";
+  if (IsHostCompatible(arch_)) {
+    machine += ",accel=kvm";
+    if (is_arm) {
+      machine += ",gic-version=3";
+    }
+  } else if (is_arm) {
+    // QEMU doesn't support GICv3 with TCG yet
+    machine += ",gic-version=2";
+    if (is_arm64) {
+      // Only enable MTE in TCG mode. We haven't started to run on ARMv8/ARMv9
+      // devices with KVM and MTE, so MTE will always require TCG
+      machine += ",mte=on";
+    }
+    CHECK(config.cpus() <= 8) << "CPUs must be no more than 8 with GICv2";
+  }
   qemu_cmd.AddParameter(machine, ",usb=off,dump-guest-core=off");
 
   qemu_cmd.AddParameter("-m");
