7e5a3ec0414da8ac9a2921979cc7f08dbd29ee65
Jiyong Park <jiyong@google.com>
2021-12-14 22:33:38 +0900

    Add OR_RETURN and OR_FATAL macros
    OR_RETURN is a macro that, evaluates a given statement, and then either
    yields a success value or returns from the surrounding function with an
    error value. It is generic so that it accepts any type of statement and
    can be used in functions of any return type.
    
    OR_FATAL is similar to OR_RETURN, but aborts when the statement yields
    an error value.
    
    For example:
    
    \#include <android-base/result.h>
    
    Result<T, E> inner();
    
    Result<U, E> outer() {
      // if inner() is a success, the success value is set to t.
      // if inner() is a failure, the error of type ResultError<E> is
      // returned to the caller.
      T t = OR_RETURN(inner());
      ...
    }
    
    E anotherOuter() {
      // The same macro works in a diffrent function returning E
      // if inner() is a failure, the error of type E is returned to the
      // caller.
      T t = OR_RETURN(inner());
      ...
    }
    
    Internally, it relies on a template heler class OkOrFail<T>.
    For a statement whose type is T, the specialization of the class for T
    has to be provided. The specialization is responsible for providing
    functions that the OR_RETURN macro uses. e.g. the member function
    bool is_ok(const T&) provides a way to test if T is a success of not.
    
    In this CL, the specialization for T = Result<V, E> is provided.
    
    Bug: 209929099
    Test: atest libbase_test
    Change-Id: I66c95e2ebcd063577c81f0facbfe1cec60818a21

--- cvd/android-base/errors.h
+++ cvd/android-base/errors.h
@@ -29,6 +29,8 @@
 
 #pragma once
 
+#include <assert.h>
+
 #include <string>
 
 namespace android {
@@ -38,6 +40,102 @@ namespace base {
 // be errno on Unix or GetLastError()/WSAGetLastError() on Windows. Passing
 // errno on Windows has undefined behavior.
 std::string SystemErrorCodeToString(int error_code);
+
+}  // namespace base
+}  // namespace android
+
+// Convenient macros for evaluating a statement, checking if the result is error, and returning it
+// to the caller.
+//
+// Usage with Result<T>:
+//
+// Result<Foo> getFoo() {...}
+//
+// Result<Bar> getBar() {
+//   Foo foo = OR_RETURN(getFoo());
+//   return Bar{foo};
+// }
+//
+// Usage with status_t:
+//
+// status_t getFoo(Foo*) {...}
+//
+// status_t getBar(Bar* bar) {
+//   Foo foo;
+//   OR_RETURN(getFoo(&foo));
+//   *bar = Bar{foo};
+//   return OK;
+// }
+//
+// Actually this can be used for any type as long as the OkOrFail<T> contract is satisfied. See
+// below.
+#define OR_RETURN(expr)                                                                 \
+  ({                                                                                    \
+    decltype(expr)&& tmp = (expr);                                                      \
+    typedef android::base::OkOrFail<std::remove_reference_t<decltype(tmp)>> ok_or_fail; \
+    if (!ok_or_fail::IsOk(tmp)) {                                                       \
+      return ok_or_fail::Fail(std::move(tmp));                                          \
+    }                                                                                   \
+    ok_or_fail::Unwrap(std::move(tmp));                                                 \
+  })
+
+// Same as OR_RETURN, but aborts if expr is a failure.
+#if defined(__BIONIC__)
+#define OR_FATAL(expr)                                                                  \
+  ({                                                                                    \
+    decltype(expr)&& tmp = (expr);                                                      \
+    typedef android::base::OkOrFail<std::remove_reference_t<decltype(tmp)>> ok_or_fail; \
+    if (!ok_or_fail::IsOk(tmp)) {                                                       \
+      __assert(__FILE__, __LINE__, ok_or_fail::ErrorMessage(tmp).c_str());              \
+    }                                                                                   \
+    ok_or_fail::Unwrap(std::move(tmp));                                                 \
+  })
+#else
+#define OR_FATAL(expr)                                                                  \
+  ({                                                                                    \
+    decltype(expr)&& tmp = (expr);                                                      \
+    typedef android::base::OkOrFail<std::remove_reference_t<decltype(tmp)>> ok_or_fail; \
+    if (!ok_or_fail::IsOk(tmp)) {                                                       \
+      fprintf(stderr, "%s:%d: assertion \"%s\" failed", __FILE__, __LINE__,             \
+              ok_or_fail::ErrorMessage(tmp).c_str());                                   \
+      abort();                                                                          \
+    }                                                                                   \
+    ok_or_fail::Unwrap(std::move(tmp));                                                 \
+  })
+#endif
+
+namespace android {
+namespace base {
+
+// The OkOrFail contract for a type T. This must be implemented for a type T if you want to use
+// OR_RETURN(stmt) where stmt evalues to a value of type T.
+template <typename T>
+struct OkOrFail {
+  // Checks if T is ok or fail.
+  static bool IsOk(const T&);
+
+  // Turns T into the success value.
+  template <typename U>
+  static U Unwrap(T&&);
+
+  // Moves T into OkOrFail<T>, so that we can convert it to other types
+  OkOrFail(T&& v);
+  OkOrFail() = delete;
+  OkOrFail(const T&) = delete;
+
+  // And there need to be one or more conversion operators that turns the error value of T into a
+  // target type. For example, for T = Result<V, E>, there can be ...
+  //
+  // // for the case where OR_RETURN is called in a function expecting E
+  // operator E()&& { return val_.error().code(); }
+  //
+  // // for the case where OR_RETURN is called in a function expecting Result<U, E>
+  // template <typename U>
+  // operator Result<U, E>()&& { return val_.error(); }
+
+  // Returns the string representation of the fail value.
+  static std::string ErrorMessage(const T& v);
+};
 
 }  // namespace base
 }  // namespace android
