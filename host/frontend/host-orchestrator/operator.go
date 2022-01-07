861eb4048e9a228bb10cd11f8498d024ae4c338b
Jorge E. Moreira <5149948+jemoreira@users.noreply.github.com>
2022-01-06 19:05:18 -0500

    Build host-orchestrator with cuttlefish-common (#104)
    * Minor QoL improvements
    
    * Add the host-orchestrator binary to .gitignore
    * Log when the server is ready to receive connections
    * Add a build script
    
    * Fix permissions on host-orchestrator socket
    
    User and group have full access while other has no access.
    
    * Release the host-orchestrator in cuttlefish-common
    
    It doesn't start automatically yet
    
    * Install golang-1.17 before building cuttlefish-common
    
    * Implement force-reload of the host orchestrator

--- host/frontend/host-orchestrator/operator.go
+++ host/frontend/host-orchestrator/operator.go
@@ -48,6 +48,7 @@ func main() {
 	r := setupServerRoutes(pool, polledSet, config)
 
 	http.Handle("/", r)
+	log.Println("Client endpoint created")
 	if err := http.ListenAndServe(fmt.Sprint(":", port), nil); err != nil {
 		log.Fatal("ListenAndServe client: ", err)
 	}
@@ -66,6 +67,12 @@ func setupDeviceEndpoint(pool *DevicePool, config InfraConfig, path string) {
 	if err != nil {
 		log.Fatal("Failed to create unix socket: ", err)
 	}
+	// Make sure the socket is only accessible by owner and group
+	if err := os.Chmod(path, 0770); err != nil {
+		// This shouldn't happen since the creation of the socket just succeeded
+		log.Println("Failed to change permissions on socket file: ", err)
+	}
+	log.Println("Device endpoint created")
 	// Serve the register_device endpoint in a background thread
 	go func() {
 		defer sock.Close()
