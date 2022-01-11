64a003956d82f2c591138d29e2851ef1b9838e8d
A. Cody Schuffelen <schuffelen@google.com>
2022-01-11 02:16:03 -0800

    Implement cvd_test_gce_driver to create and destroy an instance.
    This puts the pieces together for interacting with the GCE API and running
    through the compute engine instance lifecycle.
    
    The next step here is to make the binary interactive to support going
    back and forth with a test runner to evaluate commands and give back
    results.
    
    Bug: 210741384
    Test: m -j
    Test: cvd_test_gce_driver --instance-name=schuffelen-test --service-account-json-private-key-path=...
    Change-Id: I323b334b122c610568263d1752c99f3f1c91c659

--- host/commands/test_gce_driver/cvd_test_gce_driver.cpp
+++ host/commands/test_gce_driver/cvd_test_gce_driver.cpp
@@ -13,4 +13,86 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-int main() {}
+#include <curl/curl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <string>
+
+#include <android-base/logging.h>
+#include <android-base/result.h>
+#include <android-base/strings.h>
+
+#include "common/libs/fs/shared_buf.h"
+#include "common/libs/fs/shared_fd.h"
+#include "common/libs/utils/archive.h"
+#include "common/libs/utils/files.h"
+#include "common/libs/utils/flag_parser.h"
+#include "common/libs/utils/subprocess.h"
+#include "host/commands/test_gce_driver/gce_api.h"
+#include "host/commands/test_gce_driver/key_pair.h"
+#include "host/commands/test_gce_driver/scoped_instance.h"
+#include "host/libs/web/build_api.h"
+#include "host/libs/web/credential_source.h"
+#include "host/libs/web/curl_wrapper.h"
+#include "host/libs/web/install_zip.h"
+
+using android::base::Error;
+using android::base::Result;
+
+namespace cuttlefish {
+namespace {
+
+android::base::Result<Json::Value> ReadJsonFromFile(const std::string& path) {
+  Json::CharReaderBuilder builder;
+  std::ifstream ifs(path);
+  Json::Value content;
+  std::string errorMessage;
+  if (!Json::parseFromStream(builder, ifs, &content, &errorMessage)) {
+    return android::base::Error()
+           << "Could not read config file \"" << path << "\": " << errorMessage;
+  }
+  return content;
+}
+
+}  // namespace
+
+int TestGceDriverMain(int argc, char** argv) {
+  std::vector<Flag> flags;
+  std::string service_account_json_private_key_path = "";
+  flags.emplace_back(GflagsCompatFlag("service-account-json-private-key-path",
+                                      service_account_json_private_key_path));
+  std::string instance_name = "";
+  flags.emplace_back(GflagsCompatFlag("instance-name", instance_name));
+
+  std::vector<std::string> args =
+      ArgsToVec(argc - 1, argv + 1);  // Skip argv[0]
+  CHECK(ParseFlags(flags, args)) << "Could not process command line flags.";
+
+  auto service_json = ReadJsonFromFile(service_account_json_private_key_path);
+  CHECK(service_json.ok()) << service_json.error();
+
+  static constexpr char COMPUTE_SCOPE[] =
+      "https://www.googleapis.com/auth/compute";
+  auto curl = CurlWrapper::Create();
+  auto credential_source = ServiceAccountOauthCredentialSource::FromJson(
+      *curl, *service_json, COMPUTE_SCOPE);
+  CHECK(credential_source);
+
+  // TODO(b/216667647): Allow these settings to be configured.
+  GceApi gce(*curl, *credential_source, "cloud-android-testing", "us-west1-a");
+
+  auto instance = ScopedGceInstance::CreateDefault(gce, instance_name);
+  CHECK(instance.ok()) << "Failed to create GCE instance: " << instance.error();
+
+  return 0;
+}
+
+}  // namespace cuttlefish
+
+int main(int argc, char** argv) {
+  return cuttlefish::TestGceDriverMain(argc, argv);
+}
