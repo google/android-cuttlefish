ef23ca515a046137d9847f22f327208782c68053
Treehugger Robot <treehugger-gerrit@google.com>
2022-01-28 03:26:02 +0000

    Merge "Remove vestiges of Linux tpm support" am: d35fd67bf1 am: 46b3c4bff8 am: d657212dfa am: 0b83a36720
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/1960034
    
    Change-Id: Id74c2d73c8062499034fece2b2c069ec680379f5
    GitOrigin-RevId: 337d081c0ddbe044502af2a87073b2bf3c07439a

--- staging/host/libs/config/cuttlefish_config.h
+++ staging/host/libs/config/cuttlefish_config.h
@@ -150,9 +150,6 @@ class CuttlefishConfig {
   void set_crosvm_binary(const std::string& crosvm_binary);
   std::string crosvm_binary() const;
 
-  void set_tpm_device(const std::string& tpm_device);
-  std::string tpm_device() const;
-
   void set_enable_sandbox(const bool enable_sandbox);
   bool enable_sandbox() const;
 
