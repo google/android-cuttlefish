a0e06d94374de6352919acc0dbedb544709e3c6b
Treehugger Robot <treehugger-gerrit@google.com>
2022-01-28 00:12:43 +0000

    [skip ci] Merge "Confirmation UI host should use cn-cbor" am: 9b4d6d6eba am: d0bf16e53b am: 232f0386af am: d2b2fcb945
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/1948836
    
    Change-Id: I68b273fdaa3d1f4f7209cf2698fa230af0902e81
    GitOrigin-RevId: c5d54943d4507f93292cf768745092b57318ef3e

--- staging/host/libs/confui/cbor.cc
+++ staging/host/libs/confui/cbor.cc
@@ -16,139 +16,47 @@
 
 #include "host/libs/confui/cbor.h"
 
-#include <algorithm>
+#include "common/libs/confui/confui.h"
 
 namespace cuttlefish {
 namespace confui {
-bool Cbor::WriteBytes(uint64_t value, uint8_t size) {
-  if (!IsOk()) {
-    return false;
-  }
-
-  if (formatted_message_buffer_.size() + size >= kMax) {
-    buffer_status_ = Cbor::Error::OUT_OF_DATA;
-    return false;
-  }
-
-  switch (size) {
-    case 8:
-      formatted_message_buffer_.emplace_back(getByte(value, 7));
-      formatted_message_buffer_.emplace_back(getByte(value, 6));
-      formatted_message_buffer_.emplace_back(getByte(value, 5));
-      formatted_message_buffer_.emplace_back(getByte(value, 4));
-      [[fallthrough]];
-    case 4:
-      formatted_message_buffer_.emplace_back(getByte(value, 3));
-      formatted_message_buffer_.emplace_back(getByte(value, 2));
-      [[fallthrough]];
-    case 2:
-      formatted_message_buffer_.emplace_back(getByte(value, 1));
-      [[fallthrough]];
-    case 1:
-      formatted_message_buffer_.emplace_back(value);
-      break;
-    case 0:
-      break;
-    default:
-      buffer_status_ = Cbor::Error::MALFORMED;
-      return false;
-  }
-  return true;
-}
-
-// TODO(kwstephenkim): replace these using cbor libraries under external
-bool Cbor::WriteHeader(const Cbor::Type type, const std::uint64_t value) {
-  /**
-   * reference implementation is, as of 08/30/2021, here:
-   *
-   * https://cs.android.com/android/platform/superproject/+/master:hardware/interfaces/confirmationui/support/src/cbor.cpp;drc=master;bpv=0;bpt=1;l=58
-   *
-   */
-  if (!IsOk()) {
-    return false;
-  }
-
-  enum class Condition { k24, k0x100, k0x10000, k0x100000000, kBeyond };
-  std::map<Condition, const std::uint8_t> bits_to_set = {
-      {Condition::k24, static_cast<uint8_t>(value)},
-      {Condition::k0x100, 24},
-      {Condition::k0x10000, 25},
-      {Condition::k0x100000000, 26},
-      {Condition::kBeyond, 27}};
-  std::map<Condition, int> optional_byte_size = {{Condition::k24, 0},
-                                                 {Condition::k0x100, 1},
-                                                 {Condition::k0x10000, 2},
-                                                 {Condition::k0x100000000, 4},
-                                                 {Condition::kBeyond, 8}};
-  Condition cond = Condition::k24;
-
-  if (value < 24) {
-    cond = Condition::k24;
-  } else if (value < 0x100) {
-    cond = Condition::k0x100;
-  } else if (value < 0x10000) {
-    cond = Condition::k0x10000;
-  } else if (value < 0x100000000) {
-    cond = Condition::k0x100000000;
-  } else {
-    cond = Condition::kBeyond;
-  }
-
-  // see if we have rooms. 1 byte for the mandatory byte in the header
-  // n bytes for optional bytes in the header
-  if (formatted_message_buffer_.size() + optional_byte_size[cond] + 1 > kMax) {
-    buffer_status_ = Cbor::Error::OUT_OF_DATA;
-    return false;
-  }
-
-  std::uint8_t header = (static_cast<uint8_t>(type) << 5) | bits_to_set[cond];
-  formatted_message_buffer_.emplace_back(header);
-  return WriteBytes(value, optional_byte_size[cond]);
-}
-
+/**
+ * basically, this creates a map as follows:
+ * {"prompt" : prompt_text_in_UTF8,
+ *  "extra"  : extra_data_in_bytes}
+ */
 void Cbor::Init() {
-  // reference implementation was available in:
-  //  1.0/generic/GenericOperation.h
-  // See class template Operation, and its init()
+  cn_cbor_errback err;
+  cb_map_ = std::unique_ptr<cn_cbor, CborDeleter>(cn_cbor_map_create(&err));
 
-  /*
-   * formatted_message_buffer_ will be:
-   *   header followed by std::string("prompt") followed by prompt_text_ (no
-   * null char) followed by std::string("extra") followed by extra_data_ (no
-   * ending null, surely)
-   *
-   */
-  if (!WriteHeader(Type::MAP, 2)) {
+  buffer_status_ = CheckUTF8Copy(prompt_text_);
+  if (!IsOk()) {
     return;
   }
-  const std::string prompt("prompt");
-  const std::string extra("extra");
 
-  (WriteTextToBuffer(prompt) && WriteTextToBuffer(prompt_text_) &&
-   WriteTextToBuffer(extra) && WriteBytesToBuffer(extra_data_));
+  auto cb_prompt_as_value = cn_cbor_string_create(prompt_text_.data(), &err);
+  auto cb_extra_data_as_value =
+      cn_cbor_data_create(extra_data_.data(), extra_data_.size(), &err);
+  cn_cbor_mapput_string(cb_map_.get(), "prompt", cb_prompt_as_value, &err);
+  cn_cbor_mapput_string(cb_map_.get(), "extra", cb_extra_data_as_value, &err);
+
+  // cn_cbor_encoder_write wants buffer_ to have a trailing 0 at the end
+  auto n_chars =
+      cn_cbor_encoder_write(buffer_.data(), 0, buffer_.size(), cb_map_.get());
+  ConfUiLog(ERROR) << "Cn-cbor encoder wrote " << n_chars << " while "
+                   << "kMax is " << kMax;
+  if (n_chars < 0) {
+    // it's either message being too long, or a potential cn_cbor bug
+    ConfUiLog(ERROR) << "Cn-cbor returns -1 which is likely message too long.";
+    buffer_status_ = Error::OUT_OF_DATA;
+  }
+  if (!IsOk()) {
+    return;
+  }
+  buffer_.resize(n_chars);
 }
 
-bool Cbor::WriteTextToBuffer(const std::string& text) {
-  // write(WriteState, const StringBuffer<T, TextStr>() is ported here
-  if (!IsOk() || !WriteHeader(Type::TEXT_STRING, text.size())) {
-    return false;
-  }
-  buffer_status_ = CheckUTF8Copy(text);
-  return IsOk();
-}
-
-bool Cbor::WriteBytesToBuffer(const std::vector<std::uint8_t>& bytes) {
-  if (!IsOk() || !WriteHeader(Type::BYTE_STRING, bytes.size())) {
-    return false;
-  }
-  if (formatted_message_buffer_.size() + bytes.size() >= kMax) {
-    buffer_status_ = Cbor::Error::OUT_OF_DATA;
-    return false;
-  }
-  formatted_message_buffer_.insert(formatted_message_buffer_.end(),
-                                   std::begin(bytes), std::end(bytes));
-  return true;
-}
+std::vector<std::uint8_t>&& Cbor::GetMessage() { return std::move(buffer_); }
 
 Cbor::Error Cbor::CheckUTF8Copy(const std::string& text) {
   auto begin = text.cbegin();
@@ -188,16 +96,7 @@ Cbor::Error Cbor::CheckUTF8Copy(const std::string& text) {
         return Cbor::Error::MALFORMED_UTF8;
       }
     }
-
-    if (formatted_message_buffer_.size() < kMax) {
-      formatted_message_buffer_.emplace_back(
-          static_cast<const std::uint8_t>(*begin));
-      begin++;
-    } else {
-      // not returning with this error to be consistent with the reference
-      // implementation
-      err_code = Cbor::Error::OUT_OF_DATA;
-    }
+    ++begin;
   }
   // if the string ends in the middle of a multi byte char it is invalid
   if (multi_byte_length) {
@@ -205,6 +104,5 @@ Cbor::Error Cbor::CheckUTF8Copy(const std::string& text) {
   }
   return err_code;
 }
-
 }  // end of namespace confui
 }  // end of namespace cuttlefish
