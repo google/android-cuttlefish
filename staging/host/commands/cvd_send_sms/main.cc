3b3a4d2d13adac43a5723cddfc405ec4b33ecdc5
Sergio Rodriguez Orama <sorama@google.com>
2022-01-28 18:42:11 +0000

    [skip ci] Merge "Adds cvd_send_sms command." am: ec0af8973a am: ecf3cf0fb5 am: 8c544e4f69 am: 4aa38c57dc
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/1959580
    
    Change-Id: Ib50f2a57659a00d1f27f78a0eff80200eda13c3c
    GitOrigin-RevId: c6ea6c266a3dc75a8323f7029b94d2b43d7313b1

---
+++ staging/host/commands/cvd_send_sms/main.cc
@@ -0,0 +1,44 @@
+#include "gflags/gflags.h"
+
+#include "android-base/logging.h"
+#include "common/libs/fs/shared_fd.h"
+#include "host/commands/cvd_send_sms/sms_sender.h"
+
+DEFINE_uint32(instance_number, 1,
+              "number of the cvd instance to send the sms to, default is 1");
+DEFINE_uint32(modem_id, 0,
+              "modem id needed for multisim devices, default is 0");
+
+namespace cuttlefish {
+namespace {
+
+// Usage examples:
+//   * cvd_send_sms "hello world"
+//   * cvd_send_sms --instance-number=2 "hello world"
+//   * cvd_send_sms --instance-number=2 --modem_id=1 "hello world"
+
+int SendSmsMain(int argc, char** argv) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  if (argc == 1) {
+    LOG(ERROR) << "Missing message content. First positional argument is used "
+                  "as the message content, `cvd_send_sms --instance-number=2 "
+                  "\"hello world\"`";
+    return -1;
+  }
+  // Builds the name of the corresponding modem simulator monitor socket.
+  // https://cs.android.com/android/platform/superproject/+/master:device/google/cuttlefish/host/commands/modem_simulator/main.cpp;l=115;drc=cbfe7dba44bfea95049152b828c1a5d35c9e0522
+  std::string socket_name = std::string("modem_simulator") +
+                            std::to_string(1000 + FLAGS_instance_number);
+  auto client_socket = cuttlefish::SharedFD::SocketLocalClient(
+      socket_name.c_str(), /* abstract */ true, SOCK_STREAM);
+  SmsSender sms_sender(client_socket);
+  if (!sms_sender.Send(argv[1], FLAGS_modem_id)) {
+    return -1;
+  }
+  return 0;
+}
+
+}  // namespace
+}  // namespace cuttlefish
+
+int main(int argc, char** argv) { return cuttlefish::SendSmsMain(argc, argv); }
