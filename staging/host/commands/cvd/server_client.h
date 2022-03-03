4e3bd009fbd74505d14b8cb45302fe7785d99b56
Treehugger Robot <treehugger-gerrit@google.com>
2022-03-03 03:37:20 +0000

    [skip ci] Merge changes I518770f5,I3667cc7e,Ib747c2e9,I9dafa8a8,I9cecbb3a, ... am: 3b376b0079 am: 01f2716bbb am: df95902e58
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2000354
    
    Change-Id: Iffe7632778fcc2fca5df213a0cc2af7d3f991d5f
    GitOrigin-RevId: 7f2a09449bb4ddf3990ebc765ffe179d520f87e5

---
+++ staging/host/commands/cvd/server_client.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <memory>
+
+#include "cvd_server.pb.h"
+
+#include "common/libs/fs/shared_fd.h"
+#include "common/libs/utils/result.h"
+
+namespace cuttlefish {
+
+struct RequestWithStdio {
+  cvd::Request request;
+  SharedFD in, out, err;
+  std::optional<SharedFD> extra;
+};
+
+class ClientMessageQueue {
+ public:
+  static Result<ClientMessageQueue> Create(SharedFD client);
+  ClientMessageQueue(ClientMessageQueue&&);
+  ~ClientMessageQueue();
+  ClientMessageQueue& operator=(ClientMessageQueue&&);
+
+  Result<RequestWithStdio> WaitForRequest();
+  Result<void> PostResponse(const cvd::Response& response);
+  Result<void> Stop();
+  void Join();
+
+ private:
+  class Internal;
+
+  std::unique_ptr<Internal> internal_;
+
+  ClientMessageQueue(std::unique_ptr<Internal>);
+};
+
+}  // namespace cuttlefish
