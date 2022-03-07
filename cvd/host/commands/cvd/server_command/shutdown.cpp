d10d1e92e9eded9b521520c81cc8e2076283d649
Presubmit Automerger Backend <android-build-presubmit-automerger-backend@system.gserviceaccount.com>
2022-03-07 20:15:47 +0000

    [automerger skipped] [automerged blank] Allow downstream devices to build sparse filesystem images 2p: 5a7e1bea06 am: a085a4c5f1 -s ours
    am skip reason: Merged-In I55ac39c448c7e36b647ef4e3c607d95df5e6c04e with SHA-1 376bcea11c is already in history
    
    Original change: https://googleplex-android-review.googlesource.com/c/device/google/cuttlefish/+/17056634
    
    Change-Id: I318bc762c0a131239290b202469b64d4f7881296

--- cvd/host/commands/cvd/server_command/shutdown.cpp
+++ cvd/host/commands/cvd/server_command/shutdown.cpp
@@ -32,7 +32,7 @@ class CvdShutdownHandler : public CvdServerHandler {
   INJECT(CvdShutdownHandler(CvdServer& server)) : server_(server) {}
 
   Result<bool> CanHandle(const RequestWithStdio& request) const override {
-    return request.request.contents_case() ==
+    return request.Message().contents_case() ==
            cvd::Request::ContentsCase::kShutdownRequest;
   }
 
@@ -41,16 +41,19 @@ class CvdShutdownHandler : public CvdServerHandler {
     cvd::Response response;
     response.mutable_shutdown_response();
 
-    if (!request.extra) {
+    if (!request.Extra()) {
       response.mutable_status()->set_code(cvd::Status::FAILED_PRECONDITION);
       response.mutable_status()->set_message(
           "Missing extra SharedFD for shutdown");
       return response;
     }
 
-    if (request.request.shutdown_request().clear()) {
-      *response.mutable_status() = server_.CvdClear(request.out, request.err);
-      return response;
+    if (request.Message().shutdown_request().clear()) {
+      *response.mutable_status() =
+          server_.CvdClear(request.Out(), request.Err());
+      if (response.status().code() != cvd::Status::OK) {
+        return response;
+      }
     }
 
     if (server_.HasAssemblies()) {
@@ -63,14 +66,16 @@ class CvdShutdownHandler : public CvdServerHandler {
 
     // Intentionally leak the write_pipe fd so that it only closes
     // when this process fully exits.
-    (*request.extra)->UNMANAGED_Dup();
+    (*request.Extra())->UNMANAGED_Dup();
 
-    WriteAll(request.out, "Stopping the cvd_server.\n");
+    WriteAll(request.Out(), "Stopping the cvd_server.\n");
     server_.Stop();
 
     response.mutable_status()->set_code(cvd::Status::OK);
     return response;
   }
+
+  Result<void> Interrupt() override { return CF_ERR("Can't interrupt"); }
 
  private:
   CvdServer& server_;
@@ -78,7 +83,7 @@ class CvdShutdownHandler : public CvdServerHandler {
 
 }  // namespace
 
-fruit::Component<> cvdShutdownComponent() {
+fruit::Component<fruit::Required<CvdServer>> cvdShutdownComponent() {
   return fruit::createComponent()
       .addMultibinding<CvdServerHandler, CvdShutdownHandler>();
 }
