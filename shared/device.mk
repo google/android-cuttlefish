e0eb327da3511487409ef4103ed58e22f9de43e3
Ram Muthiah <rammuthiah@google.com>
2022-03-07 14:27:49 +0000

    Revert "Use FUSE BPF by default"
    This reverts commit 95df011731148cec5a24fbace613a3912e28008c.
    
    Reason for revert: Needed to switch to 5.15. Feature isn't enabled on 5.15. Removing this property on internal and setting it to false from aosp.
    
    Change-Id: Ic1c444a2ce7437d623401d9f664ae68b1f37a167
    Merged-In: Ic1c444a2ce7437d623401d9f664ae68b1f37a167
    (cherry picked from commit 47ad4952970bc0b15e137e99f19c2228979ca3ea)

--- shared/device.mk
+++ shared/device.mk
@@ -95,7 +95,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.adb.tcp.port=5555 \
     ro.com.google.locationfeatures=1 \
     persist.sys.fuse.passthrough.enable=true \
-    persist.sys.fuse.bpf.enable=true \
 
 # Until we support adb keys on user builds, and fix logcat over serial,
 # spawn adbd by default without authorization for "adb logcat"
