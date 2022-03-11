ed83f66bd0c491e5e5111222aaa45ebb302f2cb4
Treehugger Robot <treehugger-gerrit@google.com>
2022-03-11 00:38:01 +0000

    [skip ci] Merge "Power CvdServer with a concurrent epoll loop." am: 3b2c2c6895 am: 7dd721ddd0 am: a787295cc8
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2016216
    
    Change-Id: Ia479b6d8cc23c0356fda8ef93563d9bce2b8dfa7
    GitOrigin-RevId: 24c2f665d6073abb817a10694815e5f2b2fc0145

--- staging/host/commands/cvd/server.cc
+++ staging/host/commands/cvd/server.cc
@@ -39,6 +39,7 @@
 #include "common/libs/utils/result.h"
 #include "common/libs/utils/shared_fd_flag.h"
 #include "common/libs/utils/subprocess.h"
+#include "host/commands/cvd/epoll_loop.h"
 #include "host/commands/cvd/server_constants.h"
 #include "host/libs/config/cuttlefish_config.h"
 #include "host/libs/config/known_paths.h"
@@ -55,10 +56,55 @@ static fruit::Component<> RequestComponent(CvdServer* server,
       .install(cvdVersionComponent);
 }
 
-CvdServer::CvdServer(InstanceManager& instance_manager)
-    : instance_manager_(instance_manager), running_(true) {}
+static constexpr int kNumThreads = 10;
+
+CvdServer::CvdServer(EpollPool& epoll_pool, InstanceManager& instance_manager)
+    : epoll_pool_(epoll_pool),
+      instance_manager_(instance_manager),
+      running_(true) {
+  for (auto i = 0; i < kNumThreads; i++) {
+    threads_.emplace_back([this]() {
+      while (running_) {
+        auto result = epoll_pool_.HandleEvent();
+        if (!result.ok()) {
+          LOG(ERROR) << "Epoll worker error:\n" << result.error();
+        }
+      }
+      auto wakeup = BestEffortWakeup();
+      CHECK(wakeup.ok()) << wakeup.error().message();
+    });
+  }
+}
+
+CvdServer::~CvdServer() {
+  running_ = false;
+  auto wakeup = BestEffortWakeup();
+  CHECK(wakeup.ok()) << wakeup.error().message();
+  Join();
+}
+
+Result<void> CvdServer::BestEffortWakeup() {
+  // This attempts to cascade through the responder threads, forcing them
+  // to wake up and see that running_ is false, then exit and wake up
+  // further threads.
+  auto eventfd = SharedFD::Event();
+  CF_EXPECT(eventfd->IsOpen(), eventfd->StrError());
+  CF_EXPECT(eventfd->EventfdWrite(1) == 0, eventfd->StrError());
+
+  auto cb = [](EpollEvent) -> Result<void> { return {}; };
+  CF_EXPECT(epoll_pool_.Register(eventfd, EPOLLIN, cb));
+  return {};
+}
 
 void CvdServer::Stop() { running_ = false; }
+
+void CvdServer::Join() {
+  for (auto& thread : threads_) {
+    if (thread.joinable()) {
+      thread.join();
+    }
+  }
+}
 
 static Result<CvdServerHandler*> RequestHandler(
     const RequestWithStdio& request,
@@ -76,87 +122,109 @@ static Result<CvdServerHandler*> RequestHandler(
   return compatible_handlers[0];
 }
 
-Result<void> CvdServer::ServerLoop(SharedFD server) {
-  while (running_) {
-    auto client_fd = SharedFD::Accept(*server);
-    CF_EXPECT(client_fd->IsOpen(), client_fd->StrError());
-
-    auto message_queue = CF_EXPECT(ClientMessageQueue::Create(client_fd));
-
-    std::atomic_bool running = true;
-    std::mutex handler_mutex;
-    CvdServerHandler* handler = nullptr;
-    std::thread message_responder([this, &message_queue, &handler,
-                                   &handler_mutex, &running]() {
-      while (running) {
-        auto request = message_queue.WaitForRequest();
-        if (!request.ok()) {
-          LOG(DEBUG) << "Didn't get client request:"
-                     << request.error().message();
-          break;
-        }
-        fruit::Injector<> request_injector(RequestComponent, this,
-                                           &instance_manager_);
-        Result<cvd::Response> response;
-        {
-          std::scoped_lock lock(handler_mutex);
-          auto handler_result = RequestHandler(
-              *request, request_injector.getMultibindings<CvdServerHandler>());
-          if (handler_result.ok()) {
-            handler = *handler_result;
-          } else {
-            handler = nullptr;
-            response = cvd::Response();
-            response->mutable_status()->set_code(cvd::Status::INTERNAL);
-            response->mutable_status()->set_message("No handler found");
-          }
-          // Drop the handler lock so it has a chance to be interrupted.
-        }
-        if (handler) {
-          response = handler->Handle(*request);
-        }
-        {
-          std::scoped_lock lock(handler_mutex);
-          handler = nullptr;
-        }
-        if (!response.ok()) {
-          LOG(DEBUG) << "Error handling request: " << request.error().message();
-          cvd::Response error_response;
-          error_response.mutable_status()->set_code(cvd::Status::INTERNAL);
-          error_response.mutable_status()->set_message(
-              response.error().message());
-          response = error_response;
-        }
-        auto write_response = message_queue.PostResponse(*response);
-        if (!write_response.ok()) {
-          LOG(DEBUG) << "Error writing response: " << write_response.error();
-          break;
-        }
-      }
-    });
-
-    message_queue.Join();
-    // The client has gone away, do our best to interrupt/stop ongoing
-    // operations.
-    running = false;
-    {
-      // This might end up executing after the handler is completed, but it at
-      // least won't race with the handler being overwritten by another pointer.
-      std::scoped_lock lock(handler_mutex);
-      if (handler) {
-        auto interrupt = handler->Interrupt();
-        if (!interrupt.ok()) {
-          LOG(ERROR) << "Failed to interrupt handler: " << interrupt.error();
-        }
-      }
+class ScopeGuard {
+ public:
+  ScopeGuard(std::function<void()> fn) : fn_(fn) {}
+  ~ScopeGuard() {
+    if (fn_) {
+      fn_();
     }
-    message_responder.join();
   }
+  void Cancel() { fn_ = nullptr; }
+
+ private:
+  std::function<void()> fn_;
+};
+
+Result<void> CvdServer::StartServer(SharedFD server_fd) {
+  auto cb = [this](EpollEvent ev) -> Result<void> {
+    CF_EXPECT(AcceptClient(ev));
+    return {};
+  };
+  CF_EXPECT(epoll_pool_.Register(server_fd, EPOLLIN, cb));
+  return {};
+}
+
+Result<void> CvdServer::AcceptClient(EpollEvent event) {
+  ScopeGuard stop_on_failure([this] { Stop(); });
+
+  CF_EXPECT(event.events & EPOLLIN);
+  auto client_fd = SharedFD::Accept(*event.fd);
+  CF_EXPECT(client_fd->IsOpen(), client_fd->StrError());
+  auto client_cb = [this](EpollEvent ev) -> Result<void> {
+    CF_EXPECT(HandleMessage(ev));
+    return {};
+  };
+  CF_EXPECT(epoll_pool_.Register(client_fd, EPOLLIN, client_cb));
+
+  auto self_cb = [this](EpollEvent ev) -> Result<void> {
+    CF_EXPECT(AcceptClient(ev));
+    return {};
+  };
+  CF_EXPECT(epoll_pool_.Register(event.fd, EPOLLIN, self_cb));
+
+  stop_on_failure.Cancel();
+  return {};
+}
+
+Result<void> CvdServer::HandleMessage(EpollEvent event) {
+  ScopeGuard abandon_client([this, event] { epoll_pool_.Remove(event.fd); });
+
+  if (event.events & EPOLLHUP) {  // Client went away.
+    epoll_pool_.Remove(event.fd);
+    return {};
+  }
+
+  CF_EXPECT(event.events & EPOLLIN);
+  auto request = CF_EXPECT(GetRequest(event.fd));
+  if (!request) {  // End-of-file / client went away.
+    epoll_pool_.Remove(event.fd);
+    return {};
+  }
+
+  fruit::Injector<> injector(RequestComponent, this, &instance_manager_);
+  auto possible_handlers = injector.getMultibindings<CvdServerHandler>();
+
+  // Even if the interrupt callback outlives the request handler, it'll only
+  // hold on to this struct which will be cleaned out when the request handler
+  // exits.
+  struct SharedState {
+    CvdServerHandler* handler;
+    std::mutex mutex;
+  };
+  auto shared = std::make_shared<SharedState>();
+  shared->handler = CF_EXPECT(RequestHandler(*request, possible_handlers));
+
+  auto interrupt_cb = [shared](EpollEvent) -> Result<void> {
+    std::lock_guard lock(shared->mutex);
+    CF_EXPECT(shared->handler != nullptr);
+    CF_EXPECT(shared->handler->Interrupt());
+    return {};
+  };
+  CF_EXPECT(epoll_pool_.Register(event.fd, EPOLLHUP, interrupt_cb));
+
+  auto response = CF_EXPECT(shared->handler->Handle(*request));
+  CF_EXPECT(SendResponse(event.fd, response));
+
+  {
+    std::lock_guard lock(shared->mutex);
+    shared->handler = nullptr;
+  }
+  CF_EXPECT(epoll_pool_.Remove(event.fd));  // Delete interrupt handler
+
+  auto self_cb = [this](EpollEvent ev) -> Result<void> {
+    CF_EXPECT(HandleMessage(ev));
+    return {};
+  };
+  CF_EXPECT(epoll_pool_.Register(event.fd, EPOLLIN, self_cb));
+
+  abandon_client.Cancel();
   return {};
 }
 
 static fruit::Component<CvdServer> ServerComponent() {
-  return fruit::createComponent();
+  return fruit::createComponent()
+      .install(EpollLoopComponent);
 }
 
 static Result<int> CvdServerMain(int argc, char** argv) {
@@ -178,7 +246,9 @@ static Result<int> CvdServerMain(int argc, char** argv) {
   CF_EXPECT(server_fd->IsOpen(), "Did not receive a valid cvd_server fd");
 
   fruit::Injector<CvdServer> injector(ServerComponent);
-  CF_EXPECT(injector.get<CvdServer&>().ServerLoop(server_fd));
+  CvdServer& server = injector.get<CvdServer&>();
+  server.StartServer(server_fd);
+  server.Join();
 
   return 0;
 }
