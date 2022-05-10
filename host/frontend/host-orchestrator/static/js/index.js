d7557e315826a2a9ac3ba7e7733721f179745735
Jorge E. Moreira <jemoreira@google.com>
2022-05-09 18:49:31 -0700

    View devices without opening new tab

--- host/frontend/host-orchestrator/static/js/index.js
+++ host/frontend/host-orchestrator/static/js/index.js
@@ -18,11 +18,9 @@
 
 class DeviceListApp {
   #url;
-  #selectDeviceCb;
 
-  constructor({url, selectDeviceCb}) {
+  constructor({url}) {
     this.#url = url;
-    this.#selectDeviceCb = selectDeviceCb;
   }
 
   start() {
@@ -51,54 +49,94 @@ class DeviceListApp {
     let ul = document.getElementById('device-list');
     ul.innerHTML = '';
     let count = 1;
-    let device_to_button_map = {};
     for (const devId of device_ids) {
-      const buttonId = 'connect_' + count++;
-      let entry = this.#createDeviceEntry(devId, buttonId);
+      const showId = 'connect_' + count++;
+      const launchId = 'connect_' + count++;
+      let entry = this.#createDeviceEntry(devId, showId, launchId);
       ul.appendChild(entry);
-      device_to_button_map[devId] = buttonId;
-    }
-
-    for (const [devId, buttonId] of Object.entries(device_to_button_map)) {
-      let button = document.getElementById(buttonId);
-      button.addEventListener('click', evt => {
-        this.#selectDeviceCb(devId);
+      let launchBtn = document.getElementById(launchId);
+      launchBtn.addEventListener('click', evt => {
+        this.#openDeviceInNewTab(devId);
+      });
+      let showBtn = document.getElementById(showId);
+      showBtn.addEventListener('click', evt => {
+        let shown = this.#toggleDevice(devId);
+        if (shown) {
+          showBtn.textContent = "visibility";
+          showBtn.title = 'Hide device';
+        } else {
+          showBtn.textContent = "visibility_off";
+          showBtn.title = 'Show device';
+        }
       });
     }
   }
 
-  #createDeviceEntry(devId, buttonId) {
+  #createDeviceEntry(devId, showId, launchId) {
     let li = document.createElement('li');
     li.className = 'device-entry';
-    li.title = 'Connect to ' + devId;
     let div = document.createElement('div');
     let span = document.createElement('span');
     span.className = 'device-label';
     span.appendChild(document.createTextNode(devId));
     span.title = devId;
-    let button = document.createElement('span');
-    button.appendChild(document.createTextNode('open_in_new'));
-    button.className = 'button material-icons';
-    button.id = buttonId;
+    let showBtn = document.createElement('span');
+    showBtn.appendChild(document.createTextNode('visibility_off'));
+    showBtn.title = 'View device';
+    showBtn.className = 'button material-icons';
+    showBtn.id = showId;
+    showBtn.title = 'Show device';
+    let launchBtn = document.createElement('span');
+    launchBtn.appendChild(document.createTextNode('open_in_new'));
+    launchBtn.className = 'button material-icons';
+    launchBtn.id = launchId;
+    launchBtn.title = 'Launch in new tab';
     div.appendChild(span);
-    div.appendChild(button);
+    div.appendChild(showBtn);
+    div.appendChild(launchBtn);
     li.appendChild(div);
     return li;
+  }
+
+  #openDeviceInNewTab(deviceId) {
+    return new Promise((resolve, reject) => {
+      let client = window.open(this.#deviceConnectUrl(deviceId), deviceId);
+      client.addEventListener('load', evt => {
+        resolve();
+      });
+    });
+  }
+
+  // Returns true if the device is visible
+  #toggleDevice(devId) {
+    let id = `device-${devId}`;
+    let viewer = document.getElementById(id);
+    if (viewer) {
+      viewer.remove();
+      return false;
+    }
+    viewer = document.createElement('div');
+    viewer.id = id;
+    viewer.className = 'device-viewer';
+    let label = document.createElement('h3');
+    label.appendChild(document.createTextNode(devId));
+    viewer.appendChild(label);
+    let iframe = document.createElement('iframe');
+    iframe.src = this.#deviceConnectUrl(devId);
+    iframe.title = `Device ${devId}`
+    viewer.appendChild(iframe);
+    let devices = document.getElementById('devices');
+    devices.appendChild(viewer);
+    return true;
+  }
+
+  #deviceConnectUrl(deviceId) {
+    return `/devices/${deviceId}/files/client.html`;
   }
 }  // DeviceListApp
 
 window.addEventListener('load', e => {
   let listDevicesUrl = '/devices';
-  let selectDeviceCb = deviceId => {
-    return new Promise((resolve, reject) => {
-      let client =
-          window.open(`/devices/${deviceId}/files/client.html`, deviceId);
-      client.addEventListener('load', evt => {
-        console.log('loaded');
-        resolve();
-      });
-    });
-  };
-  let deviceListApp = new DeviceListApp({url: listDevicesUrl, selectDeviceCb});
+  let deviceListApp = new DeviceListApp({url: listDevicesUrl});
   deviceListApp.start();
 });
