e47cf176386531928ce6d13359f274243276d6ed
Treehugger Robot <treehugger-gerrit@google.com>
2022-05-26 02:41:27 +0000

    [skip ci] Merge "Move TombstoneReceiver to another file" am: 37a97e3550 am: b6c02f2a55 am: bc42aa74f7 am: dcf22fe2b0
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2086563
    
    Change-Id: Ibacad43d70375cefde5c4d7b3d4e040cfa2c2c31
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: ed95221e9dc74e7f496fbbb81d874f8424d375ec

---
+++ staging/host/commands/run_cvd/launch/tombstone_receiver.cpp
@@ -0,0 +1,71 @@
+//
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "host/commands/run_cvd/launch/launch.h"
+
+#include "common/libs/utils/files.h"
+#include "host/libs/config/known_paths.h"
+
+namespace cuttlefish {
+
+class TombstoneReceiver : public CommandSource {
+ public:
+  INJECT(TombstoneReceiver(const CuttlefishConfig::InstanceSpecific& instance))
+      : instance_(instance) {}
+
+  // CommandSource
+  std::vector<Command> Commands() override {
+    return single_element_emplace(
+        Command(TombstoneReceiverBinary())
+            .AddParameter("-server_fd=", socket_)
+            .AddParameter("-tombstone_dir=", tombstone_dir_));
+  }
+
+  // SetupFeature
+  std::string Name() const override { return "TombstoneReceiver"; }
+  bool Enabled() const override { return true; }
+
+ private:
+  std::unordered_set<SetupFeature*> Dependencies() const override { return {}; }
+  Result<void> ResultSetup() override {
+    tombstone_dir_ = instance_.PerInstancePath("tombstones");
+    if (!DirectoryExists(tombstone_dir_)) {
+      LOG(DEBUG) << "Setting up " << tombstone_dir_;
+      CF_EXPECT(mkdir(tombstone_dir_.c_str(),
+                      S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) == 0,
+                "Failed to create tombstone directory: "
+                    << tombstone_dir_ << ". Error: " << strerror(errno));
+    }
+
+    auto port = instance_.tombstone_receiver_port();
+    socket_ = SharedFD::VsockServer(port, SOCK_STREAM);
+    CF_EXPECT(socket_->IsOpen(), "Unable to create tombstone server socket: "
+                                     << socket_->StrError());
+    return {};
+  }
+
+  const CuttlefishConfig::InstanceSpecific& instance_;
+  SharedFD socket_;
+  std::string tombstone_dir_;
+};
+
+fruit::Component<fruit::Required<const CuttlefishConfig::InstanceSpecific>>
+TombstoneReceiverComponent() {
+  return fruit::createComponent()
+      .addMultibinding<CommandSource, TombstoneReceiver>()
+      .addMultibinding<SetupFeature, TombstoneReceiver>();
+}
+
+}  // namespace cuttlefish
