8e5d936a3dd8f9b59ae133fea538cfea2eb918bd
Jorge E. Moreira <jemoreira@google.com>
2022-06-02 12:52:33 -0700

    Extract common signaling server code into a library

--- host/frontend/host-orchestrator/orchestrator/instancemanager.go
+++ host/frontend/host-orchestrator/orchestrator/instancemanager.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main
+package orchestrator
 
 import (
 	"encoding/json"
@@ -24,7 +24,8 @@ import (
 	"os"
 	"sync"
 
-	apiv1 "cuttlefish/host-orchestrator/api/v1"
+	apiv1 "cuttlefish/liboperator/api/v1"
+	"cuttlefish/liboperator/operator"
 )
 
 type EmptyFieldError string
@@ -44,7 +45,7 @@ func NewInstanceManager(cvdHandler *CVDHandler, om OperationManager) *InstanceMa
 
 func (m *InstanceManager) CreateCVD(req apiv1.CreateCVDRequest) (Operation, error) {
 	if err := validateRequest(&req); err != nil {
-		return Operation{}, NewBadRequestError("invalid CreateCVDRequest", err)
+		return Operation{}, operator.NewBadRequestError("invalid CreateCVDRequest", err)
 	}
 	op := m.om.New()
 	go m.LaunchCVD(req, op)
