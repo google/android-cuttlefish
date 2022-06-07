3ddd07043f743eb3c6717d1b12e41ee62052ee32
Treehugger Robot <treehugger-gerrit@google.com>
2022-06-07 02:14:35 +0000

    [skip ci] Merge "Move RootCanal to a separate file under run_cvd/launch" am: 8c2de05fc7 am: c3ab753646 am: 52693e4751 am: 24a40acc8f
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2099971
    
    Change-Id: Iff7120dd15d9cdeeabc0b61f6bc1f07531a211b4
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 4115eedce50b997c3697894d9154acf65b043a99

---
+++ staging/host/commands/run_cvd/launch/root_canal.cpp
@@ -0,0 +1,87 @@
+//
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "host/commands/run_cvd/launch/launch.h"
+
+#include <unordered_set>
+#include <vector>
+
+#include "host/commands/run_cvd/launch/log_tee_creator.h"
+#include "host/libs/config/cuttlefish_config.h"
+#include "host/libs/config/known_paths.h"
+
+namespace cuttlefish {
+namespace {
+
+class RootCanal : public CommandSource {
+ public:
+  INJECT(RootCanal(const CuttlefishConfig& config,
+                   const CuttlefishConfig::InstanceSpecific& instance,
+                   LogTeeCreator& log_tee))
+      : config_(config), instance_(instance), log_tee_(log_tee) {}
+
+  // CommandSource
+  std::vector<Command> Commands() override {
+    if (!Enabled()) {
+      return {};
+    }
+    Command command(RootCanalBinary());
+
+    // Test port
+    command.AddParameter(config_.rootcanal_test_port());
+    // HCI server port
+    command.AddParameter(config_.rootcanal_hci_port());
+    // Link server port
+    command.AddParameter(config_.rootcanal_link_port());
+    // Bluetooth controller properties file
+    command.AddParameter("--controller_properties_file=",
+                         config_.rootcanal_config_file());
+    // Default commands file
+    command.AddParameter("--default_commands_file=",
+                         config_.rootcanal_default_commands_file());
+
+    std::vector<Command> commands;
+    commands.emplace_back(log_tee_.CreateLogTee(command, "rootcanal"));
+    commands.emplace_back(std::move(command));
+    return commands;
+  }
+
+  // SetupFeature
+  std::string Name() const override { return "RootCanal"; }
+  bool Enabled() const override {
+    return config_.enable_host_bluetooth() && instance_.start_rootcanal();
+  }
+
+ private:
+  std::unordered_set<SetupFeature*> Dependencies() const override { return {}; }
+  bool Setup() override { return true; }
+
+  const CuttlefishConfig& config_;
+  const CuttlefishConfig::InstanceSpecific& instance_;
+  LogTeeCreator& log_tee_;
+};
+
+}  // namespace
+
+fruit::Component<
+    fruit::Required<const CuttlefishConfig,
+                    const CuttlefishConfig::InstanceSpecific, LogTeeCreator>>
+RootCanalComponent() {
+  return fruit::createComponent()
+      .addMultibinding<CommandSource, RootCanal>()
+      .addMultibinding<SetupFeature, RootCanal>();
+}
+
+}  // namespace cuttlefish
