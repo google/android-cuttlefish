a56eef0a89bf5b92e3c11801042f5355f6fc7e32
Treehugger Robot <treehugger-gerrit@google.com>
2022-06-08 22:13:04 +0000

    [skip ci] Merge "Move MetricsService out of run_cvd/launch/launch.cc" am: 831417c83e am: 52b5c13195 am: ca5135c77c am: 17c3528be4
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2115559
    
    Change-Id: Ied5d671fd1008f1b23be91ef257e57cd6fb7246c
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 5e29d6126ee8ef6ca1fc8147b30f1602edd4c184

---
+++ staging/host/commands/run_cvd/launch/metrics.cpp
@@ -0,0 +1,55 @@
+//
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "host/commands/run_cvd/launch/launch.h"
+
+#include "host/libs/config/known_paths.h"
+
+namespace cuttlefish {
+namespace {
+
+class MetricsService : public CommandSource {
+ public:
+  INJECT(MetricsService(const CuttlefishConfig& config)) : config_(config) {}
+
+  // CommandSource
+  Result<std::vector<Command>> Commands() override {
+    return single_element_emplace(Command(MetricsBinary()));
+  }
+
+  // SetupFeature
+  std::string Name() const override { return "MetricsService"; }
+  bool Enabled() const override {
+    return config_.enable_metrics() == CuttlefishConfig::kYes;
+  }
+
+ private:
+  std::unordered_set<SetupFeature*> Dependencies() const override { return {}; }
+  bool Setup() override { return true; }
+
+ private:
+  const CuttlefishConfig& config_;
+};
+
+}  // namespace
+
+fruit::Component<fruit::Required<const CuttlefishConfig>>
+MetricsServiceComponent() {
+  return fruit::createComponent()
+      .addMultibinding<CommandSource, MetricsService>()
+      .addMultibinding<SetupFeature, MetricsService>();
+}
+
+}  // namespace cuttlefish
