60eaa9d0c5a17fec1a95076fd688aca89c4e669b
Mahmoud Elsherif <moelsherif@google.com>
2022-07-13 19:02:33 +0000

    [skip ci] Merge "Create GNSS client component and add it to location library" am: 0dc8198db1 am: 7f4022e29f am: 99dd39e12c am: 72d4184a13
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2133793
    
    Change-Id: Id90154ef0aed189eb2660cbad426e246a573f316
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: fc3f78716f89c5af96f71c5f5a0726d3943cf294

--- staging/host/commands/cvd_update_location/main.cc
+++ staging/host/commands/cvd_update_location/main.cc
@@ -15,93 +15,14 @@
  */
 
 #include <android-base/logging.h>
-#include <android-base/strings.h>
 #include <gflags/gflags.h>
-
-#include <common/libs/fs/shared_buf.h>
-#include <common/libs/fs/shared_fd.h>
-#include <common/libs/fs/shared_select.h>
-#include <host/libs/config/logging.h>
 #include "host/libs/config/cuttlefish_config.h"
-
-#include <grpc/grpc.h>
-#include <grpcpp/channel.h>
-#include <grpcpp/client_context.h>
-#include <grpcpp/create_channel.h>
-#include "common/libs/utils/environment.h"
-#include "gnss_grpc_proxy.grpc.pb.h"
-#include "host/commands/run_cvd/runner_defs.h"
-
-using gnss_grpc_proxy::GnssGrpcProxy;
-using gnss_grpc_proxy::SendGpsReply;
-using gnss_grpc_proxy::SendGpsRequest;
-using grpc::Channel;
-using grpc::ClientContext;
-using grpc::Status;
+#include "host/libs/location/GnssClient.h"
 
 DEFINE_int32(instance_num, 1, "Which instance to read the configs from");
 DEFINE_string(latitude, "37.8000064", "location latitude");
 DEFINE_string(longitude, "-122.3989209", "location longitude");
 DEFINE_string(elevation, "2.5", "location elevation/altitude");
-
-class GnssClient {
- public:
-  GnssClient(std::shared_ptr<Channel> channel)
-      : stub_(GnssGrpcProxy::NewStub(channel)) {}
-  // Assambles the client's payload, sends it and presents the response back
-  // from the server.
-  std::string SendGps(const std::string& user) {
-    // Data we are sending to the server.
-    SendGpsRequest request;
-    request.set_gps(user);
-    // Container for the data we expect from the server.
-    SendGpsReply reply;
-    // Context for the client. It could be used to convey extra information to
-    // the server and/or tweak certain RPC behaviors.
-    ClientContext context;
-    // The actual RPC.
-    Status status = stub_->SendGps(&context, request, &reply);
-    // Act upon its status.
-    if (status.ok()) {
-      LOG(INFO) << "GPS data sending successful " << std::endl;
-      return reply.reply();
-    } else {
-      LOG(INFO) << "GPS data sending failed" << status.error_code() << ": "
-                << status.error_message() << std::endl;
-      return "RPC failed";
-    }
-  }
-  /*
-    Fix,GPS,      37.8000064,     -122.3989209,   -42.139252, 0.000000,3.790092,
-    0.000000,     1622580024000,  0.086023256,    0.0, 11529389988248"
-    Fix,Provider,
-    LatitudeDegrees,LongitudeDegrees,AltitudeMeters,SpeedMps,AccuracyMeters,BearingDegrees,UnixTimeMillis,SpeedAccuracyMps,BearingAccuracyDegrees,elapsedRealtimeNanos
-  */
-  std::string FormatGps(const std::string& latitude,
-                        const std::string& longitude,
-                        const std::string& elevation,
-                        const std::string& timestamp, bool inject_time) {
-    std::string unix_time_millis;
-    if (inject_time) {
-      unix_time_millis = std::to_string(
-          std::chrono::duration_cast<std::chrono::milliseconds>(
-              std::chrono::system_clock::now().time_since_epoch())
-              .count());
-    } else {
-      unix_time_millis = timestamp;
-    }
-    std::string formatted_location =
-        std::string("Fix,GPS,") + latitude + "," + longitude + "," + elevation +
-        "," + std::string("0.000000,3.790092,0.000000,") + unix_time_millis +
-        "," + std::string("0.086023256,0.0,11529389988248");
-    LOG(INFO) << "Location: " << formatted_location << std::endl;
-
-    return formatted_location;
-  }
-
- private:
-  std::unique_ptr<GnssGrpcProxy::Stub> stub_;
-};
 
 namespace cuttlefish {
 namespace {
@@ -120,17 +41,19 @@ int UpdateLocationCvdMain(int argc, char** argv) {
   auto server_port = instance.gnss_grpc_proxy_server_port();
   std::string socket_name =
       std::string("localhost:") + std::to_string(server_port);
+  LOG(INFO) << "Server port: " << server_port << " socket: " << socket_name
+            << std::endl;
+
   GnssClient gpsclient(
       grpc::CreateChannel(socket_name, grpc::InsecureChannelCredentials()));
   std::string timestamp = " ";
   std::string formatted_location = gpsclient.FormatGps(
       FLAGS_latitude, FLAGS_longitude, FLAGS_elevation, timestamp, true);
-  std::string reply = gpsclient.SendGps(formatted_location);
-
-  LOG(INFO) << "Server port: " << server_port << " socket: " << socket_name
-            << std::endl;
-  LOG(INFO) << "GnssGrpcProxy received: " << reply << std::endl;
-  LOG(INFO) << "UpdateLocationCvdMain successful";
+  auto status = gpsclient.SendGps(formatted_location);
+  CHECK(status.ok()) << "Failed to send gps location data \n";
+  if (!status.ok()) {
+    return 1;
+  }
   return 0;
 }
 
