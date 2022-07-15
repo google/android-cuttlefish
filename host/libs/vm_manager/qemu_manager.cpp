92434b861723efcaedb4e90c38da88467afb423f
Alistair Delva <adelva@google.com>
2022-07-14 20:28:03 -0700

    Switch to ranchu hwc
    Bug: 216827224
    Change-Id: I4162cb5c4c168e3fb7d734da1f21016ecc723f0d

--- host/libs/vm_manager/qemu_manager.cpp
+++ host/libs/vm_manager/qemu_manager.cpp
@@ -143,7 +143,8 @@ std::vector<std::string> QemuManager::ConfigureGraphics(
     return {
       "androidboot.cpuvulkan.version=0",
       "androidboot.hardware.gralloc=minigbm",
-      "androidboot.hardware.hwcomposer=drm",
+      "androidboot.hardware.hwcomposer=ranchu",
+      "androidboot.hardware.hwcomposer.mode=client",
       "androidboot.hardware.egl=mesa",
       // No "hardware" Vulkan support, yet
       "androidboot.opengles.version=196608"};  // OpenGL ES 3.0
