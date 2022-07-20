55dfb4d29c4db02b2304506a55634ffc9899df1f
Tristan Muntsinger <muntsinger@google.com>
2022-07-20 22:17:13 +0000

    Add metrics receiver server for host messages
    This server will receive metrics messages from
    the host-side using the predefined msg_queue
    for further processing.
    
    Bug: 230492130
    Test: local build and boot complete
    Signed-off-by: Tristan Muntsinger <muntsinger@google.com>
    Signed-off-by: Alessio Balsini <balsini@google.com>
    Change-Id: I618addcfec3ccad2a0a02d362f550a4dd5477b47

--- host/commands/metrics/metrics.cc
+++ host/commands/metrics/metrics.cc
@@ -13,27 +13,45 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <android-base/strings.h>
-#include <gflags/gflags.h>
 #include <android-base/logging.h>
+#include <android-base/strings.h>
+#include <curl/curl.h>
+#include <gflags/gflags.h>
+#include <json/json.h>
+#include <net/if.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/time.h>
+#include <sys/utsname.h>
+#include <chrono>
+#include <ctime>
+#include <iostream>
 
 #include "common/libs/utils/tee_logging.h"
+#include "host/commands/metrics/host_receiver.h"
 #include "host/commands/metrics/metrics_defs.h"
+#include "host/commands/metrics/proto/cf_metrics_proto.h"
 #include "host/libs/config/cuttlefish_config.h"
+#include "host/libs/msg_queue/msg_queue.h"
+#include "host/libs/vm_manager/crosvm_manager.h"
+#include "host/libs/vm_manager/qemu_manager.h"
 
 using cuttlefish::MetricsExitCodes;
+using std::cout;
+using std::endl;
+using std::chrono::duration_cast;
+using std::chrono::milliseconds;
+using std::chrono::seconds;
+using std::chrono::system_clock;
 
 int main(int argc, char** argv) {
-  ::android::base::InitLogging(argv, android::base::StderrLogger);
   google::ParseCommandLineFlags(&argc, &argv, true);
-
+  ::android::base::InitLogging(argv, android::base::StderrLogger);
   auto config = cuttlefish::CuttlefishConfig::Get();
-
   CHECK(config) << "Could not open cuttlefish config";
-
   auto instance = config->ForDefaultInstance();
   auto metrics_log_path = instance.PerInstanceLogPath("metrics.log");
-
   if (config->run_as_daemon()) {
     android::base::SetLogger(
         cuttlefish::LogToFiles({metrics_log_path, instance.launcher_log_path()}));
@@ -42,15 +60,17 @@ int main(int argc, char** argv) {
         cuttlefish::LogToStderrAndFiles(
             {metrics_log_path, instance.launcher_log_path()}));
   }
-
   if (config->enable_metrics() != cuttlefish::CuttlefishConfig::kYes) {
     LOG(ERROR) << "metrics not enabled, but metrics were launched.";
     return cuttlefish::MetricsExitCodes::kInvalidHostConfiguration;
   }
 
-  while (true) {
-    // do nothing
-    sleep(std::numeric_limits<unsigned int>::max());
+  cuttlefish::MetricsHostReceiver host_receiver(*config);
+  if (!host_receiver.Initialize()) {
+    LOG(ERROR) << "metrics host_receiver failed to init";
+    return cuttlefish::MetricsExitCodes::kMetricsError;
   }
+  LOG(INFO) << "Metrics started";
+  host_receiver.Join();
   return cuttlefish::MetricsExitCodes::kMetricsError;
 }
