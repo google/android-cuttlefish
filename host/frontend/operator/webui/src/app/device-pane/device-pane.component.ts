3898a0f94777593652bbcd3e0e891254cac459d3
Sumin <suminchung@google.com>
2022-07-21 01:12:18 +0000

    Temporary Device List (+checkbox, refresh button). more like a frame
    Change-Id: Icecbb7507bd4aeff0c40b3087a15e9ae0d05de60

--- host/frontend/operator/webui/src/app/device-pane/device-pane.component.ts
+++ host/frontend/operator/webui/src/app/device-pane/device-pane.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Device } from "../device-interface";
+import { DEVICES } from "../device-mocks";
 
 @Component({
-  selector: 'app-device-pane',
-  templateUrl: './device-pane.component.html',
-  styleUrls: ['./device-pane.component.sass']
+  selector: "app-device-pane",
+  templateUrl: "./device-pane.component.html",
+  styleUrls: ["./device-pane.component.sass"],
 })
 export class DevicePaneComponent implements OnInit {
+  //This is temporary variable.
+  //It will be fixed to a function (create device list function) after connecting to services.
+  devices = DEVICES;
+  selectedDevice?: Device;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSelect(device: Device): void {
+    this.selectedDevice = device;
   }
 
+  //Temporary function. Also will be updated after connecting to service.
+  //console log to check out whether refresh button works properly
+  //Unchecking checkboxes will be implemented after update.
+  onRefresh(): void {
+    DEVICES.push({ id: "another one" });
+    this.devices = DEVICES;
+  }
 }
