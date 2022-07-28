8ed7816024d06bd5397605e5d7646efe869766b6
Andrei Onea <andreionea@google.com>
2022-07-28 15:27:45 +0000

    Allow cellbroadcastservice allowlist
    On the devices that rely on the platform variant of the
    cellbroadcastservice apk, allow its respective allowlist to be included
    in the system image.
    
    Test: m # for lunch aosp_cf_arm_only_phone-eng
    Bug: 240531574
    Change-Id: Id6524892855e43d8592c9e0d940f2f10720421b7

--- vsoc_arm_only/phone/aosp_cf.mk
+++ vsoc_arm_only/phone/aosp_cf.mk
@@ -42,6 +42,7 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/apex/com.android.tethering.inprocess.apex \
     system/apex/com.android.tethering.inprocess.capex \
     system/app/PlatformCaptivePortalLogin/PlatformCaptivePortalLogin.apk \
+    system/etc/permissions/platform_privapp_allowlist_com.android.cellbroadcastservice.xml \
     system/priv-app/CellBroadcastServiceModulePlatform/CellBroadcastServiceModulePlatform.apk \
     system/priv-app/InProcessNetworkStack/InProcessNetworkStack.apk \
 
