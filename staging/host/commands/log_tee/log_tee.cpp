f83b4abfdb5127726bd9ae740df570dad5967d4b
Treehugger Robot <treehugger-gerrit@google.com>
2022-08-02 19:32:41 +0000

    [skip ci] Merge "Cast to std::string_view for android::base::Trim" am: c1ae832540 am: 83b199e7c8 am: 9cb61e18bf am: c3f8bcf422
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2164263
    
    Change-Id: I10f730566603a9a2dbcbb5aa39d4e13b8e12ff93
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 8111688485df234bd2a5f18cc5197753a275485b

--- staging/host/commands/log_tee/log_tee.cpp
+++ staging/host/commands/log_tee/log_tee.cpp
@@ -58,7 +58,7 @@ int main(int argc, char** argv) {
   LOG(DEBUG) << "Starting to read from process " << FLAGS_process_name;
 
   while ((chars_read = log_fd->Read(buf, sizeof(buf))) > 0) {
-    auto trimmed = android::base::Trim(std::string(buf, chars_read));
+    auto trimmed = android::base::Trim(std::string_view(buf, chars_read));
     // Newlines inside `trimmed` are handled by the android logging code.
     // These checks attempt to determine the log severity coming from crosvm.
     // There is no guarantee of success all the time since log line boundaries
