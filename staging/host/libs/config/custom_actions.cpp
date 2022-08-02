29f9700ceb8469c66393e53a4a163bc565a099e4
Treehugger Robot <treehugger-gerrit@google.com>
2022-08-02 00:36:10 +0000

    [skip ci] Merge changes I60f6926d,Ibb8552b0 am: 104259fd5d am: 318dd38908 am: 45954da4d0 am: 14eef7edaa
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2168671
    
    Change-Id: I3b724694b264dac5964dd0ad8972e5e777f00c7b
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 747633306ca71be1dae014639ca2a0bf839c7b78

--- staging/host/libs/config/custom_actions.cpp
+++ staging/host/libs/config/custom_actions.cpp
@@ -42,110 +42,111 @@ const char* kCustomActionButtonCommand = "command";
 const char* kCustomActionButtonTitle = "title";
 const char* kCustomActionButtonIconName = "icon_name";
 
-std::optional<CustomActionConfig> CustomActionConfigFromJson(
+CustomShellActionConfig GetCustomShellActionConfigFromJson(
     const Json::Value& dictionary) {
-  bool has_shell_command = dictionary.isMember(kCustomActionShellCommand);
-  bool has_server = dictionary.isMember(kCustomActionServer);
-  bool has_device_states = dictionary.isMember(kCustomActionDeviceStates);
-  if (!!has_shell_command + !!has_server + !!has_device_states != 1) {
-    LOG(ERROR) << "Custom action must contain exactly one of shell_command, "
-               << "server, or device_states";
-    return {};
+  CustomShellActionConfig config;
+  // Shell command with one button.
+  Json::Value button_entry = dictionary[kCustomActionButton];
+  config.button = {button_entry[kCustomActionButtonCommand].asString(),
+    button_entry[kCustomActionButtonTitle].asString(),
+    button_entry[kCustomActionButtonIconName].asString()};
+  config.shell_command = dictionary[kCustomActionShellCommand].asString();
+  return config;
+}
+
+CustomActionServerConfig GetCustomActionServerConfigFromJson(
+    const Json::Value& dictionary) {
+  CustomActionServerConfig config;
+  // Action server with possibly multiple buttons.
+  for (const Json::Value& button_entry : dictionary[kCustomActionButtons]) {
+    config.buttons.push_back(
+        {button_entry[kCustomActionButtonCommand].asString(),
+        button_entry[kCustomActionButtonTitle].asString(),
+        button_entry[kCustomActionButtonIconName].asString()});
   }
-  CustomActionConfig config;
-  if (has_shell_command) {
-    // Shell command with one button.
-    Json::Value button_entry = dictionary[kCustomActionButton];
-    config.buttons = {{button_entry[kCustomActionButtonCommand].asString(),
-                       button_entry[kCustomActionButtonTitle].asString(),
-                       button_entry[kCustomActionButtonIconName].asString()}};
-    config.shell_command = dictionary[kCustomActionShellCommand].asString();
-  } else if (has_server) {
-    // Action server with possibly multiple buttons.
-    for (const Json::Value& button_entry : dictionary[kCustomActionButtons]) {
-      ControlPanelButton button = {
-          button_entry[kCustomActionButtonCommand].asString(),
-          button_entry[kCustomActionButtonTitle].asString(),
-          button_entry[kCustomActionButtonIconName].asString()};
-      config.buttons.push_back(button);
+  config.server = dictionary[kCustomActionServer].asString();
+  return config;
+}
+
+CustomDeviceStateActionConfig GetCustomDeviceStateActionConfigFromJson(
+    const Json::Value& dictionary) {
+  CustomDeviceStateActionConfig config;
+  // Device state(s) with one button.
+  // Each button press cycles to the next state, then repeats to the first.
+  Json::Value button_entry = dictionary[kCustomActionButton];
+  config.button = {button_entry[kCustomActionButtonCommand].asString(),
+    button_entry[kCustomActionButtonTitle].asString(),
+    button_entry[kCustomActionButtonIconName].asString()};
+  for (const Json::Value& device_state_entry :
+      dictionary[kCustomActionDeviceStates]) {
+    DeviceState state;
+    if (device_state_entry.isMember(
+          kCustomActionDeviceStateLidSwitchOpen)) {
+      state.lid_switch_open =
+        device_state_entry[kCustomActionDeviceStateLidSwitchOpen].asBool();
     }
-    config.server = dictionary[kCustomActionServer].asString();
-  } else if (has_device_states) {
-    // Device state(s) with one button.
-    // Each button press cycles to the next state, then repeats to the first.
-    Json::Value button_entry = dictionary[kCustomActionButton];
-    config.buttons = {{button_entry[kCustomActionButtonCommand].asString(),
-                       button_entry[kCustomActionButtonTitle].asString(),
-                       button_entry[kCustomActionButtonIconName].asString()}};
-    for (const Json::Value& device_state_entry :
-         dictionary[kCustomActionDeviceStates]) {
-      DeviceState state;
-      if (device_state_entry.isMember(kCustomActionDeviceStateLidSwitchOpen)) {
-        state.lid_switch_open =
-            device_state_entry[kCustomActionDeviceStateLidSwitchOpen].asBool();
-      }
-      if (device_state_entry.isMember(
-              kCustomActionDeviceStateHingeAngleValue)) {
-        state.hinge_angle_value =
-            device_state_entry[kCustomActionDeviceStateHingeAngleValue].asInt();
-      }
-      config.device_states.push_back(state);
+    if (device_state_entry.isMember(
+          kCustomActionDeviceStateHingeAngleValue)) {
+      state.hinge_angle_value =
+        device_state_entry[kCustomActionDeviceStateHingeAngleValue].asInt();
     }
-  } else {
-    LOG(ERROR) << "Unknown custom action type.";
-    return {};
+    config.device_states.push_back(state);
   }
   return config;
 }
 
-Json::Value ToJson(const CustomActionConfig& custom_action) {
+Json::Value ToJson(const CustomShellActionConfig& custom_action) {
   Json::Value json;
-  if (custom_action.shell_command) {
-    // Shell command with one button.
-    json[kCustomActionShellCommand] = *custom_action.shell_command;
-    json[kCustomActionButton] = Json::Value();
-    json[kCustomActionButton][kCustomActionButtonCommand] =
-        custom_action.buttons[0].command;
-    json[kCustomActionButton][kCustomActionButtonTitle] =
-        custom_action.buttons[0].title;
-    json[kCustomActionButton][kCustomActionButtonIconName] =
-        custom_action.buttons[0].icon_name;
-  } else if (custom_action.server) {
-    // Action server with possibly multiple buttons.
-    json[kCustomActionServer] = *custom_action.server;
-    json[kCustomActionButtons] = Json::Value(Json::arrayValue);
-    for (const auto& button : custom_action.buttons) {
-      Json::Value button_entry;
-      button_entry[kCustomActionButtonCommand] = button.command;
-      button_entry[kCustomActionButtonTitle] = button.title;
-      button_entry[kCustomActionButtonIconName] = button.icon_name;
-      json[kCustomActionButtons].append(button_entry);
-    }
-  } else if (!custom_action.device_states.empty()) {
-    // Device state(s) with one button.
-    json[kCustomActionDeviceStates] = Json::Value(Json::arrayValue);
-    for (const auto& device_state : custom_action.device_states) {
-      Json::Value device_state_entry;
-      if (device_state.lid_switch_open) {
-        device_state_entry[kCustomActionDeviceStateLidSwitchOpen] =
-            *device_state.lid_switch_open;
-      }
-      if (device_state.hinge_angle_value) {
-        device_state_entry[kCustomActionDeviceStateHingeAngleValue] =
-            *device_state.hinge_angle_value;
-      }
-      json[kCustomActionDeviceStates].append(device_state_entry);
-    }
-    json[kCustomActionButton] = Json::Value();
-    json[kCustomActionButton][kCustomActionButtonCommand] =
-        custom_action.buttons[0].command;
-    json[kCustomActionButton][kCustomActionButtonTitle] =
-        custom_action.buttons[0].title;
-    json[kCustomActionButton][kCustomActionButtonIconName] =
-        custom_action.buttons[0].icon_name;
-  } else {
-    LOG(FATAL) << "Unknown custom action type.";
+  // Shell command with one button.
+  json[kCustomActionShellCommand] = custom_action.shell_command;
+  json[kCustomActionButton] = Json::Value();
+  json[kCustomActionButton][kCustomActionButtonCommand] =
+      custom_action.button.command;
+  json[kCustomActionButton][kCustomActionButtonTitle] =
+      custom_action.button.title;
+  json[kCustomActionButton][kCustomActionButtonIconName] =
+      custom_action.button.icon_name;
+  return json;
+}
+
+Json::Value ToJson(const CustomActionServerConfig& custom_action) {
+  Json::Value json;
+  // Action server with possibly multiple buttons.
+  json[kCustomActionServer] = custom_action.server;
+  json[kCustomActionButtons] = Json::Value(Json::arrayValue);
+  for (const auto& button : custom_action.buttons) {
+    Json::Value button_entry;
+    button_entry[kCustomActionButtonCommand] = button.command;
+    button_entry[kCustomActionButtonTitle] = button.title;
+    button_entry[kCustomActionButtonIconName] = button.icon_name;
+    json[kCustomActionButtons].append(button_entry);
   }
+  return json;
+}
+
+Json::Value ToJson(const CustomDeviceStateActionConfig& custom_action) {
+  Json::Value json;
+  // Device state(s) with one button.
+  json[kCustomActionDeviceStates] = Json::Value(Json::arrayValue);
+  for (const auto& device_state : custom_action.device_states) {
+    Json::Value device_state_entry;
+    if (device_state.lid_switch_open) {
+      device_state_entry[kCustomActionDeviceStateLidSwitchOpen] =
+          *device_state.lid_switch_open;
+    }
+    if (device_state.hinge_angle_value) {
+      device_state_entry[kCustomActionDeviceStateHingeAngleValue] =
+          *device_state.hinge_angle_value;
+    }
+    json[kCustomActionDeviceStates].append(device_state_entry);
+  }
+  json[kCustomActionButton] = Json::Value();
+  json[kCustomActionButton][kCustomActionButtonCommand] =
+      custom_action.button.command;
+  json[kCustomActionButton][kCustomActionButtonTitle] =
+      custom_action.button.title;
+  json[kCustomActionButton][kCustomActionButtonIconName] =
+      custom_action.button.icon_name;
   return json;
 }
 
@@ -212,14 +213,31 @@ class CustomActionConfigImpl : public CustomActionConfigProvider {
     });
   }
 
-  const std::vector<CustomActionConfig>& CustomActions() const override {
-    return custom_actions_;
+  virtual const std::vector<CustomShellActionConfig>& CustomShellActions()
+      const override {
+    return custom_shell_actions_;
+  }
+
+  virtual const std::vector<CustomActionServerConfig>& CustomActionServers()
+      const override {
+    return custom_action_servers_;
+  }
+
+  virtual const std::vector<CustomDeviceStateActionConfig>&
+  CustomDeviceStateActions() const override {
+    return custom_device_state_actions_;
   }
 
   // ConfigFragment
   Json::Value Serialize() const override {
     Json::Value actions_array(Json::arrayValue);
-    for (const auto& action : CustomActions()) {
+    for (const auto& action : CustomShellActions()) {
+      actions_array.append(ToJson(action));
+    }
+    for (const auto& action : CustomActionServers()) {
+      actions_array.append(ToJson(action));
+    }
+    for (const auto& action : CustomDeviceStateActions()) {
       actions_array.append(ToJson(action));
     }
     return actions_array;
@@ -268,24 +286,43 @@ class CustomActionConfigImpl : public CustomActionConfigProvider {
       LOG(ERROR) << "Expected a JSON array of custom actions";
       return false;
     }
-    for (const auto& custom_action_json : custom_action_array) {
-      auto custom_action = CustomActionConfigFromJson(custom_action_json);
-      if (custom_action) {
-        custom_actions_.push_back(*custom_action);
+    for (const auto& custom_action : custom_action_array) {
+      bool has_shell_command =
+          custom_action.isMember(kCustomActionShellCommand);
+      bool has_server = custom_action.isMember(kCustomActionServer);
+      bool has_device_states =
+          custom_action.isMember(kCustomActionDeviceStates);
+      if (!!has_shell_command + !!has_server + !!has_device_states != 1) {
+        LOG(ERROR) << "Custom action must contain exactly one of "
+                      "shell_command, server, or device_states";
+        return false;
+      }
+
+      if (has_shell_command) {
+        auto config = GetCustomShellActionConfigFromJson(custom_action);
+        custom_shell_actions_.push_back(config);
+      } else if (has_server) {
+        auto config = GetCustomActionServerConfigFromJson(custom_action);
+        custom_action_servers_.push_back(config);
+      } else if (has_device_states) {
+        auto config = GetCustomDeviceStateActionConfigFromJson(custom_action);
+        custom_device_state_actions_.push_back(config);
       } else {
-        LOG(ERROR) << "Validation failed on a custom action";
+        LOG(ERROR) << "Unknown custom action type.";
         return false;
       }
     }
     return true;
   }
 
-  ConfigFlag& config_;
-  Flag custom_action_config_flag_;
-  std::string custom_action_config_;
-  Flag custom_actions_flag_;
-  std::vector<CustomActionConfig> custom_actions_;
-};
+    ConfigFlag& config_;
+    Flag custom_action_config_flag_;
+    std::string custom_action_config_;
+    Flag custom_actions_flag_;
+    std::vector<CustomShellActionConfig> custom_shell_actions_;
+    std::vector<CustomActionServerConfig> custom_action_servers_;
+    std::vector<CustomDeviceStateActionConfig> custom_device_state_actions_;
+  };
 
 }  // namespace
 
