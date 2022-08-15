e81088a57b0977af6c4e8b58630ffcbed1f7b64a
Treehugger Robot <treehugger-gerrit@google.com>
2022-08-15 11:52:19 +0000

    Merge changes Icfb00d59,I10bedbb9 am: 439617969f am: 2e9156fb6f am: ccfca6bf30 am: eae60141e1
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2165255
    
    Change-Id: Ia03cc282ff2c0fb8555702db31446fedde49e8fc
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 7a432e39b72ffd876e9a8a2aa885befefd9bdf73

--- staging/host/libs/location/GnssClient.h
+++ staging/host/libs/location/GnssClient.h
@@ -27,13 +27,12 @@ class GnssClient {
  public:
   GnssClient(std::shared_ptr<grpc::Channel> channel);
 
-  // Assambles the client's payload, sends it and presents the response back
+  // Aseembles the client's payload, sends it and presents the response back
   // from the server.
-  Result<grpc::Status> SendGps(const std::string& user);
+  Result<grpc::Status> SendSingleGpsLoc(const std::string& user);
   std::string FormatGps(const std::string& latitude,
                         const std::string& longitude,
-                        const std::string& elevation,
-                        const std::string& timestamp, bool inject_time);
+                        const std::string& elevation);
 
  private:
   std::unique_ptr<gnss_grpc_proxy::GnssGrpcProxy::Stub> stub_;
