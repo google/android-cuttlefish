00b9c7bbbacf2daa74a3a857f4e454ab1eb78f02
Steve Kim <kwstephenkim@google.com>
2022-08-27 00:53:27 +0000

    [skip ci] Adding instance record data structure am: fb532aa4a1 am: 07c17ae2f5 am: 9dcfc01bc3 am: 7056b33d9d
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2192069
    
    Change-Id: I420719df240c6f2da246afc4fe11684e303ec36c
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 3d56e4c205f4c9205b15db5a8166082c0cbaa6c0

---
+++ staging/host/commands/cvd/instance_record.h
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include "common/libs/utils/result.h"
+
+namespace cuttlefish {
+namespace instance_db {
+
+class LocalInstanceGroup {
+  /* will be implemented */
+};
+
+/**
+ * TODO(kwstephenkim): add more methods, fields, and abstract out Instance
+ *
+ * Needs design changes to support both Remote and Local Instances
+ */
+class LocalInstance {
+  friend class LocalInstanceGroup;
+
+ public:
+  using LocalInstancePtr = std::unique_ptr<LocalInstance>;
+
+  template <typename... Args>
+  static LocalInstancePtr Create(Args&&... args) {
+    auto new_instance = new LocalInstance(std::forward<Args>(args)...);
+    return std::unique_ptr<LocalInstance>(new_instance);
+  }
+
+  /* names:
+   *
+   *  As of 08/21/2022, the name of a cuttlefish instance is cvd-N. For now,
+   * instance groups share the "cvd-" prefix. So, "cvd" is the group name, and
+   * "N" is the instance specific name. "cvd-N" is the device name.
+   *
+   * There will be another name the user specify for each instance. However,
+   * many components in Cuttlefish traditionally expect the name to be "cvd-N,"
+   * and rely on "N" to avoid conflicts in the global resource uses.
+   *
+   * Thus, we will eventually maintain the internal device name for those
+   * existing cuttlefish implementation, and the user-given name.
+   *
+   */
+  const std::string& InternalName() const;
+
+  int InstanceId() const;
+  const LocalInstanceGroup& Parent() const;
+
+ private:
+  LocalInstance(const int instance_id, LocalInstanceGroup& parent);
+
+  const int instance_id_;
+  const std::string internal_name_;  ///< for now, it is to_string(instance_id_)
+  LocalInstanceGroup& parent_;
+};
+
+using LocalInstancePtr = LocalInstance::LocalInstancePtr;
+
+}  // namespace instance_db
+}  // namespace cuttlefish
