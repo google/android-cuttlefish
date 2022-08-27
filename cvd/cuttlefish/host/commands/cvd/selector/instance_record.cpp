74c1afce2497e45eee010856a3c27c5248d964e3
Steve Kim <kwstephenkim@google.com>
2022-08-27 01:20:36 +0000

    Adding instance record data structure am: fb532aa4a1 am: 07c17ae2f5 am: 9dcfc01bc3 am: 7056b33d9d am: 3d56e4c205
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2192069
    
    Change-Id: Icea3d9c0bbeea6575b1d7d4bd9090d3ffa2ba8ce
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- cvd/cuttlefish/host/commands/cvd/selector/instance_record.cpp
+++ cvd/cuttlefish/host/commands/cvd/selector/instance_record.cpp
@@ -14,68 +14,23 @@
  * limitations under the License.
  */
 
-#include "host/commands/cvd/selector/instance_record.h"
-
-#include "host/commands/cvd/selector/instance_database_utils.h"
-#include "host/commands/cvd/selector/instance_group_record.h"
+#include "host/commands/cvd/instance_record.h"
 
 namespace cuttlefish {
-namespace selector {
+namespace instance_db {
 
-LocalInstance::LocalInstance(const LocalInstanceGroup& parent_group,
-                             const unsigned instance_id,
-                             const std::string& instance_name)
-    : parent_group_(parent_group),
-      instance_id_(instance_id),
+LocalInstance::LocalInstance(const int instance_id, LocalInstanceGroup& parent)
+    : instance_id_(instance_id),
       internal_name_(std::to_string(instance_id_)),
-      per_instance_name_(instance_name) {}
+      parent_(parent) {}
 
-unsigned LocalInstance::InstanceId() const { return instance_id_; }
-
-std::string LocalInstance::InternalDeviceName() const {
-  return LocalDeviceNameRule(parent_group_.InternalGroupName(), internal_name_);
-}
+int LocalInstance::InstanceId() const { return instance_id_; }
 
 const std::string& LocalInstance::InternalName() const {
   return internal_name_;
 }
 
-std::string LocalInstance::DeviceName() const {
-  return LocalDeviceNameRule(parent_group_.GroupName(), per_instance_name_);
-}
+const LocalInstanceGroup& LocalInstance::Parent() const { return parent_; }
 
-const std::string& LocalInstance::PerInstanceName() const {
-  return per_instance_name_;
-}
-
-const LocalInstanceGroup& LocalInstance::ParentGroup() const {
-  return parent_group_;
-}
-
-LocalInstance::Copy LocalInstance::GetCopy() const {
-  Copy copy(*this);
-  return copy;
-}
-
-LocalInstance::Copy::Copy(const LocalInstance& src)
-    : internal_name_{src.InternalName()},
-      internal_device_name_{src.InternalDeviceName()},
-      instance_id_{src.InstanceId()},
-      per_instance_name_{src.PerInstanceName()},
-      device_name_{src.DeviceName()},
-      mock_group_{MockParentParam{
-          .home_dir = src.ParentGroup().HomeDir(),
-          .host_artifacts_path = src.ParentGroup().HostArtifactsPath(),
-          .internal_group_name = src.ParentGroup().InternalGroupName(),
-          .group_name = src.ParentGroup().GroupName(),
-          .build_id = src.ParentGroup().BuildId()}} {}
-
-LocalInstance::Copy::MockParent::MockParent(const MockParentParam& params)
-    : home_dir_{params.home_dir},
-      host_artifacts_path_{params.host_artifacts_path},
-      internal_group_name_{params.internal_group_name},
-      group_name_{params.group_name},
-      build_id_{params.build_id} {}
-
-}  // namespace selector
+}  // namespace instance_db
 }  // namespace cuttlefish
