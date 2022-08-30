4440a4ea57816dba560ec11ce421c16d11c10c76
Treehugger Robot <treehugger-gerrit@google.com>
2022-08-30 18:48:30 +0000

    [skip ci] Merge "Add `serial_launch` below `create_phone_tablet`" am: e913aaab57 am: 12f0e10ce4 am: 7523617818 am: 8921cf3883
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2185256
    
    Change-Id: I15524a97aa3f907a20d484c795381dbb35deb997
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 2945cda1d10a5b432344c24b7de5d8956f587596

--- staging/host/commands/cvd/demo_multi_vd.cpp
+++ staging/host/commands/cvd/demo_multi_vd.cpp
@@ -27,6 +27,7 @@
 #include "common/libs/utils/result.h"
 #include "host/commands/cvd/command_sequence.h"
 #include "host/commands/cvd/server.h"
+#include "instance_lock.h"
 #include "server_client.h"
 
 namespace cuttlefish {
@@ -36,18 +37,20 @@ struct DemoCommandSequence {
   std::vector<RequestWithStdio> requests;
 };
 
-class DemoMultiVdCommand : public CvdServerHandler {
+static constexpr char kParentDir[] = "/tmp/cvd/";
+
+class SerialLaunchCommand : public CvdServerHandler {
  public:
-  INJECT(DemoMultiVdCommand(CommandSequenceExecutor& executor,
-                            InstanceLockFileManager& lock_file_manager))
+  INJECT(SerialLaunchCommand(CommandSequenceExecutor& executor,
+                             InstanceLockFileManager& lock_file_manager))
       : executor_(executor), lock_file_manager_(lock_file_manager) {}
-  ~DemoMultiVdCommand() = default;
+  ~SerialLaunchCommand() = default;
 
   Result<bool> CanHandle(const RequestWithStdio& request) const override {
     auto invocation = ParseInvocation(request.Message());
     return invocation.command == "experimental" &&
            invocation.arguments.size() >= 1 &&
-           invocation.arguments[0] == "create_phone_tablet";
+           invocation.arguments[0] == "serial_launch";
   }
   Result<cvd::Response> Handle(const RequestWithStdio& request) override {
     std::unique_lock interrupt_lock(interrupt_mutex_);
@@ -87,99 +90,108 @@ class DemoMultiVdCommand : public CvdServerHandler {
     flags.emplace_back(GflagsCompatFlag("help", help));
     flags.emplace_back(GflagsCompatFlag("credentials", credentials));
     flags.emplace_back(GflagsCompatFlag("verbose", verbose));
+
+    struct Device {
+      std::string build;
+      std::string home_dir;
+      InstanceLockFile ins_lock;
+    };
+
+    auto time = std::chrono::system_clock::now().time_since_epoch().count();
+    std::vector<Device> devices;
+    auto& device_flag = flags.emplace_back();
+    device_flag.Alias({FlagAliasMode::kFlagPrefix, "--device="});
+    device_flag.Alias({FlagAliasMode::kFlagConsumesFollowing, "--device"});
+    device_flag.Setter([this, time, &devices, &request](const FlagMatch& mat) {
+      auto lock = lock_file_manager_.TryAcquireUnusedLock();
+      if (!lock.ok()) {
+        WriteAll(request.Err(), lock.error().message());
+        return false;
+      } else if (!lock->has_value()) {
+        constexpr char kError[] = "could not acquire instance lock";
+        WriteAll(request.Err(), kError, sizeof(kError));
+        return false;
+      }
+      int num = (*lock)->Instance();
+      std::string home_dir =
+          kParentDir + std::to_string(time) + "_" + std::to_string(num) + "/";
+      devices.emplace_back(Device{
+          .build = mat.value,
+          .home_dir = std::move(home_dir),
+          .ins_lock = std::move(**lock),
+      });
+      return true;
+    });
+
     auto args = ParseInvocation(request.Message()).arguments;
     CF_EXPECT(ParseFlags(flags, args));
 
     if (help) {
       static constexpr char kHelp[] =
-          "Usage: cvd experimental "
-          "create_phone_tablet [--verbose] --credentials=XYZ";
+          "Usage: cvd experimental serial_launch [--verbose] --credentials=XYZ "
+          "--device=build/target --device=build/target";
       CF_EXPECT(WriteAll(request.Out(), kHelp, sizeof(kHelp)) == sizeof(kHelp));
       return {};
     }
 
     DemoCommandSequence ret;
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < devices.size(); i++) {
       auto lock = CF_EXPECT(lock_file_manager_.TryAcquireUnusedLock());
       CF_EXPECT(lock.has_value(), "Failed to acquire instance number");
       ret.instance_locks.emplace_back(std::move(*lock));
     }
     std::vector<cvd::Request> req_protos;
 
-    std::string parent_dir = "/tmp/cvd/";
-    auto time = std::chrono::system_clock::now().time_since_epoch().count();
-    auto phone_home = parent_dir + "/cvd_phone_" + std::to_string(time) + "/";
-
-    if (!DirectoryExists(parent_dir)) {
+    if (!DirectoryExists(kParentDir)) {
       auto& mkdir_parent = *req_protos.emplace_back().mutable_command_request();
       *mkdir_parent.mutable_env() = client_env;
       mkdir_parent.add_args("cvd");
       mkdir_parent.add_args("mkdir");
-      mkdir_parent.add_args(parent_dir);
+      mkdir_parent.add_args(kParentDir);
     }
 
-    auto& mkdir_phone = *req_protos.emplace_back().mutable_command_request();
-    *mkdir_phone.mutable_env() = client_env;
-    mkdir_phone.add_args("cvd");
-    mkdir_phone.add_args("mkdir");
-    mkdir_phone.add_args(phone_home);
+    bool is_first = true;
 
-    auto& fetch_phone = *req_protos.emplace_back().mutable_command_request();
-    *fetch_phone.mutable_env() = client_env;
-    fetch_phone.set_working_directory(phone_home);
-    fetch_phone.add_args("cvd");
-    fetch_phone.add_args("fetch");
-    fetch_phone.add_args("--directory=" + phone_home);
-    fetch_phone.add_args("-default_build=git_master/cf_x86_64_phone-userdebug");
-    fetch_phone.add_args("-credential_source=" + credentials);
+    for (const auto& device : devices) {
+      auto& mkdir_cmd = *req_protos.emplace_back().mutable_command_request();
+      *mkdir_cmd.mutable_env() = client_env;
+      mkdir_cmd.add_args("cvd");
+      mkdir_cmd.add_args("mkdir");
+      mkdir_cmd.add_args(device.home_dir);
 
-    auto& launch_phone = *req_protos.emplace_back().mutable_command_request();
-    *fetch_phone.mutable_env() = client_env;
-    launch_phone.set_working_directory(phone_home);
-    (*launch_phone.mutable_env())["HOME"] = phone_home;
-    (*launch_phone.mutable_env())["ANDROID_HOST_OUT"] = phone_home;
-    (*launch_phone.mutable_env())["ANDROID_PRODUCT_OUT"] = phone_home;
-    launch_phone.add_args("cvd");
-    launch_phone.add_args("start");
-    launch_phone.add_args("--daemon");
-    launch_phone.add_args("--report_anonymous_usage_stats=y");
-    auto phone_instance = std::to_string(ret.instance_locks[0].Instance());
-    launch_phone.add_args("--base_instance_num=" + phone_instance);
+      auto& fetch_cmd = *req_protos.emplace_back().mutable_command_request();
+      *fetch_cmd.mutable_env() = client_env;
+      fetch_cmd.set_working_directory(device.home_dir);
+      fetch_cmd.add_args("cvd");
+      fetch_cmd.add_args("fetch");
+      fetch_cmd.add_args("--directory=" + device.home_dir);
+      fetch_cmd.add_args("-default_build=" + device.build);
+      fetch_cmd.add_args("-credential_source=" + credentials);
 
-    auto tablet_home = parent_dir + "/cvd_tablet_" + std::to_string(time) + "/";
+      auto& launch_cmd = *req_protos.emplace_back().mutable_command_request();
+      *launch_cmd.mutable_env() = client_env;
+      launch_cmd.set_working_directory(device.home_dir);
+      (*launch_cmd.mutable_env())["HOME"] = device.home_dir;
+      (*launch_cmd.mutable_env())["ANDROID_HOST_OUT"] = device.home_dir;
+      (*launch_cmd.mutable_env())["ANDROID_PRODUCT_OUT"] = device.home_dir;
+      launch_cmd.add_args("cvd");
+      launch_cmd.add_args("start");
+      launch_cmd.add_args("--daemon");
+      launch_cmd.add_args("--report_anonymous_usage_stats=y");
+      launch_cmd.add_args("--base_instance_num=" +
+                          std::to_string(device.ins_lock.Instance()));
 
-    auto& mkdir_tablet = *req_protos.emplace_back().mutable_command_request();
-    *mkdir_tablet.mutable_env() = client_env;
-    mkdir_tablet.add_args("cvd");
-    mkdir_tablet.add_args("mkdir");
-    mkdir_tablet.add_args(tablet_home);
-
-    auto& fetch_tablet = *req_protos.emplace_back().mutable_command_request();
-    *fetch_tablet.mutable_env() = client_env;
-    fetch_tablet.set_working_directory(tablet_home);
-    fetch_tablet.add_args("cvd");
-    fetch_tablet.add_args("fetch");
-    fetch_tablet.add_args("--directory=" + tablet_home);
-    fetch_tablet.add_args(
-        "-default_build=git_master/cf_x86_64_tablet-userdebug");
-    fetch_tablet.add_args("-credential_source=" + credentials);
-
-    auto& launch_tablet = *req_protos.emplace_back().mutable_command_request();
-    *launch_tablet.mutable_env() = client_env;
-    launch_tablet.set_working_directory(tablet_home);
-    (*launch_tablet.mutable_env())["HOME"] = tablet_home;
-    (*launch_tablet.mutable_env())["ANDROID_HOST_OUT"] = tablet_home;
-    (*launch_tablet.mutable_env())["ANDROID_PRODUCT_OUT"] = tablet_home;
-    launch_tablet.add_args("cvd");
-    launch_tablet.add_args("start");
-    launch_tablet.add_args("--daemon");
-    launch_tablet.add_args("--report_anonymous_usage_stats=y");
-    auto hwsim_path = phone_home + "cuttlefish_runtime." + phone_instance +
-                      "/internal/vhost_user_mac80211";
-    launch_tablet.add_args("--vhost_user_mac80211_hwsim=" + hwsim_path);
-    launch_tablet.add_args("--rootcanal_attach_mode");
-    auto tablet_instance = std::to_string(ret.instance_locks[1].Instance());
-    launch_tablet.add_args("--base_instance_num=" + tablet_instance);
+      if (is_first) {
+        is_first = false;
+        continue;
+      }
+      const auto& first = devices[0];
+      auto hwsim_path = first.home_dir + "cuttlefish_runtime." +
+                        std::to_string(first.ins_lock.Instance()) +
+                        "/internal/vhost_user_mac80211";
+      launch_cmd.add_args("--vhost_user_mac80211_hwsim=" + hwsim_path);
+      launch_cmd.add_args("--rootcanal_attach_mode");
+    }
 
     std::vector<SharedFD> fds;
     if (verbose) {
@@ -206,10 +218,84 @@ class DemoMultiVdCommand : public CvdServerHandler {
   bool interrupted_ = false;
 };
 
+class SerialPreset : public CvdServerHandler {
+ public:
+  INJECT(SerialPreset(CommandSequenceExecutor& executor))
+      : executor_(executor) {}
+  ~SerialPreset() = default;
+
+  Result<bool> CanHandle(const RequestWithStdio& request) const override {
+    auto invocation = ParseInvocation(request.Message());
+    return invocation.command == "experimental" &&
+           invocation.arguments.size() >= 1 &&
+           Presets().count(invocation.arguments[0]) > 0;
+  }
+  Result<cvd::Response> Handle(const RequestWithStdio& request) override {
+    std::unique_lock interrupt_lock(interrupt_mutex_);
+    if (interrupted_) {
+      return CF_ERR("Interrupted");
+    }
+    CF_EXPECT(CF_EXPECT(CanHandle(request)));
+
+    auto invocation = ParseInvocation(request.Message());
+    CF_EXPECT(invocation.arguments.size() >= 1);
+    const auto& presets = Presets();
+    auto devices = presets.find(invocation.arguments[0]);
+    CF_EXPECT(devices != presets.end(), "could not find preset");
+
+    cvd::Request inner_req_proto = request.Message();
+    auto& cmd = *inner_req_proto.mutable_command_request();
+    cmd.clear_args();
+    cmd.add_args("cvd");
+    cmd.add_args("experimental");
+    cmd.add_args("serial_launch");
+    for (const auto& device : devices->second) {
+      cmd.add_args("--device=" + device);
+    }
+    for (int i = 1; i < invocation.arguments.size(); i++) {
+      cmd.add_args(invocation.arguments[i]);
+    }
+
+    RequestWithStdio inner_request(request.Client(), std::move(inner_req_proto),
+                                   request.FileDescriptors(),
+                                   request.Credentials());
+
+    CF_EXPECT(executor_.Execute({std::move(inner_request)}, request.Err()));
+    interrupt_lock.unlock();
+
+    cvd::Response response;
+    response.mutable_command_response();
+    return response;
+  }
+  Result<void> Interrupt() override {
+    std::scoped_lock interrupt_lock(interrupt_mutex_);
+    interrupted_ = true;
+    CF_EXPECT(executor_.Interrupt());
+    return {};
+  }
+
+ private:
+  CommandSequenceExecutor& executor_;
+
+  static std::unordered_map<std::string, std::vector<std::string>> Presets() {
+    return {
+        {"create_phone_tablet",
+         {"git_master/cf_x86_64_phone-userdebug",
+          "git_master/cf_x86_64_tablet-userdebug"}},
+        {"create_phone_wear",
+         {"git_master/cf_x86_64_phone-userdebug", "git_master/cf_gwear_x86"}},
+    };
+  }
+
+  std::mutex interrupt_mutex_;
+  bool interrupted_ = false;
+};
+
 fruit::Component<fruit::Required<CommandSequenceExecutor>>
 DemoMultiVdComponent() {
   return fruit::createComponent()
-      .addMultibinding<CvdServerHandler, DemoMultiVdCommand>();
+      .addMultibinding<CvdServerHandler, SerialLaunchCommand>()
+      .addMultibinding<CvdServerHandler, SerialPreset>();
 }
 
 }  // namespace cuttlefish
