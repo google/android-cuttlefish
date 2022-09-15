8a5b34069e41bb68755ce9ec9e64f538b0e5d98b
Treehugger Robot <treehugger-gerrit@google.com>
2022-09-15 03:56:27 +0000

    [skip ci] Merge "Abstract out common code from cvd command handlers" am: 79e6286903 am: eea5308b25 am: 7969b12fed am: df98ec2729
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2213203
    
    Change-Id: Ia2724cef95c49c5f874875a675b672b52821b031
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 8dfd53aafafdb2cb4bf7d34024ccb2dcb40baffb

--- staging/host/commands/cvd/server_command_start_impl.cpp
+++ staging/host/commands/cvd/server_command_start_impl.cpp
@@ -42,105 +42,48 @@ Result<cvd::Response> CvdStartCommandHandler::Handle(
   cvd::Response response;
   response.mutable_command_response();
 
-  auto invocation = ParseInvocation(request.Message());
-
-  auto subcommand_bin = command_to_binary_map_.find(invocation.command);
-  CF_EXPECT(subcommand_bin != command_to_binary_map_.end());
-  auto bin = subcommand_bin->second;
-
-  // HOME is used to possibly set CuttlefishConfig path env variable later.
-  // This env variable is used by subcommands when locating the config.
-  auto request_home = request.Message().command_request().env().find("HOME");
-  std::string home =
-      request_home != request.Message().command_request().env().end()
-          ? request_home->second
-          : StringFromEnv("HOME", ".");
-
-  // Create a copy of args before parsing, to be passed to subcommands.
-  auto args = invocation.arguments;
-  auto args_copy = invocation.arguments;
-
-  auto host_artifacts_path =
-      request.Message().command_request().env().find("ANDROID_HOST_OUT");
-  if (host_artifacts_path == request.Message().command_request().env().end()) {
+  auto invocation_info_opt = ExtractInfo(command_to_binary_map_, request);
+  if (!invocation_info_opt) {
     response.mutable_status()->set_code(cvd::Status::FAILED_PRECONDITION);
     response.mutable_status()->set_message(
-        "Missing ANDROID_HOST_OUT in client environment.");
+        "ANDROID_HOST_OUT in client environment is invalid.");
     return response;
   }
 
+  auto invocation_info = std::move(*invocation_info_opt);
+  auto& envs = invocation_info.envs;
+
   InstanceNumsCalculator calculator;
-  auto instance_env =
-      request.Message().command_request().env().find("CUTTLEFISH_INSTANCE");
-  if (instance_env != request.Message().command_request().env().end()) {
+  auto instance_env = envs.find("CUTTLEFISH_INSTANCE");
+  if (instance_env != envs.end()) {
     calculator.BaseInstanceNum(std::stoi(instance_env->second));
   }
 
   // Track this assembly_dir in the fleet.
   InstanceManager::InstanceGroupInfo info;
-  info.host_binaries_dir = host_artifacts_path->second + "/bin/";
+  info.host_binaries_dir = invocation_info.host_artifacts_path + "/bin/";
   info.instances = CF_EXPECT(calculator.Calculate());
-  instance_manager_.SetInstanceGroup(home, info);
+  instance_manager_.SetInstanceGroup(invocation_info.home, info);
 
-  Command command("(replaced)");
-  auto assembly_info = CF_EXPECT(instance_manager_.GetInstanceGroup(home));
-  command.SetExecutableAndName(assembly_info.host_binaries_dir + bin);
+  auto assembly_info =
+      CF_EXPECT(instance_manager_.GetInstanceGroup(invocation_info.home));
+  const std::string bin_path =
+      assembly_info.host_binaries_dir + invocation_info.bin;
 
-  for (const std::string& arg : args_copy) {
-    command.AddParameter(arg);
-  }
+  Command command = CF_EXPECT(ConstructCommand(
+      bin_path, invocation_info.home, invocation_info.args, envs,
+      request.Message().command_request().working_directory(),
+      invocation_info.bin, request.In(), request.Out(), request.Err()));
 
-  // Set CuttlefishConfig path based on assembly dir,
-  // used by subcommands when locating the CuttlefishConfig.
-  if (request.Message().command_request().env().count(
-          kCuttlefishConfigEnvVarName) == 0) {
-    auto config_path = GetCuttlefishConfigPath(home);
-    if (config_path.ok()) {
-      command.AddEnvironmentVariable(kCuttlefishConfigEnvVarName, *config_path);
-    }
-  }
-  for (auto& it : request.Message().command_request().env()) {
-    command.UnsetFromEnvironment(it.first);
-    command.AddEnvironmentVariable(it.first, it.second);
-  }
-
-  // Redirect stdin, stdout, stderr back to the cvd client
-  command.RedirectStdIO(Subprocess::StdIOChannel::kStdIn, request.In());
-  command.RedirectStdIO(Subprocess::StdIOChannel::kStdOut, request.Out());
-  command.RedirectStdIO(Subprocess::StdIOChannel::kStdErr, request.Err());
-  SubprocessOptions options;
-
-  if (request.Message().command_request().wait_behavior() ==
-      cvd::WAIT_BEHAVIOR_START) {
-    options.ExitWithParent(false);
-  }
-
-  const auto& working_dir =
-      request.Message().command_request().working_directory();
-  if (!working_dir.empty()) {
-    auto fd = SharedFD::Open(working_dir, O_RDONLY | O_PATH | O_DIRECTORY);
-    CF_EXPECT(fd->IsOpen(),
-              "Couldn't open \"" << working_dir << "\": " << fd->StrError());
-    command.SetWorkingDirectory(fd);
-  }
-
-  CF_EXPECT(subprocess_waiter_.Setup(command.Start(options)));
-
-  if (request.Message().command_request().wait_behavior() ==
-      cvd::WAIT_BEHAVIOR_START) {
+  const bool should_wait =
+      (request.Message().command_request().wait_behavior() !=
+       cvd::WAIT_BEHAVIOR_START);
+  FireCommand(std::move(command), should_wait);
+  if (!should_wait) {
     response.mutable_status()->set_code(cvd::Status::OK);
     return response;
   }
-
-  interrupt_lock.unlock();
-
-  auto infop = CF_EXPECT(subprocess_waiter_.Wait());
-
-  if (infop.si_code == CLD_EXITED && bin == kStopBin) {
-    instance_manager_.RemoveInstanceGroup(home);
-  }
-
-  return ResponseFromSiginfo(infop);
+  return UnlockAndWait(interrupt_lock);
 }
 
 Result<void> CvdStartCommandHandler::Interrupt() {
@@ -148,6 +91,24 @@ Result<void> CvdStartCommandHandler::Interrupt() {
   interrupted_ = true;
   CF_EXPECT(subprocess_waiter_.Interrupt());
   return {};
+}
+
+Result<void> CvdStartCommandHandler::FireCommand(Command&& command,
+                                                 const bool wait) {
+  SubprocessOptions options;
+  if (!wait) {
+    options.ExitWithParent(false);
+  }
+  CF_EXPECT(subprocess_waiter_.Setup(command.Start(options)));
+  return {};
+}
+
+Result<cvd::Response> CvdStartCommandHandler::UnlockAndWait(
+    std::unique_lock<std::mutex>& interrupt_lock) {
+  interrupt_lock.unlock();
+
+  auto infop = CF_EXPECT(subprocess_waiter_.Wait());
+  return ResponseFromSiginfo(infop);
 }
 
 const std::map<std::string, std::string>
