0dad621cc19b5e4856462bc9ab46d8a44530cc24
Steve Kim <kwstephenkim@google.com>
2022-09-16 08:38:25 +0000

    [skip ci] Adding InstanceDatabase am: 5297c35ce8 am: 78b7840451 am: 386dd267c5 am: 7855af2334
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2199998
    
    Change-Id: I417098fa7d2d532556c2cc152a13bdd19cab5ee8
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: a48c887a9f43c14ec861466bac3ba5e9f491f957

---
+++ staging/host/commands/cvd/instance_database_types.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+
+namespace cuttlefish {
+namespace instance_db {
+
+using FieldName = std::string;
+using Value = std::string;
+// e.g. if intended to search by --home=/home/vsoc-01,
+// field_name_ is "home" and the field_name_ is "/home/vsoc-01"
+struct Query {
+  FieldName field_name_;
+  Value field_value_;
+};
+
+template <typename T>
+using Set = std::unordered_set<T>;
+
+template <typename K, typename V>
+using Map = std::unordered_map<K, V>;
+
+}  // namespace instance_db
+}  // namespace cuttlefish
