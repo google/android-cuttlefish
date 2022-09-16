f709add50c669b99f9a653b8bde571013581f03b
Steve Kim <kwstephenkim@google.com>
2022-09-16 08:38:25 +0000

    Adding InstanceDatabase am: 5297c35ce8 am: 78b7840451 am: 386dd267c5 am: 7855af2334
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2199998
    
    Change-Id: I417098fa7d2d532556c2cc152a13bdd19cab5ee8
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: a48c887a9f43c14ec861466bac3ba5e9f491f957

---
+++ staging/host/commands/cvd/instance_database.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "host/commands/cvd/instance_database.h"
+
+namespace cuttlefish {
+namespace instance_db {
+
+bool InstanceDatabase::IsEmpty() const {
+  return local_instance_groups_.empty();
+}
+
+template <typename T>
+Result<Set<T>> InstanceDatabase::Find(
+    const Query& query,
+    const Map<FieldName, ConstHandler<T>>& handler_map) const {
+  static_assert(std::is_same<T, LocalInstance>::value ||
+                std::is_same<T, LocalInstanceGroup>::value);
+  const auto& [key, value] = query;
+  auto itr = handler_map.find(key);
+  if (itr == handler_map.end()) {
+    return CF_ERR("Handler does not exist for query " << key);
+  }
+  return (itr->second)(value);
+}
+
+Result<Set<LocalInstanceGroup>> InstanceDatabase::FindGroups(
+    const Query& query) const {
+  return Find<LocalInstanceGroup>(query, group_handlers_);
+}
+
+Result<Set<LocalInstance>> InstanceDatabase::FindInstances(
+    const Query& query) const {
+  return Find<LocalInstance>(query, instance_handlers_);
+}
+
+}  // namespace instance_db
+}  // namespace cuttlefish
