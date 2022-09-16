cbacfe613a555d914cd2fd212a57e0a2b0311679
Steve Kim <kwstephenkim@google.com>
2022-09-16 10:27:04 +0000

    Adding InstanceDatabase am: 5297c35ce8 am: 78b7840451 am: 386dd267c5 am: 7855af2334 am: a48c887a9f
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2199998
    
    Change-Id: Ib7b3dee937399e854e03b9665cabd751490dd659
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- cvd/cuttlefish/host/commands/cvd/selector/instance_database.cpp
+++ cvd/cuttlefish/host/commands/cvd/selector/instance_database.cpp
@@ -14,46 +14,17 @@
  * limitations under the License.
  */
 
-#include "host/commands/cvd/selector/instance_database.h"
-
-#include <numeric>  // std::iota
-
-#include "host/commands/cvd/selector/instance_database_utils.h"
-#include "host/commands/cvd/selector/selector_constants.h"
+#include "host/commands/cvd/instance_database.h"
 
 namespace cuttlefish {
-namespace selector {
-
-InstanceDatabase::InstanceDatabase() {
-  group_handlers_[kHomeField] = [this](const Value& field_value) {
-    return FindGroupsByHome(field_value);
-  };
-  group_handlers_[kGroupNameField] = [this](const Value& field_value) {
-    return FindGroupsByGroupName(field_value);
-  };
-  group_handlers_[kInstanceNameField] = [this](const Value& field_value) {
-    return FindGroupsByInstanceName(field_value);
-  };
-  instance_handlers_[kHomeField] = [this](const Value& field_value) {
-    return FindInstancesByHome(field_value);
-  };
-  instance_handlers_[kInstanceIdField] = [this](const Value& field_value) {
-    return FindInstancesById(field_value);
-  };
-  instance_handlers_[kGroupNameField] = [this](const Value& field_value) {
-    return FindInstancesByGroupName(field_value);
-  };
-  instance_handlers_[kInstanceNameField] = [this](const Value& field_value) {
-    return FindInstancesByInstanceName(field_value);
-  };
-}
+namespace instance_db {
 
 bool InstanceDatabase::IsEmpty() const {
   return local_instance_groups_.empty();
 }
 
 template <typename T>
-Result<Set<ConstRef<T>>> InstanceDatabase::Find(
+Result<Set<T>> InstanceDatabase::Find(
     const Query& query,
     const Map<FieldName, ConstHandler<T>>& handler_map) const {
   static_assert(std::is_same<T, LocalInstance>::value ||
@@ -66,80 +37,15 @@ Result<Set<ConstRef<T>>> InstanceDatabase::Find(
   return (itr->second)(value);
 }
 
-template <typename T>
-Result<Set<ConstRef<T>>> InstanceDatabase::Find(
-    const Queries& queries,
-    const Map<FieldName, ConstHandler<T>>& handler_map) const {
-  static_assert(std::is_same<T, LocalInstance>::value ||
-                std::is_same<T, LocalInstanceGroup>::value);
-  if (queries.empty()) {
-    return CF_ERR("Queries must not be empty");
-  }
-  auto first_set = CF_EXPECT(Find<T>(queries[0], handler_map));
-  for (int i = 1; i < queries.size(); i++) {
-    auto subset = CF_EXPECT(Find<T>(queries[i], handler_map));
-    first_set = Intersection(first_set, subset);
-  }
-  return {first_set};
-}
-
-template <typename T>
-Result<ConstRef<T>> InstanceDatabase::FindOne(
-    const Query& query,
-    const Map<FieldName, ConstHandler<T>>& handler_map) const {
-  auto set = CF_EXPECT(Find<T>(query, handler_map));
-  CF_EXPECT_EQ(set.size(), 1, "Only one Instance (Group) is allowed.");
-  return {*set.cbegin()};
-}
-
-template <typename T>
-Result<ConstRef<T>> InstanceDatabase::FindOne(
-    const Queries& queries,
-    const Map<FieldName, ConstHandler<T>>& handler_map) const {
-  auto set = CF_EXPECT(Find<T>(queries, handler_map));
-  CF_EXPECT_EQ(set.size(), 1, "Only one Instance (Group) is allowed.");
-  return {*set.cbegin()};
-}
-
-Result<Set<ConstRef<LocalInstanceGroup>>> InstanceDatabase::FindGroups(
+Result<Set<LocalInstanceGroup>> InstanceDatabase::FindGroups(
     const Query& query) const {
   return Find<LocalInstanceGroup>(query, group_handlers_);
 }
 
-Result<Set<ConstRef<LocalInstanceGroup>>> InstanceDatabase::FindGroups(
-    const Queries& queries) const {
-  return Find<LocalInstanceGroup>(queries, group_handlers_);
-}
-
-Result<Set<ConstRef<LocalInstance>>> InstanceDatabase::FindInstances(
+Result<Set<LocalInstance>> InstanceDatabase::FindInstances(
     const Query& query) const {
   return Find<LocalInstance>(query, instance_handlers_);
 }
 
-Result<Set<ConstRef<LocalInstance>>> InstanceDatabase::FindInstances(
-    const Queries& queries) const {
-  return Find<LocalInstance>(queries, instance_handlers_);
-}
-
-Result<ConstRef<LocalInstanceGroup>> InstanceDatabase::FindGroup(
-    const Query& query) const {
-  return FindOne<LocalInstanceGroup>(query, group_handlers_);
-}
-
-Result<ConstRef<LocalInstanceGroup>> InstanceDatabase::FindGroup(
-    const Queries& queries) const {
-  return FindOne<LocalInstanceGroup>(queries, group_handlers_);
-}
-
-Result<ConstRef<LocalInstance>> InstanceDatabase::FindInstance(
-    const Query& query) const {
-  return FindOne<LocalInstance>(query, instance_handlers_);
-}
-
-Result<ConstRef<LocalInstance>> InstanceDatabase::FindInstance(
-    const Queries& queries) const {
-  return FindOne<LocalInstance>(queries, instance_handlers_);
-}
-
-}  // namespace selector
+}  // namespace instance_db
 }  // namespace cuttlefish
