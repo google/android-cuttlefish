c5da306e22683067f26724e8e1861cbd4dcc3dc3
Robert Tarasov <tutankhamen@google.com>
2022-09-19 22:16:35 -0700

    Partial revert of ag/19786106
    Pushing trout back to drm hwcomposer
    
    Bug: 246500971
    Test: tested with trout_arm64 target on osprey dut
    Change-Id: I69b4da0584e71d24447d49c2677e4bbc4c3ce8c0

--- shared/device.mk
+++ shared/device.mk
@@ -401,8 +401,19 @@ PRODUCT_PACKAGES += \
 #
 # Hardware Composer HAL
 #
+# The device needs to avoid having both hwcomposer2.4 and hwcomposer3
+# services running at the same time so make the user manually enables
+# in order to run with --gpu_mode=drm.
+ifeq ($(TARGET_ENABLE_DRMHWCOMPOSER),true)
+DEVICE_MANIFEST_FILE += \
+    device/google/cuttlefish/shared/config/manifest_android.hardware.graphics.composer@2.4-service.xml
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.4-service \
+    hwcomposer.drm
+else
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer3-service.ranchu
+endif
 
 #
 # Gralloc HAL
