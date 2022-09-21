8dc9239fcacae871a7111bab4fa7413f0ba37380
Steve Kim <kwstephenkim@google.com>
2022-09-20 18:03:51 -0700

    Move ScopeGuard to common/libs/utils
    ScopeGuard is broadly useful, and not cvd-specific. It is
    good to move it to common/libs/utils.
    
    Bug: 247871942
    Test: Run locally
    Change-Id: I5c0a95bc001524144b0dadbee71bfcf4f584a40c

--- common/libs/utils/scope_guard.cpp
+++ common/libs/utils/scope_guard.cpp
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "host/commands/cvd/scope_guard.h"
+#include "common/libs/utils/scope_guard.h"
 
 #include <functional>
 
