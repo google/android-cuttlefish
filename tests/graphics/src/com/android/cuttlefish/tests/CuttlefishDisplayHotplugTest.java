b31af266df7e146767a4d73cdda2c1cbd552fcfb
Jason Macnak <natsu@google.com>
2022-09-21 16:55:35 -0700

    Add CuttlefishDisplayHotplugTest
    ... to verify that connecting and disconnecting displays at runtime
    works as intended.
    
    Bug: b/163864461
    Test: cvd start && atest CuttlefishDisplayHotplugTest
    Change-Id: I590603e9a2b15092d71ee903c9cb65839220afd7

---
+++ tests/graphics/src/com/android/cuttlefish/tests/CuttlefishDisplayHotplugTest.java
@@ -0,0 +1,199 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.cuttlefish.tests;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.android.cuttlefish.tests.utils.CuttlefishHostTest;
+import com.android.tradefed.device.DeviceNotAvailableException;
+import com.android.tradefed.log.LogUtil.CLog;
+import com.android.tradefed.testtype.DeviceJUnit4ClassRunner;
+import com.android.tradefed.testtype.junit4.BaseHostJUnit4Test;
+import com.android.tradefed.util.CommandResult;
+import com.android.tradefed.util.CommandStatus;
+import com.google.auto.value.AutoValue;
+import com.google.common.base.Strings;
+import com.google.common.collect.MapDifference;
+import com.google.common.collect.Maps;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Tests that a Cuttlefish device can interactively connect and disconnect displays.
+ */
+@RunWith(DeviceJUnit4ClassRunner.class)
+public class CuttlefishDisplayHotplugTest extends CuttlefishHostTest {
+
+    private static final long DEFAULT_TIMEOUT_MS = 5000;
+
+    private static final String CVD_BINARY_BASENAME = "cvd";
+
+    private CommandResult runCvdCommand(String... command) throws FileNotFoundException {
+        String cvdBinary = runner.getHostBinaryPath(CVD_BINARY_BASENAME);
+
+        ArrayList<String> fullCommand = new ArrayList<String>(Arrays.asList(command));
+        fullCommand.add(0, cvdBinary);
+        return runner.run(DEFAULT_TIMEOUT_MS, fullCommand.toArray(new String[0]));
+    }
+
+    @AutoValue
+    public static abstract class DisplayInfo {
+        static DisplayInfo create(int id, int width, int height) {
+            return new AutoValue_CuttlefishDisplayHotplugTest_DisplayInfo(id, width, height);
+        }
+
+        abstract int id();
+        abstract int width();
+        abstract int height();
+    }
+
+    /**
+     * Expected input JSON format:
+     *
+     *   {
+     *     "displays" : {
+     *       "<display id>": {
+     *         "mode": {
+     *           "windowed": [
+     *             <width>,
+     *             <height>,
+     *           ],
+     *         },
+     *         ...
+     *       },
+     *       ...
+     *     }
+     *   }
+     *
+     */
+    private Map<Integer, DisplayInfo> parseDisplayInfos(String inputJson) {
+        if (Strings.isNullOrEmpty(inputJson)) {
+            throw new IllegalArgumentException("Null display info json.");
+        }
+
+        Map<Integer, DisplayInfo> displayInfos = new HashMap<Integer, DisplayInfo>();
+
+        try {
+            JSONObject json = new JSONObject(inputJson);
+            JSONObject jsonDisplays = json.getJSONObject("displays");
+            for (Iterator<String> keyIt = jsonDisplays.keys(); keyIt.hasNext(); ) {
+                String displayNumberString = keyIt.next();
+
+                JSONObject jsonDisplay = jsonDisplays.getJSONObject(displayNumberString);
+                JSONObject jsonDisplayMode = jsonDisplay.getJSONObject("mode");
+                JSONArray jsonDisplayModeWindowed = jsonDisplayMode.getJSONArray("windowed");
+
+                int id = Integer.parseInt(displayNumberString);
+                int w = jsonDisplayModeWindowed.getInt(0);
+                int h = jsonDisplayModeWindowed.getInt(1);
+
+                displayInfos.put(id, DisplayInfo.create(id, w, h));
+            }
+        } catch (JSONException e) {
+            throw new IllegalArgumentException("Invalid display info json: " + inputJson, e);
+        }
+
+        return displayInfos;
+    }
+
+    public Map<Integer, DisplayInfo> getDisplays() throws FileNotFoundException {
+        CommandResult listDisplaysResult =
+            runCvdCommand("display",
+                          "list");
+        if (!CommandStatus.SUCCESS.equals(listDisplaysResult.getStatus())) {
+            throw new IllegalStateException(
+                    String.format("Failed to run list displays command:%s\n%s",
+                                  listDisplaysResult.getStdout(),
+                                  listDisplaysResult.getStderr()));
+        }
+        return parseDisplayInfos(listDisplaysResult.getStdout());
+    }
+
+    public void addDisplay(int width, int height) throws FileNotFoundException {
+        CommandResult addDisplayResult =
+            runCvdCommand("display",
+                          "add",
+                          String.format("--width=%d", width),
+                          String.format("--height=%d", height));
+
+        if (!CommandStatus.SUCCESS.equals(addDisplayResult.getStatus())) {
+            throw new IllegalStateException(
+                    String.format("Failed to run add display command:%s\n%s",
+                                  addDisplayResult.getStdout(),
+                                  addDisplayResult.getStderr()));
+        }
+    }
+
+    public void removeDisplay(int displayId) throws FileNotFoundException {
+        CommandResult removeDisplayResult =
+            runCvdCommand("display",
+                          "remove",
+                          String.valueOf(displayId));
+        if (!CommandStatus.SUCCESS.equals(removeDisplayResult.getStatus())) {
+            throw new IllegalStateException(
+                    String.format("Failed to run remove display command:%s\n%s",
+                                  removeDisplayResult.getStdout(),
+                                  removeDisplayResult.getStderr()));
+        }
+    }
+
+    @Test
+    public void testDisplayHotplug() throws Exception {
+        Map<Integer, DisplayInfo> originalDisplays = getDisplays();
+        assertThat(originalDisplays).isNotNull();
+        assertThat(originalDisplays).isNotEmpty();
+
+        addDisplay(600, 500);
+
+        Map<Integer, DisplayInfo> addedDisplays = getDisplays();
+        assertThat(addedDisplays).isNotNull();
+
+        MapDifference<Integer, DisplayInfo> addedDisplaysDiff =
+            Maps.difference(addedDisplays, originalDisplays);
+        assertThat(addedDisplaysDiff.entriesOnlyOnLeft()).hasSize(1);
+        assertThat(addedDisplaysDiff.entriesOnlyOnRight()).isEmpty();
+
+        DisplayInfo addedDisplay = addedDisplaysDiff.entriesOnlyOnLeft().values().iterator().next();
+        assertThat(addedDisplay.width()).isEqualTo(600);
+        assertThat(addedDisplay.height()).isEqualTo(500);
+
+        removeDisplay(addedDisplay.id());
+
+        Map<Integer, DisplayInfo> removedDisplays = getDisplays();
+        assertThat(removedDisplays).isNotNull();
+
+        MapDifference<Integer, DisplayInfo> removedDisplaysDiff =
+            Maps.difference(removedDisplays, originalDisplays);
+        assertThat(removedDisplaysDiff.entriesOnlyOnLeft()).isEmpty();
+        assertThat(removedDisplaysDiff.entriesOnlyOnRight()).isEmpty();
+    }
+}
