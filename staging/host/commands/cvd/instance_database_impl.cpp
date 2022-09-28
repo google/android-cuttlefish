bfd8143bc4d38ec5b4b9c56bb85af6c9bf074a95
Treehugger Robot <treehugger-gerrit@google.com>
2022-09-28 21:19:21 +0000

    [skip ci] Merge "Add Instances with per-instance name" am: ae63e3b39e am: debd9c2c1c am: bddfb320e2 am: 65667da8b0
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2211980
    
    Change-Id: I51d6ac8bb476d827175e472249a79038b9fd7e81
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 473e8b6c20880e217156ba1e41b4234c8c6e5cff

--- staging/host/commands/cvd/instance_database_impl.cpp
+++ staging/host/commands/cvd/instance_database_impl.cpp
@@ -47,20 +47,27 @@ Result<void> InstanceDatabase::AddInstanceGroup(
 }
 
 Result<void> InstanceDatabase::AddInstance(const LocalInstanceGroup& group,
-                                           const unsigned id) {
+                                           const unsigned id,
+                                           const std::string& instance_name) {
+  CF_EXPECT(IsValidInstanceName(instance_name),
+            "instance_name " << instance_name << " is invalid.");
   auto itr = std::find(local_instance_groups_.begin(),
                        local_instance_groups_.end(), group);
-  if (itr == local_instance_groups_.end()) {
-    return CF_ERR("Group at " << group.HomeDir() << " does not exist "
-                              << "inside the Instance Database");
-  }
+  CF_EXPECT(
+      itr != local_instance_groups_.end(),
+      "Adding instances to non-existing group " + group.InternalGroupName());
 
   auto instances = CF_EXPECT(
       FindInstances({selector::kInstanceIdField, std::to_string(id)}));
   if (instances.size() != 0) {
     return CF_ERR("instance id " << id << " is taken");
   }
-  return itr->AddInstance(id);
+
+  auto instances_by_name = CF_EXPECT(group.FindByInstanceName(instance_name));
+  if (!instances_by_name.empty()) {
+    return CF_ERR("instance name " << instance_name << " is taken");
+  }
+  return itr->AddInstance(id, instance_name);
 }
 
 bool InstanceDatabase::RemoveInstanceGroup(const LocalInstanceGroup& group) {
@@ -84,20 +91,30 @@ Result<Set<LocalInstanceGroup>> InstanceDatabase::FindGroupsByHome(
 
 Result<Set<LocalInstance>> InstanceDatabase::FindInstancesById(
     const std::string& id) const {
-  auto all_elements = CollectAllElements<LocalInstance, LocalInstanceGroup>(
-      [](const LocalInstanceGroup& group) { return group.Instances(); },
-      local_instance_groups_);
-
   int parsed_int = 0;
   if (!android::base::ParseInt(id, &parsed_int)) {
     return CF_ERR(id << " cannot be converted to an integer");
   }
-  auto subset = CollectToSet<LocalInstance>(
-      all_elements, [parsed_int](const LocalInstance& instance) {
-        return instance.InstanceId() == parsed_int;
-      });
-  return AtMostOne(subset,
+  auto subset = CollectAllElements<LocalInstance, LocalInstanceGroup>(
+      [parsed_int](const LocalInstanceGroup& group) {
+        return group.FindById(parsed_int);
+      },
+      local_instance_groups_);
+  CF_EXPECT(subset.ok());
+  return AtMostOne(*subset,
                    TooManyInstancesFound(1, selector::kInstanceIdField));
+}
+
+Result<Set<LocalInstance>> InstanceDatabase::FindInstancesByInstanceName(
+    const Value& instance_specific_name) const {
+  auto subset = CollectAllElements<LocalInstance, LocalInstanceGroup>(
+      [&instance_specific_name](const LocalInstanceGroup& group) {
+        return group.FindByInstanceName(instance_specific_name);
+      },
+      local_instance_groups_);
+  CF_EXPECT(subset.ok());
+  return AtMostOne(*subset,
+                   TooManyInstancesFound(1, selector::kInstanceNameField));
 }
 
 }  // namespace instance_db
