75de02f329cccb6d5498561686e4a6e70f4a04bd
Steve Kim <kwstephenkim@google.com>
2022-09-30 18:45:55 -0700

    Re-instated unit tests for InstanceDatabas
    The unit tests requires HOME and ANDROID_HOST_OUT, or their
    equivalent. In the automated testing environments, some of
    the assumptions may not hold: e.g. no ANDROID_HOST_OUT env,
    or no launch_cvd/internal_cvd_start found, etc.
    
    The unit tests now tries to create mock environment enough
    to satisfy InstanceDatabase APIs. However, if it still fails
    due to the environment set up, the unit tests are skipped,
    rather than failed.
    
    Bug: 249617375
    Test: atest -c --host --no-bazel-mode cvd_instance_db_test
    Change-Id: Ie4e61a73fa79424b7ce202c3d5c9576e7873cd95

--- cvd/host/commands/cvd/unittests/selector/instance_database_helper.h
+++ cvd/host/commands/cvd/unittests/selector/instance_database_helper.h
@@ -15,36 +15,87 @@
 
 #pragma once
 
+#include <optional>
+#include <set>
 #include <string>
+#include <unordered_map>
 #include <unordered_set>
 #include <vector>
 
+#include <gtest/gtest.h>
+
+#include "host/commands/cvd/constant_reference.h"
 #include "host/commands/cvd/instance_database.h"
 
 namespace cuttlefish {
 namespace instance_db {
-namespace unittest {
 
-class DbTester {
- public:
-  DbTester();
-  ~DbTester();
-  const std::vector<std::string>& Homes() const { return fake_homes_; }
-  const std::string& HostOutDir() const { return android_host_out_; }
-  InstanceDatabase& Db() { return db_; }
-  std::vector<std::unordered_set<std::string>> InstanceNames(
-      const int n_groups) const;
-  static constexpr const unsigned kNGroups = 10;
+/**
+ * Creates n mock HOME directories, one per group. Also, creates
+ * 1 mock ANDROID_HOST_OUT with a mock launcher file.
+ *
+ * The test suite is to assess InstanceDatabase APIs such as
+ * adding groups, adding instances to the groups, etc. The thing
+ * is that the InstanceDatabase APIs will check if HOME and/or
+ * ANDROID_HOST_OUT are directories. Also, for ANDROID_HOST_OUT,
+ * as a bare minimum validity check, it will see if there is a launcher
+ * file under the bin directory of it.
+ *
+ * Thus, the mock environment should prepare an actual directories with
+ * a mock launcher file(s). In case the test runs/tests in the suite run
+ * in parallel, we give each test run a unique directory, and that's why
+ * all mock homes are under a temp directory created by mkdtemp()
+ *
+ */
+class CvdInstanceDatabaseTest : public ::testing::Test {
+ protected:
+  enum class ErrorCode : std::int32_t {
+    kOk,
+    kFileError,
+    kInstanceDabaseError,
+  };
+
+  struct SetupError {
+    ErrorCode error_code;
+    std::string msg;
+  };
+
+  CvdInstanceDatabaseTest();
+  ~CvdInstanceDatabaseTest();
+
+  bool SetUpOk() const { return error_.error_code == ErrorCode::kOk; }
+  const std::string& Workspace() const { return workspace_dir_; }
+  /*
+   * Returns a valid host binaries directory, which is a prerequisite for
+   * InstanceDatabase APIs.
+   */
+  const std::string& HostBinariesDir() const { return android_binaries_dir_; }
+
+  // Adds InstanceGroups, each by:
+  //    "mkdir" : Workspace() + "/" + base_name, HostBinariesDir()
+  //    db_.AddInstanceGroup()
+  bool AddGroups(const std::unordered_set<std::string>& base_names);
+  struct InstanceInfo {
+    unsigned id;
+    std::string per_instance_name;
+  };
+  bool AddInstances(const ConstRef<LocalInstanceGroup> group,
+                    const std::vector<InstanceInfo>& instances_info);
+  InstanceDatabase& GetDb() { return db_; }
+  const SetupError& Error() const { return error_; }
 
  private:
-  void SetUp();
-  void Clear();
-  std::vector<std::string> fake_homes_;
-  std::string android_host_out_;
-  std::string tmp_dir_;
+  void ClearWorkspace();
+  bool InitWorkspace();
+  bool InitMockAndroidHostOut();
+  // set error_ when there is an error
+  void SetErrorCode(const ErrorCode error_code, const std::string& msg);
+
+  std::string android_binaries_dir_;
+  std::string workspace_dir_;
+  SetupError error_;
   InstanceDatabase db_;
 };
 
-}  // namespace unittest
 }  // namespace instance_db
 }  // namespace cuttlefish
