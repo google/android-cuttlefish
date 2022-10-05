e4a2751553f5d45e11cd5b9b1f51adf2a74305e5
Chad Reynolds <chadreynolds@google.com>
2022-10-05 11:29:57 -0700

    Refactor powerwash_cvd to allow logic reuse
    Goals:
      - Reusing these helpers in other commands/less duplication.
      - Improved readability of the main function.  Obviously subjective,
        but in my opinion this version trades being more dense for better
        focus and easier parsing of the high-level steps of the command.
    
    Changes outside of refactoring, the intention is no functional differences:
    - The Recv call was changed to ReadExactBinary to match the existing
      ReadExactBinary call and allow the code to be factored out.  Given
      that Recv was being called with a flags value of 0, these calls should
      be the same for our puposes.
    - I also swapped out the Send call for WriteAllBinary, for symmetry and
      to simplify the number of args since it was also called with flags =
      0.
    - I removed the notion of separate error exit codes.  A cursory code
      search did not reveal any logic depending on specific values, so I
      believe (outside of potential local use) they were unused.
    
    Bug: 242122912
    Test: run `powerwash_cvd` against a running device
    Change-Id: I56bc1e23eb4041fecea2b33a755b8c80f3b05947

---
+++ host/libs/command_util/util.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "common/libs/fs/shared_fd.h"
+#include "common/libs/utils/result.h"
+#include "host/commands/run_cvd/runner_defs.h"
+#include "host/libs/config/cuttlefish_config.h"
+
+namespace cuttlefish {
+
+Result<LauncherResponse> ReadLauncherResponse(const SharedFD& monitor_socket);
+
+Result<RunnerExitCodes> ReadExitCode(const SharedFD& monitor_socket);
+
+Result<SharedFD> GetLauncherMonitor(const CuttlefishConfig& config,
+                                    const int instance_num,
+                                    const int timeout_seconds);
+
+Result<void> WriteLauncherAction(const SharedFD& monitor_socket,
+                                 const LauncherAction request);
+
+Result<void> WaitForRead(const SharedFD& monitor_socket,
+                         const int timeout_seconds);
+
+}  // namespace cuttlefish
