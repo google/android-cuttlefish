aa79def9d7d84c1537b095165ac31c06bfbb47dc
Treehugger Robot <treehugger-gerrit@google.com>
2022-10-10 23:24:44 +0000

    Merge "Refactor webrtc streamer directory structure" am: 36037d26ac am: c795043872
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2244036
    
    Change-Id: Iaa1b5d8a6acc961f21511de100329f16a3246879
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: a188ef94231c1929ba85f2a443f86e8f870ee2f5

--- staging/host/frontend/webrtc/libdevice/video_sink.h
+++ staging/host/frontend/webrtc/libdevice/video_sink.h
@@ -18,7 +18,7 @@
 
 #include <memory>
 
-#include "host/frontend/webrtc/lib/video_frame_buffer.h"
+#include "host/frontend/webrtc/libdevice/video_frame_buffer.h"
 
 namespace cuttlefish {
 namespace webrtc_streaming {
