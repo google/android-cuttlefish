2384eecbb0be5328483aee658d7377b6823d5695
Mahmoud Elsherif <moelsherif@google.com>
2022-10-13 21:17:44 +0000

    Add unit test for extra_bootconfig_args gflag to cvd_load component.
    Bug: 252923703
    Test: atest -c --host --no-bazel-mode cvd_load_test
    Change-Id: I113cd21708cedb6d001c5aace91975825bfb4034

---
+++ cvd/host/commands/cvd/unittests/parser/instance/boot_configs_test.cc
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2015-2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#include "host/commands/cvd/parser/load_configs_parser.h"
+#include "host/commands/cvd_load/unittest/test_common.h"
+
+namespace cuttlefish {
+TEST(BootFlagsParserTest, ParseTwoInstancesExtraBootConfigFlagEmptyJson) {
+  const char* test_string = R""""(
+{
+    "instances" :
+    [
+        {
+        },
+        {
+        }
+    ]
+}
+  )"""";
+
+  std::vector<std::string> serialized_data;
+  Json::Value json_configs;
+  std::string strjson(test_string);
+
+  EXPECT_TRUE(ParseJsonString(strjson, json_configs));
+  EXPECT_TRUE(ParseCvdConfigs(json_configs, serialized_data));
+  EXPECT_TRUE(FindConfig(serialized_data, R"(--extra_bootconfig_args="","")"));
+}
+
+TEST(BootFlagsParserTest, ParseTwoInstancesExtraBootConfigFlagPartialJson) {
+  const char* test_string = R""""(
+{
+    "instances" :
+    [
+        {
+            "boot": {
+            }
+        },
+        {
+            "boot": {
+                "extra_bootconfig_args": "androidboot.X=Y"
+            }
+        }
+    ]
+}
+  )"""";
+
+  std::vector<std::string> serialized_data;
+  Json::Value json_configs;
+  std::string strjson(test_string);
+
+  EXPECT_TRUE(ParseJsonString(strjson, json_configs));
+  EXPECT_TRUE(ParseCvdConfigs(json_configs, serialized_data));
+  EXPECT_TRUE(FindConfig(serialized_data,
+                         R"(--extra_bootconfig_args="","androidboot.X=Y")"));
+}
+
+TEST(BootFlagsParserTest, ParseTwoInstancesExtraBootConfigFlagFullJson) {
+  const char* test_string = R""""(
+{
+    "instances" :
+    [
+        {
+            "boot": {
+                "extra_bootconfig_args": "androidboot.X=Y"
+            }
+        },
+        {
+            "boot": {
+                "extra_bootconfig_args": "androidboot.X=Z"
+            }
+        }
+    ]
+}
+  )"""";
+
+  std::vector<std::string> serialized_data;
+  Json::Value json_configs;
+  std::string strjson(test_string);
+
+  EXPECT_TRUE(ParseJsonString(strjson, json_configs));
+  EXPECT_TRUE(ParseCvdConfigs(json_configs, serialized_data));
+  EXPECT_TRUE(FindConfig(
+      serialized_data,
+      R"(--extra_bootconfig_args="androidboot.X=Y","androidboot.X=Z")"));
+}
+
+}  // namespace cuttlefish
