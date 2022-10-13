365ddd9a8f943b78f1c30a86d1e9941642b74482
Jason Macnak <natsu@google.com>
2022-10-13 17:19:07 +0000

    Merge "Add a callback to ScreenConnector for display events" am: e801a00d64 am: 7c83765ee5
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2241955
    
    Change-Id: I2d28b6d65ef2654dd1af0a80809c3161acaa84b2
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: df7c3fca562e321cf99023bf1ce37d538292d751

--- staging/host/libs/wayland/wayland_surfaces.cpp
+++ staging/host/libs/wayland/wayland_surfaces.cpp
@@ -28,6 +28,11 @@ void Surfaces::SetFrameCallback(FrameCallback callback) {
   callback_.emplace(std::move(callback));
 }
 
+void Surfaces::SetDisplayEventCallback(DisplayEventCallback callback) {
+  std::unique_lock<std::mutex> lock(callback_mutex_);
+  event_callback_.emplace(std::move(callback));
+}
+
 void Surfaces::HandleSurfaceFrame(std::uint32_t display_number,
                                   std::uint32_t frame_width,
                                   std::uint32_t frame_height,
@@ -37,6 +42,32 @@ void Surfaces::HandleSurfaceFrame(std::uint32_t display_number,
   if (callback_) {
     (callback_.value())(display_number, frame_width, frame_height,
                         frame_stride_bytes, frame_bytes);
+  }
+}
+
+void Surfaces::HandleSurfaceCreated(std::uint32_t display_number,
+                                    std::uint32_t display_width,
+                                    std::uint32_t display_height) {
+  const DisplayEvent event{DisplayCreatedEvent{
+      .display_number = display_number,
+      .display_width = display_width,
+      .display_height = display_height,
+  }};
+
+  std::unique_lock<std::mutex> lock(callback_mutex_);
+  if (event_callback_) {
+    (event_callback_.value())(event);
+  }
+}
+
+void Surfaces::HandleSurfaceDestroyed(std::uint32_t display_number) {
+  const DisplayEvent event{DisplayDestroyedEvent{
+      .display_number = display_number,
+  }};
+
+  std::unique_lock<std::mutex> lock(callback_mutex_);
+  if (event_callback_) {
+    (event_callback_.value())(event);
   }
 }
 
