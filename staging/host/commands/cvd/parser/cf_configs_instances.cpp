f477a248b8dbff2afbd5a86c2a6a89957162c2eb
Mahmoud Elsherif <moelsherif@google.com>
2022-10-19 00:26:50 +0000

    [skip ci] Merge changes from topic "cf-canonical" am: 17cab6f74c am: 745977c60c
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2257965
    
    Change-Id: I2d09b34ce8b9141e3d3bc0e6d967c17099d472a7
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 597aeac9638e5960de7edf7d22908facbc7b16fa

---
+++ staging/host/commands/cvd/parser/cf_configs_instances.cpp
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <android-base/logging.h>
+#include <iostream>
+
+#include "host/commands/cvd/parser/cf_configs_common.h"
+#include "host/commands/cvd/parser/cf_configs_instances.h"
+#include "host/commands/cvd/parser/instance/cf_boot_configs.h"
+#include "host/commands/cvd/parser/instance/cf_vm_configs.h"
+
+namespace cuttlefish {
+
+static std::map<std::string, Json::ValueType> kInstanceKeyMap = {
+    {"vm", Json::ValueType::objectValue},
+    {"boot", Json::ValueType::objectValue},
+    {"disk", Json::ValueType::objectValue},
+    {"graphics", Json::ValueType::objectValue},
+    {"camera", Json::ValueType::objectValue},
+    {"connectivity", Json::ValueType::objectValue},
+    {"audio", Json::ValueType::objectValue},
+    {"streaming", Json::ValueType::objectValue},
+    {"adb", Json::ValueType::objectValue},
+    {"vehicle", Json::ValueType::objectValue},
+    {"location", Json::ValueType::objectValue},
+    {"metrics", Json::ValueType::objectValue}};
+
+Result<bool> ValidateInstancesConfigs(const Json::Value& root) {
+  int num_instances = root.size();
+  for (unsigned int i = 0; i < num_instances; i++) {
+    CF_EXPECT(ValidateTypo(root[i], kInstanceKeyMap), "vm ValidateTypo fail");
+
+    if (root[i].isMember("vm")) {
+      CF_EXPECT(ValidateVmConfigs(root[i]["vm"]), "ValidateVmConfigs fail");
+    }
+
+    if (root[i].isMember("boot")) {
+      CF_EXPECT(ValidateBootConfigs(root[i]["boot"]), "ValidateBootConfigs fail");
+    }
+  }
+
+  return true;
+}
+
+void InitInstancesConfigs(Json::Value& root) {
+  InitVmConfigs(root);
+  InitBootConfigs(root);
+}
+
+void GenerateInstancesConfigs(const Json::Value& root,
+                              std::vector<std::string>& result) {
+  GenerateVmConfigs(root, result);
+  GenerateBootConfigs(root, result);
+}
+
+}  // namespace cuttlefish
---
+++ staging/host/commands/cvd/unittests/parser/instance/boot_configs_test.cc
@@ -0,0 +1,282 @@
+/*
+ * Copyright (C) 2015-2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#include "host/commands/cvd/parser/load_configs_parser.h"
+#include "host/commands/cvd/unittests/parser/test_common.h"
+
+namespace cuttlefish {
+TEST(BootFlagsParserTest, ParseTwoInstancesExtraBootConfigFlagEmptyJson) {
+  const char* test_string = R""""(
+{
+    "instances" :
+    [
+        {
+        },
+        {
+        }
+    ]
+}
+  )"""";
+
+  std::vector<std::string> serialized_data;
+  Json::Value json_configs;
+  std::string strjson(test_string);
+
+  EXPECT_TRUE(ParseJsonString(strjson, json_configs));
+  EXPECT_TRUE(ParseCvdConfigs(json_configs, serialized_data).ok());
+  EXPECT_TRUE(FindConfig(serialized_data, R"(--extra_bootconfig_args=,)"));
+}
+
+TEST(BootFlagsParserTest, ParseTwoInstancesExtraBootConfigFlagPartialJson) {
+  const char* test_string = R""""(
+{
+    "instances" :
+    [
+        {
+            "boot": {
+            }
+        },
+        {
+            "boot": {
+                "extra_bootconfig_args": "androidboot.X=Y"
+            }
+        }
+    ]
+}
+  )"""";
+
+  std::vector<std::string> serialized_data;
+  Json::Value json_configs;
+  std::string strjson(test_string);
+
+  EXPECT_TRUE(ParseJsonString(strjson, json_configs));
+  EXPECT_TRUE(ParseCvdConfigs(json_configs, serialized_data).ok());
+  EXPECT_TRUE(FindConfig(serialized_data,
+                         R"(--extra_bootconfig_args=,androidboot.X=Y)"));
+}
+
+TEST(BootFlagsParserTest, ParseTwoInstancesExtraBootConfigFlagFullJson) {
+  const char* test_string = R""""(
+{
+    "instances" :
+    [
+        {
+            "boot": {
+                "extra_bootconfig_args": "androidboot.X=Y"
+            }
+        },
+        {
+            "boot": {
+                "extra_bootconfig_args": "androidboot.X=Z"
+            }
+        }
+    ]
+}
+  )"""";
+
+  std::vector<std::string> serialized_data;
+  Json::Value json_configs;
+  std::string strjson(test_string);
+
+  EXPECT_TRUE(ParseJsonString(strjson, json_configs));
+  EXPECT_TRUE(ParseCvdConfigs(json_configs, serialized_data).ok());
+  EXPECT_TRUE(FindConfig(
+      serialized_data,
+      R"(--extra_bootconfig_args=androidboot.X=Y,androidboot.X=Z)"));
+}
+
+TEST(BootFlagsParserTest, ParseTwoInstancesSerialNumberFlagEmptyJson) {
+  const char* test_string = R""""(
+{
+    "instances" :
+    [
+        {
+        },
+        {
+        }
+    ]
+}
+  )"""";
+
+  std::vector<std::string> serialized_data;
+  Json::Value json_configs;
+  std::string strjson(test_string);
+
+  EXPECT_TRUE(ParseJsonString(strjson, json_configs));
+  EXPECT_TRUE(ParseCvdConfigs(json_configs, serialized_data).ok());
+  EXPECT_TRUE(
+      FindConfig(serialized_data,
+                 R"(--serial_number=CUTTLEFISHCVD01,CUTTLEFISHCVD01)"));
+}
+
+TEST(BootFlagsParserTest, ParseTwoInstancesSerialNumberFlagPartialJson) {
+  const char* test_string = R""""(
+{
+    "instances" :
+    [
+        {
+            "boot": {
+                "security": {
+                }
+            }
+        },
+        {
+            "boot": {
+                "security": {
+                    "serial_number": "CUTTLEFISHCVD101"
+                }
+            }
+        }
+    ]
+}
+  )"""";
+
+  std::vector<std::string> serialized_data;
+  Json::Value json_configs;
+  std::string strjson(test_string);
+
+  EXPECT_TRUE(ParseJsonString(strjson, json_configs));
+  EXPECT_TRUE(ParseCvdConfigs(json_configs, serialized_data).ok());
+  EXPECT_TRUE(
+      FindConfig(serialized_data,
+                 R"(--serial_number=CUTTLEFISHCVD01,CUTTLEFISHCVD101)"));
+}
+
+TEST(BootFlagsParserTest, ParseTwoInstancesSerialNumberFlagFullJson) {
+  const char* test_string = R""""(
+{
+    "instances" :
+    [
+        {
+            "boot": {
+                "security": {
+                    "serial_number": "CUTTLEFISHCVD101"
+                }
+            }
+        },
+        {
+            "boot": {
+                "security": {
+                    "serial_number": "CUTTLEFISHCVD102"
+                }
+            }
+        }
+    ]
+}
+  )"""";
+
+  std::vector<std::string> serialized_data;
+  Json::Value json_configs;
+  std::string strjson(test_string);
+
+  EXPECT_TRUE(ParseJsonString(strjson, json_configs));
+  EXPECT_TRUE(ParseCvdConfigs(json_configs, serialized_data).ok());
+  EXPECT_TRUE(
+      FindConfig(serialized_data,
+                 R"(--serial_number=CUTTLEFISHCVD101,CUTTLEFISHCVD102)"));
+}
+
+TEST(BootFlagsParserTest, ParseTwoInstancesKernelCmdFlagEmptyJson) {
+  const char* test_string = R""""(
+{
+    "instances" :
+    [
+        {
+        },
+        {
+        }
+    ]
+}
+  )"""";
+
+  std::vector<std::string> serialized_data;
+  Json::Value json_configs;
+  std::string strjson(test_string);
+
+  EXPECT_TRUE(ParseJsonString(strjson, json_configs));
+  EXPECT_TRUE(ParseCvdConfigs(json_configs, serialized_data).ok());
+  EXPECT_TRUE(FindConfig(serialized_data, R"(--extra_kernel_cmdline=,)"));
+}
+
+TEST(BootFlagsParserTest, ParseTwoInstancesKernelCmdFlagPartialJson) {
+  const char* test_string = R""""(
+{
+    "instances" :
+    [
+        {
+            "boot": {
+                "kernel": {
+                }
+            }
+        },
+        {
+            "boot": {
+                "kernel": {
+                    "extra_kernel_cmdline": "androidboot.selinux=permissive"
+                }
+            }
+        }
+    ]
+}
+  )"""";
+
+  std::vector<std::string> serialized_data;
+  Json::Value json_configs;
+  std::string strjson(test_string);
+
+  EXPECT_TRUE(ParseJsonString(strjson, json_configs));
+  EXPECT_TRUE(ParseCvdConfigs(json_configs, serialized_data).ok());
+  EXPECT_TRUE(FindConfig(
+      serialized_data,
+      R"(--extra_kernel_cmdline=,androidboot.selinux=permissive)"));
+}
+
+TEST(BootFlagsParserTest, ParseTwoInstancesKernelCmdFlagFullJson) {
+  const char* test_string = R""""(
+{
+    "instances" :
+    [
+        {
+            "boot": {
+                "kernel": {
+                    "extra_kernel_cmdline": "androidboot.selinux=permissive"
+                }
+            }
+        },
+        {
+            "boot": {
+                "kernel": {
+                    "extra_kernel_cmdline": "lpm_levels.sleep_disabled=1"
+                }
+            }
+        }
+    ]
+}
+  )"""";
+
+  std::vector<std::string> serialized_data;
+  Json::Value json_configs;
+  std::string strjson(test_string);
+
+  EXPECT_TRUE(ParseJsonString(strjson, json_configs));
+  EXPECT_TRUE(ParseCvdConfigs(json_configs, serialized_data).ok());
+  EXPECT_TRUE(FindConfig(
+      serialized_data,
+      R"(--extra_kernel_cmdline=androidboot.selinux=permissive,lpm_levels.sleep_disabled=1)"));
+}
+
+}  // namespace cuttlefish
