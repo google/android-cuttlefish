861236620a77a88317f60c10893494d29d5c94a7
Treehugger Robot <treehugger-gerrit@google.com>
2022-10-24 23:11:27 +0000

    Merge "Enable HCTR2 for filename encryption on Cuttlefish"

--- cvd/host/libs/config/cuttlefish_config.h
+++ cvd/host/libs/config/cuttlefish_config.h
@@ -129,9 +129,6 @@ class CuttlefishConfig {
   void set_secure_hals(const std::set<std::string>& hals);
   std::set<SecureHal> secure_hals() const;
 
-  void set_setupwizard_mode(const std::string& title);
-  std::string setupwizard_mode() const;
-
   void set_enable_bootanimation(const bool enable_bootanimation);
   bool enable_bootanimation() const;
 
@@ -529,6 +526,7 @@ class CuttlefishConfig {
 
     int memory_mb() const;
     int ddr_mem_mb() const;
+    std::string setupwizard_mode() const;
 
     // system image files
     std::string boot_image() const;
@@ -619,6 +617,7 @@ class CuttlefishConfig {
     void set_display_configs(const std::vector<DisplayConfig>& display_configs);
     void set_memory_mb(int memory_mb);
     void set_ddr_mem_mb(int ddr_mem_mb);
+    void set_setupwizard_mode(const std::string& title);
 
     // system image files
     void set_boot_image(const std::string& boot_image);
