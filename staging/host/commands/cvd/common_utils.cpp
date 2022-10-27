e7cb3a93aa036d944aa916082beb02efc14af131
Treehugger Robot <treehugger-gerrit@google.com>
2022-10-27 20:08:23 +0000

    [skip ci] Merge "Make MakeRequest() shared by the server and client" am: 397f2f4002 am: 3725e79494
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2269153
    
    Change-Id: I3a5b31dbfdfe0b86e5006f59c2c97ec12beefd3c
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: b5df4e7fb83fd66bcd886da260fc13678823f7a4

---
+++ staging/host/commands/cvd/common_utils.cpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "host/commands/cvd/common_utils.h"
+
+#include <unistd.h>
+
+#include <memory>
+
+#include <android-base/file.h>
+#include <android-base/logging.h>
+
+namespace cuttlefish {
+
+cvd::Request MakeRequest(const MakeRequestParam& args_and_envs,
+                         cvd::WaitBehavior wait_behavior) {
+  const auto& args = args_and_envs.cmd_args;
+  const auto& env = args_and_envs.env;
+  const auto& selector_args = args_and_envs.selector_args;
+  cvd::Request request;
+  auto command_request = request.mutable_command_request();
+  for (const std::string& arg : args) {
+    command_request->add_args(arg);
+  }
+  auto selector_opts = command_request->mutable_selector_opts();
+  for (const std::string& selector_arg : selector_args) {
+    selector_opts->add_args(selector_arg);
+  }
+
+  for (const std::string& e : env) {
+    auto eq_pos = e.find('=');
+    if (eq_pos == std::string::npos) {
+      LOG(WARNING) << "Environment var in unknown format: " << e;
+      continue;
+    }
+    const auto key = e.substr(0, eq_pos);
+    const auto value = e.substr(eq_pos + 1);
+    (*command_request->mutable_env())[key] = value;
+  }
+  const auto& immutable_env = command_request->env();
+  if (immutable_env.find("ANDROID_HOST_OUT") == immutable_env.end()) {
+    // see b/254418863
+    (*command_request->mutable_env())["ANDROID_HOST_OUT"] =
+        android::base::Dirname(android::base::GetExecutableDirectory());
+  }
+
+  std::unique_ptr<char, void (*)(void*)> cwd(getcwd(nullptr, 0), &free);
+  command_request->set_working_directory(cwd.get());
+  command_request->set_wait_behavior(wait_behavior);
+
+  return request;
+}
+
+}  // namespace cuttlefish
