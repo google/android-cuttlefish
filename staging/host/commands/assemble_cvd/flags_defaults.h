da1b1e268d2f3182e3e9831bdcb850838a04768c
Bill Schilit <schilit@google.com>
2022-10-27 22:23:18 +0000

    [skip ci] Merge "Make netsim the default handler for Bluetooth." am: 5de839cf47 am: 731270344f
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2258299
    
    Change-Id: Ide91a5838d17c5767864efffed0ee77ececedc4a
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: b0cc1c946f5c65ca5d67caf8a7edfcf004b4cdd8

--- staging/host/commands/assemble_cvd/flags_defaults.h
+++ staging/host/commands/assemble_cvd/flags_defaults.h
@@ -131,8 +131,9 @@
 
 // Connectivity default parameters
 #define CF_DEFAULTS_RIL_DNS "8.8.8.8"
+// Default network handler
 #define CF_DEFAULTS_NETSIM false
-#define CF_DEFAULTS_NETSIM_BT false
+#define CF_DEFAULTS_NETSIM_BT true
 
 // Wifi default parameters
 #define CF_DEFAULTS_AP_KERNEL_IMAGE \
