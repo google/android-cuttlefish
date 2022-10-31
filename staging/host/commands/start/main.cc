0b9222e9bd42286a0edbae03bd4a70437a8a6f76
Wei-Chung Hsu <weihsu@google.com>
2022-10-31 06:18:58 +0000

    [skip ci] Vectorized bool flag functions and convert special flag "--flag" "--noflag" to "--flag=true" "--flag=false". am: 8b7a6b22dd am: f8fe855b1c
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2266376
    
    Change-Id: I3e3e33bdeccc031ac4c900c8f57bcd0d862bc395
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 43b3292c1bf03b91952c0409d0014306a60fdde1

--- staging/host/commands/start/main.cc
+++ staging/host/commands/start/main.cc
@@ -13,9 +13,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <fstream>
 #include <iostream>
 #include <sstream>
-#include <fstream>
+#include <unordered_set>
 
 #include <android-base/logging.h>
 #include <android-base/parseint.h>
@@ -184,12 +185,92 @@ bool HostToolsUpdated() {
   return true;
 }
 
+// Hash table for all bool flag names
+// Used to find bool flag and convert "flag"/"noflag" to "--flag=value"
+// This is the solution for vectorize bool flags in gFlags
+std::unordered_set<std::string> kBoolFlags = {};
+
+struct BooleanFlag {
+  bool is_bool_flag;
+  bool bool_flag_value;
+  std::string name;
+};
+BooleanFlag IsBoolArg(const std::string& argument) {
+  // Validate format
+  // we only deal with special bool case: -flag, --flag, -noflag, --noflag
+  // and convert to -flag=true, --flag=true, -flag=false, --flag=false
+  // others not in this format just return false
+  std::string_view name = argument;
+  if (!android::base::ConsumePrefix(&name, "-")) {
+    return {false, false, ""};
+  }
+  android::base::ConsumePrefix(&name, "-");
+  std::size_t found = name.find('=');
+  if (found != std::string::npos) {
+    // found "=", --flag=value case, it doesn't need convert
+    return {false, false, ""};
+  }
+
+  // Validate it is part of the set
+  std::string result_name(name);
+  std::string_view new_name = result_name;
+  if (result_name.length() == 0) {
+    return {false, false, ""};
+  }
+  if (kBoolFlags.find(result_name) != kBoolFlags.end()) {
+    // matched -flag, --flag
+    return {true, true, result_name};
+  } else if (android::base::ConsumePrefix(&new_name, "no")) {
+    // 2nd chance to check -noflag, --noflag
+    result_name = new_name;
+    if (kBoolFlags.find(result_name) != kBoolFlags.end()) {
+      // matched -noflag, --noflag
+      return {true, false, result_name};
+    }
+  }
+  // return status
+  return {false, false, ""};
+}
+
+std::string FormatBoolString(const std::string& name_str, bool value) {
+  std::string new_flag = "--" + name_str;
+  if (value) {
+    new_flag += "=true";
+  } else {
+    new_flag += "=false";
+  }
+  return new_flag;
+}
+
+bool OverrideBoolArg(std::vector<std::string>& args) {
+  bool overrided = false;
+  for (int index = 0; index < args.size(); index++) {
+    const std::string curr_arg = args[index];
+    BooleanFlag value = IsBoolArg(curr_arg);
+    if (value.is_bool_flag) {
+      // Override the value
+      args[index] = FormatBoolString(value.name, value.bool_flag_value);
+      overrided = true;
+    }
+  }
+  return overrided;
+}
+
 } // namespace
 
 int main(int argc, char** argv) {
   ::android::base::InitLogging(argv, android::base::StderrLogger);
 
   FlagForwarder forwarder({kAssemblerBin, kRunnerBin});
+
+  // Used to find bool flag and convert "flag"/"noflag" to "--flag=value"
+  // This is the solution for vectorize bool flags in gFlags
+  std::vector<std::string> args(argv + 1, argv + argc);
+  if (OverrideBoolArg(args)) {
+    for (int i = 1; i < argc; i++) {
+      argv[i] = &args[i-1][0]; // args[] start from 0
+    }
+  }
 
   gflags::ParseCommandLineNonHelpFlags(&argc, &argv, false);
 
