66652fbe0ca187eeb5114cc1125295b08d226856
Jorge E. Moreira <jemoreira@google.com>
2022-11-15 15:43:49 -0800

    Add webrtc connections to api client

--- client.go
+++ client.go
@@ -22,8 +22,12 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"time"
 
 	apiv1 "github.com/google/cloud-android-orchestration/api/v1"
+	wclient "github.com/google/cloud-android-orchestration/pkg/webrtcclient"
+
+	"github.com/pion/webrtc/v3"
 )
 
 type OpTimeoutError string
@@ -44,9 +48,10 @@ type APIClient struct {
 	client  *http.Client
 	baseURL string
 	dumpOut io.Writer
+	errOut  io.Writer
 }
 
-func NewAPIClient(baseURL, proxyURL string, dumpOut io.Writer) (*APIClient, error) {
+func NewAPIClient(baseURL, proxyURL string, dumpOut, errOut io.Writer) (*APIClient, error) {
 	httpClient := &http.Client{}
 	// Handles http proxy
 	if proxyURL != "" {
@@ -60,6 +65,7 @@ func NewAPIClient(baseURL, proxyURL string, dumpOut io.Writer) (*APIClient, erro
 		client:  httpClient,
 		baseURL: baseURL,
 		dumpOut: dumpOut,
+		errOut:  errOut,
 	}, nil
 }
 
@@ -97,6 +103,146 @@ func (c *APIClient) ListHosts() (*apiv1.ListHostsResponse, error) {
 func (c *APIClient) DeleteHost(name string) error {
 	path := "/hosts/" + name
 	return c.doRequest("DELETE", path, nil, nil)
+}
+
+func (c *APIClient) GetInfraConfig(host string) (*apiv1.InfraConfig, error) {
+	var res apiv1.InfraConfig
+	if err := c.doRequest("GET", fmt.Sprintf("/hosts/%s/infra_config", host), nil, &res); err != nil {
+		return nil, err
+	}
+	return &res, nil
+}
+
+func (c *APIClient) ConnectWebRTC(host, device string, observer wclient.Observer) (*wclient.Connection, error) {
+	polledConn, err := c.createPolledConnection(host, device)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create polled connection: %w", err)
+	}
+	infraConfig, err := c.GetInfraConfig(host)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to obtain infra config: %w", err)
+	}
+	signaling := c.initHandling(host, polledConn.ConnId, infraConfig.IceServers)
+	conn, err := wclient.NewConnection(&signaling, observer)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to connect to device over webrtc: %w", err)
+	}
+	return conn, nil
+}
+
+func (c *APIClient) createPolledConnection(host, device string) (*apiv1.NewConnReply, error) {
+	path := fmt.Sprintf("/hosts/%s/connections", host)
+	req := apiv1.NewConnMsg{DeviceId: device}
+	var res apiv1.NewConnReply
+	if err := c.doRequest("POST", path, &req, &res); err != nil {
+		return nil, err
+	}
+	return &res, nil
+}
+
+func (c *APIClient) initHandling(host, connId string, iceServers []apiv1.IceServer) wclient.Signaling {
+	sendCh := make(chan interface{})
+	recvCh := make(chan map[string]interface{})
+
+	// The forwarding goroutine will close this channel and stop when the send
+	// channel is closed, which will cause the polling go routine to close its own
+	// channel and stop as well.
+	stopPollCh := make(chan bool)
+	go c.webRTCPoll(recvCh, host, connId, stopPollCh)
+	go c.webRTCForward(sendCh, host, connId, stopPollCh)
+
+	return wclient.Signaling{
+		SendCh:     sendCh,
+		RecvCh:     recvCh,
+		ICEServers: asWebRTCICEServers(iceServers),
+	}
+}
+
+const (
+	initialPollInterval  = 100 * time.Millisecond
+	maxPollInterval      = 2 * time.Second
+	maxConsecutiveErrors = 10
+)
+
+func (c *APIClient) webRTCPoll(sinkCh chan map[string]interface{}, host, connId string, stopCh chan bool) {
+	start := 0
+	pollInterval := initialPollInterval
+	errCount := 0
+	for {
+		path := fmt.Sprintf("/hosts/%s/connections/%s/messages?start=%d", host, connId, start)
+		var messages []map[string]interface{}
+		if err := c.doRequest("GET", path, nil, &messages); err != nil {
+			fmt.Fprintf(c.errOut, "Error polling messages: %v\n", err)
+			errCount++
+			if errCount >= maxConsecutiveErrors {
+				fmt.Fprintln(c.errOut, "Reached maximum number of consecutive polling errors, exiting")
+				close(sinkCh)
+				return
+			}
+		} else {
+			errCount = 0
+		}
+		if len(messages) > 0 {
+			pollInterval = initialPollInterval
+		} else {
+			pollInterval = 2 * pollInterval
+			if pollInterval > maxPollInterval {
+				pollInterval = maxPollInterval
+			}
+		}
+		for _, message := range messages {
+			if message["message_type"] != "device_msg" {
+				fmt.Fprintf(c.errOut, "unexpected message type: %s\n", message["message_type"])
+				continue
+			}
+			sinkCh <- message["payload"].(map[string]interface{})
+			start++
+		}
+		select {
+		case _, _ = <-stopCh:
+			// The forwarding goroutine has requested a stop
+			close(sinkCh)
+			return
+		case <-time.After(pollInterval):
+			// poll for messages again
+		}
+	}
+}
+
+func (c *APIClient) webRTCForward(srcCh chan interface{}, host, connId string, stopPollCh chan bool) {
+	for {
+		msg, open := <-srcCh
+		if !open {
+			// The webrtc client closed the channel
+			close(stopPollCh)
+			break
+		}
+		forwardMsg := apiv1.ForwardMsg{Payload: msg}
+		path := fmt.Sprintf("/hosts/%s/connections/%s/:forward", host, connId)
+		i := 0
+		for ; i < maxConsecutiveErrors; i++ {
+			if err := c.doRequest("POST", path, &forwardMsg, nil); err != nil {
+				fmt.Fprintf(c.errOut, "Error sending message to device: %v\n", err)
+			} else {
+				break
+			}
+		}
+		if i == maxConsecutiveErrors {
+			fmt.Fprintln(c.errOut, "Reached maximum number of sending errors, exiting")
+			close(stopPollCh)
+			return
+		}
+	}
+}
+
+func asWebRTCICEServers(in []apiv1.IceServer) []webrtc.ICEServer {
+	out := []webrtc.ICEServer{}
+	for _, s := range in {
+		out = append(out, webrtc.ICEServer{
+			URLs: s.URLs,
+		})
+	}
+	return out
 }
 
 // It either populates the passed response payload reference and returns nil
