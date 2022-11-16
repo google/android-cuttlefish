54f4c85201f7660124ababb982de1cd8b5772722
Steve Kim <kwstephenkim@google.com>
2022-11-16 23:03:27 +0000

    [skip ci] Add unit tests for CreationAnalyzer in cvd selector am: 0bbdc069af am: 9d072f05a5
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2299413
    
    Change-Id: Ia81d0831c21e00f228d918e1e318a59f5591f461
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 7ab3da9d108059bfd547b561434dfb549f8bfeec

---
+++ staging/host/commands/cvd/unittests/selector/creation_analyzer_test_helper.h
@@ -0,0 +1,79 @@
+//
+// Copyright (C) 2022 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <optional>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include "host/commands/cvd/selector/creation_analyzer.h"
+
+namespace cuttlefish {
+namespace selector {
+
+using Envs = std::unordered_map<std::string, std::string>;
+using Args = std::vector<std::string>;
+
+struct OutputInfo {
+  std::string home;
+  std::string host_artifacts_path;  ///< e.g. out/host/linux-x86
+  std::string group_name;
+  std::vector<unsigned> instances;
+  std::vector<std::string> args;
+  std::unordered_map<std::string, std::string> envs;
+};
+
+struct Expected {
+  OutputInfo output;
+  bool is_success;
+};
+
+struct InputOutput {
+  // inputs
+  std::string selector_args;
+  std::string cmd_args;
+  std::string home;
+  std::string android_host_out;
+
+  // output
+  Expected expected_output;
+};
+
+class CreationInfoGenTest : public testing::TestWithParam<InputOutput> {
+ protected:
+  CreationInfoGenTest();
+  void Init();
+
+  std::vector<std::string> selector_args_;
+  std::vector<std::string> cmd_args_;
+  std::unordered_map<std::string, std::string> envs_;
+  std::optional<ucred> credential_;
+  OutputInfo expected_output_;
+  bool expected_success_;
+  InstanceLockFileManager instance_lock_file_manager_;
+};
+
+class HomeTest : public CreationInfoGenTest {};
+class HostArtifactsTest : public CreationInfoGenTest {};
+
+}  // namespace selector
+}  // namespace cuttlefish
