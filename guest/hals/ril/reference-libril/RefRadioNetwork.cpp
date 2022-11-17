25fc316d74402e3b14f6d5c90e736dff3ef64eb5
Terry Huang <terrycrhuang@google.com>
2022-11-18 07:04:20 +0800

    [Cuttlefish] Add Location Privacy HAL
    Bug: 244637957
    Test: Build CtsTelephonyTestCases pass
    Change-Id: Ifadccead2ba38979e63bc40b053766a63a8d6017

--- guest/hals/ril/reference-libril/RefRadioNetwork.cpp
+++ guest/hals/ril/reference-libril/RefRadioNetwork.cpp
@@ -80,4 +80,13 @@ ScopedAStatus RefRadioNetwork::setN1ModeEnabled(int32_t serial, bool enable) {
     return ok();
 }
 
+ScopedAStatus RefRadioNetwork::setLocationPrivacySetting(int32_t serial, bool shareLocation) {
+    respond()->setLocationPrivacySettingResponse(responseInfo(serial));
+    return ok();
+}
+
+ScopedAStatus RefRadioNetwork::getLocationPrivacySetting(int32_t serial) {
+    respond()->getLocationPrivacySettingResponse(responseInfo(serial), false);
+    return ok();
+}
 }  // namespace cf::ril
