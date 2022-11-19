78f950392b8c7c5d32e16e8fd20f21905eab64e4
Treehugger Robot <treehugger-gerrit@google.com>
2022-11-19 03:54:41 +0000

    Merge "Support Contains() for std::string and std::string_view" am: 7d478ae711 am: 7c0920d140 am: 2bc5f46bdd
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2303566
    
    Change-Id: Iaf7f50cdf30575a9d2a68d591513ef97e2a4e72c
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- cvd/cuttlefish/common/libs/utils/contains.h
+++ cvd/cuttlefish/common/libs/utils/contains.h
@@ -18,6 +18,8 @@
 
 #include <algorithm>
 #include <iterator>
+#include <string>
+#include <string_view>
 #include <type_traits>
 
 /**
@@ -44,12 +46,28 @@ template <typename Container, typename T>
 struct HasFindImpl<Container, T, CheckFindMethodType<Container, T>>
     : std::true_type {};
 
+template <typename T>
+using RemoveCvref =
+    typename std::remove_const<typename std::remove_reference<T>::type>::type;
+
+template <typename T, typename U>
+using IsSame = typename std::is_same<RemoveCvref<T>, RemoveCvref<U>>;
+
+template <typename T>
+struct IsString : IsSame<std::string, T> {};
+
+template <typename T>
+struct IsStringView : IsSame<std::string_view, T> {};
+
 }  // namespace contains_internal_impl
 
 // TODO(kwstephenkim): Replace these when C++20 starts to be used.
 template <typename Container, typename U,
           typename = std::enable_if_t<
-              contains_internal_impl::HasFindImpl<Container, U>::value, void>>
+              contains_internal_impl::HasFindImpl<Container, U>::value &&
+                  (!contains_internal_impl::IsString<Container>::value &&
+                   !contains_internal_impl::IsStringView<Container>::value),
+              void>>
 constexpr bool Contains(Container&& container, U&& u) {
   // using O(1) or O(lgN) find()
   return container.find(std::forward<U>(u)) != container.end();
@@ -64,6 +82,17 @@ constexpr bool Contains(Container&& container, U&& u) {
   const auto itr =
       std::find(std::begin(container), std::end(container), std::forward<U>(u));
   return itr != std::end(container);
+}
+
+// std::string:: or std::string_view::find() returns index, not iterator
+template <typename T>
+constexpr bool Contains(const std::string& s, T&& t) {
+  return s.find(std::forward<T>(t)) != std::string::npos;
+}
+
+template <typename T>
+constexpr bool Contains(const std::string_view& s, T&& t) {
+  return s.find(std::forward<T>(t)) != std::string_view::npos;
 }
 
 }  // namespace cuttlefish
