fbde1be89698fd9180bf87ad04fa23ef4787f60b
Treehugger Robot <treehugger-gerrit@google.com>
2022-11-19 03:20:38 +0000

    [skip ci] Merge changes Ie01a72ae,I87a4ca7a am: be9835a4eb am: fb67c13e4e
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2301143
    
    Change-Id: Id4a91174dea1b8f6f88e8c677798dffca2456a3f
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 93805e38898cf850538d4d5f925b8e542e91df8a

---
+++ staging/host/commands/cvd/selector/instance_database_types.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "host/commands/cvd/selector/instance_database_types.h"
+
+namespace cuttlefish {
+namespace selector {
+
+Query::Query(const std::string& field_name, const std::string& field_value)
+    : field_name_(field_name), field_value_(field_value) {}
+
+}  // namespace selector
+}  // namespace cuttlefish
