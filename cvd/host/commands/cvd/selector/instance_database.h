a66f47022ac3f5553d0d7bddc7860929c5ce2e94
Steve Kim <kwstephenkim@google.com>
2022-11-22 14:08:56 -0800

    Remove auto-group-name generation from InstanceDatabase
    The logic has been moved to CreationAnalyzer/selector. As it is
    now being used, the alternative logic in InstanceDatabase should
    be removed.
    
    Bug: 258851321
    Test: Run locally
    Change-Id: If5ae54bb3e7649604bf1a11563f36e752c82780a

--- cvd/host/commands/cvd/selector/instance_database.h
+++ cvd/host/commands/cvd/selector/instance_database.h
@@ -25,7 +25,6 @@
 #include "host/commands/cvd/selector/instance_database_types.h"
 #include "host/commands/cvd/selector/instance_group_record.h"
 #include "host/commands/cvd/selector/instance_record.h"
-#include "host/commands/cvd/selector/unique_resource_allocator.h"
 
 namespace cuttlefish {
 namespace selector {
@@ -50,9 +49,6 @@ class InstanceDatabase {
   Result<ConstRef<LocalInstanceGroup>> AddInstanceGroup(
       const std::string& group_name, const std::string& home_dir,
       const std::string& host_artifacts_path);
-  // auto-generate the group_name
-  Result<ConstRef<LocalInstanceGroup>> AddInstanceGroup(
-      const std::string& home_dir, const std::string& host_artifacts_path);
 
   /**
    * Adds instance to the group.
@@ -124,9 +120,6 @@ class InstanceDatabase {
   std::vector<std::unique_ptr<LocalInstanceGroup>> local_instance_groups_;
   Map<FieldName, ConstGroupHandler> group_handlers_;
   Map<FieldName, ConstInstanceHandler> instance_handlers_;
-
-  UniqueResourceAllocator<int> auto_gen_group_name_suffice_;
-  std::unordered_map<std::string, int> auto_gen_group_name_to_suffix_map_;
 };
 
 }  // namespace selector
