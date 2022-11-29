418d325c4e93aa5a22e6a4fe7db2d3de983fa250
Wei-chung Hsu <weihsu@google.com>
2022-11-29 01:09:15 +0000

    [skip ci] Merge "Vectorized flags "enable_audio", "enable_vehicle_hal_grpc_server", "start_gnss_proxy"." am: b95b8ee978 am: 6d96a7ce44
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2316680
    
    Change-Id: Iee5501495f138f875255385dd945e0d5a989e32a
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 870d0478e46f4160ad0d8c988ee2a13b5864ad41

--- staging/host/commands/assemble_cvd/flags.cc
+++ staging/host/commands/assemble_cvd/flags.cc
@@ -275,8 +275,8 @@ DEFINE_string(gem5_debug_flags, CF_DEFAULTS_GEM5_DEBUG_FLAGS,
 DEFINE_vec(restart_subprocesses,
               cuttlefish::BoolToString(CF_DEFAULTS_RESTART_SUBPROCESSES),
               "Restart any crashed host process");
-DEFINE_bool(enable_vehicle_hal_grpc_server,
-            CF_DEFAULTS_ENABLE_VEHICLE_HAL_GRPC_SERVER,
+DEFINE_vec(enable_vehicle_hal_grpc_server,
+            cuttlefish::BoolToString(CF_DEFAULTS_ENABLE_VEHICLE_HAL_GRPC_SERVER),
             "Enables the vehicle HAL "
             "emulation gRPC server on the host");
 DEFINE_vec(bootloader, CF_DEFAULTS_BOOTLOADER, "Bootloader binary path");
@@ -301,7 +301,7 @@ DEFINE_bool(kgdb, CF_DEFAULTS_KGDB,
             "with kgdb/kdb. The kernel must have been built with "
             "kgdb support, and serial console must be enabled.");
 
-DEFINE_bool(start_gnss_proxy, CF_DEFAULTS_START_GNSS_PROXY,
+DEFINE_vec(start_gnss_proxy, cuttlefish::BoolToString(CF_DEFAULTS_START_GNSS_PROXY),
             "Whether to start the gnss proxy.");
 
 DEFINE_vec(gnss_file_path, CF_DEFAULTS_GNSS_FILE_PATH,
@@ -385,7 +385,7 @@ DEFINE_vec(use_sdcard, CF_DEFAULTS_USE_SDCARD?"true":"false",
 DEFINE_bool(protected_vm, CF_DEFAULTS_PROTECTED_VM,
             "Boot in Protected VM mode");
 
-DEFINE_bool(enable_audio, CF_DEFAULTS_ENABLE_AUDIO,
+DEFINE_vec(enable_audio, cuttlefish::BoolToString(CF_DEFAULTS_ENABLE_AUDIO),
             "Whether to play or capture audio");
 
 DEFINE_vec(camera_server_port, std::to_string(CF_DEFAULTS_CAMERA_SERVER_PORT),
@@ -775,11 +775,6 @@ Result<CuttlefishConfig> InitializeCuttlefishConfiguration(
   tmp_config_obj.set_webrtc_enable_adb_websocket(
           FLAGS_webrtc_enable_adb_websocket);
 
-  tmp_config_obj.set_enable_gnss_grpc_proxy(FLAGS_start_gnss_proxy);
-
-  tmp_config_obj.set_enable_vehicle_hal_grpc_server(
-      FLAGS_enable_vehicle_hal_grpc_server);
-
   tmp_config_obj.set_enable_metrics(FLAGS_report_anonymous_usage_stats);
 
   if (!FLAGS_boot_slot.empty()) {
@@ -894,6 +889,12 @@ Result<CuttlefishConfig> InitializeCuttlefishConfiguration(
       FLAGS_modem_simulator_sim_type, instances_size, "modem_simulator_sim_type"));
   std::vector<bool> console_vec = CF_EXPECT(GetFlagBoolValueForInstances(
       FLAGS_console, instances_size, "console"));
+  std::vector<bool> enable_audio_vec = CF_EXPECT(GetFlagBoolValueForInstances(
+      FLAGS_enable_audio, instances_size, "enable_audio"));
+  std::vector<bool> enable_vehicle_hal_grpc_server_vec = CF_EXPECT(GetFlagBoolValueForInstances(
+      FLAGS_enable_vehicle_hal_grpc_server, instances_size, "enable_vehicle_hal_grpc_server"));
+  std::vector<bool> start_gnss_proxy_vec = CF_EXPECT(GetFlagBoolValueForInstances(
+      FLAGS_start_gnss_proxy, instances_size, "start_gnss_proxy"));
 
   // At this time, FLAGS_enable_sandbox comes from SetDefaultFlagsForCrosvm
   std::vector<bool> enable_sandbox_vec = CF_EXPECT(GetFlagBoolValueForInstances(
@@ -959,6 +960,11 @@ Result<CuttlefishConfig> InitializeCuttlefishConfiguration(
     auto const_instance =
         const_cast<const CuttlefishConfig&>(tmp_config_obj).ForInstance(num);
     instance.set_use_allocd(use_allocd_vec[instance_index]);
+    instance.set_enable_audio(enable_audio_vec[instance_index]);
+    instance.set_enable_vehicle_hal_grpc_server(
+      enable_vehicle_hal_grpc_server_vec[instance_index]);
+    instance.set_enable_gnss_grpc_proxy(start_gnss_proxy_vec[instance_index]);
+
     if (use_random_serial_vec[instance_index]) {
       instance.set_serial_number(
           RandomSerialNumber("CFCVD" + std::to_string(num)));
@@ -1312,8 +1318,6 @@ Result<CuttlefishConfig> InitializeCuttlefishConfiguration(
     instance.set_enable_sandbox(enable_sandbox_vec[instance_index]);
     instance_index++;
   }
-
-  tmp_config_obj.set_enable_audio(FLAGS_enable_audio);
 
   DiskImageFlagsVectorization(tmp_config_obj, fetcher_config);
 
