e13984a61a9f4c9ff6ec7f277e8d36544f4a662f
David Drysdale <drysdale@google.com>
2022-11-29 09:39:19 +0000

    Merge changes Ib8daa9cf,I4e6ac2c6 am: b4415ee430 am: a60273e440
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2309566
    
    Change-Id: I6a51f4cf95e3188308b22d6b9d3850925a505c25
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 7e899583186614da090a0b94c8cc22ed6cea7b80

--- staging/host/commands/secure_env/rust/kmr_ta.h
+++ staging/host/commands/secure_env/rust/kmr_ta.h
@@ -8,8 +8,10 @@ extern "C" {
 // - fd_in: file descriptor for incoming serialized request messages
 // - fd_out: file descriptor for outgoing serialized response messages
 // - security_level: security level to advertize; should be one of the integer
-// values from SecurityLevel.aidl
-void kmr_ta_main(int fd_in, int fd_out, int security_level);
+//   values from SecurityLevel.aidl.
+// - trm: pointer to a valid `TpmResourceManager`, which must remain valid
+//   for the entire duration of the function execution.
+void kmr_ta_main(int fd_in, int fd_out, int security_level, void* trm);
 
 #ifdef __cplusplus
 }
