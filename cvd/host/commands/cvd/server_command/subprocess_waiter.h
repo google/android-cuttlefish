044fc9a046feb6f9f82c09de122503f66a6baf1c
Enrico Granata <egranata@google.com>
2022-12-06 23:13:01 +0000

    Merge changes I90566684,I93018920 into tm-qpr-dev am: 4c1c3fe665
    Original change: https://googleplex-android-review.googlesource.com/c/device/google/cuttlefish/+/20642256
    
    Change-Id: I52064a26f7f7a700c9d25793e6fad08bfbeb6a62
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- cvd/host/commands/cvd/server_command/subprocess_waiter.h
+++ cvd/host/commands/cvd/server_command/subprocess_waiter.h
@@ -16,78 +16,30 @@
 
 #pragma once
 
-#include <sys/types.h>
-
+#include <mutex>
 #include <optional>
-#include <string>
-#include <unordered_map>
-#include <vector>
 
-#include "cvd_server.pb.h"
+#include <fruit/fruit.h>
 
+#include "common/libs/utils/result.h"
 #include "common/libs/utils/subprocess.h"
-#include "host/commands/cvd/server_client.h"
 
 namespace cuttlefish {
 namespace cvd_cmd_impl {
 
-// methods shared by CvdCommandHandler and CvdStartCommandHandler
+class SubprocessWaiter {
+ public:
+  INJECT(SubprocessWaiter()) {}
 
-using Envs = std::unordered_map<std::string, std::string>;
-struct CommandInvocationInfo {
-  std::string command;
-  std::string bin;
-  std::string home;
-  std::string host_artifacts_path;
-  uid_t uid;
-  std::vector<std::string> args;
-  Envs envs;
+  Result<void> Setup(Subprocess subprocess);
+  Result<siginfo_t> Wait();
+  Result<void> Interrupt();
+
+ private:
+  std::optional<Subprocess> subprocess_;
+  std::mutex interruptible_;
+  bool interrupted_ = false;
 };
-
-cuttlefish::cvd::Response ResponseFromSiginfo(siginfo_t infop);
-
-std::optional<CommandInvocationInfo> ExtractInfo(
-    const std::map<std::string, std::string>& command_to_binary_map,
-    const RequestWithStdio& request);
-
-struct ConstructCommandParam {
-  const std::string& bin_path;
-  const std::string& home;
-  const std::vector<std::string>& args;
-  const Envs& envs;
-  const std::string& working_dir;
-  const std::string& command_name;
-  SharedFD in;
-  SharedFD out;
-  SharedFD err;
-};
-Result<Command> ConstructCommand(const ConstructCommandParam& cmd_param);
-
-// Constructs a command for cvd whatever --help or --help-related-option
-Result<Command> ConstructCvdHelpCommand(
-    const std::string& bin_file, const Envs& envs,
-    const std::vector<std::string>& subcmd_args,
-    const RequestWithStdio& request);
-
-Envs ConvertProtoMap(
-    const google::protobuf::Map<std::string, std::string>& proto_map);
-
-std::vector<std::string> ConvertProtoArguments(
-    const google::protobuf::RepeatedPtrField<std::string>& proto_args);
-
-// e.g. cvd start --help, cvd stop --help
-bool IsHelpSubcmd(const std::vector<std::string>& args);
-
-/**
- * Calculates absolute path based on the client's environment
- *
- * If the client sent a relative path like "bin/foo", it is relative
- * to the client's working directory, not to the server's.
- * Likewise, if the client sent a path that starts with ~, we should
- * replace ~ with the client user's home, not the server user's.
- */
-Result<std::string> ClientAbsolutePath(const std::string& path, const uid_t uid,
-                                       const std::string& client_pwd);
 
 }  // namespace cvd_cmd_impl
 }  // namespace cuttlefish
