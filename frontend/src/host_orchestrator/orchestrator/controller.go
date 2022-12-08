6e32f51c17c1cca7712e6ebf4b4c0977840f2776
Sergio Andres Rodriguez Orama <sorama@google.com>
2022-12-07 20:32:41 -0500

    [orchestrator] Refactors controller.
    - Centralizes logging message for failed requests.
    - Removes the responsability of writing the http response in the controller handlers.

--- frontend/src/host_orchestrator/orchestrator/controller.go
+++ frontend/src/host_orchestrator/orchestrator/controller.go
@@ -16,6 +16,7 @@ package orchestrator
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 	"time"
 
@@ -33,53 +34,80 @@ type Controller struct {
 }
 
 func (c *Controller) AddRoutes(router *mux.Router) {
-	router.Handle("/cvds", &createCVDHandler{im: c.InstanceManager}).Methods("POST")
-	router.Handle("/cvds", &listCVDsHandler{im: c.InstanceManager}).Methods("GET")
+	router.Handle("/cvds", httpHandler(&createCVDHandler{im: c.InstanceManager})).Methods("POST")
+	router.Handle("/cvds", httpHandler(&listCVDsHandler{im: c.InstanceManager})).Methods("GET")
 	router.PathPrefix("/cvds/{name}/logs").Handler(&getCVDLogsHandler{im: c.InstanceManager}).Methods("GET")
-	router.Handle("/operations/{name}", &getOperationHandler{om: c.OperationManager}).Methods("GET")
+	router.Handle("/operations/{name}", httpHandler(&getOperationHandler{om: c.OperationManager})).Methods("GET")
 	// The expected response of the operation in case of success.  If the original method returns no data on
 	// success, such as `Delete`, response will be empty. If the original method is standard
 	// `Get`/`Create`/`Update`, the response should be the relevant resource encoded in JSON format.
-	router.Handle("/operations/{name}/result", &getOperationResultHandler{om: c.OperationManager}).Methods("GET")
+	router.Handle("/operations/{name}/result",
+		httpHandler(&getOperationResultHandler{om: c.OperationManager})).Methods("GET")
 	// Same as `/operations/{name}/result but waits for the specified operation to be DONE or for the request
 	// to approach the specified deadline, `503 Service Unavailable` error will be returned if the deadline is
 	// reached. Be prepared to retry if the deadline was reached.
 	router.Handle("/operations/{name}/:wait",
-		&waitOperationHandler{c.OperationManager, c.WaitOperationDuration}).Methods("POST")
-	router.Handle("/userartifacts", &createUploadDirectoryHandler{c.UserArtifactsManager}).Methods("POST")
-	router.Handle("/userartifacts", &listUploadDirectoriesHandler{c.UserArtifactsManager}).Methods("GET")
+		httpHandler(&waitOperationHandler{c.OperationManager, c.WaitOperationDuration})).Methods("POST")
+	router.Handle("/userartifacts",
+		httpHandler(&createUploadDirectoryHandler{c.UserArtifactsManager})).Methods("POST")
+	router.Handle("/userartifacts",
+		httpHandler(&listUploadDirectoriesHandler{c.UserArtifactsManager})).Methods("GET")
+}
+
+type handler interface {
+	Handle(r *http.Request) (interface{}, error)
+}
+
+func httpHandler(h handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		res, err := h.Handle(r)
+		if err != nil {
+			log.Printf("request %q failed with error: %v", r.Method+" "+r.URL.Path, err)
+			replyJSONErr(w, err)
+			return
+		}
+		replyJSONOK(w, res)
+	})
+}
+
+func replyJSONOK(w http.ResponseWriter, obj interface{}) error {
+	return replyJSON(w, obj, http.StatusOK)
+}
+
+func replyJSONErr(w http.ResponseWriter, err error) error {
+	appErr, ok := err.(*operator.AppError)
+	if !ok {
+		return replyJSON(w, apiv1.ErrorMsg{Error: "Internal Server Error"}, http.StatusInternalServerError)
+	}
+	return replyJSON(w, appErr.JSONResponse(), appErr.StatusCode)
+}
+
+func replyJSON(w http.ResponseWriter, obj interface{}, statusCode int) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	encoder := json.NewEncoder(w)
+	return encoder.Encode(obj)
 }
 
 type createCVDHandler struct {
 	im InstanceManager
 }
 
-func (h *createCVDHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (h *createCVDHandler) Handle(r *http.Request) (interface{}, error) {
 	var msg apiv1.CreateCVDRequest
 	err := json.NewDecoder(r.Body).Decode(&msg)
 	if err != nil {
-		operator.ReplyJSONErr(w, operator.NewBadRequestError("Malformed JSON in request", err))
-		return
+		return nil, operator.NewBadRequestError("Malformed JSON in request", err)
 	}
-	op, err := h.im.CreateCVD(msg)
-	if err != nil {
-		operator.ReplyJSONErr(w, err)
-		return
-	}
-	operator.ReplyJSONOK(w, op)
+	return h.im.CreateCVD(msg)
 }
 
 type listCVDsHandler struct {
 	im InstanceManager
 }
 
-func (h *listCVDsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	res, err := h.im.ListCVDs()
-	if err != nil {
-		operator.ReplyJSONErr(w, err)
-		return
-	}
-	operator.ReplyJSONOK(w, res)
+func (h *listCVDsHandler) Handle(r *http.Request) (interface{}, error) {
+	return h.im.ListCVDs()
 }
 
 type getCVDLogsHandler struct {
@@ -99,28 +127,27 @@ type getOperationHandler struct {
 	om OperationManager
 }
 
-func (h *getOperationHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (h *getOperationHandler) Handle(r *http.Request) (interface{}, error) {
 	vars := mux.Vars(r)
 	name := vars["name"]
-	if op, err := h.om.Get(name); err != nil {
+	op, err := h.om.Get(name)
+	if err != nil {
 		var resErr error
 		if _, ok := err.(NotFoundOperationError); ok {
 			resErr = operator.NewNotFoundError("Operation not found", err)
 		} else {
 			resErr = operator.NewInternalError("Unexpected get operation error", err)
 		}
-		operator.ReplyJSONErr(w, resErr)
-
-	} else {
-		operator.ReplyJSONOK(w, op)
+		return nil, resErr
 	}
+	return op, nil
 }
 
 type getOperationResultHandler struct {
 	om OperationManager
 }
 
-func (h *getOperationResultHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (h *getOperationResultHandler) Handle(r *http.Request) (interface{}, error) {
 	vars := mux.Vars(r)
 	name := vars["name"]
 	res, err := h.om.GetResult(name)
@@ -133,14 +160,12 @@ func (h *getOperationResultHandler) ServeHTTP(w http.ResponseWriter, r *http.Req
 		} else {
 			resErr = operator.NewInternalError("Unexpected get operation error", err)
 		}
-		operator.ReplyJSONErr(w, resErr)
-		return
+		return nil, resErr
 	}
 	if res.Error != nil {
-		operator.ReplyJSONErr(w, res.Error)
-		return
+		return nil, res.Error
 	}
-	operator.ReplyJSONOK(w, res.Value)
+	return res.Value, nil
 }
 
 type waitOperationHandler struct {
@@ -148,7 +173,7 @@ type waitOperationHandler struct {
 	waitDuration time.Duration
 }
 
-func (h *waitOperationHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (h *waitOperationHandler) Handle(r *http.Request) (interface{}, error) {
 	vars := mux.Vars(r)
 	name := vars["name"]
 	res, err := h.om.Wait(name, h.waitDuration)
@@ -161,39 +186,26 @@ func (h *waitOperationHandler) ServeHTTP(w http.ResponseWriter, r *http.Request)
 		} else {
 			resErr = operator.NewInternalError("Unexpected get operation error", err)
 		}
-		operator.ReplyJSONErr(w, resErr)
-		return
+		return nil, resErr
 	}
 	if res.Error != nil {
-		operator.ReplyJSONErr(w, res.Error)
-		return
+		return nil, res.Error
 	}
-	operator.ReplyJSONOK(w, res.Value)
-
+	return res.Value, nil
 }
 
 type createUploadDirectoryHandler struct {
 	m UserArtifactsManager
 }
 
-func (h *createUploadDirectoryHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	res, err := h.m.NewDir()
-	if err != nil {
-		operator.ReplyJSONErr(w, operator.NewInternalError("Failed to create new directory", err))
-		return
-	}
-	operator.ReplyJSONOK(w, res)
+func (h *createUploadDirectoryHandler) Handle(r *http.Request) (interface{}, error) {
+	return h.m.NewDir()
 }
 
 type listUploadDirectoriesHandler struct {
 	m UserArtifactsManager
 }
 
-func (h *listUploadDirectoriesHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	res, err := h.m.ListDirs()
-	if err != nil {
-		operator.ReplyJSONErr(w, operator.NewInternalError("Failed retrieving upload directories", err))
-		return
-	}
-	operator.ReplyJSONOK(w, res)
+func (h *listUploadDirectoriesHandler) Handle(r *http.Request) (interface{}, error) {
+	return h.m.ListDirs()
 }
