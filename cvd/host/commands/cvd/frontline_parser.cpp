8bed058f3642f24dead0d89dbb70c790b8a9dcc6
Steve Kim <kwstephenkim@google.com>
2022-12-10 21:16:52 -0800

    Implemented the very first parser for cvd cmdline
    The parser will separate the command line arguments into
    program name/path, cvd-specific arguments, sub command,
    and sub command arguments.
    
    Bug: 247648503
    Test: Run locally
    Change-Id: Id8e751cff26845eaa7fa414816cd9acc6a2f6508

--- cvd/host/commands/cvd/frontline_parser.cpp
+++ cvd/host/commands/cvd/frontline_parser.cpp
@@ -17,12 +17,132 @@
 #include "host/commands/cvd/frontline_parser.h"
 
 #include <sstream>
+#include <type_traits>
 #include <vector>
+
+#include <android-base/strings.h>
 
 #include "common/libs/fs/shared_buf.h"
 #include "common/libs/fs/shared_fd.h"
+#include "common/libs/utils/flag_parser.h"
 
 namespace cuttlefish {
+
+struct BoolFlags {
+  std::unordered_set<std::string> selector_flags;
+  std::unordered_set<std::string> cvd_driver_flags;
+};
+
+struct ValueFlags {
+  std::unordered_set<std::string> selector_flags;
+  std::unordered_set<std::string> cvd_driver_flags;
+};
+
+static const BoolFlags bool_flags{
+    .selector_flags = std::unordered_set<std::string>{},
+    .cvd_driver_flags = {"clean", "help"}};
+
+static const ValueFlags value_flags{
+    .selector_flags = {"device_name", "group_name", "name", "instance_name"},
+    .cvd_driver_flags = {}};
+
+static std::unordered_set<std::string> Merge(
+    const std::unordered_set<std::string>& s1,
+    const std::unordered_set<std::string>& s2) {
+  std::unordered_set<std::string> out{s1};
+  out.insert(s2.begin(), s2.end());
+  return out;
+}
+
+Result<std::unique_ptr<FrontlineParser>> FrontlineParser::Parse(
+    CvdClient& client, const cvd_common::Args& all_args,
+    const cvd_common::Envs& envs) {
+  FrontlineParser* frontline_parser =
+      new FrontlineParser(client, all_args, envs);
+  CF_EXPECT(frontline_parser != nullptr,
+            "Memory allocation for FrontlineParser failed.");
+  CF_EXPECT(frontline_parser->Separate());
+  return std::unique_ptr<FrontlineParser>(frontline_parser);
+}
+
+FrontlineParser::FrontlineParser(
+    CvdClient& client, const cvd_common::Args& all_args,
+    const std::unordered_map<std::string, std::string>& envs)
+    : client_(client), all_args_(all_args), envs_{envs} {
+  known_bool_flags_ =
+      Merge(bool_flags.selector_flags, bool_flags.cvd_driver_flags);
+  known_value_flags_ =
+      Merge(value_flags.selector_flags, value_flags.cvd_driver_flags);
+  selector_flags_ =
+      Merge(bool_flags.selector_flags, value_flags.selector_flags);
+}
+
+Result<void> FrontlineParser::Separate() {
+  valid_subcmds_ = CF_EXPECT(ValidSubcmdsList());
+  arguments_separator_ = CF_EXPECT(CallSeparator());
+  auto filtered_output = CF_EXPECT(FilterNonSelectorArgs());
+  clean_ = filtered_output.clean;
+  help_ = filtered_output.help;
+  selector_args_ = std::move(filtered_output.selector_args);
+  return {};
+}
+
+Result<cvd_common::Args> FrontlineParser::ValidSubcmdsList() {
+  auto valid_subcmd_json = CF_EXPECT(ListSubcommands());
+  CF_EXPECT(valid_subcmd_json.isMember("subcmd"),
+            "Server returned the list of subcommands in Json but it is missing "
+                << " \"subcmd\" field");
+  std::string valid_subcmd_string = valid_subcmd_json["subcmd"].asString();
+  auto valid_subcmds = android::base::Tokenize(valid_subcmd_string, ",");
+  cvd_common::Args cvd_client_internal_commands{"kill-server", "server-kill"};
+  std::copy(cvd_client_internal_commands.begin(),
+            cvd_client_internal_commands.end(),
+            std::back_inserter(valid_subcmds));
+  return valid_subcmds;
+}
+
+Result<std::unique_ptr<selector::ArgumentsSeparator>>
+FrontlineParser::CallSeparator() {
+  std::unordered_set<std::string> valid_subcmds{valid_subcmds_.begin(),
+                                                valid_subcmds_.end()};
+  ArgumentsSeparator::FlagsRegistration flag_registration{
+      .known_boolean_flags = known_bool_flags_,
+      .known_value_flags = known_value_flags_,
+      .valid_subcommands = valid_subcmds};
+  auto arguments_separator =
+      CF_EXPECT(ArgumentsSeparator::Parse(flag_registration, all_args_));
+  CF_EXPECT(arguments_separator != nullptr);
+  return arguments_separator;
+}
+
+const std::string& FrontlineParser::ProgPath() const {
+  return arguments_separator_->ProgPath();
+}
+
+std::optional<std::string> FrontlineParser::SubCmd() const {
+  return arguments_separator_->SubCmd();
+}
+
+const cvd_common::Args& FrontlineParser::SubCmdArgs() const {
+  return arguments_separator_->SubCmdArgs();
+}
+
+Result<FrontlineParser::FilterOutput> FrontlineParser::FilterNonSelectorArgs() {
+  auto cvd_args = arguments_separator_->CvdArgs();
+  bool clean = false;
+  bool help = false;
+  std::vector<Flag> gflags;
+  gflags.emplace_back(GflagsCompatFlag("clean", clean));
+  gflags.emplace_back(GflagsCompatFlag("help", help));
+  CF_EXPECT(ParseFlags(gflags, cvd_args));
+  FilterOutput output{
+      .clean = clean, .help = help, .selector_args = std::move(cvd_args)};
+  return output;
+}
+
+const cvd_common::Args& FrontlineParser::SelectorArgs() const {
+  return selector_args_;
+}
 
 Result<Json::Value> FrontlineParser::ListSubcommands() {
   std::vector<std::string> args{"cvd", "cmd-list"};
