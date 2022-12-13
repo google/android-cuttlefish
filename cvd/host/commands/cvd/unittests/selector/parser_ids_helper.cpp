8bc36afc31b1a6cf0a15245bfccfbf981bfe0e0e
Steve Kim <kwstephenkim@google.com>
2022-12-12 18:48:25 -0800

    Removing dependency on old separator in id testing
    Unit tester for Instance ID related, selector flags depended
    on the old arguments separator for convenience. The dependency
    is removed as the old arguments separator is replaced/obsolete.
    
    Bug: 247648503
    Test: atest -c --host --no-bazel-mode cvd_selector_parser_test
    Change-Id: Id21f024dc89c7f78e2703e829c98558ac2691b58

---
+++ cvd/host/commands/cvd/unittests/selector/parser_ids_helper.cpp
@@ -0,0 +1,42 @@
+//
+// Copyright (C) 2022 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "host/commands/cvd/unittests/selector/parser_ids_helper.h"
+
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <android-base/strings.h>
+
+#include "host/libs/config/cuttlefish_config.h"
+
+namespace cuttlefish {
+namespace selector {
+
+InstanceIdTest::InstanceIdTest() {
+  auto cuttlefish_instance = GetParam().cuttlefish_instance;
+  if (cuttlefish_instance) {
+    envs_[kCuttlefishInstanceEnvVarName] = cuttlefish_instance.value();
+  }
+  uid_ = getuid();
+  cmd_args_ = android::base::Tokenize(GetParam().cmd_args, " ");
+  selector_args_ = android::base::Tokenize(GetParam().selector_args, " ");
+  expected_ids_ = GetParam().expected_ids;
+  expected_result_ = GetParam().expected_result;
+  requested_num_instances_ = GetParam().requested_num_instances;
+}
+
+}  // namespace selector
+}  // namespace cuttlefish
---
+++ cvd/host/commands/cvd/unittests/selector/parser_ids_helper.h
@@ -0,0 +1,53 @@
+//
+// Copyright (C) 2022 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <optional>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include "host/commands/cvd/types.h"
+
+namespace cuttlefish {
+namespace selector {
+
+struct InstanceIdTestInput {
+  std::string cmd_args;
+  std::string selector_args;
+  std::optional<std::string> cuttlefish_instance;
+  std::optional<std::vector<unsigned>> expected_ids;
+  unsigned requested_num_instances;
+  bool expected_result;
+};
+
+class InstanceIdTest : public testing::TestWithParam<InstanceIdTestInput> {
+ protected:
+  InstanceIdTest();
+
+  bool expected_result_;
+  unsigned requested_num_instances_;
+  std::optional<std::vector<unsigned>> expected_ids_;
+  uid_t uid_;
+  cvd_common::Args cmd_args_;
+  cvd_common::Args selector_args_;
+  cvd_common::Envs envs_;
+};
+
+}  // namespace selector
+}  // namespace cuttlefish
