a4eca8b118eee2c4a7ec688e0ec80ad20458be4c
Darren Hsu <darrenhsu@google.com>
2022-12-16 15:03:29 +0800

    Identify power stats as known missing
    Bug: 262403575
    Test: hal_implementation_test.Hal#AidlInterfacesImplemented
    Change-Id: Ia336ae33f497ab5f0b498c15fffbde0c873c01c3
    Signed-off-by: Darren Hsu <darrenhsu@google.com>

--- tests/hal/hal_implementation_test.cpp
+++ tests/hal/hal_implementation_test.cpp
@@ -144,7 +144,7 @@ static const std::set<std::string> kAlwaysMissingAidl = {
     "android.media.audio.common.",
     "android.hardware.radio.",
     "android.hardware.uwb.fira_android.",
-    "android.hardware.power.stats.",
+    //"android.hardware.power.stats.",
 
     // android.hardware.camera.device is an interface returned by
     // android.hardware.camera.provider.
@@ -197,6 +197,9 @@ static const std::set<VersionedAidlPackage> kKnownMissingAidl = {
 
     // No implementation on cuttlefish for fastboot AIDL hal
     {"android.hardware.fastboot.", 1},
+
+    // No implementation on cuttlefish for power stats hal
+    {"android.hardware.power.stats.", 2},
 
     // These types are only used in TV.
     {"android.hardware.tv.cec.", 1},
