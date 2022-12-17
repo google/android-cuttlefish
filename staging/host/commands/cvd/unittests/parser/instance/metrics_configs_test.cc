71472c63e8e0dbb9b949fc2983f3ab79ba0c83d6
Mahmoud Elsherif <moelsherif@google.com>
2022-12-17 04:40:36 +0000

    [skip ci] Merge changes Ibef90fbf,Ic024f080,I67028c67 am: 5f57aeb84d am: 184b3d5bbd
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2362680
    
    Change-Id: I1aa377017564dced32da9438b26bf2e6b8e11a0d
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: a91fbb03517c7474be244305ad7f4aeb9885dcd9

---
+++ staging/host/commands/cvd/unittests/parser/instance/metrics_configs_test.cc
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2015-2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#include "host/commands/cvd/parser/load_configs_parser.h"
+#include "host/commands/cvd/unittests/parser/test_common.h"
+
+namespace cuttlefish {
+TEST(MetricsFlagsParserTest, ParseOneInstanceMetricsReportInvalidValue) {
+  const char* test_string = R""""(
+{
+    "instances" :
+    [
+        {
+            "metrics": {
+            }
+        }
+    ]
+}
+  )"""";
+
+  Json::Value json_configs;
+  std::string json_text(test_string);
+
+  EXPECT_TRUE(ParseJsonString(json_text, json_configs))
+      << "Invalid Json string";
+  auto serialized_data = ParseCvdConfigs(json_configs);
+  EXPECT_FALSE(serialized_data.ok()) << serialized_data.error().Trace();
+}
+
+TEST(MetricsFlagsParserTest, ParseOneInstancesMetricsReportFlagEmptyJson) {
+  const char* test_string = R""""(
+{
+    "instances" :
+    [
+        {
+        }
+    ]
+}
+  )"""";
+
+  Json::Value json_configs;
+  std::string json_text(test_string);
+
+  EXPECT_TRUE(ParseJsonString(json_text, json_configs))
+      << "Invalid Json string";
+  auto serialized_data = ParseCvdConfigs(json_configs);
+  EXPECT_TRUE(serialized_data.ok()) << serialized_data.error().Trace();
+  EXPECT_TRUE(
+      FindConfig(*serialized_data, R"(--report_anonymous_usage_stats=n)"))
+      << "report_anonymous_usage_stats flag is missing or wrongly formatted";
+}
+
+TEST(MetricsFlagsParserTest, ParseTwoInstancesMetricsReportFlagEmptyJson) {
+  const char* test_string = R""""(
+{
+    "instances" :
+    [
+        {
+        },
+        {
+        }
+    ]
+}
+  )"""";
+
+  Json::Value json_configs;
+  std::string json_text(test_string);
+
+  EXPECT_TRUE(ParseJsonString(json_text, json_configs))
+      << "Invalid Json string";
+  auto serialized_data = ParseCvdConfigs(json_configs);
+  EXPECT_TRUE(serialized_data.ok()) << serialized_data.error().Trace();
+  EXPECT_TRUE(
+      FindConfig(*serialized_data, R"(--report_anonymous_usage_stats=n)"))
+      << "report_anonymous_usage_stats flag is missing or wrongly formatted";
+}
+
+}  // namespace cuttlefish
