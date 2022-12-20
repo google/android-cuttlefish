c9fd1343722e783f9db766eb9ab7a5a9c38d4612
Dmitrii Merkurev <dimorinny@google.com>
2022-12-20 21:43:54 +0000

    [skip ci] Merge "Proxy 7250 port range to ipv6 ethernet for fastboot" am: 055fa1ccd1 am: 6eaae1581e
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2356763
    
    Change-Id: Iffeeec04628fe217509f3e083535a18f8372c20d
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 92a054b85968245df2fc0c289f69af3e6c73cf44

---
+++ staging/host/libs/config/fastboot/launch.cpp
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "host/libs/config/fastboot/fastboot.h"
+
+#include "host/libs/config/cuttlefish_config.h"
+#include "host/libs/config/command_source.h"
+#include "host/libs/config/known_paths.h"
+
+namespace cuttlefish {
+namespace {
+
+class FastbootProxy : public CommandSource {
+ public:
+  INJECT(FastbootProxy(const CuttlefishConfig::InstanceSpecific& instance))
+      : instance_(instance) {}
+
+  Result<std::vector<Command>> Commands() override {
+    std::vector<Command> commands;
+    const std::string ethernet_host = instance_.ethernet_ipv6() + "%" +
+                                      instance_.ethernet_bridge_name();
+
+    Command tunnel(SocketVsockProxyBinary());
+    tunnel.AddParameter("--server_type=tcp");
+    tunnel.AddParameter("--server_tcp_port=", instance_.fastboot_host_port());
+    tunnel.AddParameter("--client_type=tcp");
+    tunnel.AddParameter("--client_tcp_host=", ethernet_host);
+    tunnel.AddParameter("--client_tcp_port=5554");
+    commands.emplace_back(std::move(tunnel));
+
+    return commands;
+  }
+
+  std::string Name() const override { return "FastbootProxy"; }
+  bool Enabled() const override { return true; }
+
+ private:
+  bool Setup() override { return true; }
+  std::unordered_set<SetupFeature*> Dependencies() const override { return {}; }
+
+  const CuttlefishConfig::InstanceSpecific& instance_;
+};
+
+}  // namespace
+
+fruit::Component<fruit::Required<const CuttlefishConfig::InstanceSpecific>>
+LaunchFastbootComponent() {
+  return fruit::createComponent()
+      .addMultibinding<CommandSource, FastbootProxy>()
+      .addMultibinding<SetupFeature, FastbootProxy>();
+}
+
+}  // namespace cuttlefish
