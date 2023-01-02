6a9fbef2b1a8e01f247ccf9af6e2d733126fba49
Dmitrii Merkurev <dimorinny@google.com>
2023-01-02 14:25:29 +0000

    [skip ci] Merge "Generate grub config as a part of ESP generation" am: 7ed7978583 am: 758a400747
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2371068
    
    Change-Id: Ia62adbbd34c59f99cf5df344078d134dc55442f4
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 693bdc9e4fc1c781309fbb669fcd4c4553ae749b

--- staging/host/libs/config/esp.h
+++ staging/host/libs/config/esp.h
@@ -15,11 +15,27 @@
 
 #pragma once
 
+#include <utility>
+#include <optional>
 #include <string>
 #include <vector>
 
+#include "host/libs/config/cuttlefish_config.h"
+#include "common/libs/utils/environment.h"
+
 namespace cuttlefish {
 
+// For licensing and build reproducibility reasons, pick up the bootloaders
+// from the host Linux distribution (if present) and pack them into the
+// automatically generated ESP. If the user wants their own bootloaders,
+// they can use -esp_image=/path/to/esp.img to override, so we don't need
+// to accommodate customizations of this packing process.
+
+// Currently we only support Debian based distributions, and GRUB is built
+// for those distros to always load grub.cfg from EFI/debian/grub.cfg, and
+// nowhere else. If you want to add support for other distros, make the
+// extra directories below and copy the initial grub.cfg there as well
+//
 // Currently the Cuttlefish bootloaders are built only for x86 (32-bit),
 // ARM (QEMU only, 32-bit) and AArch64 (64-bit), and U-Boot will hard-code
 // these search paths. Install all bootloaders to one of these paths.
@@ -28,44 +44,68 @@ namespace cuttlefish {
 // These are the paths Debian installs the monoliths to. If another distro
 // uses an alternative monolith path, add it to this table
 const std::string kBootSrcPathIA32 = "/usr/lib/grub/i386-efi/monolithic/grubia32.efi";
-const std::string kBootDestPathIA32 = "EFI/BOOT/BOOTIA32.EFI";
+const std::string kBootDestPathIA32 = "/EFI/BOOT/BOOTIA32.EFI";
 
 const std::string kBootSrcPathAA64 = "/usr/lib/grub/arm64-efi/monolithic/grubaa64.efi";
-const std::string kBootDestPathAA64 = "EFI/BOOT/BOOTAA64.EFI";
+const std::string kBootDestPathAA64 = "/EFI/BOOT/BOOTAA64.EFI";
 
-const std::string kModulesDestPath = "EFI/modules";
+const std::string kModulesDestPath = "/EFI/modules";
 const std::string kMultibootModuleSrcPathIA32 = "/usr/lib/grub/i386-efi/multiboot.mod";
 const std::string kMultibootModuleDestPathIA32 = kModulesDestPath + "/multiboot.mod";
 
 const std::string kMultibootModuleSrcPathAA64 = "/usr/lib/grub/arm64-efi/multiboot.mod";
 const std::string kMultibootModuleDestPathAA64 = kModulesDestPath + "/multiboot.mod";
 
-class EspBuilder final {
+const std::string kKernelDestPath = "/vmlinuz";
+const std::string kInitrdDestPath = "/initrd";
+const std::string kZedbootDestPath = "/zedboot.zbi";
+const std::string kMultibootBinDestPath = "/multiboot.bin";
+const std::string kGrubConfigDestPath = "/EFI/debian/grub.cfg";
+
+class LinuxEspBuilder final {
  public:
-  EspBuilder() = delete;
-  EspBuilder(std::string image_path): image_path_(std::move(image_path)) {}
+  LinuxEspBuilder() = delete;
+  LinuxEspBuilder(std::string image_path): image_path_(std::move(image_path)) {}
 
-  EspBuilder& File(std::string from, std::string to, bool required) &;
-  EspBuilder File(std::string from, std::string to, bool required) &&;
-
-  EspBuilder& File(std::string from, std::string to) &;
-  EspBuilder File(std::string from, std::string to) &&;
-
-  EspBuilder& Directory(std::string path) &;
-  EspBuilder Directory(std::string path) &&;
+  LinuxEspBuilder& Argument(std::string key, std::string value) &;
+  LinuxEspBuilder& Argument(std::string value) &;
+  LinuxEspBuilder& Root(std::string root) &;
+  LinuxEspBuilder& Kernel(std::string kernel) &;
+  LinuxEspBuilder& Initrd(std::string initrd) &;
+  LinuxEspBuilder& Architecture(Arch arch) &;
 
   bool Build() const;
 
  private:
-  std::string image_path_;
+  void DumpConfig(std::ostream& o) const;
 
-  struct FileToAdd {
-    std::string from;
-    std::string to;
-    bool required;
-  };
-  std::vector<std::string> directories_;
-  std::vector<FileToAdd> files_;
+  const std::string image_path_;
+  std::vector<std::pair<std::string, std::string>> arguments_;
+  std::vector<std::string> single_arguments_;
+  std::string root_;
+  std::string kernel_;
+  std::string initrd_;
+  std::optional<Arch> arch_;
+};
+
+class FuchsiaEspBuilder {
+ public:
+  FuchsiaEspBuilder() = delete;
+  FuchsiaEspBuilder(std::string image_path): image_path_(std::move(image_path)) {}
+
+  FuchsiaEspBuilder& MultibootBinary(std::string multiboot) &;
+  FuchsiaEspBuilder& Zedboot(std::string zedboot) &;
+  FuchsiaEspBuilder& Architecture(Arch arch) &;
+
+  bool Build() const;
+
+ private:
+  void DumpConfig(std::ostream& o) const;
+
+  const std::string image_path_;
+  std::string multiboot_bin_;
+  std::string zedboot_;
+  std::optional<Arch> arch_;
 };
 
 bool NewfsMsdos(const std::string& data_image, int data_image_mb,
