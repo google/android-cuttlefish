ef5ca50efa57d7ce88c99606b5badb9d201f443f
Steve Kim <kwstephenkim@google.com>
2023-01-12 19:46:59 +0000

    [skip ci] Merge "Removed --name selector option" am: a1e28f80a5 am: 49ca7ee5af
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2378769
    
    Change-Id: Id981cd443b1503b3bee364270828f303f328a682
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 9aedbef219f265d94bbab5085d534241d5e75de8

--- staging/host/commands/cvd/unittests/selector/parser_names_test.cpp
+++ staging/host/commands/cvd/unittests/selector/parser_names_test.cpp
@@ -46,13 +46,13 @@ TEST_P(ValidNamesTest, FieldsNoSubstring) {
 INSTANTIATE_TEST_SUITE_P(
     CvdParser, ValidNamesTest,
     testing::Values(
-        InputOutput{.input = "--name=cf",
+        InputOutput{.input = "--group_name=cf",
                     .expected = ExpectedOutput{.group_name = "cf"}},
-        InputOutput{.input = "--name=cvd,cf",
+        InputOutput{.input = "--instance_name=cvd,cf",
                     .expected = ExpectedOutput{.per_instance_names =
                                                    std::vector<std::string>{
                                                        "cvd", "cf"}}},
-        InputOutput{.input = "--name=cf-09-1,cf-tv-2",
+        InputOutput{.input = "--device_name=cf-09-1,cf-tv-2",
                     .expected = ExpectedOutput{.group_name = "cf",
                                                .per_instance_names =
                                                    std::vector<std::string>{
@@ -72,21 +72,15 @@ INSTANTIATE_TEST_SUITE_P(
             .expected = ExpectedOutput{.group_name = "my_cool",
                                        .per_instance_names =
                                            std::vector<std::string>{"phone"}}},
-        InputOutput{.input = "--group_name=my_cool --instance_name=phone,tv",
+        InputOutput{.input = "--group_name=my_cool --instance_name=phone-1,tv",
                     .expected = ExpectedOutput{.group_name = "my_cool",
                                                .per_instance_names =
                                                    std::vector<std::string>{
-                                                       "phone", "tv"}}},
+                                                       "phone-1", "tv"}}},
         InputOutput{
-            .input = "--group_name=my_cool",
-            .expected =
-                ExpectedOutput{
-                    .group_name = "my_cool",
-                }},
-        InputOutput{
-            .input = "--instance_name=my_cool",
+            .input = "--instance_name=my-cool",
             .expected = ExpectedOutput{
-                .per_instance_names = std::vector<std::string>{"my_cool"}}}));
+                .per_instance_names = std::vector<std::string>{"my-cool"}}}));
 
 TEST_P(InvalidNamesTest, InvalidInputs) {
   const uid_t uid = getuid();
@@ -99,9 +93,8 @@ TEST_P(InvalidNamesTest, InvalidInputs) {
 
 INSTANTIATE_TEST_SUITE_P(
     CvdParser, InvalidNamesTest,
-    testing::Values("--name", "--name=?34", "--device_name=abcd",
-                    "--group_name=3ab", "--name=x --device_name=y",
-                    "--name=x --group_name=cf",
+    testing::Values("--group_name", "--group_name=?34", "--group_name=ab-cd",
+                    "--group_name=3ab", "--group_name=x --device_name=y-z",
                     "--device_name=z --instance_name=p", "--instance_name=*79a",
                     "--device_name=abcd-e,xyz-f", "--device_name=xyz-e,xyz-e"));
 
