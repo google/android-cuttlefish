f4a694914f194cac7b56b0f3957ed7d324c29990
Mahmoud Elsherif <moelsherif@google.com>
2023-01-17 00:15:09 +0000

    Merge changes Ib8a51d42,I49ce66f5,If62c1c2c,I1d3bc12a,I0c610218 am: dc3e0b6ee8 am: efcaaaf595
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2386477
    
    Change-Id: Ic23b22594bd1f40ef02c58897189f8344934cec8
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 6345277f86e6efa93455fb1e7e4fce9db49e36f0

---
+++ staging/host/commands/cvd_load_tester/main.cc
@@ -0,0 +1,53 @@
+
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include <android-base/logging.h>
+#include <gflags/gflags.h>
+
+#include "host/commands/cvd/parser/load_configs_parser.h"
+
+DEFINE_string(config_file_path, "", "config file path for default configs");
+
+namespace cuttlefish {
+int CvdLoadParserMain(int argc, char** argv) {
+  ::android::base::InitLogging(argv, android::base::StderrLogger);
+  google::ParseCommandLineFlags(&argc, &argv, true);
+
+  auto json_configs = cuttlefish::ParseJsonFile(FLAGS_config_file_path);
+  if (!json_configs.ok()) {
+    LOG(INFO) << "parsing input file failed";
+    return 1;
+  }
+
+  auto serialized_data = cuttlefish::ParseCvdConfigs(*json_configs);
+  if (!serialized_data.ok()) {
+    LOG(INFO) << "parsing json configs failed";
+    return 1;
+  }
+  LOG(INFO) << "Parsing succeeded";
+  for (auto& parsed_flag : *serialized_data) {
+    LOG(INFO) << parsed_flag;
+  }
+  return 0;
+}
+}  // namespace cuttlefish
+int main(int argc, char** argv) {
+  return cuttlefish::CvdLoadParserMain(argc, argv);
+}
