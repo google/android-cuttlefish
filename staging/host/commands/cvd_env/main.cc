e97f15f21b0fce6306389c24c578214a3a771957
Treehugger Robot <treehugger-gerrit@google.com>
2023-01-18 06:14:44 +0000

    [skip ci] Merge "Refactor cvd env" am: 2bc42151d2 am: 40fe37208f
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2385894
    
    Change-Id: I231ca98ebf31d85465586d82f3f65b75212ef205
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 9e020708a8e16534ede80790abb3b3b5d19d30e8

--- staging/host/commands/cvd_env/main.cc
+++ staging/host/commands/cvd_env/main.cc
@@ -14,15 +14,18 @@
  * limitations under the License.
  */
 
-#include <android-base/logging.h>
-
-#include <gflags/gflags.h>
+#include <filesystem>
 #include <unordered_map>
 
+#include <android-base/logging.h>
+#include <android-base/strings.h>
+#include <gflags/gflags.h>
 #include <grpcpp/ext/proto_server_reflection_plugin.h>
 #include <grpcpp/grpcpp.h>
 #include <test/cpp/util/grpc_tool.h>
-#include <filesystem>
+
+#include "common/libs/utils/contains.h"
+#include "common/libs/utils/result.h"
 #include "host/libs/config/cuttlefish_config.h"
 
 using grpc::InsecureChannelCredentials;
@@ -72,6 +75,23 @@ std::vector<std::string> GetServiceList(const std::string& server_address) {
   return service_list;
 }
 
+std::vector<std::string> CandidateServices(
+    const std::vector<std::string>& server_address_list,
+    const std::string& service_name) {
+  std::vector<std::string> candidates;
+
+  for (const auto& server_address : server_address_list) {
+    for (auto& s : GetServiceList(server_address)) {
+      if (android::base::EndsWith(s, service_name)) {
+        candidates.emplace_back(server_address);
+        break;
+      }
+    }
+  }
+
+  return candidates;
+}
+
 std::string CallMethod(const std::string& server_address,
                        const std::string& service_name,
                        const std::string& method_name,
@@ -89,82 +109,46 @@ std::string CallMethod(const std::string& server_address,
   return output_stream.str();
 }
 
-bool HandleLsCmd(const std::vector<std::string>& server_address_list,
-                 const std::vector<std::string>& args) {
+Result<void> HandleLsCmd(const std::vector<std::string>& server_address_list,
+                         const std::vector<std::string>& args) {
   // TODO(b/264201498)
   LOG(INFO) << "TODO(b/264201498)";
-  return true;
+  return {};
 }
 
-bool HandleTypeCmd(const std::vector<std::string>& server_address_list,
-                   const std::vector<std::string>& args) {
+Result<void> HandleTypeCmd(const std::vector<std::string>& server_address_list,
+                           const std::vector<std::string>& args) {
   // TODO(b/264201498)
   LOG(INFO) << "TODO(b/264201498)";
-  return true;
+  return {};
 }
 
-bool HandleCallCmd(const std::vector<std::string>& server_address_list,
-                   const std::vector<std::string>& args) {
-  if (args.size() < 3) {
-    LOG(ERROR) << "need to specify a service name, a method name, and "
-                  "text-formatted proto";
-    return false;
-  }
+Result<void> HandleCallCmd(const std::vector<std::string>& server_address_list,
+                           const std::vector<std::string>& args) {
+  CF_EXPECT(args.size() >= 3,
+            "need to specify a service name, a method name, and text-formatted "
+            "proto");
+
   const auto& service_name = args[0];
   const auto& method_name = args[1];
+  // TODO(b/265384449): support more input options.
   const auto& proto_text_format = args[2];
-  std::string candidate;
-  bool found = false;
-  std::unordered_map<std::string, std::vector<std::string>> address_service_map;
+  const auto& candidates = CandidateServices(server_address_list, service_name);
 
-  for (const auto& server_address : server_address_list) {
-    address_service_map[server_address] = GetServiceList(server_address);
-  }
-  for (const auto& server_address : server_address_list) {
-    const auto& service_list = address_service_map[server_address];
-    // exact match
-    if (std::find(service_list.begin(), service_list.end(), service_name) ==
-        service_list.end()) {
-      continue;
-    }
-    candidate = server_address;
-    found = true;
-  }
-  if (!found) {
-    for (const auto& server_address : server_address_list) {
-      const auto& service_list = address_service_map[server_address];
-      for (auto& s : service_list) {
-        // partial match
-        if (s.find(service_name) != std::string::npos) {
-          candidate = server_address;
-          found = true;
-          break;
-        }
-      }
+  CF_EXPECT(candidates.size() != 0, service_name + "." + method_name + "(" +
+                                        proto_text_format + ") is not found");
+  CF_EXPECT(candidates.size() < 2, service_name + " is ambiguous.");
 
-      if (found) {
-        break;
-      }
-    }
-  }
-  if (found) {
-    LOG(INFO) << CallMethod(candidate, service_name, method_name,
-                            proto_text_format);
-  } else {
-    LOG(ERROR) << service_name << "." << method_name << "(" << proto_text_format
-               << ") is not found";
-  }
-  return found;
+  std::cout << CallMethod(candidates[0], service_name, method_name,
+                          proto_text_format);
+  return {};
 }
 
-int CvdEnvMain(int argc, char** argv) {
+Result<void> CvdEnvMain(int argc, char** argv) {
   ::android::base::InitLogging(argv, android::base::StderrLogger);
   FLAGS_call_creds = "none";
+  CF_EXPECT(argc >= 3, " need to specify a receiver and a command");
 
-  if (argc < 3) {
-    LOG(ERROR) << "need to specify a receiver and a command";
-    return -1;
-  }
   const auto& receiver = argv[1];
   const auto& cmd = argv[2];
   std::vector<std::string> args;
@@ -172,8 +156,8 @@ int CvdEnvMain(int argc, char** argv) {
     args.push_back(argv[i]);
   }
 
-  const auto& config = CuttlefishConfig::Get();
-  CHECK(config) << "Unable to find the config";
+  const auto* config = CuttlefishConfig::Get();
+  CF_EXPECT(config != nullptr, "Unable to find the config");
   std::vector<std::string> server_address_list;
   const auto& instances = config->Instances();
   auto receiver_instance = std::find_if(
@@ -181,11 +165,9 @@ int CvdEnvMain(int argc, char** argv) {
         return receiver == instance.instance_name();
       });
 
-  if (receiver_instance == std::end(instances)) {
-    LOG(ERROR) << "there is no instance of which name is " << receiver
-               << ". please check instance name by cvd fleet";
-    return -1;
-  }
+  CF_EXPECT(receiver_instance != std::end(instances),
+            "there is no instance of which name is "
+                << receiver << ". please check instance name by cvd fleet");
 
   for (const auto& entry : std::filesystem::directory_iterator(
            receiver_instance->grpc_socket_path())) {
@@ -193,27 +175,27 @@ int CvdEnvMain(int argc, char** argv) {
     server_address_list.emplace_back("unix:" + entry.path().string());
   }
 
-  auto commandMap =
-      std::unordered_map<std::string,
-                         std::function<bool(const std::vector<std::string>&,
-                                            const std::vector<std::string>&)>>{{
+  auto command_map =
+      std::unordered_map<std::string, std::function<Result<void>(
+                                          const std::vector<std::string>&,
+                                          const std::vector<std::string>&)>>{{
           {"call", HandleCallCmd},
           {"ls", HandleLsCmd},
           {"type", HandleTypeCmd},
       }};
 
-  if (commandMap.count(cmd) == 0) {
-    LOG(ERROR) << cmd << " isn't supported";
-    return -1;
-  }
-  if (!commandMap[cmd](server_address_list, args)) {
-    LOG(ERROR) << cmd << " fails";
-    return -1;
-  }
-  return 0;
+  CF_EXPECT(Contains(command_map, cmd), cmd << " isn't supported");
+
+  CF_EXPECT(command_map[cmd](server_address_list, args));
+
+  return {};
 }
 
 }  // namespace
 }  // namespace cuttlefish
 
-int main(int argc, char** argv) { return cuttlefish::CvdEnvMain(argc, argv); }
+int main(int argc, char** argv) {
+  const auto& ret = cuttlefish::CvdEnvMain(argc, argv);
+  CHECK(ret.ok()) << ret.error().Message();
+  return 0;
+}
