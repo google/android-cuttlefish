982b691a11fd7bc38539208b45ad15476f318dc0
Mahmoud Elsherif <moelsherif@google.com>
2023-01-20 05:53:08 +0000

    Merge changes I5c29bad4,I84552bf7,I2b6729a1 am: 8548dcf8d9 am: 12f5b95a95 am: d282838f87
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2395837
    
    Change-Id: I4fd805aa1a8c953270a5163cd5b61428055e51c9
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- cvd/cuttlefish/host/commands/cvd/parser/selector_parser.h
+++ cvd/cuttlefish/host/commands/cvd/parser/selector_parser.h
@@ -15,17 +15,11 @@
  */
 
 #pragma once
-
-#include <string>
-#include <vector>
-
 #include <json/json.h>
-
 #include "common/libs/utils/result.h"
 
 namespace cuttlefish {
 
-Result<std::vector<std::string>> ParseSelectorConfigs(Json::Value& root);
+Result<std::vector<std::string>> ParseLaunchCvdConfigs(Json::Value& root);
 
 };  // namespace cuttlefish
-
