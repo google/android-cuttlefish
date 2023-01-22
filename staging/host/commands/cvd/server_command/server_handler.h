124c6639e7fec4373717d6dd77ae91a975ceeb46
Steve Kim <kwstephenkim@google.com>
2023-01-22 20:23:54 +0000

    [skip ci] Merge changes I47be9b3b,I04c1324a,I80ec22a3,Ieb5c0cf2,If9b7e13b, ... am: 1ba0bc3516 am: edf5b20ff8
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2397803
    
    Change-Id: I2107c66c658b8fdb76a60181f721c999eff38a55
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: fd05f9e43f8c8f46c7ea1415a61b50f03b6e8e41

---
+++ staging/host/commands/cvd/server_command/server_handler.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "common/libs/utils/result.h"
+#include "host/commands/cvd/server_client.h"
+#include "host/commands/cvd/types.h"
+
+namespace cuttlefish {
+
+class CvdServerHandler {
+ public:
+  virtual ~CvdServerHandler() = default;
+
+  virtual Result<bool> CanHandle(const RequestWithStdio&) const = 0;
+  virtual Result<cvd::Response> Handle(const RequestWithStdio&) = 0;
+  virtual Result<void> Interrupt() = 0;
+  // returns the list of subcommand it can handle
+  virtual cvd_common::Args CmdList() const = 0;
+};
+
+}  // namespace cuttlefish
