3b2a1fc4d7fe003860e5e592693027366b8058d8
Steve Kim <kwstephenkim@google.com>
2023-01-23 20:57:15 +0000

    Merge changes I66df262b,I0e0bb5e3,I9a4ea33f am: 3d50d436bb am: 7ee0d4e98a am: 2cb91d80fa
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2397807
    
    Change-Id: I4c5a7dfd9078090ace8b57c92099a0c2ff03db8e
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- cvd/cuttlefish/host/commands/cvd/server_command/components.h
+++ cvd/cuttlefish/host/commands/cvd/server_command/components.h
@@ -21,13 +21,15 @@
 #include "host/commands/cvd/command_sequence.h"
 #include "host/commands/cvd/instance_manager.h"
 #include "host/commands/cvd/server.h"
+#include "host/commands/cvd/server_command/subprocess_waiter.h"
 #include "host/libs/web/build_api.h"
 
 namespace cuttlefish {
 
 fruit::Component<fruit::Required<CommandSequenceExecutor>> CvdHelpComponent();
 
-fruit::Component<fruit::Required<InstanceManager>> cvdCommandComponent();
+fruit::Component<fruit::Required<InstanceManager, SubprocessWaiter>>
+cvdCommandComponent();
 
 fruit::Component<fruit::Required<BuildApi, CvdServer, InstanceManager>>
 CvdRestartComponent();
