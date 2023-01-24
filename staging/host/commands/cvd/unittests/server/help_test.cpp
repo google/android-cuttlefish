37ba3dd6443096659de2ca63c5c9ad7dece756fa
Treehugger Robot <treehugger-gerrit@google.com>
2023-01-24 00:18:45 +0000

    [skip ci] Merge "Added various cvd help test to cvd binary test suites" am: 707b992d1b am: 870b7e087e
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2397808
    
    Change-Id: I09d24d6d410d441fd76731f00e9e7ab56f06015c
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: f97c2dddf5da107e80dc596a06ed2c3dc72c72be

--- staging/host/commands/cvd/unittests/server/help_test.cpp
+++ staging/host/commands/cvd/unittests/server/help_test.cpp
@@ -14,6 +14,7 @@
 // limitations under the License.
 
 #include <string>
+#include <vector>
 
 #include <gtest/gtest.h>
 
@@ -25,6 +26,16 @@ namespace cuttlefish {
 namespace selector {
 namespace {
 
+bool ContainsAll(const std::string& stream,
+                 const std::vector<std::string>& tokens) {
+  for (const auto& token : tokens) {
+    if (!Contains(stream, token)) {
+      return false;
+    }
+  }
+  return true;
+}
+
 /*
  * Sees if this might be cvd --help output.
  *
@@ -32,8 +43,20 @@ namespace {
  */
 bool MaybeCvdHelp(const CmdResult& result) {
   const auto& stdout = result.Stdout();
-  return Contains(stdout, "help") && Contains(stdout, "start") &&
-         Contains(stdout, "stop") && Contains(stdout, "fleet");
+  return ContainsAll(stdout, {"help", "start", "stop", "fleet"});
+}
+
+bool MaybeCvdStop(const CmdResult& result) {
+  const auto& stderr = result.Stderr();
+  const auto& stdout = result.Stdout();
+  return Contains(stderr, "cvd_internal_stop") ||
+         Contains(stdout, "cvd_internal_stop") ||
+         Contains(stderr, "stop_cvd") || Contains(stdout, "stop_cvd");
+}
+
+bool MaybeCvdStart(const CmdResult& result) {
+  const auto& stdout = result.Stdout();
+  return ContainsAll(stdout, {"vhost", "modem", "daemon", "adb"});
 }
 
 }  // namespace
@@ -49,6 +72,70 @@ TEST(CvdDriver, CvdHelp) {
   ASSERT_TRUE(MaybeCvdHelp(cmd_help));
   ASSERT_TRUE(cmd_dash_help.Success()) << cmd_dash_help.Stderr();
   ASSERT_TRUE(MaybeCvdHelp(cmd_dash_help));
+
+  // clean up for the next test
+  CmdRunner::Run("cvd kill-server", envs);
+}
+
+TEST(CvdDriver, CvdOnly) {
+  cvd_common::Envs envs;
+  CmdRunner::Run("cvd kill-server", envs);
+
+  auto cmd_help = CmdRunner::Run("cvd help", envs);
+  auto cmd_only = CmdRunner::Run("cvd", envs);
+
+  ASSERT_TRUE(cmd_help.Success()) << cmd_help.Stderr();
+  ASSERT_TRUE(cmd_only.Success()) << cmd_only.Stderr();
+  ASSERT_EQ(cmd_help.Stdout(), cmd_only.Stdout());
+
+  // clean up for the next test
+  CmdRunner::Run("cvd kill-server", envs);
+}
+
+// this test is expected to fail. included proactively.
+TEST(CvdDriver, CvdHelpWrong) {
+  cvd_common::Envs envs;
+  CmdRunner::Run("cvd kill-server", envs);
+
+  auto cmd_help_ref = CmdRunner::Run("cvd help", envs);
+  auto cmd_help_wrong = CmdRunner::Run("cvd help not_exist", envs);
+
+  EXPECT_TRUE(cmd_help_ref.Success()) << cmd_help_ref.Stderr();
+  EXPECT_TRUE(cmd_help_wrong.Success()) << cmd_help_wrong.Stderr();
+  EXPECT_EQ(cmd_help_ref.Stdout(), cmd_help_wrong.Stdout());
+
+  // clean up for the next test
+  CmdRunner::Run("cvd kill-server", envs);
+}
+
+TEST(CvdSubtool, CvdStopHelp) {
+  cvd_common::Envs envs;
+  CmdRunner::Run("cvd kill-server", envs);
+
+  auto cmd_stop_help = CmdRunner::Run("cvd help stop", envs);
+
+  ASSERT_TRUE(cmd_stop_help.Success()) << cmd_stop_help.Stderr();
+  ASSERT_TRUE(MaybeCvdStop(cmd_stop_help))
+      << "stderr: " << cmd_stop_help.Stderr()
+      << "stdout: " << cmd_stop_help.Stdout();
+
+  // clean up for the next test
+  CmdRunner::Run("cvd kill-server", envs);
+}
+
+TEST(CvdSubtool, CvdStartHelp) {
+  cvd_common::Envs envs;
+  CmdRunner::Run("cvd kill-server", envs);
+
+  auto cmd_start_help = CmdRunner::Run("cvd help start", envs);
+
+  ASSERT_TRUE(cmd_start_help.Success()) << cmd_start_help.Stderr();
+  ASSERT_TRUE(MaybeCvdStart(cmd_start_help))
+      << "stderr: " << cmd_start_help.Stderr()
+      << "stdout: " << cmd_start_help.Stdout();
+
+  // clean up for the next test
+  CmdRunner::Run("cvd kill-server", envs);
 }
 
 }  // namespace selector
