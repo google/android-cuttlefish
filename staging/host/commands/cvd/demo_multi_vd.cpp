f76b574eb7c1444dbf4b6d99db3f72cb590121f9
Treehugger Robot <treehugger-gerrit@google.com>
2023-01-26 04:02:32 +0000

    Merge "Moved SerialPreset command handler to handlers' directory" am: 74180f456d am: eb81ff65de
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2398619
    
    Change-Id: I405f7d7cabacab867558d88ee52802f81c9efef0
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 95d4bfb5342cfb9f56c19eebf23de100b3513cef

--- staging/host/commands/cvd/demo_multi_vd.cpp
+++ staging/host/commands/cvd/demo_multi_vd.cpp
@@ -31,6 +31,7 @@
 #include "host/commands/cvd/command_sequence.h"
 #include "host/commands/cvd/instance_lock.h"
 #include "host/commands/cvd/server_client.h"
+#include "host/commands/cvd/server_command/serial_preset.h"
 #include "host/commands/cvd/server_command/utils.h"
 #include "host/commands/cvd/types.h"
 
@@ -398,88 +399,11 @@ class SerialLaunchCommand : public CvdServerHandler {
   bool interrupted_ = false;
 };
 
-class SerialPreset : public CvdServerHandler {
- public:
-  INJECT(SerialPreset(CommandSequenceExecutor& executor))
-      : executor_(executor) {}
-  ~SerialPreset() = default;
-
-  Result<bool> CanHandle(const RequestWithStdio& request) const override {
-    auto invocation = ParseInvocation(request.Message());
-    return invocation.command == "experimental" &&
-           invocation.arguments.size() >= 1 &&
-           Presets().count(invocation.arguments[0]) > 0;
-  }
-
-  Result<cvd::Response> Handle(const RequestWithStdio& request) override {
-    std::unique_lock interrupt_lock(interrupt_mutex_);
-    if (interrupted_) {
-      return CF_ERR("Interrupted");
-    }
-    CF_EXPECT(CF_EXPECT(CanHandle(request)));
-
-    auto invocation = ParseInvocation(request.Message());
-    CF_EXPECT(invocation.arguments.size() >= 1);
-    const auto& presets = Presets();
-    auto devices = presets.find(invocation.arguments[0]);
-    CF_EXPECT(devices != presets.end(), "could not find preset");
-
-    cvd::Request inner_req_proto = request.Message();
-    auto& cmd = *inner_req_proto.mutable_command_request();
-    cmd.clear_args();
-    cmd.add_args("cvd");
-    cmd.add_args("experimental");
-    cmd.add_args("serial_launch");
-    for (const auto& device : devices->second) {
-      cmd.add_args("--device=" + device);
-    }
-    for (int i = 1; i < invocation.arguments.size(); i++) {
-      cmd.add_args(invocation.arguments[i]);
-    }
-
-    RequestWithStdio inner_request(request.Client(), std::move(inner_req_proto),
-                                   request.FileDescriptors(),
-                                   request.Credentials());
-
-    CF_EXPECT(executor_.Execute({std::move(inner_request)}, request.Err()));
-    interrupt_lock.unlock();
-
-    cvd::Response response;
-    response.mutable_command_response();
-    return response;
-  }
-
-  Result<void> Interrupt() override {
-    std::scoped_lock interrupt_lock(interrupt_mutex_);
-    interrupted_ = true;
-    CF_EXPECT(executor_.Interrupt());
-    return {};
-  }
-
-  cvd_common::Args CmdList() const override { return {"experimental"}; }
-
- private:
-  CommandSequenceExecutor& executor_;
-
-  static std::unordered_map<std::string, std::vector<std::string>> Presets() {
-    return {
-        {"create_phone_tablet",
-         {"git_master/cf_x86_64_phone-userdebug",
-          "git_master/cf_x86_64_tablet-userdebug"}},
-        {"create_phone_wear",
-         {"git_master/cf_x86_64_phone-userdebug", "git_master/cf_gwear_x86"}},
-    };
-  }
-
-  std::mutex interrupt_mutex_;
-  bool interrupted_ = false;
-};
-
 fruit::Component<fruit::Required<CommandSequenceExecutor>>
 DemoMultiVdComponent() {
   return fruit::createComponent()
       .addMultibinding<CvdServerHandler, SerialLaunchCommand>()
-      .addMultibinding<CvdServerHandler, SerialPreset>();
+      .install(cvdSerialPresetComponent);
 }
 
 }  // namespace cuttlefish
