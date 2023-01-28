94a084e476dea11e7304526906bc3f8329a8eed2
David Drysdale <drysdale@google.com>
2023-01-28 09:16:11 +0000

    [skip ci] Merge "KeyMint: use same epoch as Gatekeeper" am: afa64c0c11 am: e218098bc4
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2405813
    
    Change-Id: Ifb1a8d5a77d5b5c91c51d0f6a20d0e452c860d7d
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 906a3bfd38263f2b8de5c04d4dd705e1b33ee126

--- staging/host/commands/secure_env/rust/clock.rs
+++ staging/host/commands/secure_env/rust/clock.rs
@@ -15,28 +15,22 @@
 
 //! Monotonic clock implementation.
 
-use core::convert::TryInto;
 use kmr_common::crypto;
-// The normal upstream version of [`std::time::Instant`] isn't quite right for use here, because it
-// uses `clock_gettime(CLOCK_MONOTONIC)`, which stops ticking during a suspend (see
-// https://github.com/rust-lang/rust/issues/87906).  However, the local Android version of
-// std/src/sys/unit/time.rs has been patched to use `clock_gettime(CLOCK_BOOTTIME)` instead, which
-// does included suspended time.
-use std::time::Instant;
 
 /// Monotonic clock.
-pub struct StdClock(Instant);
-
-impl Default for StdClock {
-    fn default() -> Self {
-        Self(Instant::now())
-    }
-}
+#[derive(Default)]
+pub struct StdClock;
 
 impl crypto::MonotonicClock for StdClock {
     fn now(&self) -> crypto::MillisecondsSinceEpoch {
-        let millis: i64 =
-            self.0.elapsed().as_millis().try_into().expect("failed to fit timestamp in i64");
-        crypto::MillisecondsSinceEpoch(millis)
+        let mut time = libc::timespec { tv_sec: 0, tv_nsec: 0 };
+        // Safety: `time` is a valid structure.
+        let rc =
+            unsafe { libc::clock_gettime(libc::CLOCK_BOOTTIME, &mut time as *mut libc::timespec) };
+        if rc < 0 {
+            log::warn!("failed to get time!");
+            return crypto::MillisecondsSinceEpoch(0);
+        }
+        crypto::MillisecondsSinceEpoch((time.tv_sec * 1000) + (time.tv_nsec / 1000 / 1000))
     }
 }
