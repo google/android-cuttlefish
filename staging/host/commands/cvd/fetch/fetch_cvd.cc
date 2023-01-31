45cfa91dcbf09e285f8399a828194149697dacc0
Treehugger Robot <treehugger-gerrit@google.com>
2023-01-31 08:16:41 +0000

    [skip ci] Merge "Add vbmeta_vendor_dlkm chain" am: 270f37b61d am: 90c847fb64
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2381823
    
    Change-Id: I90d7ab8342477970a92ddd8cb04df2025b8ad001
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: f74d5d6ff7b19a47a29fe63e72334445a83837fb

--- staging/host/commands/cvd/fetch/fetch_cvd.cc
+++ staging/host/commands/cvd/fetch/fetch_cvd.cc
@@ -541,6 +541,19 @@ Result<void> FetchCvdMain(int argc, char** argv) {
                                 << "\"" << target_vbmeta_system
                                 << "\") failed: \"" << strerror(errno) << "\"");
         }
+        if (ExtractImages(target_files[0], target_dir,
+                          {"IMAGES/vbmeta_vendor_dlkm.img"}) !=
+            std::vector<std::string>{}) {
+          std::string extracted_vbmeta_vendor_dlkm =
+              target_dir + "/IMAGES/vbmeta_vendor_dlkm.img";
+          std::string target_vbmeta_vendor_dlkm =
+              target_dir + "/vbmeta_vendor_dlkm.img";
+          CF_EXPECT(rename(extracted_vbmeta_vendor_dlkm.c_str(),
+                           target_vbmeta_vendor_dlkm.c_str()) == 0,
+                    "rename(\"" << extracted_vbmeta_vendor_dlkm << "\", \""
+                                << "\"" << target_vbmeta_vendor_dlkm
+                                << "\") failed: \"" << strerror(errno) << "\"");
+        }
         // This should technically call AddFilesToConfig with the produced
         // files, but it will conflict with the ones produced from the default
         // system image and pie doesn't care about the produced file list
