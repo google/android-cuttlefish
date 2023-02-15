ae0e7905fbc7228908612e67cd01760c24545663
Wei-chung Hsu <weihsu@google.com>
2023-02-15 05:15:37 +0000

    [skip ci] Merge "Added flags for CF acloud create, "--bootloader-branch", "--bootloader-build-id", "--bootloader-build-target"." am: bf065c00b4 am: 90a786f137
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2433614
    
    Change-Id: I054859a8049eb5cb1bdec344f44086b70f693f61
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 1e67d6e042c01aa340412c4528cfd905aa75970d

--- staging/host/commands/cvd/acloud_command.cpp
+++ staging/host/commands/cvd/acloud_command.cpp
@@ -200,6 +200,34 @@ class ConvertAcloudCreateCommand {
               return true;
             }));
 
+    std::optional<std::string> bootloader_build_id;
+    flags.emplace_back(
+        Flag()
+            .Alias({FlagAliasMode::kFlagConsumesFollowing, "--bootloader-build-id"})
+            .Alias({FlagAliasMode::kFlagConsumesFollowing, "--bootloader_build_id"})
+            .Setter([&bootloader_build_id](const FlagMatch& m) {
+              bootloader_build_id = m.value;
+              return true;
+            }));
+    std::optional<std::string> bootloader_build_target;
+    flags.emplace_back(
+        Flag()
+            .Alias({FlagAliasMode::kFlagConsumesFollowing, "--bootloader-build-target"})
+            .Alias({FlagAliasMode::kFlagConsumesFollowing, "--bootloader_build_target"})
+            .Setter([&bootloader_build_target](const FlagMatch& m) {
+              bootloader_build_target = m.value;
+              return true;
+            }));
+    std::optional<std::string> bootloader_branch;
+    flags.emplace_back(
+        Flag()
+            .Alias({FlagAliasMode::kFlagConsumesFollowing, "--bootloader-branch"})
+            .Alias({FlagAliasMode::kFlagConsumesFollowing, "--bootloader_branch"})
+            .Setter([&bootloader_branch](const FlagMatch& m) {
+              bootloader_branch = m.value;
+              return true;
+            }));
+
     std::optional<std::string> launch_args;
     flags.emplace_back(
         Flag()
@@ -291,6 +319,8 @@ class ConvertAcloudCreateCommand {
     if (local_image) {
       CF_EXPECT(!(system_branch || system_build_target || system_build_id),
                 "--local-image incompatible with --system-* flags");
+      CF_EXPECT(!(bootloader_branch || bootloader_build_target || bootloader_build_id),
+                "--local-image incompatible with --bootloader-* flags");
       cvd::Request& mkdir_request = request_protos.emplace_back();
       auto& mkdir_command = *mkdir_request.mutable_command_request();
       mkdir_command.add_args("cvd");
@@ -331,6 +361,16 @@ class ConvertAcloudCreateCommand {
         }
         auto build =
             system_build_id.value_or(system_branch.value_or("aosp-master"));
+        fetch_command.add_args(build + target);
+      }
+      if (bootloader_branch || bootloader_build_id || bootloader_build_target) {
+        fetch_command.add_args("--bootloader_build");
+        auto target = bootloader_build_target.value_or("");
+        if (target != "") {
+          target = "/" + target;
+        }
+        auto build =
+            bootloader_build_id.value_or(bootloader_branch.value_or("aosp_u-boot-mainline"));
         fetch_command.add_args(build + target);
       }
       if (kernel_branch || kernel_build_id || kernel_build_target) {
