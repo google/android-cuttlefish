5045626b021fef9958c2f4854c7aa0e6ec008138
Mahmoud Elsherif <moelsherif@google.com>
2023-02-17 02:57:56 +0000

    add parsers and mappers for fetch_cvd flags
    Bug: b/269288853 , b/266072511 , b/266072210 , b/266072067
    
    Test: m,atest -c --host --no-bazel-mode cvd_load_test
    Change-Id: I2cb412dea25ae13cd0a18532b2db973eb2e2ecbb

--- cvd/host/commands/cvd/parser/fetch_cvd_parser.cpp
+++ cvd/host/commands/cvd/parser/fetch_cvd_parser.cpp
@@ -26,15 +26,56 @@
 #include "common/libs/utils/files.h"
 #include "common/libs/utils/json.h"
 #include "host/commands/assemble_cvd/flags_defaults.h"
+#include "host/commands/cvd/parser/cf_configs_common.h"
 
 namespace cuttlefish {
 
-FetchCvdConfigs GenerateFetchCvdFlags(const Json::Value&) {
-  FetchCvdConfigs result;
+#define EMPTY_CREDENTIAL ""
+#define EMPTY_DEFAULT_BUILD ""
+#define EMPTY_SYSTEM_BUILD ""
+#define EMPTY_KERNEL_BUILD ""
+
+void InitFetchInstanceConfigs(Json::Value& instances) {
+  // Handle common flags
+  InitStringConfig(instances, "disk", "default_build", EMPTY_DEFAULT_BUILD);
+  InitStringConfig(instances, "disk", "system_build", EMPTY_SYSTEM_BUILD);
+  InitStringConfig(instances, "disk", "kernel_build", EMPTY_KERNEL_BUILD);
+}
+
+void InitFetchCvdConfigs(Json::Value& root) {
+  if (!root.isMember("credential")) {
+    root["credential"] = EMPTY_CREDENTIAL;
+  }
+  InitFetchInstanceConfigs(root["instances"]);
+}
+
+FetchCvdDeviceConfigs ParseFetchInstanceConfigs(const Json::Value& instance) {
+  FetchCvdDeviceConfigs result;
+  result.default_build = instance["disk"]["default_build"].asString();
+  result.system_build = instance["disk"]["system_build"].asString();
+  result.kernel_build = instance["disk"]["kernel_build"].asString();
+  if (result.default_build != EMPTY_DEFAULT_BUILD ||
+      result.system_build != EMPTY_SYSTEM_BUILD ||
+      result.kernel_build != EMPTY_KERNEL_BUILD) {
+    result.use_fetch_artifact = true;
+  } else {
+    result.use_fetch_artifact = false;
+  }
+
   return result;
 }
 
-void InitFetchCvdConfigs(Json::Value&) {}
+FetchCvdConfigs GenerateFetchCvdFlags(const Json::Value& root) {
+  FetchCvdConfigs result;
+  result.credential = root["credential"].asString();
+  int num_instances = root["instances"].size();
+  for (unsigned int i = 0; i < num_instances; i++) {
+    auto instance_config = ParseFetchInstanceConfigs(root["instances"][i]);
+    result.instances.emplace_back(instance_config);
+  }
+
+  return result;
+}
 
 FetchCvdConfigs ParseFetchCvdConfigs(Json::Value& root) {
   InitFetchCvdConfigs(root);
