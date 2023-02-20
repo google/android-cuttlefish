e900642fbe64ef0f32866855d0d1835eb2b7f678
Steve Kim <kwstephenkim@google.com>
2023-02-20 10:30:38 +0000

    Merge "Make FindDefaultGroup() logic available for InstanceSelector" am: 04b5d72ada am: 8cbad4d5e0 am: c779799847 am: 60619830ea
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2439532
    
    Change-Id: Ie46fe9357c0f2cfaf07c0156dafb05db2f9a88f3
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- base/cvd/cuttlefish/host/commands/cvd/selector/device_selector_utils.h
+++ base/cvd/cuttlefish/host/commands/cvd/selector/device_selector_utils.h
@@ -30,7 +30,7 @@ namespace cuttlefish {
 namespace selector {
 
 Result<LocalInstanceGroup> GetDefaultGroup(
-    const InstanceDatabase& instance_database);
+    const InstanceDatabase& instance_database, const uid_t client_uid);
 
 }  // namespace selector
 }  // namespace cuttlefish
