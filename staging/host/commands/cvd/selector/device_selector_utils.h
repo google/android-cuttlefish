c3792fa1abfd6cfac923b9fa67fd21c44af48fac
Steve Kim <kwstephenkim@google.com>
2023-02-20 07:50:02 +0000

    Merge "Make FindDefaultGroup() logic available for InstanceSelector" am: 04b5d72ada am: 8cbad4d5e0
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2439532
    
    Change-Id: I9b7050f75b37518f9231a0f7374d6e2389e7b9ee
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: c7797998475a785299a5f35310907cbf1458a7a1

---
+++ staging/host/commands/cvd/selector/device_selector_utils.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+/**
+ * @file Utils shared by device selectors for non-start operations
+ *
+ */
+
+#include <sys/types.h>
+
+#include "common/libs/utils/result.h"
+#include "host/commands/cvd/selector/instance_database.h"
+
+namespace cuttlefish {
+namespace selector {
+
+Result<LocalInstanceGroup> GetDefaultGroup(
+    const InstanceDatabase& instance_database, const uid_t client_uid);
+
+}  // namespace selector
+}  // namespace cuttlefish
