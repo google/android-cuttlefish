adfbf8c80015c7102e01c756ae729f7a1aae5a77
Treehugger Robot <treehugger-gerrit@google.com>
2023-02-24 23:56:32 +0000

    Merge "Added API for higher level information for process" am: ef5a092c5e am: 50f6ca50fd am: 82ad7e72f4 am: e479b1c32b am: 607bb6ed5e
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2452885
    
    Change-Id: I7e0203c41bb8efc92e1203a1f3f2f43c6bd437eb
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- base/cvd/cuttlefish/common/libs/utils/proc_file_utils.h
+++ base/cvd/cuttlefish/common/libs/utils/proc_file_utils.h
@@ -34,22 +34,41 @@ namespace cuttlefish {
 
 static constexpr char kProcDir[] = "/proc";
 
+struct ProcInfo {
+  pid_t pid_;
+  std::string actual_exec_path_;
+  std::unordered_map<std::string, std::string> envs_;
+  std::vector<std::string> args_;
+};
+Result<ProcInfo> ExtractProcInfo(const pid_t pid);
+
 // collects all pids whose owner is uid
 Result<std::vector<pid_t>> CollectPids(const uid_t uid = getuid());
 
 /* collects all pids that meet the following:
  *
  * 1. Belongs to the uid
- * 2. cpp_basename(`cat /proc/<pid>/cmdline`.front()) == exec_name
- * 3. cpp_basename(exec_name) == exec_name
+ * 2. cpp_basename(readlink(/proc/<pid>/exe)) == exec_name
  *
  */
 Result<std::vector<pid_t>> CollectPidsByExecName(const std::string& exec_name,
                                                  const uid_t uid = getuid());
 
-// "exec_path" is treated as an absolute path
+/* collects all pids that meet the following:
+ *
+ * 1. Belongs to the uid
+ * 2. readlink(/proc/<pid>/exe) == exec_name
+ *
+ */
 Result<std::vector<pid_t>> CollectPidsByExecPath(const std::string& exec_path,
                                                  const uid_t uid = getuid());
+
+/**
+ * When argv[0] != exec_path, collects PIDs based on argv[0]
+ *
+ */
+Result<std::vector<pid_t>> CollectPidsByArgv0(const std::string& expected_argv0,
+                                              const uid_t uid = getuid());
 
 Result<uid_t> OwnerUid(const pid_t pid);
 
