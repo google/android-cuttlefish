2aaac348c875fdecfb1fb85018d3616b1818eef0
Treehugger Robot <treehugger-gerrit@google.com>
2023-02-25 07:04:52 +0000

    Merge "Give a symbolic name to the server executable path" am: 872e0b6910 am: 0f49f10975 am: cb6b81315b am: a19961952a
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2452886
    
    Change-Id: I4670342c96d6564eea203421f0e632176745611f
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- base/cvd/cuttlefish/host/commands/cvd/main.cc
+++ base/cvd/cuttlefish/host/commands/cvd/main.cc
@@ -108,7 +108,7 @@ Result<void> HandleReset(CvdClient& client,
     LOG(ERROR) << "However, cvd reset will continue cleaning up.";
   }
   // cvd reset handler placeholder. identical to cvd kill-server for now.
-  CF_EXPECT(KillAllCuttlefishInstances({false, true}));
+  CF_EXPECT(KillAllCuttlefishInstances(false));
   return {};
 }
 
