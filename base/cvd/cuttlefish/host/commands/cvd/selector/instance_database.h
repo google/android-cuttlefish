5a08e2ab31cedc1fb006ae23c2c611340d30edfa
Steve Kim <kwstephenkim@google.com>
2023-02-27 18:51:53 +0000

    Merge "AddInstance() will take group name, not reference" am: d88663d5b1 am: 7e236b05c6 am: 9c62d3d1f3 am: 6dd956c224 am: 31b7b63467
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2433887
    
    Change-Id: I1e0f962c39e39aa6771f52d4e547f1ce7ea8b738
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- base/cvd/cuttlefish/host/commands/cvd/selector/instance_database.h
+++ base/cvd/cuttlefish/host/commands/cvd/selector/instance_database.h
@@ -59,6 +59,16 @@ class InstanceDatabase {
   Result<void> AddInstance(const std::string& group_name, const unsigned id,
                            const std::string& instance_name);
 
+  struct InstanceInfo {
+    const unsigned id;
+    const std::string name;
+  };
+  Result<void> AddInstances(const std::string& group_name,
+                            const std::vector<InstanceInfo>& instances);
+
+  Result<void> SetBuildId(const std::string& group_name,
+                          const std::string& build_id);
+
   /*
    *  auto group = CF_EXPEC(FindGroups(...));
    *  RemoveInstanceGroup(group)
@@ -120,6 +130,8 @@ class InstanceDatabase {
       const Value& instance_specific_name) const;
   Result<Set<ConstRef<LocalInstance>>> FindInstancesByInstanceName(
       const Value& group_name) const;
+
+  Result<LocalInstanceGroup*> FindMutableGroup(const std::string& group_name);
 
   std::vector<std::unique_ptr<LocalInstanceGroup>> local_instance_groups_;
   Map<FieldName, ConstGroupHandler> group_handlers_;
