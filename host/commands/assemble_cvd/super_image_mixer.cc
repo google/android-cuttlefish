946f2259634862adccb06385b2df1b8854884618
Kelvin Zhang <zhangkelvin@google.com>
2023-03-03 11:47:16 -0800

    Do not extract vbmeta_vendor_dlkm from system target files
    When we created a mixed build cuttlefish, vendor_dlkm is counted as
    "vendor" images but vbmeta_vendor_dlkm is counted as "system" image.
    This causes boot failures. To address the issue, do not overwrite
    default vbmeta_vendor_dlkm.img with ones included in system
    target_files.
    
    Test: acloud create --system-build-id
    Change-Id: Iefe735ac85f1279695bc81281132998b5d1c137c

--- host/commands/assemble_cvd/super_image_mixer.cc
+++ host/commands/assemble_cvd/super_image_mixer.cc
@@ -69,9 +69,11 @@ std::string TargetFilesZip(const FetcherConfig& fetcher_config,
 
 const std::string kMiscInfoPath = "META/misc_info.txt";
 const std::set<std::string> kDefaultTargetImages = {
-    "IMAGES/boot.img",        "IMAGES/init_boot.img", "IMAGES/odm.img",
-    "IMAGES/odm_dlkm.img",    "IMAGES/recovery.img",  "IMAGES/userdata.img",
-    "IMAGES/vbmeta.img",      "IMAGES/vendor.img",    "IMAGES/vendor_dlkm.img",
+    "IMAGES/boot.img",        "IMAGES/init_boot.img",
+    "IMAGES/odm.img",         "IMAGES/odm_dlkm.img",
+    "IMAGES/recovery.img",    "IMAGES/userdata.img",
+    "IMAGES/vbmeta.img",      "IMAGES/vendor.img",
+    "IMAGES/vendor_dlkm.img", "IMAGES/vbmeta_vendor_dlkm.img",
     "IMAGES/system_dlkm.img",
 };
 const std::set<std::string> kDefaultTargetBuildProp = {
