7851cd83bcd15d166d5d3008edf5bc8d4487bc4b
Treehugger Robot <treehugger-gerrit@google.com>
2023-03-03 01:21:42 +0000

    [skip ci] Merge "Fix condition when message is printed." am: c4aba40dc5 am: f677ede189
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2464756
    
    Change-Id: If747dcac892dc154e762c515d3cd1046e9a9602f
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 35736e527dd7807af5c0b11b51a99bd340d45a19

--- staging/host/libs/allocd/resource_manager.cpp
+++ staging/host/libs/allocd/resource_manager.cpp
@@ -108,9 +108,8 @@ bool ResourceManager::AddInterface(const std::string& iface, IfaceType ty,
         break;
       }
       case IfaceType::etap: {
-        auto w = std::make_shared<EthernetIface>(iface, uid, small_id,
-                                                 resource_id, "cvd-ebr",
-                                                 kEthernetIp);
+        auto w = std::make_shared<EthernetIface>(
+            iface, uid, small_id, resource_id, "cvd-ebr", kEthernetIp);
         w->SetUseEbtablesLegacy(use_ebtables_legacy_);
         w->SetHasIpv4(use_ipv4_bridge_);
         w->SetHasIpv6(use_ipv6_bridge_);
@@ -176,7 +175,7 @@ bool ResourceManager::RemoveInterface(const std::string& iface, IfaceType ty) {
     LOG(WARNING) << "Interface not managed: " << iface;
   }
 
-  if (isManagedIface) {
+  if (removedIface) {
     LOG(INFO) << "Removed interface: " << iface;
   } else {
     LOG(WARNING) << "Could not remove interface: " << iface;
