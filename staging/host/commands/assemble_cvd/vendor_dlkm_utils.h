f6cd7b47718eeb72363bd6ab5585f3ce0c2a8197
Treehugger Robot <treehugger-gerrit@google.com>
2023-03-14 17:08:36 +0000

    [skip ci] Merge "Move vendor_dlkm functions to a separate file" am: c334c9561a am: f979184c19
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2481156
    
    Change-Id: I34ce1a6820be8644add3a9e76b55fe1ce6428e19
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 96ce9e8af27f0b58af6d448c82f2f539904df784

---
+++ staging/host/commands/assemble_cvd/vendor_dlkm_utils.h
@@ -0,0 +1,31 @@
+//
+// Copyright (C) 2023 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <string>
+
+namespace cuttlefish {
+
+bool RepackSuperWithVendorDLKM(const std::string& superimg_path,
+                               const std::string& vendor_dlkm_path);
+
+bool BuildVendorDLKM(const std::string& src_dir, const bool is_erofs,
+                     const std::string& output_image);
+
+bool RebuildVbmetaVendor(const std::string& vendor_dlkm_img,
+                         const std::string& vbmeta_path);
+
+}  // namespace cuttlefish
