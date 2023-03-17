7481255e04c7c7fe6a456fc25bb8d59b73b3e206
Wei-chung Hsu <weihsu@google.com>
2023-03-17 21:31:42 +0000

    [skip ci] Merge "Added "--config-file "flag for CF acloud create and load/parse alcoud config files." am: 3b0a2a500b am: a1c1d3b434
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2459130
    
    Change-Id: I4f143d2745e769de60801d069edb55908bd8d28f
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 41d9da57f456cdd5bf158ec2f15226de2464959d

---
+++ staging/host/commands/cvd/acloud_config.cpp
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <fstream>
+
+#include <google/protobuf/text_format.h>
+
+#include "common/libs/utils/files.h"
+#include "common/libs/utils/users.h"
+#include "host/commands/cvd/acloud_config.h"
+
+namespace cuttlefish {
+
+AcloudConfig::AcloudConfig(const acloud::UserConfig& usr_cfg)
+    : launch_args(usr_cfg.launch_args()) {
+  // TODO(weihsu): Add back fields/variables (except of cheeps and emulator
+  // fields) in config files. Remove cheeps (Android on ChromeOS) and emulator
+  // fields.
+
+  // TODO(weihsu): Verify validity of configurations.
+}
+
+template <typename ProtoType>
+Result<ProtoType> ParseTextProtoConfigHelper(const std::string& config_path) {
+  std::ifstream t(config_path);
+  std::stringstream buffer;
+  buffer << t.rdbuf();
+
+  ProtoType proto_result;
+  google::protobuf::TextFormat::Parser p;
+  CF_EXPECT(p.ParseFromString(buffer.str(), &proto_result),
+            "Failed to parse config: " << config_path);
+  return proto_result;
+}
+
+/**
+ * Return path to default config file.
+ */
+Result<const std::string> GetDefaultConfigFile(const uid_t uid) {
+  const std::string home = CF_EXPECT(SystemWideUserHome(uid));
+  return (std::string(home) + "/.config/acloud/acloud.config");
+}
+
+Result<AcloudConfig> LoadAcloudConfig(const std::string& user_config_path,
+                                      const uid_t uid) {
+  acloud::UserConfig proto_result_user;
+  if (FileExists(user_config_path)) {
+    proto_result_user = CF_EXPECT(
+        ParseTextProtoConfigHelper<acloud::UserConfig>(user_config_path));
+  } else {
+    const std::string conf_path = CF_EXPECT(GetDefaultConfigFile(uid));
+    CF_EXPECT(user_config_path == conf_path,
+              "The specified config file does not exist.");
+
+    // If the default config does not exist, acloud creates an empty object.
+    proto_result_user = acloud::UserConfig();
+  }
+  return AcloudConfig(proto_result_user);
+}
+
+}  // namespace cuttlefish
---
+++ staging/host/commands/cvd/acloud_config.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <optional>
+#include <vector>
+
+#include "internal_config.pb.h"
+#include "user_config.pb.h"
+
+#include "common/libs/utils/result.h"
+#include "host/libs/config/inject.h"
+
+namespace cuttlefish {
+
+class AcloudConfig {
+ public:
+  INJECT(AcloudConfig(const acloud::UserConfig&));
+  ~AcloudConfig() = default;
+
+ public:
+  // UserConfig/user_config.proto members
+  std::string launch_args;
+
+  // InternalConfig/internal_config.proto members
+
+  // In both config
+};
+
+Result<const std::string> GetDefaultConfigFile(const uid_t uid);
+Result<AcloudConfig> LoadAcloudConfig(const std::string& user_config_path,
+                                      const uid_t uid);
+
+}  // namespace cuttlefish
