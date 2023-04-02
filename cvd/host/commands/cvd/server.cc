e6a3a8e6f4d7d549f7d4784550a23e78363bd1c4
Steve Kim <kwstephenkim@google.com>
2023-04-02 12:51:57 -0700

    Create symbolic links for Python acloud compatibility
    Cvd start should create symbolic links from host_bins to
    ANDROID_HOST_OUT. Also, it should creates symbolic links to
    the HOME directory from /tmp/acloud_cvd_temp.
    
    Bug: 276466342
    Test: Run locally
    Change-Id: I6b65e34df450bf157cc79c9bd9306c2a113bd75d

--- cvd/host/commands/cvd/server.cc
+++ cvd/host/commands/cvd/server.cc
@@ -49,8 +49,6 @@
 #include "host/commands/cvd/logger.h"
 #include "host/commands/cvd/server_command/cmd_list.h"
 #include "host/commands/cvd/server_command/crosvm.h"
-#include "host/commands/cvd/server_command/display.h"
-#include "host/commands/cvd/server_command/env.h"
 #include "host/commands/cvd/server_command/generic.h"
 #include "host/commands/cvd/server_command/handler_proxy.h"
 #include "host/commands/cvd/server_command/load_configs.h"
@@ -113,15 +111,13 @@ fruit::Component<> CvdServer::RequestComponent(CvdServer* server) {
       .install(CommandSequenceExecutorComponent)
       .install(CvdCrosVmComponent)
       .install(cvdCommandComponent)
-      .install(CvdDisplayComponent)
-      .install(CvdEnvComponent)
       .install(cvdGenericCommandComponent)
       .install(CvdHandlerProxyComponent)
       .install(CvdHelpComponent)
       .install(CvdResetComponent)
       .install(CvdRestartComponent)
       .install(cvdShutdownComponent)
-      .install(cvdStartCommandComponent)
+      .install(CvdStartCommandComponent)
       .install(cvdVersionComponent)
       .install(DemoMultiVdComponent)
       .install(LoadConfigsComponent);
