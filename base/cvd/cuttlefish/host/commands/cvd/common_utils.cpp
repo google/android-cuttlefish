a1a92d120424f791f39482ab17490e8fe5d15ed6
Steve Kim <kwstephenkim@google.com>
2023-04-04 20:12:22 +0000

    Merge changes I11bae24f,Iadba2682 am: 1ce0de9ccd am: aa89492590 am: fe2684bf56 am: 813ad7f390
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2519036
    
    Change-Id: I461d8df5276a7d13b1af8f4521106ea0715aa409
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- base/cvd/cuttlefish/host/commands/cvd/common_utils.cpp
+++ base/cvd/cuttlefish/host/commands/cvd/common_utils.cpp
@@ -16,13 +16,20 @@
 
 #include "host/commands/cvd/common_utils.h"
 
+#include <unistd.h>
+
+#include <algorithm>
 #include <memory>
 #include <sstream>
+#include <stack>
 
 #include <android-base/file.h>
 #include <android-base/logging.h>
+#include <android-base/strings.h>
 
 #include "common/libs/utils/contains.h"
+#include "common/libs/utils/files.h"
+#include "common/libs/utils/users.h"
 
 namespace cuttlefish {
 
@@ -78,6 +85,135 @@ cvd::Request MakeRequest(const MakeRequestParam& args_and_envs,
   command_request->set_wait_behavior(wait_behavior);
 
   return request;
+}
+
+// given /a/b/c/d/e, ensures
+// all directories from /a through /a/b/c/d/e exist
+Result<void> EnsureDirectoryExistsAllTheWay(const std::string& dir) {
+  CF_EXPECT(!dir.empty() && dir.at(0) == '/',
+            "EnsureDirectoryExistsAllTheWay() handles absolute paths only.");
+  if (dir == "/") {
+    return {};
+  }
+  std::string path_exclude_root = dir.substr(1);
+  std::vector<std::string> tokens =
+      android::base::Tokenize(path_exclude_root, "/");
+  std::string current_dir = "/";
+  for (int i = 0; i < tokens.size(); i++) {
+    current_dir.append(tokens[i]);
+    CF_EXPECT(EnsureDirectoryExists(current_dir),
+              current_dir << " does not exist and cannot be created.");
+    current_dir.append("/");
+  }
+  return {};
+}
+
+static std::vector<std::string> Reverse(std::stack<std::string>& s) {
+  std::vector<std::string> reversed;
+  while (!s.empty()) {
+    reversed.push_back(s.top());
+    s.pop();
+  }
+  std::reverse(reversed.begin(), reversed.end());
+  return reversed;
+}
+
+static std::vector<std::string> EmulateAbsolutePathImpl(
+    std::stack<std::string>& so_far, const std::vector<std::string>& tokens,
+    const size_t idx = 0) {
+  if (idx == tokens.size()) {
+    return Reverse(so_far);
+  }
+  const std::string& token = tokens.at(idx);
+  if (token == "." || token.empty()) {
+    // If token is empty, it might be //, so should be simply ignored
+    return EmulateAbsolutePathImpl(so_far, tokens, idx + 1);
+  }
+  if (token == "..") {
+    if (!so_far.empty()) {
+      // at /, ls ../../.. shows just the root. So, if too many ..s are here,
+      // we silently ignore them
+      so_far.pop();
+    }
+    return EmulateAbsolutePathImpl(so_far, tokens, idx + 1);
+  }
+  so_far.push(token);
+  return EmulateAbsolutePathImpl(so_far, tokens, idx + 1);
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const std::vector<T>& v) {
+  if (v.empty()) {
+    out << "{}";
+    return out;
+  }
+  if (v.size() == 1) {
+    out << "{" << v.front() << "}";
+    return out;
+  }
+  out << "{";
+  for (size_t i = 0; i != v.size() - 1; i++) {
+    out << v.at(i) << ", ";
+  }
+  out << v.back() << "}";
+  return out;
+}
+
+Result<std::string> EmulateAbsolutePath(const InputPathForm& path_info) {
+  const auto& path = path_info.path_to_convert;
+  std::string working_dir;
+  if (path_info.current_working_dir) {
+    working_dir = *path_info.current_working_dir;
+  } else {
+    std::unique_ptr<char, void (*)(void*)> cwd(getcwd(nullptr, 0), &free);
+    std::string process_cwd(cwd.get());
+    working_dir = std::move(process_cwd);
+  }
+  CF_EXPECT(android::base::StartsWith(working_dir, '/'),
+            "Current working directory should be given in an absolute path.");
+
+  const std::string home_dir = path_info.home_dir
+                                   ? *path_info.home_dir
+                                   : CF_EXPECT(SystemWideUserHome());
+  CF_EXPECT(android::base::StartsWith(home_dir, '/'),
+            "Home directory should be given in an absolute path.");
+
+  if (path.empty()) {
+    LOG(ERROR) << "The requested path to convert an absolute path is empty.";
+    return "";
+  }
+  if (path == "/") {
+    return path;
+  }
+  std::vector<std::string> tokens = android::base::Tokenize(path, "/");
+  std::stack<std::string> prefix_dir_stack;
+  if (path == "~" || android::base::StartsWith(path, "~/")) {
+    // tokens == {"~", "some", "dir", "file"}
+    std::vector<std::string> home_dir_tokens =
+        android::base::Tokenize(home_dir, "/");
+    tokens.erase(tokens.begin());
+    for (const auto& home_dir_token : home_dir_tokens) {
+      prefix_dir_stack.push(home_dir_token);
+    }
+  } else if (!android::base::StartsWith(path, "/")) {
+    // path was like "a/b/c", which should be expanded to $PWD/a/b/c
+    std::vector<std::string> working_dir_tokens =
+        android::base::Tokenize(working_dir, "/");
+    for (const auto& working_dir_token : working_dir_tokens) {
+      prefix_dir_stack.push(working_dir_token);
+    }
+  }
+
+  auto result = EmulateAbsolutePathImpl(prefix_dir_stack, tokens, 0);
+  std::stringstream assemble_output;
+  assemble_output << "/";
+  if (!result.empty()) {
+    assemble_output << android::base::Join(result, "/");
+  }
+  if (path_info.follow_symlink) {
+    return AbsolutePath(assemble_output.str());
+  }
+  return assemble_output.str();
 }
 
 }  // namespace cuttlefish
