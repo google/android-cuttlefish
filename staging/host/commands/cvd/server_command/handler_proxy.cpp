742fb3dfbe8ea2aae718ee81cb23c4ba05f92c14
Steve Kim <kwstephenkim@google.com>
2023-04-04 19:08:32 +0000

    [skip ci] Merge changes I361b870f,I6b65e34d,I6fd72499 am: b18e04a106 am: 1aebad3fdc
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2519377
    
    Change-Id: I310a9ba6f4ff39cbe70df802905549e6758abf85
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 82cb9f720e27089b0e652eda3a36bf5b067fb6f8

--- staging/host/commands/cvd/server_command/handler_proxy.cpp
+++ staging/host/commands/cvd/server_command/handler_proxy.cpp
@@ -82,12 +82,13 @@ class CvdServerHandlerProxy : public CvdServerHandler {
       new_exec_args.push_back(*new_sub_cmd);
     }
     new_exec_args.insert(new_exec_args.end(), cmd_args.begin(), cmd_args.end());
-    cvd::Request exec_request =
-        MakeRequest({.cmd_args = new_exec_args,
-                     .env = envs,
-                     .selector_args = selector_args},
-                    request.Message().command_request().wait_behavior(),
-                    request.Message().command_request().working_directory());
+    cvd::Request exec_request = MakeRequest(
+        {.cmd_args = new_exec_args,
+         .env = envs,
+         .selector_args = selector_args,
+         .working_dir =
+             request.Message().command_request().working_directory()},
+        request.Message().command_request().wait_behavior());
 
     RequestWithStdio forwarded_request(
         request.Client(), std::move(exec_request), request.FileDescriptors(),
