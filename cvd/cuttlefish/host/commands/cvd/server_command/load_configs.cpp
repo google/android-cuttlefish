b3320544b871094510f7664a9e5d1f795ff3b77a
Mahmoud Elsherif <moelsherif@google.com>
2023-04-04 15:52:05 +0000

    Merge changes I34a730a0,I9f4bca4c am: d02d6e1e9f am: 4e6aceb478 am: 4e8b4d280e
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2517655
    
    Change-Id: I213b0c79d4567310cbb8c5ff668d3b47c4ef8332
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- cvd/cuttlefish/host/commands/cvd/server_command/load_configs.cpp
+++ cvd/cuttlefish/host/commands/cvd/server_command/load_configs.cpp
@@ -21,12 +21,14 @@
 #include <string>
 
 #include <fruit/fruit.h>
+#include <android-base/parseint.h>
 
 #include "common/libs/fs/shared_buf.h"
 #include "common/libs/utils/files.h"
 #include "common/libs/utils/flag_parser.h"
 #include "common/libs/utils/result.h"
 #include "host/commands/cvd/command_sequence.h"
+#include "host/commands/cvd/parser/cf_configs_common.h"
 #include "host/commands/cvd/parser/load_configs_parser.h"
 #include "host/commands/cvd/selector/selector_constants.h"
 #include "host/commands/cvd/server.h"
@@ -75,6 +77,128 @@ class LoadConfigsCommand : public CvdServerHandler {
 
   cvd_common::Args CmdList() const override { return {kLoadSubCmd}; }
 
+  // TODO: expand this enum in the future to support more types ( double , float
+  // , etc) if neeeded
+  enum ArgValueType { UINTEGER, BOOLEAN, TEXT };
+
+  bool IsUnsignedInteger(const std::string& str) {
+    return !str.empty() && std::all_of(str.begin(), str.end(),
+                                       [](char c) { return std::isdigit(c); });
+  }
+
+  ArgValueType GetArgValueType(const std::string& str) {
+    if (IsUnsignedInteger(str)) {
+      return UINTEGER;
+    }
+
+    if (str == "true" || str == "false") {
+      return BOOLEAN;
+    }
+
+    // Otherwise, treat the string as text
+    return TEXT;
+  }
+
+  Json::Value ConvertArgToJson(const std::string& key,
+                               const std::string& leafValue) {
+    std::stack<std::string> levels;
+    std::stringstream ks(key);
+    std::string token;
+    while (std::getline(ks, token, '.')) {
+      levels.push(token);
+    }
+
+    // assign the leaf value based on the type of input value
+    Json::Value leaf;
+    if (GetArgValueType(leafValue) == UINTEGER) {
+      std::uint32_t leaf_val;
+      if (!android::base::ParseUint(leafValue ,&leaf_val)){
+        LOG(ERROR) << "Failed to parse unsigned integer " << leafValue;
+        return Json::Value::null;
+      };
+      leaf = leaf_val;
+    } else if (GetArgValueType(leafValue) == BOOLEAN) {
+      leaf = (leafValue == "true");
+    } else {
+      leaf = leafValue;
+    }
+
+    while (!levels.empty()) {
+      Json::Value curr;
+      std::string index = levels.top();
+
+      if (GetArgValueType(index) == UINTEGER) {
+        std::uint32_t index_val;
+        if (!android::base::ParseUint(index, &index_val)){
+          LOG(ERROR) << "Failed to parse unsigned integer " << index;
+          return Json::Value::null;
+        }
+        curr[index_val] = leaf;
+      } else {
+        curr[index] = leaf;
+      }
+
+      leaf = curr;
+      levels.pop();
+    }
+
+    return leaf;
+  }
+
+  Json::Value ParseArgsToJson(const std::vector<std::string>& strings) {
+    Json::Value jsonValue;
+    for (const auto& str : strings) {
+      std::string key;
+      std::string value;
+      size_t equals_pos = str.find('=');
+      if (equals_pos != std::string::npos) {
+        key = str.substr(0, equals_pos);
+        value = str.substr(equals_pos + 1);
+      } else {
+        key = str;
+        value.clear();
+        LOG(WARNING) << "No value provided for key " << key;
+        return Json::Value::null;
+      }
+      MergeTwoJsonObjs(jsonValue, ConvertArgToJson(key, value));
+    }
+
+    return jsonValue;
+  }
+
+  bool HasValidDotSeparatedPrefix(const std::string& str) {
+    auto equalsPos = str.find('=');
+    if (equalsPos == std::string::npos) {
+      return false;
+    }
+    std::string prefix = str.substr(0, equalsPos);
+    // return false if prefix is empty, has no dots, start with dots, end with dot
+    // or has cosequence of dots
+    if (prefix.empty() || prefix.find('.') == std::string::npos ||
+        prefix.find('.') == 0 || prefix.find("..") != std::string::npos ||
+        prefix.back() == '.') {
+      return false;
+    }
+    return true;
+  }
+
+  bool hasEqualsWithValidDotSeparatedPrefix(const std::string& str) {
+    auto equalsPos = str.find('=');
+    return equalsPos != std::string::npos && equalsPos < str.length() - 1 &&
+           HasValidDotSeparatedPrefix(str);
+  }
+
+  bool ValidateArgsFormat(const std::vector<std::string>& strings) {
+    for (const auto& str : strings) {
+      if (!hasEqualsWithValidDotSeparatedPrefix(str)) {
+        LOG(ERROR) << "Invalid  argument format. " << str
+                   << " Please use arg=value";
+        return false;
+      }
+    }
+    return true;
+  }
+
   Result<DemoCommandSequence> CreateCommandSequence(
       const RequestWithStdio& request) {
     bool help = false;
@@ -87,16 +211,30 @@ class LoadConfigsCommand : public CvdServerHandler {
     auto args = ParseInvocation(request.Message()).arguments;
     CF_EXPECT(ParseFlags(flags, args));
 
+    Json::Value json_configs;
     if (help) {
       std::stringstream help_msg_stream;
       help_msg_stream << "Usage: cvd " << kLoadSubCmd << std::endl;
       const auto help_msg = help_msg_stream.str();
       CF_EXPECT(WriteAll(request.Out(), help_msg) == help_msg.size());
       return {};
-    }
+    } else {
+      json_configs =
+          CF_EXPECT(ParseJsonFile(config_path), "parsing input file failed");
 
-    Json::Value json_configs =
-        CF_EXPECT(ParseJsonFile(config_path), "parsing input file failed");
+      if (args.size() > 0) {
+        for (auto& single_arg : args) {
+          LOG(INFO) << "Filtered args " << single_arg;
+        }
+        // Validate all arguments follow specific pattern
+        if (!ValidateArgsFormat(args)) {
+          return {};
+        }
+        // Convert all arguments to json tree
+        auto args_tree = ParseArgsToJson(args);
+        MergeTwoJsonObjs(json_configs, args_tree);
+      }
+    }
 
     auto cvd_flags =
         CF_EXPECT(ParseCvdConfigs(json_configs), "parsing json configs failed");
