9868ff12e6052de08c3e7264ef7d1a328c5dabfc
Charlie Boutier <charliebout@google.com>
2023-04-10 18:14:23 +0000

    Revert^2 "Add pica launcher"
    550fa6a4ca5e0a6f63a5437496f4f1191bae7290
    
    Change-Id: I9ec0f4d811f9a69e55b9e3a826b804cce94e1214

--- host/commands/run_cvd/main.cc
+++ host/commands/run_cvd/main.cc
@@ -137,6 +137,7 @@ fruit::Component<> runCvdComponent(
       .install(MetricsServiceComponent)
       .install(OpenWrtComponent)
       .install(OpenwrtControlServerComponent)
+      .install(PicaComponent)
       .install(RootCanalComponent)
       .install(NetsimServerComponent)
       .install(SecureEnvComponent)
