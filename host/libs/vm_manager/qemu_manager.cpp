3eb3a13251e96ebb57348052a6b752015c8dcce7
Dmitrii Merkurev <dimorinny@google.com>
2023-04-13 22:41:29 +0100

    Specify mac addresses for qemu tap devices
    To unblock fastbootd and fastboot on qemu
    
    Bug: 274107153
    Test: fastbootd/fastboot works
    Test: mobile network works on qemu CVD
    Change-Id: I0016ba2f77e33d618ce2fb91c3e29c80f42b2724
    Signed-off-by: Dmitrii Merkurev <dimorinny@google.com>

--- host/libs/vm_manager/qemu_manager.cpp
+++ host/libs/vm_manager/qemu_manager.cpp
@@ -621,21 +621,23 @@ Result<std::vector<Command>> QemuManager::StartCommands(
                         ",script=no,downscript=no", vhost_net);
 
   qemu_cmd.AddParameter("-device");
-  qemu_cmd.AddParameter("virtio-net-pci-non-transitional,netdev=hostnet0,id=net0");
+  qemu_cmd.AddParameter("virtio-net-pci-non-transitional,netdev=hostnet0,id=net0,mac=",
+                        instance.mobile_mac());
 
   qemu_cmd.AddParameter("-netdev");
   qemu_cmd.AddParameter("tap,id=hostnet1,ifname=", instance.ethernet_tap_name(),
                         ",script=no,downscript=no", vhost_net);
 
   qemu_cmd.AddParameter("-device");
-  qemu_cmd.AddParameter("virtio-net-pci-non-transitional,netdev=hostnet1,id=net1");
+  qemu_cmd.AddParameter("virtio-net-pci-non-transitional,netdev=hostnet1,id=net1,mac=",
+                        instance.ethernet_mac());
   if (!config.virtio_mac80211_hwsim()) {
     qemu_cmd.AddParameter("-netdev");
     qemu_cmd.AddParameter("tap,id=hostnet2,ifname=", instance.wifi_tap_name(),
                           ",script=no,downscript=no", vhost_net);
     qemu_cmd.AddParameter("-device");
-    qemu_cmd.AddParameter(
-        "virtio-net-pci-non-transitional,netdev=hostnet2,id=net2");
+    qemu_cmd.AddParameter("virtio-net-pci-non-transitional,netdev=hostnet2,id=net2,mac=",
+                          instance.wifi_mac());
   }
 
   if (is_x86 || is_arm) {
