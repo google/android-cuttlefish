d5c01582af9963185de49e1ebc7e1e4dda67b9dd
Steve Kim <kwstephenkim@google.com>
2023-04-14 15:12:58 -0700

    Support restart_cvd via cvd
    Supporting cvd [selector] restart [flags].
    
    Bug: 278298725
    Test: cvd --instance_name=X restart
    Test: cvd restart --instance_num=2
    Test: cvd help restart
    Change-Id: Ifdcfb9f05e229a3c0cad499c40f301143eb14051

--- cvd/host/commands/cvd/server_command/display.cpp
+++ cvd/host/commands/cvd/server_command/display.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#include "host/commands/cvd/server_command/crosvm.h"
+#include "host/commands/cvd/server_command/display.h"
 
 #include <android-base/strings.h>
 
@@ -82,6 +82,7 @@ class CvdDisplayCommandHandler : public CvdServerHandler {
     CF_EXPECT(subprocess_waiter_.Interrupt());
     return {};
   }
+
   cvd_common::Args CmdList() const override {
     return cvd_common::Args(cvd_display_operations_.begin(),
                             cvd_display_operations_.end());
