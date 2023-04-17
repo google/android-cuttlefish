8531d0ac39cc551cbef3198c7af6eebed9806dbe
Treehugger Robot <treehugger-gerrit@google.com>
2023-04-17 22:07:30 +0000

    Merge "Prevent a user to access to other users' cvd server processes" am: 1062e05fad am: 6caa70beeb am: f746d3211f am: b30dc2f6c2
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2536856
    
    Change-Id: I5eaa2cb7fca9f05e28074defdec6aa6056df42b9
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- base/cvd/cuttlefish/host/commands/cvd/server.cc
+++ base/cvd/cuttlefish/host/commands/cvd/server.cc
@@ -57,7 +57,6 @@
 #include "host/commands/cvd/server_command/load_configs.h"
 #include "host/commands/cvd/server_command/operation_to_bins_map.h"
 #include "host/commands/cvd/server_command/reset.h"
-#include "host/commands/cvd/server_command/restart_device.h"
 #include "host/commands/cvd/server_command/start.h"
 #include "host/commands/cvd/server_command/subcmd.h"
 #include "host/commands/cvd/server_constants.h"
@@ -124,7 +123,6 @@ fruit::Component<> CvdServer::RequestComponent(CvdServer* server) {
       .install(CvdHelpComponent)
       .install(CvdResetComponent)
       .install(CvdRestartComponent)
-      .install(CvdRestartDeviceComponent)
       .install(cvdShutdownComponent)
       .install(CvdStartCommandComponent)
       .install(cvdVersionComponent)
