bb6d60cfe8cf12b3109843b483c2c8469f1dcfcf
Steve Kim <kwstephenkim@google.com>
2023-04-17 21:57:49 -0700

    Implement selector for cvd powerwash
    1. Refactored cvd restart so the availability of the restart binary
    is checked at runtime using HostToolTargetManager
    
    2. Renamed and extended the cvd restart handler so that it also
    handles powerwash in the exactly same manner
    
    Bug: 278638339
    Test: cvd help powerwash
    Test: cvd --instance_name=X powerwash
    Test: cvd --instance_name=X restart
    Change-Id: I5273472916339034532fbce5663417abd8e4d0bc

---
+++ cvd/host/commands/cvd/server_command/power.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <fruit/fruit.h>
+
+#include "host/commands/cvd/instance_manager.h"
+#include "host/commands/cvd/server_command/host_tool_target_manager.h"
+#include "host/commands/cvd/server_command/subprocess_waiter.h"
+
+namespace cuttlefish {
+
+// restart, powerwash
+fruit::Component<
+    fruit::Required<HostToolTargetManager, InstanceManager, SubprocessWaiter>>
+CvdDevicePowerComponent();
+
+}  // namespace cuttlefish
