08cd08af275ea8852c54f70718b23b609b4d9b5c
Gurchetan Singh <gurchetansingh@google.com>
2023-04-25 22:31:02 +0000

    [skip ci] Merge "cuttlefish: update for gfxstream/crosvm changes" am: e72716fd70
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2559150
    
    Change-Id: Iafc5bcfb5b988af0566e3235b6e8653a1851ddd8
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 4948c4456b2e37dd949c100afca6e9d9413772ea

--- staging/host/libs/vm_manager/crosvm_manager.cpp
+++ staging/host/libs/vm_manager/crosvm_manager.cpp
@@ -181,10 +181,8 @@ Result<std::vector<MonitorCommand>> CrosvmManager::StartCommands(
   const auto gpu_capture_enabled = !instance.gpu_capture_binary().empty();
   const auto gpu_mode = instance.gpu_mode();
 
-  const std::string gpu_angle_string =
-      gpu_mode == kGpuModeGfxstreamGuestAngle ? ",angle=true" : "";
   const std::string gles_string =
-      gpu_mode == kGpuModeGfxstreamGuestAngle ? ",gles=true" : ",gles=false";
+      gpu_mode == kGpuModeGfxstreamGuestAngle ? ",gles=false" : ",gles=true";
   // 256MB so it is small enough for a 32-bit kernel.
   const std::string gpu_pci_bar_size = ",pci-bar-size=268435456";
   const std::string gpu_udmabuf_string =
@@ -202,9 +200,8 @@ Result<std::vector<MonitorCommand>> CrosvmManager::StartCommands(
   } else if (gpu_mode == kGpuModeGfxstream ||
              gpu_mode == kGpuModeGfxstreamGuestAngle) {
     const std::string capset_names = ",context-types=gfxstream";
-    crosvm_cmd.Cmd().AddParameter("--gpu=backend=gfxstream,gles31=true",
-                                  gpu_common_3d_string, gpu_angle_string,
-                                  capset_names);
+    crosvm_cmd.Cmd().AddParameter("--gpu=backend=gfxstream,vulkan=true",
+                                  gpu_common_3d_string, capset_names);
   }
 
   if (instance.hwcomposer() != kHwComposerNone) {
