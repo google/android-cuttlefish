204ba40efef328a55caeb9cd5260fcf9352eab01
Steven Moreland <smoreland@google.com>
2023-04-26 16:42:04 +0000

    Merge "install se HAL"

--- cvd/common/libs/utils/inotify.h
+++ cvd/common/libs/utils/inotify.h
@@ -13,10 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #pragma once
+
+#include <fruit/fruit.h>
 
 namespace cuttlefish {
 
-std::vector<std::string> GetCreatedFileListFromInotifyFd(int fd);
+fruit::Component<> CvdResetComponent();
 
 }  // namespace cuttlefish
--- cvd/host/commands/cvd/server_command/cmd_list.h
+++ cvd/host/commands/cvd/server_command/cmd_list.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 The Android Open Source Project
+ * Copyright (C) 2023 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,18 +16,13 @@
 
 #pragma once
 
-#include <atomic>
-
 #include <fruit/fruit.h>
 
 #include "host/commands/cvd/command_sequence.h"
 
 namespace cuttlefish {
 
-struct AcloudTranslatorOptOut {};
+fruit::Component<fruit::Required<CommandSequenceExecutor>>
+CvdCmdlistComponent();
 
-fruit::Component<fruit::Required<
-    CommandSequenceExecutor,
-    fruit::Annotated<AcloudTranslatorOptOut, std::atomic<bool>>>>
-AcloudCommandComponent();
-}
+}  // namespace cuttlefish
