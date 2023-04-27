a112544f57c6a899da2a562d540ad7978197ec5d
Wei-chung Hsu <weihsu@google.com>
2023-04-27 06:33:10 +0000

    Merge "Added flags for CF acloud create, "--local-system-image"." am: 164a34931c
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2399133
    
    Change-Id: I20eea13eac30ee2e4aab5c58411df4789ec0bba4
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 9b5bd0e1ac04c9b27567ba9844b41a088ba8e6d3

--- staging/host/commands/cvd/server_command/acloud_command.cpp
+++ staging/host/commands/cvd/server_command/acloud_command.cpp
@@ -46,7 +46,8 @@ class AcloudCommand : public CvdServerHandler {
     auto invocation = ParseInvocation(request.Message());
     if (invocation.arguments.size() >= 2) {
       if (invocation.command == "acloud" &&
-          invocation.arguments[0] == "translator") {
+          (invocation.arguments[0] == "translator" ||
+           invocation.arguments[0] == "mix-super-image")) {
         return false;
       }
     }
