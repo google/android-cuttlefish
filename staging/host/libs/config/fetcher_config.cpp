74e4bbc1df29384c88ae95f3264cf3a5490edf45
Joe Fradley <joefradley@google.com>
2023-05-02 14:33:25 +0000

    [skip ci] Merge "Adjust cvd_files paths if not in current directory" am: 760e5f3347
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2568932
    
    Change-Id: I800cd182c20eb8c55589f45aeb878edc2b57b39b
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 34f44555899416567b2cdf1bce439958115acbbb

--- staging/host/libs/config/fetcher_config.cpp
+++ staging/host/libs/config/fetcher_config.cpp
@@ -136,6 +136,17 @@ bool FetcherConfig::LoadFromFile(const std::string& file) {
     LOG(ERROR) << "Could not read config file " << file << ": " << errorMessage;
     return false;
   }
+
+  auto base_dir = cpp_dirname(file);
+  if (base_dir != "." && dictionary_->isMember(kCvdFiles)) {
+    LOG(INFO) << "Adjusting cvd_file paths to directory: " << base_dir;
+    for (const auto& member_name : (*dictionary_)[kCvdFiles].getMemberNames()) {
+      (*dictionary_)[kCvdFiles][base_dir + "/" + member_name] =
+          (*dictionary_)[kCvdFiles][member_name];
+      (*dictionary_)[kCvdFiles].removeMember(member_name);
+    }
+  }
+
   return true;
 }
 
