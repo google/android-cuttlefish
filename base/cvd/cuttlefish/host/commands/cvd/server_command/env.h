e10512732e877eb69ae475975c1d58fe1df61615
Steve Kim <kwstephenkim@google.com>
2023-05-09 23:51:57 +0000

    Merge "Send crosvm commands to group" am: 474f3cbfdc am: 853a6a7a71 am: 3306c6788e am: 9e8534b14a am: f648a5dcf7 am: ec0244e50d
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2579792
    
    Change-Id: Ibc12f9bf60b1a72e68b5941f72c693ffde6eb16b
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- base/cvd/cuttlefish/host/commands/cvd/server_command/env.h
+++ base/cvd/cuttlefish/host/commands/cvd/server_command/env.h
@@ -19,11 +19,9 @@
 #include <fruit/fruit.h>
 
 #include "host/commands/cvd/instance_manager.h"
-#include "host/commands/cvd/server_command/subprocess_waiter.h"
 
 namespace cuttlefish {
 
-fruit::Component<fruit::Required<InstanceManager, SubprocessWaiter>>
-CvdEnvComponent();
+fruit::Component<fruit::Required<InstanceManager>> CvdCrosVmComponent();
 
 }  // namespace cuttlefish
