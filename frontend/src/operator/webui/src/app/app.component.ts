12f2fe77f1eed7f07e327f38b64b455a311bc5e6
JaeMan Park <jaeman@google.com>
2023-05-09 18:07:48 +0900

    Show the version number in the lower right corner of the web UI

--- frontend/src/operator/webui/src/app/app.component.ts
+++ frontend/src/operator/webui/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, HostListener, Injectable} from '@angular/core';
 
 import {DisplaysService} from './displays.service';
+import {BUILD_VERSION} from '../environments/version'
 
 @Injectable()
 @Component({
@@ -9,6 +10,7 @@ import {DisplaysService} from './displays.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  readonly version = BUILD_VERSION;
   constructor(private displaysService: DisplaysService) {}
 
   @HostListener('window:message', ['$event'])
