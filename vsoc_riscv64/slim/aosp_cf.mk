f84c662866e3f8a5b71ed3a8e10f51974a8e9620
Deyao Ren <deyaoren@google.com>
2023-05-12 14:26:25 +0000

    Remove LOCAL_PREFER_VENDOR_APEX variable from .mk files for cuttlefish
    Bug: 259196894
    Test: launch cuttlefish, CtsWifiTestCases
    Change-Id: I82a8af4861e338c771d2ec1b539e891ac6628f5c

--- vsoc_riscv64/slim/aosp_cf.mk
+++ vsoc_riscv64/slim/aosp_cf.mk
@@ -48,7 +48,6 @@ PRODUCT_PACKAGES += FakeSystemApp
 #
 # All components inherited here go to vendor image
 #
-LOCAL_PREFER_VENDOR_APEX := true
 #$(call inherit-product, device/google/cuttlefish/shared/slim/device_vendor.mk)
 
 # TODO: FIXME: Start workaround for slim/device_vendor.mk ####################
