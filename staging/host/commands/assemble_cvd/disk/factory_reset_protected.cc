dd52704c99e9bc7b8ada702d82689d8a1203f9cf
Treehugger Robot <android-test-infra-autosubmit@system.gserviceaccount.com>
2023-05-20 03:35:59 +0000

    [skip ci] Merge "Separate factory reset protected to a separate file." am: 61945facde
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2595003
    
    Change-Id: I107beedfc24c6fbda854c2b8c057ec7eb38ffba2
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: d1cc7f16f8da4d802049c19f88270948e36b7e1b

---
+++ staging/host/commands/assemble_cvd/disk/factory_reset_protected.cc
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "host/commands/assemble_cvd/disk/disk.h"
+
+#include <string>
+
+#include "common/libs/utils/files.h"
+#include "common/libs/utils/result.h"
+#include "host/libs/config/data_image.h"
+
+namespace cuttlefish {
+
+class InitializeFactoryResetProtectedImpl
+    : public InitializeFactoryResetProtected {
+ public:
+  INJECT(InitializeFactoryResetProtectedImpl(
+      const CuttlefishConfig::InstanceSpecific& instance))
+      : instance_(instance) {}
+
+  // SetupFeature
+  std::string Name() const override { return "InitializeSdCard"; }
+  bool Enabled() const override { return !instance_.protected_vm(); }
+
+ private:
+  std::unordered_set<SetupFeature*> Dependencies() const override { return {}; }
+  Result<void> ResultSetup() override {
+    auto frp = instance_.factory_reset_protected_path();
+    if (FileExists(frp)) {
+      return {};
+    }
+    CF_EXPECT(CreateBlankImage(frp, 1 /* mb */, "none"),
+              "Failed to create \"" << frp << "\"");
+    return {};
+  }
+
+  const CuttlefishConfig::InstanceSpecific& instance_;
+};
+
+fruit::Component<fruit::Required<const CuttlefishConfig::InstanceSpecific>,
+                 InitializeFactoryResetProtected>
+InitializeFactoryResetProtectedComponent() {
+  return fruit::createComponent()
+      .bind<InitializeFactoryResetProtected,
+            InitializeFactoryResetProtectedImpl>()
+      .addMultibinding<SetupFeature, InitializeFactoryResetProtected>();
+}
+
+}  // namespace cuttlefish
