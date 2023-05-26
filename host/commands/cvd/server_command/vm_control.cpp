bf24bec3c530bead53548d9b00c0922af109b475
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2023-05-26 09:52:39 +0000

    Revert "Move unix sockets to /run/cuttlefish"
    This revert was created by Android Culprit Assistant. The culprit was identified in the following culprit search session (http://go/aca-get/8b9160a2-b923-489e-bece-f8d642456e88).
    
    This reverts commit c233775dc8158dee0c296b7a818ffad546cfd02d.
    
    Reason for revert: b/284414427, b/284405167
    
    Bug: 279085447
    Bug: 284414427
    Bug: 284405167
    Change-Id: Ie209071dfe0ccaba7267c8f29f4d7e63e5ee0032

--- host/commands/cvd/server_command/vm_control.cpp
+++ host/commands/cvd/server_command/vm_control.cpp
@@ -29,7 +29,6 @@
 
 #include "common/libs/fs/shared_buf.h"
 #include "common/libs/utils/contains.h"
-#include "common/libs/utils/files.h"
 #include "common/libs/utils/subprocess.h"
 #include "host/commands/cvd/flag.h"
 #include "host/commands/cvd/selector/instance_group_record.h"
@@ -283,13 +282,10 @@ class CvdVmControlCommandHandler : public CvdServerHandler {
       const std::string& crosvm_op, const cvd_common::Args& subcmd_args,
       const cvd_common::Envs& envs) {
     const auto& instance_group = instance.ParentGroup();
-    const auto& internal_name = instance.InternalDeviceName();
+    const auto instance_id = instance.InstanceId();
     auto home = instance_group.HomeDir();
-    const auto socket_parent_path = DirectoryExists("/run/cuttlefish")
-                                        ? "/run/cuttlefish/instances"
-                                        : "/tmp/cuttlefish/instances";
     const auto socket_file_path =
-        ConcatToString(socket_parent_path, "/", internal_name,
+        ConcatToString(home, "/cuttlefish_runtime.", instance_id,
                        "/internal/"
                        "crosvm_control.sock");
 
--- host/commands/stop/main.cc
+++ host/commands/stop/main.cc
@@ -64,7 +64,6 @@ std::set<std::string> DirsForInstance(
   return {
       config.assembly_dir(),
       instance.instance_dir(),
-      instance.instance_uds_dir(),
   };
 }
 
