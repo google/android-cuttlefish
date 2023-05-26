682db9a736a6ea72dba067541312827afbdccacc
JaeMan Park <jaeman@google.com>
2023-05-26 05:00:05 +0000

    Merge "Move unix sockets to /run/cuttlefish" into udc-dev-plus-aosp am: 811823f185 am: 7248b186a3 am: f13c0b2ce6
    Original change: https://googleplex-android-review.googlesource.com/c/device/google/cuttlefish/+/23358297
    
    Change-Id: I5c948f34455c98b747b292ff8077b87bd782c3d4
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- base/cvd/cuttlefish/host/libs/config/cuttlefish_config.cpp
+++ base/cvd/cuttlefish/host/libs/config/cuttlefish_config.cpp
@@ -569,6 +569,22 @@ std::string CuttlefishConfig::AssemblyPath(
   return AbsolutePath(assembly_dir() + "/" + file_name);
 }
 
+std::string CuttlefishConfig::instances_uds_dir() const {
+  if (DirectoryExists("/run/cuttlefish")) {
+    return AbsolutePath("/run/cuttlefish/instances");
+  } else {
+    // '/run/cuttlefish' directory can be non-exist when the packages for
+    // Cuttlefish is not installed. In that case, use the path '/tmp/cuttlefish'
+    // instead because creating directory under '/run/' requires root privilege.
+    return AbsolutePath("/tmp/cuttlefish/instances");
+  }
+}
+
+std::string CuttlefishConfig::InstancesUdsPath(
+    const std::string& file_name) const {
+  return AbsolutePath(instances_uds_dir() + "/" + file_name);
+}
+
 CuttlefishConfig::MutableInstanceSpecific CuttlefishConfig::ForInstance(int num) {
   return MutableInstanceSpecific(this, std::to_string(num));
 }
@@ -599,6 +615,7 @@ std::vector<std::string> CuttlefishConfig::instance_dirs() const {
   std::vector<std::string> result;
   for (const auto& instance : Instances()) {
     result.push_back(instance.instance_dir());
+    result.push_back(instance.instance_uds_dir());
   }
   return result;
 }
