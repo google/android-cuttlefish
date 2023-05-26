41ece29770ed65c9e247e5dcd1bf07b8c541f63a
JaeMan Park <jaeman@google.com>
2023-05-26 07:09:22 +0000

    [skip ci] Merge "Move unix sockets to /tmp/cuttlefish" am: d07d2c74c4
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2598285
    
    Change-Id: I0364bf6f0e269e608e2af285e60854a73b461792
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: b3617627198b7aee48d76592262d88bc29c0da5c

--- staging/host/commands/cvd/server_command/vm_control.cpp
+++ staging/host/commands/cvd/server_command/vm_control.cpp
@@ -29,6 +29,7 @@
 
 #include "common/libs/fs/shared_buf.h"
 #include "common/libs/utils/contains.h"
+#include "common/libs/utils/files.h"
 #include "common/libs/utils/subprocess.h"
 #include "host/commands/cvd/flag.h"
 #include "host/commands/cvd/selector/instance_group_record.h"
@@ -282,10 +283,10 @@ class CvdVmControlCommandHandler : public CvdServerHandler {
       const std::string& crosvm_op, const cvd_common::Args& subcmd_args,
       const cvd_common::Envs& envs) {
     const auto& instance_group = instance.ParentGroup();
-    const auto instance_id = instance.InstanceId();
+    const auto& internal_name = instance.InternalDeviceName();
     auto home = instance_group.HomeDir();
     const auto socket_file_path =
-        ConcatToString(home, "/cuttlefish_runtime.", instance_id,
+        ConcatToString("/tmp/cuttlefish/instances/", internal_name,
                        "/internal/"
                        "crosvm_control.sock");
 
