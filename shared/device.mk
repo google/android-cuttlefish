3b78c86bc4ef07b36d211a569e207973615f7efb
Jooyung Han <jooyung@google.com>
2023-05-31 11:26:42 +0900

    Install Face HAL APEX
    Bug: 279674662
    Test: VtsHalBiometricsFaceTargetTest
    Change-Id: I4dc60426df0603306020743fdb8672ce7b24bd32

--- shared/device.mk
+++ shared/device.mk
@@ -347,7 +347,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_AUDIO_DEVICE_PACKAGE_OVERLAYS)
 # BiometricsFace HAL (AIDL)
 #
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.face-service.example
+    com.android.hardware.biometrics.face
 
 #
 # BiometricsFingerprint HAL (AIDL)
