a2272a8973e80b75d2ad144fe378d249bedf9c6d
Treehugger Robot <treehugger-gerrit@google.com>
2023-06-01 02:22:14 +0000

    Merge "Build overlay for persistent composite image" am: f607160c54 am: dc44f59a14
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2598106
    
    Change-Id: Ie8ff2a73c1d4ecb7f2affe776fdc59af49179092
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- cvd/cuttlefish/host/libs/config/cuttlefish_config_instance.cpp
+++ cvd/cuttlefish/host/libs/config/cuttlefish_config_instance.cpp
@@ -698,6 +698,14 @@ bool CuttlefishConfig::InstanceSpecific::enable_audio() const {
   return (*Dictionary())[kEnableAudio].asBool();
 }
 
+static constexpr char kEnableVehicleHalServer[] = "enable_vehicle_hal_server";
+void CuttlefishConfig::MutableInstanceSpecific::set_enable_vehicle_hal_grpc_server(bool enable_vehicle_hal_grpc_server) {
+  (*Dictionary())[kEnableVehicleHalServer] = enable_vehicle_hal_grpc_server;
+}
+bool CuttlefishConfig::InstanceSpecific::enable_vehicle_hal_grpc_server() const {
+  return (*Dictionary())[kEnableVehicleHalServer].asBool();
+}
+
 static constexpr char kEnableGnssGrpcProxy[] = "enable_gnss_grpc_proxy";
 void CuttlefishConfig::MutableInstanceSpecific::set_enable_gnss_grpc_proxy(const bool enable_gnss_grpc_proxy) {
   (*Dictionary())[kEnableGnssGrpcProxy] = enable_gnss_grpc_proxy;
@@ -1015,9 +1023,20 @@ std::string CuttlefishConfig::InstanceSpecific::persistent_composite_disk_path()
   return AbsolutePath(PerInstancePath("persistent_composite.img"));
 }
 
+std::string
+CuttlefishConfig::InstanceSpecific::persistent_composite_overlay_path() const {
+  return AbsolutePath(PerInstancePath("persistent_composite_overlay.img"));
+}
+
 std::string CuttlefishConfig::InstanceSpecific::persistent_ap_composite_disk_path()
     const {
   return AbsolutePath(PerInstancePath("ap_persistent_composite.img"));
+}
+
+std::string
+CuttlefishConfig::InstanceSpecific::persistent_ap_composite_overlay_path()
+    const {
+  return AbsolutePath(PerInstancePath("ap_persistent_composite_overlay.img"));
 }
 
 std::string CuttlefishConfig::InstanceSpecific::os_composite_disk_path()
@@ -1296,6 +1315,14 @@ int CuttlefishConfig::InstanceSpecific::tombstone_receiver_port() const {
 }
 void CuttlefishConfig::MutableInstanceSpecific::set_tombstone_receiver_port(int tombstone_receiver_port) {
   (*Dictionary())[kTombstoneReceiverPort] = tombstone_receiver_port;
+}
+
+static constexpr char kVehicleHalServerPort[] = "vehicle_hal_server_port";
+int CuttlefishConfig::InstanceSpecific::vehicle_hal_server_port() const {
+  return (*Dictionary())[kVehicleHalServerPort].asInt();
+}
+void CuttlefishConfig::MutableInstanceSpecific::set_vehicle_hal_server_port(int vehicle_hal_server_port) {
+  (*Dictionary())[kVehicleHalServerPort] = vehicle_hal_server_port;
 }
 
 static constexpr char kAudioControlServerPort[] = "audiocontrol_server_port";
