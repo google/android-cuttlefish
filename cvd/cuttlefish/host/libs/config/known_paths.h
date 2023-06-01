748ff009edfdd141544876bed1f714f21e77deca
Alistair Delva <adelva@google.com>
2023-06-01 02:03:48 +0000

    [automerger skipped] Revert^2 "Generate ext4/f2fs userdata fstabs at build time" am: 44d4dab7d4 -s ours am: 75973d3b68 -s ours
    am skip reason: Merged-In I34503af2e479fe7ec3dcca6a6301e4046936eaa4 with SHA-1 187bbe38ee is already in history
    
    Original change: https://googleplex-android-review.googlesource.com/c/device/google/cuttlefish/+/23447983
    
    Change-Id: I888010133b714a29315fe6c83e594485a780452b
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- cvd/cuttlefish/host/libs/config/known_paths.h
+++ cvd/cuttlefish/host/libs/config/known_paths.h
@@ -38,7 +38,6 @@ std::string SocketVsockProxyBinary();
 std::string StopCvdBinary();
 std::string TcpConnectorBinary();
 std::string TombstoneReceiverBinary();
-std::string VehicleHalGrpcServerBinary();
 std::string WebRtcBinary();
 std::string WebRtcSigServerBinary();
 std::string WebRtcSigServerProxyBinary();
