d3b68dac3cdc7431687aacae2b183af5f4429505
Dmitrii Merkurev <dimorinny@google.com>
2023-06-09 12:08:09 +0000

    [skip ci] Merge "Use ManagedMessage in channel signatures" am: 51c21c9f04
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2615650
    
    Change-Id: I64355d9908745de02047a91e95af2e8d7a0e4a0b
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    GitOrigin-RevId: 911cc44db44c79366ea89ba75b756ba0eec41604

--- staging/common/libs/security/channel_sharedfd.cpp
+++ staging/common/libs/security/channel_sharedfd.cpp
@@ -20,35 +20,16 @@
 
 namespace cuttlefish {
 namespace secure_env {
-namespace {
-
-/**
- * Allocates memory for a RawMessage carrying a message of size
- * `payload_size`.
- */
-Result<ManagedMessage> CreateMessage(uint32_t command, bool is_response, size_t payload_size) {
-  const auto bytes_to_allocate = sizeof(RawMessage) + payload_size;
-  auto memory = std::malloc(bytes_to_allocate);
-  CF_EXPECT(memory != nullptr,
-            "Cannot allocate " << bytes_to_allocate << " bytes for secure_env RPC message");
-  auto message = reinterpret_cast<RawMessage*>(memory);
-  message->command = command;
-  message->is_response = is_response;
-  message->payload_size = payload_size;
-  return ManagedMessage(message);
-}
-
-}
 
 SharedFdChannel::SharedFdChannel(SharedFD input, SharedFD output)
     : input_(std::move(input)), output_(std::move(output)) {}
 
-Result<void> SharedFdChannel::SendRequest(uint32_t command, void* message, size_t message_size) {
-  return SendMessage(command, false, message, message_size);
+Result<void> SharedFdChannel::SendRequest(RawMessage& message) {
+  return SendMessage(message, false);
 }
 
-Result<void> SharedFdChannel::SendResponse(uint32_t command, void* message, size_t message_size) {
-  return SendMessage(command, true, message, message_size);
+Result<void> SharedFdChannel::SendResponse(RawMessage& message) {
+  return SendMessage(message, true);
 }
 
 Result<ManagedMessage> SharedFdChannel::ReceiveMessage() {
@@ -69,14 +50,12 @@ Result<ManagedMessage> SharedFdChannel::ReceiveMessage() {
   return message;
 }
 
-Result<void> SharedFdChannel::SendMessage(uint32_t command, bool response,
-                                          void* message, size_t message_size) {
-  auto to_send = CF_EXPECT(CreateMessage(command, response, message_size));
-  memcpy(to_send->payload, message, message_size);
-  auto write_size = sizeof(RawMessage) + message_size;
-  auto to_send_bytes = reinterpret_cast<const char*>(to_send.get());
-  auto written = WriteAll(output_, to_send_bytes, write_size);
-  CF_EXPECT(written != -1,
+Result<void> SharedFdChannel::SendMessage(RawMessage& message, bool response) {
+  message.is_response = response;
+  auto write_size = sizeof(RawMessage) + message.payload_size;
+  auto message_bytes = reinterpret_cast<const char*>(&message);
+  auto written = WriteAll(output_, message_bytes, write_size);
+  CF_EXPECT(written == write_size,
             "Could not write message: " << output_->StrError());
   return {};
 }
