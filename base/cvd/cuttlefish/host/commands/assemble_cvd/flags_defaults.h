2126d149a225719a57fecb7714a29465730ea4e0
Cody Schuffelen <schuffelen@google.com>
2023-06-14 04:29:27 +0000

    Merge "Disable interaction with the core scheduling feature." am: 7d6ba31108 am: c6be62ea1c am: cff0f8db74 am: 77ab230d70 am: 1050418312 am: 480b15b790
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2625111
    
    Change-Id: I160dc6891170e271b15f49da14355cada989b4dd
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- base/cvd/cuttlefish/host/commands/assemble_cvd/flags_defaults.h
+++ base/cvd/cuttlefish/host/commands/assemble_cvd/flags_defaults.h
@@ -54,7 +54,7 @@
 #define CF_DEFAULTS_VERBOSITY "INFO"
 #define CF_DEFAULTS_RUN_FILE_DISCOVERY true
 #define CF_DEFAULTS_MEMORY_MB CF_DEFAULTS_DYNAMIC_INT
-#define CF_DEFAULTS_SHARE_SCHED_CORE true
+#define CF_DEFAULTS_SHARE_SCHED_CORE false
 #define CF_DEFAULTS_TRACK_HOST_TOOLS_CRC false
 // TODO: defined twice, please remove redundant definitions
 #define CF_DEFAULTS_USE_OVERLAY true
