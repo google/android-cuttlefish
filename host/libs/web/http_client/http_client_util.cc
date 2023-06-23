19c543c8b00dcc0742d62673113e3e257d82ee22
Chad Reynolds <chadreynolds@google.com>
2023-06-23 14:06:39 -0700

    Scrub and LOG cURL outputs
    This prevents user credentials from being leaked, and redirects cURL
    outputs to android logging instead of stderr.  I did not line-by-line
    compare, but the number of lines is approximately the same as before, so
    I assume this is not removing any information that was previously
    displayed.  I did adjust the logging levels so that less is output by
    default.
    
    I thought about making it the default behavior, but given the change in
    output I decided to avoid updates for anywhere outside of `fetch_cvd`.
    
    Bug: 284213006
    Test: fetch_cvd --target_directory=fetch_test
    --default_build=aosp-master/aosp_cf_x86_64_phone-userdebug
    --verbosity=DEBUG 2> fetch.log
    Test: grep "Authorization: Bearer" fetch.log
    Test: verify the token is truncated
    Test: atest --host --no-bazel-mode libcuttlefish_web_test
    
    Change-Id: I13b12519916876046aff7093ed04810ef39e327a

---
+++ host/libs/web/http_client/http_client_util.cc
@@ -0,0 +1,37 @@
+//
+// Copyright (C) 2023 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "host/libs/web/http_client/http_client_util.h"
+
+#include <regex>
+#include <string>
+
+namespace cuttlefish {
+
+std::string ScrubSecrets(const std::string& data) {
+  std::string result = data;
+  // eg [<head>]Authorization: Bearer token_text[<tail>] ->
+  //    [<head>]Authorization: Bearer token_...[<tail>]
+  result = std::regex_replace(
+      result, std::regex("(Authorization:[ ]+\\S+[ ]+)(\\S{6})\\S*"),
+      "$1$2...");
+  // eg [<head>]client_secret=token_text[<tail>] ->
+  //    [<head>]client_secret=token_...[<tail>]
+  result = std::regex_replace(
+      result, std::regex("(client_secret=)(\\S{6})[^\\&\\s]*"), "$1$2...");
+  return result;
+}
+
+}  // namespace cuttlefish
