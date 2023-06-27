700f6389c07a39244dbe1519c954555b9ebc3e10
Treehugger Robot <treehugger-gerrit@google.com>
2023-06-27 01:08:25 +0000

    Merge "Swap JSON defaults to null for fetch flags" am: 00a1706165
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2631358
    
    Change-Id: I790428fa7489d3dc4a2d4e1438022339f852b8af
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- cvd/cuttlefish/host/commands/cvd/parser/fetch_cvd_parser.cpp
+++ cvd/cuttlefish/host/commands/cvd/parser/fetch_cvd_parser.cpp
@@ -16,6 +16,7 @@
 
 #include "host/commands/cvd/parser/fetch_cvd_parser.h"
 
+#include <algorithm>
 #include <string>
 #include <vector>
 
@@ -26,39 +27,31 @@
 namespace cuttlefish {
 namespace {
 
-#define EMPTY_CREDENTIAL ""
-#define EMPTY_DEFAULT_BUILD ""
-#define EMPTY_SYSTEM_BUILD ""
-#define EMPTY_KERNEL_BUILD ""
-
 void InitFetchInstanceConfigs(Json::Value& instances) {
-  // Handle common flags
-  InitStringConfig(instances, "disk", "default_build", EMPTY_DEFAULT_BUILD);
-  InitStringConfig(instances, "disk", "system_build", EMPTY_SYSTEM_BUILD);
-  InitStringConfig(instances, "disk", "kernel_build", EMPTY_KERNEL_BUILD);
+  InitNullGroupConfig(instances, "disk", "default_build");
+  InitNullGroupConfig(instances, "disk", "system_build");
+  InitNullGroupConfig(instances, "disk", "kernel_build");
 }
 
 void InitFetchCvdConfigs(Json::Value& root) {
-  if (!root.isMember("credential_source")) {
-    root["credential_source"] = EMPTY_CREDENTIAL;
-  }
+  InitNullConfig(root, "credential_source");
   InitFetchInstanceConfigs(root["instances"]);
 }
 
-FetchCvdInstanceConfig ParseFetchInstanceConfigs(const Json::Value& instance) {
-  FetchCvdInstanceConfig result;
-  result.default_build = instance["disk"]["default_build"].asString();
-  result.system_build = instance["disk"]["system_build"].asString();
-  result.kernel_build = instance["disk"]["kernel_build"].asString();
-  if (result.default_build != EMPTY_DEFAULT_BUILD ||
-      result.system_build != EMPTY_SYSTEM_BUILD ||
-      result.kernel_build != EMPTY_KERNEL_BUILD) {
-    result.should_fetch = true;
-  } else {
-    result.should_fetch = false;
-  }
+bool ShouldFetch(const std::vector<Json::Value>& values) {
+  return std::any_of(std::begin(values), std::end(values),
+                     [](const Json::Value& value) { return !value.isNull(); });
+}
 
-  return result;
+FetchCvdInstanceConfig ParseFetchInstanceConfigs(const Json::Value& instance) {
+  Json::Value default_build = instance["disk"]["default_build"];
+  Json::Value system_build = instance["disk"]["system_build"];
+  Json::Value kernel_build = instance["disk"]["kernel_build"];
+  return FetchCvdInstanceConfig{
+      .default_build = default_build.asString(),
+      .system_build = system_build.asString(),
+      .kernel_build = kernel_build.asString(),
+      .should_fetch = ShouldFetch({default_build, system_build, kernel_build})};
 }
 
 FetchCvdConfig GenerateFetchCvdFlags(const Json::Value& root) {
