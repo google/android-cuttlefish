c22d1e6bc2519d63b67f6d91ddacf71cb8d52c80
Chad Reynolds <chadreynolds@google.com>
2023-06-28 04:36:42 +0000

    Merge "Add new verbosity flag" am: f1eb027db8 am: 723d7afe2e am: 55c695dc3a am: 3b8fd31d24 am: 6e8ec6d135 am: a393fd45dc
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2636735
    
    Change-Id: I5d27d82b6a134a6d433e427058c9de6e9e08db5f
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- base/cvd/cuttlefish/common/libs/utils/flag_parser.h
+++ base/cvd/cuttlefish/common/libs/utils/flag_parser.h
@@ -143,6 +143,10 @@ bool ParseFlags(const std::vector<Flag>& flags, std::vector<std::string>&&,
 
 bool WriteGflagsCompatXml(const std::vector<Flag>&, std::ostream&);
 
+/* If -verbosity or --verbosity flags have a value, translates it to an android
+ * LogSeverity */
+Flag VerbosityFlag(android::base::LogSeverity& value);
+
 /* If any of these are used, they should be evaluated after all other flags, and
  * in the order defined here (help before invalid flags, invalid flags before
  * unexpected arguments). */
