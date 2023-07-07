dbe9e2bea8416d2053a3ae2474d0963ac885d74b
Dmitrii Merkurev <dimorinny@google.com>
2023-07-07 12:34:41 +0100

    Remove misc_image flag
    Getting rid of redundant misc_image related flags and
    json config fields
    
    Test: cvd start cold/warm
    Bug: 290332265
    Change-Id: Ia7ce7fe0407567353405a69a537861883621ec93
    Signed-off-by: Dmitrii Merkurev <dimorinny@google.com>

--- cvd/host/libs/config/cuttlefish_config.h
+++ cvd/host/libs/config/cuttlefish_config.h
@@ -568,7 +568,6 @@ class CuttlefishConfig {
     std::string super_image() const;
     std::string new_super_image() const;
     std::string misc_image() const;
-    std::string new_misc_image() const;
     std::string misc_info_txt() const;
     std::string metadata_image() const;
     std::string new_metadata_image() const;
@@ -748,8 +747,6 @@ class CuttlefishConfig {
     void set_new_data_image(const std::string& new_data_image);
     void set_super_image(const std::string& super_image);
     void set_new_super_image(const std::string& super_image);
-    void set_misc_image(const std::string& misc_image);
-    void set_new_misc_image(const std::string& new_misc_image);
     void set_misc_info_txt(const std::string& misc_image);
     void set_metadata_image(const std::string& metadata_image);
     void set_new_metadata_image(const std::string& new_metadata_image);
