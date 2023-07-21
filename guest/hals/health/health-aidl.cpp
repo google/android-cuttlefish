5644784fe3331851611070c9428ee2d68ca2f3b6
Peter Collingbourne <pcc@google.com>
2023-07-21 14:33:09 -0700

    Reduce reported battery temperature to 25°C.
    One of Chromium's benchmarks [1] waits for the temperature to drop
    below 32°C before starting, so we would never start this test
    on Cuttlefish. This check seems reasonable for real devices but
    for Cuttlefish we basically want these types of checks to always
    pass. Therefore, switch to a lower but still realistic 25°C so we have
    a better chance of passing these types of checks.
    
    [1] https://crsrc.org/c/tools/perf/benchmarks/startup_mobile.py;drc=34ab1c02f040dd965998bf28993b4f6a1f826b8f;l=131
    
    Change-Id: I8887e95f7ca69c7dfbe27dce4b9b334cf6c9858f

--- guest/hals/health/health-aidl.cpp
+++ guest/hals/health/health-aidl.cpp
@@ -69,7 +69,7 @@ void HealthImpl::UpdateHealthInfo(HealthInfo* health_info) {
   health_info->batteryPresent = true;
   health_info->batteryLevel = 85;
   health_info->batteryVoltageMillivolts = 3600;
-  health_info->batteryTemperatureTenthsCelsius = 350;
+  health_info->batteryTemperatureTenthsCelsius = 250;
   health_info->batteryCurrentMicroamps = 400000;
   health_info->batteryCycleCount = 32;
   health_info->batteryFullChargeUah = 4000000;
