8cc9906771865d5f12f0a7db6bcc5ca3516125a0
Treehugger Robot <android-test-infra-autosubmit@system.gserviceaccount.com>
2023-07-24 22:29:21 +0000

    Merge "Fix typos/errors" into main

--- cvd/host/libs/config/cuttlefish_config_instance.cpp
+++ cvd/host/libs/config/cuttlefish_config_instance.cpp
@@ -159,18 +159,6 @@ void CuttlefishConfig::MutableInstanceSpecific::set_new_super_image(
     const std::string& super_image) {
   (*Dictionary())[kNewSuperImage] = super_image;
 }
-static constexpr char kMiscImage[] = "misc_image";
-std::string CuttlefishConfig::InstanceSpecific::misc_image() const {
-  return (*Dictionary())[kMiscImage].asString();
-}
-void CuttlefishConfig::MutableInstanceSpecific::set_misc_image(
-    const std::string& misc_image) {
-  (*Dictionary())[kMiscImage] = misc_image;
-}
-static constexpr char kNewMiscImage[] = "new_misc_image";
-std::string CuttlefishConfig::InstanceSpecific::new_misc_image() const {
-  return (*Dictionary())[kNewMiscImage].asString();
-}
 static constexpr char kMiscInfoTxt[] = "misc_info_txt";
 std::string CuttlefishConfig::InstanceSpecific::misc_info_txt() const {
   return (*Dictionary())[kMiscInfoTxt].asString();
@@ -178,26 +166,6 @@ std::string CuttlefishConfig::InstanceSpecific::misc_info_txt() const {
 void CuttlefishConfig::MutableInstanceSpecific::set_misc_info_txt(
     const std::string& misc_info) {
   (*Dictionary())[kMiscInfoTxt] = misc_info;
-}
-void CuttlefishConfig::MutableInstanceSpecific::set_new_misc_image(
-    const std::string& new_misc_image) {
-  (*Dictionary())[kNewMiscImage] = new_misc_image;
-}
-static constexpr char kMetadataImage[] = "metadata_image";
-std::string CuttlefishConfig::InstanceSpecific::metadata_image() const {
-  return (*Dictionary())[kMetadataImage].asString();
-}
-void CuttlefishConfig::MutableInstanceSpecific::set_metadata_image(
-    const std::string& metadata_image) {
-  (*Dictionary())[kMetadataImage] = metadata_image;
-}
-static constexpr char kNewMetadataImage[] = "new_metadata_image";
-std::string CuttlefishConfig::InstanceSpecific::new_metadata_image() const {
-  return (*Dictionary())[kNewMetadataImage].asString();
-}
-void CuttlefishConfig::MutableInstanceSpecific::set_new_metadata_image(
-    const std::string& new_metadata_image) {
-  (*Dictionary())[kNewMetadataImage] = new_metadata_image;
 }
 static constexpr char kVendorBootImage[] = "vendor_boot_image";
 std::string CuttlefishConfig::InstanceSpecific::vendor_boot_image() const {
@@ -1068,6 +1036,14 @@ std::string CuttlefishConfig::InstanceSpecific::launcher_log_path() const {
   return AbsolutePath(PerInstanceLogPath("launcher.log"));
 }
 
+std::string CuttlefishConfig::InstanceSpecific::metadata_image() const {
+  return AbsolutePath(PerInstancePath("metadata.img"));
+}
+
+std::string CuttlefishConfig::InstanceSpecific::misc_image() const {
+  return AbsolutePath(PerInstancePath("misc.img"));
+}
+
 std::string CuttlefishConfig::InstanceSpecific::sdcard_path() const {
   return AbsolutePath(PerInstancePath("sdcard.img"));
 }
@@ -1398,6 +1374,15 @@ void CuttlefishConfig::MutableInstanceSpecific::set_webrtc_device_id(
 }
 std::string CuttlefishConfig::InstanceSpecific::webrtc_device_id() const {
   return (*Dictionary())[kWebrtcDeviceId].asString();
+}
+
+static constexpr char kGroupId[] = "group_id";
+void CuttlefishConfig::MutableInstanceSpecific::set_group_id(
+    const std::string& id) {
+  (*Dictionary())[kGroupId] = id;
+}
+std::string CuttlefishConfig::InstanceSpecific::group_id() const {
+  return (*Dictionary())[kGroupId].asString();
 }
 
 static constexpr char kStartSigServer[] = "webrtc_start_sig_server";
