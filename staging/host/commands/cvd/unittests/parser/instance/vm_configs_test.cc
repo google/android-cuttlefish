3ce15dadda9b86e3e929dc4de3b6861f5f6b83b6
Chad Reynolds <chadreynolds@google.com>
2023-07-31 16:41:36 +0000

    [skip ci] Merge changes Iac40a00b,I28b3d590 into main
    * changes:
      Remove MVP gate in prep for outputing more flags
      Update unit tests to use assertion helpers
    
    GitOrigin-RevId: f6c6087a76bdec6bbc80b5f8306bb4576e156e76

--- staging/host/commands/cvd/unittests/parser/instance/vm_configs_test.cc
+++ staging/host/commands/cvd/unittests/parser/instance/vm_configs_test.cc
@@ -554,7 +554,6 @@ TEST(VmFlagsParserTest, ParseTwoInstancesSetupWizardFlagFullJson) {
       << "setupwizard_mode flag is missing or wrongly formatted";
 }
 
-#ifndef GENERATE_MVP_FLAGS_ONLY
 TEST(VmFlagsParserTest, ParseTwoInstancesUuidFlagEmptyJson) {
   const char* test_string = R""""(
 {
@@ -659,7 +658,6 @@ TEST(VmFlagsParserTest, ParseTwoInstancesUuidFlagFullJson) {
       R"(--uuid=870acfc4-c8c4-11e7-99ac-5065f31dc250,870acfc4-c8c4-11e7-99ac-5065f31dc251)"))
       << "uuid flag is missing or wrongly formatted";
 }
-#endif
 
 TEST(VmFlagsParserTest, ParseTwoInstancesSandboxFlagEmptyJson) {
   const char* test_string = R""""(
