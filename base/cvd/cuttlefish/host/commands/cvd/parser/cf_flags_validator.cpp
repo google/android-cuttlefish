dd36b908beef4ad89139f35a56422bdae7ed6a49
Treehugger Robot <android-test-infra-autosubmit@system.gserviceaccount.com>
2023-08-01 23:05:12 +0000

    Merge changes I9a513bd3,I39ae580a,I8bdb1f92,Ib041459c into main am: 1aa5bccf9b am: 7b40cb852d am: f1fbca56a0 am: a973d924f8 am: f26a856454
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2681220
    
    Change-Id: Iceb56c0b55e4ed16876da10ca2c7be300cbad229
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- base/cvd/cuttlefish/host/commands/cvd/parser/cf_flags_validator.cpp
+++ base/cvd/cuttlefish/host/commands/cvd/parser/cf_flags_validator.cpp
@@ -32,13 +32,18 @@ namespace {
 
 // json main parameters definitions
 static std::map<std::string, Json::ValueType> kConfigsKeyMap = {
+    {"netsim_bt", Json::ValueType::booleanValue},
+    {"instances", Json::ValueType::arrayValue},
+    {"fetch", Json::ValueType::objectValue},
+};
+
+static std::map<std::string, Json::ValueType> kFetchKeyMap = {
     {"api_key", Json::ValueType::stringValue},
     {"credential", Json::ValueType::stringValue},
     {"wait_retry_period", Json::ValueType::uintValue},
     {"external_dns_resolver", Json::ValueType::booleanValue},
     {"keep_downloaded_archives", Json::ValueType::booleanValue},
-    {"netsim_bt", Json::ValueType::booleanValue},
-    {"instances", Json::ValueType::arrayValue}};
+};
 
 // instance object parameters definitions
 static std::map<std::string, Json::ValueType> kInstanceKeyMap = {
@@ -81,9 +86,17 @@ static std::map<std::string, Json::ValueType> kBootKeyMap = {
     {"extra_bootconfig_args", Json::ValueType::stringValue},
     {"kernel", Json::ValueType::objectValue},
     {"enable_bootanimation", Json::ValueType::booleanValue},
+    {"build", Json::ValueType::stringValue},
+    {"bootloader", Json::ValueType::objectValue},
 };
-static std::map<std::string, Json::ValueType> kernelkeyMap = {
+
+static std::map<std::string, Json::ValueType> kKernelKeyMap = {
     {"extra_kernel_cmdline", Json::ValueType::stringValue},
+    {"build", Json::ValueType::stringValue},
+};
+
+static std::map<std::string, Json::ValueType> kBootloaderKeyMap = {
+    {"build", Json::ValueType::stringValue},
 };
 
 // supported import values for graphics category and displays subcategory
@@ -100,20 +113,25 @@ static std::map<std::string, Json::ValueType> kDisplayKeyMap = {
 // supported import values for security category
 static std::map<std::string, Json::ValueType> kSecurityKeyMap = {
     {"serial_number", Json::ValueType::stringValue},
+    {"use_random_serial", Json::ValueType::stringValue},
     {"guest_enforce_security", Json::ValueType::booleanValue},
 };
 
 // supported import values for disk category
 static std::map<std::string, Json::ValueType> kDiskKeyMap = {
     {"default_build", Json::ValueType::stringValue},
-    {"system_build", Json::ValueType::stringValue},
-    {"kernel_build", Json::ValueType::stringValue},
+    {"super", Json::ValueType::objectValue},
     {"download_img_zip", Json::ValueType::booleanValue},
     {"download_target_zip_files", Json::ValueType::booleanValue},
     {"blank_data_image_mb", Json::ValueType::uintValue},
+    {"otatools", Json::ValueType::stringValue},
+    {"host_package", Json::ValueType::stringValue},
 };
 
-// Validate the security json parameters
+static std::map<std::string, Json::ValueType> kSuperKeyMap = {
+    {"system", Json::ValueType::stringValue},
+};
+
 Result<void> ValidateSecurityConfigs(const Json::Value& root) {
   CF_EXPECT(ValidateTypo(root, kSecurityKeyMap),
             "ValidateSecurityConfigs ValidateTypo fail");
@@ -122,6 +140,10 @@ Result<void> ValidateSecurityConfigs(const Json::Value& root) {
 Result<void> ValidateDiskConfigs(const Json::Value& root) {
   CF_EXPECT(ValidateTypo(root, kDiskKeyMap),
             "ValidateDiskConfigs ValidateTypo fail");
+  if (root.isMember("super")) {
+    CF_EXPECT(ValidateTypo(root["super"], kSuperKeyMap),
+              "ValidateDiskSuperConfigs ValidateTypo fail");
+  }
   return {};
 }
 
@@ -159,23 +181,28 @@ Result<void> ValidateVmConfigs(const Json::Value& root) {
   return {};
 }
 
-// Validate the kernel json parameters
 Result<void> ValidateKernelConfigs(const Json::Value& root) {
-  CF_EXPECT(ValidateTypo(root, kernelkeyMap),
+  CF_EXPECT(ValidateTypo(root, kKernelKeyMap),
             "ValidateKernelConfigs ValidateTypo fail");
   return {};
 }
 
-// Validate the boot json parameters
+Result<void> ValidateBootloaderConfigs(const Json::Value& root) {
+  CF_EXPECT(ValidateTypo(root, kBootloaderKeyMap),
+            "ValidateBootloaderConfigs ValidateTypo fail");
+  return {};
+}
+
 Result<void> ValidateBootConfigs(const Json::Value& root) {
   CF_EXPECT(ValidateTypo(root, kBootKeyMap),
             "ValidateBootConfigs ValidateTypo fail");
 
   if (root.isMember("kernel")) {
-    CF_EXPECT(ValidateKernelConfigs(root["kernel"]),
-              "ValidateKernelConfigs fail");
+    CF_EXPECT(ValidateKernelConfigs(root["kernel"]));
   }
-
+  if (root.isMember("bootloader")) {
+    CF_EXPECT(ValidateBootloaderConfigs(root["bootloader"]));
+  }
   return {};
 }
 
@@ -228,6 +255,8 @@ Result<void> ValidateInstancesConfigs(const Json::Value& instances) {
 Result<void> ValidateCfConfigs(const Json::Value& root) {
   CF_EXPECT(ValidateTypo(root, kConfigsKeyMap),
             "Typo in config main parameters");
+  CF_EXPECT(ValidateTypo(root["fetch"], kFetchKeyMap),
+            "Typo in config fetch parameters");
   CF_EXPECT(root.isMember("instances"), "instances object is missing");
   CF_EXPECT(ValidateInstancesConfigs(root["instances"]),
             "ValidateInstancesConfigs failed");
