67056f79d0f66c7c6466dc184ff6e3f5c91107a4
Treehugger Robot <android-test-infra-autosubmit@system.gserviceaccount.com>
2023-08-01 18:14:38 +0000

    [skip ci] Merge changes I9a513bd3,I39ae580a,I8bdb1f92,Ib041459c into main
    * changes:
      Add prefix-enforcement to fetch targets
      Add the remaining fetch_cvd flags to cvd load
      Adjusting flag selectors to match spec
      Remove duplicate paths of behavior
    
    GitOrigin-RevId: 1aa5bccf9b5c1b5e3c3cf61910509d9df3a24d9d

--- staging/host/commands/cvd/unittests/parser/fetch_cvd_parser_tests.cpp
+++ staging/host/commands/cvd/unittests/parser/fetch_cvd_parser_tests.cpp
@@ -16,8 +16,10 @@
 
 #include "host/commands/cvd/parser/fetch_cvd_parser.h"
 
+#include <optional>
 #include <string>
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include <json/json.h>
 
@@ -25,6 +27,12 @@
 #include "common/libs/utils/result_matchers.h"
 #include "host/commands/cvd/parser/cf_flags_validator.h"
 #include "host/commands/cvd/unittests/parser/test_common.h"
+
+using ::testing::Eq;
+using ::testing::Ne;
+using ::testing::Not;
+using ::testing::SizeIs;
+using ::testing::StartsWith;
 
 namespace cuttlefish {
 namespace {
@@ -55,21 +63,95 @@ TEST(FetchCvdParserTests, SingleFetch) {
         "cpus" : 4
       },
       "disk" : {
-        "default_build" : "git_master/cf_x86_64_phone-userdebug",
-        "download_img_zip" : true
+        "default_build" : "@ab/git_master/cf_x86_64_phone-userdebug",
+        "download_img_zip" : true,
+        "otatools" : "@ab/git_master/cf_x86_64_phone-userdebug",
+        "host_package" : "@ab/git_master/cf_x86_64_phone-userdebug"
+      },
+      "boot" : {
+        "build" : "@ab/git_master/cf_x86_64_phone-userdebug",
+        "kernel" : {
+          "build" : "@ab/git_master/cf_x86_64_phone-userdebug"
+        },
+        "bootloader" : {
+          "build" : "@ab/git_master/cf_x86_64_phone-userdebug"
+        }
       }
     }
   ],
-  "wait_retry_period" : 20,
-  "keep_downloaded_archives" : false
+  "fetch":{
+    "wait_retry_period" : 20,
+    "keep_downloaded_archives" : false
+  }
 }
   )"""";
   Json::Value json_config = GetTestJson(raw_json);
 
   auto result_config = FetchCvdParserTestHelper(json_config);
 
-  EXPECT_THAT(result_config, IsOk())
+  ASSERT_THAT(result_config, IsOk())
       << "Parsing config failed:  " << result_config.error().Trace();
+  const auto top_level_config = result_config.value();
+  EXPECT_THAT(top_level_config.wait_retry_period, Ne(std::nullopt));
+  EXPECT_THAT(top_level_config.keep_downloaded_archives, Ne(std::nullopt));
+
+  ASSERT_THAT(top_level_config.instances, SizeIs(1));
+  const auto instance_config = *top_level_config.instances.cbegin();
+  EXPECT_THAT(instance_config.default_build,
+              AllOf(Ne(std::nullopt), Not(Optional(StartsWith("@ab/")))));
+  EXPECT_THAT(instance_config.download_img_zip, Ne(std::nullopt));
+  EXPECT_THAT(instance_config.otatools_build,
+              AllOf(Ne(std::nullopt), Not(Optional(StartsWith("@ab/")))));
+  EXPECT_THAT(instance_config.host_package_build,
+              AllOf(Ne(std::nullopt), Not(Optional(StartsWith("@ab/")))));
+  EXPECT_THAT(instance_config.boot_build,
+              AllOf(Ne(std::nullopt), Not(Optional(StartsWith("@ab/")))));
+  EXPECT_THAT(instance_config.kernel_build,
+              AllOf(Ne(std::nullopt), Not(Optional(StartsWith("@ab/")))));
+  EXPECT_THAT(instance_config.bootloader_build,
+              AllOf(Ne(std::nullopt), Not(Optional(StartsWith("@ab/")))));
+}
+
+TEST(FetchCvdParserTests, SingleFetchNoPrefix) {
+  const char* raw_json = R""""(
+{
+  "instances" : [
+    {
+      "@import" : "phone",
+      "disk" : {
+        "default_build" : "git_master/cf_x86_64_phone-userdebug",
+        "otatools" : "git_master/cf_x86_64_phone-userdebug",
+        "host_package" : "git_master/cf_x86_64_phone-userdebug"
+      },
+      "boot" : {
+        "build" : "git_master/cf_x86_64_phone-userdebug",
+        "kernel" : {
+          "build" : "git_master/cf_x86_64_phone-userdebug"
+        },
+        "bootloader" : {
+          "build" : "git_master/cf_x86_64_phone-userdebug"
+        }
+      }
+    }
+  ]
+}
+  )"""";
+  Json::Value json_config = GetTestJson(raw_json);
+
+  auto result_config = FetchCvdParserTestHelper(json_config);
+
+  ASSERT_THAT(result_config, IsOk())
+      << "Parsing config failed:  " << result_config.error().Trace();
+  const auto top_level_config = result_config.value();
+
+  ASSERT_THAT(top_level_config.instances, SizeIs(1));
+  const auto instance_config = *top_level_config.instances.cbegin();
+  EXPECT_THAT(instance_config.default_build, Eq(std::nullopt));
+  EXPECT_THAT(instance_config.otatools_build, Eq(std::nullopt));
+  EXPECT_THAT(instance_config.host_package_build, Eq(std::nullopt));
+  EXPECT_THAT(instance_config.boot_build, Eq(std::nullopt));
+  EXPECT_THAT(instance_config.kernel_build, Eq(std::nullopt));
+  EXPECT_THAT(instance_config.bootloader_build, Eq(std::nullopt));
 }
 
 TEST(FetchCvdParserTests, MultiFetch) {
@@ -84,8 +166,19 @@ TEST(FetchCvdParserTests, MultiFetch) {
         "cpus" : 4
       },
       "disk" : {
-        "default_build" : "git_master/cf_x86_64_phone-userdebug",
-        "download_img_zip" : true
+        "default_build" : "@ab/git_master/cf_x86_64_phone-userdebug",
+        "download_img_zip" : true,
+        "otatools" : "@ab/git_master/cf_x86_64_phone-userdebug",
+        "host_package" : "@ab/git_master/cf_x86_64_phone-userdebug"
+      },
+      "boot" : {
+        "build" : "@ab/git_master/cf_x86_64_phone-userdebug",
+        "kernel" : {
+          "build" : "@ab/git_master/cf_x86_64_phone-userdebug"
+        },
+        "bootloader" : {
+          "build" : "@ab/git_master/cf_x86_64_phone-userdebug"
+        }
       }
     },
     {
@@ -96,21 +189,47 @@ TEST(FetchCvdParserTests, MultiFetch) {
         "cpus" : 4
       },
       "disk" : {
-        "default_build" : "git_master/cf_gwear_x86-userdebug",
+        "default_build" : "@ab/git_master/cf_gwear_x86-userdebug",
         "download_img_zip" : true
       }
     }
   ],
-  "wait_retry_period" : 20,
-  "keep_downloaded_archives" : false
+  "fetch":{
+    "wait_retry_period" : 20,
+    "keep_downloaded_archives" : false
+  }
 }
   )"""";
   Json::Value json_config = GetTestJson(raw_json);
 
   auto result_config = FetchCvdParserTestHelper(json_config);
 
-  EXPECT_THAT(result_config, IsOk())
+  ASSERT_THAT(result_config, IsOk())
       << "Parsing config failed:  " << result_config.error().Trace();
+  const auto top_level_config = result_config.value();
+  EXPECT_THAT(top_level_config.wait_retry_period, Ne(std::nullopt));
+  EXPECT_THAT(top_level_config.keep_downloaded_archives, Ne(std::nullopt));
+
+  ASSERT_THAT(top_level_config.instances, SizeIs(2));
+  auto config_iterator = top_level_config.instances.cbegin();
+  const auto phone_config = *config_iterator;
+  EXPECT_THAT(phone_config.default_build, Ne(std::nullopt));
+  EXPECT_THAT(phone_config.download_img_zip, Ne(std::nullopt));
+  EXPECT_THAT(phone_config.otatools_build, Ne(std::nullopt));
+  EXPECT_THAT(phone_config.host_package_build, Ne(std::nullopt));
+  EXPECT_THAT(phone_config.boot_build, Ne(std::nullopt));
+  EXPECT_THAT(phone_config.kernel_build, Ne(std::nullopt));
+  EXPECT_THAT(phone_config.bootloader_build, Ne(std::nullopt));
+
+  ++config_iterator;
+  const auto wearable_config = *config_iterator;
+  EXPECT_THAT(wearable_config.default_build, Ne(std::nullopt));
+  EXPECT_THAT(wearable_config.download_img_zip, Ne(std::nullopt));
+  EXPECT_THAT(wearable_config.otatools_build, Eq(std::nullopt));
+  EXPECT_THAT(wearable_config.host_package_build, Eq(std::nullopt));
+  EXPECT_THAT(wearable_config.boot_build, Eq(std::nullopt));
+  EXPECT_THAT(wearable_config.kernel_build, Eq(std::nullopt));
+  EXPECT_THAT(wearable_config.bootloader_build, Eq(std::nullopt));
 }
 
 }  // namespace cuttlefish
