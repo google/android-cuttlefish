d0b771d30ff5794f160e20f82d26d63ea63b9cc9
Treehugger Robot <android-test-infra-autosubmit@system.gserviceaccount.com>
2023-08-11 03:53:56 +0000

    Merge "Move metrics to a top-level config category" into main am: 5a0234d9a3 am: 4fc1caee96 am: 310df88d0e am: 32238f5fe6 am: 7e9352ce96
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2704715
    
    Change-Id: I433bd5113448609abca23d027d7041e9bb49db09
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- base/cvd/cuttlefish/host/commands/cvd/parser/cf_metrics_configs.cpp
+++ base/cvd/cuttlefish/host/commands/cvd/parser/cf_metrics_configs.cpp
@@ -19,34 +19,22 @@
 #include <string>
 #include <vector>
 
+#include <android-base/logging.h>
 #include <json/json.h>
 
-#include "common/libs/utils/json.h"
-#include "common/libs/utils/result.h"
-#include "host/commands/cvd/parser/cf_configs_common.h"
+#define DEFAULT_ENABLE_REPORTING "n"
 
 namespace cuttlefish {
-namespace {
 
-constexpr bool kEnableMetricsDefault = false;
-
-std::string EnabledToReportAnonUsageStats(const bool enabled) {
-  return enabled ? "y" : "n";
+static std::string GenerateReportFlag() {
+  std::stringstream result_flag;
+  result_flag << "--report_anonymous_usage_stats=" << DEFAULT_ENABLE_REPORTING;
+  return result_flag.str();
 }
 
-}  // namespace
-
-Result<void> InitMetricsConfigs(Json::Value& root) {
-  CF_EXPECT(InitConfig(root, kEnableMetricsDefault, {"metrics", "enable"}));
-  return {};
-}
-
-Result<std::vector<std::string>> GenerateMetricsFlags(const Json::Value& root) {
+std::vector<std::string> GenerateMetricsFlags(const Json::Value&) {
   std::vector<std::string> result;
-  auto report_flag_value = EnabledToReportAnonUsageStats(
-      CF_EXPECT(GetValue<bool>(root, {"metrics", "enable"})));
-  result.emplace_back(
-      GenerateGflag("report_anonymous_usage_stats", {report_flag_value}));
+  result.emplace_back(GenerateReportFlag());
   return result;
 }
 
