2908932f264a3d122e18982e212e88b26e15339c
A. Cody Schuffelen <schuffelen@google.com>
2023-08-10 18:31:00 -0700

    Revert "Revert "Remove boolean setter callbacks for Cuttlefish flag library""
    This reverts commit 722663e513d06d32803246babf04594279fdee8d.
    
    Bug: 285955928
    Test: stop_cvd --helpxml
    Change-Id: I03c7f96ffd904cbedc39b54f99f1f6077e6eb83c

--- cvd/host/commands/cvd/server_command/load_configs.cpp
+++ cvd/host/commands/cvd/server_command/load_configs.cpp
@@ -347,10 +347,10 @@ class LoadConfigsCommand : public CvdServerHandler {
     flags.emplace_back(GflagsCompatFlag("help", help));
     std::vector<std::string> overrides;
     FlagAlias alias = {FlagAliasMode::kFlagPrefix, "--override="};
-    flags.emplace_back(
-        Flag().Alias(alias).Setter([&overrides](const FlagMatch& m) {
+    flags.emplace_back(Flag().Alias(alias).Setter(
+        [&overrides](const FlagMatch& m) -> Result<void> {
           overrides.push_back(m.value);
-          return true;
+          return {};
         }));
     auto args = ParseInvocation(request.Message()).arguments;
     CF_EXPECT(ParseFlags(flags, args));
