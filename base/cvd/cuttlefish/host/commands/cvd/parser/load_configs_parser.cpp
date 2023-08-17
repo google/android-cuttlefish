21abdd686ea4c9036eb218f2ecac1be810f5fa00
Treehugger Robot <android-test-infra-autosubmit@system.gserviceaccount.com>
2023-08-17 20:10:29 +0000

    Merge "Move parsing logic into parsing file" into main am: c6ea7fc896 am: 8ae2f83658 am: 90378bb47b am: b55942fe05 am: 01757fae13
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2708516
    
    Change-Id: I9976ff2f2c02e51bd0e76dd65567ced19a8df894
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- base/cvd/cuttlefish/host/commands/cvd/parser/load_configs_parser.cpp
+++ base/cvd/cuttlefish/host/commands/cvd/parser/load_configs_parser.cpp
@@ -16,21 +16,138 @@
 
 #include "host/commands/cvd/parser/load_configs_parser.h"
 
-#include <android-base/file.h>
-#include <gflags/gflags.h>
-
-#include <stdio.h>
+#include <cstdio>
 #include <fstream>
 #include <string>
+
+#include <android-base/file.h>
+#include <android-base/parseint.h>
+#include <gflags/gflags.h>
+#include <json/json.h>
 
 #include "common/libs/utils/files.h"
 #include "common/libs/utils/json.h"
 #include "common/libs/utils/result.h"
+#include "host/commands/cvd/parser/cf_configs_common.h"
 #include "host/commands/cvd/parser/cf_flags_validator.h"
 #include "host/commands/cvd/parser/fetch_cvd_parser.h"
 #include "host/commands/cvd/parser/launch_cvd_parser.h"
 
 namespace cuttlefish {
+namespace {
+
+Result<void> ValidateArgFormat(const std::string& str) {
+  auto equalsPos = str.find('=');
+  CF_EXPECT(equalsPos != std::string::npos,
+            "equal value is not provided in the argument");
+  std::string prefix = str.substr(0, equalsPos);
+  CF_EXPECT(!prefix.empty(), "argument value should not be empty");
+  CF_EXPECT(prefix.find('.') != std::string::npos,
+            "argument value must be dot separated");
+  CF_EXPECT(prefix[0] != '.', "argument value should not start with a dot");
+  CF_EXPECT(prefix.find("..") == std::string::npos,
+            "argument value should not contain two consecutive dots");
+  CF_EXPECT(prefix.back() != '.', "argument value should not end with a dot");
+  return {};
+}
+
+Result<void> ValidateArgsFormat(const std::vector<std::string>& strings) {
+  for (const auto& str : strings) {
+    CF_EXPECT(ValidateArgFormat(str),
+              "Invalid  argument format. " << str << " Please use arg=value");
+  }
+  return {};
+}
+
+// TODO(moelsherif): expand this enum in the future to support more types (
+// double , float , etc) if neeeded
+enum ArgValueType { UINTEGER, BOOLEAN, TEXT };
+
+bool IsUnsignedInteger(const std::string& str) {
+  return !str.empty() && std::all_of(str.begin(), str.end(),
+                                     [](char c) { return std::isdigit(c); });
+}
+
+ArgValueType GetArgValueType(const std::string& str) {
+  if (IsUnsignedInteger(str)) {
+    return UINTEGER;
+  }
+
+  if (str == "true" || str == "false") {
+    return BOOLEAN;
+  }
+
+  // Otherwise, treat the string as text
+  return TEXT;
+}
+
+Json::Value ConvertArgToJson(const std::string& key,
+                             const std::string& leafValue) {
+  std::stack<std::string> levels;
+  std::stringstream ks(key);
+  std::string token;
+  while (std::getline(ks, token, '.')) {
+    levels.push(token);
+  }
+
+  // assign the leaf value based on the type of input value
+  Json::Value leaf;
+  if (GetArgValueType(leafValue) == UINTEGER) {
+    std::uint32_t leaf_val{};
+    if (!android::base::ParseUint(leafValue, &leaf_val)) {
+      LOG(ERROR) << "Failed to parse unsigned integer " << leafValue;
+      return Json::Value::null;
+    };
+    leaf = leaf_val;
+  } else if (GetArgValueType(leafValue) == BOOLEAN) {
+    leaf = (leafValue == "true");
+  } else {
+    leaf = leafValue;
+  }
+
+  while (!levels.empty()) {
+    Json::Value curr;
+    std::string index = levels.top();
+
+    if (GetArgValueType(index) == UINTEGER) {
+      std::uint32_t index_val{};
+      if (!android::base::ParseUint(index, &index_val)) {
+        LOG(ERROR) << "Failed to parse unsigned integer " << index;
+        return Json::Value::null;
+      }
+      curr[index_val] = leaf;
+    } else {
+      curr[index] = leaf;
+    }
+
+    leaf = curr;
+    levels.pop();
+  }
+
+  return leaf;
+}
+
+Json::Value ParseArgsToJson(const std::vector<std::string>& strings) {
+  Json::Value jsonValue;
+  for (const auto& str : strings) {
+    std::string key;
+    std::string value;
+    size_t equals_pos = str.find('=');
+    if (equals_pos != std::string::npos) {
+      key = str.substr(0, equals_pos);
+      value = str.substr(equals_pos + 1);
+    } else {
+      key = str;
+      value.clear();
+      LOG(WARNING) << "No value provided for key " << key;
+      return Json::Value::null;
+    }
+    MergeTwoJsonObjs(jsonValue, ConvertArgToJson(key, value));
+  }
+  return jsonValue;
+}
+
+}  // namespace
 
 Result<Json::Value> ParseJsonFile(const std::string& file_path) {
   CF_EXPECT(FileExists(file_path), "provided File to cvd load does not exist");
@@ -42,6 +159,22 @@ Result<Json::Value> ParseJsonFile(const std::string& file_path) {
             "Failed to read file");
   auto root = CF_EXPECT(ParseJson(file_content), "Failed parsing JSON file");
   return root;
+}
+
+Result<Json::Value> GetOverridedJsonConfig(
+    const std::string& config_path,
+    const std::vector<std::string>& override_flags) {
+  Json::Value result = CF_EXPECT(ParseJsonFile(config_path),
+                                 "Parsing json config input file failed");
+
+  if (override_flags.size() > 0) {
+    CF_EXPECT(ValidateArgsFormat(override_flags),
+              "override parameters are not in the correct format");
+    auto args_tree = ParseArgsToJson(override_flags);
+    MergeTwoJsonObjs(result, args_tree);
+  }
+
+  return result;
 }
 
 Result<CvdFlags> ParseCvdConfigs(Json::Value& root) {
