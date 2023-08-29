56548f7f9ee2197a62824f6aeb0a05063a3c027f
Treehugger Robot <android-test-infra-autosubmit@system.gserviceaccount.com>
2023-08-29 21:59:47 +0000

    [skip ci] Merge changes I2b9e8eed,Ifeb63861,I1738d5ae into main
    * changes:
      Trace run_cvd status for snapshot
      Actually taking guest snapshot in run_cvd
      Implement snapshot-taking at group-level on the host
    
    GitOrigin-RevId: 076e2ee12d6a042ec2d467e29a54a16a99d50795

--- staging/host/libs/command_util/snapshot_utils.h
+++ staging/host/libs/command_util/snapshot_utils.h
@@ -19,6 +19,7 @@
 #include <string>
 
 #include "common/libs/utils/result.h"
+#include "host/libs/config/cuttlefish_config.h"
 
 namespace cuttlefish {
 
@@ -29,5 +30,18 @@ namespace cuttlefish {
 Result<void> CopyDirectoryRecursively(const std::string& src_dir_path,
                                       const std::string& dest_dir_path,
                                       const bool verify_dest_dir_empty = false);
+
+Result<Json::Value> CreateMetaInfo(const CuttlefishConfig& config,
+                                   const std::string& snapshot_path);
+
+Result<std::string> InstanceGuestSnapshotPath(const Json::Value& meta_json,
+                                              const std::string& instance_id);
+std::string SnapshotMetaJsonPath(const std::string& snapshot_path);
+
+inline constexpr const char kMetaInfoJsonFileName[] = "snapshot_meta_info.json";
+inline constexpr const char kGuestSnapshotField[] = "guest_snapshot";
+inline constexpr const char kSnapshotPathField[] = "snapshot_path";
+inline constexpr const char kCfHomeField[] = "HOME";
+inline constexpr const char kGuestSnapshotBase[] = "guest_vm";
 
 }  // namespace cuttlefish
