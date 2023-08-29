4f163a1b2eee67ae2c0604d723916b9cc9ddfa82
Steve Kim <kwstephenkim@google.com>
2023-08-29 06:00:42 +0000

    Merge changes If6aff93d,I52d18fc7 into main
    * changes:
      Remove duplicated logic in EnsureDirectoryExists()
      Added routine to copy directory recursively for snapshot
    
    GitOrigin-RevId: a1022d7efcf1af4ce42b8afe75fdfb95ecfd12ba

---
+++ staging/host/libs/command_util/snapshot_utils.cc
@@ -0,0 +1,165 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "host/libs/command_util/snapshot_utils.h"
+
+#include <unistd.h>
+
+#include <string>
+
+#include <android-base/file.h>
+#include <android-base/logging.h>
+
+#include "common/libs/utils/files.h"
+#include "common/libs/utils/result.h"
+
+namespace cuttlefish {
+namespace {
+
+bool IsFifo(const struct stat& file_stat) {
+  return S_ISFIFO(file_stat.st_mode);
+}
+
+bool IsSocket(const struct stat& file_stat) {
+  return S_ISSOCK(file_stat.st_mode);
+}
+
+bool IsSymlink(const struct stat& file_stat) {
+  return S_ISLNK(file_stat.st_mode);
+}
+
+bool IsRegular(const struct stat& file_stat) {
+  return S_ISREG(file_stat.st_mode);
+}
+
+// assumes that src_dir_path and dest_dir_path exist and both are
+// existing directories or links to the directories. Also they are
+// different directories.
+Result<void> CopyDirectoryImpl(const std::string& src_dir_path,
+                               const std::string& dest_dir_path) {
+  // create an empty dest_dir_path with the same permission as src_dir_path
+  // and then, recursively copy the contents
+  LOG(DEBUG) << "Making sure " << dest_dir_path
+             << " exists and is effectively a directory.";
+  CF_EXPECTF(EnsureDirectoryExists(dest_dir_path),
+             "Directory {} cannot to be created; it does not exist, either.",
+             dest_dir_path);
+  const auto src_contents = CF_EXPECT(DirectoryContents(src_dir_path));
+  for (const auto& src_base_path : src_contents) {
+    if (src_base_path == "." || src_base_path == "..") {
+      LOG(DEBUG) << "Skipping \"" << src_base_path << "\"";
+      continue;
+    }
+    std::string src_path = src_dir_path + "/" + src_base_path;
+    std::string dest_path = dest_dir_path + "/" + src_base_path;
+
+    LOG(DEBUG) << "Handling... " << src_path;
+
+    struct stat src_stat;
+    CF_EXPECTF(lstat(src_path.data(), &src_stat) != -1, "Failed in lstat({})",
+               src_path);
+    if (IsSymlink(src_stat)) {
+      std::string target;
+      CF_EXPECTF(android::base::Readlink(src_path, &target),
+                 "Readlink failed for {}", src_path);
+      LOG(DEBUG) << "Creating link from " << dest_path << " to " << target;
+      if (FileExists(dest_path, /* follow_symlink */ false)) {
+        CF_EXPECTF(RemoveFile(dest_path), "Failed to unlink/remove file \"{}\"",
+                   dest_path);
+      }
+      CF_EXPECTF(symlink(target.data(), dest_path.data()) == 0,
+                 "Creating symbolic link from {} to {} failed.", dest_path,
+                 target);
+      continue;
+    }
+
+    if (IsFifo(src_stat) || IsSocket(src_stat)) {
+      LOG(DEBUG) << "Ignoring a named pipe or socket " << src_path;
+      continue;
+    }
+
+    if (DirectoryExists(src_path)) {
+      LOG(DEBUG) << "Recursively calling CopyDirectoryImpl(" << src_path << ", "
+                 << dest_path << ")";
+      CF_EXPECT(CopyDirectoryImpl(src_path, dest_path));
+      LOG(DEBUG) << "Returned from Recursive call CopyDirectoryImpl("
+                 << src_path << ", " << dest_path << ")";
+      continue;
+    }
+
+    CF_EXPECTF(IsRegular(src_stat),
+               "File {} must be directory, link, socket, pipe or regular."
+               "{} is none of those",
+               src_path, src_path);
+
+    CF_EXPECTF(Copy(src_path, dest_path), "Copy from {} to {} failed", src_path,
+               dest_path);
+  }
+  return {};
+}
+
+/*
+ * Returns Realpath(path) if successful, or the absolute path of "path"
+ *
+ * If emulating absolute path fails, "path" is returned as is.
+ */
+std::string RealpathOrSelf(const std::string& path) {
+  std::string output;
+  if (android::base::Realpath(path, &output)) {
+    return output;
+  }
+  struct InputPathForm input_form {
+    .path_to_convert = path, .follow_symlink = true,
+  };
+  auto absolute_path = EmulateAbsolutePath(input_form);
+  return absolute_path.ok() ? *absolute_path : path;
+}
+
+}  // namespace
+
+Result<void> CopyDirectoryRecursively(const std::string& src_dir_path,
+                                      const std::string& dest_dir_path,
+                                      const bool verify_dest_dir_empty) {
+  CF_EXPECTF(FileExists(src_dir_path),
+             "A file/directory \"{}\" does not exist.", src_dir_path);
+  CF_EXPECTF(DirectoryExists(src_dir_path), "\"{}\" is not a directory.",
+             src_dir_path);
+  if (verify_dest_dir_empty) {
+    CF_EXPECTF(!FileExists(dest_dir_path, /* follow symlink */ false),
+               "Delete the destination directiory \"{}\" first", dest_dir_path);
+  }
+
+  std::string dest_final_target = RealpathOrSelf(dest_dir_path);
+  std::string src_final_target = RealpathOrSelf(src_dir_path);
+  if (dest_final_target == src_final_target) {
+    LOG(DEBUG) << "\"" << src_dir_path << "\" and \"" << dest_dir_path
+               << "\" are effectively the same.";
+    return {};
+  }
+
+  LOG(INFO) << "Copy from \"" << src_final_target << "\" to \""
+            << dest_final_target;
+
+  /**
+   * On taking snapshot, we should delete dest_dir first. On Restoring,
+   * we don't delete the runtime directory, eventually. We could, however,
+   * start with deleting it.
+   */
+  CF_EXPECT(CopyDirectoryImpl(src_final_target, dest_final_target));
+  return {};
+}
+
+}  // namespace cuttlefish
