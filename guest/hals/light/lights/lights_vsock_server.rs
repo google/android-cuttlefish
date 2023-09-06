bac3bc2d7a8edfb99d081f9281f7710fad57ce3a
Jesus Sanchez-Palencia <jesussanp@google.com>
2023-09-06 15:01:04 -0700

    lights: Use the vsocks crate in VsockServer
    Remove our Rust bindings for the vsock_connection library from
    Cuttlefish and adopt the vsocks crate instead. In addition to that,
    change the design of the VsockServer helper and use a channel to
    communicate with the thread owning the accepted connection socket
    instead.
    
    We are not sending any messages yet, so this CL only puts the
    scaffolding in place. The thread started from VsockServer::start() will
    block and wait for a message on the channel and for now only
    VsockServer::stop() sends a message on the sender endpoint. This
    is needed to unblock the thread on waiting on the recv() call and allow
    it to exit since is_running is set to false.
    
    The next CLs on this stack will make use of this infra to send JSON
    messages to the LightsService client.
    
    Bug: 286106270
    Tested: launched Cuttlefish and inspected logs to see that a connection
    Tested: was established.
    Change-Id: I6a332af259e27f31f999542fafc0021681fb3528

--- guest/hals/light/lights/lights_vsock_server.rs
+++ guest/hals/light/lights/lights_vsock_server.rs
@@ -17,23 +17,24 @@
 
 use log::info;
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::{Arc, Mutex};
+use std::sync::mpsc;
+use std::sync::Arc;
 use std::thread;
-use vsock_utils::VsockServerConnection;
+use vsock::VsockListener;
 
 /// Vsock server helper.
 pub struct VsockServer {
-    vsock_connection: Arc<Mutex<VsockServerConnection>>,
     is_server_running: Arc<AtomicBool>,
     thread_handle: Option<thread::JoinHandle<()>>,
+    connection_thread_sender: Option<mpsc::Sender<bool>>,
 }
 
 impl VsockServer {
     pub fn new() -> Self {
         Self {
-            vsock_connection: Arc::new(Mutex::new(VsockServerConnection::new())),
             is_server_running: Arc::new(AtomicBool::new(false)),
             thread_handle: None,
+            connection_thread_sender: None,
         }
     }
 
@@ -44,24 +45,39 @@ impl VsockServer {
 
         self.is_server_running.store(true, Ordering::SeqCst);
 
-        let vsock_connection = self.vsock_connection.clone();
         let is_running = self.is_server_running.clone();
+        let (sender, receiver) = mpsc::channel::<bool>();
+        let server = VsockListener::bind_with_cid_port(cid, port).expect("bind failed");
+
         self.thread_handle = Some(thread::spawn(move || {
             while is_running.load(Ordering::SeqCst) {
-                if vsock_connection.lock().unwrap().connect(port, cid) {
-                    info!("Lights service Vsock server connection established.");
-                    // TODO: finish this once we know the protocol
-                }
+                let (mut _connection, _addr) = server.accept().expect("accept failed");
+                info!("Lights service vsock server connection established.");
+
+                // Block until we receive a message to send on the socket.
+                let _ = receiver.recv().expect("Unable to read from channel");
+
+                // TODO: finish client-server protocol implementation
             }
         }));
+
+        self.connection_thread_sender = Some(sender);
     }
 
     /// Stops the server thread.
     pub fn stop(&mut self) {
         info!("Stopping vsocks server for Lights service");
 
-        self.vsock_connection.lock().unwrap().server_shutdown();
         self.is_server_running.store(false, Ordering::SeqCst);
+
+        // Send a message on the channel to unblock the recv() call.
+        if self.connection_thread_sender.is_some() {
+            self.connection_thread_sender
+                .as_ref()
+                .unwrap()
+                .send(false)
+                .expect("Unable to send on channel");
+        }
 
         if self.thread_handle.is_some() {
             let handle = self.thread_handle.take().expect("Thread handle should be valid");
