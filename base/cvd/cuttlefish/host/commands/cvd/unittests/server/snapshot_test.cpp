77bbd741a71507c5ea37e89d7fdf489f669d613a
Elie Kheirallah <khei@google.com>
2023-09-07 23:06:17 +0000

    Merge "add suspend/snapshot tests" into main am: 63bb20e175 am: 8640227489 am: 12c1da6fab am: 170b97ce8f am: efb8481afd
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2733769
    
    Change-Id: I0000e321d4b0aec4f72f09af65e757b0734e77fe
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- base/cvd/cuttlefish/host/commands/cvd/unittests/server/snapshot_test.cpp
+++ base/cvd/cuttlefish/host/commands/cvd/unittests/server/snapshot_test.cpp
@@ -21,7 +21,7 @@
 #include "common/libs/utils/contains.h"
 #include "host/commands/cvd/types.h"
 #include "host/commands/cvd/unittests/server/cmd_runner.h"
-#include "host/commands/cvd/unittests/server/snapshot_test_helper.h"
+#include "host/commands/cvd/unittests/server/snapshot_test_helper.cpp"
 
 namespace cuttlefish {
 namespace cvdsnapshot {
@@ -66,18 +66,20 @@ TEST_F(CvdSnapshotTest, CvdSuspendSnapshotResumeRestore) {
   auto cmd_stop = CmdRunner::Run("cvd stop", envs);
   ASSERT_TRUE(cmd_stop.Success()) << cmd_stop.Stderr();
 
-  // clean up for the next test
-  CmdRunner::Run("cvd reset -y", envs);
+  // TODO(khei): un-comment the remaining lines after aosp/2726020 is merged
+  // // clean up for the next test
+  // CmdRunner::Run("cvd reset -y", envs);
 
-  cvd_common::Args start_args{"cvd", "start",
-                              "--report_anonymous_usage_stats=yes", "--daemon",
-                              "--snapshot_path=/tmp/snapshots/snapshot"};
+  // cvd_common::Args start_args{"cvd", "start",
+  //                             "--report_anonymous_usage_stats=yes",
+  //                             "--daemon",
+  //                             "--snapshot_path=/tmp/snapshots/snapshot"};
 
-  auto cmd_start_2 = CmdRunner::Run(start_args, envs);
-  ASSERT_TRUE(cmd_start_2.Success()) << cmd_start_2.Stderr();
+  // auto cmd_start_2 = CmdRunner::Run(start_args, envs);
+  // ASSERT_TRUE(cmd_start_2.Success()) << cmd_start_2.Stderr();
 
-  auto cmd_stop_2 = CmdRunner::Run("cvd stop", envs);
-  ASSERT_TRUE(cmd_stop_2.Success()) << cmd_stop_2.Stderr();
+  // auto cmd_stop_2 = CmdRunner::Run("cvd stop", envs);
+  // ASSERT_TRUE(cmd_stop_2.Success()) << cmd_stop_2.Stderr();
 
   auto cmd_rm = CmdRunner::Run("rm -rf /tmp/snapshots/snapshot", envs);
   ASSERT_TRUE(cmd_rm.Success()) << cmd_rm.Stderr();
