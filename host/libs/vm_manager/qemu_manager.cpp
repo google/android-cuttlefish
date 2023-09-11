45012c9e4799798236fe3f9ab85c79767b13ab4e
Dmitrii Merkurev <dimorinny@google.com>
2023-09-12 00:35:28 +0100

    Don't use sensors vconsole for qemu configuration
    Bug: 299984777
    Test: launch qemu cvd locally
    Change-Id: Icbf96275fd44aa15a9727dd58b858e908cc66838
    Signed-off-by: Dmitrii Merkurev <dimorinny@google.com>

--- host/libs/vm_manager/qemu_manager.cpp
+++ host/libs/vm_manager/qemu_manager.cpp
@@ -584,8 +584,10 @@ Result<std::vector<MonitorCommand>> QemuManager::StartCommands(
     add_hvc_sink();
   }
 
+  // sensors_fifo_vm.{in/out} are created along with the streamer process,
+  // which is not created w/ QEMU.
   // /dev/hvc13 = sensors
-  add_hvc(instance.PerInstanceInternalPath("sensors_fifo_vm"));
+  add_hvc_sink();
 
   auto disk_num = instance.virtual_disk_paths().size();
 
