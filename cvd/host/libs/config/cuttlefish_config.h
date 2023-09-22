c281b951937d52e42564e5fd3e60de75175644e6
Sanket Chinchalkar <schinchalkar@google.com>
2023-09-22 23:18:11 +0000

    Merge "Start Automotive Proxy Service From Cuttlefish" into main

--- cvd/host/libs/config/cuttlefish_config.h
+++ cvd/host/libs/config/cuttlefish_config.h
@@ -175,6 +175,8 @@ class CuttlefishConfig {
   int casimir_instance_num() const;
   void set_casimir_nci_port(int port);
   int casimir_nci_port() const;
+  void set_casimir_rf_port(int port);
+  int casimir_rf_port() const;
 
   void set_enable_wifi(const bool enable_wifi);
   bool enable_wifi() const;
@@ -190,6 +192,14 @@ class CuttlefishConfig {
   bool netsim_radio_enabled(NetsimRadio flag) const;
   void set_netsim_instance_num(int netsim_instance_num);
   int netsim_instance_num() const;
+  // Netsim has a built-in connector to forward packets to another daemon based
+  // on instance number.  This is set in the serial launch case when FLAGS
+  // rootcanal_instance_num is specified. The non-netsim case uses
+  // bluetooth_connector and rootcanal_hci_port for the same purpose. Purposely
+  // restricted to legacy bluetooth serial invocation because new cases should
+  // use cvd.
+  int netsim_connector_instance_num() const;
+  void set_netsim_connector_instance_num(int netsim_instance_num);
   void set_netsim_args(const std::string& netsim_args);
   std::vector<std::string> netsim_args() const;
 
@@ -277,6 +287,8 @@ class CuttlefishConfig {
   std::string snapshot_path() const;
   void set_snapshot_path(const std::string& snapshot_path);
 
+  bool IsCrosvm() const;
+
   class InstanceSpecific;
   class MutableInstanceSpecific;
 
@@ -360,6 +372,7 @@ class CuttlefishConfig {
     std::string PerInstanceInternalPath(const std::string& file_name) const;
     std::string PerInstanceLogPath(const std::string& file_name) const;
 
+    std::string CrosvmSocketPath() const;
     std::string instance_dir() const;
 
     std::string instance_internal_dir() const;
