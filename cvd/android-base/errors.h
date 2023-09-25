3394f6715ce3ee09ac0de83629a9a8a1721d5ee3
Jiakai Zhang <jiakaiz@google.com>
2023-09-25 16:21:30 +0100

    Use longer names for temporary vars in OR_RETURN/OR_FATAL.
    Before this change, the macros declare a variable called `tmp`, which
    may shadow a variable above that has the same name and cause a problem
    when `expr` expends to something that contains `tmp`.
    
    For example:
    
    Result<int> tmp(1);
    int a = OR_RETURN(std::move(tmp));
                                ^^^
              The `tmp` in the macro shadows it.
    
    Without -Wall, this can lead to undefined behavior. Fortunately, the
    Android build system uses -Wall so that it can catch this issue and
    throw a compilation error, so undefined behavior will not happen.
    However, it's still not great that the macros don't allow the operand to
    expend to something that contains `tmp`.
    
    This change mitigates the issue by using a longer name with a leading
    "__" for the temporary variable.
    
    Test: Presubmit
    Change-Id: I5035570a21b6f0e1baf60a1d089a4b5eabd299f9

--- cvd/android-base/errors.h
+++ cvd/android-base/errors.h
@@ -72,38 +72,41 @@ std::string SystemErrorCodeToString(int error_code);
 // If implicit conversion compilation errors occur involving a value type with a templated
 // forwarding ref ctor, compilation with cpp20 or explicitly converting to the desired
 // return type is required.
-#define OR_RETURN(expr)                                                                 \
-  ({                                                                                    \
-    decltype(expr)&& tmp = (expr);                                                      \
-    typedef android::base::OkOrFail<std::remove_reference_t<decltype(tmp)>> ok_or_fail; \
-    if (!ok_or_fail::IsOk(tmp)) {                                                       \
-      return ok_or_fail::Fail(std::move(tmp));                                          \
-    }                                                                                   \
-    ok_or_fail::Unwrap(std::move(tmp));                                                 \
+#define OR_RETURN(expr)                                                                  \
+  ({                                                                                     \
+    decltype(expr)&& __or_return_expr = (expr);                                          \
+    typedef android::base::OkOrFail<std::remove_reference_t<decltype(__or_return_expr)>> \
+        ok_or_fail;                                                                      \
+    if (!ok_or_fail::IsOk(__or_return_expr)) {                                           \
+      return ok_or_fail::Fail(std::move(__or_return_expr));                              \
+    }                                                                                    \
+    ok_or_fail::Unwrap(std::move(__or_return_expr));                                     \
   })
 
 // Same as OR_RETURN, but aborts if expr is a failure.
 #if defined(__BIONIC__)
 #define OR_FATAL(expr)                                                                  \
   ({                                                                                    \
-    decltype(expr)&& tmp = (expr);                                                      \
-    typedef android::base::OkOrFail<std::remove_reference_t<decltype(tmp)>> ok_or_fail; \
-    if (!ok_or_fail::IsOk(tmp)) {                                                       \
-      __assert(__FILE__, __LINE__, ok_or_fail::ErrorMessage(tmp).c_str());              \
+    decltype(expr)&& __or_fatal_expr = (expr);                                          \
+    typedef android::base::OkOrFail<std::remove_reference_t<decltype(__or_fatal_expr)>> \
+        ok_or_fail;                                                                     \
+    if (!ok_or_fail::IsOk(__or_fatal_expr)) {                                           \
+      __assert(__FILE__, __LINE__, ok_or_fail::ErrorMessage(__or_fatal_expr).c_str());  \
     }                                                                                   \
-    ok_or_fail::Unwrap(std::move(tmp));                                                 \
+    ok_or_fail::Unwrap(std::move(__or_fatal_expr));                                     \
   })
 #else
 #define OR_FATAL(expr)                                                                  \
   ({                                                                                    \
-    decltype(expr)&& tmp = (expr);                                                      \
-    typedef android::base::OkOrFail<std::remove_reference_t<decltype(tmp)>> ok_or_fail; \
-    if (!ok_or_fail::IsOk(tmp)) {                                                       \
+    decltype(expr)&& __or_fatal_expr = (expr);                                          \
+    typedef android::base::OkOrFail<std::remove_reference_t<decltype(__or_fatal_expr)>> \
+        ok_or_fail;                                                                     \
+    if (!ok_or_fail::IsOk(__or_fatal_expr)) {                                           \
       fprintf(stderr, "%s:%d: assertion \"%s\" failed", __FILE__, __LINE__,             \
-              ok_or_fail::ErrorMessage(tmp).c_str());                                   \
+              ok_or_fail::ErrorMessage(__or_fatal_expr).c_str());                       \
       abort();                                                                          \
     }                                                                                   \
-    ok_or_fail::Unwrap(std::move(tmp));                                                 \
+    ok_or_fail::Unwrap(std::move(__or_fatal_expr));                                     \
   })
 #endif
 
