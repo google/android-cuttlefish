b7668d99262e3680b4a0aaccd58f7a9d71e5fbd0
Charlie Boutier <charliebout@google.com>
2023-10-09 22:33:57 +0000

    Revert "Revert "cf: Add uwb feature to cuttlefish""
    This reverts commit 19a3e3f5d96c27f1aa2ec9b8a907ad328770a0ec.
    
    Reason for revert: enabling virtual uwb for testing
    
    Change-Id: I65a900367b26bf5d81eb60fa442ffcac0d89a30c

--- shared/device.mk
+++ shared/device.mk
@@ -246,6 +246,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/surround_sound_configuration_5_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/surround_sound_configuration_5_0.xml \
+    frameworks/native/data/etc/android.hardware.uwb.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.uwb.xml \
     device/google/cuttlefish/shared/config/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
     frameworks/native/data/etc/android.software.credentials.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.credentials.xml \
 
