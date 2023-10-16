15bd0bc278bb8c723082526467e6b20d486d6e42
Chad Reynolds <chadreynolds@google.com>
2023-10-16 17:28:27 +0000

    [skip ci] Merge "Extend the build string syntax with a filepath" into main
    GitOrigin-RevId: cc2c866893972eeab55ba715d5eb641a77394bdc

--- staging/host/libs/web/build_string.cc
+++ staging/host/libs/web/build_string.cc
@@ -21,6 +21,7 @@
 #include <ostream>
 #include <sstream>
 #include <string>
+#include <utility>
 #include <variant>
 
 #include <android-base/strings.h>
@@ -33,11 +34,45 @@ namespace cuttlefish {
 
 namespace {
 
-Result<BuildString> ParseDeviceBuildString(const std::string& build_string) {
+Result<std::pair<std::string, std::optional<std::string>>> ParseFilepath(
+    const std::string& build_string) {
+  std::string remaining_build_string = build_string;
+  std::optional<std::string> filepath;
+  std::size_t open_bracket = build_string.find('{');
+  std::size_t close_bracket = build_string.find('}');
+
+  bool has_open = open_bracket != std::string::npos;
+  bool has_close = close_bracket != std::string::npos;
+  CF_EXPECTF(
+      has_open == has_close,
+      "Open or close curly bracket exists without its complement in \"{}\"",
+      build_string);
+  if (has_open && has_close) {
+    std::string remaining_substring = build_string.substr(0, open_bracket);
+    CF_EXPECTF(
+        !remaining_substring.empty(),
+        "The build string excluding filepath cannot be empty.  Input: {}",
+        build_string);
+    std::size_t filepath_start = open_bracket + 1;
+    std::string filepath_substring =
+        build_string.substr(filepath_start, close_bracket - filepath_start);
+    CF_EXPECTF(
+        !filepath_substring.empty(),
+        "The filepath between positions {},{} cannot be empty.  Input: {}",
+        filepath_start, close_bracket, build_string);
+    remaining_build_string = remaining_substring;
+    filepath = filepath_substring;
+  }
+  return {{remaining_build_string, filepath}};
+}
+
+Result<BuildString> ParseDeviceBuildString(
+    const std::string& build_string,
+    const std::optional<std::string>& filepath) {
   std::size_t slash_pos = build_string.find('/');
   std::string branch_or_id = build_string.substr(0, slash_pos);
-  auto result =
-      DeviceBuildString{.branch_or_id = build_string.substr(0, slash_pos)};
+  auto result = DeviceBuildString{
+      .branch_or_id = build_string.substr(0, slash_pos), .filepath = filepath};
   if (slash_pos != std::string::npos) {
     std::size_t next_slash_pos = build_string.find('/', slash_pos + 1);
     CF_EXPECTF(next_slash_pos == std::string::npos,
@@ -50,8 +85,9 @@ Result<BuildString> ParseDeviceBuildString(const std::string& build_string) {
 }
 
 Result<DirectoryBuildString> ParseDirectoryBuildString(
-    const std::string& build_string) {
-  DirectoryBuildString result;
+    const std::string& build_string,
+    const std::optional<std::string>& filepath) {
+  auto result = DirectoryBuildString{.filepath = filepath};
   std::vector<std::string> split = android::base::Split(build_string, ":");
   result.target = split.back();
   split.pop_back();
@@ -63,13 +99,15 @@ Result<DirectoryBuildString> ParseDirectoryBuildString(
 
 std::ostream& operator<<(std::ostream& out,
                          const DeviceBuildString& build_string) {
-  fmt::print(out, "(branch_or_id=\"{}\", target=\"{}\")",
-             build_string.branch_or_id, build_string.target.value_or(""));
+  fmt::print(out, "(branch_or_id=\"{}\", target=\"{}\", filepath=\"{}\")",
+             build_string.branch_or_id, build_string.target.value_or(""),
+             build_string.filepath.value_or(""));
   return out;
 }
 
 bool operator==(const DeviceBuildString& lhs, const DeviceBuildString& rhs) {
-  return lhs.branch_or_id == rhs.branch_or_id && lhs.target == rhs.target;
+  return lhs.branch_or_id == rhs.branch_or_id && lhs.target == rhs.target &&
+         lhs.filepath == rhs.filepath;
 }
 
 bool operator!=(const DeviceBuildString& lhs, const DeviceBuildString& rhs) {
@@ -78,14 +116,16 @@ bool operator!=(const DeviceBuildString& lhs, const DeviceBuildString& rhs) {
 
 std::ostream& operator<<(std::ostream& out,
                          const DirectoryBuildString& build_string) {
-  fmt::print(out, "(paths=\"{}\", target=\"{}\")",
-             fmt::join(build_string.paths, ":"), build_string.target);
+  fmt::print(out, "(paths=\"{}\", target=\"{}\", filepath=\"{}\")",
+             fmt::join(build_string.paths, ":"), build_string.target,
+             build_string.filepath.value_or(""));
   return out;
 }
 
 bool operator==(const DirectoryBuildString& lhs,
                 const DirectoryBuildString& rhs) {
-  return lhs.paths == rhs.paths && lhs.target == rhs.target;
+  return lhs.paths == rhs.paths && lhs.target == rhs.target &&
+         lhs.filepath == rhs.filepath;
 }
 
 bool operator!=(const DirectoryBuildString& lhs,
@@ -108,12 +148,23 @@ std::ostream& operator<<(std::ostream& out,
   return out;
 }
 
+std::optional<std::string> GetFilepath(const BuildString& build_string) {
+  return std::visit([](auto&& arg) { return arg.filepath; }, build_string);
+}
+
+void SetFilepath(BuildString& build_string, const std::string& value) {
+  std::visit([&value](auto&& arg) { arg.filepath = value; }, build_string);
+}
+
 Result<BuildString> ParseBuildString(const std::string& build_string) {
   CF_EXPECT(!build_string.empty(), "The given build string cannot be empty");
-  if (build_string.find(':') != std::string::npos) {
-    return CF_EXPECT(ParseDirectoryBuildString(build_string));
+  auto [remaining_build_string, filepath] =
+      CF_EXPECT(ParseFilepath(build_string));
+  if (remaining_build_string.find(':') != std::string::npos) {
+    return CF_EXPECT(
+        ParseDirectoryBuildString(remaining_build_string, filepath));
   } else {
-    return CF_EXPECT(ParseDeviceBuildString(build_string));
+    return CF_EXPECT(ParseDeviceBuildString(remaining_build_string, filepath));
   }
 }
 
