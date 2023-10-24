8e92ac2dff750e3c48421144e81b34ce19d9b6ab
Mahmoud Elsherif <moelsherif@google.com>
2023-10-24 01:29:04 +0000

    Merge "Decouple cvd events from cuttlefish events." into main
    GitOrigin-RevId: 86630b43cac5a9f42d6f582b06bd835cefa76ba6

--- staging/host/commands/metrics/events.cc
+++ staging/host/commands/metrics/events.cc
@@ -20,7 +20,6 @@
 #include "host/commands/metrics/events.h"
 #include "host/commands/metrics/metrics_defs.h"
 #include "host/commands/metrics/proto/cf_metrics_protos.h"
-#include "host/commands/metrics/proto/cvd_metrics_protos.h"
 #include "host/commands/metrics/utils.h"
 #include "shared/api_level.h"
 
@@ -29,9 +28,6 @@ namespace cuttlefish {
 namespace {
 
 constexpr int kLogSourceId = 1753;
-// 971 for atest internal events, while 934 for external events
-constexpr int kAtestInternalLogSourceId = 971;
-constexpr char kToolName[] = "cvd";
 
 constexpr char kLogSourceStr[] = "CUTTLEFISH_METRICS";
 constexpr int kCppClientType =
@@ -62,41 +58,6 @@ std::unique_ptr<CuttlefishLogEvent> BuildCfLogEvent(
 
   return cfEvent;
 }
-
-std::string GenerateUUID() {
-  uuid_t uuid;
-  uuid_generate_random(uuid);
-  std::string uuid_str = "xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx";
-  uuid_unparse(uuid, uuid_str.data());
-  LOG(INFO) << "uuid_str: " << uuid_str;
-  return uuid_str;
-}
-
-std::unique_ptr<AtestLogEventInternal> BuildAtestLogEvent(
-    const std::string& command_line) {
-  std::unique_ptr<AtestLogEventInternal> event =
-      std::make_unique<AtestLogEventInternal>();
-
-  //  Set common fields
-  std::string user_key = GenerateUUID();
-  std::string run_id = GenerateUUID();
-  std::string os_name = metrics::GetOsName();
-  std::string dir = CurrentDirectory();
-  event->set_user_key(user_key);
-  event->set_run_id(run_id);
-  event->set_tool_name(kToolName);
-  event->set_user_type(UserType::GOOGLE);
-
-  // Create and populate AtestStartEvent
-  AtestLogEventInternal::AtestStartEvent* start_event =
-      event->mutable_atest_start_event();
-  start_event->set_command_line(command_line);
-  start_event->set_cwd(dir);
-  start_event->set_os(os_name);
-
-  return event;
-}
-
 // Builds the 2nd level MetricsEvent.
 void AddCfMetricsEventToLog(uint64_t now_ms, CuttlefishLogEvent* cfEvent,
                             MetricsEvent::EventType event_type) {
@@ -144,35 +105,7 @@ std::unique_ptr<LogRequest> BuildLogRequest(uint64_t now_ms,
 
   return log_request;
 }
-
-std::unique_ptr<LogRequest> BuildAtestLogRequest(
-    uint64_t now_ms, AtestLogEventInternal* cfEvent) {
-  // "log_request" is the top level LogRequest
-  auto log_request = std::make_unique<LogRequest>();
-  log_request->set_request_time_ms(now_ms);
-  log_request->set_log_source(kAtestInternalLogSourceId);
-  log_request->set_log_source_name(kLogSourceStr);
-
-  ClientInfo* client_info = log_request->mutable_client_info();
-  client_info->set_client_type(kCppClientType);
-
-  std::string atest_log_event;
-  if (!cfEvent->SerializeToString(&atest_log_event)) {
-    LOG(ERROR) << "Serialization failed for atest event";
-    return nullptr;
-  }
-
-  LogEvent* logEvent = log_request->add_log_event();
-  logEvent->set_event_time_ms(now_ms);
-  logEvent->set_source_extension(atest_log_event);
-
-  return log_request;
-}
-
 }  // namespace
-
-Clearcut::Clearcut() = default;
-Clearcut::~Clearcut() = default;
 
 int Clearcut::SendEvent(CuttlefishLogEvent::DeviceType device_type,
                         MetricsEvent::EventType event_type) {
@@ -212,24 +145,6 @@ int Clearcut::SendDeviceBoot(CuttlefishLogEvent::DeviceType device) {
 int Clearcut::SendLockScreen(CuttlefishLogEvent::DeviceType device) {
   return SendEvent(device,
                    MetricsEvent::CUTTLEFISH_EVENT_TYPE_LOCK_SCREEN_AVAILABLE);
-}
-
-int Clearcut::SendLaunchCommand(const std::string& command_line) {
-  uint64_t now_ms = metrics::GetEpochTimeMs();
-  auto cfEvent = BuildAtestLogEvent(command_line);
-
-  auto logRequest = BuildAtestLogRequest(now_ms, cfEvent.get());
-  if (!logRequest) {
-    LOG(ERROR) << "Failed to build atest LogRequest";
-    return MetricsExitCodes::kMetricsError;
-  }
-
-  std::string logRequestStr;
-  if (!logRequest->SerializeToString(&logRequestStr)) {
-    LOG(ERROR) << "Serialization failed for atest LogRequest";
-    return MetricsExitCodes::kMetricsError;
-  }
-  return metrics::PostRequest(logRequestStr, metrics::kProd);
 }
 
 }  // namespace cuttlefish
