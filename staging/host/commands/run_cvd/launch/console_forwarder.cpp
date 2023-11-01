36dcc8dc4b7a37c314d753e4dc2ee55e6f1f7eb7
Cody Schuffelen <schuffelen@google.com>
2023-11-01 23:15:40 +0000

    [skip ci] Merge "Create and use an AutoDiagnostic utility" into main
    GitOrigin-RevId: 5d320aada0a4ce061feb10c15973656b31588b33

--- staging/host/commands/run_cvd/launch/console_forwarder.cpp
+++ staging/host/commands/run_cvd/launch/console_forwarder.cpp
@@ -16,90 +16,56 @@
 #include "host/commands/run_cvd/launch/launch.h"
 
 #include <string>
-#include <unordered_set>
-#include <utility>
-#include <vector>
 
 #include <fruit/fruit.h>
 
 #include "common/libs/utils/result.h"
-#include "host/commands/run_cvd/reporting.h"
 #include "host/libs/config/command_source.h"
 #include "host/libs/config/known_paths.h"
 
 namespace cuttlefish {
-namespace {
 
-class ConsoleForwarder : public CommandSource, public DiagnosticInformation {
- public:
-  INJECT(ConsoleForwarder(const CuttlefishConfig::InstanceSpecific& instance))
-      : instance_(instance) {}
-  // DiagnosticInformation
-  std::vector<std::string> Diagnostics() const override {
-    if (Enabled()) {
-      return {"To access the console run: screen " + instance_.console_path()};
-    } else {
-      return {"Serial console is disabled; use -console=true to enable it."};
-    }
+std::string ConsoleInfo(const CuttlefishConfig::InstanceSpecific& instance) {
+  if (instance.console()) {
+    return "To access the console run: screen " + instance.console_path();
+  } else {
+    return "Serial console is disabled; use -console=true to enable it.";
   }
+}
 
-  // CommandSource
-  Result<std::vector<MonitorCommand>> Commands() override {
-    Command console_forwarder_cmd(ConsoleForwarderBinary());
-    console_forwarder_cmd.AddParameter("--console_in_fd=",
-                                       console_forwarder_in_wr_);
-    console_forwarder_cmd.AddParameter("--console_out_fd=",
-                                       console_forwarder_out_rd_);
-    std::vector<MonitorCommand> commands;
-    commands.emplace_back(std::move(console_forwarder_cmd));
-    return commands;
-  }
-
-  // SetupFeature
-  std::string Name() const override { return "ConsoleForwarder"; }
-  bool Enabled() const override { return instance_.console(); }
-
- private:
-  std::unordered_set<SetupFeature*> Dependencies() const override { return {}; }
-  Result<void> ResultSetup() override {
-    auto console_in_pipe_name = instance_.console_in_pipe_name();
-    CF_EXPECT(
-        mkfifo(console_in_pipe_name.c_str(), 0600) == 0,
-        "Failed to create console input fifo for crosvm: " << strerror(errno));
-
-    auto console_out_pipe_name = instance_.console_out_pipe_name();
-    CF_EXPECT(
-        mkfifo(console_out_pipe_name.c_str(), 0660) == 0,
-        "Failed to create console output fifo for crosvm: " << strerror(errno));
-
-    // These fds will only be read from or written to, but open them with
-    // read and write access to keep them open in case the subprocesses exit
-    console_forwarder_in_wr_ =
-        SharedFD::Open(console_in_pipe_name.c_str(), O_RDWR);
-    CF_EXPECT(console_forwarder_in_wr_->IsOpen(),
-              "Failed to open console_forwarder input fifo for writes: "
-                  << console_forwarder_in_wr_->StrError());
-
-    console_forwarder_out_rd_ =
-        SharedFD::Open(console_out_pipe_name.c_str(), O_RDWR);
-    CF_EXPECT(console_forwarder_out_rd_->IsOpen(),
-              "Failed to open console_forwarder output fifo for reads: "
-                  << console_forwarder_out_rd_->StrError());
+Result<std::optional<MonitorCommand>> ConsoleForwarder(
+    const CuttlefishConfig::InstanceSpecific& instance) {
+  if (!instance.console()) {
     return {};
   }
+  auto console_in_pipe_name = instance.console_in_pipe_name();
+  CF_EXPECT(
+      mkfifo(console_in_pipe_name.c_str(), 0600) == 0,
+      "Failed to create console input fifo for crosvm: " << strerror(errno));
 
-  const CuttlefishConfig::InstanceSpecific& instance_;
-  SharedFD console_forwarder_in_wr_;
-  SharedFD console_forwarder_out_rd_;
-};
+  auto console_out_pipe_name = instance.console_out_pipe_name();
+  CF_EXPECT(
+      mkfifo(console_out_pipe_name.c_str(), 0660) == 0,
+      "Failed to create console output fifo for crosvm: " << strerror(errno));
 
-}  // namespace
+  // These fds will only be read from or written to, but open them with
+  // read and write access to keep them open in case the subprocesses exit
+  auto console_forwarder_in_wr =
+      SharedFD::Open(console_in_pipe_name.c_str(), O_RDWR);
+  CF_EXPECT(console_forwarder_in_wr->IsOpen(),
+            "Failed to open console_forwarder input fifo for writes: "
+                << console_forwarder_in_wr->StrError());
 
-fruit::Component<fruit::Required<const CuttlefishConfig::InstanceSpecific>>
-ConsoleForwarderComponent() {
-  return fruit::createComponent()
-      .addMultibinding<CommandSource, ConsoleForwarder>()
-      .addMultibinding<SetupFeature, ConsoleForwarder>();
+  auto console_forwarder_out_rd =
+      SharedFD::Open(console_out_pipe_name.c_str(), O_RDWR);
+  CF_EXPECT(console_forwarder_out_rd->IsOpen(),
+            "Failed to open console_forwarder output fifo for reads: "
+                << console_forwarder_out_rd->StrError());
+
+  Command console_forwarder_cmd(ConsoleForwarderBinary());
+  return Command(ConsoleForwarderBinary())
+      .AddParameter("--console_in_fd=", console_forwarder_in_wr)
+      .AddParameter("--console_out_fd=", console_forwarder_out_rd);
 }
 
 }  // namespace cuttlefish
