895291e3796a6c2ebf6e3204418bc0f01e287df9
Cody Schuffelen <schuffelen@google.com>
2023-11-01 23:15:40 +0000

    Merge "Create and use an AutoDiagnostic utility" into main
    GitOrigin-RevId: 5d320aada0a4ce061feb10c15973656b31588b33

--- staging/host/commands/run_cvd/launch/logcat_receiver.cpp
+++ staging/host/commands/run_cvd/launch/logcat_receiver.cpp
@@ -16,71 +16,34 @@
 #include "host/commands/run_cvd/launch/launch.h"
 
 #include <string>
-#include <unordered_set>
-#include <utility>
-#include <vector>
 
 #include <fruit/fruit.h>
 
 #include "common/libs/utils/result.h"
-#include "host/commands/run_cvd/reporting.h"
 #include "host/libs/config/command_source.h"
 #include "host/libs/config/known_paths.h"
 
 namespace cuttlefish {
-namespace {
 
-class LogcatReceiver : public CommandSource, public DiagnosticInformation {
- public:
-  INJECT(LogcatReceiver(const CuttlefishConfig::InstanceSpecific& instance))
-      : instance_(instance) {}
-  // DiagnosticInformation
-  std::vector<std::string> Diagnostics() const override {
-    return {"Logcat output: " + instance_.logcat_path()};
-  }
+std::string LogcatInfo(const CuttlefishConfig::InstanceSpecific& instance) {
+  return "Logcat output: " + instance.logcat_path();
+}
 
-  // CommandSource
-  Result<std::vector<MonitorCommand>> Commands() override {
-    Command command(LogcatReceiverBinary());
-    command.AddParameter("-log_pipe_fd=", pipe_);
-    std::vector<MonitorCommand> commands;
-    commands.emplace_back(std::move(command));
-    return commands;
-  }
+Result<MonitorCommand> LogcatReceiver(
+    const CuttlefishConfig::InstanceSpecific& instance) {
+  auto log_name = instance.logcat_pipe_name();
+  CF_EXPECT(
+      mkfifo(log_name.c_str(), 0600) == 0,
+      "Unable to create named pipe at " << log_name << ": " << strerror(errno));
+  // Open the pipe here (from the launcher) to ensure the pipe is not deleted
+  // due to the usage counters in the kernel reaching zero. If this is not
+  // done and the logcat_receiver crashes for some reason the VMM may get
+  // SIGPIPE.
+  auto pipe = SharedFD::Open(log_name.c_str(), O_RDWR);
+  CF_EXPECT(pipe->IsOpen(),
+            "Can't open \"" << log_name << "\": " << pipe->StrError());
 
-  // SetupFeature
-  std::string Name() const override { return "LogcatReceiver"; }
-  bool Enabled() const override { return true; }
-
- private:
-  std::unordered_set<SetupFeature*> Dependencies() const override { return {}; }
-  Result<void> ResultSetup() {
-    auto log_name = instance_.logcat_pipe_name();
-    CF_EXPECT(mkfifo(log_name.c_str(), 0600) == 0,
-              "Unable to create named pipe at " << log_name << ": "
-                                                << strerror(errno));
-    // Open the pipe here (from the launcher) to ensure the pipe is not deleted
-    // due to the usage counters in the kernel reaching zero. If this is not
-    // done and the logcat_receiver crashes for some reason the VMM may get
-    // SIGPIPE.
-    pipe_ = SharedFD::Open(log_name.c_str(), O_RDWR);
-    CF_EXPECT(pipe_->IsOpen(),
-              "Can't open \"" << log_name << "\": " << pipe_->StrError());
-    return {};
-  }
-
-  const CuttlefishConfig::InstanceSpecific& instance_;
-  SharedFD pipe_;
-};
-
-}  // namespace
-
-fruit::Component<fruit::Required<const CuttlefishConfig::InstanceSpecific>>
-LogcatReceiverComponent() {
-  return fruit::createComponent()
-      .addMultibinding<CommandSource, LogcatReceiver>()
-      .addMultibinding<SetupFeature, LogcatReceiver>()
-      .addMultibinding<DiagnosticInformation, LogcatReceiver>();
+  return Command(LogcatReceiverBinary()).AddParameter("-log_pipe_fd=", pipe);
 }
 
 }  // namespace cuttlefish
--- staging/host/commands/run_cvd/reporting.h
+++ staging/host/commands/run_cvd/reporting.h
@@ -16,9 +16,11 @@
 
 #pragma once
 
-#include <fruit/fruit.h>
 #include <string>
+#include <tuple>
 #include <vector>
+
+#include <fruit/fruit.h>
 
 namespace cuttlefish {
 
@@ -29,5 +31,43 @@ class DiagnosticInformation {
 
   static void PrintAll(const std::vector<DiagnosticInformation*>&);
 };
+
+template <auto Fn, typename R, typename... Args>
+class DiagnosticInformationFn : public DiagnosticInformation {
+ public:
+  INJECT(DiagnosticInformationFn(Args... args))
+      : args_(std::forward_as_tuple(args...)) {}
+
+  std::vector<std::string> Diagnostics() const override {
+    if constexpr (std::is_same_v<R, std::vector<std::string>>) {
+      return std::apply(Fn, args_);
+    } else if constexpr (std::is_same_v<R, std::string>) {
+      return {std::apply(Fn, args_)};
+    } else {
+      static_assert(false, "Unexpected AutoDiagnostic return type");
+    }
+  }
+
+ private:
+  std::tuple<Args...> args_;
+};
+
+template <auto Fn1, typename Fn2>
+struct DiagnosticInformationFnImpl;
+
+template <auto Fn, typename R, typename... Args>
+struct DiagnosticInformationFnImpl<Fn, R (*)(Args...)> {
+  using Type = DiagnosticInformationFn<Fn, R, Args...>;
+
+  static fruit::Component<
+      fruit::Required<typename std::remove_reference_t<Args>...>, Type>
+  Component() {
+    return fruit::createComponent()
+        .template addMultibinding<DiagnosticInformation, Type>();
+  }
+};
+
+template <auto Fn>
+using AutoDiagnostic = DiagnosticInformationFnImpl<Fn, decltype(Fn)>;
 
 }  // namespace cuttlefish
