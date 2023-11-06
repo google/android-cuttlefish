a130792e95eccd7ea33bf18702c0d0f5b65e53e0
Chad Reynolds <chadreynolds@google.com>
2023-11-06 23:59:10 +0000

    Merge "Split `fetch_cvd` flag parsing into own file" into main
    GitOrigin-RevId: 104281a3a3512f84f569c3424d872786df603c4d

--- staging/host/commands/cvd/fetch/fetch_cvd.cc
+++ staging/host/commands/cvd/fetch/fetch_cvd.cc
@@ -24,26 +24,21 @@
 #include <iterator>
 #include <memory>
 #include <optional>
-#include <sstream>
 #include <string>
-#include <thread>
-#include <utility>
 #include <vector>
 
 #include <android-base/logging.h>
-#include <android-base/parseint.h>
 #include <android-base/strings.h>
 #include <curl/curl.h>
-#include <gflags/gflags.h>
 
 #include "common/libs/fs/shared_buf.h"
 #include "common/libs/fs/shared_fd.h"
 #include "common/libs/utils/archive.h"
 #include "common/libs/utils/environment.h"
 #include "common/libs/utils/files.h"
-#include "common/libs/utils/flag_parser.h"
 #include "common/libs/utils/result.h"
 #include "common/libs/utils/tee_logging.h"
+#include "host/commands/cvd/fetch/fetch_cvd_parser.h"
 #include "host/libs/config/fetcher_config.h"
 #include "host/libs/web/build_api.h"
 #include "host/libs/web/build_string.h"
@@ -53,51 +48,8 @@
 namespace cuttlefish {
 namespace {
 
-constexpr char kDefaultBuildTarget[] =
-    "aosp_cf_x86_64_phone-trunk_staging-userdebug";
-constexpr char kUsageMessage[] =
-    "*_build flags accept values in the following format:\n"
-    "{<branch> | <build_id>}[/<build_target>][{<filepath>}]\n"
-    "For example: "
-    "\"aosp-main/aosp_cf_x86_64_phone-trunk_staging-userdebug{file.txt}\""
-    "<branch> fetches artifacts from the latest build of the argument\n"
-    "{<filepath>} is used for certain artifacts to specify the file to "
-    "download location in the build artifacts\n"
-    "if <build_target> is not specified then the default build target is: ";
 constexpr mode_t kRwxAllMode = S_IRWXU | S_IRWXG | S_IRWXO;
 constexpr bool kOverrideEntries = true;
-
-struct BuildApiFlags {
-  std::string api_key = kDefaultApiKey;
-  std::string credential_source = kDefaultCredentialSource;
-  std::chrono::seconds wait_retry_period = kDefaultWaitRetryPeriod;
-  bool external_dns_resolver = kDefaultExternalDnsResolver;
-  std::string api_base_url = kAndroidBuildServiceUrl;
-};
-
-struct VectorFlags {
-  std::vector<std::optional<BuildString>> default_build;
-  std::vector<std::optional<BuildString>> system_build;
-  std::vector<std::optional<BuildString>> kernel_build;
-  std::vector<std::optional<BuildString>> boot_build;
-  std::vector<std::optional<BuildString>> bootloader_build;
-  std::vector<std::optional<BuildString>> otatools_build;
-  std::vector<bool> download_img_zip;
-  std::vector<bool> download_target_files_zip;
-  std::vector<std::string> boot_artifact;
-};
-
-struct FetchFlags {
-  std::string target_directory = kDefaultTargetDirectory;
-  std::vector<std::string> target_subdirectory;
-  std::optional<BuildString> host_package_build;
-  bool keep_downloaded_archives = kDefaultKeepDownloadedArchives;
-  android::base::LogSeverity verbosity = android::base::INFO;
-  bool helpxml = false;
-  BuildApiFlags build_api_flags;
-  VectorFlags vector_flags;
-  int number_of_builds = 0;
-};
 
 struct BuildStrings {
   std::optional<BuildString> default_build;
@@ -141,168 +93,6 @@ struct HostToolsTarget {
   std::optional<BuildString> build_string;
   std::string host_tools_directory;
 };
-
-Flag GflagsCompatFlagSeconds(const std::string& name,
-                             std::chrono::seconds& value) {
-  return GflagsCompatFlag(name)
-      .Getter([&value]() { return std::to_string(value.count()); })
-      .Setter([&value](const FlagMatch& match) -> Result<void> {
-        int parsed_int;
-        CF_EXPECTF(android::base::ParseInt(match.value, &parsed_int),
-                   "Failed to parse \"{}\" as an integer", match.value);
-        value = std::chrono::seconds(parsed_int);
-        return {};
-      });
-}
-
-std::vector<Flag> GetFlagsVector(FetchFlags& fetch_flags,
-                                 BuildApiFlags& build_api_flags,
-                                 VectorFlags& vector_flags,
-                                 std::string& directory) {
-  std::vector<Flag> flags;
-  flags.emplace_back(
-      GflagsCompatFlag("directory", directory)
-          .Help("Target directory to fetch files into. (deprecated)"));
-  flags.emplace_back(
-      GflagsCompatFlag("target_directory", fetch_flags.target_directory)
-          .Help("Target directory to fetch files into."));
-  flags.emplace_back(GflagsCompatFlag("keep_downloaded_archives",
-                                      fetch_flags.keep_downloaded_archives)
-                         .Help("Keep downloaded zip/tar."));
-  flags.emplace_back(VerbosityFlag(fetch_flags.verbosity));
-  flags.emplace_back(
-      GflagsCompatFlag("target_subdirectory", fetch_flags.target_subdirectory)
-          .Help("Target subdirectory to fetch files into.  Specifically aimed "
-                "at organizing builds when there are multiple fetches. "
-                "**Note**: directory separator automatically prepended, only "
-                "give the subdirectory name."));
-  flags.emplace_back(
-      GflagsCompatFlag("host_package_build", fetch_flags.host_package_build)
-          .Help("source for the host cvd tools"));
-
-  flags.emplace_back(GflagsCompatFlag("api_key", build_api_flags.api_key)
-                         .Help("API key ofr the Android Build API"));
-  flags.emplace_back(
-      GflagsCompatFlag("credential_source", build_api_flags.credential_source)
-          .Help("Build API credential source"));
-  flags.emplace_back(GflagsCompatFlagSeconds("wait_retry_period",
-                                             build_api_flags.wait_retry_period)
-                         .Help("Retry period for pending builds given in "
-                               "seconds. Set to 0 to not wait."));
-  flags.emplace_back(
-      GflagsCompatFlag("external_dns_resolver",
-                       build_api_flags.external_dns_resolver)
-          .Help("Use an out-of-process mechanism to resolve DNS queries"));
-  flags.emplace_back(
-      GflagsCompatFlag("api_base_url", build_api_flags.api_base_url)
-          .Help("The base url for API requests to download artifacts from"));
-
-  flags.emplace_back(
-      GflagsCompatFlag("default_build", vector_flags.default_build)
-          .Help("source for the cuttlefish build to use (vendor.img + host)"));
-  flags.emplace_back(GflagsCompatFlag("system_build", vector_flags.system_build)
-                         .Help("source for system.img and product.img"));
-  flags.emplace_back(GflagsCompatFlag("kernel_build", vector_flags.kernel_build)
-                         .Help("source for the kernel or gki target"));
-  flags.emplace_back(GflagsCompatFlag("boot_build", vector_flags.boot_build)
-                         .Help("source for the boot or gki target"));
-  flags.emplace_back(
-      GflagsCompatFlag("bootloader_build", vector_flags.bootloader_build)
-          .Help("source for the bootloader target"));
-  flags.emplace_back(
-      GflagsCompatFlag("otatools_build", vector_flags.otatools_build)
-          .Help("source for the host ota tools"));
-
-  flags.emplace_back(
-      GflagsCompatFlag("boot_artifact", vector_flags.boot_artifact)
-          .Help("name of the boot image in boot_build"));
-  flags.emplace_back(GflagsCompatFlag("download_img_zip",
-                                      vector_flags.download_img_zip,
-                                      kDefaultDownloadImgZip)
-                         .Help("Whether to fetch the -img-*.zip file."));
-  flags.emplace_back(
-      GflagsCompatFlag("download_target_files_zip",
-                       vector_flags.download_target_files_zip,
-                       kDefaultDownloadTargetFilesZip)
-          .Help("Whether to fetch the -target_files-*.zip file."));
-
-  std::stringstream help_message;
-  help_message << kUsageMessage << kDefaultBuildTarget;
-  flags.emplace_back(HelpFlag(flags, help_message.str()));
-  flags.emplace_back(
-      HelpXmlFlag(flags, std::cout, fetch_flags.helpxml, help_message.str()));
-
-  flags.emplace_back(UnexpectedArgumentGuard());
-  return flags;
-}
-
-Result<int> GetNumberOfBuilds(
-    const VectorFlags& flags,
-    const std::vector<std::string>& subdirectory_flag) {
-  std::optional<int> number_of_builds;
-  for (const auto& flag_size :
-       {flags.default_build.size(), flags.system_build.size(),
-        flags.kernel_build.size(), flags.boot_build.size(),
-        flags.bootloader_build.size(), flags.otatools_build.size(),
-        flags.boot_artifact.size(), flags.download_img_zip.size(),
-        flags.download_target_files_zip.size(), subdirectory_flag.size()}) {
-    if (flag_size == 0) {
-      // a size zero flag vector means the flag was not given
-      continue;
-    }
-    if (number_of_builds) {
-      CF_EXPECT(
-          flag_size == *number_of_builds,
-          "Mismatched flag lengths: " << *number_of_builds << "," << flag_size);
-    }
-    number_of_builds = flag_size;
-  }
-  // if no flags had values there is 1 all-default build
-  return number_of_builds.value_or(1);
-}
-
-Result<FetchFlags> GetFlagValues(int argc, char** argv) {
-  FetchFlags fetch_flags;
-  BuildApiFlags build_api_flags;
-  VectorFlags vector_flags;
-  std::string directory;
-
-  std::vector<Flag> flags =
-      GetFlagsVector(fetch_flags, build_api_flags, vector_flags, directory);
-  std::vector<std::string> args = ArgsToVec(argc - 1, argv + 1);
-  CF_EXPECT(ParseFlags(flags, args), "Could not process command line flags.");
-
-  if (!directory.empty()) {
-    LOG(ERROR) << "Please use --target_directory instead of --directory";
-    if (fetch_flags.target_directory.empty()) {
-      fetch_flags.target_directory = directory;
-    }
-  } else {
-    if (fetch_flags.target_directory.empty()) {
-      fetch_flags.target_directory = CurrentDirectory();
-    }
-  }
-  fetch_flags.target_directory = AbsolutePath(fetch_flags.target_directory);
-
-  if (!vector_flags.boot_artifact.empty()) {
-    LOG(ERROR) << "Please use the build string filepath syntax instead of "
-                  "deprecated --boot_artifact";
-    for (const auto& build_string : vector_flags.boot_build) {
-      if (build_string) {
-        CF_EXPECT(!GetFilepath(*build_string),
-                  "Cannot use both the --boot_artifact flag and set the "
-                  "filepath in the boot build string.  Please use only the "
-                  "build string filepath");
-      }
-    }
-  }
-
-  fetch_flags.build_api_flags = build_api_flags;
-  fetch_flags.vector_flags = vector_flags;
-  fetch_flags.number_of_builds = CF_EXPECT(GetNumberOfBuilds(
-      fetch_flags.vector_flags, fetch_flags.target_subdirectory));
-  return {fetch_flags};
-}
 
 bool ShouldAppendSubdirectory(const FetchFlags& flags) {
   return flags.number_of_builds > 1 || !flags.target_subdirectory.empty();
