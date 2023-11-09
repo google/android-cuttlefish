badfad63da5287e848c896f2a76766f478d92c7a
Elie Kheirallah <khei@google.com>
2023-11-09 04:32:06 +0000

    Use cuttlefish_config to restore VM
    cuttlefish_config has all the config required by CF. This allows for a
    much simpler restore command: `cvd start --snapshot_path=PATH` and all
    the other arguments can be read from the config file.
    
    Bug: 306445837
    Test: cvd start --snapshot_path=PATH
    Change-Id: I75e398fdb8e3ae6b209c891231cc2430a18c7eda

--- cvd/cuttlefish/host/libs/config/cuttlefish_config.h
+++ cvd/cuttlefish/host/libs/config/cuttlefish_config.h
@@ -107,6 +107,7 @@ class CuttlefishConfig {
   // Saves the configuration object in a file, it can then be read in other
   // processes by passing the --config_file option.
   bool SaveToFile(const std::string& file) const;
+  bool LoadFromFile(const char* file);
 
   bool SaveFragment(const ConfigFragment&);
   bool LoadFragment(ConfigFragment&) const;
@@ -140,15 +141,6 @@ class CuttlefishConfig {
     int height;
     int dpi;
     int refresh_rate_hz;
-  };
-
-  struct TouchpadConfig {
-    int width;
-    int height;
-
-    static Json::Value Serialize(
-        const CuttlefishConfig::TouchpadConfig& config);
-    static TouchpadConfig Deserialize(const Json::Value& config_json);
   };
 
   void set_secure_hals(const std::set<std::string>& hals);
@@ -397,7 +389,7 @@ class CuttlefishConfig {
 
     std::string instance_internal_uds_dir() const;
 
-    std::string touch_socket_path(int touch_dev_idx) const;
+    std::string touch_socket_path(int screen_idx) const;
     std::string rotary_socket_path() const;
     std::string keyboard_socket_path() const;
     std::string switches_socket_path() const;
@@ -561,7 +553,6 @@ class CuttlefishConfig {
     int gdb_port() const;
 
     std::vector<DisplayConfig> display_configs() const;
-    std::vector<TouchpadConfig> touchpad_configs() const;
 
     std::string grpc_socket_path() const;
     int memory_mb() const;
@@ -750,8 +741,6 @@ class CuttlefishConfig {
     void set_blank_data_image_mb(int blank_data_image_mb);
     void set_gdb_port(int gdb_port);
     void set_display_configs(const std::vector<DisplayConfig>& display_configs);
-    void set_touchpad_configs(
-        const std::vector<TouchpadConfig>& touchpad_configs);
     void set_memory_mb(int memory_mb);
     void set_ddr_mem_mb(int ddr_mem_mb);
     Result<void> set_setupwizard_mode(const std::string& title);
@@ -936,7 +925,6 @@ class CuttlefishConfig {
  private:
   std::unique_ptr<Json::Value> dictionary_;
 
-  bool LoadFromFile(const char* file);
   static CuttlefishConfig* BuildConfigImpl(const std::string& path);
 
   CuttlefishConfig(const CuttlefishConfig&) = delete;
