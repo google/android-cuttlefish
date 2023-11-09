48cb79f1d90f84dc41b1aceaf52a6063604d4fc3
Gil Cukierman <cukie@google.com>
2023-11-09 16:05:12 +0000

    Implement Cellular Identifier Transparency In Cuttlefish
    Bug: 276752426
    
    Change-Id: I8a7b8715e5491ed345449f2291832acea746c675
    Test: mm

--- guest/hals/ril/reference-libril/RefRadioNetwork.h
+++ guest/hals/ril/reference-libril/RefRadioNetwork.h
@@ -22,6 +22,8 @@ namespace cf::ril {
 class RefRadioNetwork : public android::hardware::radio::compat::RadioNetwork {
     ::aidl::android::hardware::radio::network::UsageSetting mUsageSetting =
             ::aidl::android::hardware::radio::network::UsageSetting::VOICE_CENTRIC;
+    // As per the specs, the default is true.
+    bool mIsCellualIdentifierTransparencyEnabled = true;
 
   public:
     using android::hardware::radio::compat::RadioNetwork::RadioNetwork;
@@ -51,6 +53,10 @@ class RefRadioNetwork : public android::hardware::radio::compat::RadioNetwork {
     ::ndk::ScopedAStatus setNullCipherAndIntegrityEnabled(int32_t serial, bool enabled) override;
 
     ::ndk::ScopedAStatus isNullCipherAndIntegrityEnabled(int32_t serial) override;
+
+    ::ndk::ScopedAStatus setCellularIdentifierTransparencyEnabled(int32_t serial, bool enabled) override;
+
+    ::ndk::ScopedAStatus isCellularIdentifierTransparencyEnabled(int32_t serial) override;
 };
 
 }  // namespace cf::ril
