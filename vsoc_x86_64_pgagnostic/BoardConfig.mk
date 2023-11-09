36c305282bddf957b6c6021103990b3b4cec67da
Juan Yescas <jyescas@google.com>
2023-11-08 17:42:35 -0800

    16k: Add x86-64 page size agnostic target
    The aosp_cf_x86_64_phone_pgagnostic target will be used
    to simulate 16k page size in x86-64 cuttefish.
    
    Bug: 309816695
    Test: source build/envsetup.sh
          lunch aosp_cf_x86_64_phone_pgagnostic-trunk-userdebug
          m
    Change-Id: I3ab2a79062e44be4f293a0e29db9445f53dc6404

---
+++ vsoc_x86_64_pgagnostic/BoardConfig.mk
@@ -0,0 +1,47 @@
+#
+# Copyright 2023 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# x86_64 (64-bit only) page size agnostic target for Cuttlefish
+#
+
+TARGET_BOARD_PLATFORM := vsoc_x86_64
+TARGET_ARCH := x86_64
+TARGET_ARCH_VARIANT := silvermont
+TARGET_CPU_ABI := x86_64
+
+TARGET_NATIVE_BRIDGE_ARCH := arm64
+TARGET_NATIVE_BRIDGE_ARCH_VARIANT := armv8-a
+TARGET_NATIVE_BRIDGE_CPU_VARIANT := generic
+TARGET_NATIVE_BRIDGE_ABI := arm64-v8a
+
+AUDIOSERVER_MULTILIB := first
+
+-include device/google/cuttlefish/shared/BoardConfig.mk
+-include device/google/cuttlefish/shared/bluetooth/BoardConfig.mk
+-include device/google/cuttlefish/shared/camera/BoardConfig.mk
+-include device/google/cuttlefish/shared/gnss/BoardConfig.mk
+-include device/google/cuttlefish/shared/graphics/BoardConfig.mk
+-include device/google/cuttlefish/shared/identity/BoardConfig.mk
+-include device/google/cuttlefish/shared/reboot_escrow/BoardConfig.mk
+-include device/google/cuttlefish/shared/sensors/BoardConfig.mk
+-include device/google/cuttlefish/shared/swiftshader/BoardConfig.mk
+-include device/google/cuttlefish/shared/telephony/BoardConfig.mk
+-include device/google/cuttlefish/shared/vibrator/BoardConfig.mk
+
+ifneq ($(BOARD_IS_AUTOMOTIVE), true)
+-include device/google/cuttlefish/shared/virgl/BoardConfig.mk
+endif
