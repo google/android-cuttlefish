184330651f083d72088c50d9621710dd52e1c396
Salvatore Ferro <sferro@google.com>
2023-11-14 19:21:58 +0000

    Merge "Revert "Add config support for extra_bootconfig_args flag"" into main am: bc6c31bb69 am: be4517acbc
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2828610
    
    Change-Id: Iecce20f7a90e469010ef9a60a5cb1707f2d8d547
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- base/cvd/cuttlefish/host/commands/cvd/parser/instance/cf_boot_configs.cpp
+++ base/cvd/cuttlefish/host/commands/cvd/parser/instance/cf_boot_configs.cpp
@@ -30,8 +30,6 @@ Result<void> InitBootConfigs(Json::Value& instances) {
   for (auto& instance : instances) {
     CF_EXPECT(InitConfig(instance, CF_DEFAULTS_ENABLE_BOOTANIMATION,
                          {"boot", "enable_bootanimation"}));
-    CF_EXPECT(InitConfig(instance, CF_DEFAULTS_EXTRA_BOOTCONFIG_ARGS,
-                         {"boot", "bootconfig_args"}));
   }
   return {};
 }
@@ -41,8 +39,6 @@ Result<std::vector<std::string>> GenerateBootFlags(
   std::vector<std::string> result;
   result.emplace_back(CF_EXPECT(GenerateGflag(
       instances, "enable_bootanimation", {"boot", "enable_bootanimation"})));
-  result.emplace_back(CF_EXPECT(GenerateGflag(
-      instances, "extra_bootconfig_args", {"boot", "bootconfig_args"})));
   return result;
 }
 
