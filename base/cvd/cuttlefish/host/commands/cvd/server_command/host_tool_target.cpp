152e41b2f53bd26ed775ffe95b0ebbe5656b906f
Treehugger Robot <android-test-infra-autosubmit@system.gserviceaccount.com>
2023-11-15 18:35:15 +0000

    Merge "Don't ignore failure to run subcommands from cvd" into main am: 10d34b8925
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2830822
    
    Change-Id: Icd01e7a2b7c6d62ac471fc944f4196fee4d111f2
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- base/cvd/cuttlefish/host/commands/cvd/server_command/host_tool_target.cpp
+++ base/cvd/cuttlefish/host/commands/cvd/server_command/host_tool_target.cpp
@@ -76,11 +76,13 @@ Result<HostToolTarget> HostToolTarget::Create(
     command.AddEnvironmentVariable(kAndroidSoongHostOut, artifacts_path);
 
     std::string xml_str;
+    std::string err_out;
     RunWithManagedStdio(std::move(command), nullptr, std::addressof(xml_str),
-                        nullptr);
+                        std::addressof(err_out));
     auto flags_opt = CollectFlagsFromHelpxml(xml_str);
     if (!flags_opt) {
       LOG(ERROR) << bin_path << " --helpxml failed.";
+      LOG(ERROR) << err_out;
       continue;
     }
     auto flags = std::move(*flags_opt);
