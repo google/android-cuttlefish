816416aa782398968af848cbd94e3d0ae7e03ed7
Ram Muthiah <rammuthiah@google.com>
2023-11-23 20:26:05 +0000

    Revert "Separated the status handler from generics" am: 987ae9a322 am: 7fc8f3a319
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2845381
    
    Change-Id: I2669360c4e45d96d54a3e564ee0f1e0ac336d5ac
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- base/cvd/cuttlefish/host/commands/cvd/server.cc
+++ base/cvd/cuttlefish/host/commands/cvd/server.cc
@@ -58,7 +58,6 @@
 #include "host/commands/cvd/server_command/reset.h"
 #include "host/commands/cvd/server_command/snapshot.h"
 #include "host/commands/cvd/server_command/start.h"
-#include "host/commands/cvd/server_command/status.h"
 #include "host/commands/cvd/server_command/subcmd.h"
 #include "host/commands/cvd/server_constants.h"
 #include "host/libs/config/known_paths.h"
