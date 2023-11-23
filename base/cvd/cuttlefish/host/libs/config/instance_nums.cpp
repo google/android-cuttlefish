b5a15751b05d233456451f75112973acc7e418b8
Treehugger Robot <android-test-infra-autosubmit@system.gserviceaccount.com>
2023-11-23 02:19:46 +0000

    Merge changes Ic30bacc9,I41ea6b88,Iea8569e3 into main am: 22f38ba770 am: f4ea5ab175
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2844930
    
    Change-Id: I45a43ee0316a27cfcc13beb7be33198765e1f3db
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- base/cvd/cuttlefish/host/libs/config/instance_nums.cpp
+++ base/cvd/cuttlefish/host/libs/config/instance_nums.cpp
@@ -21,7 +21,7 @@
 
 #include "common/libs/utils/contains.h"
 #include "common/libs/utils/flag_parser.h"
-#include "host/libs/config/cuttlefish_config.h"
+#include "host/libs/config/config_utils.h"
 
 namespace cuttlefish {
 
