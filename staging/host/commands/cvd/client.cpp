234af262536b9ba9a2f67b4fe2d6cb1564fe4587
Ram Muthiah <rammuthiah@google.com>
2023-11-23 08:16:06 -0800

    [skip ci] Revert "Replace "-INTERNAL_*" flags with symbolic names"
    This reverts commit 7bec32f6546ccc21ecf31b945012c15fefcae7c0.
    
    Bug: 312643212
    Change-Id: I86a0821c547e4438eb87d59b5c2049c26a5a212f
    GitOrigin-RevId: 191000343b7bf70d46987392ab8c2dd678c222f4

--- staging/host/commands/cvd/client.cpp
+++ staging/host/commands/cvd/client.cpp
@@ -35,7 +35,6 @@
 #include "host/commands/cvd/frontline_parser.h"
 #include "host/commands/cvd/handle_reset.h"
 #include "host/commands/cvd/metrics/cvd_metrics_api.h"
-#include "host/commands/cvd/run_server.h"
 #include "host/libs/config/host_tools_version.h"
 
 namespace cuttlefish {
@@ -389,7 +388,7 @@ Result<void> CvdClient::StartCvdServer() {
   CF_EXPECT(server_fd->IsOpen(), server_fd->StrError());
 
   Command command(kServerExecPath);
-  command.AddParameter("-", kInternalServerFd, "=", server_fd);
+  command.AddParameter("-INTERNAL_server_fd=", server_fd);
   SubprocessOptions options;
   options.ExitWithParent(false);
   command.Start(options);
