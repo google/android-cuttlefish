b4d3b28cda23148cb362a46d4754a9959997dac5
Seungjae Yoo <seungjaeyoo@google.com>
2023-11-24 10:41:32 +0900

    Golint: Add package description to host_orchestrator

--- frontend/src/host_orchestrator/main.go
+++ frontend/src/host_orchestrator/main.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package exists for running host orchestrator(HO) server.
 package main
 
 import (
