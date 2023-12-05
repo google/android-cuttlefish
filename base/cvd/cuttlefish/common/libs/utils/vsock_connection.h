a22a8eda41fce996c039fadbbd541277a852117e
Sean Dooher <dooher@google.com>
2023-12-05 20:56:51 +0000

    Merge "vsock: Rewrite vsock library to use threadsafe nonblocking IO" into main am: 61673e9ead am: a95f0ce4ab
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2717340
    
    Change-Id: I2cac87fb4acf8a6980944925719dbf0bedad58c3
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- base/cvd/cuttlefish/common/libs/utils/vsock_connection.h
+++ base/cvd/cuttlefish/common/libs/utils/vsock_connection.h
@@ -15,10 +15,14 @@
  */
 #pragma once
 
+#include <sys/time.h>
+
+#include <atomic>
 #include <cstddef>
 #include <cstdint>
 #include <functional>
 #include <future>
+#include <memory>
 #include <mutex>
 #include <string>
 #include <vector>
@@ -26,65 +30,98 @@
 #include <json/json.h>
 
 #include "common/libs/fs/shared_fd.h"
+#include "common/libs/utils/result.h"
 
 namespace cuttlefish {
+using VsockLockGuard = std::vector<std::unique_lock<std::mutex>>;
 
 class VsockConnection {
  public:
   virtual ~VsockConnection();
-  virtual bool Connect(unsigned int port, unsigned int cid,
-                       std::optional<int> vhost_user_vsock_cid) = 0;
-  virtual void Disconnect();
-  std::future<bool> ConnectAsync(unsigned int port, unsigned int cid,
-                                 std::optional<int> vhost_user_vsock_cid);
-  void SetDisconnectCallback(std::function<void()> callback);
 
+  void Disconnect();
   bool IsConnected();
+  void SetDisconnectCallback(std::function<void()> callback);
   bool DataAvailable();
-  int32_t Read();
-  bool Read(std::vector<char>& data);
-  std::vector<char> Read(size_t size);
-  std::future<std::vector<char>> ReadAsync(size_t size);
 
-  bool ReadMessage(std::vector<char>& data);
-  std::vector<char> ReadMessage();
-  std::future<std::vector<char>> ReadMessageAsync();
-  Json::Value ReadJsonMessage();
-  std::future<Json::Value> ReadJsonMessageAsync();
+  Result<int32_t> ReadInt32();
+  Result<void> Read(std::vector<char>& data);
+  Result<std::vector<char>> Read(size_t size);
+  Result<void> ReadMessage(std::vector<char>& data);
+  Result<std::vector<char>> ReadMessage();
+  Result<Json::Value> ReadJsonMessage();
+  std::future<Result<std::vector<char>>> ReadAsync(size_t size);
+  std::future<Result<std::vector<char>>> ReadMessageAsync();
+  std::future<Result<Json::Value>> ReadJsonMessageAsync();
 
-  bool Write(int32_t data);
-  bool Write(const char* data, unsigned int size);
-  bool Write(const std::vector<char>& data);
-  bool WriteMessage(const std::string& data);
-  bool WriteMessage(const std::vector<char>& data);
-  bool WriteMessage(const Json::Value& data);
-  bool WriteStrides(const char* data, unsigned int size,
-                    unsigned int num_strides, int stride_size);
+  Result<void> Write(int32_t data);
+  Result<void> Write(const char* data, unsigned int size);
+  Result<void> Write(const std::vector<char>& data);
+  Result<void> WriteMessage(const std::string& data);
+  Result<void> WriteMessage(const std::vector<char>& data);
+  Result<void> WriteMessage(const Json::Value& data);
 
  protected:
-  std::recursive_mutex read_mutex_;
-  std::recursive_mutex write_mutex_;
-  std::function<void()> disconnect_callback_;
+  enum class WaitStatus { kReady, kTimedOut, kDisconnected };
+
+  static constexpr struct timeval default_timeout = {.tv_sec = 60,
+                                                     .tv_usec = 0};
+
+  VsockConnection();
+
+  // Unsynchronized variants
+  Result<int32_t> ReadInt32Internal();
+  Result<void> ReadInternal(std::vector<char>& data);
+  Result<void> WriteInternal(int32_t data);
+  Result<void> WriteInternal(const char* data, unsigned int size);
+
+  WaitStatus WaitForReadReady(struct timeval timeout = default_timeout);
+  WaitStatus WaitForWriteReady(struct timeval timeout = default_timeout);
+  void SignalDisconnect();
+  void ClearDisconnect();
+  VsockLockGuard DisconnectInternal();
+  VsockLockGuard AcquireReadLock();
+  VsockLockGuard AcquireWriteLock();
+  VsockLockGuard AcquireReadWriteLocks();
+
+  std::mutex read_mutex_;
+  std::mutex write_mutex_;
   SharedFD fd_;
+  SharedFD disconnect_notifier_;
+  std::atomic_bool is_connected_;
+  std::function<void()> disconnect_callback_;
 };
 
 class VsockClientConnection : public VsockConnection {
  public:
-  // the value of vhost_user_vsock_cid isn't actually used, it works like bool,
-  // so any value except nullopt means true
-  bool Connect(unsigned int port, unsigned int cid,
-               std::optional<int> vhost_user) override;
+  bool Connect(unsigned int port, unsigned int cid, bool vhost_user);
+  std::future<bool> ConnectAsync(unsigned int port, unsigned int cid,
+                                 bool vhost_user);
+
+ protected:
+  virtual SharedFD CreateSocket(unsigned int port, unsigned int cid,
+                                bool vhost_user);
 };
 
-class VsockServerConnection : public VsockConnection {
+class VsockServer : private VsockConnection {
  public:
-  virtual ~VsockServerConnection();
-  void ServerShutdown();
-  bool Connect(unsigned int port, unsigned int cid,
-               std::optional<int> vhost_user_vsock_cid) override;
+  virtual ~VsockServer();
+  Result<void> Start(unsigned int port, unsigned int cid,
+                     std::optional<int> vhost_user_vsock_cid);
+  void Stop();
+  bool IsRunning();
+  Result<std::unique_ptr<VsockConnection>> AcceptConnection();
+  std::future<Result<std::unique_ptr<VsockConnection>>> AcceptConnectionAsync();
+
+ protected:
+  virtual SharedFD CreateSocket(unsigned int port, unsigned int cid,
+                                std::optional<int> vhost_user_vsock_cid);
 
  private:
-  SharedFD server_fd_;
+  class VsockServerConnection : public VsockConnection {
+    VsockServerConnection(SharedFD vsock_fd);
+    friend VsockServer;
+  };
 };
 
 }  // namespace cuttlefish
