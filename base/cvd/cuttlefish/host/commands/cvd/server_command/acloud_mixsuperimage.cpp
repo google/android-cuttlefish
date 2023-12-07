febc294f3569c6f7c2ba078598bf95c620fe3198
Cody Schuffelen <schuffelen@google.com>
2023-12-07 09:44:52 +0000

    Merge "Handle unused Result<>s in acloud_mixsuperimage.cpp" into main am: 60040bcb38 am: 70233dda9b
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2856401
    
    Change-Id: I2a277f89cc4e63e76f2f33cf6fea60ce5ee02b60
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- base/cvd/cuttlefish/host/commands/cvd/server_command/acloud_mixsuperimage.cpp
+++ base/cvd/cuttlefish/host/commands/cvd/server_command/acloud_mixsuperimage.cpp
@@ -242,8 +242,8 @@ class AcloudMixSuperImageCommand : public CvdServerHandler {
 
     TemporaryFile new_misc_info;
     std::string new_misc_info_path = new_misc_info.path;
-    _RewriteMiscInfo(new_misc_info_path, misc_info_path, lpmake_binary,
-                     get_image);
+    CF_EXPECT(_RewriteMiscInfo(new_misc_info_path, misc_info_path,
+                               lpmake_binary, get_image));
 
     Command command(build_super_image_binary);
     command.AddParameter(new_misc_info_path);
@@ -252,7 +252,7 @@ class AcloudMixSuperImageCommand : public CvdServerHandler {
     auto subprocess = command.Start(options);
     CF_EXPECT(subprocess.Started());
     CF_EXPECT(waiter_.Setup(std::move(subprocess)));
-    callback_unlock();
+    CF_EXPECT(callback_unlock());
     CF_EXPECT(waiter_.Wait());
     return {};
   }
