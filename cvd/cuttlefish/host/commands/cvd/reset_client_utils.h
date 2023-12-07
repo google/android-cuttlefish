6c83fa2eef9154a6656e0a563480dc36c1dc3814
Steve Kim <kwstephenkim@google.com>
2023-12-07 19:43:36 +0000

    Merge "Fix the location of In Client message" into main

--- cvd/cuttlefish/host/commands/cvd/reset_client_utils.h
+++ cvd/cuttlefish/host/commands/cvd/reset_client_utils.h
@@ -33,18 +33,25 @@ class RunCvdProcessManager {
   using GroupProcInfo = RunCvdProcessCollector::GroupProcInfo;
 
   static Result<RunCvdProcessManager> Get();
-  Result<void> KillAllCuttlefishInstances(const bool cvd_server_children_only,
-                                          const bool clear_runtime_dirs);
+  // called by cvd reset handler
+  Result<void> KillAllCuttlefishInstances(bool cvd_server_children_only,
+                                          bool clear_runtime_dirs);
+  // called by cvd start
+  Result<void> ForcefullyStopGroup(bool cvd_server_children_only,
+                                   uid_t any_id_in_group);
 
  private:
   RunCvdProcessManager() = delete;
   RunCvdProcessManager(RunCvdProcessCollector&&);
   static Result<void> RunStopCvd(const GroupProcInfo& run_cvd_info,
-                                 const bool clear_runtime_dirs);
-  Result<void> RunStopCvdAll(const bool cvd_server_children_only,
-                             const bool clear_runtime_dirs);
-  Result<void> SendSignals(const bool cvd_server_children_only);
-  void DeleteLockFiles(const bool cvd_server_children_only);
+                                 bool clear_runtime_dirs);
+  Result<void> RunStopCvdAll(bool cvd_server_children_only,
+                             bool clear_runtime_dirs);
+  Result<void> SendSignal(bool cvd_server_children_only, const GroupProcInfo&);
+  Result<void> DeleteLockFile(bool cvd_server_children_only,
+                              const GroupProcInfo&);
+  Result<void> ForcefullyStopGroup(bool cvd_server_children_only,
+                                   const GroupProcInfo& group);
 
   RunCvdProcessCollector run_cvd_process_collector_;
 };
