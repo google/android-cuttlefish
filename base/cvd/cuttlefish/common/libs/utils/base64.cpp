90dbaa7d0c2f58e4f5f068c028e53b6e5aa1c679
Jorge Moreira <jemoreira@google.com>
2023-12-11 20:03:30 +0000

    Merge "Revert "Avoid using boringssl specific fuctions in cvd"" into main am: 7e32647836
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2864877
    
    Change-Id: Ica0d079819faee71d94b2cdeac3934be7199d24b
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- base/cvd/cuttlefish/common/libs/utils/base64.cpp
+++ base/cvd/cuttlefish/common/libs/utils/base64.cpp
@@ -18,56 +18,20 @@
 
 #include <cstddef>
 #include <cstdint>
-#include <optional>
 #include <string>
 #include <vector>
 
-#include <openssl/evp.h>
+#include <openssl/base64.h>
 
 namespace cuttlefish {
 
-namespace {
-
-// EVP_EncodedLength is boringssl specific so it can't be used outside of
-// android.
-std::optional<size_t> EncodedLength(size_t len) {
-  if (len + 2 < len) {
-    return std::nullopt;
-  }
-  len += 2;
-  len /= 3;
-
-  if (((len << 2) >> 2) != len) {
-    return std::nullopt;
-  }
-  len <<= 2;
-
-  if (len + 1 < len) {
-    return std::nullopt;
-  }
-  len++;
-
-  return {len};
-}
-
-// EVP_DecodedLength is boringssl specific so it can't be used outside of
-// android.
-std::optional<size_t> DecodedLength(size_t len) {
-  if (len % 4 != 0) {
-    return std::nullopt;
-  }
-
-  return {(len / 4) * 3};
-}
-
-}  // namespace
-
 bool EncodeBase64(const void *data, std::size_t size, std::string *out) {
-  auto len_res = EncodedLength(size);
+  std::size_t enc_len = 0;
+  auto len_res = EVP_EncodedLength(&enc_len, size);
   if (!len_res) {
     return false;
   }
-  out->resize(*len_res);
+  out->resize(enc_len);
   auto enc_res =
       EVP_EncodeBlock(reinterpret_cast<std::uint8_t *>(out->data()),
                       reinterpret_cast<const std::uint8_t *>(data), size);
@@ -79,26 +43,17 @@ bool EncodeBase64(const void *data, std::size_t size, std::string *out) {
 }
 
 bool DecodeBase64(const std::string &data, std::vector<std::uint8_t> *buffer) {
-  auto len_res = DecodedLength(data.size());
+  std::size_t out_len;
+  auto len_res = EVP_DecodedLength(&out_len, data.size());
   if (!len_res) {
     return false;
   }
-  auto out_len = *len_res;
   buffer->resize(out_len);
-  auto actual_len = EVP_DecodeBlock(buffer->data(),
-                                reinterpret_cast<const uint8_t *>(data.data()),
-                                data.size());
-  if (actual_len < 0) {
-    return false;
-  }
-
-  // DecodeBlock leaves null characters at the end of the buffer when the
-  // decoded message is not a multiple of 3.
-  while (!buffer->empty() && buffer->back() == '\0') {
-    buffer->pop_back();
-  }
-
-  return true;
+  auto result = EVP_DecodeBase64(buffer->data(), &out_len, out_len,
+                          reinterpret_cast<const std::uint8_t *>(data.data()),
+                          data.size());
+  buffer->resize(out_len); // remove padding '=' characters
+  return result;
 }
 
 }  // namespace cuttlefish
