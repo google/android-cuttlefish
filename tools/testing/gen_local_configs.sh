b389cec16ae25130c71f8fcd42c7cfef12065bef
sferro <sferro@google.com>
2023-12-13 10:52:23 -0500

    Implement per-instance config driven boot properties for `cvd load`.
    Test: 1) Sync AOSP 'main' branch,
    2) lunch aosp_cf_x86_64_minidroid-trunk_staging-userdebug
    3) follow instructions in /device/google/cuttlefish/tools/testing/gen_local_configs.sh to generate some test configs
    4) run `cvd load /tmp/local_bootconfig.json`
    5) then `adb shell` to running instance and verify value via 'adb getprop ro.boot.W'
    
    Change-Id: I2afd741fa6608d5c235cf609f100a58e71f0b0e2

--- tools/testing/gen_local_configs.sh
+++ tools/testing/gen_local_configs.sh
@@ -1,10 +1,16 @@
 
-# Run this script to generate.json files in
+# This utility generates .json files in
 # /tmp that match current AOSP build environment for testing.
+# These configs are used for cvd multi-vd testing.
 
-# Usage : run this script within the same shell /
+# Usage:
+# - First ensure you are within the same shell /
 # environment as a successful AOSP build.(or init
-# the environment via `lunch`, then execute this script)
+# the environment via `lunch` in new shell)
+# - Then navigate to the folder:
+# cd /device/google/cuttlefish/host/cvd_test_configs/local_tests
+# - Run this script
+# ../../../tools/testing/gen_local_configs.sh
 
 if [[ -z "${ANDROID_PRODUCT_OUT}" ]] || [[ -z "${ANDROID_HOST_OUT}" ]]; then
   echo "ANDROID_PRODUCT_OUT and ANDROID_HOST_OUT must be defined for this script to work. Did you run lunch?"
