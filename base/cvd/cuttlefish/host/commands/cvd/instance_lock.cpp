13a29365016ad1e5f563a8496a1f28e2f19e7c98
Treehugger Robot <android-test-infra-autosubmit@system.gserviceaccount.com>
2024-01-09 21:59:58 +0000

    Merge "LockAllAvailable should move on on file-opening failure" into main am: 8fa9f024e2
    Original change: https://android-review.googlesource.com/c/device/google/cuttlefish/+/2901261
    
    Change-Id: I8343e0533f3042b7053063b429dd2282fa9e1938
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- base/cvd/cuttlefish/host/commands/cvd/instance_lock.cpp
+++ base/cvd/cuttlefish/host/commands/cvd/instance_lock.cpp
@@ -19,7 +19,6 @@
 #include <sys/file.h>
 
 #include <algorithm>
-#include <cstring>
 #include <regex>
 #include <sstream>
 #include <string>
@@ -237,12 +236,6 @@ InstanceLockFileManager::TryAcquireUnusedLock() {
       return std::move(*lock);
     }
   }
-  return {};
-}
-
-Result<void> InstanceLockFileManager::RemoveLockFile(int instance_num) {
-  const auto lock_file_path = CF_EXPECT(LockFilePath(instance_num));
-  CF_EXPECT(RemoveFile(lock_file_path), std::strerror(errno));
   return {};
 }
 
