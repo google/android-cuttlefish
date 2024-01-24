61b42797418a454694022d4342eded247e0e7e30
Seungjae Yoo <seungjaeyoo@google.com>
2024-01-24 14:40:22 +0900

    HO is now usable in ARM64 docker image

--- docker/arm-server/run_services.sh
+++ docker/arm-server/run_services.sh
@@ -2,5 +2,5 @@
 
 service cuttlefish-host-resources start
 service cuttlefish-operator start
-#service cuttlefish-host_orchestrator start
+service cuttlefish-host_orchestrator start
 /root/cvd_home/bin/cvd start --vhost-user-vsock=true --report_anonymous_usage_stats=y $@
