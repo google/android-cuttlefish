5e50d0973bb072d0d091c3510621fc38ce528165
Treehugger Robot <android-test-infra-autosubmit@system.gserviceaccount.com>
2024-01-26 19:44:51 +0000

    Merge "frontend: Report configured touchpads to frontend" into main

--- cvd/cuttlefish/host/commands/cvd/server_constants.h
+++ cvd/cuttlefish/host/commands/cvd/server_constants.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 The Android Open Source Project
+ * Copyright (C) 2022 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,19 +16,105 @@
 
 #pragma once
 
+#include <atomic>
+#include <map>
+#include <optional>
+#include <shared_mutex>
 #include <string>
+#include <vector>
+
+#include "cvd_server.pb.h"
+
+#include "common/libs/fs/epoll.h"
+#include "common/libs/fs/shared_fd.h"
+#include "common/libs/utils/subprocess.h"
+#include "common/libs/utils/unix_sockets.h"
+#include "host/commands/cvd/command_sequence.h"
+#include "host/commands/cvd/epoll_loop.h"
+#include "host/commands/cvd/instance_lock.h"
+#include "host/commands/cvd/instance_manager.h"
+#include "host/commands/cvd/logger.h"
+// including "server_command/subcmd.h" causes cyclic dependency
+#include "host/commands/cvd/server_command/host_tool_target_manager.h"
+#include "host/commands/cvd/server_command/server_handler.h"
+#include "host/libs/web/build_api.h"
 
 namespace cuttlefish {
-namespace cvd {
 
-// Major version uprevs are backwards incompatible.
-// Minor version uprevs are backwards compatible within major version.
-constexpr int kVersionMajor = 1;
-constexpr int kVersionMinor = 7;
+struct ServerMainParam {
+  SharedFD internal_server_fd;
+  SharedFD carryover_client_fd;
+  std::optional<SharedFD> memory_carryover_fd;
+  std::optional<bool> acloud_translator_optout;
+  std::unique_ptr<ServerLogger> server_logger;
+  /* scoped logger that carries the stderr of the carried-over
+   * client. The client may have called "cvd restart-server."
+   *
+   * The scoped_logger should expire just after AcceptCarryoverClient()
+   */
+  std::unique_ptr<ServerLogger::ScopedLogger> scoped_logger;
+  // If true, the server restarted in the previous server process
+  // This is common for restart-server operations.
+  bool restarted_in_process;
+};
+Result<int> CvdServerMain(ServerMainParam&& fds);
 
-}  // namespace cvd
+class CvdServer {
+  // for server_logger_.
+  // server_logger_ shouldn't be exposed to anything but CvdServerMain()
+  friend Result<int> CvdServerMain(ServerMainParam&& fds);
 
-// Pathname of the abstract cvd_server socket.
-std::string ServerSocketPath();
+ public:
+  CvdServer(BuildApi&, EpollPool&, InstanceLockFileManager&, InstanceManager&,
+            HostToolTargetManager&, ServerLogger&);
+  ~CvdServer();
+
+  Result<void> StartServer(SharedFD server);
+  struct ExecParam {
+    SharedFD new_exe;
+    SharedFD carryover_client_fd;  // the client that called cvd restart-server
+    std::optional<SharedFD>
+        in_memory_data_fd;  // fd to carry over in-memory data
+    bool verbose;
+  };
+  Result<void> Exec(ExecParam&&);
+  Result<void> AcceptCarryoverClient(SharedFD client);
+  void Stop();
+  void Join();
+  Result<void> InstanceDbFromJson(const std::string& json_string);
+
+ private:
+  struct OngoingRequest {
+    CvdServerHandler* handler;
+    std::mutex mutex;
+    std::thread::id thread_id;
+  };
+
+  Result<void> AcceptClient(EpollEvent);
+  Result<void> HandleMessage(EpollEvent);
+  Result<cvd::Response> HandleRequest(RequestWithStdio, SharedFD client);
+  Result<void> BestEffortWakeup();
+
+  SharedFD server_fd_;
+  BuildApi& build_api_;
+  EpollPool& epoll_pool_;
+  InstanceLockFileManager& instance_lockfile_manager_;
+  InstanceManager& instance_manager_;
+  HostToolTargetManager& host_tool_target_manager_;
+  ServerLogger& server_logger_;
+  std::atomic_bool running_ = true;
+
+  std::mutex ongoing_requests_mutex_;
+  std::set<std::shared_ptr<OngoingRequest>> ongoing_requests_;
+  // TODO(schuffelen): Move this thread pool to another class.
+  std::mutex threads_mutex_;
+  std::vector<std::thread> threads_;
+
+  // translator optout
+  std::atomic<bool> optout_;
+};
+
+// Read all contents from the file
+Result<std::string> ReadAllFromMemFd(const SharedFD& mem_fd);
 
 }  // namespace cuttlefish
