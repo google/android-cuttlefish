c9743d7a12f6744606e7d6829332b3b62424a854
Jeongik Cha <jeongik@google.com>
2024-01-30 18:51:53 +0900

    Add tail -f /dev/null to keep it running
    Because daemon is turned on by default, docker instance ends when cvd
    finishes. But we want it to keep running

--- docker/arm-server/run_services.sh
+++ docker/arm-server/run_services.sh
@@ -4,3 +4,6 @@ service cuttlefish-host-resources start
 service cuttlefish-operator start
 service cuttlefish-host_orchestrator start
 /root/cvd_home/bin/cvd start --vhost-user-vsock=true --report_anonymous_usage_stats=y $@
+# To keep it running
+tail -f /dev/null
+
