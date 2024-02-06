95771a5b66175d00ef60a88f4edf6b01dd4e46e1
seungjaeyoo <seungjaeyoo@google.com>
2024-02-06 15:39:29 +0900

    Dockerfile for ARM64 uses debian packages built with local code (#428)
    * Dockerfile for ARM64 uses debian packages built with local code
    
    * Run cvd start in docker/arm-server/run_services.sh when binary exists
    
    * Remove unnecessary dependencies in docker/arm-server/Dockerfile

--- docker/arm-server/run_services.sh
+++ docker/arm-server/run_services.sh
@@ -3,7 +3,12 @@
 service cuttlefish-host-resources start
 service cuttlefish-operator start
 service cuttlefish-host_orchestrator start
-/root/cvd_home/bin/cvd start --vhost-user-vsock=true --report_anonymous_usage_stats=y $@
+
+if [ -f /root/cvd_home/bin/cvd ]; then
+  /root/cvd_home/bin/cvd start \
+    --vhost-user-vsock=true \
+    --report_anonymous_usage_stats=y \
+    $@
+fi
 # To keep it running
 tail -f /dev/null
-
