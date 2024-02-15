321f61e847f52655a8e508957749a1c73dbbec81
A. Cody Schuffelen <schuffelen@google.com>
2024-02-15 13:50:48 -0800

    Extend kokoro test script with placeholder test results

--- .kokoro/public_test.sh
+++ .kokoro/public_test.sh
@@ -4,3 +4,16 @@
 set -e
 
 echo "Cuttlefish debian package build and testing placeholder script for kokoro"
+
+echo "current directory:"
+pwd
+
+echo "contents of github/android-cuttlefish"
+ls github/android-cuttlefish
+
+echo "producing placeholder xUnit test results"
+mkdir -p test_results
+cp github/android-cuttlefish/.kokoro/demo_result.xml test_results/sponge_log.xml
+echo "placeholder log" > test_results/sponge_log.log
+
+echo "done"
