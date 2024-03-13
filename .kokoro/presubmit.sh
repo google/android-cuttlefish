b76cda41c54c5f3622c2c66c903483a5b6e4e4e8
Jorge E. Moreira <jemoreira@google.com>
2024-03-13 10:46:21 -0700

    Move test scripts to the tools directory

--- .kokoro/presubmit.sh
+++ .kokoro/presubmit.sh
@@ -2,7 +2,9 @@
 
 set -e -x
 
-TOOL_DIR="$(realpath "$(dirname "$0")")"
+# realpath .kokoro/..
+REPO_DIR="$(realpath "$(dirname "$0")"/..)"
+TOOL_DIR="${REPO_DIR}/tools/testutils"
 
 # Add test user to the kokoro group so it has access to the source dir
 "${TOOL_DIR}/prepare_host.sh" -u testrunner -g kokoro
