0f65dec42d523eef0a03ce427f5eb6163539fa0a
Wei-chung Hsu <weihsu@google.com>
2024-03-16 20:31:29 +0000

    [skip ci] Merge "Added locking system for ConvertToRawImage by using flock and inotify+select (to wait for lock file)." into main
    GitOrigin-RevId: 2aeeeddcfba9c5609aa44b5a571ac5a556e10496

--- staging/host/libs/image_aggregator/sparse_image_utils.cc
+++ staging/host/libs/image_aggregator/sparse_image_utils.cc
@@ -16,17 +16,45 @@
 
 #include "host/libs/image_aggregator/sparse_image_utils.h"
 
+#include <android-base/file.h>
+#include <android-base/logging.h>
 #include <string.h>
+#include <sys/file.h>
 
 #include <fstream>
 
-#include <android-base/logging.h>
-
+#include "common/libs/fs/shared_fd.h"
+#include "common/libs/fs/shared_select.h"
 #include "common/libs/utils/subprocess.h"
 #include "host/libs/config/cuttlefish_config.h"
 
+
 const char ANDROID_SPARSE_IMAGE_MAGIC[] = "\x3A\xFF\x26\xED";
 namespace cuttlefish {
+
+void ReleaseLock(const SharedFD& fd,
+                 const std::string& tmp_lock_image_path) {
+  auto funlock_result = fd->Flock(LOCK_UN | LOCK_NB);
+  fd->Close();
+  if (!funlock_result.ok()) {
+    LOG(FATAL) << "It failed to unlock file " << tmp_lock_image_path;
+  }
+}
+
+bool AcquireLock(SharedFD& fd, const std::string& tmp_lock_image_path) {
+  fd = SharedFD::Open(tmp_lock_image_path.c_str(),
+                        O_RDWR | O_CREAT, 0666);
+  if (!fd->IsOpen()) {
+    LOG(FATAL) << tmp_lock_image_path << " file open failed";
+    return false;
+  }
+  auto flock_result = fd->Flock(LOCK_EX);
+  if (!flock_result.ok()) {
+    LOG(FATAL) << "flock failed";
+    return false;
+  }
+  return true;
+}
 
 bool IsSparseImage(const std::string& image_path) {
   std::ifstream file(image_path, std::ios::binary);
@@ -41,7 +69,15 @@ bool IsSparseImage(const std::string& image_path) {
 }
 
 bool ConvertToRawImage(const std::string& image_path) {
+  SharedFD fd;
+  std::string tmp_lock_image_path = image_path + ".lock";
+
+  if(AcquireLock(fd, tmp_lock_image_path) == false) {
+    return false;
+  }
+
   if (!IsSparseImage(image_path)) {
+    // Release lock before return
     LOG(DEBUG) << "Skip non-sparse image " << image_path;
     return false;
   }
@@ -55,6 +91,7 @@ bool ConvertToRawImage(const std::string& image_path) {
   // Use simg2img to convert sparse image to raw image.
   int success = simg2img_cmd.Start().Wait();
   if (success != 0) {
+    // Release lock before FATAL and return
     LOG(FATAL) << "Unable to convert Android sparse image " << image_path
                << " to raw image. " << success;
     return false;
@@ -62,6 +99,7 @@ bool ConvertToRawImage(const std::string& image_path) {
 
   // Replace the original sparse image with the raw image.
   if (unlink(image_path.c_str()) != 0) {
+    // Release lock before FATAL and return
     PLOG(FATAL) << "Unable to delete original sparse image";
   }
 
@@ -70,6 +108,8 @@ bool ConvertToRawImage(const std::string& image_path) {
   mv_cmd.AddParameter(tmp_raw_image_path);
   mv_cmd.AddParameter(image_path);
   success = mv_cmd.Start().Wait();
+  // Release lock and leave critical section
+  ReleaseLock(fd, tmp_lock_image_path);
   if (success != 0) {
     LOG(FATAL) << "Unable to rename raw image " << success;
     return false;
