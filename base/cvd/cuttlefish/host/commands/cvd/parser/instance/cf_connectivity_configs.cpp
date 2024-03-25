d0aa8770df1417f76b10701bc8ab62df9af7ba52
sal <sferro@google.com>
2024-03-25 14:45:41 -0400

    Implement configurable multi-vd vsock isolation groups
    This CL allows to configure a 'groups' parameter as part of a new 'connectivity' section, which controls vsock communications isolation. The vhost-device-vsock service documents this feature with examples at: https://github.com/rust-vmm/vhost-device/blob/main/vhost-device-vsock/README.md

---
+++ base/cvd/cuttlefish/host/commands/cvd/parser/instance/cf_connectivity_configs.cpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <fmt/format.h>
+#include <json/json.h>
+
+#include "common/libs/utils/result.h"
+
+namespace cuttlefish {
+
+std::string GenerateGroupFlag(const Json::Value& instances_json) {
+  std::vector<std::string> vsock_groups;
+
+  for (const auto& instance_json : instances_json) {
+    vsock_groups.emplace_back(
+        instance_json["connectivity"]["vsock"]["guest_group"].asString());
+  }
+
+  std::string args = fmt::format("{}", fmt::join(vsock_groups, ","));
+  return "--vsock_guest_group=" + args;
+}
+
+Result<std::vector<std::string>> GenerateConnectivityFlags(
+    const Json::Value& instances) {
+  std::vector<std::string> result;
+  result.emplace_back(GenerateGroupFlag(instances));
+  return result;
+}
+
+}  // namespace cuttlefish
