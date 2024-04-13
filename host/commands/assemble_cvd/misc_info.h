1f4c0594d19602cd802ba2fc8923ae19c11e8aba
Chad Reynolds <chadreynolds@google.com>
2024-04-12 18:41:32 -0700

    Extend misc_info.txt merging logic
    Some of the extra key merges are for the future vbmeta regeneration,
    some are just to better align with build scripts.
    
    Bug: 322887496
    Test: see the bug comment 37 for steps to test this stack of changes
    
    Change-Id: I224d22abbd6d01a19b611d2bce92f6b00ec3edf8

--- host/commands/assemble_cvd/misc_info.h
+++ host/commands/assemble_cvd/misc_info.h
@@ -28,9 +28,13 @@ namespace cuttlefish {
 using MiscInfo = std::map<std::string, std::string>;
 
 Result<MiscInfo> ParseMiscInfo(const std::string& file_contents);
-std::string WriteMiscInfo(const MiscInfo& info);
+Result<void> WriteMiscInfo(const MiscInfo& misc_info,
+                           const std::string& output_path);
 Result<MiscInfo> GetCombinedDynamicPartitions(const MiscInfo& vendor_info,
                                               const MiscInfo& system_info);
-void MergeInKeys(const MiscInfo& source, MiscInfo& target);
+Result<MiscInfo> MergeMiscInfos(
+    const MiscInfo& vendor_info, const MiscInfo& system_info,
+    const MiscInfo& combined_dp_info,
+    const std::vector<std::string>& system_partitions);
 
 } // namespace cuttlefish
