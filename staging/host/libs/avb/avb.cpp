ddc3d67897894572e3c93c9aef2bc9aab8b8483c
Treehugger Robot <android-test-infra-autosubmit@system.gserviceaccount.com>
2024-04-19 05:33:32 +0000

    [skip ci] Merge "Utilize `Avb` everywhere* we call add_hash_footer" into main
    GitOrigin-RevId: c4df492125d6a6fb850417529c5d23130c06fe75

--- staging/host/libs/avb/avb.cpp
+++ staging/host/libs/avb/avb.cpp
@@ -17,45 +17,72 @@
 
 #include "host/libs/avb/avb.h"
 
+#include <memory>
+#include <string>
+
+#include <fruit/fruit.h>
+
+#include "common/libs/utils/result.h"
 #include "common/libs/utils/subprocess.h"
 #include "host/libs/config/cuttlefish_config.h"
 #include "host/libs/config/known_paths.h"
 
 namespace cuttlefish {
+namespace {
+
+constexpr char kAddHashFooter[] = "add_hash_footer";
+constexpr char kDefaultAlgorithm[] = "SHA256_RSA4096";
+
+}  // namespace
+
+Avb::Avb(std::string avbtool_path) : avbtool_path_(std::move(avbtool_path)) {}
 
 Avb::Avb(std::string avbtool_path, std::string algorithm, std::string key)
     : avbtool_path_(std::move(avbtool_path)),
       algorithm_(std::move(algorithm)),
       key_(std::move(key)) {}
 
+Command Avb::GenerateAddHashFooter(const std::string& image_path,
+                                   const std::string& partition_name,
+                                   const off_t partition_size_bytes) const {
+  Command command(avbtool_path_);
+  command.AddParameter(kAddHashFooter);
+  if (!algorithm_.empty()) {
+    command.AddParameter("--algorithm");
+    command.AddParameter(algorithm_);
+  }
+  if (!key_.empty()) {
+    command.AddParameter("--key");
+    command.AddParameter(key_);
+  }
+  command.AddParameter("--image");
+  command.AddParameter(image_path);
+  command.AddParameter("--partition_name");
+  command.AddParameter(partition_name);
+  command.AddParameter("--partition_size");
+  command.AddParameter(partition_size_bytes);
+  return std::move(command);
+}
+
 Result<void> Avb::AddHashFooter(const std::string& image_path,
                                 const std::string& partition_name,
-                                off_t partition_size_bytes) const {
-  int res;
-
-  Command avb_cmd(avbtool_path_);
-  avb_cmd.AddParameter("add_hash_footer");
-  avb_cmd.AddParameter("--image");
-  avb_cmd.AddParameter(image_path);
-  avb_cmd.AddParameter("--partition_size");
-  avb_cmd.AddParameter(partition_size_bytes);
-  avb_cmd.AddParameter("--partition_name");
-  avb_cmd.AddParameter(partition_name);
-  avb_cmd.AddParameter("--key");
-  avb_cmd.AddParameter(key_);
-  avb_cmd.AddParameter("--algorithm");
-  avb_cmd.AddParameter(algorithm_);
-  res = avb_cmd.Start().Wait();
-
-  CF_EXPECT(res == 0, "Unable to run avbtool. Exited with status " << res);
+                                const off_t partition_size_bytes) const {
+  auto command =
+      GenerateAddHashFooter(image_path, partition_name, partition_size_bytes);
+  int exit_code = command.Start().Wait();
+  CF_EXPECTF(exit_code == 0, "Failure running {} {}. Exited with status {}",
+             command.Executable(), kAddHashFooter, exit_code);
   return {};
 }
 
+std::unique_ptr<Avb> GetDefaultAvb() {
+  return std::unique_ptr<Avb>(
+      new Avb(AvbToolBinary(), kDefaultAlgorithm, TestKeyRsa4096()));
+}
+
 fruit::Component<Avb> CuttlefishKeyAvbComponent() {
-  return fruit::createComponent().registerProvider([]() -> Avb* {
-    return new Avb(HostBinaryPath("avbtool"), "SHA256_RSA4096",
-                   TestKeyRsa4096());
-  });
+  return fruit::createComponent().registerProvider(
+      []() -> Avb* { return GetDefaultAvb().release(); });
 }
 
 } // namespace cuttlefish
