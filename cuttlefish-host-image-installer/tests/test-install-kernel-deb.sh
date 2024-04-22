eb9fae705bc41c8afe9d3f3e0457339fb6a28e2d
Wei-chung Hsu <weihsu@google.com>
2024-04-22 16:30:03 -0700

    Added some test scripts for cross-platform testing.

---
+++ cuttlefish-host-image-installer/tests/test-install-kernel-deb.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+apt-get install -y sudo
+apt-get install -y debconf-utils
+echo "tzdata tzdata/Areas select Etc" | debconf-set-selections -v
+echo "tzdata tzdata/Zones/Etc select UTC" | debconf-set-selections -v
+DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
+dpkg-reconfigure --frontend noninteractive tzdata
+apt-get install -y p7zip-full zstd
+apt-get install -y initramfs-tools
+
+
+7z x $KERNEL_PACKAGE
+apt -o Apt::Get::Assume-Yes=true -o APT::Color=0 -o DPkgPM::Progress-Fancy=0 install ./*.deb
+test -e /boot/vmlinuz-*aosp*-linaro*
+test -e /boot/initrd.img-*aosp*-linaro*
+grep "CONFIG_FHANDLE=y" /boot/config-*aosp*-linaro*
+grep 'CONFIG_MODPROBE_PATH="/sbin/modprobe"' /boot/config-*aosp*-linaro*
+echo "Done"
