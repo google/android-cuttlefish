eaa714aca66bae10c2417ea7c1c8670f98af70b7
Jorge E. Moreira <jemoreira@google.com>
2024-05-03 14:35:54 -0700

    Allow running installbazel scrip inside container
    where sudo is not present

--- tools/installbazel/main.sh
+++ tools/installbazel/main.sh
@@ -20,12 +20,12 @@ set -e
 
 function install_bazel() {
   echo "Installing bazel"
-  sudo apt install apt-transport-https curl gnupg -y
+  apt install apt-transport-https curl gnupg -y
   curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg
-  sudo mv bazel-archive-keyring.gpg /usr/share/keyrings
-  echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+  mv bazel-archive-keyring.gpg /usr/share/keyrings
+  echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
   # bazel needs the zip command to gather test outputs but doesn't depend on it
-  sudo apt-get update && sudo apt-get install -y bazel zip unzip
+  apt-get update && apt-get install -y bazel zip unzip
 }
 
 install_bazel
