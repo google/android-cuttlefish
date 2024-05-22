google/android-cuttlefish

commit 136b95e7004ce622789531084de669f4fd3bce29

Re-land: Move unix sockets to /tmp/cf_avd
Bug: 279085447
Merged-In: Ia264affc58ade2a8261977dbaa0c05cd7dc50d8c
Test: launch_cvd
      Check unix sockets are under /tmp/cf_avd/cvd-1/...
      Not ~/cuttlefush_runtime.1/...
Change-Id: Ia264affc58ade2a8261977dbaa0c05cd7dc50d8c

common/libs/utils/files.cpp
@@ -58,6 +58,7 @@
 #include "common/libs/utils/result.h"
 #include "common/libs/utils/scope_guard.h"
 #include "common/libs/utils/subprocess.h"
+#include "common/libs/utils/users.h"
 
 namespace cuttlefish {
 
@@ -93,19 +94,45 @@ bool DirectoryExists(const std::string& path, bool follow_symlinks) {
 }
 
 Result<void> EnsureDirectoryExists(const std::string& directory_path,
-                                   const mode_t mode) {
+                                   const mode_t mode,
+                                   const std::string& group_name) {
   if (DirectoryExists(directory_path)) {
     return {};
   }
   const auto parent_dir = cpp_dirname(directory_path);
   if (parent_dir.size() > 1) {
-    EnsureDirectoryExists(parent_dir);
+    EnsureDirectoryExists(parent_dir, mode, group_name);
   }
   LOG(DEBUG) << "Setting up " << directory_path;
   if (mkdir(directory_path.c_str(), mode) < 0 && errno != EEXIST) {
     return CF_ERRNO("Failed to create directory: \"" << directory_path << "\"");
   }
+
+  if (group_name != "") {
+    ChangeGroup(directory_path, group_name);
+  }
+
   return {};
+}
+
+Result<void> ChangeGroup(const std::string& path,
+                         const std::string& group_name) {
+  auto groupId = GroupIdFromName(group_name);
+
+  if (groupId == -1) {
+    return CF_ERR("Failed to get group id: ") << group_name;
+  }
+
+  if (chown(path.c_str(), -1, groupId) != 0) {
+    return CF_ERRNO("Feailed to set group for path: "
+                    << path << ", " << group_name << ", " << strerror(errno));
+  }
+
+  return {};
+}
+
+bool CanAccess(const std::string& path, const int mode) {
+  return access(path.c_str(), mode) == 0;
 }
 
 bool IsDirectoryEmpty(const std::string& path) {

common/libs/utils/files.h
@@ -31,7 +31,11 @@ Result<std::vector<std::string>> DirectoryContents(const std::string& path);
 bool DirectoryExists(const std::string& path, bool follow_symlinks = true);
 Result<void> EnsureDirectoryExists(const std::string& directory_path,
                                    const mode_t mode = S_IRWXU | S_IRWXG |
-                                                       S_IROTH | S_IXOTH);
+                                                       S_IROTH | S_IXOTH,
+                                   const std::string& group_name = "");
+Result<void> ChangeGroup(const std::string& path,
+                         const std::string& group_name);
+bool CanAccess(const std::string& path, const int mode);
 bool IsDirectoryEmpty(const std::string& path);
 bool RecursivelyRemoveDirectory(const std::string& path);
 bool Copy(const std::string& from, const std::string& to);

common/libs/utils/users.cpp
@@ -37,6 +37,24 @@
 
 namespace cuttlefish {
 namespace {
+std::vector<gid_t> GetSuplementaryGroups() {
+  int num_groups = getgroups(0, nullptr);
+  if (num_groups < 0) {
+    LOG(ERROR) << "Unable to get number of suplementary groups: "
+               << std::strerror(errno);
+    return {};
+  }
+  std::vector<gid_t> groups(num_groups + 1);
+  int retval = getgroups(groups.size(), groups.data());
+  if (retval < 0) {
+    LOG(ERROR) << "Error obtaining list of suplementary groups (list size: "
+               << groups.size() << "): " << std::strerror(errno);
+    return {};
+  }
+  return groups;
+}
+}  // namespace
+
 gid_t GroupIdFromName(const std::string& group_name) {
   struct group grp{};
   struct group* grp_p{};
@@ -63,24 +81,6 @@ gid_t GroupIdFromName(const std::string& group_name) {
     return -1;
   }
 }
-
-std::vector<gid_t> GetSuplementaryGroups() {
-  int num_groups = getgroups(0, nullptr);
-  if (num_groups < 0) {
-    LOG(ERROR) << "Unable to get number of suplementary groups: "
-               << std::strerror(errno);
-    return {};
-  }
-  std::vector<gid_t> groups(num_groups + 1);
-  int retval = getgroups(groups.size(), groups.data());
-  if (retval < 0) {
-    LOG(ERROR) << "Error obtaining list of suplementary groups (list size: "
-               << groups.size() << "): " << std::strerror(errno);
-    return {};
-  }
-  return groups;
-}
-}  // namespace
 
 bool InGroup(const std::string& group) {
   auto gid = GroupIdFromName(group);

common/libs/utils/users.h
@@ -15,12 +15,15 @@
  */
 #pragma once
 
+#include <grp.h>
+
 #include <string>
 
 #include "common/libs/utils/result.h"
 
 namespace cuttlefish {
 
+gid_t GroupIdFromName(const std::string& group_name);
 bool InGroup(const std::string& group);
 
 /**

host/commands/assemble_cvd/assemble_cvd.cc
@@ -153,6 +153,19 @@ Result<void> CreateLegacySymlinks(
     return CF_ERRNO("symlink(\"" << instance.instance_dir() << "\", \""
                                  << legacy_instance_path << "\") failed");
   }
+
+  const auto mac80211_uds_name = "vhost_user_mac80211";
+
+  const auto mac80211_uds_path =
+      instance.PerInstanceInternalUdsPath(mac80211_uds_name);
+  const auto legacy_mac80211_uds_path =
+      instance.PerInstanceInternalPath(mac80211_uds_name);
+
+  if (symlink(mac80211_uds_path.c_str(), legacy_mac80211_uds_path.c_str())) {
+    return CF_ERRNO("symlink(\"" << mac80211_uds_path << "\", \""
+                                 << legacy_mac80211_uds_path << "\") failed");
+  }
+
   return {};
 }
 
@@ -253,9 +266,17 @@ Result<const CuttlefishConfig*> InitFilesystemAndCreateConfig(
                               config.instance_dirs()),
               "Failed to clean prior files");
 
+    auto defaultGroup = "cvdnetwork";
+    const mode_t defaultMode = S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH;
+
     CF_EXPECT(EnsureDirectoryExists(config.root_dir()));
     CF_EXPECT(EnsureDirectoryExists(config.assembly_dir()));
     CF_EXPECT(EnsureDirectoryExists(config.instances_dir()));
+    CF_EXPECT(EnsureDirectoryExists(config.instances_uds_dir(), defaultMode,
+                                    defaultGroup));
+
+    LOG(INFO) << "Path for instance UDS: " << config.instances_uds_dir();
+
     if (log->LinkAtCwd(config.AssemblyPath("assemble_cvd.log"))) {
       LOG(ERROR) << "Unable to persist assemble_cvd log at "
                   << config.AssemblyPath("assemble_cvd.log")
@@ -266,13 +287,19 @@ Result<const CuttlefishConfig*> InitFilesystemAndCreateConfig(
       CF_EXPECT(EnsureDirectoryExists(instance.instance_dir()));
       auto internal_dir = instance.instance_dir() + "/" + kInternalDirName;
       CF_EXPECT(EnsureDirectoryExists(internal_dir));
-      auto grpc_socket_dir = instance.instance_dir() + "/" + kGrpcSocketDirName;
-      CF_EXPECT(EnsureDirectoryExists(grpc_socket_dir));
       auto shared_dir = instance.instance_dir() + "/" + kSharedDirName;
       CF_EXPECT(EnsureDirectoryExists(shared_dir));
       auto recording_dir = instance.instance_dir() + "/recording";
       CF_EXPECT(EnsureDirectoryExists(recording_dir));
       CF_EXPECT(EnsureDirectoryExists(instance.PerInstanceLogPath("")));
+
+      CF_EXPECT(EnsureDirectoryExists(instance.instance_uds_dir(), defaultMode,
+                                      defaultGroup));
+      CF_EXPECT(EnsureDirectoryExists(instance.instance_internal_uds_dir(),
+                                      defaultMode, defaultGroup));
+      CF_EXPECT(EnsureDirectoryExists(instance.PerInstanceGrpcSocketPath(""),
+                                      defaultMode, defaultGroup));
+
       // TODO(schuffelen): Move this code somewhere better
       CF_EXPECT(CreateLegacySymlinks(instance));
     }

host/commands/assemble_cvd/flags.cc
@@ -1393,9 +1393,9 @@ Result<CuttlefishConfig> InitializeCuttlefishConfiguration(
     if (start_wmediumd) {
       // TODO(b/199020470) move this to the directory for shared resources
       auto vhost_user_socket_path =
-          const_instance.PerInstanceInternalPath("vhost_user_mac80211");
+          const_instance.PerInstanceInternalUdsPath("vhost_user_mac80211");
       auto wmediumd_api_socket_path =
-          const_instance.PerInstanceInternalPath("wmediumd_api_server");
+          const_instance.PerInstanceInternalUdsPath("wmediumd_api_server");
 
       tmp_config_obj.set_vhost_user_mac80211_hwsim(vhost_user_socket_path);
       tmp_config_obj.set_wmediumd_api_server_socket(wmediumd_api_socket_path);

host/commands/display/main.cpp
@@ -107,7 +107,7 @@ Result<int> RunCrosvmDisplayCommand(const std::vector<std::string>& args) {
 
   const std::string crosvm_binary_path = instance.crosvm_binary();
   const std::string crosvm_control_path =
-      instance.PerInstanceInternalPath("crosvm_control.sock");
+      instance.PerInstanceInternalUdsPath("crosvm_control.sock");
 
   cuttlefish::Command command(crosvm_binary_path);
   command.AddParameter("gpu");

host/commands/health/health.cpp
@@ -24,7 +24,7 @@
 #include "host/libs/vm_manager/vm_manager.h"
 
 std::string GetControlSocketPath(const cuttlefish::CuttlefishConfig& config) {
-  return config.ForDefaultInstance().PerInstanceInternalPath(
+  return config.ForDefaultInstance().PerInstanceInternalUdsPath(
       "crosvm_control.sock");
 }
 

commit a73b7c3cae3d598c37f7801739d9c0e5bace2d25

Fix a typo in an error message
Fixes: 136b95e7004c ("Re-land: Move unix sockets to /tmp/cf_avd")
Change-Id: I366bb17cafb9d21a2dabe5df0c815cc030ba6fe8
Signed-off-by: Bart Van Assche <bvanassche@google.com>

common/libs/utils/files.cpp
@@ -150,7 +150,7 @@ Result<void> ChangeGroup(const std::string& path,
   }
 
   if (chown(path.c_str(), -1, groupId) != 0) {
-    return CF_ERRNO("Feailed to set group for path: "
+    return CF_ERRNO("Failed to set group for path: "
                     << path << ", " << group_name << ", " << strerror(errno));
   }
 