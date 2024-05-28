c2b08f33587605b7b1e830f76e41241600ecee80
Wei-chung Hsu <weihsu@google.com>
2024-05-28 01:16:04 -0700

    Added update_github_releases.sh script to handle/update latest and stable releases.

---
+++ tools/cuttlefish-host-image-installer/update_github_releases.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+set -e
+
+# should update tags before do anything
+LATEST_VERSION="v"$(python tools/cuttlefish-host-image-installer/version_parser.py base/debian/changelog latest)
+STABLE_VERSION="v"$(python tools/cuttlefish-host-image-installer/version_parser.py base/debian/changelog stable)
+RELEASE_TAG_LIST=$(gh release list --json tagName --jq '.[].tagName')
+stringarray=($RELEASE_TAG_LIST)
+index=0
+stable_index=-1
+for i in "${stringarray[@]}"
+do
+  if [[ "$i" == "stable" ]]
+  then
+    stable_index=$index
+  fi
+  let index=index+1
+done
+
+RELEASE_NAME_LIST=$(gh release list --json name --jq '.[].name')
+stringarray=($RELEASE_NAME_LIST)
+index=0
+for i in "${stringarray[@]}"
+do
+  if [[ "$index" == "$stable_index" ]]
+  then
+    if [[ "$i" != "$STABLE_VERSION" ]]
+    then
+      gh release edit stable --tag "$i"
+      gh release edit "$STABLE_VERSION" --tag stable
+    fi
+  fi
+  let index=index+1
+done
+
+# copy result with version name
+cp u-boot.bin u-boot"_${STABLE_VERSION}".bin
+cp preseed-mini.iso.xz preseed-mini"_${STABLE_VERSION}".iso.xz
+cp cuttlefish_packages.7z cuttlefish_packages"_${STABLE_VERSION}".7z
+cp meta_gigamp_packages.7z meta_gigamp_packages"_${STABLE_VERSION}".7z
+cp aosp_kernel_aosp14-6.1.7z aosp_kernel_aosp14-6.1"_${STABLE_VERSION}".7z
+cp aosp_kernel_aosp15-6.1.7z aosp_kernel_aosp15-6.1"_${STABLE_VERSION}".7z
+cp aosp_kernel_aosp15-6.6.7z aosp_kernel_aosp15-6.6"_${STABLE_VERSION}".7z
+
+# upload assets to both latest and stable versions
+changed_releases=("latest" "stable")
+for version in "${changed_releases[@]}"
+do
+  ASSETS_LIST=$(gh release view "$version" --json assets --jq '.assets.[].name')
+  stringarray=($ASSETS_LIST)
+  for i in "${stringarray[@]}"
+  do
+    gh release delete-asset "$version" "$i" -y
+  done
+
+  # TODO update changelog ad descriptions
+  gh release upload "$version" u-boot"_${STABLE_VERSION}".bin
+  gh release upload "$version" preseed-mini"_${STABLE_VERSION}".iso.xz
+  gh release upload "$version" cuttlefish_packages"_${STABLE_VERSION}".7z
+  gh release upload "$version" meta_gigamp_packages"_${STABLE_VERSION}".7z
+  gh release upload "$version" aosp_kernel_aosp14-6.1"_${STABLE_VERSION}".7z
+  gh release upload "$version" aosp_kernel_aosp15-6.1"_${STABLE_VERSION}".7z
+  gh release upload "$version" aosp_kernel_aosp15-6.6"_${STABLE_VERSION}".7z
+
+  gh release upload "$version" u-boot.bin
+  gh release upload "$version" preseed-mini.iso.xz
+  gh release upload "$version" cuttlefish_packages.7z
+  gh release upload "$version" meta_gigamp_packages.7z
+  gh release upload "$version" aosp_kernel_aosp14-6.1.7z
+  gh release upload "$version" aosp_kernel_aosp15-6.1.7z
+  gh release upload "$version" aosp_kernel_aosp15-6.6.7z
+
+  
+done
+
+
+
+
+
