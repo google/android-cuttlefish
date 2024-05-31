f5df6c2faf0aacc682234e05d883a8bd164797b7
Jooyung Han <jooyung@google.com>
2024-05-31 00:16:18 +0000

    Remove LOCAL_PREFER_VENDOR_APEX
    It's not used any longer.
    
    Bug: 259196894
    Change-Id: Icf639968cc045fb0c7db850aa49904d037a405bd
    Test: presubmit

--- vsoc_arm64_only/slim/aosp_cf.mk
+++ vsoc_arm64_only/slim/aosp_cf.mk
@@ -37,7 +37,6 @@ PRODUCT_PACKAGES += FakeSystemApp
 #
 # All components inherited here go to vendor image
 #
-LOCAL_PREFER_VENDOR_APEX := true
 $(call inherit-product, device/google/cuttlefish/shared/slim/device_vendor.mk)
 
 #
