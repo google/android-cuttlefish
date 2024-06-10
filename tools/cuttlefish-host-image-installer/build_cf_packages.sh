7bed0480e20f5e4d5542be347737ad5b51f9d0d0
Wei-chung Hsu <weihsu@google.com>
2024-06-10 13:37:41 -0700

    Cross-platform compilation can't direct execute buildutils/installbazel.sh.

--- tools/cuttlefish-host-image-installer/build_cf_packages.sh
+++ tools/cuttlefish-host-image-installer/build_cf_packages.sh
@@ -10,6 +10,12 @@ apt-get install -y git ca-certificates less
 apt-get install -y build-essential
 apt-get install -y devscripts equivs fakeroot dpkg-dev
 
+apt install wget
+wget "https://github.com/bazelbuild/bazelisk/releases/download/v1.19.0/bazelisk-linux-arm64"
+mv bazelisk-linux-arm64 /usr/local/bin/bazel
+chmod 0755 /usr/local/bin/bazel
+export PATH=$PATH:/usr/local/bin
+
 mkdir -p /tmp/b1
 
 cd /tmp/b1
@@ -32,10 +38,6 @@ if [ x"${CI_PIPELINE_ID}" != x ]; then
 export CI_PIPELINE_ID="${CI_PIPELINE_ID}"
 EOF
 fi
-
-cat <<EOF >> "${TDIR}"/buildscript_cf_1
-sudo bash ../buildutils/installbazel.sh
-EOF
 
 cat build_cf_packages_native.sh >> "${TDIR}"/buildscript_cf_1
 
