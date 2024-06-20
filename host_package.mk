fe07d9c414ec5b090b730501b8a6293724717431
Herbert Xue <herbertxue@google.com>
2024-06-20 16:34:05 +0800

    Move "hosttar" to soong build
    - Create "hosttar" phony target with tarball path.
    - Dist for goal with tarball path.
    - The riscv64 cuttlefish builds can be run on qemu on an x86_64 or arm64 host.
      Dist both sets of host packages.
    
    Bug: 348120216
    Test: 1. lunch aosp_cf_riscv64_phone-trunk_staging-userdebug
          2. m hosttar dist dist_files
    Change-Id: I3085e9106e9087dd95024e774581732440dd26fe

--- host_package.mk
+++ host_package.mk
@@ -4,24 +4,10 @@ ifeq ($(HOST_CROSS_OS), linux_musl)
 endif
 
 cvd_host_dir_stamps := $(addsuffix .stamp,$(cvd_host_packages))
-cvd_host_tarballs := $(addsuffix .tar.gz,$(cvd_host_packages))
-
-.PHONY: hosttar
-hosttar: $(cvd_host_tarballs)
 
 # Build this by default when a developer types make.
 # Skip the tarballs by default as it is time consuming.
 droidcore: $(cvd_host_dir_stamps)
-
-# Dist
-# Dist the first package as cvd-host_package.tar.gz.  It would be from x86 in case of cf_x86_phone,
-# and from arm64 in case of cf_arm64_phone.
-$(call dist-for-goals, dist_files,$(firstword $(cvd_host_tarballs)))
-ifeq ($(HOST_CROSS_OS)-$(HOST_CROSS_ARCH), linux_musl-arm64)
-  # If the normal package is arm64, also dist an x86_64 package.
-  $(call dist-for-goals, dist_files,$(lastword $(cvd_host_tarballs)):cvd-host_package-x86_64.tar.gz)
-endif
-
 
 cvd_host_dir_stamps :=
 cvd_host_packages :=
