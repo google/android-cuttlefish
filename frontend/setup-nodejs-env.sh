820243050301568ba88e5d279a2581bc468044bb
Aaron Germuth <germuth@google.com>
2024-06-27 10:41:08 -0600

    Add --retry to curl in nodejs installation
    This curl occasionally fails, adding --retry manually has resolved the issue for us
    
    > curl: (92) HTTP/2 stream 1 was not closed cleanly: INTERNAL_ERROR (err 2)
    > node-v16.17.0-linux-x64.tar.xz: FAILED
    > sha256sum: WARNING: 1 computed checksum did NOT match

--- frontend/setup-nodejs-env.sh
+++ frontend/setup-nodejs-env.sh
@@ -26,8 +26,8 @@ install_nodejs() {
   uninstall_nodejs
   mkdir -p $NODE_ROOT
   pushd $NODE_ROOT >/dev/null
-  curl https://nodejs.org/dist/$NODE_VERSION/node-$NODE_VERSION-$NODE_DISTRO.tar.xz -O
-  curl https://nodejs.org/dist/$NODE_VERSION/SHASUMS256.txt -O
+  curl https://nodejs.org/dist/$NODE_VERSION/node-$NODE_VERSION-$NODE_DISTRO.tar.xz -O --retry 3 --retry_delay 3
+  curl https://nodejs.org/dist/$NODE_VERSION/SHASUMS256.txt -O --retry 3 --retry_delay 3
   if ! echo "$NODE_SHA256SUM node-$NODE_VERSION-$NODE_DISTRO.tar.xz" | sha256sum -c ; then
     echo "** ERROR: KEY MISMATCH **"; popd >/dev/null; exit 1;
   fi
