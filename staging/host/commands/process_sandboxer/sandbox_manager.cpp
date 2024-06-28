31990f8bbd185a752f966c28d812fa9ac600bca9
A. Cody Schuffelen <schuffelen@google.com>
2024-06-28 15:02:20 -0700

    [skip ci] Add event handling to SandboxManager
    Use a `poll` loop over `signalfd` and `eventfd` resources. The
    `signalfd` resource watches for signals coming in to the current
    process. A thread per sandbox watches for an exit event, and reports it
    to the `eventfd` resource in the `poll` loop.
    
    Bug: b/318575092
    Change-Id: I9aae36939238c460931c1133fc95e85991483b64
    Test: cvd start --resume=false --enable_host_sandbox=true
    Test: launch_cvd --resume=false --enable_host_sandbox=true
    GitOrigin-RevId: eb5c37d242c1d58b3bb20a602fd540acebc7f80b

--- staging/host/commands/process_sandboxer/sandbox_manager.cpp
+++ staging/host/commands/process_sandboxer/sandbox_manager.cpp
@@ -15,6 +15,10 @@
  */
 #include "host/commands/process_sandboxer/sandbox_manager.h"
 
+#include <poll.h>
+#include <signal.h>
+#include <sys/eventfd.h>
+#include <sys/signalfd.h>
 #include <unistd.h>
 
 #include <memory>
@@ -25,35 +29,101 @@
 #include <absl/status/status.h>
 #include <absl/status/statusor.h>
 #include <absl/strings/str_format.h>
-#include "sandboxed_api/util/path.h"
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-parameter"
+#include <sandboxed_api/sandbox2/executor.h>
+#include <sandboxed_api/sandbox2/policy.h>
+#include <sandboxed_api/sandbox2/sandbox2.h>
+#include <sandboxed_api/util/path.h>
+#pragma clang diagnostic pop
 
 #include "host/commands/process_sandboxer/policies.h"
 
+using absl::ErrnoToStatusCode;
 using absl::OkStatus;
 using absl::Status;
 using absl::StatusCode;
 using absl::StatusOr;
 using sandbox2::Executor;
+using sandbox2::Policy;
 using sandbox2::Sandbox2;
 using sapi::file::CleanPath;
 
 namespace cuttlefish {
 namespace process_sandboxer {
 
+class SandboxManager::ManagedProcess {
+ public:
+  ManagedProcess(int event_fd, std::unique_ptr<Sandbox2> sandbox)
+      : event_fd_(event_fd), sandbox_(std::move(sandbox)) {
+    waiter_thread_ = std::thread([this]() {
+      sandbox_->AwaitResult().IgnoreResult();
+      uint64_t buf = 1;
+      if (write(event_fd_, &buf, sizeof(buf)) < 0) {
+        PLOG(ERROR) << "Failed to write to eventfd";
+      }
+    });
+  }
+  ManagedProcess(ManagedProcess&) = delete;
+  ~ManagedProcess() {
+    sandbox_->Kill();
+    waiter_thread_.join();
+    auto res = sandbox_->AwaitResult().ToStatus();
+    if (!res.ok()) {
+      LOG(ERROR) << "Issue in closing sandbox: '" << res.ToString() << "'";
+    }
+    if (close(event_fd_) < 0) {
+      PLOG(ERROR) << "`close(event_fd_)` failed";
+    }
+  }
+
+  int EventFd() const { return event_fd_; }
+
+ private:
+  int event_fd_;
+  std::thread waiter_thread_;
+  std::unique_ptr<Sandbox2> sandbox_;
+};
+
 StatusOr<std::unique_ptr<SandboxManager>> SandboxManager::Create(
     HostInfo host_info) {
   std::unique_ptr<SandboxManager> manager(new SandboxManager());
   manager->host_info_ = std::move(host_info);
+  manager->runtime_dir_ =
+      absl::StrFormat("/tmp/sandbox_manager.%u.XXXXXX", getpid());
+  if (mkdtemp(manager->runtime_dir_.data()) == nullptr) {
+    return Status(ErrnoToStatusCode(errno), "mkdtemp failed");
+  }
+  VLOG(1) << "Created temporary directory '" << manager->runtime_dir_ << "'";
+
+  sigset_t mask;
+  if (sigfillset(&mask) < 0) {
+    return Status(ErrnoToStatusCode(errno), "sigfillset failed");
+  }
+  // TODO(schuffelen): Explore interaction between catching SIGCHLD and sandbox2
+  if (sigdelset(&mask, SIGCHLD) < 0) {
+    return Status(ErrnoToStatusCode(errno), "sigdelset failed");
+  }
+  if (sigprocmask(SIG_SETMASK, &mask, NULL) < 0) {
+    return Status(ErrnoToStatusCode(errno), "sigprocmask failed");
+  }
+  VLOG(1) << "Blocked signals";
+
+  manager->signal_fd_ = signalfd(-1, &mask, SFD_CLOEXEC | SFD_NONBLOCK);
+  if (manager->signal_fd_ < 0) {
+    return Status(ErrnoToStatusCode(errno), "signalfd failed");
+  }
+  VLOG(1) << "Created signalfd";
   return manager;
 }
 
 SandboxManager::~SandboxManager() {
-  for (const auto& [pid, sandbox] : sandboxes_) {
-    sandbox->Kill();
-    auto res = sandbox->AwaitResult().ToStatus();
-    if (!res.ok()) {
-      LOG(ERROR) << "Issue in closing sandbox: '" << res.ToString() << "'";
-    }
+  VLOG(1) << "Sandbox shutting down";
+  if (!runtime_dir_.empty() && rmdir(runtime_dir_.c_str()) < 0) {
+    LOG(ERROR) << "Failed to remove '" << runtime_dir_ << "'";
+  }
+  if (signal_fd_ >= 0 && close(signal_fd_) < 0) {
+    LOG(ERROR) << "Failed to close signal fd '" << signal_fd_ << "'";
   }
 }
 
@@ -66,6 +136,11 @@ Status SandboxManager::RunProcess(const std::vector<std::string>& argv,
       }
     }
     return Status(StatusCode::kInvalidArgument, "Not enough arguments");
+  }
+
+  int event_fd = eventfd(0, EFD_CLOEXEC);
+  if (event_fd < 0) {
+    return Status(ErrnoToStatusCode(errno), "`eventfd` failed");
   }
 
   if (VLOG_IS_ON(1)) {
@@ -100,26 +175,92 @@ Status SandboxManager::RunProcess(const std::vector<std::string>& argv,
   std::unique_ptr<Sandbox2> sandbox(
       new Sandbox2(std::move(executor), PolicyForExecutable(host_info_, exe)));
   if (!sandbox->RunAsync()) {
+    if (close(event_fd) < 0) {
+      PLOG(ERROR) << "`close(event_fd)` failed";
+    }
     return sandbox->AwaitResult().ToStatus();
   }
 
-  auto pid = sandbox->pid();
-  sandboxes_.emplace(pid, std::move(sandbox));
+  // A pidfd over the sandbox is another option, but there are two problems:
+  //
+  // 1. There's a race between launching the sandbox and opening the pidfd. If
+  // the sandboxed process exits too quickly, the monitor thread in sandbox2
+  // could reap it and another process could reuse the pid before `pidfd_open`
+  // runs. Sandbox2 could produce a pidfd itself using `CLONE_PIDFD`, but it
+  // does not do this at the time of writing.
+  //
+  // 2. The sandbox could outlive its top-level process. It's not clear to me if
+  // sandbox2 allows this in practice, but `AwaitResult` could theoretically
+  // wait on subprocesses of the original sandboxed process as well.
+  //
+  // To deal with these concerns, we use another thread blocked on AwaitResult
+  // that signals the eventfd when sandbox2 says the sandboxed process has
+  // exited.
+
+  sandboxes_.emplace_back(new ManagedProcess(event_fd, std::move(sandbox)));
 
   return OkStatus();
 }
 
-Status SandboxManager::WaitForExit() {
-  std::vector<Status> results;
-  for (const auto& [pid, sandbox] : sandboxes_) {
-    results.emplace_back(sandbox->AwaitResult().ToStatus());
+bool SandboxManager::Running() const { return running_; }
+
+Status SandboxManager::Iterate() {
+  std::vector<pollfd> poll_fds;
+  poll_fds.emplace_back(pollfd{
+      .fd = signal_fd_,
+      .events = POLLIN,
+  });
+  for (const auto& managed : sandboxes_) {
+    poll_fds.emplace_back(pollfd{
+        .fd = managed->EventFd(),
+        .events = POLLIN,
+    });
   }
-  for (const auto& result : results) {
-    if (!result.ok()) {
-      return result;
+
+  if (poll(poll_fds.data(), poll_fds.size(), /* timeout = */ 0) < 0) {
+    return Status(ErrnoToStatusCode(errno), "`poll` failed");
+  }
+
+  for (const auto& poll_fd : poll_fds) {
+    if (poll_fd.revents == 0) {
+      continue;
+    }
+    if (poll_fd.fd == signal_fd_) {
+      auto status = HandleSignal();
+      if (!status.ok()) {
+        return status;
+      }
+    }
+    for (auto it = sandboxes_.begin(); it != sandboxes_.end();) {
+      it = poll_fd.fd == (*it)->EventFd() ? sandboxes_.erase(it) : ++it;
     }
   }
+
   return OkStatus();
+}
+
+Status SandboxManager::HandleSignal() {
+  signalfd_siginfo info;
+  auto read_res = read(signal_fd_, &info, sizeof(info));
+  if (read_res < 0) {
+    return Status(ErrnoToStatusCode(errno), "`read(signal_fd_, ...)` failed");
+  } else if (read_res == 0) {
+    return Status(StatusCode::kInternal, "read(signal_fd_, ...) returned EOF");
+  } else if (read_res != (ssize_t)sizeof(info)) {
+    return Status(StatusCode::kInternal, "read(signal_fd_, ...) gave bad size");
+  }
+  VLOG(1) << "Received signal with signo '" << info.ssi_signo << "'";
+
+  switch (info.ssi_signo) {
+    case SIGHUP:
+    case SIGINT:
+    case SIGTERM:
+      LOG(INFO) << "Received signal '" << info.ssi_signo << "', exiting";
+      running_ = false;
+      return OkStatus();
+    default:
+      return Status(StatusCode::kInternal, "Received unexpected signal");
+  }
 }
 
 }  // namespace process_sandboxer
