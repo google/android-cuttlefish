25ce6b573624efe51bfa881a9e1d3dbd0b47ed42
Treehugger Robot <android-test-infra-autosubmit@system.gserviceaccount.com>
2024-07-22 16:57:18 +0000

    Merge "Add vcpu_config_path option" into main
    GitOrigin-RevId: 3ed8ddf2c699481e204be7924a5e847750ad94db

--- staging/host/libs/vm_manager/crosvm_manager.cpp
+++ staging/host/libs/vm_manager/crosvm_manager.cpp
@@ -544,6 +544,50 @@ Result<std::vector<MonitorCommand>> CrosvmManager::StartCommands(
     crosvm_cmd.Cmd().AddParameter("--mte");
   }
 
+  if (!instance.vcpu_config().empty()) {
+    auto vcpu_config_json = CF_EXPECT(LoadFromFile(instance.vcpu_config()));
+    std::string affinity_arg = "--cpu-affinity=";
+    std::string capacity_arg = "--cpu-capacity=";
+    std::string frequencies_arg = "--cpu-frequencies=";
+
+    for (int i = 0; i < instance.cpus(); i++) {
+      if (i != 0) {
+        capacity_arg += ",";
+        affinity_arg += ":";
+        frequencies_arg += ";";
+      }
+
+      auto cpu_cluster = fmt::format("--cpu-cluster={}", i);
+
+      auto cpu = fmt::format("cpu{}", i);
+      const auto cpu_json =
+          CF_EXPECT(GetValue<Json::Value>(vcpu_config_json, {cpu}));
+
+      const auto affinity =
+          CF_EXPECT(GetValue<std::string>(cpu_json, {"affinity"}));
+      auto affine_arg = fmt::format("{}={}", i, affinity);
+
+      const auto freqs =
+          CF_EXPECT(GetValue<std::string>(cpu_json, {"frequencies"}));
+      auto freq_arg = fmt::format("{}={}", i, freqs);
+
+      const auto capacity =
+          CF_EXPECT(GetValue<std::string>(cpu_json, {"capacity"}));
+      auto cap_arg = fmt::format("{}={}", i, capacity);
+
+      capacity_arg += cap_arg;
+      affinity_arg += affine_arg;
+      frequencies_arg += freq_arg;
+
+      crosvm_cmd.Cmd().AddParameter(cpu_cluster);
+    }
+
+    crosvm_cmd.Cmd().AddParameter(affinity_arg);
+    crosvm_cmd.Cmd().AddParameter(capacity_arg);
+    crosvm_cmd.Cmd().AddParameter(frequencies_arg);
+    crosvm_cmd.Cmd().AddParameter("--virt-cpufreq");
+  }
+
   auto disk_num = instance.virtual_disk_paths().size();
   CF_EXPECT(VmManager::kMaxDisks >= disk_num,
             "Provided too many disks (" << disk_num << "), maximum "
