34ab171f5e423a42d3115f5398a638e6bee5bd8d
Cody Schuffelen <schuffelen@google.com>
2024-08-01 02:56:00 +0000

    [skip ci] Merge "Add a namespace-only sandbox policy for `webRTC`" into main
    GitOrigin-RevId: a20d8d83e86ae67226db2695775a5bd368cfad1b

--- staging/host/commands/process_sandboxer/policies.h
+++ staging/host/commands/process_sandboxer/policies.h
@@ -47,6 +47,7 @@ sandbox2::PolicyBuilder ModemSimulatorPolicy(const HostInfo&);
 sandbox2::PolicyBuilder ProcessRestarterPolicy(const HostInfo&);
 sandbox2::PolicyBuilder RunCvdPolicy(const HostInfo&);
 sandbox2::PolicyBuilder SecureEnvPolicy(const HostInfo&);
+sandbox2::PolicyBuilder WebRtcPolicy(const HostInfo&);
 
 std::unique_ptr<sandbox2::Policy> PolicyForExecutable(
     const HostInfo& host_info, std::string_view server_socket_outside_path,
