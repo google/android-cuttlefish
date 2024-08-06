b499294ac5a3859aa8353655fa3b06766587ff2d
Wei-chung Hsu <weihsu@google.com>
2024-08-06 14:27:03 -0700

    Change stable tag approach to always upload to fixed release name "stable". Don't update release tag name anymore because it will mess up release codebase.

--- tools/cuttlefish-host-image-installer/update_github_releases.sh
+++ tools/cuttlefish-host-image-installer/update_github_releases.sh
@@ -19,43 +19,6 @@ done
 echo "$changes" > changelog
 
 STABLE_VERSION="v$version"
-RELEASE_TAG_LIST=$(gh release list --json tagName --jq '.[].tagName')
-stringarray=($RELEASE_TAG_LIST)
-index=0
-stable_index=-1
-for i in "${stringarray[@]}"
-do
-  if [[ "$i" == "stable" ]]
-  then
-    stable_index=$index
-  fi
-  let index=index+1
-done
-
-echo "step 2: update the stable tag"
-RELEASE_NAME_LIST=$(gh release list --json name --jq '.[].name')
-
-# if this release doesn't exist yet, create it
-stable_exist=$(echo $RELEASE_NAME_LIST|grep "$STABLE_VERSION")
-if [[ "$stable_exist" == "" ]]
-then
-  gh release create $STABLE_VERSION --latest=false --title "$STABLE_VERSION" --notes "New release placeholder"
-fi
-
-stringarray=($RELEASE_NAME_LIST)
-index=0
-for i in "${stringarray[@]}"
-do
-  if [[ "$index" == "$stable_index" ]]
-  then
-    if [[ "$i" != "$STABLE_VERSION" ]]
-    then
-      gh release edit stable --tag "$i"
-      gh release edit "$STABLE_VERSION" --tag stable
-    fi
-  fi
-  let index=index+1
-done
 
 # update changelog and descriptions
 echo "step 3: update changelog and descriptions in release"
