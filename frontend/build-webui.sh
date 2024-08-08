1c4dcfcc50ed372fc6bfaccfd84e98ef4ad30093
Matthew DeVore <matvore@chromium.org>
2024-08-08 14:27:46 -0500

    build-webui.sh: exit with error on failed build

--- frontend/build-webui.sh
+++ frontend/build-webui.sh
@@ -5,4 +5,6 @@ package_version=$(head -1 debian/changelog | sed s/.*\(\\\(.*\\\)\).*/\\1/)
 last_commit=$( (git log -1 || echo dev) | head -1 | sed s/commit\ //)
 echo "export const BUILD_VERSION = \"github-$package_version-$last_commit\";" > src/operator/webui/src/environments/version.ts
 (cd src/operator/webui/ && npm install && ./node_modules/.bin/ng build)
+ok=$?
 uninstall_nodejs
+exit $ok
