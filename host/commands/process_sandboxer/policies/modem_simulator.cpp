baba07f4dcfd1ce3d0038f2980a54ff61f914808
A. Cody Schuffelen <schuffelen@google.com>
2024-08-13 15:54:32 -0700

    Add `modem_simulator` policy rule for `modem_nvram.json`
    This fixes an error in host-sandbox mode:
    ```
    Unable to write to file /usr/local/google/home/schuffelen/cuttlefish/instances/cvd-1/modem_nvram.json
    ```
    
    Bug: b/318601112
    Change-Id: Ic8b403e06671e7fc855d4ca54f6ea0b4abc33152
    Test: launch_cvd --resume=false --enable_host_sandbox=true

--- host/commands/process_sandboxer/policies/modem_simulator.cpp
+++ host/commands/process_sandboxer/policies/modem_simulator.cpp
@@ -24,9 +24,10 @@ namespace cuttlefish::process_sandboxer {
 sandbox2::PolicyBuilder ModemSimulatorPolicy(const HostInfo& host) {
   // TODO: b/318601112 - Add system call policy. This only applies namespaces.
   return BaselinePolicy(host, host.HostToolExe("modem_simulator"))
-      .AddDirectory(host.log_dir, /* is_ro= */ false)
-      .AddFile(host.cuttlefish_config_path)
       .AddDirectory(host.host_artifacts_path + "/etc/modem_simulator")
+      .AddDirectory(host.log_dir, /* is_ro= */ false)
+      .AddDirectory(host.runtime_dir, /* is_ro= */ false)  // modem_nvram.json
+      .AddFile(host.cuttlefish_config_path)
       .DefaultAction(sandbox2::AllowAllSyscalls());
 }
 
