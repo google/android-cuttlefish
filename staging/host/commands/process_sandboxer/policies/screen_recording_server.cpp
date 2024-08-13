6357001ce444b78233b154825cdf9c58f397819f
Cody Schuffelen <schuffelen@google.com>
2024-08-13 22:22:21 +0000

    [skip ci] Merge "Add a namespace-only sandbox policy for screen_recording_server" into main
    GitOrigin-RevId: e2f55017ca019f4236f3d293bce266daa9a49cab

--- staging/host/commands/process_sandboxer/policies/screen_recording_server.cpp
+++ staging/host/commands/process_sandboxer/policies/screen_recording_server.cpp
@@ -17,24 +17,17 @@
 #include "host/commands/process_sandboxer/policies.h"
 
 #include <sandboxed_api/sandbox2/allow_all_syscalls.h>
-#include <sandboxed_api/sandbox2/allow_unrestricted_networking.h>
 #include <sandboxed_api/sandbox2/policybuilder.h>
-#include <sandboxed_api/util/path.h>
 
 namespace cuttlefish::process_sandboxer {
 
-using sapi::file::JoinPath;
-
-sandbox2::PolicyBuilder WebRtcPolicy(const HostInfo& host) {
-  // TODO: b/318609110 - Add system call policy. This only applies namespaces.
-  return BaselinePolicy(host, host.HostToolExe("webRTC"))
+sandbox2::PolicyBuilder ScreenRecordingServerPolicy(const HostInfo& host) {
+  // TODO: b/318607077 - Add system call policy. This only applies namespaces.
+  return BaselinePolicy(host, host.HostToolExe("screen_recording_server"))
+      .AddDirectory(host.instance_uds_dir, /* is_ro= */ false)
       .AddDirectory(host.log_dir, /* is_ro= */ false)
-      .AddDirectory(host.host_artifacts_path + "/usr/share/webrtc/assets")
-      .AddDirectory(JoinPath(host.runtime_dir, "recording"), /* is_ro= */ false)
+      .AddFile("/dev/urandom")  // For gRPC
       .AddFile(host.cuttlefish_config_path)
-      .AddFile("/dev/urandom")
-      .AddFile("/run/cuttlefish/operator")
-      .Allow(sandbox2::UnrestrictedNetworking())
       .DefaultAction(sandbox2::AllowAllSyscalls());
 }
 
