ccfe371e13b454114b7ef57d7fdb0ada9906b151
Cody Schuffelen <schuffelen@google.com>
2024-08-13 18:13:43 +0000

    Merge "Add a namespace-only policy for log_tee" into main
    GitOrigin-RevId: 4300890d80e5a17390f88b91061c92f8489d848e

---
+++ staging/host/commands/process_sandboxer/policies/log_tee.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "host/commands/process_sandboxer/policies.h"
+
+#include <sandboxed_api/sandbox2/allow_all_syscalls.h>
+#include <sandboxed_api/sandbox2/policybuilder.h>
+
+namespace cuttlefish::process_sandboxer {
+
+sandbox2::PolicyBuilder LogTeePolicy(const HostInfo& host) {
+  // TODO: b/318592149 - Add system call policy. This only applies namespaces.
+  return BaselinePolicy(host, host.HostToolExe("log_tee"))
+      .AddDirectory(host.log_dir, /* is_ro= */ false)
+      .AddFile(host.cuttlefish_config_path)
+      .DefaultAction(sandbox2::AllowAllSyscalls());
+}
+
+}  // namespace cuttlefish::process_sandboxer
