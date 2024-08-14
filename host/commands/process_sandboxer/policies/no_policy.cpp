12244fa87b36c4379e20aa7662247b7503505f4a
A. Cody Schuffelen <schuffelen@google.com>
2024-08-14 14:29:54 -0700

    Expand remaining `/bin/bash` uses into direct fork/exec calls in `boot_image_utils.cc`
    `/bin/bash` is not a great process to have in the policy list, as it's
    an interpreter that could theoretically run arbitrary commands based on
    the shell script text file.
    
    Bug: b/359316164
    Change-Id: I476be8f0778cee34aac549b9413379308610b13c
    Test: launch_cvd --resume=false --kernel_path=... --ramdisk_path=...
    Test: launch_cvd --enable_host_sandbox=true --resume=false

--- host/commands/process_sandboxer/policies/no_policy.cpp
+++ host/commands/process_sandboxer/policies/no_policy.cpp
@@ -25,7 +25,6 @@ namespace cuttlefish::process_sandboxer {
 // all.
 std::set<std::string> NoPolicy(const HostInfo& host) {
   return {
-      "/bin/bash",      // TODO: b/359316164
       "/usr/bin/lsof",  // TODO: b/359314623
                         // TODO: b/359309808
       "/usr/lib/cuttlefish-common/bin/capability_query.py",
