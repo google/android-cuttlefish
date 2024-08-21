94f2f14ba016e3652abc6b4c2def6422bd58142b
Treehugger Robot <android-test-infra-autosubmit@system.gserviceaccount.com>
2024-08-21 20:24:44 +0000

    [skip ci] Merge "Add a namespace-only sandbox policy for `casimir_control_server`" into main
    GitOrigin-RevId: ac8c3373ae55081d489e14b4539ed4d7ee73c78d

--- staging/host/commands/process_sandboxer/policies.h
+++ staging/host/commands/process_sandboxer/policies.h
@@ -46,6 +46,7 @@ sandbox2::PolicyBuilder BaselinePolicy(const HostInfo&, std::string_view exe);
 
 sandbox2::PolicyBuilder AdbConnectorPolicy(const HostInfo&);
 sandbox2::PolicyBuilder AssembleCvdPolicy(const HostInfo&);
+sandbox2::PolicyBuilder CasimirControlServerPolicy(const HostInfo&);
 sandbox2::PolicyBuilder EchoServerPolicy(const HostInfo&);
 sandbox2::PolicyBuilder GnssGrpcProxyPolicy(const HostInfo&);
 sandbox2::PolicyBuilder KernelLogMonitorPolicy(const HostInfo&);
