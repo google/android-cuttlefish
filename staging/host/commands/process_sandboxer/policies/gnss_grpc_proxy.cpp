de6cb1d5b46339f95360c0a73e2e48156ce840cf
A. Cody Schuffelen <schuffelen@google.com>
2024-08-26 11:31:01 -0700

    [skip ci] Add system call filters for `gnss_grpc_proxy`
    Also only runs the TCP gRPC endpoint for `gnss_grpc_proxy` outside of
    the host sandbox.
    
    Bug: b/318591948
    Change-Id: I807e552e17ddb2f956ba301b22be6ff1c9342f29
    Test: launch_cvd --resume=false --enable_host_sandbox=true && "update location" in browser UI
    GitOrigin-RevId: 198dc8e51bda7facf64f0af7453621fade4e3605

--- staging/host/commands/process_sandboxer/policies/gnss_grpc_proxy.cpp
+++ staging/host/commands/process_sandboxer/policies/gnss_grpc_proxy.cpp
@@ -16,19 +16,41 @@
 
 #include "host/commands/process_sandboxer/policies.h"
 
-#include <sandboxed_api/sandbox2/allow_all_syscalls.h>
+#include <errno.h>
+#include <sys/syscall.h>
+
 #include <sandboxed_api/sandbox2/policybuilder.h>
+#include <sandboxed_api/sandbox2/util/bpf_helper.h>
 
 namespace cuttlefish::process_sandboxer {
 
 sandbox2::PolicyBuilder GnssGrpcProxyPolicy(const HostInfo& host) {
-  // TODO: b/318591948 - Add system call policy. This only applies namespaces.
   return BaselinePolicy(host, host.HostToolExe("gnss_grpc_proxy"))
       .AddDirectory(host.instance_uds_dir, /* is_ro= */ false)
       .AddDirectory(host.log_dir, /* is_ro= */ false)
       .AddFile("/dev/urandom")  // For gRPC
       .AddFile(host.cuttlefish_config_path)
-      .DefaultAction(sandbox2::AllowAllSyscalls());
+      .AddPolicyOnSyscall(__NR_socket, {ARG_32(0), JEQ32(AF_UNIX, ALLOW)})
+      .AllowSyscall(__NR_socket)
+      .AllowEventFd()
+      .AllowSafeFcntl()
+      .AllowSleep()
+      .AllowSyscall(__NR_bind)
+      .AllowSyscall(__NR_clone)  // multithreading
+      .AllowSyscall(__NR_getpeername)
+      .AllowSyscall(__NR_getsockname)
+      .AllowSyscall(__NR_getsockopt)  // TODO: restrict
+      .AllowSyscall(__NR_listen)
+      .AllowSyscall(__NR_madvise)  // TODO: b/318591948 - restrict or remove
+      .AllowSyscall(__NR_recvmsg)
+      .AllowSyscall(__NR_sched_getparam)
+      .AllowSyscall(__NR_sched_getscheduler)
+      .AllowSyscall(__NR_sched_yield)
+      .AllowSyscall(__NR_shutdown)
+      .AllowSyscall(__NR_sendmsg)
+      .AllowSyscall(__NR_setsockopt)  // TODO: restrict
+      .AllowSyscalls({__NR_accept, __NR_accept4})
+      .AllowTCGETS();
 }
 
 }  // namespace cuttlefish::process_sandboxer
