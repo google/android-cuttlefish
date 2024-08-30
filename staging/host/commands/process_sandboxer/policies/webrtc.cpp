62e1796f3add042d1585bb9941c19e4f0e4044bc
Cody Schuffelen <schuffelen@google.com>
2024-08-30 23:56:58 +0000

    Merge "Add a `seccomp` policy for `webrtc`" into main
    GitOrigin-RevId: 4ee4cd03e193ce2a4f498e8cc5f2f2a2fecc4ceb

--- staging/host/commands/process_sandboxer/policies/webrtc.cpp
+++ staging/host/commands/process_sandboxer/policies/webrtc.cpp
@@ -16,11 +16,17 @@
 
 #include "host/commands/process_sandboxer/policies.h"
 
+#include <netinet/in.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <sys/prctl.h>
+#include <sys/socket.h>
 #include <syscall.h>
 
 #include <sandboxed_api/sandbox2/allow_unrestricted_networking.h>
 #include <sandboxed_api/sandbox2/policybuilder.h>
 #include <sandboxed_api/sandbox2/trace_all_syscalls.h>
+#include <sandboxed_api/sandbox2/util/bpf_helper.h>
 #include <sandboxed_api/util/path.h>
 
 namespace cuttlefish::process_sandboxer {
@@ -37,12 +43,111 @@ sandbox2::PolicyBuilder WebRtcPolicy(const HostInfo& host) {
       .AddFile(host.cuttlefish_config_path)
       .AddFile("/dev/urandom")
       .AddFile("/run/cuttlefish/operator")
+      // Shared memory with crosvm for audio
+      .AddPolicyOnMmap([](bpf_labels& labels) -> std::vector<sock_filter> {
+        return {
+            ARG_32(2),  // prot
+            JNE32(PROT_READ | PROT_WRITE, JUMP(&labels, cf_webrtc_mmap_end)),
+            ARG_32(3),  // flags
+            JEQ32(MAP_SHARED, ALLOW),
+            LABEL(&labels, cf_webrtc_mmap_end),
+        };
+      })
+      .AddPolicyOnSyscall(
+          __NR_getsockopt,
+          [](bpf_labels& labels) -> std::vector<sock_filter> {
+            return {
+                ARG_32(1),  // level
+                JNE32(SOL_SOCKET, JUMP(&labels, cf_webrtc_getsockopt_end)),
+                ARG_32(2),  // optname
+                JEQ32(SO_ERROR, ALLOW),
+                JEQ32(SO_PEERCRED, ALLOW),
+                LABEL(&labels, cf_webrtc_getsockopt_end),
+            };
+          })
+      .AddPolicyOnSyscall(__NR_ioctl, {ARG_32(1), JEQ32(SIOCGSTAMP, ALLOW),
+                                       JEQ32(FIONREAD, ALLOW)})
+      .AddPolicyOnSyscall(__NR_madvise,
+                          {ARG_32(2), JEQ32(MADV_WIPEONFORK, ALLOW),
+                           JEQ32(MADV_DONTNEED, ALLOW),
+                           // TODO: schuffelen - find out what this is
+                           JEQ32(0xffffffff, ALLOW)})
+      .AddPolicyOnSyscall(__NR_prctl,
+                          {ARG_32(0), JEQ32(PR_CAPBSET_READ, ALLOW)})
+      .AddPolicyOnSyscall(
+          __NR_setsockopt,
+          [](bpf_labels& labels) -> std::vector<sock_filter> {
+            return {
+                ARG_32(1),  // level
+                JEQ32(SOL_IP, JUMP(&labels, cf_webrtc_setsockopt_ip)),
+                JEQ32(SOL_IPV6, JUMP(&labels, cf_webrtc_setsockopt_ipv6)),
+                JEQ32(SOL_SOCKET, JUMP(&labels, cf_webrtc_setsockopt_sol)),
+                JNE32(IPPROTO_TCP, JUMP(&labels, cf_webrtc_setsockopt_end)),
+                // IPPROTO_TCP
+                ARG_32(2),  // optname
+                JEQ32(SO_DEBUG, ALLOW),
+                JUMP(&labels, cf_webrtc_setsockopt_end),
+                // SOL_IP
+                LABEL(&labels, cf_webrtc_setsockopt_ip),
+                ARG_32(2),  // optname
+                JEQ32(SO_NO_CHECK, ALLOW),
+                JEQ32(SO_DEBUG, ALLOW),
+                JEQ32(SO_SNDBUF, ALLOW),
+                JEQ32(SO_RCVBUF, ALLOW),
+                JUMP(&labels, cf_webrtc_setsockopt_end),
+                // SOL_IPV6
+                LABEL(&labels, cf_webrtc_setsockopt_ipv6),
+                ARG_32(2),  // optname
+                JEQ32(67 /* sometimes SO_SNDTIMEO? */, ALLOW),
+                JUMP(&labels, cf_webrtc_setsockopt_end),
+                // SOL_SOCKET
+                LABEL(&labels, cf_webrtc_setsockopt_sol),
+                ARG_32(2),  // optname
+                JEQ32(SO_REUSEADDR, ALLOW),
+                JEQ32(SO_SNDBUF, ALLOW),
+                JEQ32(SO_RCVBUF, ALLOW),
+                LABEL(&labels, cf_webrtc_setsockopt_end),
+            };
+          })
+      .AddPolicyOnSyscall(
+          __NR_socket, {ARG_32(0), JEQ32(AF_INET, ALLOW), JEQ32(AF_UNIX, ALLOW),
+                        JEQ32(AF_INET6, ALLOW),
+                        // webrtc/rtc_base/ifaddrs_android.cc
+                        JEQ32(AF_NETLINK, ALLOW), JEQ32(AF_VSOCK, ALLOW)})
       .Allow(sandbox2::UnrestrictedNetworking())
       .AllowEpoll()
       .AllowEpollWait()
+      .AllowEventFd()
+      .AllowGetIDs()
+      .AllowHandleSignals()
+      .AllowPipe()
+      .AllowPrctlSetName()
+      .AllowSafeFcntl()
+      .AllowSelect()
+      .AllowSleep()
+      .AllowSyscall(__NR_accept)
+      .AllowSyscall(__NR_accept4)
+      .AllowSyscall(__NR_bind)
+      .AllowSyscall(__NR_clone)  // Multithreading
+      .AllowSyscall(__NR_connect)
+      .AllowSyscall(__NR_ftruncate)
+      .AllowSyscall(__NR_getsockname)
+      .AllowSyscall(__NR_listen)
+      .AllowSyscall(__NR_memfd_create)
+      .AllowSyscall(__NR_recvfrom)
       .AllowSyscall(__NR_recvmsg)
+      .AllowSyscall(__NR_sched_get_priority_max)
+      .AllowSyscall(__NR_sched_get_priority_min)
+      .AllowSyscall(__NR_sched_getparam)
+      .AllowSyscall(__NR_sched_getscheduler)
+      .AllowSyscall(__NR_sched_setscheduler)
+      .AllowSyscall(__NR_sched_yield)
       .AllowSyscall(__NR_sendmsg)
-      .DefaultAction(sandbox2::TraceAllSyscalls());
+      .AllowSyscall(__NR_sendmmsg)
+      .AllowSyscall(__NR_sendto)
+      .AllowSyscall(__NR_shutdown)
+      .AllowSyscall(__NR_socketpair)
+      .AllowTCGETS();
 }
 
 }  // namespace cuttlefish::process_sandboxer
