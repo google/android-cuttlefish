2b69b23dc4db63117dea524bcff0ffee077d956b
Treehugger Robot <android-test-infra-autosubmit@system.gserviceaccount.com>
2024-08-30 01:42:19 +0000

    Merge "Add a system call policy for `adb_connector`" into main
    GitOrigin-RevId: 0c37a986e140a509539e41bd5078d92a530b2157

--- staging/host/commands/process_sandboxer/policies/adb_connector.cpp
+++ staging/host/commands/process_sandboxer/policies/adb_connector.cpp
@@ -16,19 +16,31 @@
 
 #include "host/commands/process_sandboxer/policies.h"
 
+#include <sys/socket.h>
+#include <syscall.h>
+
 #include <sandboxed_api/sandbox2/allow_unrestricted_networking.h>
 #include <sandboxed_api/sandbox2/policybuilder.h>
 #include <sandboxed_api/sandbox2/trace_all_syscalls.h>
+#include <sandboxed_api/sandbox2/util/bpf_helper.h>
 
 namespace cuttlefish::process_sandboxer {
 
 sandbox2::PolicyBuilder AdbConnectorPolicy(const HostInfo& host) {
-  // TODO: b/358663719 - Add system call policy. This only applies namespaces.
   return BaselinePolicy(host, host.HostToolExe("adb_connector"))
       .AddDirectory(host.log_dir, /* is_ro= */ false)
       .AddFile(host.cuttlefish_config_path)
       .Allow(sandbox2::UnrestrictedNetworking())  // Used to message adb server
-      .DefaultAction(sandbox2::TraceAllSyscalls());
+      .AddPolicyOnSyscall(__NR_socket, {ARG_32(0), JEQ32(AF_INET, ALLOW),
+                                        JEQ32(AF_UNIX, ALLOW)})
+      .AllowSafeFcntl()
+      .AllowSleep()
+      .AllowSyscall(__NR_clone)  // Multithreading
+      .AllowSyscall(__NR_connect)
+      .AllowSyscall(__NR_recvmsg)
+      .AllowSyscall(__NR_sendmsg)
+      .AllowSyscall(__NR_sendto)
+      .AllowTCGETS();
 }
 
 }  // namespace cuttlefish::process_sandboxer
