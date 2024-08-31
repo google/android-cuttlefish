c6e2220e1254318b9f410e0f309d8117721bba9a
Treehugger Robot <android-test-infra-autosubmit@system.gserviceaccount.com>
2024-08-31 02:44:07 +0000

    Merge "Add a `seccomp` system call policy to `echo_server`" into main
    GitOrigin-RevId: 18ee6da3cd4ee085c544984b84e467850fffc5c6

--- staging/host/commands/process_sandboxer/policies/echo_server.cpp
+++ staging/host/commands/process_sandboxer/policies/echo_server.cpp
@@ -16,22 +16,42 @@
 
 #include "host/commands/process_sandboxer/policies.h"
 
+#include <sys/mman.h>
+#include <sys/socket.h>
 #include <syscall.h>
 
 #include <sandboxed_api/sandbox2/policybuilder.h>
 #include <sandboxed_api/sandbox2/trace_all_syscalls.h>
+#include <sandboxed_api/sandbox2/util/bpf_helper.h>
 
 namespace cuttlefish::process_sandboxer {
 
 sandbox2::PolicyBuilder EchoServerPolicy(const HostInfo& host) {
-  // TODO: b/318592223 - Add system call policy. This only applies namespaces.
   return BaselinePolicy(host, host.HostToolExe("echo_server"))
       .AddDirectory(host.instance_uds_dir, /* is_ro= */ false)
       .AddDirectory(host.log_dir, /* is_ro= */ false)
       .AddFile("/dev/urandom")  // For gRPC
       .AddFile(host.cuttlefish_config_path)
+      .AddPolicyOnSyscall(__NR_madvise,
+                          {ARG_32(2), JEQ32(MADV_DONTNEED, ALLOW)})
+      // Unclear where the INET and INET6 sockets come from
+      .AddPolicyOnSyscall(__NR_socket, {ARG_32(0), JEQ32(AF_UNIX, ALLOW),
+                                        JEQ32(AF_INET, ERRNO(EACCES)),
+                                        JEQ32(AF_INET6, ERRNO(EACCES))})
+      .AllowEventFd()
+      .AllowSafeFcntl()
       .AllowSleep()
-      .DefaultAction(sandbox2::TraceAllSyscalls());
+      .AllowSyscall(__NR_accept)
+      .AllowSyscall(__NR_bind)
+      .AllowSyscall(__NR_clone)  // Multithreading
+      .AllowSyscall(__NR_getpeername)
+      .AllowSyscall(__NR_getsockname)
+      .AllowSyscall(__NR_listen)
+      .AllowSyscall(__NR_recvmsg)
+      .AllowSyscall(__NR_sendmsg)
+      .AllowSyscall(__NR_sched_getparam)
+      .AllowSyscall(__NR_sched_getscheduler)
+      .AllowSyscall(__NR_shutdown);
 }
 
 }  // namespace cuttlefish::process_sandboxer
