23562641e40ae2861a840a49231d95577dd196aa
Treehugger Robot <android-test-infra-autosubmit@system.gserviceaccount.com>
2024-08-31 12:36:03 +0000

    Merge "Add a `seccomp` system call filter for `screen_recording_server`" into main
    GitOrigin-RevId: 077f5b4098b4edf605a5c988accf7b1afc5f9aba

--- staging/host/commands/process_sandboxer/policies/screen_recording_server.cpp
+++ staging/host/commands/process_sandboxer/policies/screen_recording_server.cpp
@@ -16,22 +16,58 @@
 
 #include "host/commands/process_sandboxer/policies.h"
 
+#include <errno.h>
+#include <sys/mman.h>
+#include <sys/socket.h>
 #include <syscall.h>
 
 #include <sandboxed_api/sandbox2/policybuilder.h>
 #include <sandboxed_api/sandbox2/trace_all_syscalls.h>
+#include <sandboxed_api/sandbox2/util/bpf_helper.h>
 
 namespace cuttlefish::process_sandboxer {
 
 sandbox2::PolicyBuilder ScreenRecordingServerPolicy(const HostInfo& host) {
-  // TODO: b/318607077 - Add system call policy. This only applies namespaces.
   return BaselinePolicy(host, host.HostToolExe("screen_recording_server"))
       .AddDirectory(host.instance_uds_dir, /* is_ro= */ false)
       .AddDirectory(host.log_dir, /* is_ro= */ false)
       .AddFile("/dev/urandom")  // For gRPC
       .AddFile(host.cuttlefish_config_path)
+      .AddPolicyOnSyscalls(
+          {__NR_getsockopt, __NR_setsockopt},
+          [](bpf_labels& labels) -> std::vector<sock_filter> {
+            return {
+                ARG_32(1),  // level
+                JNE32(SOL_SOCKET,
+                      JUMP(&labels, cf_screen_recording_server_getsockopt_end)),
+                ARG_32(2),  // optname
+                JEQ32(SO_REUSEPORT, ALLOW),
+                LABEL(&labels, cf_screen_recording_server_getsockopt_end),
+            };
+          })
+      .AddPolicyOnSyscall(__NR_madvise,
+                          {ARG_32(2), JEQ32(MADV_DONTNEED, ALLOW)})
+      // Unclear where the INET and INET6 sockets come from
+      .AddPolicyOnSyscall(__NR_socket, {ARG_32(0), JEQ32(AF_UNIX, ALLOW),
+                                        JEQ32(AF_INET, ERRNO(EACCES)),
+                                        JEQ32(AF_INET6, ERRNO(EACCES))})
+      .AllowEventFd()
+      .AllowSafeFcntl()
       .AllowSleep()
-      .DefaultAction(sandbox2::TraceAllSyscalls());
+      .AllowSyscall(__NR_accept)
+      .AllowSyscall(__NR_bind)
+      .AllowSyscall(__NR_clone)  // Multithreading
+      .AllowSyscall(__NR_connect)
+      .AllowSyscall(__NR_getpeername)
+      .AllowSyscall(__NR_getsockname)
+      .AllowSyscall(__NR_listen)
+      .AllowSyscall(__NR_recvmsg)
+      .AllowSyscall(__NR_sched_getparam)
+      .AllowSyscall(__NR_sched_getscheduler)
+      .AllowSyscall(__NR_sched_yield)
+      .AllowSyscall(__NR_sendmsg)
+      .AllowSyscall(__NR_shutdown)
+      .AllowTCGETS();
 }
 
 }  // namespace cuttlefish::process_sandboxer
