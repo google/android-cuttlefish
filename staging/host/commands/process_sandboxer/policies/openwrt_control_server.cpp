9e422d5f3f31904e8d6a9c94e2658facc6ddd3b5
Treehugger Robot <android-test-infra-autosubmit@system.gserviceaccount.com>
2024-09-11 07:52:53 +0000

    Merge "Add a `seccomp` system call policy for `openwrt_control_server`" into main
    GitOrigin-RevId: ec89adbb6d917ec325ad28b044b5498be2e5b775

--- staging/host/commands/process_sandboxer/policies/openwrt_control_server.cpp
+++ staging/host/commands/process_sandboxer/policies/openwrt_control_server.cpp
@@ -16,11 +16,14 @@
 
 #include "host/commands/process_sandboxer/policies.h"
 
+#include <netinet/tcp.h>
+#include <sys/mman.h>
+#include <sys/socket.h>
 #include <syscall.h>
 
 #include <sandboxed_api/sandbox2/allow_unrestricted_networking.h>
 #include <sandboxed_api/sandbox2/policybuilder.h>
-#include <sandboxed_api/sandbox2/trace_all_syscalls.h>
+#include <sandboxed_api/sandbox2/util/bpf_helper.h>
 
 namespace cuttlefish::process_sandboxer {
 
@@ -30,9 +33,53 @@ sandbox2::PolicyBuilder OpenWrtControlServerPolicy(const HostInfo& host) {
       .AddDirectory(host.instance_uds_dir, /* is_ro= */ false)
       .AddDirectory(host.log_dir)
       .AddFile("/dev/urandom")  // For gRPC
-      .AllowSleep()
+      .AddPolicyOnSyscall(__NR_madvise,
+                          {ARG_32(2), JEQ32(MADV_DONTNEED, ALLOW)})
+      .AddPolicyOnSyscall(
+          __NR_socket, {ARG_32(0), JEQ32(AF_UNIX, ALLOW), JEQ32(AF_INET, ALLOW),
+                        JEQ32(AF_INET6, ALLOW)})
+      .AddPolicyOnSyscalls(
+          {__NR_getsockopt, __NR_setsockopt},
+          [](bpf_labels& labels) -> std::vector<sock_filter> {
+            return {
+                ARG_32(1),  // level
+                JEQ32(IPPROTO_TCP,
+                      JUMP(&labels, cf_open_wrt_control_server_sockopt_ip)),
+                JNE32(SOL_SOCKET,
+                      JUMP(&labels, cf_open_wrt_control_server_sockopt_end)),
+                // SOL_SOCKET
+                ARG_32(2),  // optname
+                JEQ32(SO_ERROR, ALLOW),
+                JEQ32(SO_REUSEPORT, ALLOW),
+                JUMP(&labels, cf_open_wrt_control_server_sockopt_end),
+                // IPPROTO_TCP
+                LABEL(&labels, cf_open_wrt_control_server_sockopt_ip),
+                ARG_32(2),  // optname
+                JEQ32(TCP_NODELAY, ALLOW),
+                LABEL(&labels, cf_open_wrt_control_server_sockopt_end),
+            };
+          })
       .Allow(sandbox2::UnrestrictedNetworking())  // HTTP calls to luci
-      .DefaultAction(sandbox2::TraceAllSyscalls());
+      .AllowEventFd()
+      .AllowSafeFcntl()
+      .AllowHandleSignals()
+      .AllowPipe()
+      .AllowSleep()
+      .AllowSyscall(__NR_accept)
+      .AllowSyscall(__NR_bind)
+      .AllowSyscall(__NR_clone)  // Multithreading
+      .AllowSyscall(__NR_connect)
+      .AllowSyscall(__NR_getpeername)
+      .AllowSyscall(__NR_getsockname)
+      .AllowSyscall(__NR_listen)
+      .AllowSyscall(__NR_recvfrom)
+      .AllowSyscall(__NR_recvmsg)
+      .AllowSyscall(__NR_sched_getparam)
+      .AllowSyscall(__NR_sched_getscheduler)
+      .AllowSyscall(__NR_sched_yield)
+      .AllowSyscall(__NR_sendmsg)
+      .AllowSyscall(__NR_sendto)
+      .AllowSyscall(__NR_shutdown);
 }
 
 }  // namespace cuttlefish::process_sandboxer
