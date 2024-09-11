3a9a51e924cfc6ee2488070faa7d0ce55a37b259
Treehugger Robot <android-test-infra-autosubmit@system.gserviceaccount.com>
2024-09-11 01:31:36 +0000

    [skip ci] Merge "Add a `seccomp` system call policy for `assemble_cvd`" into main
    GitOrigin-RevId: cbea94a060f8db376b42950b0095b0ef4afb8508

--- staging/host/commands/process_sandboxer/policies/assemble_cvd.cpp
+++ staging/host/commands/process_sandboxer/policies/assemble_cvd.cpp
@@ -15,10 +15,15 @@
  */
 #include "host/commands/process_sandboxer/policies.h"
 
+#include <sys/mman.h>
+#include <sys/prctl.h>
+#include <sys/syscall.h>
+
 #include <absl/strings/str_cat.h>
 #include <absl/strings/str_replace.h>
 #include <sandboxed_api/sandbox2/policybuilder.h>
 #include <sandboxed_api/sandbox2/trace_all_syscalls.h>
+#include <sandboxed_api/sandbox2/util/bpf_helper.h>
 #include <sandboxed_api/util/path.h>
 
 namespace cuttlefish::process_sandboxer {
@@ -65,10 +70,39 @@ sandbox2::PolicyBuilder AssembleCvdPolicy(const HostInfo& host) {
                           host.instance_uds_dir,
                           {{absl::StrCat("cf_avd_", getuid()), "cf_avd_1000"}}),
                       false)
-      // TODO(schuffelen): Write a system call policy. As written, this only
-      // uses the namespacing features of sandbox2, ignoring the seccomp
-      // features.
-      .DefaultAction(sandbox2::TraceAllSyscalls{});
+      .AddPolicyOnSyscall(__NR_madvise,
+                          {ARG_32(2), JEQ32(MADV_DONTNEED, ALLOW)})
+      .AddPolicyOnSyscall(__NR_prctl,
+                          {ARG_32(0), JEQ32(PR_SET_PDEATHSIG, ALLOW)})
+      /* sandboxer_proxy needs AF_UNIX. `assemble_cvd/network_flags.cpp` calls
+       * `getifaddrs` which won't give any interesting output in the network
+       * namespace anyway. */
+      .AddPolicyOnSyscall(__NR_socket, {ARG_32(0), JEQ32(AF_UNIX, ALLOW),
+                                        JEQ32(AF_INET, ERRNO(EACCES)),
+                                        JEQ32(AF_NETLINK, ERRNO(EACCES))})
+      .AllowDup()
+      .AllowFork()
+      .AllowGetIDs()
+      .AllowLink()
+      .AllowMkdir()
+      .AllowPipe()
+      .AllowReaddir()
+      .AllowRename()
+      .AllowSafeFcntl()
+      .AllowSymlink()
+      .AllowUnlink()
+      .AllowSyscall(__NR_execve)
+      .AllowSyscall(__NR_flock)
+      .AllowSyscall(__NR_ftruncate)
+      .AllowSyscall(__NR_fsync)
+      .AllowSyscall(__NR_umask)
+      .AllowTCGETS()
+      .AllowWait()
+      // For sandboxer_proxy
+      .AllowExit()
+      .AllowSyscall(SYS_connect)
+      .AllowSyscall(SYS_recvmsg)
+      .AllowSyscall(SYS_sendmsg);
 }
 
 }  // namespace cuttlefish::process_sandboxer
