7b1b6c4f8b656dcc8f0b7ce4356358d3e6de769c
A. Cody Schuffelen <schuffelen@google.com>
2024-09-16 16:21:33 -0700

    [skip ci] Put `webrtc_operator` in a sandbox
    Also fixes some constants in the `webrtc` policy which were combining
    constants in a nonsensical way across address families.
    
    Bug: b/359312626
    Change-Id: I26d9f7a6d9d6126574a7fe2922fc68064f0cff17
    Test: process_sandboxer -- $(which cvd_internal_start) --resume=false --report_anonymous_usage_stats=y
    Test: Use web UI
    GitOrigin-RevId: 48b35ae6a9713e987e2caab71e7a9994c181d0cd

--- staging/host/commands/process_sandboxer/policies.cpp
+++ staging/host/commands/process_sandboxer/policies.cpp
@@ -108,6 +108,7 @@ std::unique_ptr<sandbox2::Policy> PolicyForExecutable(
   builders[host.HostToolExe("tcp_connector")] = TcpConnectorPolicy;
   builders[host.HostToolExe("tombstone_receiver")] = TombstoneReceiverPolicy;
   builders[host.HostToolExe("webRTC")] = WebRtcPolicy;
+  builders[host.HostToolExe("webrtc_operator")] = WebRtcOperatorPolicy;
   builders[host.HostToolExe("wmediumd")] = WmediumdPolicy;
   builders[host.HostToolExe("wmediumd_gen_config")] = WmediumdGenConfigPolicy;
 
