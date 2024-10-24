3d93c8502f6c52889da88df137d8079929b50c87
A. Cody Schuffelen <schuffelen@google.com>
2024-10-24 15:50:58 -0700

    Support using credentials from `cvd login` in `cvd fetch`

--- base/cvd/cuttlefish/host/commands/cvd/fetch/fetch_cvd.cc
+++ base/cvd/cuttlefish/host/commands/cvd/fetch/fetch_cvd.cc
@@ -49,6 +49,7 @@
 #include "host/libs/web/http_client/curl_global_init.h"
 #include "host/libs/web/http_client/http_client.h"
 #include "host/libs/web/luci_build_api.h"
+#include "host/libs/directories/xdg.h"
 
 namespace cuttlefish {
 namespace {
@@ -346,6 +347,19 @@ Result<std::unique_ptr<BuildApi>> GetBuildApi(const BuildApiFlags& flags) {
   std::unique_ptr<HttpClient> retrying_http_client =
       HttpClient::ServerErrorRetryClient(*curl, 10,
                                          std::chrono::milliseconds(5000));
+
+  std::vector<std::string> cvd_creds =
+      CF_EXPECT(FindCvdDataFiles("credentials"));
+
+  for (const std::string& creds : cvd_creds) {
+    LOG(ERROR) << creds;
+  }
+
+  std::string oauth_filepath =
+      cvd_creds.empty() ? StringFromEnv("HOME", ".") + "/.acloud_oauth2.dat"
+                        // TODO: schuffelen - test which credential file we want
+                        : cvd_creds[0];
+
   std::unique_ptr<CredentialSource> credential_source =
       CF_EXPECT(GetCredentialSourceFromFlags(
           *retrying_http_client, flags,
