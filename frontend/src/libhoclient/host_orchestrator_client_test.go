google/android-cuttlefish

commit aa520271e19342cdda6abcd6e5e78da7c91635c4

Support user project override
- Add a new flag `--build_api_creds_user_project_id` for user project override
- Pass it by setting `X-Cutf-Host-Orchestrator-BuildAPI-Creds-User-Project-ID`
  header to HO

host_orchestrator_client.go
@@ -33,6 +33,11 @@ type ConnectWebRTCOpts struct {
 	LocalICEConfig *wclient.ICEConfig
 }
 
+type BuildAPICredential struct {
+	AccessToken   string
+	UserProjectID string
+}
+
 // A client to the host orchestrator service running in a remote host.
 type HostOrchestratorService interface {
 	// Lists currently running devices.
@@ -50,15 +55,15 @@ type HostOrchestratorService interface {
 
 	// Create a new device with artifacts from the build server or previously uploaded by the user.
 	// If not empty, the provided credentials will be used to download necessary artifacts from the build api.
-	CreateCVD(req *hoapi.CreateCVDRequest, buildAPICredentials string) (*hoapi.CreateCVDResponse, error)
-	CreateCVDOp(req *hoapi.CreateCVDRequest, buildAPICredentials string) (*hoapi.Operation, error)
+	CreateCVD(req *hoapi.CreateCVDRequest, buildAPICredentials BuildAPICredential) (*hoapi.CreateCVDResponse, error)
+	CreateCVDOp(req *hoapi.CreateCVDRequest, buildAPICredentials BuildAPICredential) (*hoapi.Operation, error)
 
 	// Deletes an existing cvd instance.
 	DeleteCVD(id string) error
 
 	// Calls cvd fetch in the remote host, the downloaded artifacts can be used to create a CVD later.
 	// If not empty, the provided credentials will be used by the host orchestrator to access the build api.
-	FetchArtifacts(req *hoapi.FetchArtifactsRequest, buildAPICredentials string) (*hoapi.FetchArtifactsResponse, error)
+	FetchArtifacts(req *hoapi.FetchArtifactsRequest, buildAPICredentials BuildAPICredential) (*hoapi.FetchArtifactsResponse, error)
 
 	// Creates a webRTC connection to a device running in this host.
 	ConnectWebRTC(device string, observer wclient.Observer, logger io.Writer, opts ConnectWebRTCOpts) (*wclient.Connection, error)
@@ -74,6 +79,7 @@ type HostOrchestratorService interface {
 }
 
 const DefaultHostOrchestratorCredentialsHeader = "X-Cutf-Host-Orchestrator-BuildAPI-Creds"
+const BuildAPICredsUserProjectIDHeader = "X-Cutf-Host-Orchestrator-BuildAPI-Creds-User-Project-ID"
 
 func NewHostOrchestratorService(url string) HostOrchestratorService {
 	return &HostOrchestratorServiceImpl{
@@ -284,12 +290,10 @@ func (c *HostOrchestratorServiceImpl) waitForOperation(name string, res any, ret
 	return c.HTTPHelper.NewPostRequest(path, nil).JSONResDoWithRetries(res, retryOpts)
 }
 
-func (c *HostOrchestratorServiceImpl) FetchArtifacts(req *hoapi.FetchArtifactsRequest, creds string) (*hoapi.FetchArtifactsResponse, error) {
+func (c *HostOrchestratorServiceImpl) FetchArtifacts(req *hoapi.FetchArtifactsRequest, creds BuildAPICredential) (*hoapi.FetchArtifactsResponse, error) {
 	var op hoapi.Operation
 	rb := c.HTTPHelper.NewPostRequest("/artifacts", req)
-	if creds != "" {
-		rb.AddHeader(c.BuildAPICredentialsHeader, creds)
-	}
+	addBuildAPICredsHeaders(rb, creds)
 	if err := rb.JSONResDo(&op); err != nil {
 		return nil, err
 	}
@@ -301,19 +305,17 @@ func (c *HostOrchestratorServiceImpl) FetchArtifacts(req *hoapi.FetchArtifactsRe
 	return res, nil
 }
 
-func (c *HostOrchestratorServiceImpl) CreateCVDOp(req *hoapi.CreateCVDRequest, creds string) (*hoapi.Operation, error) {
+func (c *HostOrchestratorServiceImpl) CreateCVDOp(req *hoapi.CreateCVDRequest, creds BuildAPICredential) (*hoapi.Operation, error) {
 	op := &hoapi.Operation{}
 	rb := c.HTTPHelper.NewPostRequest("/cvds", req)
-	if creds != "" {
-		rb.AddHeader(c.BuildAPICredentialsHeader, creds)
-	}
+	addBuildAPICredsHeaders(rb, creds)
 	if err := rb.JSONResDo(op); err != nil {
 		return nil, err
 	}
 	return op, nil
 }
 
-func (c *HostOrchestratorServiceImpl) CreateCVD(req *hoapi.CreateCVDRequest, creds string) (*hoapi.CreateCVDResponse, error) {
+func (c *HostOrchestratorServiceImpl) CreateCVD(req *hoapi.CreateCVDRequest, creds BuildAPICredential) (*hoapi.CreateCVDResponse, error) {
 	op, err := c.CreateCVDOp(req, creds)
 	if err != nil {
 		return nil, err
@@ -445,4 +447,13 @@ func asWebRTCICEServers(in []opapi.IceServer) []webrtc.ICEServer {
 		})
 	}
 	return out
+}
+
+func addBuildAPICredsHeaders(rb *HTTPRequestBuilder, creds BuildAPICredential) {
+	if creds.AccessToken != "" {
+		rb.AddHeader(DefaultHostOrchestratorCredentialsHeader, creds.AccessToken)
+		if creds.UserProjectID != "" {
+			rb.AddHeader(BuildAPICredsUserProjectIDHeader, creds.UserProjectID)
+		}
+	}
 }

host_orchestrator_client_test.go
@@ -219,7 +219,41 @@ func TestCreateCVD(t *testing.T) {
 	srv := NewHostOrchestratorService(ts.URL)
 	req := &hoapi.CreateCVDRequest{EnvConfig: map[string]interface{}{}}
 
-	res, err := srv.CreateCVD(req, "")
+	res, err := srv.CreateCVD(req, BuildAPICredential{})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	if diff := cmp.Diff(fakeRes, res); diff != "" {
+		t.Fatalf("response mismatch (-want +got):\n%s", diff)
+	}
+}
+
+func TestCreateCVDWithUserProjectOverride(t *testing.T) {
+	fakeRes := &hoapi.CreateCVDResponse{CVDs: []*hoapi.CVD{{Name: "1"}}}
+	token := "foo"
+	projectID := "fake-project"
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch ep := r.Method + " " + r.URL.Path; ep {
+		case "POST /cvds":
+			if r.Header.Get(DefaultHostOrchestratorCredentialsHeader) != token {
+				t.Fatal("unexpected access token: " + r.Header.Get(DefaultHostOrchestratorCredentialsHeader))
+			}
+			if r.Header.Get(BuildAPICredsUserProjectIDHeader) != projectID {
+				t.Fatal("unexpected user project id: " + r.Header.Get(BuildAPICredsUserProjectIDHeader))
+			}
+			writeOK(w, hoapi.Operation{Name: "foo"})
+		case "POST /operations/foo/:wait":
+			writeOK(w, fakeRes)
+		default:
+			t.Fatal("unexpected endpoint: " + ep)
+		}
+	}))
+	defer ts.Close()
+	srv := NewHostOrchestratorService(ts.URL)
+	req := &hoapi.CreateCVDRequest{EnvConfig: map[string]interface{}{}}
+
+	res, err := srv.CreateCVD(req, BuildAPICredential{AccessToken: token, UserProjectID: projectID})
 
 	if err != nil {
 		t.Fatal(err)

commit 8d86bd8e8c7268c0c0434f834c2adca60909ec65

Fix forward use of `X-Cutf-Cloud-Orchestrator-Inject-BuildAPI-Creds`.
- Use of the header got broken in https://github.com/google/android-cuttlefish/commit/aa520271e19342cdda6abcd6e5e78da7c91635c4

frontend/src/libhoclient/BUILD.bazel
@@ -17,7 +17,6 @@ go_library(
         "@com_github_google_android_cuttlefish_frontend_src_liboperator//api/v1:api",
         "@com_github_google_cloud_android_orchestration//pkg/webrtcclient",
         "@com_github_gorilla_websocket//:websocket",
-        "@com_github_hashicorp_go_multierror//:go-multierror",
         "@com_github_pion_webrtc_v3//:webrtc",
     ],
 )
@@ -32,6 +31,5 @@ go_test(
     deps = [
         "@com_github_google_android_cuttlefish_frontend_src_host_orchestrator//api/v1:api",
         "@com_github_google_go_cmp//cmp",
-        "@com_github_hashicorp_go_multierror//:go-multierror",
     ],
 )

frontend/src/libhoclient/host_orchestrator_client.go
@@ -93,8 +93,13 @@ type HostOrchestratorService interface {
 	CreateBugreport(group string, dst io.Writer) error
 }
 
-const DefaultHostOrchestratorCredentialsHeader = "X-Cutf-Host-Orchestrator-BuildAPI-Creds"
-const BuildAPICredsUserProjectIDHeader = "X-Cutf-Host-Orchestrator-BuildAPI-Creds-User-Project-ID"
+const (
+	HTTPHeaderBuildAPICreds              = "X-Cutf-Host-Orchestrator-BuildAPI-Creds"
+	HTTPHeaderBuildAPICredsUserProjectID = "X-Cutf-Host-Orchestrator-BuildAPI-Creds-User-Project-ID"
+	// If used, the Cloud Orchestrator proxy would set/override the "X-Cutf-Host-Orchestrator-BuildAPI-Creds"
+	// http header.
+	HTTPHeaderCOInjectBuildAPICreds = "X-Cutf-Cloud-Orchestrator-Inject-BuildAPI-Creds"
+)
 
 func NewHostOrchestratorService(url string) HostOrchestratorService {
 	return &HostOrchestratorServiceImpl{
@@ -102,14 +107,12 @@ func NewHostOrchestratorService(url string) HostOrchestratorService {
 			Client:       http.DefaultClient,
 			RootEndpoint: url,
 		},
-		BuildAPICredentialsHeader: DefaultHostOrchestratorCredentialsHeader,
 	}
 }
 
 type HostOrchestratorServiceImpl struct {
-	HTTPHelper                HTTPHelper
-	BuildAPICredentialsHeader string
-	ProxyURL                  string
+	HTTPHelper HTTPHelper
+	ProxyURL   string
 }
 
 func (c *HostOrchestratorServiceImpl) getInfraConfig() (*opapi.InfraConfig, error) {
@@ -308,7 +311,7 @@ func (c *HostOrchestratorServiceImpl) waitForOperation(name string, res any, ret
 func (c *HostOrchestratorServiceImpl) FetchArtifacts(req *hoapi.FetchArtifactsRequest, creds BuildAPICredential) (*hoapi.FetchArtifactsResponse, error) {
 	var op hoapi.Operation
 	rb := c.HTTPHelper.NewPostRequest("/artifacts", req)
-	addBuildAPICredsHeaders(rb, creds)
+	handleBuildAPICredsHeaders(rb, creds)
 	if err := rb.JSONResDo(&op); err != nil {
 		return nil, err
 	}
@@ -323,7 +326,7 @@ func (c *HostOrchestratorServiceImpl) FetchArtifacts(req *hoapi.FetchArtifactsRe
 func (c *HostOrchestratorServiceImpl) CreateCVDOp(req *hoapi.CreateCVDRequest, creds BuildAPICredential) (*hoapi.Operation, error) {
 	op := &hoapi.Operation{}
 	rb := c.HTTPHelper.NewPostRequest("/cvds", req)
-	addBuildAPICredsHeaders(rb, creds)
+	handleBuildAPICredsHeaders(rb, creds)
 	if err := rb.JSONResDo(op); err != nil {
 		return nil, err
 	}
@@ -464,11 +467,23 @@ func asWebRTCICEServers(in []opapi.IceServer) []webrtc.ICEServer {
 	return out
 }
 
+func handleBuildAPICredsHeaders(rb *HTTPRequestBuilder, creds BuildAPICredential) {
+	if creds.AccessToken == "inject" {
+		addCOBuildAPICredsInjectHeader(rb)
+	} else {
+		addBuildAPICredsHeaders(rb, creds)
+	}
+}
+
 func addBuildAPICredsHeaders(rb *HTTPRequestBuilder, creds BuildAPICredential) {
 	if creds.AccessToken != "" {
-		rb.AddHeader(DefaultHostOrchestratorCredentialsHeader, creds.AccessToken)
+		rb.AddHeader(HTTPHeaderBuildAPICreds, creds.AccessToken)
 		if creds.UserProjectID != "" {
-			rb.AddHeader(BuildAPICredsUserProjectIDHeader, creds.UserProjectID)
+			rb.AddHeader(HTTPHeaderBuildAPICredsUserProjectID, creds.UserProjectID)
 		}
 	}
+}
+
+func addCOBuildAPICredsInjectHeader(rb *HTTPRequestBuilder) {
+	rb.AddHeader(HTTPHeaderCOInjectBuildAPICreds, "inject" /* avoid empty header value */)
 }

frontend/src/libhoclient/host_orchestrator_client_test.go
@@ -221,11 +221,11 @@ func TestCreateCVDWithUserProjectOverride(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		switch ep := r.Method + " " + r.URL.Path; ep {
 		case "POST /cvds":
-			if r.Header.Get(DefaultHostOrchestratorCredentialsHeader) != token {
-				t.Fatal("unexpected access token: " + r.Header.Get(DefaultHostOrchestratorCredentialsHeader))
+			if r.Header.Get(HTTPHeaderBuildAPICreds) != token {
+				t.Fatal("unexpected access token: " + r.Header.Get(HTTPHeaderBuildAPICreds))
 			}
-			if r.Header.Get(BuildAPICredsUserProjectIDHeader) != projectID {
-				t.Fatal("unexpected user project id: " + r.Header.Get(BuildAPICredsUserProjectIDHeader))
+			if r.Header.Get(HTTPHeaderBuildAPICredsUserProjectID) != projectID {
+				t.Fatal("unexpected user project id: " + r.Header.Get(HTTPHeaderBuildAPICredsUserProjectID))
 			}
 			writeOK(w, hoapi.Operation{Name: "foo"})
 		case "POST /operations/foo/:wait":
@@ -239,6 +239,35 @@ func TestCreateCVDWithUserProjectOverride(t *testing.T) {
 	req := &hoapi.CreateCVDRequest{EnvConfig: map[string]interface{}{}}
 
 	res, err := srv.CreateCVD(req, BuildAPICredential{AccessToken: token, UserProjectID: projectID})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	if diff := cmp.Diff(fakeRes, res); diff != "" {
+		t.Fatalf("response mismatch (-want +got):\n%s", diff)
+	}
+}
+
+func TestCreateCVDWithInjectAccessToken(t *testing.T) {
+	fakeRes := &hoapi.CreateCVDResponse{CVDs: []*hoapi.CVD{{Name: "1"}}}
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch ep := r.Method + " " + r.URL.Path; ep {
+		case "POST /cvds":
+			if len(r.Header.Get(HTTPHeaderCOInjectBuildAPICreds)) == 0 {
+				t.Fatalf("missing %s header", HTTPHeaderCOInjectBuildAPICreds)
+			}
+			writeOK(w, hoapi.Operation{Name: "foo"})
+		case "POST /operations/foo/:wait":
+			writeOK(w, fakeRes)
+		default:
+			t.Fatal("unexpected endpoint: " + ep)
+		}
+	}))
+	defer ts.Close()
+	srv := NewHostOrchestratorService(ts.URL)
+	req := &hoapi.CreateCVDRequest{EnvConfig: map[string]interface{}{}}
+
+	res, err := srv.CreateCVD(req, BuildAPICredential{AccessToken: "inject"})
 
 	if err != nil {
 		t.Fatal(err)