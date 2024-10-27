13e8fb352f8c6c6740f85eb610f45220cc95d1ce
c0rv4x <bmth+50@me.com>
2024-10-27 21:30:35 +0300

    Remove arch being set in buildx

--- docker/image-builder.sh
+++ docker/image-builder.sh
@@ -14,23 +14,12 @@ else
     tag="$1"
 fi
 
-# Use the PLATFORM environment variable, defaulting to the user's architecture if not set
-default_arch="linux/amd64"
-user_arch=$(uname -m)
-
-if [[ "$user_arch" == "aarch64" ]]; then
-    default_arch="linux/arm64"
-fi
-
-platforms="${PLATFORM:-$default_arch}"
-
 # Set up Docker Buildx if not already set up
 docker buildx create --use || true
 
 # Build the Docker image using Buildx for x86_64 and arm64 platforms
 pushd "$android_cuttlefish_root_dir"
 docker buildx build \
-    --platform "$platforms" \
     --force-rm \
     --no-cache \
     -f docker/Dockerfile \
