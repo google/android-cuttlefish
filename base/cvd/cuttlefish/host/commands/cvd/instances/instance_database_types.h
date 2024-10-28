e864bdae6e574a689c3e3e4a65ff2ac99261cc67
Jorge E. Moreira <jemoreira@google.com>
2024-10-28 15:38:06 -0700

    Delete unused types

--- base/cvd/cuttlefish/host/commands/cvd/instances/instance_database_types.h
+++ base/cvd/cuttlefish/host/commands/cvd/instances/instance_database_types.h
@@ -18,40 +18,16 @@
 
 #include <chrono>
 #include <string>
-#include <type_traits>
-#include <unordered_map>
-#include <unordered_set>
 
 #include <json/json.h>
 
 #include "common/libs/utils/result.h"
 
 namespace cuttlefish {
-namespace database_impl {
-
-template <typename ValueType>
-using ToStringTypeReturnType =
-    decltype(void(std::to_string(std::declval<ValueType&>())));
-
-template <typename T, typename = void>
-struct IsToStringOk : std::false_type {};
-
-template <typename T>
-struct IsToStringOk<T, ToStringTypeReturnType<T>> : std::true_type {};
-
-}  // namespace database_impl
-
-template <typename T>
-using Set = std::unordered_set<T>;
-
-template <typename K, typename V>
-using Map = std::unordered_map<K, V>;
 
 using CvdServerClock = std::chrono::system_clock;
 using TimeStamp = std::chrono::time_point<CvdServerClock>;
-using CvdTimeDuration = std::chrono::milliseconds;
 
-std::string SerializeTimePoint(const TimeStamp&);
 Result<TimeStamp> DeserializeTimePoint(const Json::Value& group_json);
 std::string Format(const TimeStamp&);
 
