google/android-cuttlefish

commit e3a4eecd1a6e33104e0ff0d231047febc5cb2232

Replace uses of `ParseInvocation` with new `CommandRequest` members
Bug: b/382745645

base/cvd/cuttlefish/host/commands/cvd/cli/commands/acloud_common.cpp
@@ -16,16 +16,13 @@
 
 #include "host/commands/cvd/cli/commands/acloud_common.h"
 
-#include "host/commands/cvd/cli/utils.h"
-
 namespace cuttlefish {
 
 bool IsSubOperationSupported(const CommandRequest& request) {
-  auto invocation = ParseInvocation(request);
-  if (invocation.arguments.empty()) {
+  if (request.SubcommandArguments().empty()) {
     return false;
   }
-  return invocation.arguments[0] == "create";
+  return request.Subcommand() == "create";
 }
 
 }  // namespace cuttlefish

base/cvd/cuttlefish/host/commands/cvd/acloud/converter.cpp
@@ -145,7 +145,7 @@ namespace acloud_impl {
 
 Result<ConvertedAcloudCreateCommand> ConvertAcloudCreate(
     const CommandRequest& request) {
-  auto arguments = ParseInvocation(request).arguments;
+  std::vector<std::string> arguments = request.SubcommandArguments();
   CF_EXPECT(!arguments.empty());
   CF_EXPECT(arguments[0] == "create");
   arguments.erase(arguments.begin());

base/cvd/cuttlefish/host/commands/cvd/cli/commands/acloud_command.cpp
@@ -58,15 +58,15 @@ class AcloudCommand : public CvdServerHandler {
   ~AcloudCommand() = default;
 
   Result<bool> CanHandle(const CommandRequest& request) const override {
-    auto invocation = ParseInvocation(request);
-    if (invocation.arguments.size() >= 2) {
-      if (invocation.command == "acloud" &&
-          (invocation.arguments[0] == "translator" ||
-           invocation.arguments[0] == "mix-super-image")) {
+    std::vector<std::string> subcmd_args = request.SubcommandArguments();
+    if (subcmd_args.size() >= 2) {
+      if (request.Subcommand() == "acloud" &&
+          (subcmd_args[0] == "translator" ||
+           subcmd_args[0] == "mix-super-image")) {
         return false;
       }
     }
-    return invocation.command == "acloud";
+    return request.Subcommand() == "acloud";
   }
 
   cvd_common::Args CmdList() const override { return {"acloud"}; }
@@ -162,7 +162,7 @@ Result<ConvertedAcloudCreateCommand> AcloudCommand::ValidateLocal(
 }
 
 bool AcloudCommand::ValidateRemoteArgs(const CommandRequest& request) {
-  auto args = ParseInvocation(request).arguments;
+  std::vector<std::string> args = request.SubcommandArguments();
   return acloud_impl::CompileFromAcloudToCvdr(args).ok();
 }
 
@@ -236,7 +236,7 @@ Result<void> AcloudCommand::PrepareForDeleteCommand(
 
 Result<cvd::Response> AcloudCommand::HandleRemote(
     const CommandRequest& request) {
-  auto args = ParseInvocation(request).arguments;
+  std::vector<std::string> args = request.SubcommandArguments();
   args = CF_EXPECT(acloud_impl::CompileFromAcloudToCvdr(args));
   Command cmd = Command("cvdr");
   for (auto a : args) {

base/cvd/cuttlefish/host/commands/cvd/cli/commands/acloud_mixsuperimage.cpp
@@ -158,10 +158,10 @@ class AcloudMixSuperImageCommand : public CvdServerHandler {
   ~AcloudMixSuperImageCommand() = default;
 
   Result<bool> CanHandle(const CommandRequest& request) const override {
-    auto invocation = ParseInvocation(request);
-    if (invocation.arguments.size() >= 2) {
-      if (invocation.command == "acloud" &&
-          invocation.arguments[0] == "mix-super-image") {
+    std::vector<std::string> subcmd_args = request.SubcommandArguments();
+    if (subcmd_args.size() >= 2) {
+      if (request.Subcommand() == "acloud" &&
+          subcmd_args[0] == "mix-super-image") {
         return true;
       }
     }
@@ -179,8 +179,8 @@ class AcloudMixSuperImageCommand : public CvdServerHandler {
 
   Result<cvd::Response> Handle(const CommandRequest& request) override {
     CF_EXPECT(CanHandle(request));
-    auto invocation = ParseInvocation(request);
-    if (invocation.arguments.empty() || invocation.arguments.size() < 2) {
+    std::vector<std::string> subcmd_args = request.SubcommandArguments();
+    if (subcmd_args.empty() || subcmd_args.size() < 2) {
       return CF_ERR("Acloud mix-super-image command not support");
     }
 
@@ -193,7 +193,7 @@ class AcloudMixSuperImageCommand : public CvdServerHandler {
         GflagsCompatFlag("help", help),
         GflagsCompatFlag("super_image", flag_paths),
     };
-    CF_EXPECT(ConsumeFlags(mixsuperimage_flags, invocation.arguments),
+    CF_EXPECT(ConsumeFlags(mixsuperimage_flags, subcmd_args),
               "Failed to process mix-super-image flag.");
     if (help) {
       std::cout << kMixSuperImageHelpMessage;

base/cvd/cuttlefish/host/commands/cvd/cli/commands/acloud_translator.cpp
@@ -43,10 +43,9 @@ class AcloudTranslatorCommand : public CvdServerHandler {
   ~AcloudTranslatorCommand() = default;
 
   Result<bool> CanHandle(const CommandRequest& request) const override {
-    auto invocation = ParseInvocation(request);
-    if (invocation.arguments.size() >= 2) {
-      if (invocation.command == "acloud" &&
-          invocation.arguments[0] == "translator") {
+    std::vector<std::string> subcmd_args = request.SubcommandArguments();
+    if (subcmd_args.size() >= 2) {
+      if (request.Subcommand() == "acloud" && subcmd_args[0] == "translator") {
         return true;
       }
     }
@@ -64,8 +63,8 @@ class AcloudTranslatorCommand : public CvdServerHandler {
 
   Result<cvd::Response> Handle(const CommandRequest& request) override {
     CF_EXPECT(CanHandle(request));
-    auto invocation = ParseInvocation(request);
-    if (invocation.arguments.empty() || invocation.arguments.size() < 2) {
+    std::vector<std::string> subcmd_args = request.SubcommandArguments();
+    if (subcmd_args.empty() || subcmd_args.size() < 2) {
       return CF_ERR("Translator command not support");
     }
 
@@ -81,7 +80,7 @@ class AcloudTranslatorCommand : public CvdServerHandler {
         GflagsCompatFlag("opt-out", flag_optout),
         GflagsCompatFlag("opt-in", flag_optin),
     };
-    CF_EXPECT(ConsumeFlags(translator_flags, invocation.arguments),
+    CF_EXPECT(ConsumeFlags(translator_flags, subcmd_args),
               "Failed to process translator flag.");
     if (help) {
       std::cout << kTranslatorHelpMessage;

base/cvd/cuttlefish/host/commands/cvd/cli/commands/bugreport.cpp
@@ -72,8 +72,7 @@ CvdBugreportCommandHandler::CvdBugreportCommandHandler(
 
 Result<bool> CvdBugreportCommandHandler::CanHandle(
     const CommandRequest& request) const {
-  auto invocation = ParseInvocation(request);
-  return Contains(commands_, invocation.command);
+  return Contains(commands_, request.Subcommand());
 }
 
 Result<cvd::Response> CvdBugreportCommandHandler::Handle(
@@ -83,7 +82,7 @@ Result<cvd::Response> CvdBugreportCommandHandler::Handle(
   cvd::Response response;
   response.mutable_command_response();
 
-  auto [subcmd, cmd_args] = ParseInvocation(request);
+  std::vector<std::string> cmd_args = request.SubcommandArguments();
   cvd_common::Envs env = request.Env();
 
   std::string android_host_out;

base/cvd/cuttlefish/host/commands/cvd/cli/commands/clear.cpp
@@ -32,7 +32,7 @@
 namespace cuttlefish {
 namespace {
 
-constexpr char clearCmd[] = "clear";
+constexpr char kClearCmd[] = "clear";
 constexpr char kSummaryHelpText[] =
     "Clears the instance databaase, stopping any running instances first.";
 
@@ -57,8 +57,7 @@ CvdClearCommandHandler::CvdClearCommandHandler(
 
 Result<bool> CvdClearCommandHandler::CanHandle(
     const CommandRequest& request) const {
-  auto invocation = ParseInvocation(request);
-  return invocation.command == clearCmd;
+  return request.Subcommand() == kClearCmd;
 }
 
 Result<cvd::Response> CvdClearCommandHandler::Handle(
@@ -68,7 +67,7 @@ Result<cvd::Response> CvdClearCommandHandler::Handle(
   cvd::Response response;
   response.mutable_command_response();
 
-  auto [subcmd, cmd_args] = ParseInvocation(request);
+  std::vector<std::string> cmd_args = request.SubcommandArguments();
 
   if (CF_EXPECT(IsHelpSubcmd(cmd_args))) {
     std::cout << kSummaryHelpText << std::endl;
@@ -80,7 +79,7 @@ Result<cvd::Response> CvdClearCommandHandler::Handle(
 }
 
 std::vector<std::string> CvdClearCommandHandler::CmdList() const {
-  return {clearCmd};
+  return {kClearCmd};
 }
 
 Result<std::string> CvdClearCommandHandler::SummaryHelp() const {

base/cvd/cuttlefish/host/commands/cvd/cli/commands/cmd_list.cpp
@@ -22,7 +22,6 @@
 #include <json/value.h>
 
 #include "host/commands/cvd/cli/commands/server_handler.h"
-#include "host/commands/cvd/cli/utils.h"
 #include "host/commands/cvd/cli/types.h"
 
 namespace cuttlefish {
@@ -32,8 +31,7 @@ class CvdCmdlistHandler : public CvdServerHandler {
   CvdCmdlistHandler(CommandSequenceExecutor& executor) : executor_(executor) {}
 
   Result<bool> CanHandle(const CommandRequest& request) const override {
-    auto invocation = ParseInvocation(request);
-    return (invocation.command == "cmd-list");
+    return request.Subcommand() == "cmd-list";
   }
 
   Result<cvd::Response> Handle(const CommandRequest& request) override {

commit 9981723ee6068c6921d25058c94453706aa8c037

Fix `cvd acloud create`
This was broken in e3a4eecd1a6e33104e0ff0d231047febc5cb2232

Test: /usr/bin/bazel run '//cuttlefish/host/commands/cvd' -- acloud create --local-instance --local-image

base/cvd/cuttlefish/host/commands/cvd/cli/commands/acloud_common.cpp
@@ -22,7 +22,7 @@ bool IsSubOperationSupported(const CommandRequest& request) {
   if (request.SubcommandArguments().empty()) {
     return false;
   }
-  return request.Subcommand() == "create";
+  return request.SubcommandArguments()[0] == "create";
 }
 
 }  // namespace cuttlefish