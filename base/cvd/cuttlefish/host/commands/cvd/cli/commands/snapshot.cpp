fab0182524a043eeaad81260ddf80c112975ceca
A. Cody Schuffelen <schuffelen@google.com>
2024-12-11 18:19:14 -0800

    Implement `HandleVoid` in `CvdSnapshotCommandHandler`
    Bug: b/383619080
    Test: /usr/bin/bazel run '//cuttlefish/host/commands/cvd' -- suspend
    Test: /usr/bin/bazel run '//cuttlefish/host/commands/cvd' -- resume

--- base/cvd/cuttlefish/host/commands/cvd/cli/commands/snapshot.cpp
+++ base/cvd/cuttlefish/host/commands/cvd/cli/commands/snapshot.cpp
@@ -67,7 +67,7 @@ class CvdSnapshotCommandHandler : public CvdServerHandler {
   CvdSnapshotCommandHandler(InstanceManager& instance_manager)
       : instance_manager_{instance_manager} {}
 
-  Result<cvd::Response> Handle(const CommandRequest& request) override {
+  Result<void> HandleVoid(const CommandRequest& request) override {
     CF_EXPECT(CanHandle(request));
 
     std::string subcmd = request.Subcommand();
@@ -86,7 +86,8 @@ class CvdSnapshotCommandHandler : public CvdServerHandler {
     siginfo_t infop;
     command.Start().Wait(&infop, WEXITED);
 
-    return ResponseFromSiginfo(infop);
+    CF_EXPECT(CheckProcessExitedNormally(infop));
+    return {};
   }
 
   cvd_common::Args CmdList() const override {
