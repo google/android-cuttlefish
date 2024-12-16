10e7e89534ad7b3c2fee8ed59d396892afef44a8
Wei Li <weiwli@google.com>
2024-12-16 15:32:11 -0800

    Remove the allowlist for Android.mk files in AOSP.
    bootable/recovery/Android.mk has been converted to Android.bp and we don't need the allowlist any more in AOSP. There are still some Android.mk files in internal branches, so keep using PRODUCT_ANDROIDMK_ALLOWLIST_FILE.
    
    Bug: 318428689
    Test: CIs
    Change-Id: Ib1e07096a832274c1b63f9da361c1bc725234bb5

--- vsoc_x86_64/phone/aosp_cf.mk
+++ vsoc_x86_64/phone/aosp_cf.mk
@@ -64,8 +64,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Ignore all Android.mk files
 PRODUCT_IGNORE_ALL_ANDROIDMK := true
-# Allow the following Android.mk files
-PRODUCT_ALLOWED_ANDROIDMK_FILES := bootable/recovery/Android.mk
+# Allow some Android.mk files defined in internal branches
 PRODUCT_ANDROIDMK_ALLOWLIST_FILE := vendor/google/build/androidmk/aosp_cf_allowlist.mk
 
 # Compare target product name directly to avoid this from any product inherits aosp_cf.mk
