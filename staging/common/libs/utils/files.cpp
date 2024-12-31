11ff78246846da400e7a405b493186142e8f00a8
Treehugger Robot <android-test-infra-autosubmit@system.gserviceaccount.com>
2024-12-31 02:22:54 -0800

    [skip ci] Merge changes I96a780dd,Ib9784443,Ia8f6138b,Id1cdced0,Ice1d9983, ... into main
    * changes:
      Check for errors from EnsureDirectoryExists/RenameFile
      sensor_injection: Stop using getService
      ril: Move from volatile to std::atomic
      ril: Suppress warnings from libradiocompat
      VehicleService: Drop unused 'connected' variable
      websocket: Fix initialization order warnings
    
    GitOrigin-RevId: 8adc25482c789b818129c21679cc3289e7b4a9cf

--- staging/common/libs/utils/files.cpp
+++ staging/common/libs/utils/files.cpp
@@ -186,7 +186,7 @@ Result<void> EnsureDirectoryExists(const std::string& directory_path,
   }
   const auto parent_dir = android::base::Dirname(directory_path);
   if (parent_dir.size() > 1) {
-    EnsureDirectoryExists(parent_dir, mode, group_name);
+    CF_EXPECT(EnsureDirectoryExists(parent_dir, mode, group_name));
   }
   LOG(VERBOSE) << "Setting up " << directory_path;
   if (mkdir(directory_path.c_str(), mode) < 0 && errno != EEXIST) {
