c3500f1f0f836bb94a2618bdecd5a04565a2329b
Treehugger Robot <android-test-infra-autosubmit@system.gserviceaccount.com>
2025-01-06 14:25:03 -0800

    [skip ci] Merge "Use `TMPDIR`-derived fields in `process_sandboxer`" into main
    GitOrigin-RevId: e6c28f4b77ae7213fe62bc7729dd50c7a148db98

--- staging/host/commands/process_sandboxer/policies/openwrt_control_server.cpp
+++ staging/host/commands/process_sandboxer/policies/openwrt_control_server.cpp
@@ -32,7 +32,7 @@ namespace cuttlefish::process_sandboxer {
 
 sandbox2::PolicyBuilder OpenWrtControlServerPolicy(const HostInfo& host) {
   return BaselinePolicy(host, host.HostToolExe("openwrt_control_server"))
-      .AddDirectory(host.instance_uds_dir, /* is_ro= */ false)
+      .AddDirectory(host.InstanceUdsDir(), /* is_ro= */ false)
       .AddDirectory(host.log_dir)
       .AddFile("/dev/urandom")  // For gRPC
       .AddPolicyOnSyscall(__NR_madvise,
