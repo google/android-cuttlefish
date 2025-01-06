e3e360af9a58cbacf93277d286dd078c4be9372c
Treehugger Robot <android-test-infra-autosubmit@system.gserviceaccount.com>
2025-01-06 14:25:03 -0800

    [skip ci] Merge "Use `TMPDIR`-derived fields in `process_sandboxer`" into main
    GitOrigin-RevId: e6c28f4b77ae7213fe62bc7729dd50c7a148db98

--- staging/host/commands/process_sandboxer/policies/echo_server.cpp
+++ staging/host/commands/process_sandboxer/policies/echo_server.cpp
@@ -29,7 +29,7 @@ namespace cuttlefish::process_sandboxer {
 
 sandbox2::PolicyBuilder EchoServerPolicy(const HostInfo& host) {
   return BaselinePolicy(host, host.HostToolExe("echo_server"))
-      .AddDirectory(host.instance_uds_dir, /* is_ro= */ false)
+      .AddDirectory(host.InstanceUdsDir(), /* is_ro= */ false)
       .AddDirectory(host.log_dir, /* is_ro= */ false)
       .AddFile("/dev/urandom")  // For gRPC
       .AddFile(host.cuttlefish_config_path)
