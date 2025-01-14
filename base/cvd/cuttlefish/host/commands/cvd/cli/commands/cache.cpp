40fd1fdd41e3f740b492be2d55ea33a1ecd16a57
Chad Reynolds <chadreynolds@google.com>
2025-01-13 18:20:10 -0800

    Factor out the cache logic for reuse
    For the automatic cache management, rather than call `cvd cache`
    directly we will call `RunPrune` and bypass the `CommandHandler`.
    
    Test: cvd cache info
    Test: cvd cache prune
    Test: cvd cache empty

--- base/cvd/cuttlefish/host/commands/cvd/cli/commands/cache.cpp
+++ base/cvd/cuttlefish/host/commands/cvd/cli/commands/cache.cpp
@@ -16,25 +16,21 @@
 
 #include "host/commands/cvd/cli/commands/cache.h"
 
-#include <algorithm>
-#include <chrono>
 #include <cstddef>
 #include <memory>
 #include <string>
 #include <string_view>
 #include <unordered_map>
-#include <utility>
 #include <vector>
 
 #include <android-base/logging.h>
-#include <android-base/strings.h>
 #include <fmt/format.h>
-#include <json/json.h>
 
 #include "common/libs/utils/files.h"
 #include "common/libs/utils/flag_parser.h"
 #include "common/libs/utils/result.h"
 #include "common/libs/utils/tee_logging.h"
+#include "host/commands/cvd/cache/cache.h"
 #include "host/commands/cvd/cli/commands/command_handler.h"
 #include "host/commands/cvd/cli/types.h"
 #include "host/commands/cvd/utils/common.h"
@@ -42,6 +38,7 @@
 namespace cuttlefish {
 
 namespace {
+
 constexpr int kDefaultCacheSizeGB = 25;
 
 constexpr char kSummaryHelpText[] = "Manage the files cached by cvd";
@@ -98,79 +95,6 @@ Result<CacheArguments> ProcessArguments(
   return result;
 }
 
-Result<std::string> RunEmpty(const std::string& cache_directory) {
-  CF_EXPECT(RecursivelyRemoveDirectory(cache_directory));
-  CF_EXPECT(EnsureDirectoryExists(cache_directory));
-  return fmt::format("Cache at \"{}\" has been emptied\n", cache_directory);
-}
-
-Result<std::string> RunInfo(const std::string& cache_directory,
-                            const bool json_formatted) {
-  std::size_t cache_size = CF_EXPECT(GetDiskUsageGigabytes(cache_directory));
-  if (json_formatted) {
-    Json::Value json_output(Json::objectValue);
-    json_output["path"] = cache_directory;
-    json_output["size_in_GB"] = std::to_string(cache_size);
-    return json_output.toStyledString();
-  }
-  return fmt::format("path:{}\nsize in GB:{}\n", cache_directory, cache_size);
-}
-
-Result<std::vector<std::string>> CacheFilesDesc(
-    const std::string& cache_directory) {
-  std::vector<std::string> contents = CF_EXPECTF(
-      DirectoryContentsPaths(cache_directory),
-      "Failure retrieving contents of directory at \"{}\"", cache_directory);
-
-  auto not_self_or_parent_directory = [](std::string_view filepath) {
-    return !android::base::EndsWith(filepath, ".") &&
-           !android::base::EndsWith(filepath, "..");
-  };
-  std::vector<std::string> filtered;
-  std::copy_if(contents.begin(), contents.end(), std::back_inserter(filtered),
-               not_self_or_parent_directory);
-
-  using ModTimePair =
-      std::pair<std::string, std::chrono::system_clock::time_point>;
-  std::vector<ModTimePair> to_sort;
-  for (const std::string& filename : filtered) {
-    to_sort.emplace_back(
-        std::pair(filename, CF_EXPECT(FileModificationTime(filename))));
-  }
-  std::sort(to_sort.begin(), to_sort.end(),
-            [](const ModTimePair& a, const ModTimePair& b) {
-              return a.second > b.second;
-            });
-
-  std::vector<std::string> result;
-  for (const ModTimePair& pair : to_sort) {
-    result.emplace_back(pair.first);
-  }
-  return result;
-}
-
-Result<std::string> RunPrune(const std::string& cache_directory,
-                             const std::size_t allowed_size_GB) {
-  std::size_t cache_size = CF_EXPECT(GetDiskUsageGigabytes(cache_directory));
-  // Descending because elements are removed from the back
-  std::vector<std::string> cache_files =
-      CF_EXPECT(CacheFilesDesc(cache_directory));
-  while (cache_size > allowed_size_GB) {
-    CHECK(!cache_files.empty()) << fmt::format(
-        "Cache size is {} of {}, but there are no more files for pruning.",
-        cache_size, allowed_size_GB);
-
-    std::string next = cache_files.back();
-    cache_files.pop_back();
-    LOG(DEBUG) << fmt::format("Deleting \"{}\" for prune", next);
-    // handles removal of non-directory top-level files as well
-    CF_EXPECT(RecursivelyRemoveDirectory(next));
-    cache_size = CF_EXPECT(GetDiskUsageGigabytes(cache_directory));
-  }
-  return fmt::format("Cache at \"{}\": ~{}GB of {}GB max\n", cache_directory,
-                     cache_size, allowed_size_GB);
-}
-
 class CvdCacheCommandHandler : public CvdCommandHandler {
  public:
   Result<void> Handle(const CommandRequest& request) override;
@@ -182,25 +106,23 @@ class CvdCacheCommandHandler : public CvdCommandHandler {
 
 Result<void> CvdCacheCommandHandler::Handle(const CommandRequest& request) {
   CF_EXPECT(CanHandle(request));
-  auto logger = ScopedTeeLogger(LogToStderr());
 
   CacheArguments arguments =
       CF_EXPECT(ProcessArguments(request.SubcommandArguments()));
   std::string cache_directory = PerUserCacheDir();
-  CF_EXPECT(EnsureDirectoryExists(cache_directory));
   switch (arguments.action) {
     case Action::Empty:
-      std::cout << CF_EXPECTF(RunEmpty(cache_directory),
+      std::cout << CF_EXPECTF(EmptyCache(cache_directory),
                               "Error emptying cache at {}", cache_directory);
       break;
     case Action::Info:
       std::cout << CF_EXPECTF(
-          RunInfo(cache_directory, arguments.json_formatted),
+          GetCacheInfo(cache_directory, arguments.json_formatted),
           "Error retrieving info of cache at {}", cache_directory);
       break;
     case Action::Prune:
       std::cout << CF_EXPECTF(
-          RunPrune(cache_directory, arguments.allowed_size_GB),
+          PruneCache(cache_directory, arguments.allowed_size_GB),
           "Error pruning cache at {} to {}GB", cache_directory,
           arguments.allowed_size_GB);
       break;
