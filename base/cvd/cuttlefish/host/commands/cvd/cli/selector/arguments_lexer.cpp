5e6b05a8fa502e54b77fe476d8ab53df401777ea
A. Cody Schuffelen <schuffelen@google.com>
2025-01-14 15:50:08 -0800

    Remove `ArgumentsLexer` support for boolean flags.
    There are no boolean flags defined.
    
    Bug: b/389804379
    Test: /usr/bin/bazel run :cvd -- --instance_name=1 status

--- base/cvd/cuttlefish/host/commands/cvd/cli/selector/arguments_lexer.cpp
+++ base/cvd/cuttlefish/host/commands/cvd/cli/selector/arguments_lexer.cpp
@@ -29,11 +29,6 @@ namespace cuttlefish {
 namespace selector {
 namespace {
 
-template <typename... Sets>
-bool Included(const std::string& item, Sets&&... containers) {
-  return ((Contains(std::forward<Sets>(containers), item)) || ... || false);
-}
-
 class ArgumentsLexer {
   friend class ArgumentsLexerBuilder;
 
@@ -53,23 +48,9 @@ class ArgumentsLexer {
      *  {"-group_name", "--group_name"}
      */
     std::unordered_set<std::string> value_patterns;
-    /* boolean flags
-     * e.g. --daemon, --nodaemon
-     *
-     * With the given example, this set shall be:
-     *  {"-daemon", "--daemon"}
-     */
-    std::unordered_set<std::string> bool_patterns;
-    // e.g. {"-nodaemon", "--nodaemon"}
-    std::unordered_set<std::string> bool_no_patterns;
   };
   ArgumentsLexer(FlagPatterns&& flag_patterns);
 
-  // preprocess boolean flags:
-  //  e.g. --help=yes --> --help
-  //       --help=faLSe --> --nohelp
-  Result<std::vector<std::string>> Preprocess(
-      const std::vector<std::string>& args) const;
   Result<ArgToken> Process(const std::string& token) const;
 
   struct FlagValuePair {
@@ -84,13 +65,11 @@ class ArgumentsLexer {
   bool Registered(const std::string& flag_string) const {
     return Registered(flag_string, flag_patterns_);
   }
-  std::unordered_set<std::string> valid_bool_values_in_lower_cases_;
   FlagPatterns flag_patterns_;
 };
 
 // input to the lexer factory function
 struct LexerFlagsSpecification {
-  std::unordered_set<std::string> known_boolean_flags;
   std::unordered_set<std::string> known_value_flags;
 };
 
@@ -121,9 +100,6 @@ class ArgumentsLexerBuilder {
  *
  * Say, the two sets are BaseSet and NoPrependedSet.
  *
- * Given a boolean flag --foo, these will happen:
- *   BaseSet = BaseSet U {"--foo", "-foo"}
- *   NoPrependedSet = NoPrependedSet U  {"--nofoo", "-nofoo"}
  * Given a non boolean flag --bar, these will happen:
  *   BaseSet = BaseSet U {"--bar", "-bar"}
  *
@@ -143,27 +119,12 @@ ArgumentsLexerBuilder::GenerateFlagPatterns(
     flag_patterns.value_patterns.insert(one_dash);
     flag_patterns.value_patterns.insert(two_dashes);
   }
-  for (const auto& bool_flag : known_flags.known_boolean_flags) {
-    const auto one_dash = "-" + bool_flag;
-    const auto two_dashes = "--" + bool_flag;
-    const auto one_dash_with_no = "-no" + bool_flag;
-    const auto two_dashes_with_no = "--no" + bool_flag;
-    CF_EXPECT(!ArgumentsLexer::Registered(one_dash, flag_patterns));
-    CF_EXPECT(!ArgumentsLexer::Registered(two_dashes, flag_patterns));
-    CF_EXPECT(!ArgumentsLexer::Registered(one_dash_with_no, flag_patterns));
-    CF_EXPECT(!ArgumentsLexer::Registered(two_dashes_with_no, flag_patterns));
-    flag_patterns.bool_patterns.insert(one_dash);
-    flag_patterns.bool_patterns.insert(two_dashes);
-    flag_patterns.bool_no_patterns.insert(one_dash_with_no);
-    flag_patterns.bool_no_patterns.insert(two_dashes_with_no);
-  }
   return flag_patterns;
 }
 
 Result<std::unique_ptr<ArgumentsLexer>> ArgumentsLexerBuilder::Build() {
   // Change together: ParseCommonSelectorArguments in selector_common_parser.cpp
   LexerFlagsSpecification known_flags{
-      .known_boolean_flags = {},
       .known_value_flags = {SelectorFlags::kGroupName,
                             SelectorFlags::kInstanceName,
                             SelectorFlags::kVerbosity},
@@ -176,15 +137,11 @@ Result<std::unique_ptr<ArgumentsLexer>> ArgumentsLexerBuilder::Build() {
 }
 
 ArgumentsLexer::ArgumentsLexer(FlagPatterns&& flag_patterns)
-    : flag_patterns_{std::move(flag_patterns)} {
-  valid_bool_values_in_lower_cases_ =
-      std::unordered_set<std::string>{"true", "false", "yes", "no", "y", "n"};
-}
+    : flag_patterns_{std::move(flag_patterns)} {}
 
 bool ArgumentsLexer::Registered(const std::string& flag_string,
                                 const FlagPatterns& flag_patterns) {
-  return Included(flag_string, flag_patterns.value_patterns,
-                  flag_patterns.bool_patterns, flag_patterns.bool_no_patterns);
+  return Contains(flag_patterns.value_patterns, flag_string);
 }
 
 Result<ArgToken> ArgumentsLexer::Process(const std::string& token) const {
@@ -212,34 +169,17 @@ Result<ArgToken> ArgumentsLexer::Process(const std::string& token) const {
   if (Contains(flag_patterns_.value_patterns, token)) {
     return ArgToken{ArgType::kKnownValueFlag, token};
   }
-  if (Contains(flag_patterns_.bool_patterns, token)) {
-    return ArgToken{ArgType::kKnownBoolFlag, token};
-  }
-  if (Contains(flag_patterns_.bool_no_patterns, token)) {
-    return ArgToken{ArgType::kKnownBoolNoFlag, token};
-  }
   return ArgToken{ArgType::kUnknownFlag, token};
 }
 
 Result<std::vector<ArgToken>> ArgumentsLexer::Tokenize(
     const std::vector<std::string>& args) const {
   std::vector<ArgToken> tokenized;
-  auto intersection =
-      Intersection(flag_patterns_.value_patterns, flag_patterns_.bool_patterns);
-  CF_EXPECT(intersection.empty());
-  auto preprocessed_args = CF_EXPECT(Preprocess(args));
-  for (const auto& arg : preprocessed_args) {
+  for (const auto& arg : args) {
     auto arg_token = CF_EXPECT(Process(arg));
     tokenized.emplace_back(arg_token);
   }
   return tokenized;
-}
-
-static std::string ToLower(const std::string& src) {
-  std::string lower_cased_value;
-  lower_cased_value.resize(src.size());
-  std::transform(src.begin(), src.end(), lower_cased_value.begin(), ::tolower);
-  return lower_cased_value;
 }
 
 Result<ArgumentsLexer::FlagValuePair> ArgumentsLexer::Separate(
@@ -249,48 +189,6 @@ Result<ArgumentsLexer::FlagValuePair> ArgumentsLexer::Separate(
   auto first_token = equal_included_string.substr(0, equal_sign_pos);
   auto second_token = equal_included_string.substr(equal_sign_pos + 1);
   return FlagValuePair{.flag_string = first_token, .value = second_token};
-}
-
-Result<std::vector<std::string>> ArgumentsLexer::Preprocess(
-    const std::vector<std::string>& args) const {
-  std::vector<std::string> new_args;
-  std::regex pattern("[\\-][\\-]?[^\\-]+.*=.*");
-  for (const auto& arg : args) {
-    if (!std::regex_match(arg, pattern)) {
-      new_args.emplace_back(arg);
-      continue;
-    }
-    // needs to split based on the first '='
-    // --something=another_thing or
-    //  -something=another_thing
-    const auto [flag_string, value] = CF_EXPECT(Separate(arg));
-
-    if (Contains(flag_patterns_.bool_patterns, flag_string)) {
-      const auto low_cased_value = ToLower(value);
-      CF_EXPECT(Contains(valid_bool_values_in_lower_cases_, low_cased_value),
-                "The value for the boolean flag " << flag_string << ", "
-                                                  << value << " is not valid");
-      if (low_cased_value == "true" || low_cased_value == "yes") {
-        new_args.emplace_back(flag_string);
-        continue;
-      }
-      auto base_pos = flag_string.find_first_not_of('-');
-      auto base = flag_string.substr(base_pos);
-      new_args.emplace_back("--no" + base);
-      continue;
-    }
-
-    if (Contains(flag_patterns_.bool_no_patterns, flag_string)) {
-      CF_EXPECT(android::base::StartsWith(flag_string, "-no") ||
-                android::base::StartsWith(flag_string, "--no"));
-      // if --nohelp=XYZ, the "=XYZ" is ignored.
-      new_args.emplace_back(flag_string);
-      continue;
-    }
-
-    new_args.emplace_back(arg);
-  }
-  return new_args;
 }
 
 Result<std::vector<ArgToken>> TokenizeArguments(
