beb9d672f18eeac5dc0f61738167dd22797ac730
A. Cody Schuffelen <schuffelen@google.com>
2025-01-13 17:09:24 -0800

    Remove unused variant of `ArgumentsSeparator::Parse`
    Bug: b/389804379
    Test: /usr/bin/bazel run :cvd -- --instance_name=1 status

--- base/cvd/cuttlefish/host/commands/cvd/cli/selector/arguments_separator.cpp
+++ base/cvd/cuttlefish/host/commands/cvd/cli/selector/arguments_separator.cpp
@@ -43,15 +43,6 @@ Result<std::unique_ptr<ArgumentsSeparator>> ArgumentsSeparator::Parse(
   return std::move(arg_separator);
 }
 
-Result<std::unique_ptr<ArgumentsSeparator>> ArgumentsSeparator::Parse(
-    const FlagsRegistration& flag_registration, const std::string& input_args,
-    const std::string& delim) {
-  std::vector<std::string> input_args_vec =
-      android::base::Tokenize(input_args, delim);
-  auto arg_separator = CF_EXPECT(Parse(flag_registration, input_args_vec));
-  return std::move(arg_separator);
-}
-
 ArgumentsSeparator::ArgumentsSeparator(
     std::unique_ptr<ArgumentsLexer>&& lexer,
     const std::vector<std::string>& input_args,
