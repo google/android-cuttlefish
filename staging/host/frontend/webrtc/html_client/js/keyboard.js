1c13704274c8914edc96310c9348d3464fdc5adb
Linjiao Zhao <ruki@google.com>
2025-01-14 21:33:59 -0800

    [skip ci] Merge "Add function key combos button for Alt/Shift/Ctrl/Tab" into main
    GitOrigin-RevId: eb26b0710eb723ee84dea440df510349c216ca76

---
+++ staging/host/frontend/webrtc/html_client/js/keyboard.js
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+function processButton(buttonName, keyCode, dc) {
+  function onMouseDown(evt) {
+    dc.sendKeyEvent(keyCode, "keydown");
+  }
+
+  function onMouseUp(evt) {
+    dc.sendKeyEvent(keyCode, "keyup");
+  }
+  let button = document.getElementById(buttonName);
+  button.disabled = false;
+  button.addEventListener('mousedown', onMouseDown);
+  button.addEventListener('mouseup', onMouseUp);
+}
+
+function processToggleButton(buttonName, keyCode, dc) {
+  let toggle = false;
+  function onMouseDown(evt) {
+    toggle = !toggle;
+    if (toggle) dc.sendKeyEvent(keyCode, "keydown");
+    else dc.sendKeyEvent(keyCode, "keyup");
+    this.classList.toggle('active');
+  }
+
+  function onMouseUp(evt) {
+  }
+  let button = document.getElementById(buttonName);
+  button.disabled = false;
+  button.addEventListener('mousedown', onMouseDown);
+  button.addEventListener('mouseup', onMouseUp);
+}
+
+function enableKeyboardRewriteButton(dc) {
+  processToggleButton("shift-button", "ShiftLeft", dc);
+  processToggleButton("ctrl-button", "CtrlLeft", dc);
+  processToggleButton("alt-button", "AltLeft", dc);
+  processButton("tab-button", "Tab", dc);
+}
