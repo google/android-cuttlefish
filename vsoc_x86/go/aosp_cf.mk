88685a5066d4462c1f421bd78b76a62c0f1bff33
Maciej Żenczykowski <maze@google.com>
2025-01-15 12:46:55 -0800

    InProcessNetworkStack is long unsupported by virtue of in process tethering being dead.
    Test: TH
    Signed-off-by: Maciej Żenczykowski <maze@google.com>
    Change-Id: Icae6b5514a56e7ffc2e1d8b02e57f865640db1b1

--- vsoc_x86/go/aosp_cf.mk
+++ vsoc_x86/go/aosp_cf.mk
@@ -28,7 +28,6 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/app/PlatformCaptivePortalLogin/PlatformCaptivePortalLogin.apk \
     system/etc/permissions/platform_privapp_allowlist_com.android.cellbroadcastservice.xml \
     system/priv-app/CellBroadcastServiceModulePlatform/CellBroadcastServiceModulePlatform.apk \
-    system/priv-app/InProcessNetworkStack/InProcessNetworkStack.apk \
 
 #
 # All components inherited here go to system_ext image (same as GSI system_ext)
