118052239d8c0f66ba7ac4f04f8c696238146131
A. Cody Schuffelen <schuffelen@google.com>
2025-01-15 16:12:41 -0800

    Remove `SelectorFlags` other than constants
    Bug: b/389804379
    Test: /usr/bin/bazel test '//...'

--- base/cvd/cuttlefish/host/commands/cvd/cli/selector/selector_constants.h
+++ base/cvd/cuttlefish/host/commands/cvd/cli/selector/selector_constants.h
@@ -16,13 +16,6 @@
 
 #pragma once
 
-#include <sys/types.h>
-
-#include <string>
-
-#include "common/libs/utils/result.h"
-#include "host/commands/cvd/cli/flag.h"
-
 namespace cuttlefish {
 namespace selector {
 
@@ -50,25 +43,6 @@ class SelectorFlags {
   static constexpr char kGroupName[] = "group_name";
   static constexpr char kInstanceName[] = "instance_name";
   static constexpr char kVerbosity[] = "verbosity";
-
-  static const SelectorFlags& Get();
-  static Result<SelectorFlags> New();
-
-  Result<CvdFlagProxy> GetFlag(const std::string& search_key) const {
-    auto flag = CF_EXPECT(flags_.GetFlag(search_key));
-    return flag;
-  }
-
-  std::vector<CvdFlagProxy> Flags() const { return flags_.Flags(); }
-
- private:
-  SelectorFlags() = default;
-
-  static CvdFlag<std::string> GroupNameFlag(const std::string& name);
-  static CvdFlag<std::string> InstanceNameFlag(const std::string& name);
-  static CvdFlag<std::string> VerbosityFlag(const std::string& name);
-
-  FlagCollection flags_;
 };
 
 }  // namespace selector
