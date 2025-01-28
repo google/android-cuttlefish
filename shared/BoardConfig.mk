1d1fe907a59d2a2ae9f69e6cf8c42efaa90374c7
Viktor Martensson <vmartensson@google.com>
2025-01-28 06:17:15 +0000

    Switch most targets to 6.12, exclude TV, Wear, Go and Desktop
    Bug: 372262035
    Test: Treehugger
    Reviewed-By: Jie Jiang <jiejiang@google.com>
    Reviewed-By: Shao-Chuan Lee <shaochuan@google.com>
    Change-Id: Idfb74a46622b66e633f67924ae3bd1e5a314df8b
    Signed-off-by: Viktor Martensson <vmartensson@google.com>

--- shared/BoardConfig.mk
+++ shared/BoardConfig.mk
@@ -19,13 +19,15 @@
 #
 
 # Some targets still require 32 bit, and 6.6 kernels don't support
-# 32 bit devices (Wear, Go, Auto)
+# 32 bit devices
 ifeq (true,$(CLOCKWORK_EMULATOR_PRODUCT))
 TARGET_KERNEL_USE ?= 6.1
 else ifneq (,$(findstring x86_tv,$(PRODUCT_NAME)))
 TARGET_KERNEL_USE ?= 6.1
-else
+else ifneq (,$(findstring _desktop,$(PRODUCT_NAME)))
 TARGET_KERNEL_USE ?= 6.6
+else
+TARGET_KERNEL_USE ?= 6.12
 endif
 
 TARGET_KERNEL_ARCH ?= $(TARGET_ARCH)
