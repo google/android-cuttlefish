8dba4f8f94e2491fa245119f5cc253fb7ce096c6
Treehugger Robot <android-test-infra-autosubmit@system.gserviceaccount.com>
2025-02-07 09:26:08 -0800

    [skip ci] Merge "Refactor sensors code in webrtc" into main
    GitOrigin-RevId: 996ec5accd53c2c2d4dd595a3937f30ad8635355

--- staging/host/frontend/webrtc/html_client/js/app.js
+++ staging/host/frontend/webrtc/html_client/js/app.js
@@ -464,7 +464,10 @@ class DeviceControlApp {
 
     // Get sensor values from message.
     var sensor_vals = message.split(" ");
-    sensor_vals = sensor_vals.map((val) => parseFloat(val).toFixed(3));
+    var acc_update = sensor_vals[0].split(":").map((val) => parseFloat(val).toFixed(3));
+    var gyro_update = sensor_vals[1].split(":").map((val) => parseFloat(val).toFixed(3));
+    var mgn_update = sensor_vals[2].split(":").map((val) => parseFloat(val).toFixed(3));
+    var xyz_update = sensor_vals[3].split(":").map((val) => parseFloat(val).toFixed(3));
 
     const acc_val = document.getElementById('accelerometer-value');
     const mgn_val = document.getElementById('magnetometer-value');
@@ -474,19 +477,19 @@ class DeviceControlApp {
 
     // TODO: move to webrtc backend.
     // Inject sensors with new values.
-    adbShell(`/vendor/bin/cuttlefish_sensor_injection motion ${sensor_vals[3]} ${sensor_vals[4]} ${sensor_vals[5]} ${sensor_vals[6]} ${sensor_vals[7]} ${sensor_vals[8]} ${sensor_vals[9]} ${sensor_vals[10]} ${sensor_vals[11]}`);
+    adbShell(`/vendor/bin/cuttlefish_sensor_injection motion ${acc_update[0]} ${acc_update[1]} ${acc_update[2]} ${mgn_update[0]} ${mgn_update[1]} ${mgn_update[2]} ${gyro_update[0]} ${gyro_update[1]} ${gyro_update[2]}`);
 
     // Display new sensor values after injection.
-    acc_val.textContent = `${sensor_vals[3]} ${sensor_vals[4]} ${sensor_vals[5]}`;
-    mgn_val.textContent = `${sensor_vals[6]} ${sensor_vals[7]} ${sensor_vals[8]}`;
-    gyro_val.textContent = `${sensor_vals[9]} ${sensor_vals[10]} ${sensor_vals[11]}`;
+    acc_val.textContent = `${acc_update[0]} ${acc_update[1]} ${acc_update[2]}`;
+    mgn_val.textContent = `${mgn_update[0]} ${mgn_update[1]} ${mgn_update[2]}`;
+    gyro_val.textContent = `${gyro_update[0]} ${gyro_update[1]} ${gyro_update[2]}`;
 
     // Update xyz sliders with backend values.
     // This is needed for preserving device's state when display is turned on
     // and off, and for having the same state for multiple clients.
     for(let i = 0; i < 3; i++) {
-      xyz_val[i].textContent = sensor_vals[i];
-      xyz_range[i].value = sensor_vals[i];
+      xyz_val[i].textContent = xyz_update[i];
+      xyz_range[i].value = xyz_update[i];
     }
   }
 
--- staging/host/frontend/webrtc/sensors_simulator.cpp
+++ staging/host/frontend/webrtc/sensors_simulator.cpp
@@ -16,51 +16,59 @@
 
 #include "host/frontend/webrtc/sensors_simulator.h"
 
-#include <android-base/logging.h>
-
 #include <cmath>
+
+#include <android-base/logging.h>
 
 namespace cuttlefish {
 namespace webrtc_streaming {
+
 namespace {
 
 constexpr double kG = 9.80665;  // meter per second^2
 const Eigen::Vector3d kGravityVec{0, kG, 0}, kMagneticField{0, 5.9, -48.4};
 
-inline double toRadians(double x) { return x * M_PI / 180; }
+inline double ToRadians(double x) { return x * M_PI / 180; }
 
 // Calculate the rotation matrix of the pitch, roll, and yaw angles.
-static Eigen::Matrix3d getRotationMatrix(double x, double y, double z) {
-  x = toRadians(-x);
-  y = toRadians(-y);
-  z = toRadians(-z);
+static Eigen::Matrix3d GetRotationMatrix(double x, double y, double z) {
+  x = ToRadians(-x);
+  y = ToRadians(-y);
+  z = ToRadians(-z);
   // Create rotation matrices for each Euler angle
-  Eigen::Matrix3d rx = Eigen::AngleAxisd(x, Eigen::Vector3d::UnitX()).toRotationMatrix();
-  Eigen::Matrix3d ry = Eigen::AngleAxisd(y, Eigen::Vector3d::UnitY()).toRotationMatrix();
-  Eigen::Matrix3d rz = Eigen::AngleAxisd(z, Eigen::Vector3d::UnitZ()).toRotationMatrix();
+  Eigen::Matrix3d rx =
+      Eigen::AngleAxisd(x, Eigen::Vector3d::UnitX()).toRotationMatrix();
+  Eigen::Matrix3d ry =
+      Eigen::AngleAxisd(y, Eigen::Vector3d::UnitY()).toRotationMatrix();
+  Eigen::Matrix3d rz =
+      Eigen::AngleAxisd(z, Eigen::Vector3d::UnitZ()).toRotationMatrix();
 
   return rz * (ry * rx);
 }
 
 // Calculate new Accelerometer values of the new rotation degrees.
-static inline Eigen::Vector3d calculateAcceleration(Eigen::Matrix3d current_rotation_matrix) {
+static inline Eigen::Vector3d CalculateAcceleration(
+    Eigen::Matrix3d current_rotation_matrix) {
   return current_rotation_matrix * kGravityVec;
 }
 
 // Calculate new Magnetometer values of the new rotation degrees.
-static inline Eigen::Vector3d calculateMagnetometer(Eigen::Matrix3d current_rotation_matrix) {
+static inline Eigen::Vector3d CalculateMagnetometer(
+    Eigen::Matrix3d current_rotation_matrix) {
   return current_rotation_matrix * kMagneticField;
 }
 
 // Calculate new Gyroscope values of the new rotation degrees.
-static Eigen::Vector3d calculateGyroscope(std::chrono::duration<double> duration,
-                                          Eigen::Matrix3d prior_rotation_matrix,
-                                          Eigen::Matrix3d current_rotation_matrix) {
+static Eigen::Vector3d CalculateGyroscope(
+    std::chrono::duration<double> duration,
+    Eigen::Matrix3d prior_rotation_matrix,
+    Eigen::Matrix3d current_rotation_matrix) {
   double time_diff = duration.count();
   if (time_diff == 0) {
     return Eigen::Vector3d{0, 0, 0};
   }
-  Eigen::Matrix3d transition_matrix = prior_rotation_matrix * current_rotation_matrix.inverse();
+  Eigen::Matrix3d transition_matrix =
+      prior_rotation_matrix * current_rotation_matrix.inverse();
   // Convert rotation matrix to angular velocity numerator.
   Eigen::AngleAxisd angle_axis(transition_matrix);
   double angle = angle_axis.angle();
@@ -69,39 +77,53 @@ static Eigen::Vector3d calculateGyroscope(std::chrono::duration<double> duration
   gyro /= time_diff;
   return gyro;
 }
-
-std::string SerializeVector(const Eigen::Vector3d& v) {
-  std::stringstream s;
-  s << v(0) << " " << v(1) << " " << v(2);
-  return s.str();
-}
-
 }  // namespace
 
 SensorsSimulator::SensorsSimulator()
-    : current_rotation_matrix_(getRotationMatrix(0, 0, 0)),
-      last_event_timestamp_(std::chrono::high_resolution_clock::now()) {}
-
-// Update sensor values based on new rotation status.
-void SensorsSimulator::RefreshSensors(double x, double y, double z) {
-  xyz_ << x, y, z;
-  prior_rotation_matrix_ = current_rotation_matrix_;
-  current_rotation_matrix_ = getRotationMatrix(x, y, z);
-  acc_xyz_ = calculateAcceleration(current_rotation_matrix_);
-  mgn_xyz_ = calculateMagnetometer(current_rotation_matrix_);
-  auto current_time = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double> duration = current_time - last_event_timestamp_;
-  gyro_xyz_ = calculateGyroscope(duration, prior_rotation_matrix_,
-                                 current_rotation_matrix_);
-  last_event_timestamp_ = current_time;
+    : current_rotation_matrix_(GetRotationMatrix(0, 0, 0)),
+      last_event_timestamp_(std::chrono::high_resolution_clock::now()) {
+  // Initialize sensors_data_ based on rotation vector = (0, 0, 0)
+  RefreshSensors(0, 0, 0);
 }
 
-// Get sensors' data in string format to be passed as a message.
-std::string SensorsSimulator::GetSensorsData() {
-  std::stringstream sensors_data;
-  sensors_data << SerializeVector(xyz_) << " " << SerializeVector(acc_xyz_) << " "
-               << SerializeVector(mgn_xyz_) << " " << SerializeVector(gyro_xyz_);
-  return sensors_data.str();
+void SensorsSimulator::RefreshSensors(double x, double y, double z) {
+  auto rotation_matrix_update = GetRotationMatrix(x, y, z);
+  auto acc_update = CalculateAcceleration(rotation_matrix_update);
+  auto mgn_update = CalculateMagnetometer(rotation_matrix_update);
+
+  std::lock_guard<std::mutex> lock(sensors_data_mtx_);
+  auto current_time = std::chrono::high_resolution_clock::now();
+  auto duration = current_time - last_event_timestamp_;
+  last_event_timestamp_ = current_time;
+
+  auto gyro_update = CalculateGyroscope(duration, current_rotation_matrix_,
+                                        rotation_matrix_update);
+
+  current_rotation_matrix_ = rotation_matrix_update;
+
+  sensors_data_[sensors::kRotationVecId] << x, y, z;
+  sensors_data_[sensors::kAccelerationId] = acc_update;
+  sensors_data_[sensors::kGyroscopeId] = gyro_update;
+  sensors_data_[sensors::kMagneticId] = mgn_update;
+
+  // Copy the calibrated sensor data over for uncalibrated sensor support
+  sensors_data_[sensors::kUncalibAccelerationId] = acc_update;
+  sensors_data_[sensors::kUncalibGyroscopeId] = gyro_update;
+  sensors_data_[sensors::kUncalibMagneticId] = mgn_update;
+}
+
+std::string SensorsSimulator::GetSensorsData(const sensors::SensorsMask mask) {
+  std::stringstream sensors_msg;
+  std::lock_guard<std::mutex> lock(sensors_data_mtx_);
+  for (int id = 0; id <= sensors::kMaxSensorId; id++) {
+    if (mask & (1 << id)) {
+      auto v = sensors_data_[id];
+      sensors_msg << std::to_string(v(0)) << sensors::INNER_DELIM
+                  << std::to_string(v(1)) << sensors::INNER_DELIM
+                  << std::to_string(v(2)) << sensors::OUTER_DELIM;
+    }
+  }
+  return sensors_msg.str();
 }
 
 }  // namespace webrtc_streaming
