4ebcc8a7f1fec748fb733648b93d7ec3050a8189
A. Cody Schuffelen <schuffelen@google.com>
2025-03-11 13:54:52 -0700

    Changes to build with AOSP versions of utils/{environment,architecture}
    - Reintroduces other StringFromEnv overload from https://github.com/google/android-cuttlefish/commit/e49ca78b7b9bb0864c54effd144b6e7b0463974e
    - Imports android-base/no_destructor.h
    - Fixes includes to architecture.h
    
    Bug: b/402210740

--- base/cvd/cuttlefish/common/libs/utils/environment.h
+++ base/cvd/cuttlefish/common/libs/utils/environment.h
@@ -19,6 +19,8 @@
 
 namespace cuttlefish {
 
+std::optional<std::string> StringFromEnv(const std::string& varname);
+
 std::string StringFromEnv(const std::string& varname,
                           const std::string& defval);
 
