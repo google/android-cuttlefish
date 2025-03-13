0fd880e6401ac88f25b9c9ee407cfdd6fa814872
A. Cody Schuffelen <schuffelen@google.com>
2025-03-13 16:49:33 -0700

    Support replacing cvd_host_package.tar.gz/bin files with debian package versions in cvd fetch
    Bug: b/403354033

--- base/cvd/cuttlefish/host/commands/cvd/fetch/fetch_cvd_parser.h
+++ base/cvd/cuttlefish/host/commands/cvd/fetch/fetch_cvd_parser.h
@@ -99,6 +99,7 @@ struct FetchFlags {
   BuildApiFlags build_api_flags;
   VectorFlags vector_flags;
   int number_of_builds = 0;
+  std::vector<std::string> debian_package_executables;
 
   static Result<FetchFlags> Parse(std::vector<std::string>& args);
 };
