3af3425fbb430ebf217dcef96890b63ad3ed324b
Dmitrii Merkurev <dimorinny@google.com>
2025-03-21 07:50:47 -0700

    Revert "Customize sensor features for automotive"
    Revert submission 3441121-auto-cf-sensors
    
    Reason for revert: 400115679
    
    Reverted changes: /q/submissionid:3441121-auto-cf-sensors
    
    Change-Id: I137f73afe739b2ebbba03a69f8d152d7b29f06b8

--- shared/auto/device_vendor.mk
+++ shared/auto/device_vendor.mk
@@ -43,12 +43,11 @@ PRODUCT_COPY_FILES += \
     packages/services/Car/car_product/init/init.bootstat.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.bootstat.rc \
     packages/services/Car/car_product/init/init.car.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.car.rc
 
-# Override default sensor files in device_vendor.mk
-LOCAL_SENSOR_FILE_OVERRIDES := true
-# Add the sensor files that are supported on Automotive
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
-    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.gyroscope.xml
+ifneq ($(LOCAL_SENSOR_FILE_OVERRIDES),true)
+    PRODUCT_COPY_FILES += \
+        frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
+        frameworks/native/data/etc/android.hardware.sensor.compass.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.compass.xml
+endif
 
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.boot.uwbcountrycode=US
