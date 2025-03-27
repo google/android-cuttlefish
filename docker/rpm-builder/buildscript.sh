2b72017256906c14dece3d7a36f94a48feb8c522
Martin Zeitler <martin@syslogic.io>
2025-03-27 02:31:07 +0100

    still unsure about the destination path.

--- docker/rpm-builder/buildscript.sh
+++ docker/rpm-builder/buildscript.sh
@@ -4,6 +4,6 @@ cd /root || exit
 # git clone git@github.com:syslogic/android-cuttlefish.git
 git clone https://github.com/syslogic/android-cuttlefish.git
 cd android-cuttlefish || exit
-git switch rpm-workflow
+git switch rpm-build
 
 ./tools/buildutils/build_packages.sh
