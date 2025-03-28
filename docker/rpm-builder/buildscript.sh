11d6b86d99ce20e3a6b2b7a8a2b8be3dbce2c970
Martin Zeitler <martin@syslogic.io>
2025-03-28 13:36:34 +0100

    typo.

--- docker/rpm-builder/buildscript.sh
+++ docker/rpm-builder/buildscript.sh
@@ -9,7 +9,7 @@ git switch rpm-build
 ./tools/buildutils/build_packages.sh
 
 cd tools/rpmbuild/RPMS || exit
-tar zcvf /root/android-cuttlefish-rpm.tar.gz /*
+tar zcvf /home/android-cuttlefish-rpm.tar.gz /*
 
 cd ../SRPMS || exit
-tar zcvf /root/android-cuttlefish-src-rpm.tar.gz /*
+tar zcvf /home/android-cuttlefish-src-rpm.tar.gz /*
