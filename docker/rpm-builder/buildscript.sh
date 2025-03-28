7653476d31c9e6f106cf6bf6a4495e8b5e8218ff
Martin Zeitler <syslogic@users.noreply.github.com>
2025-03-28 23:08:46 +0100

    it still seems to use thr wrong registry ...

--- docker/rpm-builder/buildscript.sh
+++ docker/rpm-builder/buildscript.sh
@@ -4,8 +4,7 @@ REPO_NAME=android-cuttlefish
 
 git clone https://github.com/${REPO_OWNER}/${REPO_NAME}.git
 cd $REPO_NAME || exit
-git switch rpm-build
-
+IF [ $$REPO_OWNER -eq syslogic ] ; THEN  git switch rpm-build ; FI
 ./tools/buildutils/build_packages.sh
-
-tar zcvf /root/android-cuttlefish-src-rpm.tar.gz /root/${REPO_NAME}/tools/rpmbuild/*/*.rpm
+tar zcvf ~/android-cuttlefish-src-rpm.tar.gz ~/${REPO_NAME}/tools/rpmbuild/*/*.rpm
+la -la ~
