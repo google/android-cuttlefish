7922dd5aee2c4b5e051154fb12e27fe838411d79
Martin Zeitler <martin@syslogic.io>
2025-03-28 10:45:27 +0100

    buildscript now packages all packages into one file, with a stable filename.

--- docker/rpm-builder/buildscript.sh
+++ docker/rpm-builder/buildscript.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
 cd /root || exit
-# git clone git@github.com:syslogic/android-cuttlefish.git
+# git clone https://github.com/google/android-cuttlefish.git
 git clone https://github.com/syslogic/android-cuttlefish.git
 cd android-cuttlefish || exit
 git switch rpm-build
 
 ./tools/buildutils/build_packages.sh
+
+cd tools/rpmbuild/RPMS || exit
+tar zcvf /root/android-cuttlefish-rpm.tar.gz /*
+
+cd ../SRPMS || exit
+tar zcvf /root/android-cuttlefish-src-rpm.tar.gz /*
