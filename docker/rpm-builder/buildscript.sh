f8ff7fba7358fb4a03f5a1ff7022904faaf6201c
Martin Zeitler <martin@syslogic.io>
2025-03-29 00:50:22 +0100

    buildscript.

--- docker/rpm-builder/buildscript.sh
+++ docker/rpm-builder/buildscript.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
-REPO_OWNER=syslogic
 REPO_NAME=android-cuttlefish
+REPO_OWNER=syslogic
 
 git clone https://github.com/${REPO_OWNER}/${REPO_NAME}.git
 cd $REPO_NAME || exit
-IF [ $$REPO_OWNER -eq syslogic ] ; THEN  git switch rpm-build ; FI
+if [[ ${REPO_OWNER} = syslogic ]]
+then
+  git switch rpm-build
+fi
+
 ./tools/buildutils/build_packages.sh
-tar zcvf ~/android-cuttlefish-src-rpm.tar.gz ~/${REPO_NAME}/tools/rpmbuild/*/*.rpm
+tar zcvf ~/android-cuttlefish-src-rpm.tar.gz ~/${REPO_NAME}/tools/rpmbuild/*.rpm
 la -la ~
