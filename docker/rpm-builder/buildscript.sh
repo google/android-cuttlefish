be0b38a7a0723d16ee0dfc1fea4199b96693dc10
Martin Zeitler <martin@syslogic.io>
2025-03-29 13:31:17 +0100

    the syntax error is just not there?

--- docker/rpm-builder/buildscript.sh
+++ docker/rpm-builder/buildscript.sh
@@ -4,11 +4,8 @@ REPO_OWNER=syslogic
 
 git clone https://github.com/${REPO_OWNER}/${REPO_NAME}.git
 cd $REPO_NAME || exit
-if [[ ${REPO_OWNER} = syslogic ]]
-then
-  git switch rpm-build
-fi
 
+if [[ ${REPO_OWNER} = syslogic ]]; then git switch rpm-build; fi
 ./tools/buildutils/build_packages.sh
 
 tar zcvf ~/android-cuttlefish-rpm.tar.gz ~/${REPO_NAME}/tools/rpmbuild/*.rpm
