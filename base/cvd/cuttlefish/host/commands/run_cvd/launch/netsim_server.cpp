9f73fb97ef1a1628a84a0ed72a3907ed0b6dd66a
A. Cody Schuffelen <schuffelen@google.com>
2025-03-29 13:29:04 -0700

    Extract //cuttlefish/host/commands/run_cvd/launch:netsim_server
    https://bazel.build/docs/bazel-and-cpp

--- base/cvd/cuttlefish/host/commands/run_cvd/launch/netsim_server.cpp
+++ base/cvd/cuttlefish/host/commands/run_cvd/launch/netsim_server.cpp
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "cuttlefish/host/commands/run_cvd/launch/launch.h"
+#include "cuttlefish/host/commands/run_cvd/launch/netsim_server.h"
 
 #include <string>
 #include <unordered_set>
