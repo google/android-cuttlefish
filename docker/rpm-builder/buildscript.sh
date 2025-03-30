7a8ccd8d1a231d9e72bc9c58addfabc291f42df9
Martin Zeitler <martin@syslogic.io>
2025-03-30 06:23:12 +0200

    buildscript.sh

--- docker/rpm-builder/buildscript.sh
+++ docker/rpm-builder/buildscript.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env bssh
+#!/usr/bin/env bash
 # The file deployed seems stuck.
 REPO_NAME=android-cuttlefish
 REPO_OWNER=syslogic
 
 git clone https://github.com/${REPO_OWNER}/${REPO_NAME}.git
 cd $REPO_NAME || exit
-if [ "${REPO_OWNER}" = "syslogic" ]; then git switch rpm-build; fi
+[ "${REPO_OWNER}" == "syslogic" ] && echo git switch rpm-build
 ./tools/buildutils/build_packages.sh
 tar zcvf ~/android-cuttlefish-rpm.tar.gz ~/${REPO_NAME}/tools/rpmbuild/*.rpm
 ls -la ~
