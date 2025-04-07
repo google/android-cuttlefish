ca171287bd87e4c2f5ef1198a927805eff9a6c4f
Wei Li <weiwli@google.com>
2025-04-07 14:22:11 -0700

    Convert PRODUCT_COPY_FILES for audio_effects_config.xml to Android.bp
    Bug: 406021517
    Test: presubmits
    Flag: EXEMPT refactor
    Change-Id: I8e3fd9e57cbd38c158f9b91cadd9610ec06156db

--- shared/device.mk
+++ shared/device.mk
@@ -325,8 +325,7 @@ ifndef LOCAL_AUDIO_PRODUCT_COPY_FILES
 PRODUCT_PACKAGES += device_google_cuttlefish_shared_config_audio_policy
 $(call soong_config_set_bool,cuttlefish_config,use_audio_policy,true)
 
-LOCAL_AUDIO_PRODUCT_COPY_FILES += \
-    hardware/interfaces/audio/aidl/default/audio_effects_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects_config.xml
+$(call inherit-product, hardware/interfaces/audio/aidl/default/audio_effects.mk)
 endif
 endif
 
