14cabe490df0c40b86f136c20d88ad373d0f5e12
Martin Zeitler <martin@syslogic.io>
2025-04-11 05:18:00 +0200

    generating ARTIFACT_NAME.

--- docker/rpm-builder/scripts/sign_rpm.sh
+++ docker/rpm-builder/scripts/sign_rpm.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+# Check if the environmental variables were set.
+if [ -z ${RPM_SIGN_SIGNATURE+x} ]; then echo "RPM_SIGN_SIGNATURE is unset"; else echo "RPM_SIGN_SIGNATURE is set"; fi
+if [ -z ${RPM_SIGN_PASSPHRASE+x} ]; then echo "RPM_SIGN_PASSPHRASE is unset"; else echo "RPM_SIGN_PASSPHRASE is set"; fi
+
 # It signs all packages below the `rpmbuild/RPMS` directory.
 REPO_DIR="$(realpath "$(dirname "$0")/../..")"
 RPMS="${REPO_DIR}/tools/rpmbuild/RPMS/*/*.rpm"
 
-for file in $RPMS; do
-  ./rpm_sign.exp "$file" "$PGP_PASSPHRASE"
+for FILE in $RPMS; do
+  rpm_sign.exp "${FILE}" "${RPM_SIGN_PASSPHRASE}"
 done
