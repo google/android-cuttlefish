b74440c33e1948490fdd94dcd2e4cea8e28d6bc0
Seungjae Yoo <seungjaeyoo@google.com>
2025-04-21 14:22:04 +0900

    Disable auto updating CF debian packages at the runtime of Docker image

--- docker/guest/run_services.sh
+++ docker/guest/run_services.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-apt update
-apt --only-upgrade -y --no-install-recommends install \
-  cuttlefish-base \
-  cuttlefish-user \
-  cuttlefish-orchestration
-
 service nginx start
 service cuttlefish-host-resources start
 service cuttlefish-operator start
