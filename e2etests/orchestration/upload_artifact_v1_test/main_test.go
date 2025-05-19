2f8eac3e1177dd23e152e90928bdff7b57b8134f
Sergio Andres Rodriguez Orama <sorama@google.com>
2025-05-19 11:57:25 -0400

    Update uploading artifacts e2e test with stat operation.
    - Recreate two clients uploading the same artifact at the same time.

--- e2etests/orchestration/upload_artifact_v1_test/main_test.go
+++ e2etests/orchestration/upload_artifact_v1_test/main_test.go
@@ -16,6 +16,7 @@ package main
 
 import (
 	"path/filepath"
+	"sync"
 	"testing"
 
 	hoclient "github.com/google/android-cuttlefish/frontend/src/libhoclient"
@@ -26,15 +27,31 @@ const baseURL = "http://0.0.0.0:2080"
 
 func TestUploadArtifact(t *testing.T) {
 	srv := hoclient.NewHostOrchestratorService(baseURL)
-
-	if err := srv.UploadArtifact("../artifacts/cvd-host_package.tar.gz"); err != nil {
-		t.Fatal(err)
-	}
-
 	expected, err := hoclient.SHA256Checksum("../artifacts/cvd-host_package.tar.gz")
 	if err != nil {
 		t.Fatal(err)
 	}
+	_, err = srv.StatArtifact(expected)
+	if err == nil {
+		t.Fatal("expected error: file does not exist")
+	}
+
+	// Recreate two clients uploading the same artifact at the same time.
+	var wg sync.WaitGroup
+	for i := 0; i < 2; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if err := srv.UploadArtifact("../artifacts/cvd-host_package.tar.gz"); err != nil {
+				t.Error(err)
+			}
+			if _, err := srv.StatArtifact(expected); err != nil {
+				t.Error(err)
+			}
+		}()
+	}
+	wg.Wait()
+
 	got, err := hoclient.SHA256Checksum(filepath.Join("/var/lib/cuttlefish-common/user_artifacts", expected))
 	if err != nil {
 		t.Fatal(err)
