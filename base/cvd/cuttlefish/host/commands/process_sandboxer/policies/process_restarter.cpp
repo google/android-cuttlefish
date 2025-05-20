bcfb2f6df82894986692a47ec7b3b601df3b161a
A. Cody Schuffelen <schuffelen@google.com>
2025-05-20 16:18:10 -0700

    Build `//cuttlefish/host/commands/process_sandboxer/policies:process_restarter`
    Bug: b/401511284
    Test: `bazel test //cuttlefish/host/commands/process_sandboxer/policies:all --test_summary=terse`

--- base/cvd/cuttlefish/host/commands/process_sandboxer/policies/process_restarter.cpp
+++ base/cvd/cuttlefish/host/commands/process_sandboxer/policies/process_restarter.cpp
@@ -21,8 +21,8 @@
 
 #include <string>
 
-#include <sandboxed_api/sandbox2/policybuilder.h>
-#include <sandboxed_api/sandbox2/util/bpf_helper.h>
+#include "sandboxed_api/sandbox2/policybuilder.h"
+#include "sandboxed_api/sandbox2/util/bpf_helper.h"
 
 namespace cuttlefish::process_sandboxer {
 
