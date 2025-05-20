69514b7ec161fdbf0326df49883b72090509b7ad
A. Cody Schuffelen <schuffelen@google.com>
2025-05-20 16:24:59 -0700

    Build `//cuttlefish/host/commands/process_sandboxer/policies:simg2img`
    Bug: b/401511284
    Test: `bazel test //cuttlefish/host/commands/process_sandboxer/policies:all --test_summary=terse`

--- base/cvd/cuttlefish/host/commands/process_sandboxer/policies/simg2img.cpp
+++ base/cvd/cuttlefish/host/commands/process_sandboxer/policies/simg2img.cpp
@@ -22,8 +22,8 @@
 
 #include <vector>
 
-#include <sandboxed_api/sandbox2/policybuilder.h>
-#include <sandboxed_api/sandbox2/util/bpf_helper.h>
+#include "sandboxed_api/sandbox2/policybuilder.h"
+#include "sandboxed_api/sandbox2/util/bpf_helper.h"
 
 namespace cuttlefish::process_sandboxer {
 
