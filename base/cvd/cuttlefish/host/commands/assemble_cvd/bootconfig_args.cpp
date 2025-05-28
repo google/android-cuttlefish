e995902212152bd1ff08384a580dc32f050bd774
A. Cody Schuffelen <schuffelen@google.com>
2025-05-28 13:05:48 -0700

    Extract `//cuttlefish/host/commands/assemble_cvd:bootconfig_args`
    Bug: b/420961790

--- base/cvd/cuttlefish/host/commands/assemble_cvd/bootconfig_args.cpp
+++ base/cvd/cuttlefish/host/commands/assemble_cvd/bootconfig_args.cpp
@@ -17,18 +17,14 @@
 #include "host/commands/assemble_cvd/bootconfig_args.h"
 
 #include <array>
-#include <sstream>
 #include <string>
 #include <vector>
 
 #include <android-base/parseint.h>
 
-#include "common/libs/utils/environment.h"
-#include "common/libs/utils/files.h"
 #include "common/libs/utils/json.h"
 #include "host/libs/config/config_constants.h"
 #include "host/libs/config/cuttlefish_config.h"
-#include "host/libs/config/known_paths.h"
 #include "host/libs/vm_manager/crosvm_manager.h"
 #include "host/libs/vm_manager/qemu_manager.h"
 #include "host/libs/vm_manager/vm_manager.h"
@@ -149,7 +145,7 @@ Result<std::unordered_map<std::string, std::string>> BootconfigArgsFromConfig(
   }
 
   if (instance.enable_modem_simulator() &&
-      instance.modem_simulator_ports() != "") {
+      !instance.modem_simulator_ports().empty()) {
     bootconfig_args["androidboot.modem_simulator_ports"] =
         instance.modem_simulator_ports();
   }
