179968f76b527fdff61ca9d4063a84daa92777ee
A. Cody Schuffelen <schuffelen@google.com>
2025-05-28 17:15:28 -0700

    Extract `//cuttlefish/host/commands/assemble_cvd/disk:generate_persistent_bootconfig`
    Bug: 420961790

--- base/cvd/cuttlefish/host/commands/assemble_cvd/disk/disk.h
+++ base/cvd/cuttlefish/host/commands/assemble_cvd/disk/disk.h
@@ -19,15 +19,13 @@
 #include "host/commands/assemble_cvd/boot_config.h"
 #include "host/commands/assemble_cvd/disk/factory_reset_protected.h"
 #include "host/commands/assemble_cvd/disk/gem5_image_unpacker.h"
+#include "host/commands/assemble_cvd/disk/generate_persistent_bootconfig.h"
 #include "host/commands/assemble_cvd/disk/kernel_ramdisk_repacker.h"
 #include "host/libs/avb/avb.h"
 #include "host/libs/config/cuttlefish_config.h"
 #include "host/libs/feature/feature.h"
 
 namespace cuttlefish {
-
-Result<void> GeneratePersistentBootconfig(
-    const CuttlefishConfig&, const CuttlefishConfig::InstanceSpecific&);
 
 Result<void> GeneratePersistentVbmeta(
     const CuttlefishConfig::InstanceSpecific&,
