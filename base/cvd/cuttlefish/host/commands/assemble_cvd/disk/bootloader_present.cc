1b4c058404a4e7c638b6bd08f0d1b3f51f829919
A. Cody Schuffelen <schuffelen@google.com>
2025-06-10 17:24:15 -0700

    Extract `assemble_cvd/disk/bootloader_present.{cc,h}`
    Bug: b/424001881
    Test: bazel test '//cuttlefish/host/commands/assemble_cvd/...' --test_summary=terse

---
+++ base/cvd/cuttlefish/host/commands/assemble_cvd/disk/bootloader_present.cc
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "cuttlefish/host/commands/assemble_cvd/disk/bootloader_present.h"
+
+#include "cuttlefish/common/libs/utils/files.h"
+#include "cuttlefish/common/libs/utils/result.h"
+#include "cuttlefish/host/libs/config/cuttlefish_config.h"
+
+namespace cuttlefish {
+
+Result<void> BootloaderPresentCheck(
+    const CuttlefishConfig::InstanceSpecific& instance) {
+  CF_EXPECT(FileHasContent(instance.bootloader()),
+            "File not found: " << instance.bootloader());
+  return {};
+}
+
+}  // namespace cuttlefish
