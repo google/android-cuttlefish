d6f7739a2447251d514491f55628e6010c4c614b
JaeMan Park <jaeman@google.com>
2025-06-22 14:25:23 +0900

    Use bazel remote cache for Kokoro

--- .kokoro/presubmit.sh
+++ .kokoro/presubmit.sh
@@ -9,8 +9,13 @@ sudo DEBIAN_FRONTEND=noninteractive apt -o Dpkg::Options::="--force-confdef" -o 
 # realpath .kokoro/..
 REPO_DIR="$(realpath "$(dirname "$0")"/..)"
 TOOL_DIR="${REPO_DIR}/tools"
+CACHE_CONFIG_FILE="${REPO_DIR}/.config/cache-config.env"
 
-"${TOOL_DIR}/buildutils/build_packages.sh"
+if [ -f "$CACHE_CONFIG_FILE" ]; then
+    source "$CACHE_CONFIG_FILE"
+fi
+
+"${TOOL_DIR}/buildutils/build_packages.sh" -r "${BAZEL_REMOTE_CACHE}" -c "${CACHE_VERSION}"
 
 # Add test user to the kokoro group so it has access to the source dir
 "${TOOL_DIR}/testutils/prepare_host.sh" -d "${REPO_DIR}" -u testrunner -g kokoro
