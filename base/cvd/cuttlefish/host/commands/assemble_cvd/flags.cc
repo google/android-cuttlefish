4526571f52090df1eadc733cbeabf3270fc52e6d
A. Cody Schuffelen <schuffelen@google.com>
2025-06-30 17:55:12 -0700

    Extract `ParseDisplaysProto` to a new file.
    Bug: b/428789515

--- base/cvd/cuttlefish/host/commands/assemble_cvd/flags.cc
+++ base/cvd/cuttlefish/host/commands/assemble_cvd/flags.cc
@@ -33,7 +33,6 @@
 #include <fmt/format.h>
 #include <fruit/fruit.h>
 #include <gflags/gflags.h>
-#include <google/protobuf/text_format.h>
 #include <json/json.h>
 #include <json/writer.h>
 
@@ -53,13 +52,12 @@
 #include "cuttlefish/host/commands/assemble_cvd/boot_image_utils.h"
 #include "cuttlefish/host/commands/assemble_cvd/disk_flags.h"
 #include "cuttlefish/host/commands/assemble_cvd/display.h"
+#include "cuttlefish/host/commands/assemble_cvd/flags/display_proto.h"
 #include "cuttlefish/host/commands/assemble_cvd/flags/system_image_dir.h"
-#include "cuttlefish/host/commands/assemble_cvd/flags_defaults.h"
 #include "cuttlefish/host/commands/assemble_cvd/graphics_flags.h"
 #include "cuttlefish/host/commands/assemble_cvd/guest_config.h"
 #include "cuttlefish/host/commands/assemble_cvd/misc_info.h"
 #include "cuttlefish/host/commands/assemble_cvd/network_flags.h"
-#include "cuttlefish/host/commands/assemble_cvd/proto/launch_cvd.pb.h"
 #include "cuttlefish/host/commands/assemble_cvd/touchpad.h"
 #include "cuttlefish/host/libs/config/ap_boot_flow.h"
 #include "cuttlefish/host/libs/config/config_constants.h"
@@ -324,78 +322,6 @@ Result<std::vector<GuestConfig>> ReadGuestConfig(
 }
 
 #endif  // #ifdef __ANDROID__
-
-template <typename ProtoType>
-Result<ProtoType> ParseTextProtoFlagHelper(const std::string& flag_value,
-                                       const std::string& flag_name) {
-  ProtoType proto_result;
-  google::protobuf::TextFormat::Parser p;
-  CF_EXPECT(p.ParseFromString(flag_value, &proto_result),
-            "Failed to parse: " << flag_name << ", value: " << flag_value);
-  return proto_result;
-}
-
-template <typename ProtoType>
-Result<ProtoType> ParseBinProtoFlagHelper(const std::string& flag_value,
-                                       const std::string& flag_name) {
-  ProtoType proto_result;
-  std::vector<uint8_t> output;
-  CF_EXPECT(DecodeBase64(flag_value, &output));
-  std::string serialized = std::string(output.begin(), output.end());
-  CF_EXPECT(proto_result.ParseFromString(serialized),
-            "Failed to parse binary proto, flag: " << flag_name << ", value: "
-                                                   << flag_value);
-  return proto_result;
-}
-
-Result<std::vector<std::vector<CuttlefishConfig::DisplayConfig>>>
-    ParseDisplaysProto() {
-  auto proto_result = FLAGS_displays_textproto.empty() ? \
-  ParseBinProtoFlagHelper<InstancesDisplays>(FLAGS_displays_binproto, "displays_binproto") : \
-  ParseTextProtoFlagHelper<InstancesDisplays>(FLAGS_displays_textproto, "displays_textproto");
-
-  InstancesDisplays display_proto = CF_EXPECT(std::move(proto_result));
-
-  std::vector<std::vector<CuttlefishConfig::DisplayConfig>> result;
-  for (int i = 0; i < display_proto.instances_size(); i++) {
-    std::vector<CuttlefishConfig::DisplayConfig> display_configs;
-    const InstanceDisplays& launch_cvd_instance = display_proto.instances(i);
-    for (int display_num=0; display_num<launch_cvd_instance.displays_size(); display_num++) {
-      const InstanceDisplay& display = launch_cvd_instance.displays(display_num);
-
-      // use same code logic from ParseDisplayConfig
-      int display_dpi = CF_DEFAULTS_DISPLAY_DPI;
-      if (display.dpi() != 0) {
-        display_dpi = display.dpi();
-      }
-
-      int display_refresh_rate_hz = CF_DEFAULTS_DISPLAY_REFRESH_RATE;
-      if (display.refresh_rate_hertz() != 0) {
-        display_refresh_rate_hz = display.refresh_rate_hertz();
-      }
-
-      std::string overlays = "";
-
-      for (const auto& overlay : display.overlays()) {
-        overlays +=
-            fmt::format("{}:{} ", overlay.vm_index(), overlay.display_index());
-      }
-
-      auto dc = CuttlefishConfig::DisplayConfig{
-          .width = display.width(),
-          .height = display.height(),
-          .dpi = display_dpi,
-          .refresh_rate_hz = display_refresh_rate_hz,
-          .overlays = overlays,
-      };
-
-      display_configs.push_back(dc);
-    }
-    result.push_back(display_configs);
-  }
-
-  return result;
-}
 
 Result<std::unordered_map<int, std::string>> CreateNumToWebrtcDeviceIdMap(
     const CuttlefishConfig& tmp_config_obj,
