4cb942f56435492647da3008abc774335cb52abf
Seungjae Yoo <seungjaeyoo@google.com>
2025-07-03 14:40:05 +0900

    ExtractArtifact doesn't deal with http 409 status in HO client API level

--- frontend/src/libhoclient/host_orchestrator_client_test.go
+++ frontend/src/libhoclient/host_orchestrator_client_test.go
@@ -222,32 +222,6 @@ func TestExtractArtifactSucceeds(t *testing.T) {
 	}
 }
 
-func TestExtractArtifactReceive409WhenArtifactIsAlreadyExtracted(t *testing.T) {
-	tempDir := createTempDir(t)
-	defer os.RemoveAll(tempDir)
-	testFile := createTempFile(t, tempDir, "waldo", []byte("waldo"))
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		switch ep := r.Method + " " + r.URL.Path; ep {
-		case "GET /v1/userartifacts/d2c055002a6cdf8dd9edf90c7a666cb5f7f2d25da8519ec206f56777d74e0c7d":
-			writeOK(w, hoapi.StatArtifactResponse{})
-		case "POST /v1/userartifacts/d2c055002a6cdf8dd9edf90c7a666cb5f7f2d25da8519ec206f56777d74e0c7d/:extract":
-			writeErr(w, http.StatusConflict)
-		default:
-			t.Fatal("unexpected endpoint: " + ep)
-		}
-	}))
-	defer ts.Close()
-	client := NewHostOrchestratorClient(ts.URL)
-
-	op, err := client.ExtractArtifact(testFile)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if op != nil {
-		t.Fatal("unexpected operation: " + op.Name)
-	}
-}
-
 func TestCreateCVDWithUserProjectOverride(t *testing.T) {
 	fakeRes := &hoapi.CreateCVDResponse{CVDs: []*hoapi.CVD{{Name: "1"}}}
 	token := "foo"
