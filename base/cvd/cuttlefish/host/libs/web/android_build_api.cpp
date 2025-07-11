d50eb5e4028327a434b225687fa452f3ebfa81f8
Chad Reynolds <chadreynolds@google.com>
2025-07-11 15:22:04 -0700

    Use helper to retrieve values
    The helper concisely checks that the field exists and that it matches
    the expected type.  A couple locations were not checking for the field
    before attempting to access it.

--- base/cvd/cuttlefish/host/libs/web/android_build_api.cpp
+++ base/cvd/cuttlefish/host/libs/web/android_build_api.cpp
@@ -40,6 +40,7 @@
 #include "cuttlefish/common/libs/utils/contains.h"
 #include "cuttlefish/common/libs/utils/environment.h"
 #include "cuttlefish/common/libs/utils/files.h"
+#include "cuttlefish/common/libs/utils/json.h"
 #include "cuttlefish/common/libs/utils/result.h"
 #include "cuttlefish/host/libs/web/android_build.h"
 #include "cuttlefish/host/libs/web/android_build_string.h"
@@ -233,10 +234,7 @@ Result<std::optional<std::string>> AndroidBuildApi::LatestBuildId(
                 << target << "\" on \"" << branch << "\", but received "
                 << json["builds"].size() << ". Full response:\n"
                 << json);
-  CF_EXPECT(json["builds"][0].isMember("buildId"),
-            "\"buildId\" member missing from response.  Full response:\n"
-                << json);
-  return json["builds"][0]["buildId"].asString();
+  return CF_EXPECT(GetValue<std::string>(json["builds"][0], { "buildId" }));
 }
 
 Result<std::string> AndroidBuildApi::BuildStatus(const DeviceBuild& build) {
@@ -263,7 +261,7 @@ Result<std::string> AndroidBuildApi::BuildStatus(const DeviceBuild& build) {
             "Response had \"error\" but had http success status. Received \""
                 << json << "\"");
 
-  return json["buildAttemptStatus"].asString();
+  return CF_EXPECT(GetValue<std::string>(json, { "buildAttemptStatus" }));
 }
 
 Result<std::string> AndroidBuildApi::ProductName(const DeviceBuild& build) {
@@ -290,8 +288,7 @@ Result<std::string> AndroidBuildApi::ProductName(const DeviceBuild& build) {
             "Response had \"error\" but had http success status. Received \""
                 << json << "\"");
 
-  CF_EXPECT(json.isMember("target"), "Build was missing target field.");
-  return json["target"]["product"].asString();
+  return CF_EXPECT(GetValue<std::string>(json, { "target", "product" }));
 }
 
 Result<std::unordered_set<std::string>> AndroidBuildApi::Artifacts(
@@ -331,7 +328,8 @@ Result<std::unordered_set<std::string>> AndroidBuildApi::Artifacts(
       page_token = "";
     }
     for (const auto& artifact_json : json["artifacts"]) {
-      artifacts.emplace(artifact_json["name"].asString());
+      artifacts.emplace(
+          CF_EXPECT(GetValue<std::string>(artifact_json, {"name"})));
     }
   } while (!page_token.empty());
   return artifacts;
@@ -387,9 +385,7 @@ Result<std::string> AndroidBuildApi::GetArtifactDownloadUrl(
   CF_EXPECT(!json.isMember("error"),
             "Response had \"error\" but had http success status. "
                 << "Received \"" << json << "\"");
-  CF_EXPECT(json.isMember("signedUrl"),
-            "URL endpoint did not have json path: " << json);
-  return json["signedUrl"].asString();
+  return CF_EXPECT(GetValue<std::string>(json, { "signedUrl" }));
 }
 
 Result<void> AndroidBuildApi::ArtifactToFile(const DeviceBuild& build,
