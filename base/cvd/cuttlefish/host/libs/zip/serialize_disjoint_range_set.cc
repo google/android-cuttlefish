de0f0dd8278b895780356195c6d21402c056175a
A. Cody Schuffelen <schuffelen@google.com>
2025-08-08 14:18:31 -0700

    Add serialization operations for DisjointRangeSet
    Bug: b/438283405

---
+++ base/cvd/cuttlefish/host/libs/zip/serialize_disjoint_range_set.cc
@@ -0,0 +1,49 @@
+//
+// Copyright (C) 2025 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include "cuttlefish/host/libs/zip/serialize_disjoint_range_set.h"
+
+#include <stdint.h>
+
+#include <string>
+#include <string_view>
+#include <utility>
+
+#include "cuttlefish/common/libs/utils/result.h"
+#include "cuttlefish/host/libs/zip/disjoint_range_set.h"
+#include "cuttlefish/host/libs/zip/disjoint_range_set.pb.h"
+
+namespace cuttlefish {
+
+std::string Serialize(const DisjointRangeSet& range_set) {
+  DisjointRangeList proto;
+  for (std::pair<uint64_t, uint64_t> range : range_set.AllRanges()) {
+    DisjointRangeListMember* member = proto.add_ranges();
+    member->set_start(range.first);
+    member->set_end(range.second);
+  }
+  return proto.SerializeAsString();
+}
+
+Result<DisjointRangeSet> DeserializeDisjointRangeSet(std::string_view data) {
+  DisjointRangeList proto;
+  CF_EXPECT(proto.ParseFromArray(data.data(), data.size()));
+  DisjointRangeSet set;
+  for (const DisjointRangeListMember& range : proto.ranges()) {
+    set.InsertRange(range.start(), range.end());
+  }
+  return set;
+}
+
+}  // namespace cuttlefish
---
+++ base/cvd/cuttlefish/host/libs/zip/serialize_disjoint_range_set_test.cc
@@ -0,0 +1,59 @@
+//
+// Copyright (C) 2023 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "cuttlefish/host/libs/zip/serialize_disjoint_range_set.h"
+
+#include <string>
+
+#include "gmock/gmock-matchers.h"
+#include "gtest/gtest.h"
+
+#include "cuttlefish/common/libs/utils/result_matchers.h"
+#include "cuttlefish/host/libs/zip/disjoint_range_set.h"
+
+namespace cuttlefish {
+namespace {
+
+TEST(DisjointRangeSet, SerializeEmptySet) {
+  DisjointRangeSet set;
+
+  std::string str = Serialize(set);
+
+  EXPECT_THAT(DeserializeDisjointRangeSet(str), IsOkAndValue(set));
+}
+
+TEST(DisjointRangeSet, SerializeOneMember) {
+  DisjointRangeSet set;
+
+  set.InsertRange(5, 15);
+
+  std::string str = Serialize(set);
+
+  EXPECT_THAT(DeserializeDisjointRangeSet(str), IsOkAndValue(set));
+}
+
+TEST(DisjointRangeSet, SerializeTwoMembers) {
+  DisjointRangeSet set;
+
+  set.InsertRange(5, 15);
+  set.InsertRange(25, 35);
+
+  std::string str = Serialize(set);
+
+  EXPECT_THAT(DeserializeDisjointRangeSet(str), IsOkAndValue(set));
+}
+
+}  // namespace
+}  // namespace cuttlefish
