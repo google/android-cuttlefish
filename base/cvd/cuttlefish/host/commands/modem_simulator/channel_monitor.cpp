ecf04dc90bd7a9bf4c936021b7a111ea78bd74d3
A. Cody Schuffelen <schuffelen@google.com>
2025-08-12 19:21:08 -0700

    Separate channel_monitor and client in modem_simulator
    Bug: b/438343307

--- base/cvd/cuttlefish/host/commands/modem_simulator/channel_monitor.cpp
+++ base/cvd/cuttlefish/host/commands/modem_simulator/channel_monitor.cpp
@@ -21,64 +21,12 @@
 #include <android-base/logging.h>
 #include <android-base/strings.h>
 
-#include "cuttlefish/common/libs/fs/shared_buf.h"
 #include "cuttlefish/common/libs/fs/shared_select.h"
 #include "cuttlefish/host/commands/modem_simulator/modem_simulator.h"
 
 namespace cuttlefish {
 
 constexpr int32_t kMaxCommandLength = 4096;
-
-size_t ClientId::next_id_ = 0;
-
-ClientId::ClientId() {
-  id_ = next_id_;
-  next_id_++;
-}
-
-bool ClientId::operator==(const ClientId& other) const {
-  return id_ == other.id_;
-}
-
-Client::Client(SharedFD fd) : client_read_fd_(fd), client_write_fd_(fd) {}
-
-Client::Client(SharedFD read, SharedFD write)
-    : client_read_fd_(std::move(read)), client_write_fd_(std::move(write)) {}
-
-Client::Client(SharedFD fd, ClientType client_type)
-    : type(client_type), client_read_fd_(fd), client_write_fd_(fd) {}
-
-Client::Client(SharedFD read, SharedFD write, ClientType client_type)
-    : type(client_type),
-      client_read_fd_(std::move(read)),
-      client_write_fd_(std::move(write)) {}
-
-bool Client::operator==(const Client& other) const {
-  return client_read_fd_ == other.client_read_fd_ &&
-         client_write_fd_ == other.client_write_fd_;
-}
-
-void Client::SendCommandResponse(std::string response) const {
-  if (response.empty()) {
-    LOG(DEBUG) << "Invalid response, ignore!";
-    return;
-  }
-
-  if (response.back() != '\r') {
-    response += '\r';
-  }
-  LOG(VERBOSE) << " AT< " << response;
-
-  std::lock_guard<std::mutex> lock(write_mutex);
-  WriteAll(client_write_fd_, response);
-}
-
-void Client::SendCommandResponse(
-    const std::vector<std::string>& responses) const {
-  for (auto& response : responses) {
-    SendCommandResponse(response);
-  }
-}
 
 ChannelMonitor::ChannelMonitor(ModemSimulator& modem, SharedFD server)
     : modem_(modem), server_(std::move(server)) {
---
+++ base/cvd/cuttlefish/host/commands/modem_simulator/client.h
@@ -0,0 +1,84 @@
+//
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <mutex>
+#include <vector>
+
+#include "cuttlefish/common/libs/fs/shared_fd.h"
+
+class ModemServiceTest;
+
+namespace cuttlefish {
+
+class ChannelMonitor;
+class ModemSimulator;
+
+class ClientId {
+ public:
+  ClientId();
+
+  bool operator==(const ClientId&) const;
+
+ private:
+  static size_t next_id_;
+  size_t id_;
+};
+
+/**
+ * Client object managed by ChannelMonitor, contains two types, the RIL client
+ * and the remote client of other cuttlefish instance.
+ * Due to std::mutex does not implement its copy and operate= constructors, it
+ * can't be stored in standard contains (vector, map), so use the point instead.
+ */
+class Client {
+ public:
+  enum ClientType { RIL, REMOTE };
+
+  Client() = default;
+  ~Client() = default;
+  Client(SharedFD fd);
+  Client(SharedFD read, SharedFD write);
+  Client(SharedFD fd, ClientType client_type);
+  Client(SharedFD read, SharedFD write, ClientType client_type);
+  Client(const Client& client) = delete;
+  Client(Client&& client) = delete;
+
+  Client& operator=(Client&& other) = delete;
+
+  bool operator==(const Client& other) const;
+
+  void SendCommandResponse(std::string response) const;
+  void SendCommandResponse(const std::vector<std::string>& responses) const;
+
+  ClientId Id() const { return id_; }
+  ClientType Type() const { return type; }
+
+ private:
+  friend class ChannelMonitor;
+  friend class ::ModemServiceTest;
+
+  ClientId id_;
+  ClientType type = RIL;
+  SharedFD client_read_fd_;
+  SharedFD client_write_fd_;
+  std::string incomplete_command;
+  mutable std::mutex write_mutex;
+  bool first_read_command_;  // Only used when ClientType::REMOTE
+  bool is_valid = true;
+};
+
+}  // namespace cuttlefish
