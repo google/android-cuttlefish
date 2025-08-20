3ea59f413065a25ec2af7813553e33f42670146e
Seungjae Yoo <seungjaeyoo@google.com>
2025-08-20 10:53:42 +0900

    Cleanup tools/cuttlefish-host-image-installer

--- tools/cuttlefish-host-image-installer/metapackage-google/debian/metapackage-google.cron.d
+++
@@ -1,6 +0,0 @@
-#
-# Regular cron jobs for the metapackage-google package.
-#
-# Workarounds for Network-Manager
-@reboot root sleep 100 && /usr/sbin/dhclient
-@reboot root sleep 150 && su vsoc-01 -c '/bin/bash /usr/share/metapackage-google/cf_docker_co_run.sh'
