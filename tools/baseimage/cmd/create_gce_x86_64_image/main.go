9f5e767032dcbdd7fb72835356f532cdff52e1d1
Seungjae Yoo <seungjaeyoo@google.com>
2025-08-25 11:02:31 +0900

    Leave GCE helper code under tools/baseimage into separated file

--- tools/baseimage/cmd/create_gce_x86_64_image/main.go
+++ tools/baseimage/cmd/create_gce_x86_64_image/main.go
@@ -24,8 +24,8 @@ import (
 	"strings"
 	"time"
 
+	"github.com/google/android-cuttlefish/tools/baseimage/pkg/gce"
 	"github.com/google/android-cuttlefish/tools/baseimage/pkg/gce/scripts"
-	"google.golang.org/api/compute/v1"
 )
 
 // Cuttlefish base images are based on debian images.
@@ -42,141 +42,6 @@ var (
 	project = flag.String("project", "", "GCE project whose resources will be used for creating the image")
 	zone    = flag.String("zone", "us-central1-a", "GCE zone used for creating relevant resources")
 )
-
-type gceHelper struct {
-	Service *compute.Service
-	Project string
-	Zone    string
-}
-
-func (h *gceHelper) waitForOperation(op *compute.Operation) error {
-	for attempt := 0; attempt < 3 && op.Status != "DONE"; attempt++ {
-		var err error
-		op, err = h.Service.ZoneOperations.Wait(h.Project, h.Zone, op.Name).Do()
-		if err != nil {
-			return err
-		}
-		time.Sleep(30 * time.Second)
-	}
-	if op.Status != "DONE" {
-		return fmt.Errorf("wait for operation %q: timed out", op.Name)
-	}
-	return nil
-}
-
-func (h *gceHelper) waitForGlobalOperation(op *compute.Operation) error {
-	for attempt := 0; attempt < 3 && op.Status != "DONE"; attempt++ {
-		var err error
-		op, err = h.Service.GlobalOperations.Wait(h.Project, op.Name).Do()
-		if err != nil {
-			return err
-		}
-		time.Sleep(30 * time.Second)
-	}
-	if op.Status != "DONE" {
-		return fmt.Errorf("wait for operation %q: timed out", op.Name)
-	}
-	return nil
-}
-
-func (h *gceHelper) CreateDisk(sourceImageProject, sourceImage, name string) (*compute.Disk, error) {
-	payload := &compute.Disk{
-		Name:        name,
-		SourceImage: fmt.Sprintf("projects/%s/global/images/%s", sourceImageProject, sourceImage),
-	}
-	op, err := h.Service.Disks.Insert(h.Project, h.Zone, payload).Do()
-	if err != nil {
-		return nil, err
-	}
-	if err := h.waitForOperation(op); err != nil {
-		return nil, err
-	}
-	return payload, nil
-}
-
-func (h *gceHelper) DeleteDisk(name string) error {
-	op, err := h.Service.Disks.Delete(h.Project, h.Zone, name).Do()
-	if err != nil {
-		return err
-	}
-	return h.waitForOperation(op)
-}
-
-func (h *gceHelper) CreateInstance(name string) (*compute.Instance, error) {
-	payload := &compute.Instance{
-		Name:           name,
-		MachineType:    fmt.Sprintf("zones/%s/machineTypes/%s", h.Zone, "n1-standard-16"),
-		MinCpuPlatform: "Intel Haswell",
-		Disks: []*compute.AttachedDisk{
-			{
-				InitializeParams: &compute.AttachedDiskInitializeParams{
-					SourceImage: "projects/debian-cloud/global/images/debian-12-bookworm-v20250415",
-				},
-				Boot:       true,
-				AutoDelete: true,
-			},
-		},
-		NetworkInterfaces: []*compute.NetworkInterface{
-			{
-				AccessConfigs: []*compute.AccessConfig{
-					{
-						Name: "External NAT",
-						Type: "ONE_TO_ONE_NAT",
-					},
-				},
-			},
-		},
-	}
-	op, err := h.Service.Instances.Insert(h.Project, h.Zone, payload).Do()
-	if err != nil {
-		return nil, err
-
-	}
-	if err := h.waitForOperation(op); err != nil {
-		return nil, err
-	}
-	return payload, nil
-}
-
-func (h *gceHelper) DeleteInstance(name string) error {
-	op, err := h.Service.Instances.Delete(h.Project, h.Zone, name).Do()
-	if err != nil {
-		return err
-	}
-	return h.waitForOperation(op)
-}
-
-func (h *gceHelper) AttachDisk(ins, disk string) error {
-	attachedDisk := &compute.AttachedDisk{
-		Source: fmt.Sprintf("projects/%s/zones/%s/disks/%s", h.Project, h.Zone, disk),
-	}
-	op, err := h.Service.Instances.AttachDisk(h.Project, h.Zone, ins, attachedDisk).Do()
-	if err != nil {
-		return err
-	}
-	return h.waitForOperation(op)
-}
-
-func (h *gceHelper) DetachDisk(ins, disk string) error {
-	op, err := h.Service.Instances.DetachDisk(h.Project, h.Zone, ins, disk).Do()
-	if err != nil {
-		return err
-	}
-	return h.waitForOperation(op)
-}
-
-func (h *gceHelper) CreateImage(ins, disk, name string) error {
-	payload := &compute.Image{
-		Name:       name,
-		SourceDisk: fmt.Sprintf("projects/%s/zones/%s/disks/%s", h.Project, h.Zone, disk),
-		Licenses:   []string{"https://www.googleapis.com/compute/v1/projects/vm-options/global/licenses/enable-vmx"},
-	}
-	op, err := h.Service.Images.Insert(h.Project, payload).Do()
-	if err != nil {
-		return err
-	}
-	return h.waitForGlobalOperation(op)
-}
 
 func runCmd(name string, args ...string) error {
 	cmd := exec.CommandContext(context.TODO(), name, args...)
@@ -215,55 +80,49 @@ func uploadBashScript(project, zone, insName, scriptName, scriptContent string) 
 }
 
 func createImageMain(project, zone string) error {
-	ctx := context.Background()
-	service, err := compute.NewService(ctx)
+	h, err := gce.NewGceHelper(project, zone)
 	if err != nil {
-		return fmt.Errorf("failed to create service: %w", err)
-	}
-	gce := &gceHelper{
-		Service: service,
-		Project: project,
-		Zone:    zone,
+		return fmt.Errorf("failed to create GCE helper: %w", err)
 	}
 	insName := outImageName
 	attachedDiskName := fmt.Sprintf("%s-attached-disk", insName)
 	defer func() {
 		// DetachDisk
 		log.Printf("cleanup: detaching disk %q from instance %q...", attachedDiskName, insName)
-		if err := gce.DetachDisk(insName, attachedDiskName); err != nil {
+		if err := h.DetachDisk(insName, attachedDiskName); err != nil {
 			log.Printf("cleanup: error detaching disk: %v", err)
 		} else {
 			log.Println("cleanup: disk detached")
 		}
 		// Delete Instance
 		log.Printf("cleanup: deleting instance %q...", insName)
-		if err := gce.DeleteInstance(insName); err != nil {
+		if err := h.DeleteInstance(insName); err != nil {
 			log.Printf("cleanup: error deleting instance: %v", err)
 		} else {
 			log.Println("cleanup: instance deleted")
 		}
 		// Delete Disk
 		log.Printf("cleanup: deleting disk %q...", attachedDiskName)
-		if err := gce.DeleteDisk(attachedDiskName); err != nil {
+		if err := h.DeleteDisk(attachedDiskName); err != nil {
 			log.Printf("cleanup: error deleting disk: %v", err)
 		} else {
 			log.Println("cleanup: disk deleted")
 		}
 	}()
 	log.Println("creating disk...")
-	disk, err := gce.CreateDisk(debianSourceImageProject, debianSourceImage, attachedDiskName)
+	disk, err := h.CreateDisk(debianSourceImageProject, debianSourceImage, attachedDiskName)
 	if err != nil {
 		return fmt.Errorf("failed to create disk: %w", err)
 	}
 	log.Printf("disk created: %q", attachedDiskName)
 	log.Println("creating instance...")
-	ins, err := gce.CreateInstance(insName)
+	ins, err := h.CreateInstance(insName)
 	if err != nil {
 		return fmt.Errorf("failed to create instance: %w", err)
 	}
 	log.Printf("instance created: %q", insName)
 	log.Println("attaching disk...")
-	if err := gce.AttachDisk(insName, attachedDiskName); err != nil {
+	if err := h.AttachDisk(insName, attachedDiskName); err != nil {
 		log.Fatalf("failed to attach disk %q to instance %q: %v", disk.Name, ins.Name, err)
 	}
 	log.Println("disk attached")
@@ -307,10 +166,10 @@ func createImageMain(project, zone string) error {
 		return fmt.Errorf("waiting for instance error: %v", err)
 	}
 	log.Printf("deleting instance %q...", insName)
-	if err := gce.DeleteInstance(insName); err != nil {
+	if err := h.DeleteInstance(insName); err != nil {
 		return fmt.Errorf("error deleting instance: %v", err)
 	}
-	if err := gce.CreateImage(insName, attachedDiskName, outImageName); err != nil {
+	if err := h.CreateImage(insName, attachedDiskName, outImageName); err != nil {
 		return fmt.Errorf("failed to create image: %w", err)
 	}
 	return nil
