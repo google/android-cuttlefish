fc2a686f9252ad1eb3914aa2fc4c8fc92d535441
Seungjae Yoo <seungjaeyoo@google.com>
2025-08-28 08:53:07 +0900

    Separate pkg/gce/scripts/scripts.go into 2 golang files

--- tools/baseimage/cmd/create_gce_orchestration_image/main.go
+++ tools/baseimage/cmd/create_gce_orchestration_image/main.go
@@ -97,8 +97,8 @@ func createImageMain(project, zone string) error {
 	if err := gce.UploadBashScript(project, zone, insName, "remove_old_kernel.sh", scripts.RemoveOldKernel); err != nil {
 		return fmt.Errorf("error uploading remove_old_kernel.sh: %v", err)
 	}
-	if err := gce.UploadBashScript(project, zone, insName, "create_base_image.sh", scripts.CreateBaseImage); err != nil {
-		return fmt.Errorf("error uploading create_base_image.sh: %v", err)
+	if err := gce.UploadBashScript(project, zone, insName, "create_orchestration_image.sh", scripts.CreateOrchestrationImage); err != nil {
+		return fmt.Errorf("error uploading create_orchestration_image.sh: %v", err)
 	}
 	if err := gce.UploadBashScript(project, zone, insName, "install_nvidia.sh", scripts.InstallNvidia); err != nil {
 		return fmt.Errorf("error uploading install_nvidia.sh: %v", err)
@@ -114,7 +114,7 @@ func createImageMain(project, zone string) error {
 	if err := gce.RunCmd(project, zone, insName, "./remove_old_kernel.sh"); err != nil {
 		return err
 	}
-	if err := gce.RunCmd(project, zone, insName, "./create_base_image.sh"); err != nil {
+	if err := gce.RunCmd(project, zone, insName, "./create_orchestration_image.sh"); err != nil {
 		return err
 	}
 	// Reboot the instance to force a clean umount of the attached disk's file system.
