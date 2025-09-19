ff4a9a347af80299ba06080b266f41e116b44013
JaeMan Park <jaeman@google.com>
2025-09-19 16:34:18 +0900

    Add explicit `provideZoneChangeDetection` to providers
    for operator webui's test.ts

--- frontend/src/operator/webui/src/test.ts
+++ frontend/src/operator/webui/src/test.ts
@@ -1,14 +1,18 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
 import 'zone.js/testing';
+import {provideZoneChangeDetection, NgModule} from '@angular/core';
 import {getTestBed} from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
 
+@NgModule({ providers: [ provideZoneChangeDetection() ] })
+export class ZoneChangeDetectionModule {}
+
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
+  [ZoneChangeDetectionModule, BrowserDynamicTestingModule],
   platformBrowserDynamicTesting()
 );
