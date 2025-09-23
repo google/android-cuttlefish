da30d89613d2291c5b27e12554a5a2cc7d05fe59
Sergio Andres Rodriguez Orama <sorama@google.com>
2025-09-23 16:58:45 -0400

    Tool to create image with a given kernel version.
    Bug: b/446172232

--- tools/baseimage/pkg/gce/scripts/scripts.go
+++ tools/baseimage/pkg/gce/scripts/scripts.go
@@ -214,3 +214,46 @@ fi
 #  Sometimes systemd starts, making it hard to unmount
 #  In any case we'll unmount cleanly when the instance shuts down
 `
+
+const InstallKernelMain = `#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+
+if [[ $# -eq 0 ]] ; then
+  echo "usage: $0 <linux-image-deb>"
+  exit 1
+fi
+
+linux_image_deb=$1
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+./mount_attached_disk.sh
+
+version=$(sudo chroot /mnt/image/ /usr/bin/dpkg -s linux-image-cloud-amd64 | grep ^Depends: | \
+  cut -d: -f2 | cut -d" " -f2 )
+echo "START VERSION: ${version}"
+
+sudo chroot /mnt/image /usr/bin/apt-get update
+sudo chroot /mnt/image /usr/bin/apt-get upgrade -y
+
+version=$(sudo chroot /mnt/image/ /usr/bin/dpkg -s linux-image-cloud-amd64 | grep ^Depends: | \
+  cut -d: -f2 | cut -d" " -f2 )
+echo "AFTER UPGRADE VERSION: ${version}"
+
+sudo chroot /mnt/image /usr/bin/apt-get install -y ${linux_image_deb}
+
+version=$(sudo chroot /mnt/image/ /usr/bin/dpkg -s linux-image-cloud-amd64 | grep ^Depends: | \
+  cut -d: -f2 | cut -d" " -f2)
+echo "END VERSION: ${version}"
+
+if [ "${version}" != "${linux_image_deb}" ]; then
+  echo "CREATE IMAGE FAILED!!!"
+  echo "Expected ${linux_image_deb}, got: ${version}"
+  exit 1
+fi
+
+# Skip unmounting:
+#  Sometimes systemd starts, making it hard to unmount
+#  In any case we'll unmount cleanly when the instance shuts down
+`
