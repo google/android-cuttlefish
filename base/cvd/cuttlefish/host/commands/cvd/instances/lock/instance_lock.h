5cac5d726b7fb8b4501428a85395420e8455f0d2
3405691582 <dsk@google.com>
2025-10-01 18:05:37 +0000

    Fix a lifetime issue.
    The lock parameter path may not outlive the InstanceLockFileManager, so
    we shouldn't use a string_view at all here.

--- base/cvd/cuttlefish/host/commands/cvd/instances/lock/instance_lock.h
+++ base/cvd/cuttlefish/host/commands/cvd/instances/lock/instance_lock.h
@@ -47,7 +47,7 @@ class InstanceLockFileManager {
   using LockFileManager = cvd_impl::LockFileManager;
 
  public:
-  InstanceLockFileManager(std::string_view instance_locks_path);
+  InstanceLockFileManager(std::string instance_locks_path);
 
   Result<InstanceLockFile> AcquireLock(int instance_num);
   Result<std::set<InstanceLockFile>> AcquireLocks(const std::set<int>& nums);
@@ -72,7 +72,7 @@ class InstanceLockFileManager {
    */
   Result<std::set<int>> FindPotentialInstanceNumsFromNetDevices();
   Result<std::string> LockFilePath(int instance_num);
-  std::string_view instance_locks_path_;
+  std::string instance_locks_path_;
   std::optional<std::set<int>> all_instance_nums_;
   LockFileManager lock_file_manager_;
 };
