e538e67eb4be43e9ffc1e118dcdccae9dcb73b95
Chad Reynolds <chadreynolds@google.com>
2025-10-14 14:10:55 -0700

    Factor out metrics proto construction
    To keep the metrics orchestration scope narrow and with few dependencies.
    
    Bug: 450651404

---
+++ base/cvd/cuttlefish/host/libs/metrics/metrics_conversion.cc
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "cuttlefish/host/libs/metrics/metrics_conversion.h"
+
+#include <cstdint>
+
+#include "cuttlefish/common/libs/utils/host_info.h"
+#include "cuttlefish/host/commands/metrics/clearcut_protos.h"
+#include "cuttlefish/host/commands/metrics/events.h"
+#include "cuttlefish/host/commands/metrics/utils.h"
+#include "cuttlefish/host/libs/metrics/event_type.h"
+#include "external_proto/cf_guest.pb.h"
+#include "external_proto/cf_host.pb.h"
+#include "external_proto/cf_log.pb.h"
+#include "external_proto/cf_metrics_event_v2.pb.h"
+#include "external_proto/clientanalytics.pb.h"
+
+namespace cuttlefish {
+namespace {
+
+using logs::proto::wireless::android::cuttlefish::CuttlefishLogEvent;
+using logs::proto::wireless::android::cuttlefish::events::CuttlefishGuest;
+using logs::proto::wireless::android::cuttlefish::events::
+    CuttlefishGuest_EventType;
+using logs::proto::wireless::android::cuttlefish::events::CuttlefishHost;
+using logs::proto::wireless::android::cuttlefish::events::CuttlefishHost_OsType;
+using logs::proto::wireless::android::cuttlefish::events::MetricsEventV2;
+using wireless_android_play_playlog::LogEvent;
+using wireless_android_play_playlog::LogRequest;
+
+CuttlefishGuest_EventType ConvertEventType(EventType event_type) {
+  switch (event_type) {
+    case EventType::DeviceInstantiation:
+      return CuttlefishGuest_EventType::
+          CuttlefishGuest_EventType_CUTTLEFISH_GUEST_EVENT_TYPE_VM_INSTANTIATION;
+    case EventType::DeviceBootStart:
+      return CuttlefishGuest_EventType::
+          CuttlefishGuest_EventType_CUTTLEFISH_GUEST_EVENT_TYPE_UNSPECIFIED;
+    case EventType::DeviceBootComplete:
+      return CuttlefishGuest_EventType::
+          CuttlefishGuest_EventType_CUTTLEFISH_GUEST_EVENT_TYPE_DEVICE_BOOT_COMPLETED;
+    case EventType::DeviceStop:
+      return CuttlefishGuest_EventType::
+          CuttlefishGuest_EventType_CUTTLEFISH_GUEST_EVENT_TYPE_VM_STOP;
+  }
+}
+
+CuttlefishHost_OsType ConvertHostOs(const HostInfo& host_info) {
+  switch (host_info.os) {
+    case Os::Unknown:
+      return CuttlefishHost_OsType::
+          CuttlefishHost_OsType_CUTTLEFISH_HOST_OS_TYPE_UNSPECIFIED;
+    case Os::Linux:
+      switch (host_info.arch) {
+        case Arch::Arm:
+          return CuttlefishHost_OsType::
+              CuttlefishHost_OsType_CUTTLEFISH_HOST_OS_TYPE_LINUX_AARCH32;
+        case Arch::Arm64:
+          return CuttlefishHost_OsType::
+              CuttlefishHost_OsType_CUTTLEFISH_HOST_OS_TYPE_LINUX_AARCH64;
+        case Arch::RiscV64:
+          return CuttlefishHost_OsType::
+              CuttlefishHost_OsType_CUTTLEFISH_HOST_OS_TYPE_LINUX_RISCV64;
+        case Arch::X86:
+          return CuttlefishHost_OsType::
+              CuttlefishHost_OsType_CUTTLEFISH_HOST_OS_TYPE_LINUX_X86;
+        case Arch::X86_64:
+          return CuttlefishHost_OsType::
+              CuttlefishHost_OsType_CUTTLEFISH_HOST_OS_TYPE_LINUX_X86_64;
+      }
+  }
+}
+
+void PopulateMetricsEvent(EventType event_type,
+                          CuttlefishLogEvent& cf_log_event,
+                          const HostInfo& host_metrics,
+                          std::string_view session_id) {
+  cf_log_event.set_session_id(session_id);
+  MetricsEventV2* metrics_event = cf_log_event.mutable_metrics_event_v2();
+
+  CuttlefishGuest* guest = metrics_event->add_guest();
+  guest->set_event_type(ConvertEventType(event_type));
+  guest->set_guest_id(std::string(session_id) + "1");
+
+  CuttlefishHost* host = metrics_event->mutable_host();
+  host->set_host_os(ConvertHostOs(host_metrics));
+  host->set_host_os_version(host_metrics.release);
+}
+
+}  // namespace
+
+LogRequest ConstructLogRequest(EventType event_type,
+                               const HostInfo& host_metrics,
+                               std::string_view session_id) {
+  uint64_t now_ms = metrics::GetEpochTimeMs();
+  CuttlefishLogEvent cf_log_event = metrics::BuildCfLogEvent(now_ms);
+  PopulateMetricsEvent(event_type, cf_log_event, host_metrics, session_id);
+  LogEvent log_event = metrics::BuildLogEvent(now_ms, cf_log_event);
+  return metrics::BuildLogRequest(now_ms, std::move(log_event));
+}
+
+}  // namespace cuttlefish
---
+++ base/cvd/cuttlefish/host/libs/metrics/metrics_conversion.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <string_view>
+
+#include "cuttlefish/common/libs/utils/host_info.h"
+#include "cuttlefish/host/libs/metrics/event_type.h"
+#include "external_proto/clientanalytics.pb.h"
+
+namespace cuttlefish {
+
+wireless_android_play_playlog::LogRequest ConstructLogRequest(
+    EventType event_type, const HostInfo& host_metrics,
+    std::string_view session_id);
+
+}  // namespace cuttlefish
