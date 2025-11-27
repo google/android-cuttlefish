22d61fb1fd6e90a0c6bb18446137d7a0e657f65d
A. Cody Schuffelen <schuffelen@google.com>
2025-11-26 17:57:33 -0800

    Add VmManagerIs[Crosvm,Qemu,Gem5](VmmMode) utilities
    Bug: b/464101323

--- base/cvd/cuttlefish/host/libs/config/vmm_mode.h
+++ base/cvd/cuttlefish/host/libs/config/vmm_mode.h
@@ -36,6 +36,10 @@ std::ostream& operator<<(std::ostream&, VmmMode);
 std::string ToString(VmmMode mode);
 Result<VmmMode> ParseVmm(std::string_view);
 
+bool VmManagerIsCrosvm(VmmMode);
+bool VmManagerIsQemu(VmmMode);
+bool VmManagerIsGem5(VmmMode);
+
 }  // namespace cuttlefish
 
 template <>
