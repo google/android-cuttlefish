c30be71ae62181435c24bd10b2eb1bdd94ac4b14
A. Cody Schuffelen <schuffelen@google.com>
2025-12-22 14:09:11 -0800

    Move `cuttlefish/common/libs/utils:result*` to `cuttlefish/result:result*`
    The `common` naming was an artifact from a time when this code was used
    for both device and host builds. The new location is more central and
    shorter to include, which aligns with it being a pervasive type in the
    codebase.
    
    Creating a distinct directory gives more room to separate `result.h`
    into multiple files, with the following benefits:
    
    - Separating the `Result` type from the `EXPECT` logic allows depending
      on the `Result` type without depending on every type with special
      handling in the macros.
    - Separating out `CF_EXPECTF` makes it possible to depend on `Result`
      without depending on `@fmt`.
    - Separate files are easier to understand in isolation.
    
    The original headers and build targets are left as forwarding
    dependencies to the new location.
    
    Bug: b/471037778

--- base/cvd/cuttlefish/result/result.cc
+++ base/cvd/cuttlefish/result/result.cc
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "cuttlefish/common/libs/utils/result.h"
+#include "cuttlefish/result/result.h"
 
 #include <optional>
 #include <sstream>
