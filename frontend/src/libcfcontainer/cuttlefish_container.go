90fd022a0c3c53f692e32afb85c6225f93b241d9
Seungjae Yoo <seungjaeyoo@google.com>
2026-01-16 15:30:37 +0900

    Introduce initial impl of libcfcontainer with handling container images

---
+++ frontend/src/libcfcontainer/cuttlefish_container.go
@@ -0,0 +1,86 @@
+// Copyright (C) 2026 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package libcfcontainer
+
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/client"
+)
+
+type CuttlefishContainerManager interface {
+	// Check whether an image with the given name exists on the container engine or not
+	ImageExists(ctx context.Context, name string) (bool, error)
+	// Pull the container image
+	PullImage(ctx context.Context, name string) error
+}
+
+type CuttlefishContainerManagerOpts struct {
+	SockAddr string
+}
+
+type CuttlefishContainerManagerImpl struct {
+	cli *client.Client
+}
+
+func NewCuttlefishContainerManager(opts CuttlefishContainerManagerOpts) (*CuttlefishContainerManagerImpl, error) {
+	cliopts := []client.Opt{
+		client.WithAPIVersionNegotiation(),
+	}
+	if opts.SockAddr != "" {
+		cliopts = append(cliopts, client.WithHost(opts.SockAddr))
+	} else {
+		cliopts = append(cliopts, client.FromEnv)
+	}
+	cli, err := client.NewClientWithOpts(cliopts...)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create a new container engine client: %w", err)
+	}
+	return &CuttlefishContainerManagerImpl{
+		cli: cli,
+	}, nil
+}
+
+func (m *CuttlefishContainerManagerImpl) ImageExists(ctx context.Context, name string) (bool, error) {
+	listRes, err := m.cli.ImageList(ctx, image.ListOptions{})
+	if err != nil {
+		return false, fmt.Errorf("failed to list docker images: %w", err)
+	}
+	for _, image := range listRes {
+		for _, tag := range image.RepoTags {
+			if tag == name {
+				return true, nil
+			}
+		}
+	}
+	return false, nil
+}
+
+func (m *CuttlefishContainerManagerImpl) PullImage(ctx context.Context, name string) error {
+	reader, err := m.cli.ImagePull(ctx, name, image.PullOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to request pulling docker image %q: %w", name, err)
+	}
+	defer reader.Close()
+	// Caller of ImagePull should handle its output to complete actual ImagePull operation.
+	// Details in https://pkg.go.dev/github.com/docker/docker/client#Client.ImagePull.
+	if _, err := io.Copy(io.Discard, reader); err != nil {
+		return fmt.Errorf("failed to pull docker image %q: %w", name, err)
+	}
+	return nil
+}
---
+++ frontend/src/libcfcontainer/go.mod
@@ -0,0 +1,37 @@
+module github.com/google/android-cuttlefish/frontend/src/libcfcontainer
+
+go 1.24.0
+
+toolchain go1.24.8
+
+require github.com/docker/docker v28.5.2+incompatible
+
+require (
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/containerd/errdefs v1.0.0 // indirect
+	github.com/containerd/errdefs/pkg v0.3.0 // indirect
+	github.com/containerd/log v0.1.0 // indirect
+	github.com/distribution/reference v0.6.0 // indirect
+	github.com/docker/go-connections v0.6.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/moby/docker-image-spec v1.3.1 // indirect
+	github.com/moby/sys/atomicwriter v0.1.0 // indirect
+	github.com/moby/term v0.5.2 // indirect
+	github.com/morikuni/aec v1.1.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.64.0 // indirect
+	go.opentelemetry.io/otel v1.39.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.39.0 // indirect
+	go.opentelemetry.io/otel/metric v1.39.0 // indirect
+	go.opentelemetry.io/otel/trace v1.39.0 // indirect
+	golang.org/x/sys v0.39.0 // indirect
+	golang.org/x/time v0.14.0 // indirect
+	gotest.tools/v3 v3.5.2 // indirect
+)
