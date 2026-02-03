80aa258e04036e43953c64b2b975d66772ffd9e4
Chad Reynolds <chadreynolds@google.com>
2026-02-03 13:21:27 -0800

    Gather metrics data for `extra_kernel_cmdline`
    Not yet converted for transmission, but available.
    
    Bug: 476237221

--- base/cvd/cuttlefish/host/libs/metrics/flag_metrics.cc
+++ base/cvd/cuttlefish/host/libs/metrics/flag_metrics.cc
@@ -21,6 +21,7 @@
 #include "cuttlefish/host/commands/assemble_cvd/flags/cpus.h"
 #include "cuttlefish/host/commands/assemble_cvd/flags/daemon.h"
 #include "cuttlefish/host/commands/assemble_cvd/flags/data_policy.h"
+#include "cuttlefish/host/commands/assemble_cvd/flags/extra_kernel_cmdline.h"
 #include "cuttlefish/host/commands/assemble_cvd/flags/guest_enforce_security.h"
 #include "cuttlefish/host/commands/assemble_cvd/flags/memory_mb.h"
 #include "cuttlefish/host/commands/assemble_cvd/flags/restart_subprocesses.h"
@@ -36,17 +37,21 @@ Result<std::vector<FlagMetrics>> GetFlagMetrics(const int guest_count) {
   DaemonFlag daemon_values = CF_EXPECT(DaemonFlag::FromGlobalGflags());
   DataPolicyFlag data_policy_values =
       CF_EXPECT(DataPolicyFlag::FromGlobalGflags());
+  ExtraKernelCmdlineFlag extra_kernel_cmdline_value =
+      ExtraKernelCmdlineFlag::FromGlobalGflags();
   GuestEnforceSecurityFlag guest_enforce_security_values =
       CF_EXPECT(GuestEnforceSecurityFlag::FromGlobalGflags());
+  MemoryMbFlag memory_mb_values = CF_EXPECT(MemoryMbFlag::FromGlobalGflags());
   RestartSubprocessesFlag restart_subprocesses_values =
       CF_EXPECT(RestartSubprocessesFlag::FromGlobalGflags());
-  MemoryMbFlag memory_mb_values = CF_EXPECT(MemoryMbFlag::FromGlobalGflags());
 
   for (int i = 0; i < guest_count; i++) {
     result.emplace_back(FlagMetrics{
         .cpus = cpus_values.ForIndex(i),
         .daemon = daemon_values.ForIndex(i),
         .data_policy = data_policy_values.ForIndex(i),
+        // the value is used for all guests
+        .extra_kernel_cmdline = extra_kernel_cmdline_value.ForIndex(0),
         .guest_enforce_security = guest_enforce_security_values.ForIndex(i),
         .memory_mb = memory_mb_values.ForIndex(i),
         .restart_subprocesses = restart_subprocesses_values.ForIndex(i),
