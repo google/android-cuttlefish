0826a9e2de94554c5168aad84e6fb9491fba5a7d
Chad Reynolds <chadreynolds@google.com>
2026-02-03 16:56:46 -0800

    Add new enum for gpu mode flag
    To be used as a target for converting the flag's string value.
    
    Bug: 476237139

---
+++ base/cvd/cuttlefish/host/libs/config/gpu_mode.cc
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "cuttlefish/host/libs/config/gpu_mode.h"
+
+#include <string>
+#include <string_view>
+
+#include "cuttlefish/result/result.h"
+
+namespace cuttlefish {
+
+namespace {
+
+constexpr std::string_view kAuto = "auto";
+constexpr std::string_view kCustom = "custom";
+constexpr std::string_view kDrmVirgl = "drm_virgl";
+constexpr std::string_view kGfxstream = "gfxstream";
+constexpr std::string_view kGfxstreamGuestAngle = "gfxstream_guest_angle";
+constexpr std::string_view kGfxstreamGuestAngleHostSwiftshader =
+    "gfxstream_guest_angle_host_swiftshader";
+constexpr std::string_view kGfxstreamGuestAngleHostLavapipe =
+    "gfxstream_guest_angle_host_lavapipe";
+constexpr std::string_view kGuestSwiftshader = "guest_swiftshader";
+constexpr std::string_view kNone = "none";
+
+}  // namespace
+
+Result<GpuMode> GpuModeFromString(std::string_view mode) {
+  if (mode == kAuto) {
+    return GpuMode::Auto;
+  } else if (mode == kCustom) {
+    return GpuMode::Custom;
+  } else if (mode == kDrmVirgl) {
+    return GpuMode::DrmVirgl;
+  } else if (mode == kGfxstream) {
+    return GpuMode::Gfxstream;
+  } else if (mode == kGfxstreamGuestAngle) {
+    return GpuMode::GfxstreamGuestAngle;
+  } else if (mode == kGfxstreamGuestAngleHostSwiftshader) {
+    return GpuMode::GfxStreamGuestAngleHostSwiftshader;
+  } else if (mode == kGfxstreamGuestAngleHostLavapipe) {
+    return GpuMode::GfxStreamGuestAngleHostLavapipe;
+  } else if (mode == kGuestSwiftshader) {
+    return GpuMode::GuestSwiftshader;
+  } else if (mode == kNone) {
+    return GpuMode::None;
+  } else {
+    return CF_ERRF("Invalid gpu_mode provided: ", mode);
+  }
+}
+
+std::string GpuModeString(GpuMode mode) {
+  switch (mode) {
+    case GpuMode::Auto:
+      return std::string(kAuto);
+    case GpuMode::Custom:
+      return std::string(kCustom);
+    case GpuMode::DrmVirgl:
+      return std::string(kDrmVirgl);
+    case GpuMode::Gfxstream:
+      return std::string(kGfxstream);
+    case GpuMode::GfxstreamGuestAngle:
+      return std::string(kGfxstreamGuestAngle);
+    case GpuMode::GfxStreamGuestAngleHostLavapipe:
+      return std::string(kGfxstreamGuestAngleHostLavapipe);
+    case GpuMode::GfxStreamGuestAngleHostSwiftshader:
+      return std::string(kGfxstreamGuestAngleHostSwiftshader);
+    case GpuMode::GuestSwiftshader:
+      return std::string(kGuestSwiftshader);
+    case GpuMode::None:
+      return std::string(kNone);
+  }
+}
+
+}  // namespace cuttlefish
