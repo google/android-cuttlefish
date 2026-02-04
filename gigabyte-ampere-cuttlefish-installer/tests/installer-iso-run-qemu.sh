3f7dc4ed6296a86545f0374ae8c81041a1464646
Seungjae Yoo <seungjaeyoo@google.com>
2026-02-04 10:56:28 +0900

    Try acceleration

--- gigabyte-ampere-cuttlefish-installer/tests/installer-iso-run-qemu.sh
+++ gigabyte-ampere-cuttlefish-installer/tests/installer-iso-run-qemu.sh
@@ -39,7 +39,8 @@ for DISK1 in uboot_qemu_disk*.img; do
 done
 
 qemu-system-aarch64 -machine virt \
-     -cpu cortex-a57 \
+     -cpu host \
+     -enable-kvm \
      -nographic \
      -netdev user,id=net0,hostfwd=tcp::35555-:5555,hostfwd=tcp::33322-:22 \
      -device virtio-net-pci,mac=50:54:00:00:00:56,netdev=net0,id=net0-dev \
