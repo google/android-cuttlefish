67df05840fc23d073276037d1da379a3c1fb165b
Seungjae Yoo <seungjaeyoo@google.com>
2026-02-09 16:00:34 +0900

    Refactor: Handle a set of subcommands on a separated function

--- frontend/src/podcvd/main.go
+++ frontend/src/podcvd/main.go
@@ -18,8 +18,59 @@ import (
 	"context"
 	"log"
 
+	"github.com/google/android-cuttlefish/frontend/src/libcfcontainer"
 	"github.com/google/android-cuttlefish/frontend/src/podcvd/internal"
 )
+
+func handleSubcommandsForSingleInstanceGroup(ccm libcfcontainer.CuttlefishContainerManager, cvdArgs *internal.CvdArgs) error {
+	subcommand := cvdArgs.SubCommandArgs[0]
+	switch subcommand {
+	case "create":
+		if err := internal.CreateCuttlefishHost(ccm, cvdArgs.CommonArgs); err != nil {
+			return err
+		}
+	default:
+		// TODO(seungjaeyoo): Validate group name argument.
+	}
+	switch subcommand {
+	case "remove", "stop":
+		args := append([]string{"cvd"}, cvdArgs.SerializeCommonArgs()...)
+		args = append(args, "fleet")
+		stdout, err := ccm.ExecOnContainer(context.Background(), internal.ContainerName(cvdArgs.CommonArgs.GroupName), false, args)
+		if err != nil {
+			return err
+		}
+		instanceGroup, err := internal.ParseInstanceGroups(stdout, cvdArgs.CommonArgs.GroupName)
+		if err != nil {
+			return err
+		}
+		if err := internal.DisconnectAdb(ccm, *instanceGroup); err != nil {
+			return err
+		}
+		// If the subcommand is 'remove', it doesn't need to execute cvd on the
+		// container instance as it should be removed in the end.
+		if subcommand == "remove" {
+			return internal.DeleteCuttlefishHost(ccm, cvdArgs.CommonArgs.GroupName)
+		}
+	}
+	args := append([]string{"cvd"}, cvdArgs.SerializeCommonArgs()...)
+	args = append(args, cvdArgs.SubCommandArgs...)
+	stdout, err := ccm.ExecOnContainer(context.Background(), internal.ContainerName(cvdArgs.CommonArgs.GroupName), true, args)
+	if err != nil {
+		return err
+	}
+	switch subcommand {
+	case "create", "start":
+		instanceGroup, err := internal.ParseInstanceGroup(stdout, cvdArgs.CommonArgs.GroupName)
+		if err != nil {
+			return err
+		}
+		if err := internal.ConnectAdb(ccm, *instanceGroup); err != nil {
+			return err
+		}
+	}
+	return nil
+}
 
 func main() {
 	cvdArgs := internal.ParseCvdArgs()
@@ -27,67 +78,20 @@ func main() {
 		// TODO(seungjaeyoo): Support execution without any subcommand
 		log.Fatal("execution without any subcommand is not implemented yet")
 	}
-	subcommand := cvdArgs.SubCommandArgs[0]
-	switch subcommand {
-	case "bugreport", "create", "display", "env", "powerbtn", "powerwash", "remove", "restart", "resume", "screen_recording", "snapshot_take", "start", "status", "stop", "suspend":
-		// These are supported subcommands on podcvd.
-	default:
-		// TODO(seungjaeyoo): Support other subcommands of cvd as well.
-		log.Fatalf("subcommand %q is not implemented yet", subcommand)
-	}
 
 	ccm, err := internal.CuttlefishContainerManager()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	subcommand := cvdArgs.SubCommandArgs[0]
 	switch subcommand {
-	case "create":
-		if err := internal.CreateCuttlefishHost(ccm, cvdArgs.CommonArgs); err != nil {
+	case "bugreport", "create", "display", "env", "powerbtn", "powerwash", "remove", "restart", "resume", "screen_recording", "snapshot_take", "start", "status", "stop", "suspend":
+		if err := handleSubcommandsForSingleInstanceGroup(ccm, cvdArgs); err != nil {
 			log.Fatal(err)
 		}
 	default:
-		// TODO(seungjaeyoo): Validate group name argument.
-	}
-
-	switch subcommand {
-	case "remove", "stop":
-		args := append([]string{"cvd"}, cvdArgs.SerializeCommonArgs()...)
-		args = append(args, "fleet")
-		stdout, err := ccm.ExecOnContainer(context.Background(), internal.ContainerName(cvdArgs.CommonArgs.GroupName), false, args)
-		if err != nil {
-			log.Fatal(err)
-		}
-		instanceGroup, err := internal.ParseInstanceGroups(stdout, cvdArgs.CommonArgs.GroupName)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if err := internal.DisconnectAdb(ccm, *instanceGroup); err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	if subcommand == "remove" {
-		if err := internal.DeleteCuttlefishHost(ccm, cvdArgs.CommonArgs.GroupName); err != nil {
-			log.Fatal(err)
-		}
-		return
-	}
-
-	args := append([]string{"cvd"}, cvdArgs.SerializeCommonArgs()...)
-	args = append(args, cvdArgs.SubCommandArgs...)
-	stdout, err := ccm.ExecOnContainer(context.Background(), internal.ContainerName(cvdArgs.CommonArgs.GroupName), true, args)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	switch subcommand {
-	case "create", "start":
-		instanceGroup, err := internal.ParseInstanceGroup(stdout, cvdArgs.CommonArgs.GroupName)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if err := internal.ConnectAdb(ccm, *instanceGroup); err != nil {
-			log.Fatal(err)
-		}
+		// TODO(seungjaeyoo): Support other subcommands of cvd as well.
+		log.Fatalf("subcommand %q is not implemented yet", subcommand)
 	}
 }
