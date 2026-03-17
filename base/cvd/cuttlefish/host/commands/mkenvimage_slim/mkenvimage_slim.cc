57cec88138d759f3928f44f6fef1291305127169
A. Cody Schuffelen <schuffelen@google.com>
2026-03-16 17:35:02 -0700

    Move the functionality of mkenvimage_slim into a library
    Bug: b/493346533

--- base/cvd/cuttlefish/host/commands/mkenvimage_slim/mkenvimage_slim.cc
+++ base/cvd/cuttlefish/host/commands/mkenvimage_slim/mkenvimage_slim.cc
@@ -17,32 +17,19 @@
 // https://github.com/u-boot/u-boot/blob/master/tools/mkenvimage.c The bare
 // minimum amount of functionality for our application is replicated.
 
-#include <stdint.h>
 #include <stdlib.h>
-#include <string.h>
 
-#include <zlib.h>
-
-#include <vector>
-
-#include <gflags/gflags.h>
 #include "absl/log/log.h"
+#include "gflags/gflags.h"
 
-#include "cuttlefish/common/libs/fs/shared_buf.h"
-#include "cuttlefish/common/libs/fs/shared_fd.h"
-#include "cuttlefish/common/libs/utils/files.h"
 #include "cuttlefish/common/libs/utils/tee_logging.h"
+#include "cuttlefish/host/libs/config/mkenvimage_slim.h"
 #include "cuttlefish/result/result.h"
-
-#define PAD_VALUE (0xff)
-#define CRC_SIZE (sizeof(uint32_t))
-
-// One NULL needed at the end of the env.
-#define NULL_PAD_LENGTH (1)
 
 DEFINE_int32(env_size, 4096, "file size of resulting env");
 DEFINE_string(output_path, "", "output file path");
 DEFINE_string(input_path, "", "input file path");
+
 namespace cuttlefish {
 
 static constexpr char kUsageMessage[] =
@@ -53,7 +40,7 @@ static constexpr char kUsageMessage[] =
     "output_path - path to write resulting environment image including CRC "
     "to\n";
 
-Result<int> MkenvimageSlimMain(int argc, char** argv) {
+Result<void> MkenvimageSlimMain(int argc, char** argv) {
   cuttlefish::LogToStderr();
   gflags::SetUsageMessage(kUsageMessage);
   gflags::ParseCommandLineFlags(&argc, &argv, true);
@@ -61,40 +48,18 @@ Result<int> MkenvimageSlimMain(int argc, char** argv) {
   CF_EXPECT(FLAGS_env_size != 0, "env size can't be 0.");
   CF_EXPECT(!(FLAGS_env_size % 512), "env size must be multiple of 512.");
 
-  std::string env_readout = ReadFile(FLAGS_input_path);
-  CF_EXPECT(env_readout.length(), "Input env is empty");
   CF_EXPECT(
-      env_readout.length() <= (FLAGS_env_size - CRC_SIZE - NULL_PAD_LENGTH),
-      "Input env must fit within env_size specified.");
-
-  std::vector<uint8_t> env_buffer(FLAGS_env_size, PAD_VALUE);
-  uint8_t* env_ptr = env_buffer.data() + CRC_SIZE;
-  memcpy(env_ptr, env_readout.c_str(), FileSize(FLAGS_input_path));
-  env_ptr[env_readout.length()] = 0;  // final byte after the env must be NULL
-  uint32_t crc = crc32(0, env_ptr, FLAGS_env_size - CRC_SIZE);
-  memcpy(env_buffer.data(), &crc, sizeof(uint32_t));
-
-  auto output_fd =  // NOLINTNEXTLINE(misc-include-cleaner)
-      SharedFD::Creat(FLAGS_output_path, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
-  if (!output_fd->IsOpen()) {
-    return CF_ERR("Couldn't open the output file " + FLAGS_output_path);
-  } else if (FLAGS_env_size !=
-             WriteAll(output_fd, (char*)env_buffer.data(), FLAGS_env_size)) {
-    if (Result<void> res = RemoveFile(FLAGS_output_path); !res.ok()) {
-      LOG(ERROR) << res.error();
-    }
-    return CF_ERR("Couldn't complete write to " + FLAGS_output_path);
-  }
-
-  return 0;
+      MkenvimageSlim(FLAGS_input_path, FLAGS_output_path, FLAGS_env_size));
+  return {};
 }
 }  // namespace cuttlefish
 
 int main(int argc, char** argv) {
-  auto res = cuttlefish::MkenvimageSlimMain(argc, argv);
-  if (res.ok()) {
-    return *res;
+  if (cuttlefish::Result<void> res = cuttlefish::MkenvimageSlimMain(argc, argv);
+      res.ok()) {
+    return 0;
+  } else {
+    LOG(ERROR) << "mkenvimage_slim failed: \n" << res.error();
+    return 1;
   }
-  LOG(ERROR) << "mkenvimage_slim failed: \n" << res.error();
-  abort();
 }
