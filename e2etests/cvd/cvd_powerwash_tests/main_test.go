c10b59e59140db73bd92797a26df0abe1eb314a8
Jason Macnak <natsu@google.com>
2026-03-26 07:44:08 -0700

    Add a powerwash e2etest
    Bug: b/360440269
    Test: cd e2etests && bazel test cvd/cvd_powerwash_tests/...

---
+++ e2etests/cvd/cvd_powerwash_tests/main_test.go
@@ -0,0 +1,73 @@
+// Copyright (C) 2026 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/google/android-cuttlefish/e2etests/cvd/common"
+)
+
+func TestCvdPowerwash(t *testing.T) {
+	testcases := []struct {
+		branch string
+		target string
+	}{
+		{
+			branch: "aosp-android-latest-release",
+			target: "aosp_cf_x86_64_only_phone-userdebug",
+		},
+	}
+	c := e2etests.TestContext{}
+	for _, tc := range testcases {
+		t.Run(fmt.Sprintf("BUILD=%s/%s", tc.branch, tc.target), func(t *testing.T) {
+			c.SetUp(t)
+			defer c.TearDown()
+
+			if err := c.CVDFetch(e2etests.FetchArgs{
+				DefaultBuildBranch: tc.branch,
+				DefaultBuildTarget: tc.target,
+			}); err != nil {
+				t.Fatal(err)
+			}
+
+			if err := c.CVDCreate(e2etests.CreateArgs{}); err != nil {
+				t.Fatal(err)
+			}
+
+			if err := c.RunAdbWaitForDevice(); err != nil {
+				t.Fatalf("failed to wait for Cuttlefish device to connect to adb: %w", err)
+			}
+
+			const tmpFile = "/data/local/tmp/foo"
+			if _, err := c.RunCmd("adb", "shell", "touch", tmpFile); err != nil {
+				t.Fatalf("failed to create %s: %w", tmpFile, err)
+			}
+
+			if _, err := c.RunCmd("adb", "shell", "stat", tmpFile); err != nil {
+				t.Fatal("failed to verify %s created: %w", err)
+			}
+
+			if err := c.CVDPowerwash(); err != nil {
+				t.Fatal(err)
+			}
+
+			if _, err := c.RunCmd("adb", "shell", "stat", tmpFile); err == nil {
+				t.Fatal("failed to powerwash, %s still exists")
+			}
+		})
+	}
+}
