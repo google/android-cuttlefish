3d7baafca017889fd7cc17a6a72e9e69b05ada76
Ying-Chun Liu (PaulLiu) <paulliu@debian.org>
2026-03-30 23:39:53 +0100

    cuttlefish-integration-gigabyte-arm64: fix NetworkManager connections.
    Previously we use dhclient to workaround this issue. But the
    actual problem is the NetworkManager is stucked by a wrong
    configuration of "Wired connection 1".
    
    Since dhclient is deprecated, we wrote a script to replace the
    dhclient's functionality. Actually the script will fix the
    NetworkManager. And once it run, the NetworkManager will be well
    without this script to run again. But calling this script multiple
    times doesn't harm either because once the connection is being setup,
    the script won't touch those connected devices.
    
    Signed-off-by: Ying-Chun Liu (PaulLiu) <paulliu@debian.org>

--- cuttlefish-integration-gigabyte-arm64/debian/cuttlefish-integration-gigabyte-arm64.cron.d
+++ cuttlefish-integration-gigabyte-arm64/debian/cuttlefish-integration-gigabyte-arm64.cron.d
@@ -2,4 +2,4 @@
 # Regular cron jobs for the cuttlefish-integration-gigabyte-arm64 package.
 #
 # Workarounds for Network-Manager
-@reboot root sleep 100 && /usr/sbin/dhclient
+@reboot root sleep 100 && /usr/bin/gig_amp_fix_nm_config
