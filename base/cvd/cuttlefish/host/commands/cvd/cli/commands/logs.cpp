65ff2b28b1e19e00733351438e15e99f52796f58
Sergio Andres Rodriguez Orama <sorama@google.com>
2026-04-29 16:15:05 -0400

    Boilerplate for new subcommand: "logs"
    Bug: b/507588992

---
+++ base/cvd/cuttlefish/host/commands/cvd/cli/commands/logs.cpp
@@ -0,0 +1,51 @@
+#include "cuttlefish/host/commands/cvd/cli/commands/logs.h"
+
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "cuttlefish/host/commands/cvd/cli/command_request.h"
+#include "cuttlefish/host/commands/cvd/cli/commands/command_handler.h"
+#include "cuttlefish/host/commands/cvd/cli/types.h"
+#include "cuttlefish/result/result.h"
+
+namespace cuttlefish {
+namespace {
+
+constexpr char kSummaryHelpText[] = "Print device logs";
+
+constexpr char kDetailedHelpText[] = R"(
+usage: cvd [--group_name name [--instance_name name]] logs
+)";
+
+class CvdLogsHandler : public CvdCommandHandler {
+ public:
+  CvdLogsHandler() = default;
+
+  Result<void> Handle(const CommandRequest& request) override {
+    CF_EXPECT(CanHandle(request));
+    std::cout << "hello, logs\n";
+    return {};
+  }
+
+  cvd_common::Args CmdList() const override { return {"logs"}; }
+
+  Result<std::string> SummaryHelp() const override { return kSummaryHelpText; }
+
+  bool ShouldInterceptHelp() const override { return true; }
+
+  bool RequiresDeviceExists() const override { return true; }
+
+  Result<std::string> DetailedHelp(std::vector<std::string>&) const override {
+    return kDetailedHelpText;
+  }
+};
+
+}  // namespace
+
+std::unique_ptr<CvdCommandHandler> NewCvdLogsHandler() {
+  return std::unique_ptr<CvdCommandHandler>(new CvdLogsHandler());
+}
+
+}  // namespace cuttlefish
