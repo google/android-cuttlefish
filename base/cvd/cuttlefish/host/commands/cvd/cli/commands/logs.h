88596a25b6d4920be5b0b9ffb02e4716e7cb5480
Sergio Andres Rodriguez Orama <sorama@google.com>
2026-04-29 16:42:22 -0400

    Fail if using invalid selector.
    Bug: b/507588992

--- base/cvd/cuttlefish/host/commands/cvd/cli/commands/logs.h
+++ base/cvd/cuttlefish/host/commands/cvd/cli/commands/logs.h
@@ -3,9 +3,11 @@
 #include <memory>
 
 #include "cuttlefish/host/commands/cvd/cli/commands/command_handler.h"
+#include "cuttlefish/host/commands/cvd/instances/instance_manager.h"
 
 namespace cuttlefish {
 
-std::unique_ptr<CvdCommandHandler> NewCvdLogsHandler();
+std::unique_ptr<CvdCommandHandler> NewCvdLogsHandler(
+    InstanceManager& instance_manager);
 
 }  // namespace cuttlefish
