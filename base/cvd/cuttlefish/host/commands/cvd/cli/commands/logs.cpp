f468f2145630c06cbdfe5cd31eebc50819fe2543
Sergio Andres Rodriguez Orama <sorama@google.com>
2026-04-30 13:30:04 -0400

    Print given log name.
    Bug: b/507588992

--- base/cvd/cuttlefish/host/commands/cvd/cli/commands/logs.cpp
+++ base/cvd/cuttlefish/host/commands/cvd/cli/commands/logs.cpp
@@ -8,6 +8,8 @@
 
 #include <android-base/file.h>
 
+#include "cuttlefish/common/libs/utils/flag_parser.h"
+#include "cuttlefish/common/libs/utils/subprocess.h"
 #include "cuttlefish/host/commands/cvd/cli/command_request.h"
 #include "cuttlefish/host/commands/cvd/cli/commands/command_handler.h"
 #include "cuttlefish/host/commands/cvd/cli/selector/selector.h"
@@ -22,8 +24,41 @@ namespace {
 constexpr char kSummaryHelpText[] = "Print device logs";
 
 constexpr char kDetailedHelpText[] = R"(
-usage: cvd [--group_name name [--instance_name name]] logs
+usage: cvd [--group_name name [--instance_name name]] logs [-p|--print name]
 )";
+
+struct LogsCmdOptions {
+  std::string print_target;
+
+  std::vector<Flag> Flags() {
+    return {
+        GflagsCompatFlag("print", print_target),
+        GflagsCompatFlag("p", print_target),
+    };
+  }
+};
+
+Result<void> PrintLogsList(const std::string& dir) {
+  auto callback = [](const std::string& filename) -> Result<void> {
+    constexpr int kMaxPadding = 30;
+    std::string basename = android::base::Basename(filename);
+    std::cout << basename;
+    std::cout << std::string(std::max(int(kMaxPadding - basename.length()), 1), ' ');
+    std::cout << filename;
+    std::cout << std::endl;
+    return {};
+  };
+  return WalkDirectory(dir, callback);
+}
+
+Result<void> PrintLog(const std::string& filename) {
+  Command cat("cat");
+  cat.AddParameter(filename);
+  int exit_code = cat.Start().Wait();
+  CF_EXPECTF(exit_code == 0, "{} {} failed: exited with status {}",
+             cat.Executable(), filename, exit_code);
+  return {};
+}
 
 class CvdLogsHandler : public CvdCommandHandler {
  public:
@@ -36,25 +71,22 @@ class CvdLogsHandler : public CvdCommandHandler {
         CF_EXPECT(selector::SelectInstance(instance_manager_, request),
                   "Unable to select an instance");
 
+    LogsCmdOptions opts;
+    std::vector<std::string> args = request.SubcommandArguments();
+    CF_EXPECT(ConsumeFlags(opts.Flags(), args));
+
     std::string dir = instance.instance_dir();
     std::string logs_dir = dir + "/logs";
-
     CF_EXPECT(FileExists(logs_dir));
     CF_EXPECT(IsDirectory(logs_dir));
 
-    auto callback = [](const std::string& filename) -> Result<void> {
-      constexpr int kMaxPadding = 30;
-      std::string basename = android::base::Basename(filename);
-      std::cout << basename;
-      std::cout << std::string(std::max(int(kMaxPadding - basename.length()), 1), ' ');
-      std::cout << filename;
-      std::cout << std::endl;
-      return {};
-    };
+    if (opts.print_target.empty()) {
+      return PrintLogsList(logs_dir);
+    }
 
-    CF_EXPECT(WalkDirectory(logs_dir, callback));
-
-    return {};
+    std::string print_target = logs_dir + "/" + opts.print_target;
+    CF_EXPECT(FileExists(print_target));
+    return PrintLog(print_target);
   }
 
   cvd_common::Args CmdList() const override { return {"logs"}; }
