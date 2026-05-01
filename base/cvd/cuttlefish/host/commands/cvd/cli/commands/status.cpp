afe9c96a4ac5a2da048ad083a73c7355448c1dae
Jorge E. Moreira <jemoreira@google.com>
2026-05-01 16:15:10 -0700

    Remove unnecessary help flag handling
    The help flag is intercepted before the subcommand handler is invoked so
    it will never see it from its Handle method, only from DetailedHelp.

--- base/cvd/cuttlefish/host/commands/cvd/cli/commands/status.cpp
+++ base/cvd/cuttlefish/host/commands/cvd/cli/commands/status.cpp
@@ -104,7 +104,7 @@ Result<StatusCommandOptions> ParseFlags(cvd_common::Args& args) {
       GflagsCompatFlag("wait_for_launcher", ret.wait_for_launcher_seconds),
       GflagsCompatFlag("instance_name", ret.instance_name),
       GflagsCompatFlag("print", ret.print),
-      GflagsCompatFlag("help", ret.help),
+
   };
 
   CF_EXPECT(ConsumeFlags(flags, args));
@@ -144,11 +144,6 @@ Result<void> CvdStatusCommandHandler::Handle(const CommandRequest& request) {
 
   std::vector<std::string> cmd_args = request.SubcommandArguments();
   StatusCommandOptions flags = CF_EXPECT(ParseFlags(cmd_args));
-
-  if (flags.help) {
-    std::cout << kDetailedHelpText << std::endl;
-    return {};
-  }
 
   if (!CF_EXPECT(instance_manager_.HasInstanceGroups())) {
     return CF_ERR(NoGroupMessage(request));
