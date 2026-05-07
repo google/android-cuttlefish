eb42c83eae165bf5bd0d990cee2930e5a0867c8d
Sergio Andres Rodriguez Orama <sorama@google.com>
2026-05-07 15:17:23 -0400

    Update logs e2e tests with group-level logs.
    - Use cvd load to have `fetch.log` available.
    
    Bug: b/509586405

--- e2etests/cvd/logs_tests/main_test.go
+++ e2etests/cvd/logs_tests/main_test.go
@@ -23,67 +23,91 @@ import (
 )
 
 func TestPrintLogs(t *testing.T) {
-	testcases := []struct {
-		branch string
-		target string
-	}{
-		{
-			branch: "git_main",
-			target: "aosp_cf_x86_64_only_phone-trunk_staging-userdebug",
-		},
-	}
 	c := e2etests.TestContext{}
-	for _, tc := range testcases {
-		t.Run("foo", func(t *testing.T) {
-			c.SetUp(t)
-			defer c.TearDown()
+	c.SetUp(t)
+	defer c.TearDown()
 
-			if err := c.CVDFetch(e2etests.FetchArgs{
-				DefaultBuildBranch: tc.branch,
-				DefaultBuildTarget: tc.target,
-			}); err != nil {
-				t.Fatal(err)
-			}
+	env_config := `
+{
+  "instances": [
+    {
+      "name": "ins-1",
+      "disk": {
+        "default_build": "@ab\/git_main\/aosp_cf_x86_64_only_phone-trunk_staging-userdebug"
+      },
+      "vm": {
+        "cpus": 4,
+        "memory_mb": 4096,
+        "setupwizard_mode": "REQUIRED"
+      },
+      "graphics": {
+        "displays": [
+          {
+            "width": 720,
+            "height": 1280,
+            "dpi": 140,
+            "refresh_rate_hertz": 60
+          }
+        ],
+        "record_screen": false
+      }
+    }
+  ],
+  "netsim_bt": false,
+  "metrics": {
+    "enable": true
+  },
+  "common": {
+    "host_package": "@ab\/git_main\/aosp_cf_x86_64_only_phone-trunk_staging-userdebug"
+  }
+}
+`
 
-			if err := c.CVDCreate(e2etests.CreateArgs{}); err != nil {
-				t.Fatal(err)
-			}
-
-			res, err := c.RunCmd(c.TargetBin(), "logs")
-			if err != nil {
-				t.Fatal(err)
-			}
-			stdOut := res.Stdout
-
-			listedNames := []string{}
-			lines := strings.Split(stdOut, "\n")
-			for _, line := range lines[:len(lines)-1] {
-				fields := strings.Fields(line)
-				if len(fields) != 2 {
-					t.Errorf("line %q does not match format '<NAME> <PATH>'", line)
-				}
-				res, err := c.RunCmd("stat", fields[1])
-				if err != nil {
-					t.Fatal(res.Stderr)
-				}
-				listedNames = append(listedNames, fields[0])
-			}
-			keyExpectedNames := []string{"logcat", "launcher.log", "kernel.log"}
-			for _, name := range keyExpectedNames {
-				if !slices.Contains(listedNames, name) {
-					t.Fatalf("missing log name: %q", name)
-				}
-			}
-
-			res, err = c.RunCmd(c.TargetBin(), "logs", "--print", "launcher.log")
-			if err != nil {
-				t.Fatal(err)
-			}
-			stdOut = res.Stdout
-			if len(stdOut) == 0 {
-				t.Fatalf("empty launcher.log")
-			}
-
-		})
+	if err := c.CVDLoad(e2etests.LoadArgs{LoadConfig: env_config}); err != nil {
+		t.Fatal(err)
 	}
+
+	res, err := c.RunCmd(c.TargetBin(), "logs")
+	if err != nil {
+		t.Fatal(err)
+	}
+	stdOut := res.Stdout
+
+	listedNames := []string{}
+	lines := strings.Split(stdOut, "\n")
+	for _, line := range lines[:len(lines)-1] {
+		fields := strings.Fields(line)
+		if len(fields) != 2 {
+			t.Errorf("line %q does not match format '<NAME> <PATH>'", line)
+		}
+		res, err := c.RunCmd("stat", fields[1])
+		if err != nil {
+			t.Fatal(res.Stderr)
+		}
+		listedNames = append(listedNames, fields[0])
+	}
+	keyExpectedNames := []string{
+		// Group-level logs
+		"assemble_cvd.log",
+		"fetch.log",
+		// Instance-level logs
+		"kernel.log",
+		"launcher.log",
+		"logcat",
+	}
+	for _, name := range keyExpectedNames {
+		if !slices.Contains(listedNames, name) {
+			t.Fatalf("missing log name: %q", name)
+		}
+	}
+
+	res, err = c.RunCmd(c.TargetBin(), "logs", "--print", "launcher.log")
+	if err != nil {
+		t.Fatal(err)
+	}
+	stdOut = res.Stdout
+	if len(stdOut) == 0 {
+		t.Fatalf("empty launcher.log")
+	}
+
 }
