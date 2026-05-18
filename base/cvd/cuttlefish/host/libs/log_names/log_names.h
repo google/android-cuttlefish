a55e7fdffe121bc75c47f23c2f46c0b4c5f14360
Sergio Andres Rodriguez Orama <sorama@google.com>
2026-05-18 12:50:31 -0400

    log_names: add build_info log name
    Bug: b/511186368

--- base/cvd/cuttlefish/host/libs/log_names/log_names.h
+++ base/cvd/cuttlefish/host/libs/log_names/log_names.h
@@ -19,6 +19,7 @@ namespace cuttlefish {
 
 // Per instance log names.
 inline constexpr char kLogNameAssembleCvd[] = "assemble_cvd.log";
+inline constexpr char kLogNameBuildInfo[] = "build_info.log";
 inline constexpr char kLogNameCrosvmOpenWrt[] = "crosvm_openwrt.log";
 inline constexpr char kLogNameCrosvmOpenWrtBoot[] = "crosvm_openwrt_boot.log";
 inline constexpr char kLogNameKernel[] = "kernel.log";
