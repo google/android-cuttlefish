c1c6298a4a0d366f13681fc5516e126dad7e441f
Jason Macnak <natsu@google.com>
2026-05-18 09:28:25 -0700

    Make --gpu_mode=gfxstream_guest_angle[_host_swiftshader] default
    to start to opt users into host rendering with ANGLE by default.
    
    Bug: b/513229182

--- e2etests/cvd/graphics_detector_tests/main_test.go
+++ e2etests/cvd/graphics_detector_tests/main_test.go
@@ -15,7 +15,6 @@
 package main
 
 import (
-	"strings"
 	"testing"
 
 	"github.com/google/android-cuttlefish/e2etests/cvd/common"
@@ -41,12 +40,19 @@ func TestLaunchingWithAutoEnablesGfxstream(t *testing.T) {
 		t.Fatalf("failed to wait for Cuttlefish device to connect to adb: %w", err)
 	}
 
-	res, err := c.RunCmd("adb", "shell", "getprop", "ro.hardware.egl")
+	gl_driver, err := c.GetSyspropString("ro.hardware.egl")
 	if err != nil {
 		t.Fatalf("failed to get EGL sysprop: %w", err)
 	}
-	output := strings.TrimSpace(res.Stdout)
-	if output != "emulation" {
-		t.Errorf(`"ro.hardware.egl" was "%s"; expected "emulation"`, output)
+	if gl_driver != "angle" {
+		t.Errorf(`"ro.hardware.egl" was "%s"; expected "angle"`, gl_driver)
+	}
+
+	vk_driver, err := c.GetSyspropString("ro.hardware.vulkan")
+	if err != nil {
+		t.Fatalf("failed to get Vulkan sysprop: %w", err)
+	}
+	if vk_driver != "ranchu" {
+		t.Errorf(`"ro.hardware.vulkan" was "%s"; expected "ranchu"`, vk_driver)
 	}
 }
