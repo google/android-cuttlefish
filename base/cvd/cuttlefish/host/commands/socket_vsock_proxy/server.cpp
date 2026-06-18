636891c3eed9267bdeeacf7de3a7dfdd73931b5d
A. Cody Schuffelen <schuffelen@google.com>
2026-06-18 11:15:27 -0700

    Move socket_vsock_proxy to host/commands
    The "common" distinction doesn't apply now that these executables are
    built exclusively for the host.

--- base/cvd/cuttlefish/host/commands/socket_vsock_proxy/server.cpp
+++ base/cvd/cuttlefish/host/commands/socket_vsock_proxy/server.cpp
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "cuttlefish/common/frontend/socket_vsock_proxy/server.h"
+#include "cuttlefish/host/commands/socket_vsock_proxy/server.h"
 
 #include <chrono>
 #include <set>
