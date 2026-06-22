2ee8bdb85f2169d2a2e0821c3f080697085549eb
A. Cody Schuffelen <schuffelen@google.com>
2026-06-22 13:50:33 -0700

    Apply misc-include-cleaner to cuttlefish/common/transport
    Also uses more `const` and less `auto`.
    
    Bug: b/523396865
    Test: bazel test //...

--- base/cvd/cuttlefish/common/libs/transport/channel_sharedfd.cpp
+++ base/cvd/cuttlefish/common/libs/transport/channel_sharedfd.cpp
@@ -17,39 +17,47 @@
 #include "cuttlefish/common/libs/transport/channel_sharedfd.h"
 
 #include <poll.h>
+#include <stddef.h>
+#include <sys/types.h>
 
+#include <utility>
 #include <vector>
 
 #include "absl/log/log.h"
 
 #include "cuttlefish/common/libs/fs/shared_buf.h"
+#include "cuttlefish/common/libs/fs/shared_fd.h"
+#include "cuttlefish/common/libs/transport/channel.h"
+#include "cuttlefish/result/expect.h"
+#include "cuttlefish/result/result_type.h"
 
-namespace cuttlefish {
-namespace transport {
+namespace cuttlefish::transport {
 
 SharedFdChannel::SharedFdChannel(SharedFD input, SharedFD output)
     : input_(std::move(input)), output_(std::move(output)) {}
 
 Result<void> SharedFdChannel::SendRequest(RawMessage& message) {
-  return SendMessage(message, false);
+  CF_EXPECT(SendMessage(message, false));
+  return {};
 }
 
 Result<void> SharedFdChannel::SendResponse(RawMessage& message) {
-  return SendMessage(message, true);
+  CF_EXPECT(SendMessage(message, true));
+  return {};
 }
 
 Result<ManagedMessage> SharedFdChannel::ReceiveMessage() {
   struct RawMessage message_header;
-  auto read = ReadExactBinary(input_, &message_header);
+  ssize_t read = ReadExactBinary(input_, &message_header);
   CF_EXPECT(read == sizeof(RawMessage),
             "Expected " << sizeof(RawMessage) << ", received " << read << "\n"
                         << "Could not read message: " << input_->StrError());
   VLOG(0) << "Received message with id: " << message_header.command;
 
-  auto message = CF_EXPECT(CreateMessage(message_header.command,
-                                         message_header.is_response,
-                                         message_header.payload_size));
-  auto message_bytes = reinterpret_cast<char*>(message->payload);
+  ManagedMessage message = CF_EXPECT(
+      CreateMessage(message_header.command, message_header.is_response,
+                    message_header.payload_size));
+  char* message_bytes = reinterpret_cast<char*>(message->payload);
   read = ReadExact(input_, message_bytes, message->payload_size);
   CF_EXPECT(read == message->payload_size,
             "Could not read message: " << input_->StrError());
@@ -59,25 +67,24 @@ Result<ManagedMessage> SharedFdChannel::ReceiveMessage() {
 
 Result<int> SharedFdChannel::WaitForMessage() {
   std::vector<PollSharedFd> input_poll = {
-      {.fd = input_, .events = POLLIN},
+      {.fd = input_, .events = POLLIN},  // NOLINT(misc-include-cleaner): poll.h
   };
   const int poll_result = SharedFD::Poll(input_poll, -1);
 
-  CF_EXPECT(poll_result >= 0,
-            "Cannot execute poll on input stream to wait for incoming message");
+  CF_EXPECT_GE(poll_result, 0,
+               "Cannot poll on input stream to wait for incoming message");
 
   return poll_result;
 }
 
 Result<void> SharedFdChannel::SendMessage(RawMessage& message, bool response) {
   message.is_response = response;
-  auto write_size = sizeof(RawMessage) + message.payload_size;
-  auto message_bytes = reinterpret_cast<const char*>(&message);
-  auto written = WriteAll(output_, message_bytes, write_size);
-  CF_EXPECT(written == write_size,
-            "Could not write message: " << output_->StrError());
+  const size_t write_size = sizeof(RawMessage) + message.payload_size;
+  const char* message_bytes = reinterpret_cast<const char*>(&message);
+  const size_t written = WriteAll(output_, message_bytes, write_size);
+  CF_EXPECT_EQ(written, write_size,
+               "Could not write message: " << output_->StrError());
   return {};
 }
 
-}  // namespace transport
-}  // namespace cuttlefish
+}  // namespace cuttlefish::transport
