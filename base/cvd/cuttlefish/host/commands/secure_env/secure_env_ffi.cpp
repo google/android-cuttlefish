c946d76ea8a794f70af59b2f1fc9d244014c49a6
Karuna Wadhera <kwadhera@google.com>
2026-06-29 16:50:56 +0000

    secure_env: Move logger to common crate and rename FFI files
    Move the C++ log forwarder to the common crate to enable its use by
    other TAs. Also rename 'tpm_ffi' files to 'secure_env_ffi' to reflect
    they are no longer strictly TPM-specific. This refactoring is done in
    preparation for introducing the Weaver HAL.
    
    Bug: 432285494
    Test: bazel test //... (in base/cvd)

--- base/cvd/cuttlefish/host/commands/secure_env/secure_env_ffi.cpp
+++ base/cvd/cuttlefish/host/commands/secure_env/secure_env_ffi.cpp
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "tpm_ffi.h"
+#include "secure_env_ffi.h"
 
 #include "absl/log/log.h"
 
