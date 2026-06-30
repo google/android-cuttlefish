ed42acb30602608fe0d88a50ead2ffb3743cc5d2
A. Cody Schuffelen <schuffelen@google.com>
2026-06-30 15:45:02 -0700

    Enforce clang-format on assemble_cvd/android_build/vm_manager.*
    Bug: b/512215781

--- base/cvd/cuttlefish/host/commands/assemble_cvd/flags/vm_manager.cc
+++ base/cvd/cuttlefish/host/commands/assemble_cvd/flags/vm_manager.cc
@@ -19,7 +19,7 @@
 #include <vector>
 
 #include "absl/strings/str_split.h"
-#include <gflags/gflags.h>
+#include "gflags/gflags.h"
 
 #include "cuttlefish/host/commands/assemble_cvd/flags_defaults.h"
 #include "cuttlefish/host/commands/assemble_cvd/guest_config.h"
