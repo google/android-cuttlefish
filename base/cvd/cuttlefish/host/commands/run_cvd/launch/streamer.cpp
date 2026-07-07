google/android-cuttlefish

commit 63c95186c65c4d07bdf05e611c8ac4a3a9540a26

Remove the start_webrtc flag behavior
Instead make the behavior dependent on the VMM.  webrtc is required to
run with crosvm, and nonfunctional with gem5. I'm not sure if there is a
dependency on it with qemu, so this leaves it running for qemu.

The flag and config values are left to support some interaction with
python acloud.

https://cs.android.com/android/platform/superproject/main/+/main:tools/acloud/internal/lib/cvd_runtime_config.py;l=149;drc=70921dc34d0c06b760159a62f43258ab2ec7fd06

https://cs.android.com/android/platform/superproject/main/+/main:tools/acloud/create/local_image_local_instance.py;l=631;drc=e1d40e5e18750a85a623b15411bd0f72aa6082e2

The current behavior had some complex default logic that ultimately
amounted to "enable it by default for crosvm and qemu+swiftshader",
unless the user passes a flag value. The new behavior simplifies is it
to "always run for crosvm and qemu".

Bug: b/436356145
Test: launch with crosvm
Test: launch with qemu

base/cvd/cuttlefish/host/commands/run_cvd/launch/streamer.cpp
@@ -268,7 +268,17 @@ class WebRtcServer : public virtual CommandSource,
 
   // SetupFeature
   bool Enabled() const override {
-    return sockets_.Enabled() && instance_.enable_webrtc();
+    if (!sockets_.Enabled()) {
+      return false;
+    }
+    switch (config_.vm_manager()) {
+      case VmmMode::kCrosvm:
+      case VmmMode::kQemu:
+        return true;
+      case VmmMode::kGem5:
+      case VmmMode::kUnknown:
+        return false;
+    }
   }
 
  private:

base/cvd/cuttlefish/host/commands/assemble_cvd/assemble_cvd_flags.cpp
@@ -202,7 +202,7 @@ DEFINE_string(
     "With sandbox'ed crosvm, overrieds the security comp policy directory");
 
 DEFINE_vec(start_webrtc, fmt::format("{}", CF_DEFAULTS_START_WEBRTC),
-           "Whether to start the webrtc process.");
+           "(Deprecated, webrtc is enabled depending on the VMM)");
 
 DEFINE_vec(webrtc_assets_dir, CF_DEFAULTS_WEBRTC_ASSETS_DIR,
            "Path to WebRTC webpage assets.");

base/cvd/cuttlefish/host/commands/assemble_cvd/assemble_cvd_flags.h
@@ -93,8 +93,6 @@ DECLARE_vec(enable_virtiofs);
 
 DECLARE_string(seccomp_policy_dir);
 
-DECLARE_vec(start_webrtc);
-
 DECLARE_vec(webrtc_assets_dir);
 
 DECLARE_bool(start_webrtc_sig_server);

base/cvd/cuttlefish/host/commands/assemble_cvd/flags.cc
@@ -549,8 +549,6 @@ Result<CuttlefishConfig> InitializeCuttlefishConfiguration(
   std::vector<bool> kgdb_vec = CF_EXPECT(GET_FLAG_BOOL_VALUE(kgdb));
   std::vector<std::string> boot_slot_vec =
       CF_EXPECT(GET_FLAG_STR_VALUE(boot_slot));
-  std::vector<bool> start_webrtc_vec = CF_EXPECT(GET_FLAG_BOOL_VALUE(
-      start_webrtc));
   std::vector<std::string> webrtc_assets_dir_vec =
       CF_EXPECT(GET_FLAG_STR_VALUE(webrtc_assets_dir));
   std::vector<std::string> tcp_port_range_vec =
@@ -1179,7 +1177,6 @@ Result<CuttlefishConfig> InitializeCuttlefishConfiguration(
     instance.set_wifi_mac_prefix(5554 + (num - 1));
 
     // streaming, webrtc setup
-    instance.set_enable_webrtc(start_webrtc_vec[instance_index]);
     instance.set_webrtc_assets_dir(webrtc_assets_dir_vec[instance_index]);
 
     auto tcp_range  = ParsePortRange(tcp_port_range_vec[instance_index]);
@@ -1348,10 +1345,7 @@ Result<void> SetDefaultFlagsForQemu(
   int32_t instances_size = instance_nums.size();
   std::vector<std::string> gpu_mode_vec =
       CF_EXPECT(GET_FLAG_STR_VALUE(gpu_mode));
-  std::vector<bool> start_webrtc_vec =
-      CF_EXPECT(GET_FLAG_BOOL_VALUE(start_webrtc));
   std::string default_android_efi_loader = "";
-  std::string default_start_webrtc = "";
 
   for (int instance_index = 0; instance_index < instance_nums.size();
        instance_index++) {
@@ -1360,7 +1354,6 @@ Result<void> SetDefaultFlagsForQemu(
 
     if (instance_index > 0) {
       default_android_efi_loader += ",";
-      default_start_webrtc += ",";
     }
 
     // EFI loader isn't presented in the output folder by default and can be
@@ -1369,22 +1362,7 @@ Result<void> SetDefaultFlagsForQemu(
     if (FileExists(curr_android_efi_loader)) {
       default_android_efi_loader += curr_android_efi_loader;
     }
-    if (gpu_mode_vec[instance_index] == kGpuModeGuestSwiftshader &&
-        !start_webrtc_vec[instance_index]) {
-      // This makes WebRTC the default streamer unless the user requests
-      // another via a --star_<streamer> flag, while at the same time it's
-      // possible to run without any streamer by setting --start_webrtc=false.
-      default_start_webrtc += "true";
-    } else {
-      default_start_webrtc += fmt::format(
-          "{}", static_cast<bool>(start_webrtc_vec[instance_index]));
-    }
   }
-  // This is the 1st place to set "start_webrtc" flag value
-  // for now, we don't set non-default options for QEMU
-  SetCommandLineOptionWithMode("start_webrtc", default_start_webrtc.c_str(),
-                               google::FlagSettingMode::SET_FLAGS_DEFAULT);
-
   SetCommandLineOptionWithMode("android_efi_loader",
                                default_android_efi_loader.c_str(),
                                google::FlagSettingMode::SET_FLAGS_DEFAULT);
@@ -1397,10 +1375,6 @@ Result<void> SetDefaultFlagsForCrosvm(
     std::map<std::string, std::string>& name_to_default_value) {
   auto instance_nums =
       CF_EXPECT(InstanceNumsCalculator().FromGlobalGflags().Calculate());
-  int32_t instances_size = instance_nums.size();
-  std::vector<bool> start_webrtc_vec =
-      CF_EXPECT(GET_FLAG_BOOL_VALUE(start_webrtc));
-  std::string default_start_webrtc = "";
 
   std::set<Arch> supported_archs{Arch::X86_64};
   bool default_enable_sandbox =
@@ -1418,7 +1392,6 @@ Result<void> SetDefaultFlagsForCrosvm(
     if (instance_index > 0) {
       default_android_efi_loader += ",";
       default_enable_sandbox_str += ",";
-      default_start_webrtc += ",";
     }
 
     // EFI loader isn't presented in the output folder by default and can be
@@ -1428,21 +1401,9 @@ Result<void> SetDefaultFlagsForCrosvm(
       default_android_efi_loader += curr_android_efi_loader;
     }
     default_enable_sandbox_str += fmt::format("{}", default_enable_sandbox);
-    if (!start_webrtc_vec[instance_index]) {
-      // This makes WebRTC the default streamer unless the user requests
-      // another via a --star_<streamer> flag, while at the same time it's
-      // possible to run without any streamer by setting --start_webrtc=false.
-      default_start_webrtc += "true";
-    } else {
-      default_start_webrtc += fmt::format(
-          "{}", static_cast<bool>(start_webrtc_vec[instance_index]));
-    }
   }
   SetCommandLineOptionWithMode("android_efi_loader",
                                default_android_efi_loader.c_str(),
-                               google::FlagSettingMode::SET_FLAGS_DEFAULT);
-  // This is the 1st place to set "start_webrtc" flag value
-  SetCommandLineOptionWithMode("start_webrtc", default_start_webrtc.c_str(),
                                google::FlagSettingMode::SET_FLAGS_DEFAULT);
   // This is the 1st place to set "enable_sandbox" flag value
   SetCommandLineOptionWithMode("enable_sandbox",

base/cvd/cuttlefish/host/libs/config/cuttlefish_config.h
@@ -530,7 +530,6 @@ class CuttlefishConfig {
     std::string ril_broadcast() const;
     uint8_t ril_prefixlen() const;
 
-    bool enable_webrtc() const;
     std::string webrtc_assets_dir() const;
 
     // The range of TCP ports available for webrtc sessions.
@@ -750,7 +749,6 @@ class CuttlefishConfig {
     // Kernel and bootloader logging
     void set_enable_kernel_log(bool enable_kernel_log);
 
-    void set_enable_webrtc(bool enable_webrtc);
     void set_webrtc_assets_dir(const std::string& webrtc_assets_dir);
 
     // The range of TCP ports available for webrtc sessions.

base/cvd/cuttlefish/host/libs/config/cuttlefish_config_instance.cpp
@@ -62,6 +62,7 @@ CuttlefishConfig::MutableInstanceSpecific::MutableInstanceSpecific(
     : config_(config), id_(id) {
   // Legacy for acloud
   (*Dictionary())[kInstanceDir] = config_->InstancesPath(IdToName(id));
+  (*Dictionary())["enable_webrtc"] = true;
 }
 
 Json::Value* CuttlefishConfig::MutableInstanceSpecific::Dictionary() {
@@ -1024,14 +1025,6 @@ void CuttlefishConfig::MutableInstanceSpecific::set_enable_jcard_simulator(
 }
 bool CuttlefishConfig::InstanceSpecific::enable_jcard_simulator() const {
   return (*Dictionary())[kEnableJcardSimulator].asBool();
-}
-
-static constexpr char kEnableWebRTC[] = "enable_webrtc";
-void CuttlefishConfig::MutableInstanceSpecific::set_enable_webrtc(bool enable_webrtc) {
-  (*Dictionary())[kEnableWebRTC] = enable_webrtc;
-}
-bool CuttlefishConfig::InstanceSpecific::enable_webrtc() const {
-  return (*Dictionary())[kEnableWebRTC].asBool();
 }
 
 static constexpr char kWebRTCAssetsDir[] = "webrtc_assets_dir";

base/cvd/cuttlefish/host/libs/vm_manager/crosvm_manager.cpp
@@ -640,23 +640,21 @@ Result<std::vector<MonitorCommand>> CrosvmManager::StartCommands(
     disk_i++;
   }
 
-  if (instance.enable_webrtc()) {
-    auto display_configs = instance.display_configs();
-    CF_EXPECT(!display_configs.empty());
+  auto display_configs = instance.display_configs();
+  CF_EXPECT(!display_configs.empty());
 
-    const int display_cnt = instance.display_configs().size();
-    const int touchpad_cnt = instance.touchpad_configs().size();
-    const int total_touch_cnt = display_cnt + touchpad_cnt;
-    for (int touch_idx = 0; touch_idx < total_touch_cnt; ++touch_idx) {
-      crosvm_cmd.AddVhostUser("input", instance.touch_socket_path(touch_idx));
-    }
-    if (instance.enable_mouse()) {
-      crosvm_cmd.AddVhostUser("input", instance.mouse_socket_path());
-    }
-    crosvm_cmd.AddVhostUser("input", instance.rotary_socket_path());
-    crosvm_cmd.AddVhostUser("input", instance.keyboard_socket_path());
-    crosvm_cmd.AddVhostUser("input", instance.switches_socket_path());
+  const int display_cnt = instance.display_configs().size();
+  const int touchpad_cnt = instance.touchpad_configs().size();
+  const int total_touch_cnt = display_cnt + touchpad_cnt;
+  for (int touch_idx = 0; touch_idx < total_touch_cnt; ++touch_idx) {
+    crosvm_cmd.AddVhostUser("input", instance.touch_socket_path(touch_idx));
   }
+  if (instance.enable_mouse()) {
+    crosvm_cmd.AddVhostUser("input", instance.mouse_socket_path());
+  }
+  crosvm_cmd.AddVhostUser("input", instance.rotary_socket_path());
+  crosvm_cmd.AddVhostUser("input", instance.keyboard_socket_path());
+  crosvm_cmd.AddVhostUser("input", instance.switches_socket_path());
 
   // GPU capture can only support named files and not file descriptors due to
   // having to pass arguments to crosvm via a wrapper script.

commit b6342dd38432ba11995d8f98d75b90d6ce0cc8bc

Do not launch WebRtcServer when GpuMode::None
When instance.gpu_mode() is GpuMode::None (headless mode), ScreenConnector
explicitly rejects initialization and aborts process execution with:
LOG(FATAL) << "Invalid gpu mode: none".

Commit 63c95186c ("Remove the start_webrtc flag behavior") inadvertently
caused WebRtcServer to launch even when `start_webrtc` is explicitly set
to `false` on sdv_core targets. To fix this issue, let's skip enabling
WebRtcServer whenever instance_.gpu_mode() == GpuMode::None.

Bug: b/532203075

base/cvd/cuttlefish/host/commands/run_cvd/launch/streamer.cpp
@@ -265,7 +265,7 @@ class WebRtcServer : public virtual CommandSource,
 
   // SetupFeature
   bool Enabled() const override {
-    return sockets_.Enabled() &&
+    return sockets_.Enabled() && instance_.gpu_mode() != GpuMode::None &&
            (VmManagerIsCrosvm(config_) || VmManagerIsQemu(config_));
   }
 