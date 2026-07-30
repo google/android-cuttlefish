67a26342b80fb3555ac0159e16b80463b1d98476
A. Cody Schuffelen <schuffelen@google.com>
2026-07-29 19:29:46 -0700

    misc-include-cleaner for assemble_cvd/boot_image
    Bug: b/523396865

--- base/cvd/cuttlefish/host/commands/assemble_cvd/boot_image/boot_image.cc
+++ base/cvd/cuttlefish/host/commands/assemble_cvd/boot_image/boot_image.cc
@@ -16,9 +16,13 @@
 #include "cuttlefish/host/commands/assemble_cvd/boot_image/boot_image.h"
 
 #include <stdint.h>
+#include <string.h>
 
 #include <map>
+#include <memory>
+#include <optional>
 #include <string>
+#include <string_view>
 #include <utility>
 #include <variant>
 
