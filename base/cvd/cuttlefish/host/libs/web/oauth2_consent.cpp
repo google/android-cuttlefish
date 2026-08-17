39a9d3953c559f761bf726bdd5382b381fb88644
A. Cody Schuffelen <schuffelen@google.com>
2026-08-17 16:26:48 -0700

    Replace UniqueFd with Fd
    This type is redundant now that Fd is movable.
    
    Bug: b/545278508

--- base/cvd/cuttlefish/host/libs/web/oauth2_consent.cpp
+++ base/cvd/cuttlefish/host/libs/web/oauth2_consent.cpp
@@ -36,9 +36,9 @@
 #include "json/value.h"
 #include <zlib.h>
 
+#include "cuttlefish/common/libs/fs/fd.h"
 #include "cuttlefish/common/libs/fs/shared_buf.h"
 #include "cuttlefish/common/libs/fs/shared_fd.h"
-#include "cuttlefish/common/libs/fs/unique_fd.h"
 #include "cuttlefish/common/libs/utils/contains.h"
 #include "cuttlefish/common/libs/utils/files.h"
 #include "cuttlefish/common/libs/utils/json.h"
@@ -85,7 +85,7 @@ class HttpServer {
   uint16_t Port() { return 8888; }
 
   Result<std::string> CodeFromClient() {
-    SharedFD client = UniqueFd::Accept(*server_);
+    SharedFD client = Fd::Accept(*server_);
     CF_EXPECT(client->IsOpen(), client->StrError());
 
     std::stringstream request;
