9d892f41a41f7b488c2088f8797a7815360e1b3b
A. Cody Schuffelen <schuffelen@google.com>
2026-09-16 15:34:05 -0700

    SharedFD -> Fd in MkenvimageSlim
    Bug: b/545278508

--- base/cvd/cuttlefish/host/libs/config/mkenvimage_slim.cc
+++ base/cvd/cuttlefish/host/libs/config/mkenvimage_slim.cc
@@ -24,9 +24,9 @@
 
 #include <zlib.h>
 
-#include "cuttlefish/common/libs/fs/shared_buf.h"
-#include "cuttlefish/common/libs/fs/shared_fd.h"
+#include "cuttlefish/common/libs/fs/fd.h"
 #include "cuttlefish/common/libs/utils/files.h"
+#include "cuttlefish/io/write_exact.h"
 #include "cuttlefish/result/expect.h"
 #include "cuttlefish/result/result_type.h"
 
@@ -39,28 +39,25 @@ static constexpr size_t kNullPadLength = 1;
 
 Result<void> MkenvimageSlim(const std::string& input_path,
                             const std::string& output_path, size_t env_size) {
-  std::string env_readout = ReadFile(std::string(input_path));
+  const std::string env_readout = CF_EXPECT(ReadFileContents(input_path));
   CF_EXPECT_GT(env_readout.length(), 0, "Input env is empty");
-  CF_EXPECT(env_readout.length() <= (env_size - kCrcSize - kNullPadLength),
-            "Input env must fit within env_size specified.");
 
-  std::vector<uint8_t> env_buffer(env_size, kPadValue);
-  uint8_t* env_ptr = env_buffer.data() + kCrcSize;
-  memcpy(env_ptr, env_readout.c_str(), FileSize(std::string(input_path)));
+  CF_EXPECT_LE(env_readout.length(), env_size - kCrcSize - kNullPadLength,
+               "Input env must fit within env_size specified.");
+
+  std::vector<char> env_buffer(env_size, kPadValue);
+  char* const env_ptr = env_buffer.data() + kCrcSize;
+  memcpy(env_ptr, env_readout.c_str(), env_readout.size());
   env_ptr[env_readout.length()] = 0;  // final byte after the env must be NULL
-  uint32_t crc = crc32(0, env_ptr, env_size - kCrcSize);
-  memcpy(env_buffer.data(), &crc, sizeof(uint32_t));
+  const uint32_t crc =
+      crc32(0, reinterpret_cast<uint8_t*>(env_ptr), env_size - kCrcSize);
+  memcpy(env_buffer.data(), &crc, sizeof(crc));
 
-  SharedFD output_fd =  // NOLINTNEXTLINE(misc-include-cleaner)
+  Fd output_fd =  // NOLINTNEXTLINE(misc-include-cleaner)
       CF_EXPECT(Fd::Creat(output_path, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP));
 
-  CF_EXPECTF(output_fd->IsOpen(), "Couldn't open the output file '{}': '{}'",
-             output_path, output_fd->StrError());
-
-  CF_EXPECT_EQ(env_size,
-               WriteAll(output_fd, (char*)env_buffer.data(), env_size),
-               "Couldn't complete write to '"
-                   << output_path << "': " << output_fd->StrError());
+  CF_EXPECTF(WriteExact(output_fd, env_buffer),
+             "Couldn't complete write to '{}'", output_path);
 
   return {};
 }
